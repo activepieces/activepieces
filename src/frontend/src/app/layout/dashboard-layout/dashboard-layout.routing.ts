29801d1c2d35d6419bdfbdbd5116af23f7cd6ef3
AbdulTheActivePiecer <abdulyki@activepieces.com>
2022-12-04 13:58:14 +0200

    fixed dashboard items

--- src/frontend/src/app/layout/dashboard-layout/dashboard-layout.routing.ts
+++ src/frontend/src/app/layout/dashboard-layout/dashboard-layout.routing.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { AccountsComponent } from './pages/accounts/accounts.component';
 import { RunsComponent } from './pages/runs/runs.component';
 import { InstancesComponent } from './pages/instances/instances.component';
 import { CollectionComponent } from './pages/collections/collection-components.component';
@@ -7,9 +6,9 @@ import { ListPiecesResolver } from './resolvers/list-pieces-resolver.service';
 import { ApiKeysComponent } from './pages/api-keys/api-keys.component';
 import { ListInstancesResolver } from './resolvers/list-instances.resolver';
 import { ListInstancesRunResolver } from './resolvers/list-instances-runs.resolver';
-import { ListAccountsResolver } from './resolvers/list-accounts.resolver';
 import { TrialExpiredGuard } from 'src/app/guards/trial-expired.guard';
 import { TrialStatusComponent } from './pages/trial-status/trial-status.component';
+import { EnvironmentsComponent } from './pages/environments/environments.component';
 
 export const DashboardLayoutRouting: Routes = [
 	{
@@ -18,11 +17,10 @@ export const DashboardLayoutRouting: Routes = [
 		children: [
 			{ path: '', pathMatch: 'full', redirectTo: '/flows' },
 			{
-				path: 'accounts',
+				path: 'environments',
 				pathMatch: 'full',
-				component: AccountsComponent,
-				resolve: { accounts: ListAccountsResolver },
-				runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+				component: EnvironmentsComponent,
+				resolve: {},
 			},
 			{
 				path: 'api-keys',
