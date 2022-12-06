13e48d74b919aae26a1008c655d1c8214f836f67
Khaled Mashaly <kmmashaly@gmail.com>
2022-12-06 12:31:46 +0200

    feat: configure docker build for frontend

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
 	production: true,
-	apiDomainUrl: 'api.activepieces.com',
-	apiUrl: 'https://api.activepieces.com',
+	apiDomainUrl: 'localhost:8000',
+	apiUrl: 'http://localhost:8000',
 	jwtTokenName: 'token',
 	stageName: 'prod',
-	redirectUrl: 'https://app.activepieces.com/redirect',
+	redirectUrl: 'http://localhost:8080/redirect',
 	appConnectors: 'https://cdn.activepieces.com/connectors/app_connectors.json',
 	userPropertyNameInLocalStorage: 'currentUser',
 	feature: {
