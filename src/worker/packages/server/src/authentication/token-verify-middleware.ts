95b61f08d3da5194496db1aac5ef0519b7fa5c90
Khaled Mashaly <kmmashaly@gmail.com>
2022-12-23 01:12:11 +0200

    feat(server): create a token verification middleware

---
+++ src/worker/packages/server/src/authentication/token-verify-middleware.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { tokenUtils } from './lib/token-utils';
+
+const ignoredRoutes = new Set([
+    '/v1/authentication/sign-in',
+    '/v1/authentication/sign-up',
+]);
+
+const HEADER_PREFIX = 'Bearer ';
+
+export const tokenVerifyMiddleware = async (request: FastifyRequest, _reply: FastifyReply) => {
+    if (ignoredRoutes.has(request.routerPath)) {
+        return;
+    }
+
+    const token = request.headers.authorization?.substring(HEADER_PREFIX.length);
+    const user = await tokenUtils.decode(token);
+    request.user = user;
+}
