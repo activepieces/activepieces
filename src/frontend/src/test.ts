cb70feab0af9cb0f6f9445b678dccf90f5720575
AbdulTheActivePiecer <abdulyki@activepieces.com>
2022-12-22 17:16:24 +0300

    update to angular 15

--- src/frontend/src/test.ts
+++ src/frontend/src/test.ts
@@ -4,21 +4,5 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-	context(
-		path: string,
-		deep?: boolean,
-		filter?: RegExp
-	): {
-		keys(): string[];
-		<T>(id: string): T;
-	};
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
-
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
