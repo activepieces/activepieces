82161a7c503c9e3db4191c374f476408147c4510
Khaled Mashaly <kmmashaly@gmail.com>
2022-12-22 12:48:02 +0200

    chore: delete user controller

--- src/worker/packages/server/src/controller/user.controller.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance, FastifyPluginOptions } from "fastify"
-
-export const userController = async (fastify: FastifyInstance, options: FastifyPluginOptions) => {
-    fastify.get('/', async (_request, _reply) => {
-        return { hello: 'world' }
-    })
-};
