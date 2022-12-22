6fcabf4e6c381c3a8debb6e61c3c75e5aa366b38
hiasat <moabuaboud@gmail.com>
2022-12-22 20:36:09 +0300

    Updated Component Controller

--- src/worker/packages/components/src/apps/index.ts
+++ src/worker/packages/components/src/apps/index.ts
@@ -1,10 +1,14 @@
-import {slack} from './slack';
 import type {Component} from '../framework/component';
 import { gmail } from './gmail';
 import { facebook } from './facebook';
+import {slack} from "./slack";
 
-export const apps: Component[] = [
+export const components: Component[] = [
 	slack,
 	gmail,
 	facebook
 ];
+
+export const getComponent = (name: string): Component | undefined => {
+	return components.find(f => name.toLowerCase() === f.name.toLowerCase());
+}
--- src/worker/packages/server/src/app/app.ts
+++ src/worker/packages/server/src/app/app.ts
@@ -1,11 +1,25 @@
 import Fastify from 'fastify';
 import { userController } from '../controller/user.controller';
+import {componentsController} from "../controller/components.controller";
+import {ActivepiecesError} from "../helper/activepieces-error";
+import {StatusCodes} from "http-status-codes";
 
 const fastify = Fastify({
     logger: true
 });
 
 fastify.register(userController);
+fastify.register(componentsController);
+
+fastify.setErrorHandler(function (error, request, reply) {
+    if (error instanceof ActivepiecesError) {
+        let apError = error as ActivepiecesError;
+        reply.status(StatusCodes.BAD_REQUEST).send({
+            code: apError.error.code
+        })
+    }
+    reply.status(error.statusCode).send(error)
+})
 
 const start = async () => {
     try {
