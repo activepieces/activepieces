c6af7018e076e2fce59680cc37458302df626b5c
Khaled Mashaly <kmmashaly@gmail.com>
2022-12-25 17:01:38 +0200

    refactor(shared): use esm instead of cjs for nanoid

--- src/worker/packages/shared/src/common/id-generator.ts
+++ src/worker/packages/shared/src/common/id-generator.ts
@@ -1,4 +1,5 @@
-const { customAlphabet } = require('nanoid');
+import { customAlphabet } from "nanoid";
+
 const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
 export type ApId = string;
