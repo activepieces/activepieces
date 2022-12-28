020bd9b62bc945b5f8fa6537fccd58d5ef62bc1f
Khaled Mashaly <kmmashaly@gmail.com>
2022-12-28 13:43:05 +0200

    feat(pieces): export trigger type enum

--- src/worker/packages/pieces/index.ts
+++ src/worker/packages/pieces/index.ts
@@ -1,8 +1,9 @@
 export {Context} from "./src/framework/context";
 
-export {PropsValue} from "./src/framework/property/prop.model";
 export * from "./src/framework/property/prop.model";
 
 export {pieces, getPiece} from './src/apps';
 export {Piece} from "./src/framework/piece";
+
 export {Trigger} from "./src/framework/trigger/trigger"
+export {TriggerType} from "./src/framework/trigger/trigger-type";
