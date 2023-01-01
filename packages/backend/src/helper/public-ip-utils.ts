0275f98aa531f82f4f169b4aa02493bf8cdde653
Khaled Mashaly <kmmashaly@gmail.com>
2023-01-02 01:34:35 +0200

    feat(backend): get public ip address for webhooks

---
+++ packages/backend/src/helper/public-ip-utils.ts
@@ -0,0 +1,26 @@
+import dns from "node:dns/promises";
+
+const GOOGLE_DNS = "216.239.32.10";
+const PUBLIC_IP_ADDRESS_QUERY = "o-o.myaddr.l.google.com";
+
+let ipMetadata: IpMetadata | undefined;
+
+export const getPublicIp = async (): Promise<IpMetadata> => {
+  if (ipMetadata !== undefined) {
+    return ipMetadata;
+  }
+
+  dns.setServers([GOOGLE_DNS]);
+
+  const ipList = await dns.resolve(PUBLIC_IP_ADDRESS_QUERY, "TXT");
+
+  ipMetadata = {
+    ip: ipList[0][0],
+  };
+
+  return ipMetadata;
+};
+
+interface IpMetadata {
+  ip: string;
+}
