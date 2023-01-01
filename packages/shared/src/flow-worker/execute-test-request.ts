0ad0bc9fe27270d5fa0d759315dab52bc7ca25c4
Khaled Mashaly <kmmashaly@gmail.com>
2023-01-01 14:12:05 +0200

    refactor(shared): rename CreateExecutionRequest to CreateFlowRunRequest

--- packages/shared/src/flow-worker/execute-test-request.ts
+++
@@ -1,9 +0,0 @@
-import { Static, Type } from "@sinclair/typebox";
-
-export const ExecuteTestRequest = Type.Object({
-    flowVersionId: Type.String(),
-    collectionVersionId: Type.String({}),
-    payload: Type.Object({})
-});
-
-export type ExecuteTestRequest = Static<typeof ExecuteTestRequest>;
