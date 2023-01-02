e82e5178d3dc8987a3b371fb6a26cc407888fd0e
Khaled Mashaly <kmmashaly@gmail.com>
2023-01-02 21:43:00 +0200

    fix(backend): update redis lock to use redis config from env var

--- packages/backend/src/database/redis-connection.ts
+++ packages/backend/src/database/redis-connection.ts
@@ -4,15 +4,18 @@ import createRedisLock from "redis-lock";
 import { system } from "../helper/system/system";
 import { SystemProp } from "../helper/system/system-prop";
 
-export const redisLockClient = createClient();
+const host = system.getOrThrow(SystemProp.REDIS_HOST);
+const serializedPort = system.getOrThrow(SystemProp.REDIS_PORT);
+const port = Number.parseInt(serializedPort, 10);
+
+export const redisLockClient = createClient({
+  url: `redis://${host}:${port}`,
+});
+
 export const redisLock = createRedisLock(redisLockClient, 5000);
 redisLockClient.on("error", (err: unknown) => console.log("Redis Client Error", err));
 
 export const createRedisClient = (): Redis => {
-  const host = system.getOrThrow(SystemProp.REDIS_HOST);
-  const serializedPort = system.getOrThrow(SystemProp.REDIS_PORT);
-  const port = Number.parseInt(serializedPort, 10);
-
   return new Redis({
     host,
     port,
