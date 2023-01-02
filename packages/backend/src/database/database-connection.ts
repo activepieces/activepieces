1d38c98d70b87367e7b87cbc07d2ea63efad1071
Khaled Mashaly <kmmashaly@gmail.com>
2023-01-02 20:03:25 +0200

    chore(backend): use env to configure postgres

--- packages/backend/src/database/database-connection.ts
+++ packages/backend/src/database/database-connection.ts
@@ -10,14 +10,23 @@ import { StoreEntryEntity } from "../store-entry/store-entry-entity";
 import { InstanceEntity } from "../instance/instance-entity";
 import { FlowRunEntity } from "../flow-run/flow-run-entity";
 import { FlagEntity } from "../flags/flag-entity";
+import { system } from "../helper/system/system";
+import { SystemProp } from "../helper/system/system-prop";
+
+const database = system.getOrThrow(SystemProp.POSTGRES_DATABASE);
+const host = system.getOrThrow(SystemProp.POSTGRES_HOST);
+const password = system.getOrThrow(SystemProp.POSTGRES_PASSWORD);
+const serializedPort = system.getOrThrow(SystemProp.POSTGRES_PORT);
+const port = Number.parseInt(serializedPort, 10);
+const username = system.getOrThrow(SystemProp.POSTGRES_USERNAME);
 
 export const databaseConnection = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 7432,
-  username: "postgres",
-  password: "A79Vm5D4p2VQHOp2gd5",
-  database: "activepieces",
+  host,
+  port,
+  username,
+  password,
+  database,
   synchronize: true,
   entities: [
     CollectionEntity,
