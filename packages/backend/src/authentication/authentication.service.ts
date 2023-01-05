cd7eb8622d449e42b995ff3ce4c3404c1976c267
Khaled Mashaly <kmmashaly@gmail.com>
2023-01-05 18:37:23 +0200

    feat(backend): throw custom error if user already exists

--- packages/backend/src/authentication/authentication.service.ts
+++ packages/backend/src/authentication/authentication.service.ts
@@ -5,29 +5,43 @@ import { tokenUtils } from "./lib/token-utils";
 import { ActivepiecesError, ErrorCode } from "../helper/activepieces-error";
 import { projectService } from "../project/project.service";
 import { FlagId, flagService } from "../flags/flag.service";
+import { QueryFailedError } from "typeorm";
 
 export const authenticationService = {
   signUp: async (request: SignUpRequest): Promise<AuthenticationResponse> => {
-    const user = await userService.create(request);
+    try {
+      const user = await userService.create(request);
 
-    await flagService.save({ id: FlagId.USER_CREATED, value: true });
+      await flagService.save({ id: FlagId.USER_CREATED, value: true });
 
-    await projectService.create({
-      displayName: "Project",
-      ownerId: user.id,
-    });
+      await projectService.create({
+        displayName: "Project",
+        ownerId: user.id,
+      });
 
-    const token = await tokenUtils.encode({
-      id: user.id,
-      type: PrincipalType.USER,
-    });
+      const token = await tokenUtils.encode({
+        id: user.id,
+        type: PrincipalType.USER,
+      });
 
-    const { password, ...userResponse } = user;
+      const { password, ...userResponse } = user;
 
-    return {
-      ...userResponse,
-      token,
-    };
+      return {
+        ...userResponse,
+        token,
+      };
+    } catch (e: unknown) {
+      if (e instanceof QueryFailedError) {
+        throw new ActivepiecesError({
+          code: ErrorCode.EXISTING_USER,
+          params: {
+            email: request.email,
+          },
+        });
+      }
+
+      throw e;
+    }
   },
 
   signIn: async (request: SignInRequest): Promise<AuthenticationResponse> => {
