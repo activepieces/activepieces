756bf4af9a739353b7f35deae6df9d683d9e0e7a
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-01-08 12:30:27 +0300

    loading icon in step type list fix

--- packages/frontend/src/app/modules/flow-builder/page/flow-builder/collection-builder.component.ts
+++ packages/frontend/src/app/modules/flow-builder/page/flow-builder/collection-builder.component.ts
@@ -97,7 +97,6 @@ export class CollectionBuilderComponent implements OnInit, OnDestroy {
 			passive: false,
 			capture: true,
 		});
-		this.store.dispatch(FlowItemDetailsActions.clearFlowItemsDetails());
 		this.store.dispatch(FlowItemDetailsActions.loadFlowItemsDetails());
 	}
 
--- packages/frontend/src/app/modules/flow-builder/store/action/flow-items-details.action.ts
+++ packages/frontend/src/app/modules/flow-builder/store/action/flow-items-details.action.ts
@@ -2,12 +2,10 @@ import { createAction, props } from '@ngrx/store';
 import { FlowItemsDetailsState } from '../model/flow-items-details-state.model';
 
 export enum FlowItemDetailsActionType {
-	CLEAR = '[FLOW_ITEMS_DETAILS] CLEAR',
 	LOAD = '[FLOW_ITEMS_DETAILS] LOAD',
 	LOADED_SUCCESSFULLY = '[FLOW_ITEMS_DETAILS] LOADED_SUCCESSFULLY',
 }
 
-const clearFlowItemsDetails = createAction(FlowItemDetailsActionType.CLEAR);
 const loadFlowItemsDetails = createAction(FlowItemDetailsActionType.LOAD);
 
 const flowItemsDetailsLoadedSuccessfully = createAction(
@@ -16,7 +14,6 @@ const flowItemsDetailsLoadedSuccessfully = createAction(
 );
 
 export const FlowItemDetailsActions = {
-	clearFlowItemsDetails,
 	loadFlowItemsDetails,
 	flowItemsDetailsLoadedSuccessfully,
 };
