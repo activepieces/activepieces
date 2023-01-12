49d86f2dafdfc8d9dee7df92eed32123e53314d9
Mohammed Abu Aboud <moabuaboud@gmail.com>
2023-01-12 12:09:14 +0100

    Update index.ts

--- packages/pieces/src/apps/cloud-vision/index.ts
+++ packages/pieces/src/apps/cloud-vision/index.ts
@@ -4,7 +4,7 @@ import { convertPdfToText } from './actions/get-text-from-pdf';
 export const cloudVisionAI = createPiece({
   name: 'cloudVisionAI',
   displayName: 'Google Cloud Vision AI',
-  logoUrl: 'https://cdn.cdnlogo.com/logos/g/46/google-cloud.png',
+  logoUrl: 'https://cdn.activepieces.com/pieces/googleCloud.png',
   actions: [convertPdfToText],
   triggers: [],
 });
