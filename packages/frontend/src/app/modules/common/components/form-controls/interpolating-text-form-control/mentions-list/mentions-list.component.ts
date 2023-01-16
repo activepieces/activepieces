3fc4690bc9c25c14ce919bbfa25b8221ec1ae171
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-01-16 13:11:10 +0300

    added dropdown title + close button

--- packages/frontend/src/app/modules/common/components/form-controls/interpolating-text-form-control/mentions-list/mentions-list.component.ts
+++ packages/frontend/src/app/modules/common/components/form-controls/interpolating-text-form-control/mentions-list/mentions-list.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { combineLatest, map, Observable, startWith } from 'rxjs';
 import { BuilderSelectors } from 'src/app/modules/flow-builder/store/selector/flow-builder.selector';
+import { InsertMentionOperation } from '../utils';
 
 @Component({
 	selector: 'app-mentions-list',
@@ -12,6 +13,10 @@ import { BuilderSelectors } from 'src/app/modules/flow-builder/store/selector/fl
 export class MentionsListComponent {
 	searchFormControl: FormControl<string> = new FormControl('', { nonNullable: true });
 	dropDownItems$: Observable<{ label: string; value: string }[]>;
+	@Output()
+	addMention: EventEmitter<InsertMentionOperation> = new EventEmitter();
+	@Output()
+	closeMenu: EventEmitter<void> = new EventEmitter();
 	constructor(private store: Store) {
 		const allConfigs$ = this.store.select(BuilderSelectors.selectAllConfigsForMentionsDropdown);
 		const allSteps$ = this.store.select(BuilderSelectors.selectAllStepsForMentionsDropdown);
@@ -22,9 +27,13 @@ export class MentionsListComponent {
 		}).pipe(
 			map(res => {
 				const items = [...res.configs, ...res.steps];
-				console.log(items);
 				return items.filter(item => item.label.toLowerCase().includes(res.search.toLowerCase()));
 			})
 		);
+	}
+	mentionClicked(mention: { label: string; value: string }) {
+		this.addMention.emit({
+			insert: { mention: { serverValue: mention.value, value: mention.label, denotationChar: '' } },
+		});
 	}
 }
