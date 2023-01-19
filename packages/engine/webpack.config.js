82b634b67c418409550f3cd53a1867355812f47c
Khaled Mashaly <kmmashaly@gmail.com>
2023-01-19 14:35:08 +0200

    chore: introduce nx integrated repo

--- packages/engine/webpack.config.js
+++ packages/engine/webpack.config.js
@@ -1,28 +1,63 @@
 const path = require('path');
 const IgnoreDynamicRequire = require('webpack-ignore-dynamic-require');
+const { composePlugins, withNx } = require('@nrwl/webpack');
 
-module.exports = {
-  devtool: false,
-  context: __dirname,
-  plugins: [new IgnoreDynamicRequire()],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: ['/node_modules/', '/coverage/', '/sample/'],
-        use: ['ts-loader'],
-      },
-    ],
-  },
-  entry: path.resolve(__dirname, 'src/index.ts'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'activepieces-engine.js',
-    publicPath: '/dist/',
-  },
-  resolve: {
-    extensions: ['.ts', '.js', 'jsx', 'tsx'],
-  },
-  target: 'node',
-  externals: [path.resolve(__dirname, '/codes/')],
-};
+// Nx plugins for webpack.
+module.exports = composePlugins(withNx(), (config) => {
+  config = {
+    ...config,
+    devtool: false,
+    context: __dirname,
+    plugins: [new IgnoreDynamicRequire()],
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: ['/node_modules/', '/coverage/', '/sample/'],
+          use: ['ts-loader'],
+        },
+      ],
+    },
+    entry: path.resolve(__dirname, 'src/main.ts'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'activepieces-engine.js',
+      publicPath: '/dist/',
+    },
+    // resolve: {
+    //   extensions: ['.ts', '.js', 'jsx', 'tsx'],
+    // },
+    target: 'node',
+    externals: [path.resolve(__dirname, '/codes/')],
+  }
+  return config;
+});
+
+/*
+  config = {
+    ...config,
+    devtool: false,
+    context: __dirname,
+    plugins: [new IgnoreDynamicRequire()],
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: ['/node_modules/', '/coverage/', '/sample/'],
+          use: ['ts-loader'],
+        },
+      ],
+    },
+    entry: path.resolve(__dirname, 'src/main.ts'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'activepieces-engine.js',
+      publicPath: '/dist/',
+    },
+    resolve: {
+      extensions: ['.ts', '.js', 'jsx', 'tsx'],
+    },
+    target: 'node',
+    externals: [path.resolve(__dirname, '/codes/')],
+  }
+  */
---
+++ packages/shared/README.md
@@ -0,0 +1,7 @@
+# shared
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build shared` to build the library.
