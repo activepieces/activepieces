b3c0cbcd74ec8710a7550e407c0c9fc977173430
Mohammed Abu Aboud <moabuaboud@gmail.com>
2023-01-21 02:03:07 +0100

    add pipedrive

--- packages/pieces/src/lib/apps/pipedrive/index.ts
+++ packages/pieces/src/lib/apps/pipedrive/index.ts
@@ -1,10 +1,10 @@
-import {createPiece} from '../../framework/piece';
-import {addPerson} from './actions/add-person.action/add-person.action'
-import {newPerson} from './trigger/new-person'
-import {newDeal} from './trigger/new-deal'
-import {newActivity} from './trigger/new-activity'
-import {updatedPerson} from './trigger/updated-person'
-import {updatedDeal} from './trigger/updated-deal'
+import { createPiece } from '../../framework/piece';
+import { addPerson } from './actions/add-person.action/add-person.action'
+import { newPerson } from './trigger/new-person'
+import { newDeal } from './trigger/new-deal'
+import { newActivity } from './trigger/new-activity'
+import { updatedPerson } from './trigger/updated-person'
+import { updatedDeal } from './trigger/updated-deal'
 
 export const pipedrive = createPiece({
 	name: 'pipedrive',
