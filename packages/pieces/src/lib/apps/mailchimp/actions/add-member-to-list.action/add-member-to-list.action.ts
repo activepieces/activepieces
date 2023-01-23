eac023894920877523fc08b2d9adb873921802f7
Khaled Mashaly <kmmashaly@gmail.com>
2023-01-23 21:34:15 +0200

    refactor(pieces): move mailchimp listId dropdown property to common

--- packages/pieces/src/lib/apps/mailchimp/actions/add-member-to-list.action/add-member-to-list.action.ts
+++ packages/pieces/src/lib/apps/mailchimp/actions/add-member-to-list.action/add-member-to-list.action.ts
@@ -1,6 +1,6 @@
 import {createAction} from "../../../../framework/action/action";
-import { OAuth2PropertyValue, Property } from "../../../../framework/property";
-import {getMailChimpServerPrefix, mailChimpAuth} from "../../common";
+import { Property } from "../../../../framework/property";
+import {getMailChimpServerPrefix, mailChimpAuth, mailChimpListIdDropdown} from "../../common";
 import * as mailchimp from "@mailchimp/mailchimp_marketing";
 
 
@@ -15,32 +15,7 @@ export const addMemberToList = createAction({
             description: 'Email of the new contact',
             required: true,
         }),
-        listId: Property.Dropdown<string>({
-            displayName: "Audience",
-            refreshers: ["authentication"],
-            description: "Audience you want to add the contact to",
-            required: true,
-            options: async (propsValue) => {
-                if (propsValue['authentication'] === undefined) {
-                    return {
-                        disabled: true,
-                        options: [],
-                        placeholder: "Please select an authentication"
-                    }
-                }
-                const authProp: OAuth2PropertyValue = propsValue['authentication'] as OAuth2PropertyValue;
-                const lists =  (await getUserLists(authProp)).lists;
-                return {
-                    disabled: false,
-                    options: lists.map(l => {
-                        return {
-                            label:l.name,
-                            value: l.id
-                        }
-                    })
-                };
-            }
-        }),
+        listId: mailChimpListIdDropdown,
         status: Property.Dropdown<'subscribed' | 'unsubscribed' | 'cleaned' | 'pending' | 'transactional'>({
             displayName: "Status",
             refreshers: [],
@@ -53,10 +28,10 @@ export const addMemberToList = createAction({
                 {label: 'Pending', value: 'pending'},
                 {label:'Transactional',value:'transactional'}
             ]} ;}
-            
+
         })
     },
-    
+
     async run(context) {
         const access_token= context.propsValue.authentication?.access_token;
         const mailChimpServerPrefix = await getMailChimpServerPrefix(access_token!);
@@ -64,24 +39,7 @@ export const addMemberToList = createAction({
             accessToken: access_token,
             server: mailChimpServerPrefix
           });
-        
+
        return await mailchimp.lists.addListMember(context.propsValue.listId!,{email_address:context.propsValue.email!, status:context.propsValue.status!})
     },
 });
-async function getUserLists(authProp: OAuth2PropertyValue): Promise<{lists:MailChimpList[]}> {
-    const access_token= authProp.access_token;
-        const mailChimpServerPrefix = await getMailChimpServerPrefix(access_token!);
-        mailchimp.setConfig({
-            accessToken: access_token,
-            server: mailChimpServerPrefix
-          });
-          console.log(`token:${access_token}`);
-          console.log(`server ${ mailChimpServerPrefix}`);
-          //mailchimp types are not complete this is from the docs.
-          return (await (mailchimp as any).lists.getAllLists({fields:["lists.id","lists.name","total_items"],count:1000}));
-}
-interface MailChimpList
-{
-    id:string;
-    name:string;
-}
