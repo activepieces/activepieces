9eec051551373a4df8927b03bea70ac9f9641a45
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-01-23 14:34:27 +0300

    autocomplete samples are collapsable

--- packages/frontend/src/app/modules/common/components/form-controls/interpolating-text-form-control/builder-autocomplete-mentions-dropdown/builder-autocomplete-mentions-dropdown.component.ts
+++ packages/frontend/src/app/modules/common/components/form-controls/interpolating-text-form-control/builder-autocomplete-mentions-dropdown/builder-autocomplete-mentions-dropdown.component.ts
@@ -7,15 +7,14 @@ import { InsertMentionOperation } from '../utils';
 @Component({
   selector: 'app-builder-autocomplete-mentions-dropdown',
   templateUrl: './builder-autocomplete-mentions-dropdown.component.html',
-  styleUrls: ['./builder-autocomplete-mentions-dropdown.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations:[fadeIn400ms]
 })
 export class BuilderAutocompleteMentionsDropdownComponent {
   @Output() addMention : EventEmitter<InsertMentionOperation>= new EventEmitter();
   @Output() menuClosed:EventEmitter<void> = new EventEmitter();
-  showMenu$:BehaviorSubject<boolean>=new BehaviorSubject(false);
+  editorFocused$:BehaviorSubject<boolean>=new BehaviorSubject(false);
   @Input() width:string="90%";
   @Input() left:string= "unset";
-
+  searchInputIsFocused=false;
  }
