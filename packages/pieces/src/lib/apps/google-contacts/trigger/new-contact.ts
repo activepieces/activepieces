a94fb40c061a9a12c2c69d425e2bf983fa3fed17
abuaboud <moabuaboud@gmail.com>
2023-01-30 14:50:39 +0000

    fix: google contacts ignore deleted

--- packages/pieces/src/lib/apps/google-contacts/trigger/new-contact.ts
+++ packages/pieces/src/lib/apps/google-contacts/trigger/new-contact.ts
@@ -19,16 +19,71 @@ export const googleContactNewOrUpdatedContact = createTrigger({
         authentication: googleContactsCommon.authentication,
     },
     sampleData: {
+        "resourceName": "people/c4278485694217203807",
+        "etag": "%EiMBAgMFBgcICQoLDA0ODxATFBUWGSEiIyQlJicuNDU3PT4/QBoEAQIFByIMZFVwNlJPNEVKUzg9",
+        "metadata": {
+            "sources": [
+                {
+                    "type": "CONTACT",
+                    "id": "3b603c120c68305f",
+                    "etag": "#dUp6RO4EJS8=",
+                    "updateTime": "2023-01-30T14:35:18.142565Z"
+                }
+            ],
+            "objectType": "PERSON"
+        },
+        "names": [
+            {
+                "metadata": {
+                    "primary": true,
+                    "source": {
+                        "type": "CONTACT",
+                        "id": "3b603c120c68305f"
+                    }
+                },
+                "displayName": "Shahed Mashni",
+                "familyName": "Mashni",
+                "givenName": "Shahed",
+                "displayNameLastFirst": "Mashni, Shahed",
+                "unstructuredName": "Shahed Mashni"
+            }
+        ],
+        "photos": [
+            {
+                "metadata": {
+                    "primary": true,
+                    "source": {
+                        "type": "CONTACT",
+                        "id": "3b603c120c68305f"
+                    }
+                },
+                "url": "https://lh3.googleusercontent.com/cm/AAkddurmZojs4vCcxrpkfSxH9tnqcH-hI82ESDnwv6eq86nZeLStcjYEIe_TCx8r8g5Y=s100",
+                "default": true
+            }
+        ],
+        "memberships": [
+            {
+                "metadata": {
+                    "source": {
+                        "type": "CONTACT",
+                        "id": "3b603c120c68305f"
+                    }
+                },
+                "contactGroupMembership": {
+                    "contactGroupId": "myContacts",
+                    "contactGroupResourceName": "contactGroups/myContacts"
+                }
+            }
+        ]
     },
     type: TriggerStrategy.POLLING,
     async onEnable(ctx) {
         const response = await listContacts(getAccessTokenOrThrow(ctx.propsValue['authentication']), undefined);
-        console.log(`Saved sync token ${response.body.nextSyncToken}`);
         await ctx.store?.put("syncToken", response.body.nextSyncToken);
     },
     async onDisable(ctx) { },
     async run(ctx) {
-        let newContacts: unknown[] = [];
+        let newContacts: Connection[] = [];
         let fetchMore = true;
         while (fetchMore) {
             const syncToken: string = (await ctx.store?.get("syncToken"))!;
@@ -43,11 +98,13 @@ export const googleContactNewOrUpdatedContact = createTrigger({
             }
         }
         console.log(`Found ${newContacts.length} new contacts`);
-        return newContacts;
+        return newContacts.filter(f => {
+            return f.metadata.deleted !== true;
+        });
     },
 });
 
-function listContacts(access_token: string, syncToken: string | undefined): Promise<HttpResponse<{ connections: unknown[], nextSyncToken: string }>> {
+function listContacts(access_token: string, syncToken: string | undefined): Promise<HttpResponse<{ connections: Connection[], nextSyncToken: string }>> {
     let qParams: Record<string, string> = {
         personFields: "Addresses,ageRanges,biographies,birthdays,calendarUrls,clientData,coverPhotos,emailAddresses,events,externalIds,genders,imClients,interests,locales,locations,memberships,metadata,miscKeywords,names,nicknames,occupations,organizations,phoneNumbers,photos,relations,sipAddresses,skills,urls,userDefined",
         requestSyncToken: "true"
@@ -58,7 +115,7 @@ function listContacts(access_token: string, syncToken: string | undefined): Prom
             syncToken: syncToken
         }
     }
-    return httpClient.sendRequest<{ connections: unknown[], nextSyncToken: string }>({
+    return httpClient.sendRequest<{ connections: Connection[], nextSyncToken: string }>({
         method: HttpMethod.GET,
         url: googleContactsCommon.baseUrl + "/me/connections",
         authentication: {
@@ -69,3 +126,8 @@ function listContacts(access_token: string, syncToken: string | undefined): Prom
     });
 }
 
+interface Connection {
+    metadata: {
+        deleted?: boolean;
+    }
+}
