dec25de400bf9b5aa737de5b51ea057412533504
Khaled Mashaly <kmmashaly@gmail.com>
2023-01-31 18:42:31 +0200

    fix(http): add send request action #271

--- packages/pieces/src/lib/apps/http/index.ts
+++ packages/pieces/src/lib/apps/http/index.ts
@@ -1,10 +1,12 @@
 import {createPiece} from '../../framework/piece';
+import { httpSendRequestAction } from './actions/send-http-request-action';
 
 export const http = createPiece({
 	name: 'http',
 	displayName: 'HTTP requests',
 	logoUrl: 'https://cdn.activepieces.com/pieces/http.png',
 	actions: [
+    httpSendRequestAction,
   ],
 	triggers: [
   ],
