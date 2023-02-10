a55b911c05ba80088593735cf876cd195638e878
Muhammad Tabaza <muhammadtabaza@gmail.com>
2023-02-10 19:35:53 +0300

    Initial Binance connector

---
+++ packages/pieces/src/lib/apps/binance/index.ts
@@ -0,0 +1,9 @@
+import { createPiece } from '../../framework/piece';
+
+export const binance = createPiece({
+  name: 'binance',
+  displayName: 'Binance',
+  logoUrl: 'https://logolook.net/wp-content/uploads/2022/06/Binance-Emblem.png',
+  actions: [],
+  triggers: [],
+});
