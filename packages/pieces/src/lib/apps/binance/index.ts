d0e6ea7ec7ec95fd7bdfaa6d2ef574ee574fed62
Muhammad Tabaza <muhammadtabaza@gmail.com>
2023-02-11 14:10:11 +0300

    Use logo from AP CDN

--- packages/pieces/src/lib/apps/binance/index.ts
+++ packages/pieces/src/lib/apps/binance/index.ts
@@ -3,7 +3,7 @@ import { createPiece } from '../../framework/piece';
 export const binance = createPiece({
   name: 'binance',
   displayName: 'Binance',
-  logoUrl: 'https://logolook.net/wp-content/uploads/2022/06/Binance-Emblem.png',
+  logoUrl: 'https://cdn.activepieces.com/pieces/binance.png',
   actions: [],
   triggers: [],
 });
