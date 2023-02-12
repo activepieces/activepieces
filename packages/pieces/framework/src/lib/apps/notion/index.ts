3d495179d7724f584bde6593121248acf32605af
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-02-13 00:25:50 +0300

    feat(notion): adds notion app

---
+++ packages/pieces/framework/src/lib/apps/notion/index.ts
@@ -0,0 +1,9 @@
+import { createPiece } from '../../framework/piece';
+
+export const notion = createPiece({
+	name: 'notion',
+	displayName: "Notion",
+	logoUrl: 'https://www.notion.so/cdn-cgi/image/format=auto,width=128,quality=100/front-static/shared/icons/notion-app-icon-3d.png',
+	actions: [],
+	triggers: [],
+});
