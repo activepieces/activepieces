8e8cff315a8007a79f20d66eae310efd19d29037
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-02-16 15:10:38 +0300

    feat(cal-com): clean up

--- packages/pieces/framework/src/lib/apps/cal-com/index.ts
+++ packages/pieces/framework/src/lib/apps/cal-com/index.ts
@@ -2,9 +2,9 @@ import { createPiece } from '../../framework/piece';
 import { triggers } from './triggers';
 
 export const calcom = createPiece({
-	name: 'cal.com',
-	displayName: 'Cal.com',
-	logoUrl: 'https://2465849500-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2Fyln2maO5KoPmX2Iy6dxo%2Flogo%2FA5xFxtmGmYFOArCg7AiX%2Flogo.svg?alt=media&token=cac37928-1817-4217-a343-11b8446e5860',
-	actions: [],
-	triggers,
+  name: 'cal.com',
+  displayName: 'Cal.com',
+  logoUrl: 'https://2465849500-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2Fyln2maO5KoPmX2Iy6dxo%2Flogo%2FA5xFxtmGmYFOArCg7AiX%2Flogo.svg?alt=media&token=cac37928-1817-4217-a343-11b8446e5860',
+  actions: [],
+  triggers,
 });
