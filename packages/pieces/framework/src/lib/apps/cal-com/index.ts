dffbe3a82098be65d6c89ef8d79974b0976d19d9
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-02-16 16:57:15 +0300

    feat(cal-com): changes logo url

--- packages/pieces/framework/src/lib/apps/cal-com/index.ts
+++ packages/pieces/framework/src/lib/apps/cal-com/index.ts
@@ -4,7 +4,7 @@ import { triggers } from './triggers';
 export const calcom = createPiece({
   name: 'cal.com',
   displayName: 'Cal.com',
-  logoUrl: 'https://2465849500-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2Fyln2maO5KoPmX2Iy6dxo%2Flogo%2FA5xFxtmGmYFOArCg7AiX%2Flogo.svg?alt=media&token=cac37928-1817-4217-a343-11b8446e5860',
+  logoUrl: 'https://cal.com/logo.svg',
   actions: [],
   triggers,
 });
