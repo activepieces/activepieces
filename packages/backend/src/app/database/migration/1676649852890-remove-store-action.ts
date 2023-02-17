84292d3bf7515ddea433fbe98b742e348616a37b
abuaboud <moabuaboud@gmail.com>
2023-02-17 16:59:57 +0000

    fix: store migration

--- packages/backend/src/app/database/migration/1676649852890-remove-store-action.ts
+++ packages/backend/src/app/database/migration/1676649852890-remove-store-action.ts
@@ -3,17 +3,21 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class removeStoreAction1676649852890 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        console.log("Running migration removeStoreAction1676649852890");
         const flowVersionRepo = queryRunner.connection.getRepository("flow_version");
         const flowVersions = await flowVersionRepo.find({});
+        let count = 0;
         for (let i = 0; i < flowVersions.length; ++i) {
             const currentFlowVersion = flowVersions[i];
             let action = currentFlowVersion.trigger?.nextAction;
+            let changed = false;
             while (action !== undefined && action !== null) {
-                if (action.type === "SOTRAGE") {
+                if (action.type === "STORAGE") {
                     action.type = "PIECE";
-                    const operation = action.setings.operation === 'GET' ? 'get' : 'put';
+                    const operation = action.settings.operation === 'GET' ? 'get' : 'put';
                     const key = action.settings.key;
                     const value = action.settings.value;
+                    count++;
                     action.settings = {
                         pieceName: "storage",
                         actionName: operation,
@@ -23,13 +27,17 @@ export class removeStoreAction1676649852890 implements MigrationInterface {
                         },
                         inputUiInfo: {}
                     }
+                    changed = true;
                 }
                 action = action.nextAction;
             }
 
-            
-            await flowVersionRepo.update(currentFlowVersion.id, currentFlowVersion);
+            if(changed){
+                await flowVersionRepo.update(currentFlowVersion.id, currentFlowVersion);
+            }
         }
+
+        console.log("Finished running migration removeStoreAction1676649852890, changed " + count + " actions");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -37,6 +45,7 @@ export class removeStoreAction1676649852890 implements MigrationInterface {
         const flowVersions = await flowVersionRepo.find({});
         for (let i = 0; i < flowVersions.length; ++i) {
             const currentFlowVersion = flowVersions[i];
+            let changed = false;
             let action = currentFlowVersion.trigger?.nextAction;
             while (action !== undefined && action !== null) {
                 if (action.type === "PIECE" && action.settings.pieceName === "storage") {
@@ -46,10 +55,13 @@ export class removeStoreAction1676649852890 implements MigrationInterface {
                         key: action.settings.key,
                         value: action.settings.value
                     }
+                    changed = true;
                 }
                 action = action.nextAction;
             }
-            await flowVersionRepo.update(currentFlowVersion.id, currentFlowVersion);
+            if(changed){
+                await flowVersionRepo.update(currentFlowVersion.id, currentFlowVersion);
+            }
         }
     }
 
