ed798400aaac1e6edcba74279e7f1f4aeb280a99
Shay Punter <icodinqs@gmail.com>
2023-02-18 17:02:43 +0000

    feat: added get space and get spaces

---
+++ packages/pieces/apps/src/lib/clickup/actions/get-spaces.ts
@@ -0,0 +1,23 @@
+import { createAction, Property, HttpMethod, getAccessTokenOrThrow } from "@activepieces/framework";
+import { clickupCommon, callClickUpApi } from "../common";
+
+
+export const getClickupSpaces = createAction({
+	name: 'get_spaces',
+	description: 'Gets spaces in a ClickUp workspace',
+	displayName: 'Get Spaces',
+	props: {
+		authentication: clickupCommon.authentication,
+		team_id: Property.ShortText({
+			description: 'The id of the space to get',
+			displayName: 'Team ID',
+			required: true,
+		}),
+	},
+	async run(configValue) {
+		const { team_id, authentication } = configValue.propsValue;
+		return await callClickUpApi(HttpMethod.GET,
+			`team/${team_id}/space`, getAccessTokenOrThrow(authentication), {
+		});
+	},
+});
