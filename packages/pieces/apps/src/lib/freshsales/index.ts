ce4bbcda16c069d3bc9640665572123822bbbb0e
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-02-19 22:30:08 +0300

    chore(freshsales): adds freshsales logo

--- packages/pieces/apps/src/lib/freshsales/index.ts
+++ packages/pieces/apps/src/lib/freshsales/index.ts
@@ -4,7 +4,7 @@ import { freshSalesCreateContact } from "./actions/create-contact";
 export const freshsales = createPiece({
   name: 'freshsales',
   displayName: "Freshsales",
-  logoUrl: 'https://cdn.activepieces.com/pieces/github.png',
+  logoUrl: 'https://logos-download.com/wp-content/uploads/2020/07/Freshsales_Logo-700x149.png',
   authors: ['kanarelo'],
   actions: [freshSalesCreateContact],
   triggers: [],
