b4f9aa03b89a60eb8290bd5306817f1fe12a3b18
abuaboud <moabuaboud@gmail.com>
2023-02-20 22:07:30 +0000

    chore: add shay punter to contributors

--- packages/pieces/apps/src/lib/clickup/index.ts
+++ packages/pieces/apps/src/lib/clickup/index.ts
@@ -18,6 +18,6 @@ export const clickup = createPiece({
         getClickupSpaces,
         getClickupTaskCommants,
     ],
-    authors: ['abuaboud', 'Shay Punter'],
+    authors: ['abuaboud', 'ShayPunter'],
     triggers: [],
 });
