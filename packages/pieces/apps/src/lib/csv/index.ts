81039681cda6202b91d678f8612100e791cce7e5
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-02-24 00:40:52 +0300

    feat(csv): introduces csv file parser

---
+++ packages/pieces/apps/src/lib/csv/index.ts
@@ -0,0 +1,13 @@
+import { createPiece } from "@activepieces/framework";
+import { parseCSVTextAction } from "./actions/parse-csv";
+
+export const csv = createPiece({
+  name: 'csv',
+  displayName: "CSV",
+  //provide logo for CSV
+  logoUrl: 'https://forcetalks.s3.amazonaws.com/wp-content/uploads/2018/05/25081331/csv-logo.png',
+  version: '0.0.0',
+  actions: [parseCSVTextAction],
+  authors: ['kanarelo'],
+  triggers: [],
+});
