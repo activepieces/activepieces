c92c76b9b9ae8bc3ff9d849a3590bcd91398a5a6
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-02-28 08:07:27 +0300

    chore: clean up for merge

--- packages/pieces/apps/src/lib/hubspot/triggers/register-trigger.ts
+++ packages/pieces/apps/src/lib/hubspot/triggers/register-trigger.ts
@@ -1,4 +1,4 @@
-import { createTrigger, TriggerStrategy } from "@activepieces/framework"
+import { createTrigger, Property, TriggerStrategy } from "@activepieces/framework"
 
 interface Props {
   name: string,
@@ -9,29 +9,49 @@ interface Props {
 }
 
 export const hubspotRegisterTrigger = 
-  ({ name, eventType, displayName, description, sampleData}: Props) => createTrigger({
-    name: name,
-    displayName,
-    description,
-    props: {
-      
-    },
-    sampleData,
-    type: TriggerStrategy.WEBHOOK,
-    async onEnable(context) {
-      const { repo, owner } = context.propsValue['repository']!;
-      
-      await context.store.put<WebhookInformation>(`github_${name}_trigger`, {
-        webhookId: webhook.id,
-        owner: owner,
-        repo: repo,
-      });
-    },
-    async onDisable(context) {
-    },
-    async run(context) {
-      console.debug("payload received", context.payload.body)
+  ({ name, eventType, displayName, description, sampleData}: Props) => 
+    createTrigger({
+      name: name,
+      displayName,
+      description,
+      props: {
+        is_active: Property.Checkbox({
+          displayName: 'Folder name',
+          description: 'The name of the new folder',
+          required: true,
+        }),
+        propery_name: Property.DynamicProperties({
+          
+        })
+      },
+      sampleData,
+      type: TriggerStrategy.WEBHOOK,
+      async onEnable(context) {
+        const request: HttpRequest = {
+          method: HttpMethod.POST,
+          url: `https://api.hubapi.com/webhooks/v3/${appId}/subscriptions`,
+          body: {
+            active: true,
+            eventType,
+          },
+          authentication: {
+            type: AuthenticationType.BEARER_TOKEN,
+            token: context.propsValue['authentication']['access_token'],
+          },
+          queryParams: {},
+        };
+        const { body: webhook } = await httpClient.sendRequest<{ id: string }>(request);
+        await context.store.put<WebhookInformation>(`github_${name}_trigger`, {
+          webhookId: webhook.id,
+          owner: owner,
+          repo: repo,
+        });
+      },
+      async onDisable(context) {
+      },
+      async run(context) {
+        console.debug("payload received", context.payload.body)
 
-      return [context.payload.body];
-    }
-  })
+        return [context.payload.body];
+      }
+    })
