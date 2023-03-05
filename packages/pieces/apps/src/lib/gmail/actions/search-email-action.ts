3e2d0cf292eb05a41b174c46fe15bf601bc01cf9
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-03-05 16:03:17 +0300

    feat(gmail): adds search mail action

---
+++ packages/pieces/apps/src/lib/gmail/actions/search-email-action.ts
@@ -0,0 +1,31 @@
+import { createAction } from "@activepieces/framework";
+import { GmailRequests } from "../common/data";
+import { GmailLabel } from "../common/models";
+import { GmailProps } from "../common/props";
+
+export const gmailSearchMail = createAction({
+  name: 'gmail_search_mail',
+  description: 'Search for an email in your Gmail account',
+  displayName: 'Search Email',
+  props: {
+    authentication: GmailProps.authentication,
+    from: GmailProps.from,
+    to: GmailProps.to,
+    subject: GmailProps.subject,
+    label: GmailProps.label,
+    category: GmailProps.category
+  },
+  sampleData: {},
+  async run({ propsValue: { authentication, from, to, subject, label, category } }) {
+    const response = await GmailRequests.searchMail({
+      access_token: (authentication.access_token as string), 
+      from: from as string, 
+      to: to as string, 
+      subject: subject as string, 
+      label: label as GmailLabel, 
+      category: category as string
+    })
+
+    return [response.body]
+  }
+})
