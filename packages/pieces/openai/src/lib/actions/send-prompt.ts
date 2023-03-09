595f40582ce1fce296200551cfaaa89496fc1b51
Alexander Storozhevsky <astorozhevsky@ronasit.com>
2023-03-09 18:55:30 +0100

    feat(openai): add extra properties
    Added more optional inputs: Model, Top P, Frequency penalty, Presence penalty
    
    refs: https://platform.openai.com/docs/api-reference/chat/create

--- packages/pieces/openai/src/lib/actions/send-prompt.ts
+++ packages/pieces/openai/src/lib/actions/send-prompt.ts
@@ -15,7 +15,23 @@ export const askOpenAI = createAction({
       required: true,
       description: 'The question to ask OpenAI.',
     }),
-
+    model: Property.StaticDropdown({
+      displayName: 'Model',
+      required: false,
+      description: 'The model which will generate the completion. Some models are suitable for natural language tasks, others specialize in code.',
+      options: {
+        options: [
+          {
+            label: 'gpt-3.5-turbo',
+            value: 'gpt-3.5-turbo',
+          },
+          {
+            label: 'gpt-3.5-turbo-0301',
+            value: 'gpt-3.5-turbo-0301',
+          },
+        ],
+      },
+    }),
     temperature: Property.Number({
       displayName: 'Temperature',
       required: false,
@@ -26,6 +42,21 @@ export const askOpenAI = createAction({
       required: false,
       description: 'The maximum number of tokens in the generated text.',
     }),
+    topP: Property.Number({
+      displayName: 'Top P',
+      required: false,
+      description: 'Controls diversity via nucleus sampling: 0.5 means half of all likelihood-weighted options are considered.',
+    }),
+    frequencyPenalty: Property.Number({
+      displayName: 'Frequency penalty',
+      required: false,
+      description: 'How much to penalize new tokens based on their existing frequency in the text so far. Decreases the model\'s likelihood to repeat the same line verbatim.',
+    }),
+    presencePenalty: Property.Number({
+      displayName: 'Presence penalty',
+      required: false,
+      description: 'How much to penalize new tokens based on whether they appear in the text so far. Increases the model\'s likelihood to talk about new topics.',
+    }),
   },
   sampleData: {},
   async run({ propsValue }) {
@@ -34,6 +65,10 @@ export const askOpenAI = createAction({
     });
     const openai = new OpenAIApi(configuration);
 
+    let model = 'gpt-3.5-turbo';
+    if (propsValue.model) {
+      model = propsValue.model;
+    }
     let temperature = 0.9;
     if (propsValue.temperature) {
       temperature = propsValue.temperature;
@@ -42,18 +77,30 @@ export const askOpenAI = createAction({
     if (propsValue.maxTokens) {
       maxTokens = propsValue.maxTokens;
     }
+    let topP = 1;
+    if (propsValue.topP) {
+      topP = propsValue.topP;
+    }
+    let frequencyPenalty = 0.0;
+    if (propsValue.frequencyPenalty) {
+      frequencyPenalty = propsValue.frequencyPenalty;
+    }
+    let presencePenalty = 0.6;
+    if (propsValue.presencePenalty) {
+      presencePenalty = propsValue.presencePenalty;
+    }
 
     const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
+      model: model,
       messages: [{
         role: "user",
         content: propsValue['prompt']!
       }],
       temperature: temperature,
       max_tokens: maxTokens,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.6,
+      top_p: topP,
+      frequency_penalty: frequencyPenalty,
+      presence_penalty: presencePenalty,
     });
     return response.data.choices[0].message?.content.trim();
   }
