787971a4760e74fa1923d0228baaea8abc551b47
Khaled Mashaly <kmmashaly@gmail.com>
2023-03-12 17:09:02 +0200

    refactor: remove old pieces endpoint

--- packages/backend/src/app/pieces/pieces.controller.ts
+++ packages/backend/src/app/pieces/pieces.controller.ts
@@ -1,16 +1,11 @@
 import { FastifyPluginAsync, FastifyRequest } from "fastify";
 import { GetPieceRequestParams, GetPieceRequestQuery, PieceOptionRequest } from "@activepieces/shared";
-import { pieces } from "@activepieces/pieces-apps";
 import { collectionVersionService } from "../collections/collection-version/collection-version.service";
 import { collectionService } from "../collections/collection.service";
 import { engineHelper } from "../helper/engine-helper";
 import { pieceMetadataLoader } from "./piece-metadata-loader";
 
 export const piecesController: FastifyPluginAsync = async (app) => {
-    app.get("/v1/pieces", async () => {
-        return pieces.map(p => p.metadata());
-    });
-
     app.post(
         "/v1/pieces/:pieceName/options",
         {
@@ -38,12 +33,12 @@ export const piecesController: FastifyPluginAsync = async (app) => {
         }
     );
 
-    app.get("/v2/pieces", async () => {
+    app.get("/v1/pieces", async () => {
         return await pieceMetadataLoader.manifest();
     });
 
     app.get(
-        "/v2/pieces/:name",
+        "/v1/pieces/:name",
         {
             schema: {
                 params: GetPieceRequestParams,
