4aa533677d4fb499124efef8a78c5f9abdc9f8e5
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-03-15 15:43:30 +0300

    fix: remove empty strings from fields values

--- packages/pieces/airtable/src/lib/actions/create-record.ts
+++ packages/pieces/airtable/src/lib/actions/create-record.ts
@@ -1,4 +1,4 @@
-import { createAction } from "@activepieces/framework";
+import { createAction, DynamicPropsValue } from "@activepieces/framework";
 import { airtableCommon } from "../common";
 
 export const airtableCreateRecord = createAction({
@@ -16,12 +16,18 @@ export const airtableCreateRecord = createAction({
   },
   async run(context) {
     const {authentication: personalToken, base: baseId, tableId, fields} = context.propsValue
-
+    const fieldsWithoutEmptyStrings:DynamicPropsValue = {};
+    Object.keys(fields).forEach(k=>{
+      if(fields[k] !=='')
+      {
+        fieldsWithoutEmptyStrings[k]=fields[k];
+      }
+    })
     return airtableCommon.createRecord({
       personalToken,
       baseId,
       tableId: tableId as string,
-      fields
+      fields:fieldsWithoutEmptyStrings
     })
   }
 })
