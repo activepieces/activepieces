b4f32a86a84c9b37f7aaa301a6525c9c6e5f2474
Khaled Mashaly <kmmashaly@gmail.com>
2023-03-21 17:23:43 +0200

    chore(scripts): add 'dist' package to ignored pacakges for pieces

--- tools/scripts/utils/get-available-piece-names.ts
+++ tools/scripts/utils/get-available-piece-names.ts
@@ -1,7 +1,7 @@
 import { readdir } from 'node:fs/promises'
 
 export const getAvailablePieceNames = async (): Promise<string[]> => {
-  const ignoredPackages = ['framework', 'apps']
+  const ignoredPackages = ['framework', 'apps', 'dist']
   const packageNames = await readdir('packages/pieces')
   return packageNames.filter(p => !ignoredPackages.includes(p))
 }
