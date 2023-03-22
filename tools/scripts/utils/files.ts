be2db514fcefbd6fb84efd4d03a8a360e907522f
Khaled Mashaly <kmmashaly@gmail.com>
2023-03-22 17:07:46 +0200

    chore: use dependencies instead of peerDependencies for pieces

--- tools/scripts/utils/files.ts
+++ tools/scripts/utils/files.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
 
 export type PackageJson = {
   name: string
@@ -7,6 +7,13 @@ export type PackageJson = {
 
 export type ProjectJson = {
   name: string
+  targets?: {
+    build?: {
+      options?: {
+        buildableProjectDepsInPackageJsonType?: 'peerDependencies' | 'dependencies'
+      }
+    }
+  }
 }
 
 const readJsonFile = async <T> (path: string): Promise<T> => {
@@ -16,10 +23,21 @@ const readJsonFile = async <T> (path: string): Promise<T> => {
   return JSON.parse(jsonFile) as T
 }
 
+const writeJsonFile = async (path: string, data: unknown): Promise<void> => {
+  console.info(`[writeJsonFile]`)
+
+  const serializedData = JSON.stringify(data)
+  await writeFile(path, serializedData, { encoding: 'utf-8' })
+}
+
 export const readPackageJson = async (path: string): Promise<PackageJson> => {
   return await readJsonFile(`${path}/package.json`)
 }
 
 export const readProjectJson = async (path: string): Promise<ProjectJson> => {
   return await readJsonFile(`${path}/project.json`)
+}
+
+export const writeProjectJson = async (path: string, projectJson: ProjectJson): Promise<void> => {
+  return await writeJsonFile(`${path}/project.json`, projectJson)
 }
