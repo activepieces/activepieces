a3cd76def44cf127491176cd6b456a39856ce3f2
Khaled Mashaly <kmmashaly@gmail.com>
2023-03-23 14:30:59 +0200

    feat(frontend): filter unsupported pieces

--- packages/frontend/src/app/modules/flow-builder/service/action-meta.service.ts
+++ packages/frontend/src/app/modules/flow-builder/service/action-meta.service.ts
@@ -3,6 +3,7 @@ import { FlowItemDetails } from '../page/flow-builder/flow-right-sidebar/step-ty
 import {
   ActionType,
   ApEdition,
+  compareSemVer,
   PieceMetadata,
   PieceMetadataSummary,
   PieceOptionRequest,
@@ -18,10 +19,17 @@ import { FlagService } from '../../common/service/flag.service';
 
 type TriggersMetadata = Record<string, TriggerBase>;
 
+type FilterUnSupportedPiecesParams = {
+  piecesManifest: PieceMetadataSummary[];
+  release: string;
+};
+
 @Injectable({
   providedIn: 'root',
 })
 export class ActionMetaService {
+  private release$ = this.flagsService.getRelease().pipe(shareReplay(1));
+
   private piecesManifest$ = this.http
     .get<PieceMetadataSummary[]>(`${environment.apiUrl}/pieces`)
     .pipe(shareReplay(1));
@@ -74,6 +82,22 @@ export class ActionMetaService {
 
   constructor(private http: HttpClient, private flagsService: FlagService) {}
 
+  private filterUnSupportedPieces = (params: FilterUnSupportedPiecesParams) => {
+    const { piecesManifest, release } = params;
+
+    return piecesManifest.filter((piece) => {
+      const minRelease = piece.minimumSupportedRelease;
+      const maxRelease = piece.maximumSupportedRelease;
+      if (minRelease && compareSemVer(release, minRelease) === -1) {
+        return false;
+      } else if (maxRelease && compareSemVer(release, maxRelease) === 1) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  };
+
   private getCacheKey(pieceName: string, pieceVersion: string): string {
     return `${pieceName}-${pieceVersion}`;
   }
@@ -105,7 +129,10 @@ export class ActionMetaService {
   }
 
   getPiecesManifest(): Observable<PieceMetadataSummary[]> {
-    return this.piecesManifest$;
+    return forkJoin({
+      piecesManifest: this.piecesManifest$,
+      release: this.release$,
+    }).pipe(map(this.filterUnSupportedPieces), shareReplay(1));
   }
 
   getPieceMetadata(
