ea7ba1eca27793b47167ff34734c7c5563a9b991
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-03-24 14:34:52 +0000

    chore: add migration to bum[ schedule piece to 0.0.2

--- packages/backend/src/app/database/migration/1679667911974-bump-schedule-piece.ts
+++ packages/backend/src/app/database/migration/1679667911974-bump-schedule-piece.ts
@@ -34,7 +34,31 @@ export class bumpSchedulePiece1679667911974 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        // Ignored
+        logger.info("bumpSchedulePiece1679667911974, started");
+
+        let count = 0;
+        const flowVersionRepo = queryRunner.connection.getRepository(FLOW_VERSION_TABLE);
+        const flowVersions = await flowVersionRepo.find();
+
+        for (const flowVersion of flowVersions) {
+            const step = flowVersion.trigger;
+            if (step.type === "PIECE_TRIGGER") {
+                if (step.settings.pieceName === "schedule") {
+                    step.type = "PIECE_TRIGGER";
+                    step.settings = {
+                        input: {
+                            cronExpression: step.settings.cronExpression
+                        },
+                        triggerName: "cron_expression",
+                        pieceName: "schedule",
+                        pieceVersion: "0.0.1",
+                    }
+                    count++;
+                    await flowVersionRepo.update(flowVersion.id, flowVersion);
+                }
+            }
+        }
+        logger.info("bumpSchedulePiece1679667911974, finished flows " + count);
     }
 
 }
