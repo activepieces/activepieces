9a061529f39dd037cd517942581b236a28b90192
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-03-26 17:14:56 +0300

    feat: adds sendinblue action ands

--- packages/pieces/sendinblue/src/index.ts
+++ packages/pieces/sendinblue/src/index.ts
@@ -1,13 +1,18 @@
 
 import { createPiece } from "@activepieces/framework";
 import packageJson from "../package.json";
+import { sendinBlueCreateContact } from "./lib/actions/create-contact";
+import { sendinBlueUpdateContact } from "./lib/actions/update-contact";
 
 export const sendinblue = createPiece({
   name: "sendinblue",
   displayName: "Sendinblue",
-  logoUrl: "https://cdn.activepieces.com/pieces/sendinblue.png",
+  //TODO: Get logos at 
+  // - https://www.pngwing.com/en/free-png-aaryr or
+  // - https://www.pngwing.com/en/free-png-aaryl
+  logoUrl: "https://www.sendinblue.com/wp-content/themes/sendinblue2019/assets/images/common/logo-color.svg",
   version: packageJson.version,
   authors: [],
-  actions: [],
+  actions: [sendinBlueCreateContact, sendinBlueUpdateContact],
   triggers: [],
 });
---
+++ packages/pieces/sendinblue/src/lib/common/props.ts
@@ -0,0 +1,55 @@
+import { Property } from "@activepieces/framework";
+
+export const sendInBlueProps = {
+  api_key: Property.SecretText({
+    displayName: "Project API key",
+    description: "Your project API key",
+    required: true
+  }),
+  email: Property.ShortText({
+    displayName: "Email",
+    description: `Email address of the user. Mandatory if "SMS" field is not passed in "attributes" parameter. Mobile Number in SMS field should be passed with proper country code. For example: {"SMS":"+91xxxxxxxxxx"} or {"SMS":"0091xxxxxxxxxx"}`,
+    required: true
+  }),
+  ext_id: Property.ShortText({
+    displayName: "External ID",
+    description: `Pass your own Id to create a contact.`,
+    required: false
+  }),
+  attributes: Property.Object({
+    displayName: "Attributes",
+    description: `Pass the set of attributes and their values. The attribute's parameter should be passed in capital letter while creating a contact. These attributes must be present in your SendinBlue account. For eg:
+    {"FNAME":"Elly", "LNAME":"Roger"}`,
+    required: false
+  }),
+  email_blacklisted: Property.Checkbox({
+    displayName: "Email Blacklisted?",
+    description: `Set this field to blacklist the contact for emails (emailBlacklisted = true)`,
+    required: false,
+    defaultValue: false
+  }),
+  sms_blacklisted: Property.Checkbox({
+    displayName: "SMS Blacklisted?",
+    description: `Set this field to blacklist the contact for SMS (smsBlacklisted = true)`,
+    required: false,
+    defaultValue: false
+  }),
+  list_ids: Property.Array({
+    displayName: "List IDs",
+    description: `Ids of the lists to add the contact to.`,
+    required: false,
+    defaultValue: []
+  }),
+  update_enabled: Property.Checkbox({
+    displayName: "Update Enabled?",
+    description: `Facilitate to update the existing contact in the same request (updateEnabled = true).`,
+    required: false,
+    defaultValue: false
+  }),
+  smtp_blacklist_sender: Property.Checkbox({
+    displayName: "SMTP Blacklist Sender",
+    description: `transactional email forbidden sender for contact. Use only for email Contact ( only available if updateEnabled = true )`,
+    required: false,
+    defaultValue: false
+  }),
+}
