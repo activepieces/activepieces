c1eb7fa4a2288ccc21acb90ee640e38eb85304f5
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-03-26 20:25:07 +0000

    feat: merge actions into create or update contact

--- packages/pieces/sendinblue/src/index.ts
+++ packages/pieces/sendinblue/src/index.ts
@@ -1,18 +1,14 @@
 
 import { createPiece } from "@activepieces/framework";
 import packageJson from "../package.json";
-import { sendinBlueCreateContact } from "./lib/actions/create-contact";
-import { sendinBlueUpdateContact } from "./lib/actions/update-contact";
+import { createOrUpdateContact } from "./lib/actions/create-or-update-contact";
 
 export const sendinblue = createPiece({
   name: "sendinblue",
   displayName: "Sendinblue",
-  //TODO: Get logos at 
-  // - https://www.pngwing.com/en/free-png-aaryr or
-  // - https://www.pngwing.com/en/free-png-aaryl
-  logoUrl: "https://www.sendinblue.com/wp-content/themes/sendinblue2019/assets/images/common/logo-color.svg",
+  logoUrl: "https://cdn.activepieces.com/pieces/sendinblue.png",
   version: packageJson.version,
-  authors: [],
-  actions: [sendinBlueCreateContact, sendinBlueUpdateContact],
+  authors: ['kanarelo'],
+  actions: [createOrUpdateContact],
   triggers: [],
 });
