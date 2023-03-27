e68a68c97af25de41f30a7d86b02f020f3d7d98b
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-03-28 00:04:47 +0300

    chore(xero): adds logo and actions registered

--- packages/pieces/xero/src/index.ts
+++ packages/pieces/xero/src/index.ts
@@ -1,13 +1,15 @@
 
 import { createPiece } from "@activepieces/framework";
 import packageJson from "../package.json";
+import { xeroCreateContact } from "./lib/actions/create-contact";
+import { xeroCreateInvoice } from "./lib/actions/create-invoice";
 
 export const xero = createPiece({
   name: "xero",
   displayName: "Xero",
-  logoUrl: "https://cdn.activepieces.com/pieces/xero.png",
+  logoUrl: "https://upload.wikimedia.org/wikipedia/en/9/9f/Xero_software_logo.svg",
   version: packageJson.version,
-  authors: [],
-  actions: [],
+  authors: ['kanarelo'],
+  actions: [xeroCreateContact, xeroCreateInvoice],
   triggers: [],
 });
