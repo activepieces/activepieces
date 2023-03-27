b382d34b2682b220ab4dfabe12d39a9cecdf5ceb
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-03-27 20:17:30 +0300

    chore(xero): adds xero app

--- packages/pieces/apps/src/index.ts
+++ packages/pieces/apps/src/index.ts
@@ -45,6 +45,7 @@ import { youtube } from '@activepieces/piece-youtube';
 import { intercom } from '@activepieces/piece-intercom';
 import { trello } from '@activepieces/piece-trello';
 import { square } from '@activepieces/piece-square';
+import { xero } from '@activepieces/piece-xero';
 import { delay } from '@activepieces/piece-delay';
 import { dataMapper } from '@activepieces/piece-data-mapper';
 import { schedule } from '@activepieces/piece-schedule';
@@ -98,7 +99,8 @@ export const pieces: Piece[] = [
     delay,
     dataMapper,
     intercom,
-    schedule
+    schedule,
+    xero
 ].sort((a, b) => a.displayName > b.displayName ? 1 : -1);
 
 export const getPiece = (name: string): Piece | undefined => {
