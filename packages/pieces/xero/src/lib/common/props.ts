167d7ad4b3dba6994370571187c5958f31094689
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-03-28 08:48:17 +0300

    feat(xero): adds a xero props file for input

---
+++ packages/pieces/xero/src/lib/common/props.ts
@@ -0,0 +1,76 @@
+import { AuthenticationType, httpClient, HttpMethod, HttpRequest, OAuth2PropertyValue, Property } from "@activepieces/framework";
+
+export const props = {
+  authentication: Property.OAuth2({
+    description: "",
+    displayName: 'Authentication',
+    authUrl: "https://login.xero.com/identity/connect/authorize",
+    tokenUrl: "https://identity.xero.com/connect/token",
+    required: true,
+    scope: [
+      'accounting.contacts',
+      'accounting.transactions'
+    ]
+  }),
+  tenant_id: Property.Dropdown({
+    displayName: 'Tenant ID',
+    description: 'Tenant ID',
+    refreshers: ['authentication'],
+    required: true,
+    options: async ({ authentication }) => {
+      if (!authentication)
+        return {
+          disabled: true,
+          options: [],
+          placeholder: 'Please authenticate first'
+        }
+
+      const request: HttpRequest = {
+        method: HttpMethod.GET,
+        url: 'https://api.xero.com/connections',
+        authentication: {
+          type: AuthenticationType.BEARER_TOKEN,
+          token: (authentication as OAuth2PropertyValue).access_token
+        }
+      }
+  
+      const result = await httpClient.sendRequest<{
+        id: string,
+        authEventId: string,
+        tenantId: string,
+        tenantType: string,
+        tenantName: string,
+        createdDateUtc: string,
+        updatedDateUtc: string,
+      }[]>(request)
+      
+      console.debug(result)
+
+      if (result.status === 200) {
+        return {
+          disabled: false,
+          options: [{
+            label: `Tenant Id: ${result.body?.[0].tenantId}`, 
+            value: result.body?.[0].tenantId
+          }]
+        }
+      }
+
+      return {
+        disabled: true,
+        options: [],
+        placeholder: "Error processing tenant_id"
+      }
+    }
+  }),
+  invoice_id: Property.ShortText({
+    displayName: "Invoice ID",
+    description: "ID of the invoice to update",
+    required: false
+  }),
+  contact_id: (required=false) => Property.ShortText({
+    displayName: "Contact ID",
+    description: "ID of the contact to create invoice for.",
+    required: required
+  })
+}
