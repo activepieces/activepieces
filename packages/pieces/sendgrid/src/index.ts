20292df9c704db37cf768a0447141202a99ed5e8
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-03-31 22:46:00 +0000

    chore: update sendgrid piece

--- packages/pieces/sendgrid/src/index.ts
+++ packages/pieces/sendgrid/src/index.ts
@@ -1,13 +1,14 @@
 import packageJson from '../package.json';
 import { createPiece } from '@activepieces/framework';
 import { sendEmail } from './lib/actions/send-email';
+import { sendDynamicTemplate } from './lib/actions/send-dynamic-template';
 
 export const sendgrid = createPiece({
 	name: 'sendgrid',
 	displayName: "SendGrid",
 	logoUrl: 'https://cdn.activepieces.com/pieces/sendgrid.png',
-  version: packageJson.version,
-	authors: ['ashrafsamhouri'],
-	actions: [sendEmail],
+	version: packageJson.version,
+	authors: ['ashrafsamhouri', "abuaboud"],
+	actions: [sendEmail, sendDynamicTemplate],
 	triggers: [],
 });
