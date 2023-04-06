483c11a6be50d32f4f516a5bb30b7a659cd84a1c
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-04-06 12:33:58 +0300

    fix(activecampaign): cleans up active campaign. Tests, works

--- packages/pieces/activecampaign/src/index.ts
+++ packages/pieces/activecampaign/src/index.ts
@@ -5,8 +5,8 @@ import { activeCampaignTriggers } from "./lib/triggers"
 import packageJson from "../package.json"
 
 export const activecampaign = createPiece({
-  name: "activecampaign",
-  displayName: "Activecampaign",
+  name: "activeampaign",
+  displayName: "ActiveCampaign",
   logoUrl: "https://www.activecampaign.com/themes/v2/images/favicons/favicon.svg",
   version: packageJson.version,
   authors: [],
