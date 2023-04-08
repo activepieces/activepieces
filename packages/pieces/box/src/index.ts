fea203fead82e40210c5ed41a1a5f7aa7da4c488
abuaboud <moabuaboud@gmail.com>
2023-04-08 00:14:49 +0000

    chore: replace box logo

--- packages/pieces/box/src/index.ts
+++ packages/pieces/box/src/index.ts
@@ -6,9 +6,9 @@ import { boxTriggers } from "./lib/triggers";
 export const box = createPiece({
   name: "box",
   displayName: "Box",
-  logoUrl: "https://upload.wikimedia.org/wikipedia/commons/5/57/Box%2C_Inc._logo.svg",
+  logoUrl: "https://cdn.activepieces.com/pieces/box.png",
   version: packageJson.version,
-  authors: [],
+  authors: ["mukewa"],
   actions: [],
   triggers: boxTriggers,
 });
