39cc760de09a52a83268f85e324dea75de2dcba7
abuaboud <moabuaboud@gmail.com>
2023-04-10 07:05:08 +0000

    fix: currentSelectData should be optional

--- packages/shared/src/lib/flows/sample-data.ts
+++ packages/shared/src/lib/flows/sample-data.ts
@@ -1,5 +1,5 @@
 import { Type, Static } from '@sinclair/typebox';
-export const SampleDataSettingsObject =Type.Object({
-    currentSelectedData:Type.Unknown(),
-  });
-  export type SampleDataSettings = Static<typeof SampleDataSettingsObject>;
+export const SampleDataSettingsObject = Type.Object({
+  currentSelectedData: Type.Optional(Type.Unknown()),
+});
+export type SampleDataSettings = Static<typeof SampleDataSettingsObject>;
