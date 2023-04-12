d1d749f4c612e185911aff57a08a0bbdb1d697cc
abuaboud <moabuaboud@gmail.com>
2023-04-12 22:37:23 +0000

    fix: export test webhook component

--- packages/ui/feature-builder-test-steps/src/index.ts
+++ packages/ui/feature-builder-test-steps/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/test-piece-webhook-trigger/test-piece-webhook-trigger.component';
 export * from './lib/ui-feature-builder-test-steps.module';
 export * from './lib/test-steps-core.component';
 export * from './lib/test-webhook-trigger/test-webhook-trigger.component';
