45dd272197418ea365041a4577e5f2bfc39b8c29
Khaled Mashaly <kmmashaly@gmail.com>
2023-04-12 17:17:19 +0200

    chore: setup turbowatch to build pieces in development

---
+++ turbowatch.ts
@@ -0,0 +1,35 @@
+import { defineConfig, type ChangeEvent } from 'turbowatch'
+
+export default defineConfig({
+    project: `${__dirname}/packages/pieces`,
+    triggers: [
+        {
+            expression: ['match', '*.ts', 'basename'],
+            name: 'build-pieces',
+            initialRun: true,
+            interruptible: false,
+            persistent: false,
+            onChange: async ({ spawn, first, files } :ChangeEvent) => {
+                if (first) {
+                    await spawn`nx run-many -t build --projects=pieces-* --parallel=10`
+                    return
+                }
+
+                const projects = files
+                    .map(file => {
+                        const fileNameRegex = /^.+pieces\/(?<pieceName>.+)\/src.+$/
+                        const matchResult = file.name.match(fileNameRegex)
+                        const pieceName = matchResult?.groups?.pieceName
+                        return `piece-${pieceName}`
+                    })
+                    .filter(Boolean)
+                    .join(',')
+
+                await spawn`nx run-many -t build --projects=${projects}`
+            },
+        },
+    ],
+    debounce: {
+        wait: 0,
+    },
+})
