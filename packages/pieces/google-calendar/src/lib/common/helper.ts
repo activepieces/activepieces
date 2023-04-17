12f6a5270027f32a3568628347c378b7fe5fc5aa
abuaboud <moabuaboud@gmail.com>
2023-04-17 10:51:21 +0000

    fix(google-calendar): typo & calendar list

--- packages/pieces/google-calendar/src/lib/common/helper.ts
+++ packages/pieces/google-calendar/src/lib/common/helper.ts
@@ -48,12 +48,20 @@ export async function watchEvent(
 }
 
 export async function getCalendars(
-  authProp: OAuth2PropertyValue
+  authProp: OAuth2PropertyValue,
+  minAccessRole?: "writer"
 ): Promise<CalendarObject[]> {
   // docs: https://developers.google.com/calendar/api/v3/reference/calendarList/list
+  const queryParams: Record<string, string> = {
+    showDeleted: "false"
+  }
+  if (minAccessRole) {
+    queryParams['minAccessRole'] = minAccessRole;
+  }
   const request: HttpRequest = {
     method: HttpMethod.GET,
     url: `${googleCalendarCommon.baseUrl}/users/me/calendarList`,
+    queryParams: queryParams,
     authentication: {
       type: AuthenticationType.BEARER_TOKEN,
       token: authProp.access_token,
