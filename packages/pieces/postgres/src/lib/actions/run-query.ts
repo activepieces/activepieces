a6e4aa3c9db166417bff99e468bb26ac580b1ec9
Willian <willian.wg70@gmail.com>
2023-04-19 13:31:50 -0300

    feat: add postgres error return

--- packages/pieces/postgres/src/lib/actions/run-query.ts
+++ packages/pieces/postgres/src/lib/actions/run-query.ts
@@ -6,25 +6,31 @@ export const runQuery = createAction({
     displayName: "Run Query",
     description: "Run Query",
     props: {
-        host: Property.SecretText({
-            displayName: 'Host',
-            required: true,
-        }),
-        port: Property.Number({
-            displayName: 'Port',
-            required: true,
-        }),
-        user: Property.ShortText({
-            displayName: 'User',
-            required: true,
-        }),
-        password: Property.SecretText({
-            displayName: 'Password',
-            required: true,
-        }),
-        database: Property.ShortText({
-            displayName: 'Database',
-            required: true,
+        authentication: Property.CustomAuth({
+            displayName: "Authentication",
+            props: {
+                host: Property.ShortText({
+                    displayName: 'Host',
+                    required: true,
+                }),
+                port: Property.ShortText({
+                    displayName: 'Port',
+                    required: true,
+                }),
+                user: Property.ShortText({
+                    displayName: 'User',
+                    required: true,
+                }),
+                password: Property.SecretText({
+                    displayName: 'Password',
+                    required: true,
+                }),
+                database: Property.ShortText({
+                    displayName: 'Database',
+                    required: true,
+                })
+            },
+            required: true
         }),
         query: Property.ShortText({
             displayName: 'Query',
@@ -34,27 +40,56 @@ export const runQuery = createAction({
             displayName: 'Reject Unauthorized',
             required: true,
             defaultValue: true
-        })
+        }),
+        keepAlive: Property.Checkbox({
+            displayName: 'Reject Unauthorized',
+            required: false,
+        }),
+        query_timeout: Property.Number({
+            displayName: 'Query Timeout',
+            required: false,
+        }),
+        statement_timeout: Property.Number({
+            displayName: 'Statement Timeout',
+            required: false,
+        }),
+        connectionTimeoutMillis: Property.Number({
+            displayName: 'Connection Timeout (ms)',
+            required: false,
+        }),
+        application_name: Property.ShortText({
+            displayName: 'Application Name',
+            required: false,
+        }),
     },
     async run(context) {
-        const { host, user, database, password, port, query, rejectUnauthorized } = context.propsValue;
+        const { host, user, database, password, port } = context.propsValue.authentication;
+        const { query, rejectUnauthorized, keepAlive, query_timeout, statement_timeout, application_name, connectionTimeoutMillis } = context.propsValue;
 
         const client = new pg.Client({
             host,
-            port,
+            port: Number(port),
             user,
             password,
             database,
             ssl: { rejectUnauthorized },
+            keepAlive,
+            query_timeout,
+            statement_timeout,
+            application_name,
+            connectionTimeoutMillis,
         })
         await client.connect();
 
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             client.query(query, function (error, results) {
+                if (error) {
+                    client.end();
+                    return reject(error);
+                }
                 resolve(results.rows);
                 client.end();
             });
         })
-
     },
 });
