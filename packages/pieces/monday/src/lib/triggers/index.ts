d81f9769e38800e5a0fb2806965dc1ecabcbd73d
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-04-21 01:14:25 +0300

    chore: fixes sample data

--- packages/pieces/monday/src/lib/triggers/index.ts
+++ packages/pieces/monday/src/lib/triggers/index.ts
@@ -11,23 +11,8 @@ export const mondayTriggers = [
     description: 'Triggered when a new item is created',
     polling: itemPolling,
     sampleData: {
-      "event": {
-        "userId": 9603417,
-        "originalTriggerUuid": null,
-        "boardId": 1771812698,
-        "pulseId": 1772099344,
-        "pulseName": "Create_item webhook",
-        "groupId": "topics",
-        "groupName": "Group Title",
-        "groupColor": "#579bfc",
-        "isTopGroup": true,
-        "columnValues": {},
-        "app": "monday",
-        "type": "create_pulse",
-        "triggerTime": "2021-10-11T09:07:28.210Z",
-        "subscriptionId": 73759690,
-        "triggerUuid": "b5ed2e17c530f43668de130142445cba"
-      }
+     "id": "22222",
+     "name": "Project"
     }
   },
   {
@@ -39,26 +24,8 @@ export const mondayTriggers = [
       item_id: mondayProps.item_id(false)
     },
     sampleData: {
-      "event": {
-        "userId": 9603417,
-        "originalTriggerUuid": null,
-        "boardId": 1772135370,
-        "pulseId": 1772139123,
-        "itemId": 1772139123,
-        "pulseName": "sub-item",
-        "groupId": "topics",
-        "groupName": "Subitems",
-        "groupColor": "#579bfc",
-        "isTopGroup": true,
-        "columnValues": {},
-        "app": "monday",
-        "type": "create_pulse",
-        "triggerTime": "2021-10-11T09:24:51.835Z",
-        "subscriptionId": 73761697,
-        "triggerUuid": "5c28578c66653a87b00a80aa4f7a6ce3",
-        "parentItemId": "1771812716",
-        "parentItemBoardId": "1771812698"
-      }
+      "id": "22222",
+      "name": "Project"
     }
   }
 ].map(trigger => createTrigger({
