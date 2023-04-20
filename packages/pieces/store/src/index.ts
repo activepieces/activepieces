0d2d320ea62333cccdb836b57267069ac84ccf08
abuaboud <moabuaboud@gmail.com>
2023-04-20 18:58:48 +0000

    chore: remove minimum verison

--- packages/pieces/store/src/index.ts
+++ packages/pieces/store/src/index.ts
@@ -12,7 +12,6 @@ export const storage = createPiece({
     description: "Store or retrieve data from activepieces key/value database",
     logoUrl: 'https://cdn.activepieces.com/pieces/store.png',
     version: packageJson.version,
-    minimumSupportedRelease: '0.3.11',
     actions: [storageGetAction, storagePutAction, storageRemoveValue, storageAddtoList, storageRemoveFromList],
     triggers: [],
 });
