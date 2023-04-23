f9fd20e202f4652836976e3bab54c6c2c1abbd70
abuaboud <moabuaboud@gmail.com>
2023-04-23 13:35:51 +0000

    chore(project): fix strict project compilation

--- packages/backend/src/app/project/project.service.ts
+++ packages/backend/src/app/project/project.service.ts
@@ -2,21 +2,25 @@ import { databaseConnection } from '../database/database-connection'
 import { ProjectEntity } from './project.entity'
 import { apId, NotificationStatus, Project, ProjectId, UpdateProjectRequest, UserId } from '@activepieces/shared'
 
-const projectRepo = databaseConnection.getRepository(ProjectEntity)
+const projectRepo = databaseConnection.getRepository<Project>(ProjectEntity)
 
 export const projectService = {
     async create(request: { ownerId: UserId, displayName: string }): Promise<Project> {
         return await projectRepo.save({ id: apId(), ...request, notifications: NotificationStatus.ALWAYS })
     },
-    async update(projectId: ProjectId, request: UpdateProjectRequest): Promise<Project> {
+    async update(projectId: ProjectId, request: UpdateProjectRequest): Promise<Project | null> {
         const project = await projectRepo.findOneBy({
             id: projectId,
         })
+        // This shouldn't happen because we get the project from JWT token.
+        if (!project) {
+            throw new Error(`Project with id ${projectId} not found`)
+        }
         await projectRepo.update(projectId, {
             ...project,
             ...request,
-        })
-        return await projectRepo.findOneBy({
+        });
+        return projectRepo.findOneBy({
             id: projectId,
         })
     },
