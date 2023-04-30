94dd94ad9272719de608d0b6ddd867442459f8c2
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-04-30 16:12:11 +0000

    chore: remove checks for execution mode

--- packages/backend/src/app/helper/system/system.ts
+++ packages/backend/src/app/helper/system/system.ts
@@ -1,7 +1,5 @@
 import { ActivepiecesError, ErrorCode } from '@activepieces/shared'
 import { SystemProp } from './system-prop'
-import fs from 'fs'
-
 
 export const system = {
     get(prop: SystemProp): string | undefined {
@@ -64,38 +62,8 @@ export const validateEnvPropsOnStartup = () => {
             },
         }, `System property AP_${SystemProp.ENCRYPTION_KEY} must be 256 bit (32 hex charaters)`)
     }
-
-    const executionMode = system.getOrThrow(SystemProp.EXECUTION_MODE)
-    const signedUpEnabled = system.getBoolean(SystemProp.SIGN_UP_ENABLED) ?? false
-    if (executionMode === ExecutionMode.SANDBOXED && !hasPrivilegedAccess()) {
-        throw new ActivepiecesError({
-            code: ErrorCode.SYSTEM_PROP_INVALID,
-            params: {
-                prop: SystemProp.EXECUTION_MODE,
-            },
-        }, `The docker container must be run with the --privileged flag to be able to sandbox the execution of the user code, or change the value of AP_${SystemProp.EXECUTION_MODE}, please check the documentation`)
-    }
-    if(executionMode === ExecutionMode.UNSANDBOXED && signedUpEnabled){
-        throw new ActivepiecesError({
-            code: ErrorCode.SYSTEM_PROP_INVALID,
-            params: {
-                prop: SystemProp.EXECUTION_MODE,
-            },
-        }, `Allowing users to sign up is not allowed in unsandboxed mode, please change the value of AP_${SystemProp.EXECUTION_MODE}, please check the documentation`)
-    }
 }
 
-function hasPrivilegedAccess() {
-    const data = fs.readFileSync('/proc/self/status', 'utf8')
-    const lines = data.split('\n')
-    for (const line of lines) {
-        if (line.startsWith('CapEff:')) {
-            const value = line.split('\t')[1]
-            return value === '0000003fffffffff'
-        }
-    }
-    return false
-}
 
 
 export enum ExecutionMode {
