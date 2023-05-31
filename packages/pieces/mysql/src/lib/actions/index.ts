d4d0467000b9aa886a4419037bb94c3ad618a2a0
JanHolger <jan@bebendorf.eu>
2023-05-31 02:28:58 +0000

    chore(mysql): ux improvements

--- packages/pieces/mysql/src/lib/actions/index.ts
+++ packages/pieces/mysql/src/lib/actions/index.ts
@@ -1,9 +1,9 @@
 import executeQuery from "./execute-query";
 import getTables from "./get-tables";
 import insertRow from "./insert-row";
-import updateRows from "./update-rows";
-import deleteRows from "./delete-rows";
-import selectRows from "./select-rows";
+import updateRows from "./update-row";
+import deleteRows from "./delete-row";
+import selectRows from "./find-rows";
 
 export default [
     selectRows,
