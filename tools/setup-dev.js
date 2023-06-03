5e821a9b07d5dc418f3571f111ab6eb06df0f062
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-06-03 14:15:27 +0200

    feat: auto detect node path

--- tools/setup-dev.js
+++ tools/setup-dev.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const path = require('path');
+
+  // Modify packages/backend/.env key AP_NODE_EXECUTABLE_PATH with absolute path to node executable
+  try {
+    const nodeExecutablePath = execSync('which node').toString().trim();
+    const absoluteNodeExecutablePath = path.resolve(nodeExecutablePath);
+    console.info(`Found path to node executable: ${absoluteNodeExecutablePath}`)
+
+    execSync(`sed -i 's#AP_NODE_EXECUTABLE_PATH=.*#AP_NODE_EXECUTABLE_PATH=${absoluteNodeExecutablePath}#' packages/backend/.env`);
+  } catch (error) {
+    console.error('Error: Could not find path to node executable.');
+    process.exit(1);
+  }
 
 // Check Node.js version
 const nodeVersion = execSync('node --version').toString().trim();
@@ -22,3 +35,6 @@ execSync('npx pnpm store add \
   typescript@4.8.4 \
   webpack@5.74.0 \
   webpack-cli@4.10.0');
+
+
+
