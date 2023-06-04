d9a41d1b7f24fac0928df769ada1efe158b90d84
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-06-05 00:54:49 +0200

    docs(spotify): add authentication docs

--- packages/pieces/spotify/src/lib/common/index.ts
+++ packages/pieces/spotify/src/lib/common/index.ts
@@ -2,10 +2,22 @@ import { getAccessTokenOrThrow } from '@activepieces/pieces-common'
 import { Property, StaticPropsValue } from '@activepieces/pieces-framework'
 import { SpotifyWebApi } from './client'
 
+
+const markdownDescription = `
+To obtain a client ID and client secret for Spotify, follow these simple steps:
+
+1. Go to the [Spotify Developer Dashboard](https://developer.spotify.com/).
+2. **Log in** to your Spotify account.
+3. Click on the **"Create an App"** button.
+4. Fill in the required information, such as the **App Name** and **App Description**.
+5. Once your app is created, you will see the **client ID** and **client secret** on the app's dashboard.
+6. **Copy** the client ID and client secret and **paste** them below.
+`
+
 export const spotifyCommon = {
     authentication: Property.OAuth2({
         displayName: 'Authentication',
-        description: 'Authenticate with Spotify',
+        description: markdownDescription,
         required: true,
         authUrl: "https://accounts.spotify.com/authorize",
         tokenUrl: "https://accounts.spotify.com/api/token",
