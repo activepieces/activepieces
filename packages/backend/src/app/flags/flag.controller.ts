83948c0c536a1bbe3d68447d75a3c6402678c7ea
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-06-06 11:27:25 +0300

    chore: add env flag for templates source url

--- packages/backend/src/app/flags/flag.controller.ts
+++ packages/backend/src/app/flags/flag.controller.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { flagService } from './flag.service'
+import { logger } from '../helper/logger'
 
 export const flagController = async (app: FastifyInstance) => {
     app.get('/', async (_request: FastifyRequest, reply: FastifyReply) => {
         const flags = await flagService.getAll()
+        logger.debug('flags: ------')
+        logger.debug(JSON.stringify(flags, null, 2))
         const flagMap: Record<string, unknown> = {}
         flags.forEach(flag => {
             flagMap[flag.id as string] = flag.value
