3aa28166a5497a7d6203becc5e784d1622e6fbf5
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-06-07 23:17:25 +0300

    chore: remove logger from flag controller

--- packages/backend/src/app/flags/flag.controller.ts
+++ packages/backend/src/app/flags/flag.controller.ts
@@ -1,11 +1,9 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { flagService } from './flag.service'
-import { logger } from '../helper/logger'
 
 export const flagController = async (app: FastifyInstance) => {
     app.get('/', async (_request: FastifyRequest, reply: FastifyReply) => {
         const flags = await flagService.getAll()
-        logger.debug(JSON.stringify(flags, null, 2))
         const flagMap: Record<string, unknown> = {}
         flags.forEach(flag => {
             flagMap[flag.id as string] = flag.value
