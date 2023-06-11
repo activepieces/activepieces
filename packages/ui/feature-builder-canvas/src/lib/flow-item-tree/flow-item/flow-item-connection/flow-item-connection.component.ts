676e83d2ec4d13bd3c7cd5a62973288e7a9c4e89
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-06-11 14:27:52 +0300

    chore: change viewMode name to readOnly

--- packages/ui/feature-builder-canvas/src/lib/flow-item-tree/flow-item/flow-item-connection/flow-item-connection.component.ts
+++ packages/ui/feature-builder-canvas/src/lib/flow-item-tree/flow-item/flow-item-connection/flow-item-connection.component.ts
@@ -14,7 +14,7 @@ import { FlowItem } from '@activepieces/ui/feature-builder-store';
 })
 export class FlowItemConnectionComponent {
   @Input() flowItem: FlowItem;
-  @Input() viewMode: boolean;
+  @Input() readOnly: boolean;
   @Input() insideLoopOrBranch = false;
 
   get ActionType() {
