8e681ddf774054cab646cebb1a1e67b70bc77438
Herman Kudria <h.kudrya@hotmail.com>
2023-06-12 14:22:50 +0200

    feat: init action for Bloomerang

---
+++ packages/pieces/bloomerang/src/index.ts
@@ -0,0 +1,10 @@
+
+import { createPiece } from "@activepieces/pieces-framework";
+
+export const bloomerang = createPiece({
+  displayName: "Bloomerang",
+  logoUrl: "https://cdn.activepieces.com/pieces/bloomerang.png",
+  authors: [],
+  actions: [],
+  triggers: [],
+});
