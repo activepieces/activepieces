08b4dec9b9d9d56161061b1004402744353053c6
Mohammed Abu Aboud <moabuaboud@gmail.com>
2023-06-12 08:44:47 +0200

    chore: update author in certopus & remove name, version

--- packages/pieces/certopus/src/index.ts
+++ packages/pieces/certopus/src/index.ts
@@ -1,13 +1,10 @@
 import { createPiece } from '@activepieces/pieces-framework';
-import packageJson from '../package.json';
 import { createCredential } from './lib/actions/create-credential';
 
 export const certopus = createPiece({
-  name: 'certopus',
   displayName: 'Certopus',
   logoUrl: 'https://cdn.activepieces.com/pieces/certopus.png',
-  version: packageJson.version,
-  authors: [],
+  authors: ['VrajGohil'],
   actions: [createCredential],
   triggers: [],
 });
