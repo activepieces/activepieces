78bf8a4a52819c2bb57625a7636abc6e8fb706b2
Herman Kudria <h.kudrya@hotmail.com>
2023-06-13 09:28:32 +0200

    feat: create actions Create transaction for Bloomerang

--- packages/pieces/bloomerang/src/index.ts
+++ packages/pieces/bloomerang/src/index.ts
@@ -1,10 +1,14 @@
 
 import { createPiece } from "@activepieces/pieces-framework";
+import {bloomerangUpsertContacts} from './lib/actions/upsert_contact';
+import {bloomerangCreateTransaction} from './lib/actions/create_transaction';
+import {bloomerangGetContacts} from './lib/actions/get_contacts';
+import {bloomerangGetTransactionStuff} from './lib/actions/get_transaction_stuff';
 
 export const bloomerang = createPiece({
   displayName: "Bloomerang",
   logoUrl: "https://cdn.activepieces.com/pieces/bloomerang.png",
-  authors: [],
-  actions: [],
+  authors: ['HKudria'],
+  actions: [bloomerangUpsertContacts, bloomerangCreateTransaction, bloomerangGetContacts, bloomerangGetTransactionStuff],
   triggers: [],
 });
