e5b77ec8c882419097059596b3439f80413867ec
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-06-14 00:14:38 +0200

    chore: remove name & version of jotforms

--- packages/pieces/jotform/src/index.ts
+++ packages/pieces/jotform/src/index.ts
@@ -1,12 +1,9 @@
 import { createPiece } from "@activepieces/pieces-framework";
-import packageJson from "../package.json";
 import { newSubmission } from "./lib/triggers/new-submission";
 
 export const jotform = createPiece({
-	name: "jotform",
 	displayName: "Jotform",
 	logoUrl: "https://cdn.activepieces.com/pieces/jotform.svg",
-	version: packageJson.version,
 	authors: ['MoShizzle'],
 	actions: [],
 	triggers: [newSubmission],
