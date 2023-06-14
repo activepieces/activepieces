42e1b7235b8ff1823698560e073eb0052db39ac9
Ahmad-AbuOsbeh <ahmadabuosbeh20@gmail.com>
2023-06-14 19:27:39 +0300

    feat(webflow): review feedback, return whole form body and make small updates

--- packages/pieces/webflow/src/lib/triggers/new-form-submitted.ts
+++ packages/pieces/webflow/src/lib/triggers/new-form-submitted.ts
@@ -1,5 +1,5 @@
 import { webflowCommon, sitesDropdown } from '../common/common';
-import { createTrigger, TriggerStrategy } from '@activepieces/pieces-framework';
+import { createTrigger, TriggerStrategy, Property } from '@activepieces/pieces-framework';
 import { getAccessTokenOrThrow } from "@activepieces/pieces-common";
 
 const triggerNameInStore = 'webflow_created_form_submissions_trigger';
@@ -10,15 +10,21 @@ export const webflowNewSubmission = createTrigger({
     description: 'Triggers when Webflow Site receives a new submission',
     props: {
         authentication: webflowCommon.authentication,
-        _id: sitesDropdown,
+        site_id: sitesDropdown,
+        formName: Property.ShortText({
+            displayName: 'Form Name',
+            required: false,
+            description: "your form name"
+        }),
     },
     type: TriggerStrategy.WEBHOOK,
     sampleData: {
       "name": "Sample Form",
       "site": "62749158efef318abc8d5a0f",
-      "data": {
+      "data": { //these fields are custom fields and can be changed, user can add more, delete, or change field name.
           "name": "Some One",
-          "email": "some.one@home.com"
+          "email": "some.one@home.com",
+          "subject": "message subject"
       },
       "d": "2022-09-14T12:35:16.117Z",
       "_id": "6321ca84df3949bfc6752327"
@@ -27,7 +33,7 @@ export const webflowNewSubmission = createTrigger({
         const formSubmissionTag ="form_submission";
         
        const res = await webflowCommon.subscribeWebhook(
-            context.propsValue['_id']!,
+            context.propsValue['site_id']!,
             formSubmissionTag,
             context.webhookUrl,
             getAccessTokenOrThrow(context.propsValue['authentication'])
@@ -40,15 +46,25 @@ export const webflowNewSubmission = createTrigger({
         const response = await context.store?.get<WebhookInformation>(triggerNameInStore);
         if (response !== null && response !== undefined) {
             await webflowCommon.unsubscribeWebhook(
-                context.propsValue['_id']!,
+                context.propsValue['site_id']!,
                 response.webhookId,
                 getAccessTokenOrThrow(context.propsValue['authentication'])
             );
         }
     },
     async run(context) {
-        const body = context.payload.body as { _id: unknown };
-        return [body._id];
+        const body = context.payload.body;
+        const { formName } = context.propsValue;
+        //if formName provided, trigger only required formName if it's matched; else trigger all forms in selected webflow site.
+        if (formName) { 
+            if (body.name == formName) {
+                return [body];
+            } else {
+                return [];
+            }
+        } else {
+            return [body];
+        }
     },
 });
 
