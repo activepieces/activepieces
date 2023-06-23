a719017e66f1c2d4f5a013ebd5875a13b14a5a87
Kritan Banstola <kbanstola@growthzilla.com>
2023-06-23 17:56:57 +0545

    fix: proma published to npm

--- packages/pieces/proma/src/index.ts
+++ packages/pieces/proma/src/index.ts
@@ -14,11 +14,9 @@ import { updatePromaRow } from './lib/actions/update-item';
 import { teamMemberAddedWorkspace } from './lib/triggers/team-member-added-ws';
 
 export const proma = createPiece({
-  name: 'proma',
   displayName: 'Proma',
   logoUrl:
     'https://growthzilla-notes-759987269.development.catalystserverless.com/staticfiles/eb5ade4a-27dc-4d91-b80e-89807962f19d-proma-logo.png',
-  version: packageJson.version,
   authors: [],
   actions: [
     addPromaRow,
