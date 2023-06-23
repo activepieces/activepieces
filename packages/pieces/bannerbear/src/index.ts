fcaf3fbeb9d8410aa38efa16e183c54898e8e1ad
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-23 21:04:19 +0300

    chore(pieces-bannerbear): upgrade to piece level auth

--- packages/pieces/bannerbear/src/index.ts
+++ packages/pieces/bannerbear/src/index.ts
@@ -1,13 +1,20 @@
-import { AuthProp, Piece } from '@activepieces/pieces-framework';
+import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
+import { bannerbearCreateImageAction } from './lib/actions/create-image';
 
-export const bannerbear = Piece.create({
+export const bannerbearAuth = PieceAuth.SecretText({
+  displayName: 'API Key',
+  description: 'Bannerbear API Key',
+  required: true,
+});
+
+export const bannerbear = createPiece({
   displayName: "Bannerbear",
   logoUrl: 'https://cdn.activepieces.com/pieces/bannerbear.png',
   authors: ["kanarelo"],
-  auth: AuthProp.SecretText({
-    displayName: 'API Key',
-    description: 'Bannerbear API Key',
-    required: true,
-  }),
+  auth: bannerbearAuth,
+  actions: [
+    bannerbearCreateImageAction,
+  ],
+  triggers: [
+  ],
 });
-
--- packages/pieces/bannerbear/src/lib/actions/create-image.ts
+++ packages/pieces/bannerbear/src/lib/actions/create-image.ts
@@ -1,185 +1,188 @@
-import { Property, DynamicPropsValue } from '@activepieces/pieces-framework';
+import { Property, DynamicPropsValue, createAction } from '@activepieces/pieces-framework';
 import { AuthenticationType, httpClient, HttpMethod, HttpRequest } from '@activepieces/pieces-common';
-import { bannerbear } from '../../';
+import { bannerbearAuth } from '../../';
 
-bannerbear.addAction({
-  name: 'bannerbear_create_image', // Must be a unique across the piece, this shouldn't be changed.
-  displayName: 'Create Image',
-  description: 'Create image from Bannerbear template',
-  sampleData: {
-    "created_at": "2023-02-13T23:53:05.445Z",
-    "status": "completed",
-    "self": "https://api.bannerbear.com/v2/images/VA54EW2ZqQr5eKOP6egGPNXJl",
-    "uid": "VA54EW2ZqQr5eKOP6egGPNXJl",
-    "image_url": "https://images.bannerbear.com/direct/2by4GqMJbkdMERad9x/requests/000/034/020/670/VA54EW2ZqQr5eKOP6egGPNXJl/0b067422c8a3023c3301a64a5e8fcb8d7099b8ad.png",
-    "image_url_png": "https://images.bannerbear.com/direct/2by4GqMJbkdMERad9x/requests/000/034/020/670/VA54EW2ZqQr5eKOP6egGPNXJl/0b067422c8a3023c3301a64a5e8fcb8d7099b8ad.png",
-    "image_url_jpg": "https://images.bannerbear.com/direct/2by4GqMJbkdMERad9x/requests/000/034/020/670/VA54EW2ZqQr5eKOP6egGPNXJl/0b067422c8a3023c3301a64a5e8fcb8d7099b8ad.jpg",
-    "template": "Rqg32K5QE6Y58V07Y6",
-    "template_version": null,
-    "modifications": [
-      {
-        "name": "message",
-        "text": "You can change this text",
-        "color": null,
-        "background": null
-      },
-      {
-        "name": "face",
-        "image_url": "https://cdn.bannerbear.com/sample_images/welcome_bear_photo.jpg"
-      }
-    ],
-    "webhook_url": null,
-    "webhook_response_code": null,
-    "transparent": false,
-    "metadata": null,
-    "template_name": "Template 1",
-    "width": 1200,
-    "height": 700,
-    "render_pdf": false,
-    "pdf_url": null,
-    "pdf_url_compressed": null
-  },
-  props: {
-    template: Property.Dropdown({
-      displayName: 'Template',
-      description: 'The template to use in image creation.',
-      required: true,
-      refreshers: ['authentication'],
-      options: async ({ auth }) => {
-        if (!auth) {
+export const bannerbearCreateImageAction = createAction({
+  auth: bannerbearAuth,
+  action: {
+    name: 'bannerbear_create_image', // Must be a unique across the piece, this shouldn't be changed.
+    displayName: 'Create Image',
+    description: 'Create image from Bannerbear template',
+    sampleData: {
+      "created_at": "2023-02-13T23:53:05.445Z",
+      "status": "completed",
+      "self": "https://api.bannerbear.com/v2/images/VA54EW2ZqQr5eKOP6egGPNXJl",
+      "uid": "VA54EW2ZqQr5eKOP6egGPNXJl",
+      "image_url": "https://images.bannerbear.com/direct/2by4GqMJbkdMERad9x/requests/000/034/020/670/VA54EW2ZqQr5eKOP6egGPNXJl/0b067422c8a3023c3301a64a5e8fcb8d7099b8ad.png",
+      "image_url_png": "https://images.bannerbear.com/direct/2by4GqMJbkdMERad9x/requests/000/034/020/670/VA54EW2ZqQr5eKOP6egGPNXJl/0b067422c8a3023c3301a64a5e8fcb8d7099b8ad.png",
+      "image_url_jpg": "https://images.bannerbear.com/direct/2by4GqMJbkdMERad9x/requests/000/034/020/670/VA54EW2ZqQr5eKOP6egGPNXJl/0b067422c8a3023c3301a64a5e8fcb8d7099b8ad.jpg",
+      "template": "Rqg32K5QE6Y58V07Y6",
+      "template_version": null,
+      "modifications": [
+        {
+          "name": "message",
+          "text": "You can change this text",
+          "color": null,
+          "background": null
+        },
+        {
+          "name": "face",
+          "image_url": "https://cdn.bannerbear.com/sample_images/welcome_bear_photo.jpg"
+        }
+      ],
+      "webhook_url": null,
+      "webhook_response_code": null,
+      "transparent": false,
+      "metadata": null,
+      "template_name": "Template 1",
+      "width": 1200,
+      "height": 700,
+      "render_pdf": false,
+      "pdf_url": null,
+      "pdf_url_compressed": null
+    },
+    props: {
+      template: Property.Dropdown({
+        displayName: 'Template',
+        description: 'The template to use in image creation.',
+        required: true,
+        refreshers: ['authentication'],
+        options: async ({ auth }) => {
+          if (!auth) {
+            return {
+              disabled: true,
+              options: [],
+              placeholder: 'Please enter your API key first.'
+            }
+          }
+
+          const response = await httpClient.sendRequest<BannerbearTemplate[]>({
+            method: HttpMethod.GET,
+            url: `https://api.bannerbear.com/v2/templates`,
+            authentication: {
+              type: AuthenticationType.BEARER_TOKEN,
+              token: auth as string
+            }
+          })
+
+          if (response.status === 200) {
+            return {
+              disabled: false,
+              options: response.body.map((template) => {
+                return {
+                  label: template.name,
+                  value: template
+                }
+              })
+            }
+          }
+
           return {
             disabled: true,
             options: [],
-            placeholder: 'Please enter your API key first.'
+            placeholder: "Error processing templates"
           }
         }
+      }),
+      template_version: Property.Number({
+        displayName: 'Template version',
+        description: 'Create image based on a specific version of the template.',
+        required: false
+      }),
+      modifications: Property.DynamicProperties({
+        displayName: 'Template modifications',
+        description: 'A list of modifications you want to make on the template.',
+        required: true,
+        refreshers: ["authentication", "template"],
+        props: async ({ authentication, template }) => {
+          if (!authentication) return {}
+          if (!template) return {}
 
-        const response = await httpClient.sendRequest<BannerbearTemplate[]>({
-          method: HttpMethod.GET,
-          url: `https://api.bannerbear.com/v2/templates`,
-          authentication: {
-            type: AuthenticationType.BEARER_TOKEN,
-            token: auth as string
-          }
-        })
+          let fields: DynamicPropsValue = {};
 
-        if (response.status === 200) {
-          return {
-            disabled: false,
-            options: response.body.map((template) => {
-              return {
-                label: template.name,
-                value: template
+          (template as BannerbearTemplate).available_modifications.map((modification) => {
+            if ('text' in modification) {
+              fields = {
+                ...BannerbearModificationsMapping.main,
+                ...BannerbearModificationsMapping.text,
+                ...BannerbearModificationsMapping.common,
               }
-            })
-          }
-        }
+            } else if ('rating' in modification) {
+              fields = {
+                ...BannerbearModificationsMapping.main,
+                ...BannerbearModificationsMapping.star_rating,
+                ...BannerbearModificationsMapping.common,
+              }
+            } else if ('image_url' in modification) {
+              fields = {
+                ...BannerbearModificationsMapping.main,
+                ...BannerbearModificationsMapping.image,
+                ...BannerbearModificationsMapping.common,
+              }
+            } else if ('chart_data' in modification) {
+              fields = {
+                ...BannerbearModificationsMapping.main,
+                ...BannerbearModificationsMapping.barline_chart,
+                ...BannerbearModificationsMapping.common,
+              }
+            } else if ('bar_code_data' in modification) {
+              fields = {
+                ...BannerbearModificationsMapping.main,
+                ...BannerbearModificationsMapping.bar_code,
+                ...BannerbearModificationsMapping.common,
+              }
+            }
+          })
 
-        return {
-          disabled: true,
-          options: [],
-          placeholder: "Error processing templates"
+          return fields
+        }
+      }),
+      transparent: Property.Checkbox({
+        displayName: 'Transparent background',
+        description: 'Render a PNG with a transparent background. Default is false.',
+        required: false
+      }),
+      render_pdf: Property.Checkbox({
+        displayName: 'Render a PDF',
+        description: 'Render a PDF instead of a PNG. Default is false.',
+        required: false
+      }),
+      metadata: Property.LongText({
+        displayName: 'Metadata',
+        description: 'Any metadata that you need to store e.g. ID of a record in your DB.',
+        required: false
+      }),
+    },
+    async run({ auth, propsValue }) {
+      const body = {
+        modifications: Object.keys(propsValue.modifications).length === 0 ? [] : [propsValue.modifications],
+        template_version: propsValue.template_version,
+        transparent: propsValue.transparent,
+        render_pdf: propsValue.render_pdf,
+        template: propsValue.template.uid,
+        metadata: propsValue.metadata
+      }
+
+      const request: HttpRequest = {
+        method: HttpMethod.POST,
+        url: `https://sync.api.bannerbear.com/v2/images`,
+        body,
+        authentication: {
+          type: AuthenticationType.BEARER_TOKEN,
+          token: auth
         }
       }
-    }),
-    template_version: Property.Number({
-      displayName: 'Template version',
-      description: 'Create image based on a specific version of the template.',
-      required: false
-    }),
-    modifications: Property.DynamicProperties({
-      displayName: 'Template modifications',
-      description: 'A list of modifications you want to make on the template.',
-      required: true,
-      refreshers: ["authentication", "template"],
-      props: async ({ authentication, template }) => {
-        if (!authentication) return {}
-        if (!template) return {}
 
-        let fields: DynamicPropsValue = {};
+      const result = await httpClient.sendRequest<BannerbearTemplate>(request)
+      console.debug("Image creation complete", result)
 
-        (template as BannerbearTemplate).available_modifications.map((modification) => {
-          if ('text' in modification) {
-            fields = {
-              ...BannerbearModificationsMapping.main,
-              ...BannerbearModificationsMapping.text,
-              ...BannerbearModificationsMapping.common,
-            }
-          } else if ('rating' in modification) {
-            fields = {
-              ...BannerbearModificationsMapping.main,
-              ...BannerbearModificationsMapping.star_rating,
-              ...BannerbearModificationsMapping.common,
-            }
-          } else if ('image_url' in modification) {
-            fields = {
-              ...BannerbearModificationsMapping.main,
-              ...BannerbearModificationsMapping.image,
-              ...BannerbearModificationsMapping.common,
-            }
-          } else if ('chart_data' in modification) {
-            fields = {
-              ...BannerbearModificationsMapping.main,
-              ...BannerbearModificationsMapping.barline_chart,
-              ...BannerbearModificationsMapping.common,
-            }
-          } else if ('bar_code_data' in modification) {
-            fields = {
-              ...BannerbearModificationsMapping.main,
-              ...BannerbearModificationsMapping.bar_code,
-              ...BannerbearModificationsMapping.common,
-            }
-          }
-        })
-
-        return fields
+      if (result.status === 200 || result.status === 202) {
+        return result.body
+      } else {
+        return result
       }
-    }),
-    transparent: Property.Checkbox({
-      displayName: 'Transparent background',
-      description: 'Render a PNG with a transparent background. Default is false.',
-      required: false
-    }),
-    render_pdf: Property.Checkbox({
-      displayName: 'Render a PDF',
-      description: 'Render a PDF instead of a PNG. Default is false.',
-      required: false
-    }),
-    metadata: Property.LongText({
-      displayName: 'Metadata',
-      description: 'Any metadata that you need to store e.g. ID of a record in your DB.',
-      required: false
-    }),
+    },
   },
-  async run({ auth, propsValue }) {
-    const body = {
-      modifications: Object.keys(propsValue.modifications).length === 0 ? [] : [propsValue.modifications],
-      template_version: propsValue.template_version,
-      transparent: propsValue.transparent,
-      render_pdf: propsValue.render_pdf,
-      template: propsValue.template.uid,
-      metadata: propsValue.metadata
-    }
-
-    const request: HttpRequest = {
-      method: HttpMethod.POST,
-      url: `https://sync.api.bannerbear.com/v2/images`,
-      body,
-      authentication: {
-        type: AuthenticationType.BEARER_TOKEN,
-        token: auth
-      }
-    }
-
-    const result = await httpClient.sendRequest<BannerbearTemplate>(request)
-    console.debug("Image creation complete", result)
-
-    if (result.status === 200 || result.status === 202) {
-      return result.body
-    } else {
-      return result
-    }
-  }
 })
 
 interface BannerbearTemplate {
