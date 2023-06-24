f081d5df9ad4a239f4eb26ec031049ec8dfbbb2e
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-24 19:20:26 +0300

    chore(pieces-dropbox): upgrade to piece level auth

--- packages/pieces/dropbox/src/index.ts
+++ packages/pieces/dropbox/src/index.ts
@@ -1,6 +1,15 @@
-import { createPiece } from '@activepieces/pieces-framework';
+import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
 import { dropboxCreateNewFolder } from './lib/actions/create-new-folder';
 import { dropboxCreateNewTextFile } from './lib/actions/create-new-text-file';
+
+export const dropboxAuth = PieceAuth.OAuth2({
+  description: "",
+  displayName: 'Authentication',
+  authUrl: "https://www.dropbox.com/oauth2/authorize",
+  tokenUrl: "https://api.dropboxapi.com/oauth2/token",
+  required: true,
+  scope: ["files.content.write"]
+})
 
 export const dropbox = createPiece({
   logoUrl: 'https://cdn.activepieces.com/pieces/dropbox.png',
@@ -8,4 +17,5 @@ export const dropbox = createPiece({
   displayName: "DropBox",
   authors: ['kanarelo'],
   triggers: [],
+  auth: dropboxAuth,
 });
