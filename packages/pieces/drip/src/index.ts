35289d661ca6e0c146e5df93ae2d0cd319082ef4
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-24 19:14:58 +0300

    chore(pieces-drip): upgrade to piece level auth

--- packages/pieces/drip/src/index.ts
+++ packages/pieces/drip/src/index.ts
@@ -1,15 +1,21 @@
-import { createPiece } from '@activepieces/pieces-framework';
+import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
 import { dripAddSubscriberToCampaign } from './lib/actions/add-subscriber-to-campaign.action';
 import { dripApplyTagToSubscriber } from './lib/actions/apply-tag-to-subscriber.action';
 import { dripUpsertSubscriberAction } from './lib/actions/upsert-subscriber.action';
 import { dripNewSubscriberEvent } from './lib/trigger/new-subscriber.trigger';
 import { dripTagAppliedEvent } from './lib/trigger/new-tag.trigger';
 
+export const dripAuth = PieceAuth.SecretText({
+    displayName: "API Key",
+    required: true,
+    description: "Get it from https://www.getdrip.com/user/edit"
+})
 
 export const drip = createPiece({
 	displayName: 'Drip',
 	logoUrl: 'https://cdn.activepieces.com/pieces/drip.png',
 	authors: ['AbdulTheActivePiecer'],
+    auth: dripAuth,
 	actions: [dripApplyTagToSubscriber, dripAddSubscriberToCampaign, dripUpsertSubscriberAction],
 	triggers: [dripNewSubscriberEvent, dripTagAppliedEvent],
 });
--- packages/pieces/drip/src/lib/trigger/new-subscriber.trigger.ts
+++ packages/pieces/drip/src/lib/trigger/new-subscriber.trigger.ts
@@ -1,61 +1,64 @@
 import { HttpRequest, HttpMethod, httpClient } from '@activepieces/pieces-common';
 import { TriggerStrategy , createTrigger} from "@activepieces/pieces-framework";
 import { dripCommon } from '../common';
+import { dripAuth } from '../../';
 
 const triggerNameInStore = 'drip_new_subscriber_trigger';
 export const dripNewSubscriberEvent = createTrigger({
-  name: 'new_subscriber',
-  displayName: 'New Subscriber',
-  description: 'Triggers when a subscriber is created in your Drip account.',
-  props: {
-    authentication: dripCommon.authentication,
-    account_id: dripCommon.account_id
-  },
-  sampleData: {
-    "event": "subscriber.created",
-    "data": {
-      "account_id": "9999999",
-      "subscriber": {}
+  auth: dripAuth,
+  trigger: {
+    name: 'new_subscriber',
+    displayName: 'New Subscriber',
+    description: 'Triggers when a subscriber is created in your Drip account.',
+    props: {
+      account_id: dripCommon.account_id
     },
-    "occurred_at": "2013-06-21T10:31:58Z"
-  },
-  type: TriggerStrategy.WEBHOOK,
-  async onEnable(context) {
-
-    const request: HttpRequest = {
-      method: HttpMethod.POST,
-      url: `${dripCommon.baseUrl(context.propsValue["account_id"]!)}/webhooks`,
-      body: {
-        webhooks: [{ post_url: context.webhookUrl, events: ["subscriber.created"] }],
+    sampleData: {
+      "event": "subscriber.created",
+      "data": {
+        "account_id": "9999999",
+        "subscriber": {}
       },
-      headers: {
-        'Authorization': dripCommon.authorizationHeader(context.propsValue["authentication"]!)
-      },
-      queryParams: {},
-    };
-    const { body } = await httpClient.sendRequest<{ webhooks: { id: string }[] }>(request);
-    await context.store?.put<DripWebhookInformation>(triggerNameInStore, {
-      webhookId: body.webhooks[0].id,
-      userId: context.propsValue["account_id"]!
-    });
+      "occurred_at": "2013-06-21T10:31:58Z"
+    },
+    type: TriggerStrategy.WEBHOOK,
+    async onEnable({ auth, propsValue, webhookUrl, store }) {
 
-
-  },
-  async onDisable(context) {
-    const response = await context.store?.get<DripWebhookInformation>(triggerNameInStore);
-    if (response !== null && response !== undefined) {
       const request: HttpRequest = {
-        method: HttpMethod.DELETE,
-        url: `${dripCommon.baseUrl(response.userId)}/webhooks/${response.webhookId}`,
-        headers: {
-          'Authorization': dripCommon.authorizationHeader(context.propsValue["authentication"]!)
+        method: HttpMethod.POST,
+        url: `${dripCommon.baseUrl(propsValue.account_id)}/webhooks`,
+        body: {
+          webhooks: [{ post_url: webhookUrl, events: ["subscriber.created"] }],
         },
+        headers: {
+          'Authorization': dripCommon.authorizationHeader(auth)
+        },
+        queryParams: {},
       };
-      await httpClient.sendRequest(request);
-    }
-  },
-  async run(context) {
-    return [context.payload.body];
+      const { body } = await httpClient.sendRequest<{ webhooks: { id: string }[] }>(request);
+      await store.put<DripWebhookInformation>(triggerNameInStore, {
+        webhookId: body.webhooks[0].id,
+        userId: propsValue.account_id
+      });
+
+
+    },
+    async onDisable(context) {
+      const response = await context.store?.get<DripWebhookInformation>(triggerNameInStore);
+      if (response !== null && response !== undefined) {
+        const request: HttpRequest = {
+          method: HttpMethod.DELETE,
+          url: `${dripCommon.baseUrl(response.userId)}/webhooks/${response.webhookId}`,
+          headers: {
+            'Authorization': dripCommon.authorizationHeader(context.auth)
+          },
+        };
+        await httpClient.sendRequest(request);
+      }
+    },
+    async run(context) {
+      return [context.payload.body];
+    },
   },
 });
 
