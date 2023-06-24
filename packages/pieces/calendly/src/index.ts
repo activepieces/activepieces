601e0a98ff1855dad01f2b0f2711b21a17517bfb
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-24 14:54:31 +0300

    chore(pieces-calendly): upgrade to piece level auth

--- packages/pieces/calendly/src/index.ts
+++ packages/pieces/calendly/src/index.ts
@@ -1,12 +1,18 @@
-import { AuthProp, Piece } from '@activepieces/pieces-framework';
+import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
+import { calendlyInviteeCanceled } from './lib/trigger/invitee-canceled.trigger';
+import { calendlyInviteeCreated } from './lib/trigger/invitee-created.trigger';
 
-export const calendly = Piece.create({
-    displayName: 'Calendly',
-    logoUrl: 'https://cdn.activepieces.com/pieces/calendly.png',
-    authors: ['AbdulTheActivePiecer'],
-    auth: AuthProp.SecretText({
-        displayName: "Personal Token",
-        required: true,
-        description: "Get it from https://calendly.com/integrations/api_webhooks"
-    }),
+export const calendlyAuth = PieceAuth.SecretText({
+    displayName: "Personal Token",
+    required: true,
+    description: "Get it from https://calendly.com/integrations/api_webhooks"
+})
+
+export const calendly = createPiece({
+	displayName: 'Calendly',
+	logoUrl: 'https://cdn.activepieces.com/pieces/calendly.png',
+	authors: ['AbdulTheActivePiecer'],
+    auth: calendlyAuth,
+	actions: [],
+	triggers: [calendlyInviteeCreated, calendlyInviteeCanceled],
 });
