6a3eeab1f585a4233d6c18f7658f8dc07b8463d5
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-25 07:26:51 +0300

    chore(pieces-figma): upgrade to piece level auth

--- packages/pieces/figma/src/lib/actions/get-file-action.ts
+++ packages/pieces/figma/src/lib/actions/get-file-action.ts
@@ -1,33 +1,35 @@
 import { createAction, Property } from "@activepieces/pieces-framework";
 import { assertNotNullOrUndefined } from "@activepieces/pieces-common";
-import { figmaAuth } from '../common/props';
 import { figmaCommon } from "../common";
 import { figmaGetRequest } from '../common/utils';
+import { figmaAuth } from "../../";
 
 export const getFileAction = createAction({
-  name: 'get_file',
-  displayName: 'Get File',
-  description: 'Get file',
-  sampleData: {
-    success: true,
-  },
-  props: {
-    authentication: figmaAuth,
-    file_key: Property.ShortText({
-      displayName: 'File Key',
-      description: 'The Figma file key (copy from Figma file URL)',
-      required: true,
-    }),
-  },
-  async run(context) {
-    const token = context.propsValue.authentication?.access_token;
-    const fileKey = context.propsValue.file_key;
+  auth: figmaAuth,
+  action: {
+    name: 'get_file',
+    displayName: 'Get File',
+    description: 'Get file',
+    sampleData: {
+      success: true,
+    },
+    props: {
+      file_key: Property.ShortText({
+        displayName: 'File Key',
+        description: 'The Figma file key (copy from Figma file URL)',
+        required: true,
+      }),
+    },
+    async run(context) {
+      const token = context.auth.access_token;
+      const fileKey = context.propsValue.file_key;
 
-    assertNotNullOrUndefined(token, 'token');
-    assertNotNullOrUndefined(fileKey, 'file_key');
-    
-    const url = `${figmaCommon.baseUrl}/${figmaCommon.files}`.replace(':file_key', fileKey);
+      assertNotNullOrUndefined(token, 'token');
+      assertNotNullOrUndefined(fileKey, 'file_key');
 
-    return figmaGetRequest({ token, url });
-  },
+      const url = `${figmaCommon.baseUrl}/${figmaCommon.files}`.replace(':file_key', fileKey);
+
+      return figmaGetRequest({ token, url });
+    },
+  }
 });
--- packages/pieces/figma/src/lib/actions/post-comment-action.ts
+++ packages/pieces/figma/src/lib/actions/post-comment-action.ts
@@ -1,39 +1,41 @@
 import { createAction, Property } from "@activepieces/pieces-framework";
 import { assertNotNullOrUndefined } from "@activepieces/pieces-common";
-import { figmaAuth } from '../common/props';
 import { figmaCommon } from "../common";
 import { figmaPostRequestWithMessage } from '../common/utils';
+import { figmaAuth } from "../../";
 
 export const postCommentAction = createAction({
-  name: 'post_comment',
-  displayName: 'Post File Comment',
-  description: 'Post file comment',
-  sampleData: {
-    success: true,
-  },
-  props: {
-    authentication: figmaAuth,
-    file_key: Property.ShortText({
-      displayName: 'File Key',
-      description: 'The Figma file key (copy from Figma file URL)',
-      required: true,
-    }),
-    message: Property.LongText({
-      displayName: 'Comment',
-      description: 'Your comment',
-      required: true,
-    }),
-  },
-  async run(context) {
-    const token = context.propsValue.authentication?.access_token;
-    const { file_key, message } = context.propsValue;
+  auth: figmaAuth,
+  action: {
+    name: 'post_comment',
+    displayName: 'Post File Comment',
+    description: 'Post file comment',
+    sampleData: {
+      success: true,
+    },
+    props: {
+      file_key: Property.ShortText({
+        displayName: 'File Key',
+        description: 'The Figma file key (copy from Figma file URL)',
+        required: true,
+      }),
+      message: Property.LongText({
+        displayName: 'Comment',
+        description: 'Your comment',
+        required: true,
+      }),
+    },
+    async run(context) {
+      const token = context.auth.access_token;
+      const { file_key, message } = context.propsValue;
 
-    assertNotNullOrUndefined(token, 'token');
-    assertNotNullOrUndefined(file_key, 'file_key');
-    assertNotNullOrUndefined(message, 'comment');
-    
-    const url = `${figmaCommon.baseUrl}/${figmaCommon.comments}`.replace(':file_key', file_key);
+      assertNotNullOrUndefined(token, 'token');
+      assertNotNullOrUndefined(file_key, 'file_key');
+      assertNotNullOrUndefined(message, 'comment');
 
-    return figmaPostRequestWithMessage({ token, url, message });
+      const url = `${figmaCommon.baseUrl}/${figmaCommon.comments}`.replace(':file_key', file_key);
+
+      return figmaPostRequestWithMessage({ token, url, message });
+    },
   },
 });
