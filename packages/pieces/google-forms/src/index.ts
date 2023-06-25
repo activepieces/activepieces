893f6ffc142091f7282250f00cb5ae5db59d749d
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-25 15:00:09 +0300

    chore(pieces-google-forms): upgrade to piece level auth

--- packages/pieces/google-forms/src/index.ts
+++ packages/pieces/google-forms/src/index.ts
@@ -1,11 +1,20 @@
 
-import { createPiece } from "@activepieces/pieces-framework";
+import { PieceAuth, createPiece } from "@activepieces/pieces-framework";
 import { newResponse } from "./lib/triggers/new-form-response";
+
+export const googleFormsAuth = PieceAuth.OAuth2({
+  displayName: 'Authentication',
+  authUrl: "https://accounts.google.com/o/oauth2/auth",
+  tokenUrl: "https://oauth2.googleapis.com/token",
+  required: true,
+  scope: ["https://www.googleapis.com/auth/forms.responses.readonly", "https://www.googleapis.com/auth/drive.readonly"]
+})
 
 export const googleForms = createPiece({
   displayName: "Google Forms",
   logoUrl: "https://cdn.activepieces.com/pieces/google-forms.png",
   authors: ["abuaboud"],
+  auth: googleFormsAuth,
   actions: [],
   triggers: [newResponse],
 });
