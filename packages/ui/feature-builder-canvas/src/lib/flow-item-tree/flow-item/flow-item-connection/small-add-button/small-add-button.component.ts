44025b447907466ff2b70dff15eb076c598ec1db
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-06-25 15:36:24 +0300

    chore: show focused add btn ui

--- packages/ui/feature-builder-canvas/src/lib/flow-item-tree/flow-item/flow-item-connection/small-add-button/small-add-button.component.ts
+++ packages/ui/feature-builder-canvas/src/lib/flow-item-tree/flow-item/flow-item-connection/small-add-button/small-add-button.component.ts
@@ -1,18 +1,21 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FlowRendererService } from '@activepieces/ui/feature-builder-store';
 import { Observable } from 'rxjs';
+import { AddButtonCore } from '../add-button-core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-small-add-button',
   templateUrl: './small-add-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SmallAddButtonComponent {
+export class SmallAddButtonComponent extends AddButtonCore {
   @Input() left = '';
   @Input() top = '';
   @Input() showDropzoneIndicator = false;
   isInDraggingMode$: Observable<boolean>;
-  constructor(private flowRendererService: FlowRendererService) {
+  constructor(private flowRendererService: FlowRendererService, store: Store) {
+    super(store);
     this.isInDraggingMode$ =
       this.flowRendererService.draggingSubject.asObservable();
   }
