a6e6dda09a7c632e4fd5a3a26091e41fce47a4c2
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-25 14:45:47 +0300

    chore(pieces-google-contacts): upgrade to piece level auth

--- packages/pieces/google-contacts/src/index.ts
+++ packages/pieces/google-contacts/src/index.ts
@@ -1,6 +1,15 @@
-import { createPiece } from '@activepieces/pieces-framework';
+import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
 import { googleContactsAddContactAction } from './lib/action/create-contact';
 import { googleContactNewOrUpdatedContact } from './lib/trigger/new-contact';
+
+export const googleContactsAuth = PieceAuth.OAuth2({
+    description: "",
+    displayName: 'Authentication',
+    authUrl: "https://accounts.google.com/o/oauth2/auth",
+    tokenUrl: "https://oauth2.googleapis.com/token",
+    required: true,
+    scope: ["https://www.googleapis.com/auth/contacts"]
+})
 
 export const googleContacts = createPiece({
 	logoUrl: 'https://cdn.activepieces.com/pieces/google-contacts.png',
@@ -8,4 +17,5 @@ export const googleContacts = createPiece({
 	displayName: "Google Contacts",
 	authors: ['abuaboud'],
 	triggers: [googleContactNewOrUpdatedContact],
+    auth: googleContactsAuth,
 });
