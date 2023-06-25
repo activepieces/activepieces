7a1f48d389d5301b1fefac221d31fa0a952fcf43
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-25 18:31:04 +0300

    chore(pieces-mysql): upgrade to piece level auth

--- packages/pieces/mysql/src/lib/actions/delete-row.ts
+++ packages/pieces/mysql/src/lib/actions/delete-row.ts
@@ -1,31 +1,34 @@
 import { createAction, Property } from "@activepieces/pieces-framework";
 import { mysqlCommon, mysqlConnect, sanitizeColumnName } from "../common";
+import { mysqlAuth } from "../..";
 
 export default createAction({
-    name: 'delete_row',
-    displayName: 'Delete Row',
-    description: 'Deletes one or more rows from a table',
-    props: {
-        authentication: mysqlCommon.authentication,
-        timezone: mysqlCommon.timezone,
-        table: mysqlCommon.table(),
-        search_column: Property.ShortText({
-            displayName: 'Search Column',
-            required: true
-        }),
-        search_value: Property.ShortText({
-            displayName: 'Search Value',
-            required: true
-        })
-    },
-    async run(context) {
-        const qs = "DELETE FROM `" + context.propsValue.table + "` WHERE " + sanitizeColumnName(context.propsValue.search_column) + "=?;"
-        const conn = await mysqlConnect(context.propsValue);
-        try {
-            const result = await conn.query(qs, [context.propsValue.search_value])
-            return result
-        } finally {
-            await conn.end()
+    auth: mysqlAuth,
+    action: {
+        name: 'delete_row',
+        displayName: 'Delete Row',
+        description: 'Deletes one or more rows from a table',
+        props: {
+            timezone: mysqlCommon.timezone,
+            table: mysqlCommon.table(),
+            search_column: Property.ShortText({
+                displayName: 'Search Column',
+                required: true
+            }),
+            search_value: Property.ShortText({
+                displayName: 'Search Value',
+                required: true
+            })
+        },
+        async run(context) {
+            const qs = "DELETE FROM `" + context.propsValue.table + "` WHERE " + sanitizeColumnName(context.propsValue.search_column) + "=?;"
+            const conn = await mysqlConnect(context.auth, context.propsValue);
+            try {
+                const result = await conn.query(qs, [context.propsValue.search_value])
+                return result
+            } finally {
+                await conn.end()
+            }
         }
     }
 })
--- packages/pieces/mysql/src/lib/actions/get-tables.ts
+++ packages/pieces/mysql/src/lib/actions/get-tables.ts
@@ -1,20 +1,23 @@
 import { createAction } from "@activepieces/pieces-framework";
-import { mysqlCommon, mysqlConnect, mysqlGetTableNames } from "../common";
+import { mysqlConnect, mysqlGetTableNames } from "../common";
+import { mysqlAuth } from "../..";
 
 export default createAction({
-    name: 'get_tables',
-    displayName: 'Get Tables',
-    description: 'Returns a list of tables in the database',
-    props: {
-        authentication: mysqlCommon.authentication
-    },
-    async run(context) {
-        const conn = await mysqlConnect(context.propsValue)
-        try {
-            const tables = await mysqlGetTableNames(conn)
-            return { tables }
-        } finally {
-            await conn.end()
+    auth: mysqlAuth,
+    action: {
+        name: 'get_tables',
+        displayName: 'Get Tables',
+        description: 'Returns a list of tables in the database',
+        props: {
+        },
+        async run(context) {
+            const conn = await mysqlConnect(context.auth, context.propsValue)
+            try {
+                const tables = await mysqlGetTableNames(conn)
+                return { tables }
+            } finally {
+                await conn.end()
+            }
         }
     }
 })
