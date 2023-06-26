f30f13fb8745bb9ffc6872a44d6e9d3a1a1c349f
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-26 07:14:20 +0300

    chore(pieces-sendinblue): upgrade to piece level auth

--- packages/pieces/sendinblue/src/index.ts
+++ packages/pieces/sendinblue/src/index.ts
@@ -1,11 +1,18 @@
 
-import { createPiece } from "@activepieces/pieces-framework";
+import { PieceAuth, createPiece } from "@activepieces/pieces-framework";
 import { createOrUpdateContact } from "./lib/actions/create-or-update-contact";
+
+export const sendinblueAuth = PieceAuth.SecretText({
+  displayName: "Project API key",
+  description: "Your project API key",
+  required: true
+})
 
 export const sendinblue = createPiece({
   displayName: "Sendinblue",
   logoUrl: "https://cdn.activepieces.com/pieces/sendinblue.png",
   authors: ['kanarelo'],
+  auth: sendinblueAuth,
   actions: [createOrUpdateContact],
   triggers: [],
 });
