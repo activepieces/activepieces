3a60edc68fe19ddea882c30ca31db257d4ab9e87
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-06-26 12:18:08 +0200

    fix(facebook-leads): remove name & version

--- packages/pieces/facebook-leads/src/index.ts
+++ packages/pieces/facebook-leads/src/index.ts
@@ -1,12 +1,9 @@
 import { createPiece } from "@activepieces/pieces-framework";
-import packageJson from "../package.json";
 import { newLead } from "./lib/triggers/new-lead";
 
 export const facebookLeads = createPiece({
-    name: "facebook-leads",
     displayName: "Facebook Leads",
     logoUrl: "https://cdn.activepieces.com/pieces/facebook.png",
-    version: packageJson.version,
     authors: ['MoShizzle'],
     actions: [],
     triggers: [newLead],
