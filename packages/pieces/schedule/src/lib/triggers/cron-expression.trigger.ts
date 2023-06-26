7fd4701c8a86f65ff88f60a50cde9000249473b0
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-26 07:05:39 +0300

    chore(pieces-schedule): upgrade to piece level auth

--- packages/pieces/schedule/src/lib/triggers/cron-expression.trigger.ts
+++ packages/pieces/schedule/src/lib/triggers/cron-expression.trigger.ts
@@ -2,28 +2,30 @@ import { TriggerStrategy } from "@activepieces/pieces-framework";
 import { createTrigger, Property } from "@activepieces/pieces-framework";
 
 export const cronExpressionTrigger = createTrigger({
-    name: 'cron_expression',
-    displayName: 'Cron Expression',
-    description: 'Trigger based on cron expression',
-    props: {
-        cronExpression: Property.ShortText({
-            displayName: 'Cron Expression',
-            description: 'Cron expression to trigger',
-            required: true,
-            defaultValue: '0/5 * * * *',
-        }),
-    },
-    type: TriggerStrategy.POLLING,
-    sampleData: {},
-    onEnable: async (ctx) => {
-        ctx.setSchedule({
-            cronExpression: ctx.propsValue.cronExpression,
-        })        
-    },
-    run(context) {
-        return Promise.resolve([{}]);
-    },
-    onDisable: async () => {
-        console.log('onDisable');
+    trigger: {
+        name: 'cron_expression',
+        displayName: 'Cron Expression',
+        description: 'Trigger based on cron expression',
+        props: {
+            cronExpression: Property.ShortText({
+                displayName: 'Cron Expression',
+                description: 'Cron expression to trigger',
+                required: true,
+                defaultValue: '0/5 * * * *',
+            }),
+        },
+        type: TriggerStrategy.POLLING,
+        sampleData: {},
+        onEnable: async (ctx) => {
+            ctx.setSchedule({
+                cronExpression: ctx.propsValue.cronExpression,
+            })
+        },
+        run(context) {
+            return Promise.resolve([{}]);
+        },
+        onDisable: async () => {
+            console.log('onDisable');
+        }
     }
 });
