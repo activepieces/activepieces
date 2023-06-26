ada6609fc5f9c5015f4ca2ec54f54f8deb23b615
Khaled Mashaly <kmmashaly@gmail.com>
2023-06-26 18:43:24 +0300

    chore(pieces-stripe): upgrade to piece level auth

--- packages/pieces/stripe/src/index.ts
+++ packages/pieces/stripe/src/index.ts
@@ -1,13 +1,20 @@
-import { createPiece } from '@activepieces/pieces-framework';
+import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
 import {stripeNewPayment} from "./lib/trigger/new-payment";
 import {stripeNewCustomer} from "./lib/trigger/new-customer";
 import {stripePaymentFailed} from "./lib/trigger/payment-failed";
 import { stripeNewSubscription } from './lib/trigger/new-subscription';
 
+export const stripeAuth = PieceAuth.SecretText({
+    displayName:"API Key",
+    required:true,
+    description:"API key acquired from your Stripe dashboard"
+})
+
 export const stripe = createPiece({
 	displayName: "Stripe",
 	logoUrl: 'https://cdn.activepieces.com/pieces/stripe.png',
 	authors: ['ashrafsamhouri'],
+    auth: stripeAuth,
 	actions: [],
 	triggers: [stripeNewPayment, stripeNewCustomer, stripePaymentFailed, stripeNewSubscription],
 });
