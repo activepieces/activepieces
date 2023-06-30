1e8d8cda81bcf9e495abe86c9e70bf7e46023042
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-07-01 01:45:13 +0200

    fix(invoice-ninja):  author name

--- packages/pieces/invoiceninja/src/index.ts
+++ packages/pieces/invoiceninja/src/index.ts
@@ -1,11 +1,11 @@
 
 import { createPiece } from "@activepieces/pieces-framework";
-import { CreateTask } from './lib/actions/create-task';
+import { createTask } from './lib/actions/create-task';
 
 export const invoiceninja = createPiece({
-  displayName: "Invoiceninja",
+  displayName: "Invoice Ninja",
   logoUrl: "https://cdn.activepieces.com/pieces/invoiceninja.png",
-  authors: ["Mark van Bellen, All Tech Plus: mark@all-tech-plus.com"],
-  actions: [ CreateTask ],
+  authors: ["buttonsbond"],
+  actions: [createTask],
   triggers: [],
 });
