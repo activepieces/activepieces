9eda135653363a48d23b7177d63fd8cd9a56e6ed
Khaled Mashaly <kmmashaly@gmail.com>
2023-07-02 15:13:27 +0300

    chore(pieces-typeform): upgrade to piece level auth

--- packages/pieces/typeform/src/lib/common/index.ts
+++ packages/pieces/typeform/src/lib/common/index.ts
@@ -13,8 +13,8 @@ export const formsDropdown = Property.Dropdown<string>({
     description: 'Form Name',
     required: true,
     refreshers: ['authentication'],
-    async options(propsValue) {
-        const auth = propsValue['authentication'] as OAuth2PropertyValue;
+    async options({ auth: authentication }) {
+        const auth = authentication as OAuth2PropertyValue;
 
         if (!auth) {
             return {
@@ -51,13 +51,6 @@ export const formsDropdown = Property.Dropdown<string>({
 
 export const typeformCommon = {
     baseUrl: "https://api.typeform.com",
-    authentication: Property.OAuth2({
-        displayName: "Authentication",
-        required: true,
-        tokenUrl: 'https://api.typeform.com/oauth/token',
-        authUrl: 'https://admin.typeform.com/oauth/authorize',
-        scope: ['webhooks:write', 'forms:read'],
-    }),
     subscribeWebhook: async (formId: string, tag: string, webhookUrl: string, accessToken: string) => {
         const request: HttpRequest = {
             method: HttpMethod.PUT,
