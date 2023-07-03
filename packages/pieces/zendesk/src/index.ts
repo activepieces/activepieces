69ac782417f55862e28f62a0997357255c96afa4
Khaled Mashaly <kmmashaly@gmail.com>
2023-07-03 07:18:08 +0300

    chore(pieces-zendesk): upgrade to piece level auth

--- packages/pieces/zendesk/src/index.ts
+++ packages/pieces/zendesk/src/index.ts
@@ -1,6 +1,37 @@
 
-import { createPiece } from '@activepieces/pieces-framework';
+import { PieceAuth, Property, createPiece } from '@activepieces/pieces-framework';
 import { newTicketInView } from './lib/trigger/new-ticket-in-view';
+
+const markdownProperty = `
+**Organization**: The organization name can be found in the URL (e.g https://ORGANIZATION_NAME.zendesk.com).
+
+**Agent Email**: The email you use to log in to Zendesk.
+
+**API Token**: You can find this in the Zendesk Admin Panel under Settings > APIs > Zendesk API.
+`
+
+export const zendeskAuth = PieceAuth.CustomAuth({
+  displayName: 'Authentication',
+  description: markdownProperty,
+  props: {
+      email: Property.ShortText({
+          displayName: 'Agent Email',
+          description: 'The email address you use to login to Zendesk',
+          required: true,
+      }),
+      token: Property.ShortText({
+          displayName: 'Token',
+          description: 'The API token you can generate in Zendesk',
+          required: true,
+      }),
+      subdomain: Property.ShortText({
+          displayName: 'Organization (e.g activepieceshelp)',
+          description: 'The subdomain of your Zendesk instance',
+          required: true,
+      }),
+  },
+  required: true,
+})
 
 export const zendesk = createPiece({
   displayName: 'Zendesk',
@@ -8,6 +39,7 @@ export const zendesk = createPiece({
   authors: [
     "abuaboud"
   ],
+  auth: zendeskAuth,
   actions: [
   ],
   triggers: [
