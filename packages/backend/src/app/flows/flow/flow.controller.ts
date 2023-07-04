3a415ac7ded33c1789a61db314ca0457b9fa07f5
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-07-04 18:54:38 +0300

    fix: lint

--- packages/backend/src/app/flows/flow/flow.controller.ts
+++ packages/backend/src/app/flows/flow/flow.controller.ts
@@ -123,7 +123,7 @@ export const flowController = async (fastify: FastifyInstance) => {
                 template: flow.version,
                 tags:[],
                 blogUrl:'', 
-                pinnedOrder:null
+                pinnedOrder:null,
             }
             return template
         },
