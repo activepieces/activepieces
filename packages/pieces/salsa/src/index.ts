6be7753c5420e73eb16ccd5d261f280f2be89dc6
Khaled Mashaly <kmmashaly@gmail.com>
2023-07-04 18:06:44 +0300

    chore(pieces-salsa): upgrade to piece level auth

--- packages/pieces/salsa/src/index.ts
+++ packages/pieces/salsa/src/index.ts
@@ -1,13 +1,20 @@
 
-import { createPiece } from '@activepieces/pieces-framework';
+import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
 import { salsaSupporterSearch } from "./lib/actions/supporter-search";
 import { salsaSupporterUpsert } from "./lib/actions/supporter-upsert";
 import { salsaOfflineDonationUpsert } from "./lib/actions/offline-donation-upsert";
+
+export const salsaAuth = PieceAuth.SecretText({
+    displayName: "API Key",
+    required: true,
+    description: "API key acquired from your Salsa crm"
+})
 
 export const salsa = createPiece({
   displayName: "Salsa",
   logoUrl: "https://cdn.activepieces.com/pieces/salsa.png",
   authors: ['mnatanek'],
+  auth: salsaAuth,
   actions: [
     salsaSupporterSearch,
     salsaSupporterUpsert,
--- packages/pieces/salsa/src/lib/actions/offline-donation-upsert.ts
+++ packages/pieces/salsa/src/lib/actions/offline-donation-upsert.ts
@@ -1,21 +1,24 @@
 import { createAction, Property } from '@activepieces/pieces-framework';
 import { salsaCommon } from '../common/common';
+import { salsaAuth } from '../..';
 import { upsertOfflineDonation } from './offline-donation';
 
 export const salsaOfflineDonationUpsert = createAction({
-    name: 'offline_donation_upsert',
-    description: 'Upsert Offline Donation based on Email',
-    displayName: 'Upsert Offline Donation',
-    props: {
-        authentication: salsaCommon.authentication,
-        baseUrl: salsaCommon.baseUrl,
-        donations: Property.Json({
-            displayName: "Offline Donation (JSON Array)",
-            description: "The Offline Donation JSON Array",
-            required: true
-        })
-    },
-    async run(context) {
-        return await upsertOfflineDonation(context.propsValue);
+    auth: salsaAuth,
+    action: {
+        name: 'offline_donation_upsert',
+        description: 'Upsert Offline Donation based on Email',
+        displayName: 'Upsert Offline Donation',
+        props: {
+            baseUrl: salsaCommon.baseUrl,
+            donations: Property.Json({
+                displayName: "Offline Donation (JSON Array)",
+                description: "The Offline Donation JSON Array",
+                required: true
+            })
+        },
+        async run(context) {
+            return await upsertOfflineDonation(context.propsValue);
+        },
     },
 });
