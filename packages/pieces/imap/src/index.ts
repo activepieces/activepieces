d02dc299181a350787831f36169cf20ce494f321
Khaled Mashaly <kmmashaly@gmail.com>
2023-07-04 18:10:40 +0300

    chore(pieces-imap): upgrade to piece level auth

--- packages/pieces/imap/src/index.ts
+++ packages/pieces/imap/src/index.ts
@@ -1,11 +1,46 @@
-import { createPiece } from "@activepieces/pieces-framework";
+import { PieceAuth, Property, createPiece } from "@activepieces/pieces-framework";
 
 import { newEmail } from "./lib/triggers/new-email";
+
+export const imapAuth = PieceAuth.CustomAuth({
+    displayName: 'Authentication',
+    description: 'Enter your IMAP server authentication details',
+    props: {
+        host: Property.ShortText({
+            displayName: 'Host',
+            description: 'The host of the IMAP server',
+            required: true,
+        }),
+        username: Property.ShortText({
+            displayName: 'Username',
+            description: 'The username of the IMAP server',
+            required: true,
+        }),
+        password: PieceAuth.SecretText({
+            displayName: 'Password',
+            description: 'The password of the IMAP server',
+            required: true,
+        }),
+        port: Property.Number({
+            displayName: 'Port',
+            description: 'The port of the IMAP server',
+            required: true,
+            defaultValue: 143,
+        }),
+        tls: Property.Checkbox({
+            displayName: 'Use TLS',
+            defaultValue: false,
+            required: true,
+        }),
+    },
+    required: true
+})
 
 export const imap = createPiece({
     displayName: "IMAP",
     logoUrl: "https://cdn.activepieces.com/pieces/imap.png",
     authors: ['MoShizzle'],
+    auth: imapAuth,
     actions: [],
     triggers: [newEmail],
 });
--- packages/pieces/imap/src/lib/triggers/new-email.ts
+++ packages/pieces/imap/src/lib/triggers/new-email.ts
@@ -1,20 +1,21 @@
-import { createTrigger } from '@activepieces/pieces-framework';
+import { PiecePropValueSchema, createTrigger } from '@activepieces/pieces-framework';
 import { TriggerStrategy } from "@activepieces/pieces-framework";
 import { DedupeStrategy, Polling, pollingHelper } from '@activepieces/pieces-common';
 
 import { imapCommon } from '../common';
 
 import dayjs from 'dayjs';
+import { imapAuth } from '../..';
 
-const polling: Polling<{ authentication: any, subject: string | undefined, to: string | undefined, from: string | undefined }> = {
+const polling: Polling<PiecePropValueSchema<typeof imapAuth>, { subject: string | undefined, to: string | undefined, from: string | undefined }> = {
     strategy: DedupeStrategy.TIMEBASED,
-    items: async ({ propsValue, lastFetchEpochMS }) => {
+    items: async ({ auth, propsValue, lastFetchEpochMS }) => {
         const imapConfig = {
-            host: propsValue.authentication.host,
-            user: propsValue.authentication.username,
-            password: propsValue.authentication.password,
-            port: propsValue.authentication.port,
-            tls: propsValue.authentication.tls
+            host: auth.host,
+            user: auth.username,
+            password: auth.password,
+            port: auth.port,
+            tls: auth.tls
         };
 
         // When generating sample data, lastFetchEpochMS is 0, which breaks the trigger
@@ -37,71 +38,77 @@ const polling: Polling<{ authentication: any, subject: string | undefined, to: s
 };
 
 export const newEmail = createTrigger({
-    name: 'new_email',
-    displayName: 'New Email',
-    description: 'Trigger when a new email is received.',
-    props: {
-        authentication: imapCommon.authentication,
-        subject: imapCommon.subject,
-        to: imapCommon.to,
-        from: imapCommon.from
-    },
-    type: TriggerStrategy.POLLING,
-    onEnable: async (context) => {
-        await pollingHelper.onEnable(polling, {
-            store: context.store,
-            propsValue: context.propsValue,
-        })
-    },
-    onDisable: async (context) => {
-        await pollingHelper.onDisable(polling, {
-            store: context.store,
-            propsValue: context.propsValue,
-        })
-    },
-    run: async (context) => {
-        return await pollingHelper.poll(polling, {
-            store: context.store,
-            propsValue: context.propsValue,
-        });
-    },
-    test: async (context) => {
-        return await pollingHelper.test(polling, {
-            store: context.store,
-            propsValue: context.propsValue,
-        });
-    },
+    auth: imapAuth,
+    trigger: {
+        name: 'new_email',
+        displayName: 'New Email',
+        description: 'Trigger when a new email is received.',
+        props: {
+            subject: imapCommon.subject,
+            to: imapCommon.to,
+            from: imapCommon.from
+        },
+        type: TriggerStrategy.POLLING,
+        onEnable: async (context) => {
+            await pollingHelper.onEnable(polling, {
+                auth: context.auth,
+                store: context.store,
+                propsValue: context.propsValue,
+            })
+        },
+        onDisable: async (context) => {
+            await pollingHelper.onDisable(polling, {
+                auth: context.auth,
+                store: context.store,
+                propsValue: context.propsValue,
+            })
+        },
+        run: async (context) => {
+            return await pollingHelper.poll(polling, {
+                auth: context.auth,
+                store: context.store,
+                propsValue: context.propsValue,
+            });
+        },
+        test: async (context) => {
+            return await pollingHelper.test(polling, {
+                auth: context.auth,
+                store: context.store,
+                propsValue: context.propsValue,
+            });
+        },
 
-    sampleData: {
-        html: 'My email body',
-        text: 'My email body',
-        textAsHtml: '<p>My email body</p>',
-        subject: 'Email Subject',
-        date: '2023-06-18T11:30:09.000Z',
-        to: {
-            value: [
-                {
-                    address: 'email@address.com',
-                    name: 'Name'
-                }
-            ]
-        },
-        from: {
-            value: [
-                {
-                    address: 'email@address.com',
-                    name: 'Name'
-                }
-            ]
-        },
-        cc: {
-            value: [
-                {
-                    address: 'email@address.com',
-                    name: 'Name'
-                }
-            ]
-        },
-        messageId: '<CxE49ifJT5YZN9OE2O6j6Ef+BYgkKWq7X-deg483GkM1ui1xj3g@mail.gmail.com>'
+        sampleData: {
+            html: 'My email body',
+            text: 'My email body',
+            textAsHtml: '<p>My email body</p>',
+            subject: 'Email Subject',
+            date: '2023-06-18T11:30:09.000Z',
+            to: {
+                value: [
+                    {
+                        address: 'email@address.com',
+                        name: 'Name'
+                    }
+                ]
+            },
+            from: {
+                value: [
+                    {
+                        address: 'email@address.com',
+                        name: 'Name'
+                    }
+                ]
+            },
+            cc: {
+                value: [
+                    {
+                        address: 'email@address.com',
+                        name: 'Name'
+                    }
+                ]
+            },
+            messageId: '<CxE49ifJT5YZN9OE2O6j6Ef+BYgkKWq7X-deg483GkM1ui1xj3g@mail.gmail.com>'
+        }
     }
 });
