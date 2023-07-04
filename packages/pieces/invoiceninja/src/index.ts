2962fe3153f6519fc4ca52f55d6421d6a151b871
Khaled Mashaly <kmmashaly@gmail.com>
2023-07-04 16:15:52 +0300

    chore(pieces-invoiceninja): upgrade to piece level auth

--- packages/pieces/invoiceninja/src/index.ts
+++ packages/pieces/invoiceninja/src/index.ts
@@ -1,11 +1,28 @@
-
-import { createPiece } from "@activepieces/pieces-framework";
+import { PieceAuth, Property, createPiece } from "@activepieces/pieces-framework";
 import { createTask } from './lib/actions/create-task';
+
+export const invoiceninjaAuth = PieceAuth.CustomAuth({
+  displayName: 'Custom Authentication',
+  props: {
+      base_url: Property.ShortText({
+          displayName: 'Base URL',
+          description: 'Enter the base URL',
+          required: true,
+      }),
+      access_token: Property.LongText({
+          displayName: 'API Token',
+          description: 'Enter the API token',
+          required: true,
+      })
+  },
+  required: true
+})
 
 export const invoiceninja = createPiece({
   displayName: "Invoice Ninja",
   logoUrl: "https://cdn.activepieces.com/pieces/invoiceninja.png",
   authors: ["buttonsbond"],
+  auth: invoiceninjaAuth,
   actions: [createTask],
   triggers: [],
 });
