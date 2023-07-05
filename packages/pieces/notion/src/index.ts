a3e9be6a711a4f84d86136289c1651cac56cb5e7
Abdallah-Alwarawreh <abooddevyt@outlook.com>
2023-07-05 15:09:24 +0000

    feat(notion): enhanced notion piece

--- packages/pieces/notion/src/index.ts
+++ packages/pieces/notion/src/index.ts
@@ -1,18 +1,29 @@
 
 import { createPiece } from '@activepieces/pieces-framework';
 import { newDatabaseItem } from './lib/triggers/new-database-item';
+import { updatedDatabaseItem } from './lib/triggers/updated-database-item';
+import createDatabaseItem from './lib/actions/create-database-item';
+import createPage from './lib/actions/create-page';
+import updateDatabaseItem from './lib/actions/update-database-item';
+import findDatabaseItem from './lib/actions/find-database-item';
+import findPage from './lib/actions/find-page';
 
 export const notion = createPiece({
   displayName: 'Notion',
   logoUrl: 'https://cdn.activepieces.com/pieces/notion.png',
   minimumSupportedRelease: '0.3.10',
   authors: [
-    'ShayPunter', 'abuaboud'
+    'ShayPunter', 'abuaboud', 'abdallah-alwarawreh'
   ],
   actions: [
-
+    updateDatabaseItem,
+    createDatabaseItem,
+    findDatabaseItem,
+    findPage,
+    createPage
   ],
   triggers: [
-    newDatabaseItem
+    newDatabaseItem,
+    updatedDatabaseItem
   ],
 });
---
+++ packages/pieces/notion/src/lib/actions/create-page.ts
@@ -0,0 +1,55 @@
+import { createAction, Property } from "@activepieces/pieces-framework";
+import { notionCommon } from "../common";
+import { Client } from "@notionhq/client";
+
+export default createAction({
+    name: 'create_page',
+    displayName: 'Create Page',
+    description: 'Creates a page',
+    props: {
+        authentication: notionCommon.authentication,
+        parent_page: notionCommon.parent_page,
+        title: Property.ShortText({
+            displayName: 'Title',
+            description: 'The title of the page to create',
+            required: true
+        }),
+        content: Property.ShortText({
+            displayName: 'Content',
+            description: 'The content of the page to create',
+            required: true
+        })
+    },
+    async run(context) {
+        const parentPageId = context.propsValue.parent_page;
+        const title = context.propsValue.title;
+        const content = context.propsValue.content;
+        if (!parentPageId) throw new Error('Parent Page ID is required');
+
+        const notion = new Client({
+            auth: context.propsValue.authentication.access_token,
+            notionVersion: "2022-02-22",
+        });
+
+        const response = await notion.pages.create({
+            parent: {
+                page_id: parentPageId
+            },
+            properties: {
+                title: {
+                    title: [
+                        {
+                            type: "text",
+                            text: {
+                                content: title
+                            }
+                        }
+                    ]
+                },
+            },
+        });
+
+        return { response }
+
+    }
+})
