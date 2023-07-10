2eae4ac20984172adf2da473678ce6c976e69c6b
Khaled Mashaly <kmmashaly@gmail.com>
2023-07-10 14:53:03 +0300

    fix(approval): update icon to svg

--- packages/pieces/approval/src/index.ts
+++ packages/pieces/approval/src/index.ts
@@ -4,7 +4,7 @@ import { approvalAction } from "./lib/actions/approval";
 export const approval = createPiece({
   displayName: "Approval",
   auth: PieceAuth.None(),
-  logoUrl: "https://cdn.activepieces.com/pieces/approval.png",
+  logoUrl: "https://cdn.activepieces.com/pieces/approval.svg",
   authors: ['abuaboud'],
   actions: [approvalAction],
   triggers: [],
