5ee6ee62829a96c07ebd7c4b870069fdaf8f61b6
Abdallah-Alwarawreh <abooddevyt@outlook.com>
2023-07-11 14:03:12 +0000

    fix(gravity-forms): added md tutorial

--- packages/pieces/gravityforms/src/lib/triggers/new-submission.ts
+++ packages/pieces/gravityforms/src/lib/triggers/new-submission.ts
@@ -1,12 +1,20 @@
 import { createTrigger, Property, TriggerStrategy } from '@activepieces/pieces-framework';
 
+
+const markdown = `
+Go to plugins and activate the webhook plugin.
+Then go to the form you want to trigger on and add a webhook to it.
+The webhook url should be {{webhookUrl}}.
+You can leave the other settings as default.
+`
+
 export const gravityFormsNewSubmission = createTrigger({
     name: 'gravityforms-new-submission',
     displayName: 'New Submission',
     description: 'Triggers when form receives a new submission',
     props: {
         md: Property.MarkDown({
-            description: "{{webhookUrl}}",
+            description: markdown,
             required: true,
         }),
     },
