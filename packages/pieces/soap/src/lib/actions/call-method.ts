e88021172c162bed39ce617ea505ebf438831aa0
x7airworker <jake@jakes-co.de>
2023-07-14 20:00:21 +0000

    feat(soap): added dynamic arguments

--- packages/pieces/soap/src/lib/actions/call-method.ts
+++ packages/pieces/soap/src/lib/actions/call-method.ts
@@ -1,5 +1,8 @@
-import soap from "soap";
-import { Property, createAction } from "@activepieces/pieces-framework";
+import * as soap from "soap";
+import { Property, ShortTextProperty, StaticDropdownProperty, StaticMultiSelectDropdownProperty, createAction } from "@activepieces/pieces-framework";
+
+
+type DynamicProp = ShortTextProperty<boolean> | StaticDropdownProperty<any, boolean> | StaticMultiSelectDropdownProperty<any,boolean>;
 
 export const callMethod = createAction({
     name: 'call_method',
@@ -38,11 +41,35 @@ export const callMethod = createAction({
                     })
                 }
             }
+        }),
+        args: Property.DynamicProperties({
+            description: 'Arguments for the SOAP method',
+            displayName: 'Parameters',
+            required: true,
+            refreshers: ['wsdl', 'method'],
+            async props({wsdl, method}) {
+                const client = await soap.createClientAsync(wsdl as unknown as string);
+                const spec = client.describe();
+                const methods = Object.values(Object.values(spec)[0] as object)[0];
+
+                const properties: Record<string, DynamicProp > = {};
+
+                for (const key in methods[method as unknown as string]['input']) {
+                    properties[key as string] = Property.ShortText({
+                        displayName: key,
+                        required: true
+                    });
+                }
+
+                return properties;
+            }
         })
     },
     async run (ctx) {
-        const { wsdl } = ctx.propsValue;
+        const { wsdl, method, args } = ctx.propsValue;
         
         const client = await soap.createClientAsync(wsdl);
+        
+        return await client[method + 'Async'](args);
     }
 });
