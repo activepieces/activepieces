fd6f3f504dfafeeb82e321c146355c571e0d8824
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-07-20 03:26:26 +0200

    fix: add soap piece png

--- packages/pieces/soap/src/index.ts
+++ packages/pieces/soap/src/index.ts
@@ -7,7 +7,7 @@ export const soap = createPiece({
   displayName: "SOAP",
   auth: soapAuth(),
   minimumSupportedRelease: '0.5.0',
-  logoUrl: "https://www.flowgear.net/wp-content/uploads/2019/03/soap-request.png",
+  logoUrl: "https://cdn.activepieces.com/pieces/soap.png",
   authors: ["x7airworker"],
   actions: [
     callMethod
