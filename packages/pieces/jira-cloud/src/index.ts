f3f1eed05c10ec1b3569eac2942a395277a7a8e3
Moayyad Shaddad <shaddadmoayyad@gmail.com>
2023-08-03 20:44:36 +0300

    feat: init

--- packages/pieces/jira-cloud/src/index.ts
+++ packages/pieces/jira-cloud/src/index.ts
@@ -4,15 +4,18 @@ import { newWorklog } from "./lib/triggers/new-worklog";
 
 export const jiraCloudAuth = PieceAuth.OAuth2({
     displayName: 'Authorization',
-    authUrl: 'https://auth.atlassian.com/authorize?audience=api.atlassian.com',
+    authUrl: 'https://auth.atlassian.com/authorize',
     tokenUrl: 'https://auth.atlassian.com/oauth/token',
     required: true,
-    scope: ['read:jira-work', 'manage:jira-webhook'],
+    scope: ['read:jira-work', 'manage:jira-webhook', 'read:me', 'read:account'],
     props: {
         siteUrl: Property.ShortText({
             displayName: 'Site URL',
             required: true
         })
+    },
+    extra: {
+        audience: 'api.atlassian.com'
     }
 });
 
