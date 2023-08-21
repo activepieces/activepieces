df8ca50092841c64693335f02bfcb7788004ad7f
Owlcept <owlcept@owlcept.com>
2023-08-20 20:57:15 -0500

    fix: update

--- packages/pieces/google-slides/src/index.ts
+++ packages/pieces/google-slides/src/index.ts
@@ -1,12 +1,22 @@
 
 import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { createPresentation } from "./lib/actions/create-presentation";
+
+export const googleSlidesAuth = PieceAuth.OAuth2({
+  description: "",
+  
+  authUrl: "https://accounts.google.com/o/oauth2/auth",
+  tokenUrl: "https://oauth2.googleapis.com/token",
+  required: true,
+  scope: ["https://www.googleapis.com/auth/presentations", "https://www.googleapis.com/auth/drive.readonly"]
+})
 
 export const googleSlides = createPiece({
-  displayName: "Google-slides",
-  auth: PieceAuth.None(),
+  displayName: "Google-Slides",
+  auth: googleSlidesAuth,
   minimumSupportedRelease: '0.7.1',
   logoUrl: "https://cdn.activepieces.com/pieces/google-slides.png",
-  authors: [],
-  actions: [],
+  authors: ['Owlcept'],
+  actions: [createPresentation],
   triggers: [],
 });
---
+++ packages/pieces/google-slides/src/lib/actions/create-presentation.ts
@@ -0,0 +1,37 @@
+import { googleSlidesAuth } from "../..";
+import { createAction, Property, OAuth2PropertyValue } from "@activepieces/pieces-framework";
+import { HttpMethod, httpClient, HttpRequest, AuthenticationType } from "@activepieces/pieces-common";
+
+
+export const createPresentation = createAction({
+    auth: googleSlidesAuth,
+    name: "create_presentation",
+    description: "Create a new presentation",
+    displayName: "Create Presentation",
+    props: {
+        title: Property.ShortText({
+            displayName: "Title",
+            description: "Title of the presentation",
+            required: false
+        }),
+
+    },
+    async run(context) {
+        const baseUrl = "https://slides.googleapis.com/v1/presentations";
+        const authProp: OAuth2PropertyValue = context.auth as OAuth2PropertyValue;
+        const { title } = context.propsValue;
+        const request: HttpRequest = {
+            method: HttpMethod.POST,
+            url: baseUrl,
+            authentication: {
+                type: AuthenticationType.BEARER_TOKEN,
+                token: authProp['access_token'],
+            },
+            body: {
+                "title":title
+            }
+        }
+        return await httpClient.sendRequest(request);
+
+    }
+})
