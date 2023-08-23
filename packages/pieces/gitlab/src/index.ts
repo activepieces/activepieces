b2a0dbd466fa519afcbf904c826d9baab27fb067
Kishan Parmar <code.kishanparmar@gmail.com>
2023-08-23 09:45:20 +0530

    feat(gitlab): adds Gitlab OAuth2 authentication

--- packages/pieces/gitlab/src/index.ts
+++ packages/pieces/gitlab/src/index.ts
@@ -1,11 +1,16 @@
-
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+export const gitlabAuth = PieceAuth.OAuth2({
+  required: true,
+  authUrl: 'https://gitlab.com/oauth/authorize',
+  tokenUrl: 'https://gitlab.com/oauth/token',
+  scope: ['api', 'read_user'],
+});
 
 export const gitlab = createPiece({
-  displayName: "Gitlab",
-  auth: PieceAuth.None(),
+  displayName: 'Gitlab',
+  auth: gitlabAuth,
   minimumSupportedRelease: '0.7.1',
-  logoUrl: "https://cdn.activepieces.com/pieces/gitlab.png",
+  logoUrl: 'https://cdn.activepieces.com/pieces/gitlab.png',
   authors: [],
   actions: [],
   triggers: [],
