75ff9ea4ac01efeebfc49c988948854c2fbe5037
Salem-Alaa <salem-alaa@outlook.com>
2023-08-24 17:04:00 +0000

    feat: adding sendfox actions

---
+++ packages/pieces/sendfox/src/index.ts
@@ -0,0 +1,29 @@
+
+import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { createList } from "./lib/actions/create-list";
+import { unsubscribe } from "./lib/actions/unsubscribe-contact";
+import { createContact } from "./lib/actions/create-contact";
+
+export const sendfoxAuth = PieceAuth.SecretText({
+    displayName: 'API Key',
+    description: `
+    To obtain your personal token, follow these steps:
+
+    1. Log in to your SendFox account.
+    2. Visit https://sendfox.com/account/oauth to create one
+    3. From OAuth Apps click on Create New Token.
+    4. Enter any name you want then click create.
+    5. Copy and paste your token here.
+    `,
+    required: true,
+});
+
+export const sendfox = createPiece({
+  displayName: "Sendfox",
+  auth: sendfoxAuth,
+  minimumSupportedRelease: '0.7.1',
+  logoUrl: "https://cdn.activepieces.com/pieces/sendfox.png",
+  authors: ["Salem-Alaa"],
+  actions: [ createList , unsubscribe , createContact ],
+  triggers: [],
+});
