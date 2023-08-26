95dce425e50a92609c9999165d00742f994bd23c
Moayyad Shaddad <shaddadmoayyad@gmail.com>
2023-08-26 22:34:59 +0300

    chore: markdown

--- packages/pieces/whatsapp-business/src/index.ts
+++ packages/pieces/whatsapp-business/src/index.ts
@@ -4,7 +4,16 @@ import { sendMessage } from "./lib/actions/send-message";
 import { newMessage } from "./lib/triggers/new-message";
 import { sendReply } from "./lib/actions/send-reply";
 
+const authMarkdown = `
+To get your WhatsApp Business ID, follow the steps below:
+1. Go to Business Manager -> Settings
+2. Under Accounts, click WhatsApp Business Accounts
+3. Click on the account you want to connect
+4. Copy the WhatsApp Account ID from the summary
+`;
+
 export const whatappBusinessAuth = PieceAuth.OAuth2({
+    description: authMarkdown,
     authUrl: "https://graph.facebook.com/oauth/authorize",
     tokenUrl: "https://graph.facebook.com/oauth/access_token",
     required: true,
