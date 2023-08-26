cda0cc368d7517ad1cd42b325987c107d2ae813b
Moayyad Shaddad <shaddadmoayyad@gmail.com>
2023-08-26 22:25:32 +0300

    feat: trigger

--- packages/pieces/whatsapp-business/src/lib/triggers/new-message.ts
+++ packages/pieces/whatsapp-business/src/lib/triggers/new-message.ts
@@ -8,14 +8,13 @@ export const newMessage = createTrigger({
     displayName: 'New Message',
     description: 'Triggers when a new message is received',
     type: TriggerStrategy.APP_WEBHOOK,
-    sampleData: {},
     props: {},
 
     async onEnable(context) {
         await whatsappBusinessCommon.subscribeWhatsappToApp((context.auth.props as any)['whatsappBusinessAccountId'], context.auth.access_token)
-        
+
         // context.app.createListeners({ events: ['whatsappBusinessMessage'], identifierValue: context.auth.props!['whatsappBusinessAccountId'] as string })
-        context.app.createListeners({ events: ['whatsappBusinessMessage'], identifierValue: context.auth.props!['whatsappBusinessAccountId'] as string })
+        context.app.createListeners({ events: ['whatsappBusinessMessage'], identifierValue: '123456123' })
     },
 
     async onDisable() {
@@ -25,5 +24,45 @@ export const newMessage = createTrigger({
     //Return new lead
     async run(context) {
         return [context.payload.body];
+    },
+    
+    sampleData: {
+        "object": "whatsapp_business_account",
+        "entry": [
+            {
+                "id": "0",
+                "changes": [
+                    {
+                        "field": "messages",
+                        "value": {
+                            "messaging_product": "whatsapp",
+                            "metadata": {
+                                "display_phone_number": "16505551111",
+                                "phone_number_id": "123456123"
+                            },
+                            "contacts": [
+                                {
+                                    "profile": {
+                                        "name": "test user name"
+                                    },
+                                    "wa_id": "16315551181"
+                                }
+                            ],
+                            "messages": [
+                                {
+                                    "from": "16315551181",
+                                    "id": "ABGGFlA5Fpa",
+                                    "timestamp": "1504902988",
+                                    "type": "text",
+                                    "text": {
+                                        "body": "this is a text message"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     },
 })
