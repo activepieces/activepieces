0d709ba753805bd56ebe318e8a1e205a7610b747
Salem-Alaa <salem-alaa@outlook.com>
2023-08-27 02:51:52 +0000

    feat: sendfox piece

--- packages/pieces/sendfox/src/index.ts
+++ packages/pieces/sendfox/src/index.ts
@@ -3,7 +3,6 @@ import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
 import { createList } from "./lib/actions/create-list";
 import { unsubscribe } from "./lib/actions/unsubscribe-contact";
 import { createContact } from "./lib/actions/create-contact";
-
 export const sendfoxAuth = PieceAuth.SecretText({
     displayName: 'API Key',
     description:"To obtain your personal token, follow these steps:\n1. Log in to your SendFox account.\n2. Visit https://sendfox.com/account/oauth to create one\n3. From OAuth Apps click on Create New Token.\n4. Enter any name you want then click create.\n5. Copy and paste your token here.",
