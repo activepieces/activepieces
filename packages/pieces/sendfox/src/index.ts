152bfd6433209da6f3113c6f8f54f4bea23d7afb
Salem-Alaa <salem-alaa@outlook.com>
2023-08-27 02:46:04 +0000

    feat(sendfox): create list / create contact / unsubscribe contact action

--- packages/pieces/sendfox/src/index.ts
+++ packages/pieces/sendfox/src/index.ts
@@ -6,15 +6,7 @@ import { createContact } from "./lib/actions/create-contact";
 
 export const sendfoxAuth = PieceAuth.SecretText({
     displayName: 'API Key',
-    description: `
-    To obtain your personal token, follow these steps:
-
-    1. Log in to your SendFox account.
-    2. Visit https://sendfox.com/account/oauth to create one
-    3. From OAuth Apps click on Create New Token.
-    4. Enter any name you want then click create.
-    5. Copy and paste your token here.
-    `,
+    description:"To obtain your personal token, follow these steps:\n1. Log in to your SendFox account.\n2. Visit https://sendfox.com/account/oauth to create one\n3. From OAuth Apps click on Create New Token.\n4. Enter any name you want then click create.\n5. Copy and paste your token here.",
     required: true,
 });
 
