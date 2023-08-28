557d60c47936c39a44fb1f6e6e03a26e14a45340
Armand Giauffret 3 <142923353+Armangiau3@users.noreply.github.com>
2023-08-28 15:01:37 +0000

    fix(extract-text): adding text extraction action to index.ts

--- packages/pieces/extract-text/src/index.ts
+++ packages/pieces/extract-text/src/index.ts
@@ -1,12 +1,13 @@
 
 import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { nativeTextExtraction } from "./lib/actions/native-text-extraction";
 
 export const extractText = createPiece({
   displayName: "Extract-text",
   auth: PieceAuth.None(),
   minimumSupportedRelease: '0.8.0',
   logoUrl: "https://cdn.activepieces.com/pieces/extract-text.png",
-  authors: [],
-  actions: [],
+  authors: ['ArmanGiau'],
+  actions: [nativeTextExtraction],
   triggers: [],
 });
