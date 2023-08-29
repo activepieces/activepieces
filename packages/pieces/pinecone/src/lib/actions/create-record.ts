691ed6b78fea90dfbe80d81bb66e1d64a4c7af4f
Franck Nijimbere <nijimbere.franck@gmail.com>
2023-08-29 16:27:14 +0200

    chore: nits

--- packages/pieces/pinecone/src/lib/actions/create-record.ts
+++ packages/pieces/pinecone/src/lib/actions/create-record.ts
@@ -13,35 +13,6 @@ export const pineconeCreateRecordAction = createAction({
   async run(context) {
     const apiKey = context.auth
     const index = context.propsValue
-    
-    const fieldsWithoutEmptyStrings: DynamicPropsValue = {}
-    try {
-      const response = await httpClient.sendRequest<string[]>({
-          method: HttpMethod.GET,
-          url: "https://controller.us-central1-gcp.pinecone.io/databases",
-          authentication: {
-              type: AuthenticationType.API_KEY, // The actual type will depend on how your httpClient is implemented
-              apiKey: auth as string
-            }
-        })
-        if (response.status === 200) {
-          return {
-            disabled: false,
-            options: response.body.map((optionName) => {
-              return { value: optionName, label: optionName };
-            })
-          }
-        }
-    } catch (e) {
-      console.debug(e)
-      return {
-        disabled: true,
-        options: [],
-        placeholder: "Please check your permission scope"
-      }
-    }
-
-    
     return index;
   },
 })
