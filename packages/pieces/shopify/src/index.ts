8d456150764ed93e44d3271e1c1dc5b4115280ab
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-08-29 17:36:22 +0200

    fix: shopify authentication

--- packages/pieces/shopify/src/index.ts
+++ packages/pieces/shopify/src/index.ts
@@ -4,27 +4,62 @@ import { newCancelledOrder } from './lib/triggers/new-cancelled-order';
 import { newCustomer } from './lib/triggers/new-customer';
 import { newOrder } from './lib/triggers/new-order';
 import { newPaidOrder } from './lib/triggers/new-paid-order';
+import { HttpMethod, httpClient } from '@activepieces/pieces-common';
 
-export const shopifyAuth = PieceAuth.OAuth2({
-  props: {
-      shop: Property.ShortText({
-          displayName: 'Shop Name',
-          description: 'Shop Name',
-          required: true
-      })
-  },
-  
-  description: 'Authentication for the webhook',
+const markdown = `
+To Obtain an Admin Token, follow these steps:
+
+1. Login to your Shopify account
+2. Go to Settings -> Apps
+3. Click on Develop apps
+4. Create an App
+5. Fill the app name
+6. Click on Configure Admin API Scopes (Select the following scopes 'read_orders', 'read_customers')
+7. Click on Install app
+8. Copy the Admin Access Token
+
+**Shop Name**
+1- You can find your shop name in the url For example, if the URL is https://example.myshopify.com/admin, then your shop name is **example**.
+`
+
+export const shopifyAuth = PieceAuth.CustomAuth({
+  description: markdown,
   required: true,
-  authUrl: "https://{shop}.myshopify.com/admin/oauth/authorize",
-  tokenUrl: "https://{shop}.myshopify.com/admin/oauth/access_token",
-  scope: ['read_orders', 'read_customers']
+  props: {
+    shopName: Property.ShortText({
+      displayName: 'Shop Name',
+      required: true
+    }),
+    adminToken: PieceAuth.SecretText({
+      displayName: 'Admin Token',
+      required: true
+    })
+  },
+  validate: async ({ auth }) => {
+    const { shopName, adminToken } = auth;
+    try {
+      const response = await httpClient.sendRequest({
+        method: HttpMethod.GET,
+        url: `https://${shopName}.myshopify.com/admin/api/2023-01/shop.json`,
+        headers: {
+          "X-Shopify-Access-Token": adminToken
+        }
+      })
+      return {
+        valid: true
+      }
+    } catch (e) {
+      return {
+        valid: false,
+        error: 'Invalid Shop Name or Admin Token'
+      }
+    }
+  },
 })
 
 export const shopify = createPiece({
   displayName: 'Shopify',
-
-    logoUrl: 'https://cdn.activepieces.com/pieces/shopify.png',
+  logoUrl: 'https://cdn.activepieces.com/pieces/shopify.png',
   authors: [
     "abuaboud"
   ],
