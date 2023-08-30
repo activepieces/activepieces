1b57b83422c519596910ac836ea0efa42b9bebd6
Armand Giauffret 3 <142923353+Armangiau3@users.noreply.github.com>
2023-08-30 17:58:45 +0000

    fix(qdrant): replace short text by files for searching points

--- packages/pieces/qdrant/src/lib/actions/search-points.ts
+++ packages/pieces/qdrant/src/lib/actions/search-points.ts
@@ -10,13 +10,13 @@ export const searchPoints = createAction({
   description: 'Search for points closest to your given vector (= embedding)',
   props: {
     collectionName,
-    vector: Property.ShortText({
+    vector: Property.File({
       displayName: 'Embedding',
       required: true,
       description: 'The vector (= embedding) you want to search for.',
     }),
     ...filteringProps,
-    negativeVector: Property.ShortText({
+    negativeVector: Property.File({
       displayName: 'Negative Vector',
       required: false,
       description: 'The vector (= embedding) you want to be the farthest.',
@@ -40,8 +40,8 @@ export const searchPoints = createAction({
       must_not,
     });
 
-    let vector = Array.from(decodeEmbeddings(propsValue.vector)[0])
-    const negativeVector = propsValue.negativeVector ? Array.from(decodeEmbeddings(propsValue.negativeVector)[0]) : undefined 
+    let vector = Array.from(decodeEmbeddings(propsValue.vector.data)[0])
+    const negativeVector = propsValue.negativeVector ? Array.from(decodeEmbeddings(propsValue.vector.data)[0]) : undefined 
 
     if (
       !(vector instanceof Array) ||
@@ -55,8 +55,7 @@ export const searchPoints = createAction({
       // math func on: https://qdrant.tech/documentation/concepts/search/?selector=aHRtbCA%2BIGJvZHkgPiBkaXY6bnRoLW9mLXR5cGUoMSkgPiBzZWN0aW9uID4gZGl2ID4gZGl2ID4gZGl2ID4gYXJ0aWNsZSA%2BIGgyOm50aC1vZi10eXBlKDUp
       vector = vector.map((vec, i) => vec*2 + negativeVector[i]);
     }
-    const collectionName = propsValue.collectionName as string
-    return await client.search(collectionName, {
+    return await client.search(propsValue.collectionName, {
       vector,
       filter,
       limit,
