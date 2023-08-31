2f643772454235e47d7919ce302ae7ff4d569fb8
Armand Giauffret 4 <143703129+ArmanGiau4@users.noreply.github.com>
2023-08-31 13:33:19 +0000

    fix(openai-embedding): do not parse JSON when it is plain text

--- packages/pieces/openai/src/lib/actions/generate-embeddings.ts
+++ packages/pieces/openai/src/lib/actions/generate-embeddings.ts
@@ -169,8 +169,12 @@ export const createEmbeddingsFromText = createAction({
       );
 
     const model = 'text-embedding-ada-002';
-    const textInput = JSON.parse(propsValue.textInput.data.toString('utf-8'));
+    let textInput = propsValue.textInput.data.toString('utf-8')
 
+    try {
+      textInput = JSON.parse(textInput);
+    } catch (e) {null}
+    
     if (
       typeof textInput !== 'string' &&
       !(Array.isArray(textInput) && typeof textInput[0] !== 'string')
