77c110c601f7da2290bedaea80bc5e090fb773d3
Armand Giauffret 4 <143703129+ArmanGiau4@users.noreply.github.com>
2023-09-02 11:21:08 +0000

    feat(extract-text): support text/plain data formats like .txt

--- packages/pieces/extract-text/src/lib/actions/native-text-extraction.ts
+++ packages/pieces/extract-text/src/lib/actions/native-text-extraction.ts
@@ -1,7 +1,8 @@
 import { Property, createAction } from '@activepieces/pieces-framework';
 import { parseOfficeAsync } from 'officeparser';
+import { lookup } from 'mime-types';
 import { getDocument, type PDFDocumentProxy } from 'pdfjs-dist';
-import { TextItem } from 'pdfjs-dist/types/src/display/api';
+import type { TextItem } from 'pdfjs-dist/types/src/display/api';
 
 export const nativeTextExtraction = createAction({
   name: 'native_text_extraction',
@@ -16,84 +17,107 @@ export const nativeTextExtraction = createAction({
     }),
     precisePdfBreakingLines: Property.Checkbox({
       displayName: 'Precise PDF Breaking Lines',
-      description: 'The pdf breaking lines annalisis is expensive but should works for small files',
+      description:
+        'The pdf breaking lines annalisis is expensive but should works for small files',
       defaultValue: false,
       required: false,
-    })
+    }),
   },
   run: async ({ propsValue, files }) => {
     const file = propsValue.file;
 
     const preciseBreaksAnalysis = async (doc: PDFDocumentProxy) => {
-      let extractText = ''
+      let extractText = '';
       for (let i = 0; i < doc.numPages; i++) {
-        const extractedChunks = {} as {[y: number]: { x: number; text: string }[]}; 
-        const textItems = (await (await doc.getPage(i)).getTextContent()).items
-        let lastY: number | undefined
+        const extractedChunks = {} as {
+          [y: number]: { x: number; text: string }[];
+        };
+        const textItems = (await (await doc.getPage(i)).getTextContent()).items;
+        let lastY: number | undefined;
         for (const item of textItems) {
-            if (!('str' in item)) continue
-            
-            const text = item.str
-            const breakLine = item.hasEOL
-            const x = item.transform[4] as number
-            let y = item.transform[5] as number
-            const h = item.height
+          if (!('str' in item)) continue;
 
-            // check if there is a real break line
-            // if the new chunk is upon or under (45% of the height if there a breakline specified and 100% else) the last chunk there is a break
-            if (lastY && ((breakLine && !(y-h*0.45 > lastY || y+h*0.45 < lastY)) || !(y-h > lastY || y+h < lastY))) {
-                y = lastY
-            } else {
-                lastY = y
-            }
+          const text = item.str;
+          const breakLine = item.hasEOL;
+          const x = item.transform[4] as number;
+          let y = item.transform[5] as number;
+          const h = item.height;
 
-            if (extractedChunks[y]) {
-                extractedChunks[y].push({ x, text })
-            } else {
-                extractedChunks[y] = [{ x, text }]
-            }
+          // check if there is a real break line
+          // if the new chunk is upon or under (45% of the height if there a breakline specified and 100% else) the last chunk there is a break
+          if (
+            lastY &&
+            ((breakLine && !(y - h * 0.45 > lastY || y + h * 0.45 < lastY)) ||
+              !(y - h > lastY || y + h < lastY))
+          ) {
+            y = lastY;
+          } else {
+            lastY = y;
+          }
+
+          if (extractedChunks[y]) {
+            extractedChunks[y].push({ x, text });
+          } else {
+            extractedChunks[y] = [{ x, text }];
+          }
         }
 
         for (const y in extractedChunks) {
-            extractText += extractedChunks[y].sort((a, b) => a.x - b.x).map(chunk => chunk.text).join('') + '\n' 
+          extractText +=
+            extractedChunks[y]
+              .sort((a, b) => a.x - b.x)
+              .map((chunk) => chunk.text)
+              .join('') + '\n';
         }
 
-        lastY = undefined
-        extractText += '\n'
+        lastY = undefined;
+        extractText += '\n';
       }
-      return extractText
-    }
+      return extractText;
+    };
 
     const fastbreakAnalysis = async (doc: PDFDocumentProxy) => {
       const numPages = doc.numPages;
       const pageTextPromises = [];
       for (let i = 1; i <= numPages; i++) {
-          const pagePromise = doc.getPage(i)
-              .then(page => page.getTextContent())
-              .then(content => content.items.map(item =>(item as TextItem).hasEOL ? (item as TextItem).str + '\n' : (item as TextItem).str).join(''));
-          pageTextPromises.push(pagePromise);
+        const pagePromise = doc
+          .getPage(i)
+          .then((page) => page.getTextContent())
+          .then((content) =>
+            content.items
+              .map((item) =>
+                (item as TextItem).hasEOL
+                  ? (item as TextItem).str + '\n'
+                  : (item as TextItem).str
+              )
+              .join('')
+          );
+        pageTextPromises.push(pagePromise);
       }
       const pageTexts = await Promise.all(pageTextPromises);
-      return pageTexts.join('\n')
-    }
+      return pageTexts.join('\n');
+    };
 
-    let extractTextResult: string
-    if (file.extension === 'pdf') {
+    let extractTextResult: string;
+    const extension = file.extension;
+    if (extension === 'pdf') {
       const doc = await getDocument({
         data: file.data.toString('binary'),
       }).promise;
-      if (propsValue.precisePdfBreakingLines){
-        extractTextResult = await preciseBreaksAnalysis(doc)
+      if (propsValue.precisePdfBreakingLines) {
+        extractTextResult = await preciseBreaksAnalysis(doc);
       } else {
-        extractTextResult = await fastbreakAnalysis(doc)
+        extractTextResult = await fastbreakAnalysis(doc);
       }
+    } else if (extension && lookup(extension) === 'text/plain') {
+      extractTextResult = Buffer.from(file.data).toString('utf-8');
     } else {
-        extractTextResult = await parseOfficeAsync(file.data)
+      extractTextResult = await parseOfficeAsync(file.data);
     }
 
     return await files.write({
       data: Buffer.from(extractTextResult, 'utf-8'),
-      fileName: file.filename + '.textract.txt'
-    })
+      fileName: file.filename + '.textract.txt',
+    });
   },
 });
