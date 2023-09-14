c7a7355717a306cd6381028ef955aaf39f89fbf7
Khaled Mashaly <kmmashaly@gmail.com>
2023-09-14 15:45:09 +0300

    feat(backend): add type and compression to file entity

--- packages/shared/src/lib/file/file.ts
+++ packages/shared/src/lib/file/file.ts
@@ -2,6 +2,8 @@ import { Static, Type } from "@sinclair/typebox";
 import {BaseModel} from "../common/base-model";
 import {ApId} from "../common/id-generator";
 import { ProjectId } from "../project/project";
+import { FileType } from "./file-type";
+import { FileCompression } from "./file-compression";
 
 export type FileId = ApId;
 
@@ -9,6 +11,8 @@ export type FileId = ApId;
 export interface File extends BaseModel<FileId> {
     data: Buffer;
     projectId: ProjectId;
+    type: FileType;
+    compression: FileCompression;
 }
 
 export const FileData = Type.Object({
