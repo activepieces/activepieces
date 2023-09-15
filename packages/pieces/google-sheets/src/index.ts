130cde0743a8b371f63a0bd3a280036cc6e78080
Bastien Meffre <meffre@kizeo.com>
2023-09-15 16:11:28 +0200

    feat: cancel unwanted space

--- packages/pieces/google-sheets/src/index.ts
+++ packages/pieces/google-sheets/src/index.ts
@@ -9,7 +9,6 @@ import { findRowByNumAction } from './lib/actions/find-row-by-num';
 
 export const googleSheetsAuth = PieceAuth.OAuth2({
     description: "",
-
     authUrl: "https://accounts.google.com/o/oauth2/auth",
     tokenUrl: "https://oauth2.googleapis.com/token",
     required: true,
