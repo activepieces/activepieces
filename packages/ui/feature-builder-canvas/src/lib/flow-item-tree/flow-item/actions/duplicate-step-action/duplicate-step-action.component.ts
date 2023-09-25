ad43d322c840717ccac4d1be0fd2efa0480efdef
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-09-25 12:51:18 +0300

    chore: added ui for duplicating steps

--- packages/ui/feature-builder-canvas/src/lib/flow-item-tree/flow-item/actions/duplicate-step-action/duplicate-step-action.component.ts
+++ packages/ui/feature-builder-canvas/src/lib/flow-item-tree/flow-item/actions/duplicate-step-action/duplicate-step-action.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { FlowItem } from '../../../../../../../feature-builder-store/src';
-
+import { BuilderSelectors, FlowItem } from '../../../../../../../feature-builder-store/src';
+import { forkJoin, take,tap } from 'rxjs';
+import { Store } from '@ngrx/store';
 @Component({
   selector: 'app-duplicate-step-action',
   templateUrl: './duplicate-step-action.component.html',
@@ -9,4 +10,21 @@ import { FlowItem } from '../../../../../../../feature-builder-store/src';
 export class DuplicateStepActionComponent {
   @Input({required:true})
   flowItem: FlowItem;
+  constructor(private store:Store){}
+  duplicateStep()
+  {
+  const x =  forkJoin({
+      currentFlow: this.store
+        .select(BuilderSelectors.selectCurrentFlow)
+        .pipe(take(1)),
+      rightSideBar: this.store
+        .select(BuilderSelectors.selectCurrentRightSideBar)
+        .pipe(take(1)),
+      currentStep: this.store
+        .select(BuilderSelectors.selectCurrentStep)
+        .pipe(take(1)),
+    }).pipe(tap(res=>{
+
+    }))
+  }
 }
