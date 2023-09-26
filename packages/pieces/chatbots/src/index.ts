0d6890f0b288aae1069150c60849ce32916b13fd
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-09-26 16:40:36 +0300

    chore: bump framework version

--- packages/pieces/chatbots/src/index.ts
+++ packages/pieces/chatbots/src/index.ts
@@ -2,7 +2,7 @@ import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 import { askBot } from './lib/ask-bot';
 
 export const activebots = createPiece({
-  displayName: 'Activebots',
+  displayName: 'Chatbots (Beta)',
   description: 'Use chatbots created in your Activepieces project',
   auth: PieceAuth.None(),
   minimumSupportedRelease: '0.9.0',
