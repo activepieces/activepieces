3182c024f7f9866d5ab16a4a5e0a9c206f1fc170
Shay Punter <icodinqs@gmail.com>
2023-10-01 17:30:47 +0000

    feat(brilliant-directories): add authentication

--- packages/pieces/brilliant-directories/src/index.ts
+++ packages/pieces/brilliant-directories/src/index.ts
@@ -1,42 +1,26 @@
 
 import { createPiece, PieceAuth, Property } from "@activepieces/pieces-framework";
 import { createNewUser } from "./lib/actions/create-new-user";
-import { httpClient, HttpMethod } from "@activepieces/pieces-common";
 
 export const brilliantDirectoriesAuth = PieceAuth.CustomAuth({
-  description: 'Enter your Brilliant Directories website URL & API Key',
   required: true,
+  description: `
+  Enter your Brilliant Directories website URL & API Key
+  `,
   props: {
-    site_url: Property.ShortText({
-      displayName: 'Site URL',
-      description: 'Your brilliant directories site URL',
+    token: PieceAuth.SecretText({
+      displayName: 'API Key',
+      description: 'The api key of the brilliant directories account account',
       required: true,
     }),
-    api_key: PieceAuth.SecretText({
-      displayName: 'API Key',
-      description: 'Your brilliant directories API key',
+    apiTableUrl: Property.ShortText({
+      displayName: 'Instance Url',
+      description: 'The url of the brillant directories instance.',
       required: true,
-    })
-  },
-//   validate: async ({auth}) => {
-//     // Validate the Site URL & API key using the verfy token endpoint
-//     const request = await httpClient.sendRequest<string[]>({
-//       url: auth.site_url + '/api/v2/token/verify',
-//       method: HttpMethod.GET
-//     });
-
-//     if (request.status === 200) {
-//       return {
-//         valid: true,
-//       }
-//     }
-
-//     return {
-//       valid: false,
-//       error: request.body
-//     }
-// }
-});
+      defaultValue: 'https://brilliant-directories.com/api',
+    }),
+  }
+})
 
 export const brilliantDirectories = createPiece({
   displayName: "Brilliant Directories",
