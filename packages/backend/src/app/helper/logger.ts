e3d9d89caaaca06c49a272a6f183bc4a8b8e6435
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-10-02 13:46:34 +0300

    feat: support loki

--- packages/backend/src/app/helper/logger.ts
+++ packages/backend/src/app/helper/logger.ts
@@ -6,24 +6,51 @@ export const captureException = (error: unknown): void => {
     logger.error(error)
 }
 
+const lokiUrl = system.get(SystemProp.LOKI_URL)
+const lokiUsername = system.get(SystemProp.LOKI_USERNAME)
+const lokiPassword = system.get(SystemProp.LOKI_PASSWORD)
+
 const initLogger = (): Logger => {
     const level = system.get<Level>(SystemProp.LOG_LEVEL) ?? 'info'
     const pretty = system.getBoolean(SystemProp.LOG_PRETTY) ?? false
 
-    const transport = pretty
-        ? {
+    const targets = []
+    if (pretty) {
+        targets.push({
+            level,
             target: 'pino-pretty',
             options: {
                 translateTime: 'HH:MM:ss Z',
                 colorize: true,
                 ignore: 'pid,hostname',
             },
-        }
-        : undefined
-
+        })
+    }
+    if (lokiUrl) {
+        targets.push({
+            target: 'pino-loki',
+            level,
+            options: {
+                batching: true,
+                interval: 5,
+                host: lokiUrl,
+                basicAuth: {
+                    username: lokiUsername!,
+                    password: lokiPassword!,
+                },
+            },
+        })
+        targets.push({
+            target: 'pino/file',
+            level,
+            options: {},
+        })
+    }
     return pino({
         level,
-        transport,
+        transport: pino.transport({
+            targets,
+        }),
     })
 }
 
