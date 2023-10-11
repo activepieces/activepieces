1dc1875a7cdbc7a08fbd92d1d0b7adec3eb78096
Salem-Alaa <salem-alaa@outlook.com>
2023-10-11 09:44:57 +0000

    fix(files): fix name

--- packages/pieces/files/src/index.ts
+++ packages/pieces/files/src/index.ts
@@ -3,7 +3,7 @@ import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
 import { readFileAction } from "./lib/actions/read-file";
 
 export const readFile = createPiece({
-  displayName: "Read-file",
+  displayName: "files",
   auth: PieceAuth.None(),
   minimumSupportedRelease: '0.9.0',
   logoUrl: "https://cdn.activepieces.com/pieces/file-piece.svg",
