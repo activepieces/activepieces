4716149ee04a7966673e5fd8edf399209a03f256
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-10-17 13:37:28 +0300

    fix: typo

--- packages/pieces/filesHelper/src/index.ts
+++ packages/pieces/filesHelper/src/index.ts
@@ -3,7 +3,7 @@ import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
 import { readFileAction } from "./lib/actions/read-file";
 
 export const filesHelper = createPiece({
-  displayName: "Files helper",
+  displayName: "Files Helper",
   auth: PieceAuth.None(),
   minimumSupportedRelease: '0.9.0',
   logoUrl: "https://cdn.activepieces.com/pieces/file-piece.svg",
