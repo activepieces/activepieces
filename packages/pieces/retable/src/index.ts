7715c89b015bccd5d38207ab96afbb3584c0612e
Kishan Parmar <code.kishanparmar@gmail.com>
2023-10-18 10:50:00 +0530

    feat(retable): add Auth

--- packages/pieces/retable/src/index.ts
+++ packages/pieces/retable/src/index.ts
@@ -1,12 +1,34 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+const markdown = `
+To obtain your API key, follow these steps:
 
+1. Go to Account Overview by clicking your profile-pic (top-right).
+2. Go to API section and enable API key.
+3. Copy API key.`;
+
+export const retableAuth = PieceAuth.SecretText({
+  displayName: 'API Key',
+  required: true,
+  description: markdown,
+  validate: async ({ auth }) => {
+    if (auth.startsWith('RTBLv1-')) {
+      return {
+        valid: true,
+      };
+    }
+    return {
+      valid: false,
+      error: 'Invalid API Key',
+    };
+  },
+});
 export const retable = createPiece({
-  displayName: "Retable",
-  auth: PieceAuth.None(),
+  displayName: 'Retable',
+  auth: retableAuth,
   minimumSupportedRelease: '0.9.0',
-  logoUrl: "https://cdn.activepieces.com/pieces/retable.png",
-  authors: [],
+  logoUrl: 'https://cdn.activepieces.com/pieces/retable.png',
+  authors: ['kishanprmr'],
   actions: [],
   triggers: [],
 });
