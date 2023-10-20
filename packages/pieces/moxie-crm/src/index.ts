becfd6838b12cdab3f6ed68d00b17666d6640318
Kishan Parmar <code.kishanparmar@gmail.com>
2023-10-20 10:14:58 +0530

    feat(moxie-crm): add auth for piece

--- packages/pieces/moxie-crm/src/index.ts
+++ packages/pieces/moxie-crm/src/index.ts
@@ -1,12 +1,39 @@
+import {
+  createPiece,
+  PieceAuth,
+  Property,
+} from '@activepieces/pieces-framework';
 
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+export const moxieCRMAuth = PieceAuth.CustomAuth({
+  required: true,
+  description: `
+  To obtain your Moxie CRM token, follow these steps:
+
+  1. Log in to your Moxie CRM account and click on **Workspace Settings** (Bottom left).
+  2. Click on **Connected Apps** and navigate to **Integrations** tab.
+  3. Now, under "Custom Integration", click on **Enable Custom Integration**.
+  4. Copy **API Key** and **Base URL** and click on Save button.
+  `,
+  props: {
+    apiKey: PieceAuth.SecretText({
+      displayName: 'API Key',
+      description: 'The API Key of the Moxie CRM account',
+      required: true,
+    }),
+    baseUrl: Property.ShortText({
+      displayName: 'Base URL',
+      description: 'The Base URL of the Moxie CRM account',
+      required: true,
+    }),
+  },
+});
 
 export const moxieCrm = createPiece({
-  displayName: "Moxie-crm",
-  auth: PieceAuth.None(),
+  displayName: 'Moxie CRM',
+  auth: moxieCRMAuth,
   minimumSupportedRelease: '0.9.0',
-  logoUrl: "https://cdn.activepieces.com/pieces/moxie-crm.png",
-  authors: [],
+  logoUrl: 'https://cdn.activepieces.com/pieces/moxie-crm.png',
+  authors: ['kishanprmr'],
   actions: [],
   triggers: [],
 });
