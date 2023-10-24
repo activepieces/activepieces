887ac509c98c6a4467e0f1268c8783284eb6177a
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-10-24 16:26:23 +0000

    fix: platform jwt token

--- packages/backend/src/app/ee/projects/enterprise-project-controller.ts
+++ packages/backend/src/app/ee/projects/enterprise-project-controller.ts
@@ -64,7 +64,7 @@ const enterpriseProjectController: FastifyPluginCallbackTypebox = (fastify, _opt
                     type: request.principal.type,
                     projectId: request.params.projectId,
                     projectType: project.type,
-                    platformId: project.platformId,
+                    platformId: request.principal.platformId,
                 }),
             }
         },
