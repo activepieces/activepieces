20cea0e78a9e3d946c11ecc5cc22a8b27574b1e1
Kishan Parmar <code.kishanparmar@gmail.com>
2023-10-27 10:42:28 +0530

    feat(moxie-crm): add sample data to client webhooks

--- packages/pieces/moxie-crm/src/lib/triggers/register-trigger.ts
+++ packages/pieces/moxie-crm/src/lib/triggers/register-trigger.ts
@@ -10,11 +10,13 @@ export const moxieCRMRegisterTrigger = ({
   displayName,
   description,
   eventType,
+  sampleData
 }: {
   name: string;
   displayName: string;
   description: string;
   eventType: MoxieCRMEventType;
+  sampleData:unknown;
 }) =>
   createTrigger({
     auth: moxieCRMAuth,
@@ -26,21 +28,21 @@ export const moxieCRMRegisterTrigger = ({
         value: `
         - Go to the **Workspace Settngs -> Connected Apps -> Integration** section.
         - Under **Custom Integration** , click on **Add Rest Hook**.
-        - Click on "Integrations" section.
+        - Click on **Integrations** section.
         - In the endpoint field, paste this URL: 
           \`{{webhookUrl}}\`
-        - Select the event as \`{{displayName}}\` and click on "Save".
+        - Select the event as **\`${displayName}\`** and click on **Save**.
         `,
       }),
     },
     type: TriggerStrategy.WEBHOOK,
-    sampleData: {},
+    sampleData: sampleData,
     async onEnable(context) {},
     async onDisable(context) {},
     async run(context) {
-      return [context.payload];
+      return [context.payload.body];
     },
     async test(context) {
-      return [context.payload];
+      return [context.payload.body];
     },
   });
