430e9f2f293d8551d8b952f3eefa9bc02f7503cd
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-10-30 23:21:07 +0300

    chore: delete key dialog

--- packages/ee/ui-platform/src/lib/components/platform-settings/signing-keys-table/signing-keys-table.component.ts
+++ packages/ee/ui-platform/src/lib/components/platform-settings/signing-keys-table/signing-keys-table.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SigningKeysDataSource } from './signing-keys-table.datasource';
-import { Subject } from 'rxjs';
+import { Subject, tap } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateSigningKeyDialogComponent } from './create-signing-key-dialog/create-signing-key-dialog.component';
 import { startWith } from 'rxjs';
 import { SigningKeysService } from '@activepieces/ee-components';
 import { SigningKey } from '@activepieces/ee-shared';
+import {
+  DeleteEntityDialogComponent,
+  DeleteEntityDialogData,
+} from '@activepieces/ui/common';
 
 @Component({
   selector: 'app-signing-keys-table',
@@ -32,6 +36,17 @@ export class SigningKeysTableComponent {
   }
 
   deleteKey(key: SigningKey) {
-    key;
+    const dialogData: DeleteEntityDialogData = {
+      deleteEntity$: this.signingKeysService.delete(key.id).pipe(
+        tap(() => {
+          this.refresh$.next(true);
+        })
+      ),
+      entityName: key.displayName,
+      note: $localize`This will permanently delete the key, all embedding that was using this key will break.`,
+    };
+    this.matDialog.open(DeleteEntityDialogComponent, {
+      data: dialogData,
+    });
   }
 }
