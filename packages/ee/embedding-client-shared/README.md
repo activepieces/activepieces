1464498dbee3cf4c7072334bc2bf7ead6a9d645b
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-10-31 13:03:34 +0300

    chore: added embedding client

---
+++ packages/ee/embedding-client-shared/README.md
@@ -0,0 +1,7 @@
+# ee-embedding-client-shared
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build ee-embedding-client-shared` to build the library.
---
+++ packages/ee/embedding-client-shared/src/lib/ee-embedding-client-shared.ts
@@ -0,0 +1,40 @@
+
+export enum ActivepiecesClientEventName {
+  CLIENT_INIT = "CLIENT_INIT",
+  CLIENT_ROUTE_CHANGED = "CLIENT_ROUTE_CHANGED",
+}
+export interface ActivepiecesClientInit {
+  type: ActivepiecesClientEventName.CLIENT_INIT;
+}
+export interface ActivepiecesClientRouteChanged {
+  type: ActivepiecesClientEventName.CLIENT_ROUTE_CHANGED;
+  data: {
+    route: string;
+  };
+}
+
+
+export  type ActivepiecesClientEvent =
+  | ActivepiecesClientInit
+  | ActivepiecesClientRouteChanged;
+
+  export enum ActivepiecesVendorEventName {
+    VENDOR_INIT = "VENDOR_INIT",
+    VENDOR_ROUTE_CHANGED = "VENDOR_ROUTE_CHANGED",
+  }
+
+  
+  export  interface ActivepiecesVendorRouteChanged {
+    type: ActivepiecesVendorEventName.VENDOR_ROUTE_CHANGED;
+    data: {
+        vendorRoute: string;
+    };
+  }
+  
+  export interface ActivepiecesVendorInit {
+    type: ActivepiecesVendorEventName.VENDOR_INIT;
+    data: {
+        prefix: string;
+        initialClientRoute:string
+    };
+  }
