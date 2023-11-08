5f930c56ea02c888bb31042fb4aac054f329124b
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-11-08 12:12:22 +0300

    chore: get platform managed app details for oauth2

--- packages/ui/feature-connections/src/lib/services/cloud-auth-configs.service.ts
+++ packages/ui/feature-connections/src/lib/services/cloud-auth-configs.service.ts
@@ -1,69 +1,65 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of, switchMap } from 'rxjs';
-import {
-  AuthenticationService,
-  FlagService,
-  environment,
-} from '@activepieces/ui/common';
+import { Observable, forkJoin, map, of, switchMap } from 'rxjs';
+import { FlagService, environment } from '@activepieces/ui/common';
 import { ApEdition, ApFlagId, SeekPage } from '@activepieces/shared';
 import { OAuthApp } from '@activepieces/ee-shared';
+import { returnEmptyRecordInCaseErrorOccurs } from '../add-edit-connection-button/utils';
 
 type AppsClientIdMap = { [appName: string]: { clientId: string } };
 @Injectable({
   providedIn: 'root',
 })
 export class CloudAuthConfigsService {
-  constructor(
-    private http: HttpClient,
-    private flagsService: FlagService,
-    private authenticationService: AuthenticationService
-  ) {}
-  getAppsAndTheirClientIds(): Observable<Record<string, { clientId: string }>> {
+  constructor(private http: HttpClient, private flagsService: FlagService) {}
+  getAppsAndTheirClientIds(): Observable<AppsClientIdMap> {
     return this.flagsService.getAllFlags().pipe(
       switchMap((flags) => {
-        const edition = flags[ApFlagId.EDITION];
+        const edition = flags[ApFlagId.EDITION] as ApEdition;
         const CLOUD_AUTH_ENABLED = flags[ApFlagId.CLOUD_AUTH_ENABLED];
-        if (!CLOUD_AUTH_ENABLED) {
-          const empty: Record<string, { clientId: string }> = {};
-          return of(empty);
-        }
-        return this.http
-          .get<AppsClientIdMap>(
-            'https://secrets.activepieces.com/apps?edition=' + edition
-          )
+        const platformAuth$: Observable<AppsClientIdMap> = this.http
+          .get<SeekPage<OAuthApp>>(environment.apiUrl + '/oauth-apps')
           .pipe(
-            switchMap((cloudApps) => {
-              const edition = flags[ApFlagId.EDITION];
-              if (
-                edition === ApEdition.COMMUNITY ||
-                !this.authenticationService.getPlatformId()
-              ) {
-                return of({ ...cloudApps });
-              }
-              return this.http
-                .get<SeekPage<OAuthApp>>(environment.apiUrl + '/oauth-apps')
-                .pipe(
-              
-                  map((res) => {
-                    const platformAppsClientIdMap: AppsClientIdMap = {};
-                    res.data.forEach((app) => {
-                      platformAppsClientIdMap[app.pieceName] = {
-                        clientId: app.clientId,
-                      };
-                    });
-                    return {
-                      ...cloudApps,
-                      ...platformAppsClientIdMap,
-                    };
-                  }),
-                  catchError((err)=>{
-                    console.warn(err);
-                    return of({...cloudApps});
-                  })
-                );
-            })
+            map((res) => {
+              const platformAppsClientIdMap: AppsClientIdMap = {};
+              res.data.forEach((app) => {
+                platformAppsClientIdMap[app.pieceName] = {
+                  clientId: app.clientId,
+                };
+              });
+              return platformAppsClientIdMap;
+            }),
+            returnEmptyRecordInCaseErrorOccurs
           );
+
+        const cloudAuth$ = this.http.get<AppsClientIdMap>(
+          'https://secrets.activepieces.com/apps?edition=' + edition
+        );
+        const both$: Observable<AppsClientIdMap> = forkJoin({
+          cloudAuth: cloudAuth$,
+          platformAuth: platformAuth$,
+        }).pipe(
+          map((res) => {
+            return {
+              ...res.cloudAuth,
+              ...res.platformAuth,
+            };
+          })
+        );
+
+        switch (edition) {
+          case ApEdition.COMMUNITY: {
+            if (CLOUD_AUTH_ENABLED) return cloudAuth$;
+            return of({});
+          }
+          case ApEdition.CLOUD: {
+            return both$;
+          }
+          case ApEdition.ENTERPRISE: {
+            if (CLOUD_AUTH_ENABLED) return both$;
+            return platformAuth$;
+          }
+        }
       })
     );
   }
