9810b1181f6f37e30a31bec7d1faf7a07268b851
Khaled Mashaly <kmmashaly@gmail.com>
2023-11-14 22:17:52 +0200

    test: project member send invitation to already existing user

--- packages/backend/test/integration/cloud/project-members/project-members.test.ts
+++ packages/backend/test/integration/cloud/project-members/project-members.test.ts
@@ -6,6 +6,8 @@ import { createMockProject, createMockUser } from '../../../helpers/mocks'
 import { generateMockToken } from '../../../helpers/auth'
 import { stripeHelper } from '../../../../src/app/ee/billing/stripe/stripe-helper'
 import { emailService } from '../../../../src/app/ee/helper/email/email-service'
+import { UserStatus } from '@activepieces/shared'
+import { faker } from '@faker-js/faker'
 
 let app: FastifyInstance | null = null
 
@@ -21,7 +23,7 @@ afterAll(async () => {
 
 describe('Project Member API', () => {
     describe('Invite member to project Endpoint', () => {
-        it('Adds new user', async () => {
+        it('Adds new invited user', async () => {
             const mockUser = createMockUser()
             await databaseConnection.getRepository('user').save(mockUser)
 
@@ -41,7 +43,7 @@ describe('Project Member API', () => {
                 role: 'VIEWER',
             }
 
-            stripeHelper.createCustomer = jest.fn().mockResolvedValue('cus_123')
+            stripeHelper.createCustomer = jest.fn().mockResolvedValue(faker.string.uuid())
             emailService.sendInvitation = jest.fn()
 
             // act
@@ -67,6 +69,56 @@ describe('Project Member API', () => {
 
             expect(invitedUser?.status).toBe('INVITED')
             expect(invitedUser?.platformId).toBe(mockProject.platformId)
+        })
+
+        it('Uses existing invited user', async () => {
+            const mockProjectOwner = createMockUser()
+            const mockInvitedUser = createMockUser({
+                status: UserStatus.INVITED,
+            })
+            await databaseConnection.getRepository('user').save([mockProjectOwner, mockInvitedUser])
+
+            const mockProject = createMockProject({
+                ownerId: mockProjectOwner.id,
+            })
+            await databaseConnection.getRepository('project').save(mockProject)
+
+            const mockToken = await generateMockToken({
+                id: mockProjectOwner.id,
+                projectId: mockProject.id,
+                platformId: mockProject.platformId,
+            })
+
+            const mockInviteProjectMemberRequest = {
+                email: mockInvitedUser.email,
+                role: 'VIEWER',
+            }
+
+            stripeHelper.createCustomer = jest.fn().mockResolvedValue(faker.string.uuid())
+            emailService.sendInvitation = jest.fn()
+
+            // act
+            const response = await app?.inject({
+                method: 'POST',
+                url: '/v1/project-members/invite',
+                headers: {
+                    authorization: `Bearer ${mockToken}`,
+                },
+                body: mockInviteProjectMemberRequest,
+            })
+
+            // assert
+            const responseBody = response?.json()
+
+            expect(response?.statusCode).toBe(StatusCodes.OK)
+            expect(Object.keys(responseBody)).toHaveLength(1)
+            expect(responseBody?.token).toBeDefined()
+
+            const invitedUser = await databaseConnection.getRepository('user').findOneBy({
+                email: mockInviteProjectMemberRequest.email,
+            })
+
+            expect(invitedUser?.id).toBe(mockInvitedUser.id)
         })
     })
 })
