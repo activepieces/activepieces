e3c19e603aa52b83772555d387364ea888043b9c
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-11-14 12:17:20 +0300

    chore: added tabs navigation to history

--- packages/ee/ui-platform/src/lib/pages/platform-settings/platform-settings.component.ts
+++ packages/ee/ui-platform/src/lib/pages/platform-settings/platform-settings.component.ts
@@ -1,15 +1,68 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+} from '@angular/core';
+import { MatTabChangeEvent, MatTabGroup } from '@angular/material/tabs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-platform-settings',
   templateUrl: './platform-settings.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlatformSettingsComponent {
+export class PlatformSettingsComponent implements AfterViewInit {
+  @ViewChild('tabs') tabGroup?: MatTabGroup;
   title = $localize`Settings`;
-  platformId = '';
+  fragmentChanged$: Observable<string | null>;
   readonly signingKeys = $localize`Signing Keys`;
   readonly termsAndPrivacy = $localize`Privacy & Terms`;
   readonly accountManagementEmail = $localize`Account Management Emails`;
-  readonly message = $localize`Please set the following TXT and CNAME records in your DNS provider, then click verify to confirm your control over the domain.`;
+  readonly tabIndexFragmentMap: { [index: number]: string } = {
+    0: 'signingKeys',
+    1: 'SMTP',
+    2: 'TermsAndServices',
+  };
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.fragmentChanged$ = this.route.fragment.pipe(
+      tap((fragment) => {
+        if (fragment === null) {
+          this.updateFragment(this.tabIndexFragmentMap[0]);
+        } else {
+          this.fragmentCheck(fragment);
+        }
+      })
+    );
+  }
+  ngAfterViewInit(): void {
+    const fragment = this.route.snapshot.fragment;
+    if (fragment === null) {
+      this.updateFragment(this.tabIndexFragmentMap[0]);
+    } else {
+      this.fragmentCheck(fragment);
+    }
+  }
+
+  private fragmentCheck(fragment: string) {
+    if (this.tabGroup) {
+      const tabIndex = Object.values(this.tabIndexFragmentMap).indexOf(
+        fragment
+      );
+      if (tabIndex >= 0) {
+        this.tabGroup.selectedIndex = tabIndex;
+      }
+    }
+  }
+
+  updateFragment(newFragment: string) {
+    this.router.navigate([], {
+      fragment: newFragment,
+    });
+  }
+  tabChanged(event: MatTabChangeEvent) {
+    if (event.index < 0 || event.index > 2) return;
+    this.updateFragment(this.tabIndexFragmentMap[event.index]);
+  }
 }
