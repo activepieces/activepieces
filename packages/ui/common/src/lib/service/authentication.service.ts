14a5ba3bb9eb990b3f81b9dbe8eee1c71337ca6c
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-11-15 22:50:19 +0000

    fix: ja language

--- packages/ui/common/src/lib/service/authentication.service.ts
+++ packages/ui/common/src/lib/service/authentication.service.ts
@@ -105,7 +105,7 @@ export class AuthenticationService {
     const token = localStorage.getItem(environment.jwtTokenName);
     const decodedToken = this.jwtHelper.decodeToken(token || '');
     // TODO REMOVE in next release
-    if (decodedToken['platformId']) {
+    if (decodedToken && decodedToken['platformId']) {
       this.logout();
     }
     return decodedToken;
