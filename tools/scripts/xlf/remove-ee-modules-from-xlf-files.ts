71f0dd0035c6b7bdbb67710038a1ff235fb8a5e9
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-11-15 16:13:09 +0300

    chore: address comment and fix command path in crowdin.yml

--- tools/scripts/xlf/remove-ee-modules-from-xlf-files.ts
+++ tools/scripts/xlf/remove-ee-modules-from-xlf-files.ts
@@ -33,12 +33,10 @@ async function findXlfFiles(directory: string): Promise<string[]> {
 }
 
 // Example usage
-const directoryPath = 'packages/ui/core/src/locale';
+const directoryPath = 'packages/ui/core/src/locale/messages.xlf';
 const modulesToRemoveFromXlf = ['packages/ee/ui-platform','packages/ui/feature-chatbot'];
-findXlfFiles(directoryPath)
-  .then((xlfFiles) => {
-   xlfFiles.forEach(filePath=>{
-        readXLFFile(filePath, function (err:unknown, result:TranslationFile) {
+
+readXLFFile(directoryPath, function (err:unknown, result:TranslationFile) {
         if (err) {
             console.error(err);
         } else {
@@ -47,7 +45,7 @@ findXlfFiles(directoryPath)
               const fixedFile =  removeContextsWithPrefix(result, modulePath);
               var builder = new xml2js.Builder();
               var xml = builder.buildObject(fixedFile);
-              fs.writeFile(filePath, xml, (err:any) => {
+              fs.writeFile(directoryPath, xml, (err:any) => {
               if (err) {
                   console.error('Error writing to file:', err);
               } else {
@@ -58,7 +56,4 @@ findXlfFiles(directoryPath)
 
            
         }
-        });
-   })
-  })
-  .catch((error) => console.error('Error:', error));
+});
