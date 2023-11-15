61dbf7aabe7e7e5397f6656bb7027c39e9f0310a
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-11-15 12:56:50 +0300

    chore: iterate over modules we want to remove

--- tools/scripts/xlf/remove-ee-modules-from-xlf-files.ts
+++ tools/scripts/xlf/remove-ee-modules-from-xlf-files.ts
@@ -34,6 +34,7 @@ async function findXlfFiles(directory: string): Promise<string[]> {
 
 // Example usage
 const directoryPath = 'packages/ui/core/src/locale';
+const modulesToRemoveFromXlf = ['packages/ee/ui-platform','packages/ui/feature-chatbot'];
 findXlfFiles(directoryPath)
   .then((xlfFiles) => {
    xlfFiles.forEach(filePath=>{
@@ -42,16 +43,20 @@ findXlfFiles(directoryPath)
             console.error(err);
         } else {
             // Now you can work with the parsed result
-            const fixedFile =  removeContextsWithPrefix(result, 'packages/ee/ui-platform');
-            var builder = new xml2js.Builder();
-            var xml = builder.buildObject(fixedFile);
-            fs.writeFile(filePath, xml, (err:any) => {
-            if (err) {
-                console.error('Error writing to file:', err);
-            } else {
-                console.log('File written successfully');
-            }
+            modulesToRemoveFromXlf.forEach(modulePath=>{
+              const fixedFile =  removeContextsWithPrefix(result, modulePath);
+              var builder = new xml2js.Builder();
+              var xml = builder.buildObject(fixedFile);
+              fs.writeFile(filePath, xml, (err:any) => {
+              if (err) {
+                  console.error('Error writing to file:', err);
+              } else {
+                  console.log('File written successfully');
+              }
+              });
             });
+
+           
         }
         });
    })
