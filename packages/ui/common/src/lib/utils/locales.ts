97f3f04f7b53e4bc923e67121154e6d218d59f1c
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-11-22 17:28:21 +0000

    fix: add dutch translations

--- packages/ui/common/src/lib/utils/locales.ts
+++ packages/ui/common/src/lib/utils/locales.ts
@@ -2,6 +2,7 @@ import { LocalesEnum } from '@activepieces/ee-shared';
 
 export const localesMap = {
   [LocalesEnum.ENGLISH]: 'English',
+  [LocalesEnum.DUTCH]: 'Dutch',
   [LocalesEnum.ITALIAN]: 'Italiano',
   [LocalesEnum.FRENCH]: 'Français',
   [LocalesEnum.GERMAN]: 'Deutsch',
