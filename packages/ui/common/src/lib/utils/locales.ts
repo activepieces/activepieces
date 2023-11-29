0f0d79234330454bf9943929cdb0856f7ad9ed15
Mohammad AbuAboud <moabuaboud@gmail.com>
2023-11-29 15:03:38 +0000

    feat: add spanish language

--- packages/ui/common/src/lib/utils/locales.ts
+++ packages/ui/common/src/lib/utils/locales.ts
@@ -2,9 +2,10 @@ import { LocalesEnum } from '@activepieces/ee-shared';
 
 export const localesMap = {
   [LocalesEnum.ENGLISH]: 'English',
-  [LocalesEnum.DUTCH]: 'Nederlands',
-  [LocalesEnum.ITALIAN]: 'Italiano',
+  [LocalesEnum.SPANISH]: 'Español',
   [LocalesEnum.FRENCH]: 'Français',
   [LocalesEnum.GERMAN]: 'Deutsch',
+  [LocalesEnum.ITALIAN]: 'Italiano',
+  [LocalesEnum.DUTCH]: 'Nederlands',
   [LocalesEnum.JAPANESE]: '日本語',
 };
