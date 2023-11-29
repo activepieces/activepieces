0d64eb28b096cbc5915df72fc7b674898e20a0b2
Khaled Mashaly <kmmashaly@gmail.com>
2023-11-29 14:41:14 +0200

    chore: ee migration for user platformId

---
+++ packages/backend/src/app/database/migration/postgres/1701261357197-migrate-ee-users-to-oldest-platform.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+import { databaseConnection } from '../../database-connection'
+import { isNil } from '@activepieces/shared'
+
+export class MigrateEeUsersToOldestPlatform1701261357197 implements MigrationInterface {
+    name = 'MigrateEeUsersToOldestPlatform1701261357197'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const result = await databaseConnection.getRepository('platform').findOne({ order: { createdAt: 'ASC' } })
+
+        if (isNil(result)) {
+            return
+        }
+
+        await queryRunner.query(`
+            UPDATE "user"
+            SET "platformId" = '${result.id}'
+            WHERE "platformId" IS NULL
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            UPDATE "user"
+            SET "platformId" = NULL
+            WHERE "platformId" IS NOT NULL
+        `)
+    }
+
+}
