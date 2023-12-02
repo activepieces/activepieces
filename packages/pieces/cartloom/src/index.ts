5b5c1663c93d5e428b3bb489a9e60a43efa92f55
Joe Workman <joe@workmanmail.com>
2023-12-01 21:07:02 -0800

    feat(cartloom): piece ready to test

--- packages/pieces/cartloom/src/index.ts
+++ packages/pieces/cartloom/src/index.ts
@@ -1,12 +1,28 @@
 
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { createPiece } from "@activepieces/pieces-framework";
+import { getProductsAction } from "./lib/actions/get-products";
+import { cartloomAuth } from "./lib/auth";
+import { getOrderAction } from "./lib/actions/get-order";
+import { createDiscountAction } from "./lib/actions/create-discount";
+import { getDiscountAction } from "./lib/actions/get-discount";
+import { getAllDiscountsAction } from "./lib/actions/get-discounts";
+import { getOrderDateAction } from "./lib/actions/get-orders-date";
+import { getOrderEmailAction } from "./lib/actions/get-orders-date-email";
 
 export const cartloom = createPiece({
-  displayName: "Cartloom",
-  auth: PieceAuth.None(),
-  minimumSupportedRelease: '0.9.0',
-  logoUrl: "https://cdn.activepieces.com/pieces/cartloom.png",
-  authors: [],
-  actions: [],
-  triggers: [],
+	displayName             : "Cartloom",
+	auth                    : cartloomAuth,
+	minimumSupportedRelease : '0.9.0',
+	logoUrl                 : "https://sandbox.joeworkman.net/cartloom.png",
+	authors                 : ["joeworkman"],
+	actions                 : [
+		getProductsAction,
+		getOrderAction,
+		createDiscountAction,
+		getDiscountAction,
+		getAllDiscountsAction,
+		getOrderDateAction,
+		getOrderEmailAction,
+	],
+	triggers: [],
 });
---
+++ packages/pieces/cartloom/src/lib/api.ts
@@ -0,0 +1,52 @@
+import { httpClient, HttpMethod, HttpRequest } from "@activepieces/pieces-common";
+import { CartloomAuthType } from "./auth";
+
+type KeyValuePair = {[key: string]: string|boolean|number|undefined }
+
+const cartloomAPI = async (api: string, auth: CartloomAuthType, body: KeyValuePair = {}) => {
+	const request: HttpRequest = {
+		method  : HttpMethod.POST,
+		url     : `https://${auth.domain}.cartloom.com/api${api}`,
+		headers : {
+			'Content-Type' : 'application/x-www-form-urlencoded',
+			'X-API-KEY'    : auth.apiKey,
+		},
+		body : body,
+	};
+	const response = await httpClient.sendRequest(request);
+
+	if (response.status !== 200) {
+		throw new Error(`Cartloom API Error: ${response.status} ${response.body}`);
+	}
+
+	const data = Object.keys(response.body).map(key => response.body[key]);
+
+	return {
+		success : true,
+		data    : data,
+	};
+}
+
+export async function getProducts(auth: CartloomAuthType) {
+	return cartloomAPI('/products/list', auth);
+}
+
+export async function getAllDiscounts(auth: CartloomAuthType) {
+	return cartloomAPI('/discounts/list', auth);
+}
+
+export async function getDiscount(auth: CartloomAuthType, discountId: string) {
+	return cartloomAPI('/discounts/get', auth, { id : discountId });
+}
+
+export async function getOrder(auth: CartloomAuthType, invoice: string) {
+	return cartloomAPI('/orders/get', auth, { invoice : invoice });
+}
+
+export async function getOrdersByDate(auth: CartloomAuthType, data: KeyValuePair) {
+	return cartloomAPI('/orders/list', auth, data);
+}
+
+export async function createDiscount(auth: CartloomAuthType, data: KeyValuePair) {
+	return cartloomAPI('/discounts/add', auth, data);
+}
