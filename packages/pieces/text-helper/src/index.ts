a71bb814f852f71489e65052d96311ed251c868a
Joe Workman <joe@workmanmail.com>
2023-12-02 08:31:06 -0800

    feat(markdown): author

--- packages/pieces/text-helper/src/index.ts
+++ packages/pieces/text-helper/src/index.ts
@@ -10,7 +10,7 @@ export const textHelper = createPiece({
   displayName: 'Text Helper',
   auth: PieceAuth.None(),
   logoUrl: 'https://cdn.activepieces.com/pieces/text-helper.svg',
-  authors: ['abaza738'],
+  authors: ['abaza738','joeworkman'],
   actions: [concat, replace, split, find, markdownToHTML, htmlToMarkdown],
   triggers: [],
 });
