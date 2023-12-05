d6d29e3e91aa29cacf8aad41fe87bd5e47bf7125
Joe Workman <joe@workmanmail.com>
2023-12-05 09:33:05 -0800

    feat(datehelper): revert min support version back

--- packages/pieces/date-helper/src/index.ts
+++ packages/pieces/date-helper/src/index.ts
@@ -12,7 +12,7 @@ const description = `Effortlessly manipulate, format, and extract time units for
 export const utilityDate = createPiece({
   displayName: 'Date Helper',
   auth: PieceAuth.None(),
-  minimumSupportedRelease: '0.9.0',
+  minimumSupportedRelease: '0.8.0',
   logoUrl: 'https://cdn.activepieces.com/pieces/calendar_piece.svg',
   authors: ['Salem-Alaa','joeworkman'],
   actions: [
