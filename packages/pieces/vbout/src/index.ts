dce165976b475d896ed697d52f3d5ed66f754ea3
Kishan Parmar <code.kishanparmar@gmail.com>
2023-12-13 16:32:01 +0530

    feat(vbout): add piece auth

--- packages/pieces/vbout/src/index.ts
+++ packages/pieces/vbout/src/index.ts
@@ -1,8 +1,21 @@
 import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 
+const markdown = `
+To obtain your API key, follow these steps:
+
+1.Go to **settings** by clicking your profile-pic (top-right).
+2.Navigate to **API Integrations** section.
+3.Under **API USER KEY** ,copy API key.
+`;
+
+export const vboutAuth = PieceAuth.SecretText({
+  displayName: 'API Key',
+  required: true,
+  description: markdown,
+});
 export const vbout = createPiece({
   displayName: 'Vbout',
-  auth: PieceAuth.None(),
+  auth: vboutAuth,
   minimumSupportedRelease: '0.9.0',
   logoUrl: 'https://cdn.activepieces.com/pieces/vbout.png',
   authors: ['kishanprmr'],
