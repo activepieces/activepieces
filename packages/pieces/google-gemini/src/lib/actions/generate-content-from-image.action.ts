9fb49a1b66bba6d1482371214c836dce8897f741
Pablo Fernandez <pfernandez@estudiante.ceibal.edu.uy>
2023-12-16 07:39:32 -0800

    fix: replaced image_base64 and mime_type with Properties.File

--- packages/pieces/google-gemini/src/lib/actions/generate-content-from-image.action.ts
+++ packages/pieces/google-gemini/src/lib/actions/generate-content-from-image.action.ts
@@ -1,7 +1,7 @@
 import { HttpMethod, httpClient } from '@activepieces/pieces-common';
 import { googleGeminiAuth } from '../../index';
-import { Property, createAction } from "@activepieces/pieces-framework";
-
+import { Property, Validators, createAction } from "@activepieces/pieces-framework";
+import mime from 'mime-types';
 
 export const generateContentFromImageAction = createAction({
     description: 'Generate content using Google Gemini using the "gemini-pro-vision" model',
@@ -14,19 +14,14 @@ export const generateContentFromImageAction = createAction({
             required: true,
             description: 'The prompt to generate content from.'
         }),
-        image_base64: Property.LongText({
-            displayName: 'Image in Base64',
+        image: Property.File({
+            displayName: 'Image',
             required: true,
-            description: 'The image to generate content from. Base64 encoded.',
-        }),
-        mime_type: Property.ShortText({
-            displayName: 'Mime Type',
-            required: true,
-            description: 'The mime type of the image. Example: image/jpeg',
+            description: 'The image to generate content from.',
+            validators: [Validators.image]
         })
     },
-    async run({ auth, propsValue }){
-        
+    async run({ auth, propsValue }){        
         const request = await httpClient.sendRequest({
             method: HttpMethod.POST,
             url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=${auth}`,
@@ -42,8 +37,8 @@ export const generateContentFromImageAction = createAction({
                             }, 
                             {
                                 "inline_data": {
-                                    "mime_type": propsValue.mime_type,
-                                    "data": propsValue.image_base64
+                                    "mime_type": mime.lookup(propsValue.image.filename),
+                                    "data": propsValue.image.base64
                                 }
                             }
                         ]
