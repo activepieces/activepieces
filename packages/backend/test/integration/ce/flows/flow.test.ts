a000a94c94c5dd2a9daf1d27b8086eb7a269e803
Khaled Mashaly <kmmashaly@gmail.com>
2023-12-20 01:10:07 +0200

    test: update published version id endpoint

--- packages/backend/test/integration/ce/flows/flow.test.ts
+++ packages/backend/test/integration/ce/flows/flow.test.ts
@@ -4,7 +4,7 @@ import { generateMockToken } from '../../../helpers/auth'
 import { createMockUser, createMockProject, createMockFlow, createMockFlowVersion } from '../../../helpers/mocks'
 import { StatusCodes } from 'http-status-codes'
 import { FastifyInstance } from 'fastify'
-import { FlowStatus, PrincipalType } from '@activepieces/shared'
+import { FlowStatus, FlowVersionState, PrincipalType } from '@activepieces/shared'
 
 let app: FastifyInstance | null = null
 
@@ -115,7 +115,7 @@ describe('Flow API', () => {
             expect(response?.statusCode).toBe(StatusCodes.OK)
             const responseBody = response?.json()
 
-            expect(Object.keys(responseBody)).toHaveLength(8)
+            expect(Object.keys(responseBody)).toHaveLength(9)
             expect(responseBody?.id).toBe(mockFlow.id)
             expect(responseBody?.created).toBeDefined()
             expect(responseBody?.updated).toBeDefined()
@@ -124,6 +124,115 @@ describe('Flow API', () => {
             expect(responseBody?.status).toBe('ENABLED')
             expect(responseBody?.publishedVersionId).toBe(mockFlowVersion.id)
             expect(responseBody?.schedule).toBeNull()
+
+            expect(Object.keys(responseBody?.version)).toHaveLength(9)
+            expect(responseBody?.version?.id).toBe(mockFlowVersion.id)
+        })
+
+        it('Disables an enabled Flow', async () => {
+            // arrange
+            const mockUser = createMockUser()
+            await databaseConnection.getRepository('user').save([mockUser])
+
+            const mockProject = createMockProject({ ownerId: mockUser.id })
+            await databaseConnection.getRepository('project').save([mockProject])
+
+            const mockFlow = createMockFlow({ projectId: mockProject.id, status: FlowStatus.ENABLED })
+            await databaseConnection.getRepository('flow').save([mockFlow])
+
+            const mockFlowVersion = createMockFlowVersion({ flowId: mockFlow.id, updatedBy: mockUser.id })
+            await databaseConnection.getRepository('flow_version').save([mockFlowVersion])
+
+            await databaseConnection.getRepository('flow').update(mockFlow.id, {
+                publishedVersionId: mockFlowVersion.id,
+            })
+
+            const mockToken = await generateMockToken({ type: PrincipalType.USER, projectId: mockProject.id })
+
+            const mockUpdateFlowStatusRequest = {
+                status: 'DISABLED',
+            }
+
+            // act
+            const response = await app?.inject({
+                method: 'POST',
+                url: `/v1/flows/${mockFlow.id}/status`,
+                headers: {
+                    authorization: `Bearer ${mockToken}`,
+                },
+                body: mockUpdateFlowStatusRequest,
+            })
+
+            // assert
+            expect(response?.statusCode).toBe(StatusCodes.OK)
+            const responseBody = response?.json()
+
+            expect(Object.keys(responseBody)).toHaveLength(9)
+            expect(responseBody?.id).toBe(mockFlow.id)
+            expect(responseBody?.created).toBeDefined()
+            expect(responseBody?.updated).toBeDefined()
+            expect(responseBody?.projectId).toBe(mockProject.id)
+            expect(responseBody?.folderId).toBeNull()
+            expect(responseBody?.status).toBe('DISABLED')
+            expect(responseBody?.publishedVersionId).toBe(mockFlowVersion.id)
+            expect(responseBody?.schedule).toBeNull()
+
+            expect(Object.keys(responseBody?.version)).toHaveLength(9)
+            expect(responseBody?.version?.id).toBe(mockFlowVersion.id)
+        })
+    })
+
+    describe('Update published version id endpoint', () => {
+        it('Publishes latest draft version', async () => {
+            // arrange
+            const mockUser = createMockUser()
+            await databaseConnection.getRepository('user').save([mockUser])
+
+            const mockProject = createMockProject({ ownerId: mockUser.id })
+            await databaseConnection.getRepository('project').save([mockProject])
+
+            const mockFlow = createMockFlow({ projectId: mockProject.id, status: FlowStatus.DISABLED })
+            await databaseConnection.getRepository('flow').save([mockFlow])
+
+            const mockFlowVersion = createMockFlowVersion({
+                flowId: mockFlow.id,
+                updatedBy: mockUser.id,
+                state: FlowVersionState.DRAFT,
+            })
+            await databaseConnection.getRepository('flow_version').save([mockFlowVersion])
+
+            const mockToken = await generateMockToken({
+                id: mockUser.id,
+                type: PrincipalType.USER,
+                projectId: mockProject.id,
+            })
+
+            // act
+            const response = await app?.inject({
+                method: 'POST',
+                url: `/v1/flows/${mockFlow.id}/published-version-id`,
+                headers: {
+                    authorization: `Bearer ${mockToken}`,
+                },
+            })
+
+            // assert
+            expect(response?.statusCode).toBe(StatusCodes.OK)
+            const responseBody = response?.json()
+
+            expect(Object.keys(responseBody)).toHaveLength(9)
+            expect(responseBody?.id).toBe(mockFlow.id)
+            expect(responseBody?.created).toBeDefined()
+            expect(responseBody?.updated).toBeDefined()
+            expect(responseBody?.projectId).toBe(mockProject.id)
+            expect(responseBody?.folderId).toBeNull()
+            expect(responseBody?.status).toBe('ENABLED')
+            expect(responseBody?.publishedVersionId).toBe(mockFlowVersion.id)
+            expect(responseBody?.schedule).toBeNull()
+
+            expect(Object.keys(responseBody?.version)).toHaveLength(9)
+            expect(responseBody?.version?.id).toBe(mockFlowVersion.id)
+            expect(responseBody?.version?.state).toBe('LOCKED')
         })
     })
 })
