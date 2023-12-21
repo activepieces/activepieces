8b80354d0de1d60b8e3624455336baf2816aace3
AbdulTheActivePiecer <abdulyki@activepieces.com>
2023-12-21 11:45:38 +0300

    fix: replace all instances of search word instead of just one

--- packages/pieces/text-helper/src/lib/actions/replace.ts
+++ packages/pieces/text-helper/src/lib/actions/replace.ts
@@ -4,7 +4,7 @@ import {
 } from '@activepieces/pieces-framework';
 
 export const replace = createAction({
-  description: 'Replace a text with another (Regex or text).',
+  description: 'Replaces all instances of any word, character or phrase in text, with another word, character or phrase.',
   displayName: 'Replace',
   name: 'replace',
   props: {
@@ -14,20 +14,20 @@ export const replace = createAction({
     }),
     searchValue: Property.ShortText({
       displayName: 'Search Value',
-      description: 'Regex or text.',
+      description: 'Can be regex expression or plain text.',
       required: true,
       validators: [],
     }),
     replaceValue: Property.ShortText({
       displayName: 'Replace Value',
-      required: true,
+      required: true
     }),
   },
   run: async (ctx) => {
     const expression = RegExp(ctx.propsValue.searchValue);
-    return ctx.propsValue.text.replace(
+    return ctx.propsValue.text.replaceAll(
       expression,
-      ctx.propsValue.replaceValue
+      ctx.propsValue.replaceValue || ''
     );
   },
 });
