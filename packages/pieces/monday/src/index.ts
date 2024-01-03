2e3d093a5852cb71ace134d96391ce75b0b477a2
Kishan Parmar <code.kishanparmar@gmail.com>
2024-01-03 12:16:39 +0530

    feat(monday): change auth type to API Token

--- packages/pieces/monday/src/index.ts
+++ packages/pieces/monday/src/index.ts
@@ -1,34 +1,35 @@
-import { PieceAuth, createPiece } from "@activepieces/pieces-framework";
-import { mondayCreateAnItem } from "./lib/actions/create-item";
-import { mondayUpdateAnItem } from "./lib/actions/update-item";
-import { mondayGetItemColumnValues } from "./lib/actions/get-column-value-by-item";
-import { mondayItemCreatedTrigger } from "./lib/triggers/item-created-trigger";
-import { mondayNewUpdatesTrigger } from "./lib/triggers/new-update-trigger";
+import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
+import { mondayCreateAnItem } from './lib/actions/create-item';
+import { mondayGetItemColumnValues } from './lib/actions/get-column-value-by-item';
 import { mondayGetItemByColumnValues } from './lib/actions/get-item-by-column-value';
+import { mondayUpdateAnItem } from './lib/actions/update-item';
+import { mondayItemCreatedTrigger } from './lib/triggers/item-created-trigger';
+import { mondayNewUpdatesTrigger } from './lib/triggers/new-update-trigger';
 
-export const mondayAuth = PieceAuth.OAuth2({
-  
-  description: "OAuth2.0 Authentication",
-  authUrl: "https://auth.monday.com/oauth2/authorize",
-  tokenUrl: "https://auth.monday.com/oauth2/token",
+const markdown = `
+1.Log into your monday.com account.\n
+2.Click on your avatar/profile picture in the top right corner.\n
+3.Select **Administration** (this requires you to have admin permissions).\n
+4.Go to the **API** section.\n
+5.Copy your personal token`;
+
+export const mondayAuth = PieceAuth.SecretText({
+  displayName: 'API v2 Token',
+  description: markdown,
   required: true,
-  scope: [
-    'workspaces:read',
-    'boards:read',
-    'boards:write',
-    'updates:read',
-    'updates:write',
-    'webhooks:write',
-    'webhooks:read'
-  ]
-})
+});
 
 export const monday = createPiece({
-  displayName: "monday.com",
-      minimumSupportedRelease: '0.5.0',
-    logoUrl: "https://cdn.activepieces.com/pieces/monday.png",
+  displayName: 'monday.com',
+  minimumSupportedRelease: '0.5.0',
+  logoUrl: 'https://cdn.activepieces.com/pieces/monday.png',
   authors: ['kanarelo'],
   auth: mondayAuth,
-  actions: [mondayCreateAnItem, mondayUpdateAnItem, mondayGetItemColumnValues, mondayGetItemByColumnValues],
+  actions: [
+    mondayCreateAnItem,
+    mondayUpdateAnItem,
+    mondayGetItemColumnValues,
+    mondayGetItemByColumnValues,
+  ],
   triggers: [mondayItemCreatedTrigger, mondayNewUpdatesTrigger],
 });
