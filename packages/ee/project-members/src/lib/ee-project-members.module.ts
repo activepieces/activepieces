971272caf42103975946ff3f32e642d310a69e30
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-01-09 11:07:51 +0300

    fix: project members status and roles displaynames are now correctly retrieved

--- packages/ee/project-members/src/lib/ee-project-members.module.ts
+++ packages/ee/project-members/src/lib/ee-project-members.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AcceptInvitationComponent } from './accept-invitation/accept-invitation.component';
-import { InviteProjectMemberDialogComponent } from './invite-project-member-dialog/invite-project-member.component';
+import { InviteProjectMemberDialogComponent } from './dialogs/invite-project-member-dialog/invite-project-member.component';
 import { UiCommonModule } from '@activepieces/ui/common';
 import { ProjectMembersTableComponent } from './project-members-table/project-members-table.component';
 import { EeBillingUiModule } from '@activepieces/ee-billing-ui';
