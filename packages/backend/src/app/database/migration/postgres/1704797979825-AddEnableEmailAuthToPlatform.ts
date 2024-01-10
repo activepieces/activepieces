2ca9439894eb83871cbcd89bbcab5f35cbc89250
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-01-10 15:52:00 +0300

    fix: migration error

--- packages/backend/src/app/database/migration/postgres/1704797979825-AddEnableEmailAuthToPlatform.ts
+++ packages/backend/src/app/database/migration/postgres/1704797979825-AddEnableEmailAuthToPlatform.ts
@@ -4,10 +4,9 @@ export class AddEnableEmailAuthToPlatform1704797979825 implements MigrationInter
     name = 'AddEnableEmailAuthToPlatform1704797979825'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            ALTER TABLE "platform"
-            ADD "emailAuthEnabled" boolean NOT NULL
-        `)
+        await queryRunner.query('ALTER TABLE "platform" ADD "emailAuthEnabled" boolean')
+        await queryRunner.query('UPDATE "platform" SET "emailAuthEnabled" = TRUE')
+        await queryRunner.query('ALTER TABLE "platform" ALTER COLUMN "emailAuthEnabled" SET NOT NULL')
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
