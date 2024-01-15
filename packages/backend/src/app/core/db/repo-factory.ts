d2c6c7c0097c5ec54d9d87725f33fa0297d07a91
Khaled Mashaly <kmmashaly@gmail.com>
2024-01-15 13:29:57 +0200

    refactor(backend): make RepoGetters singletons

--- packages/backend/src/app/core/db/repo-factory.ts
+++ packages/backend/src/app/core/db/repo-factory.ts
@@ -5,7 +5,9 @@ import { databaseConnection } from '../../database/database-connection'
  * If given an {@link EntityManager}, returns a {@link Repository} for the current transaction.
  * Otherwise, returns the {@link Repository} for the default connection.
  */
-type RepoGetter<T extends ObjectLiteral> = (entityManager?: EntityManager) => Repository<T>
+type RepoGetter<T extends ObjectLiteral = ObjectLiteral> = (entityManager?: EntityManager) => Repository<T>
+
+const instances = new Map<EntitySchema, RepoGetter>()
 
 /**
  * Creates a {@link RepoGetter} for the given entity.
@@ -13,8 +15,15 @@ type RepoGetter<T extends ObjectLiteral> = (entityManager?: EntityManager) => Re
  * @returns A {@link RepoGetter} for the given entity.
  */
 export const repoFactory = <T extends ObjectLiteral>(entity: EntitySchema<T>): RepoGetter<T> => {
-    return (entityManager?: EntityManager) => {
+    if (instances.has(entity)) {
+        return instances.get(entity) as RepoGetter<T>
+    }
+
+    const newInstance: RepoGetter<T> = (entityManager?: EntityManager) => {
         return entityManager?.getRepository(entity)
         ?? databaseConnection.getRepository(entity)
     }
+
+    instances.set(entity, newInstance as RepoGetter)
+    return newInstance
 }
