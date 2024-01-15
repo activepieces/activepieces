ddbbe9ef91d9876ab5b748d109214d209a203ba7
Khaled Mashaly <kmmashaly@gmail.com>
2024-01-15 13:22:29 +0200

    chore(backend): added jsDoc to repoFactory

--- packages/backend/src/app/core/db/repo-factory.ts
+++ packages/backend/src/app/core/db/repo-factory.ts
@@ -1,9 +1,18 @@
 import { Repository, EntityManager, ObjectLiteral, EntitySchema } from 'typeorm'
 import { databaseConnection } from '../../database/database-connection'
 
-type Repo<T extends ObjectLiteral> = (entityManager?: EntityManager) => Repository<T>
+/**
+ * If given an {@link EntityManager}, returns a {@link Repository} for the current transaction.
+ * Otherwise, returns the {@link Repository} for the default connection.
+ */
+type RepoGetter<T extends ObjectLiteral> = (entityManager?: EntityManager) => Repository<T>
 
-export const repoFactory = <T extends ObjectLiteral>(entity: EntitySchema<T>): Repo<T> => {
+/**
+ * Creates a {@link RepoGetter} for the given entity.
+ * @param entity The entity to create a {@link RepoGetter} for.
+ * @returns A {@link RepoGetter} for the given entity.
+ */
+export const repoFactory = <T extends ObjectLiteral>(entity: EntitySchema<T>): RepoGetter<T> => {
     return (entityManager?: EntityManager) => {
         return entityManager?.getRepository(entity)
         ?? databaseConnection.getRepository(entity)
