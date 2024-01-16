6caf37cf54cce2cdf5d2c09a74c3fa04ef74795c
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2024-01-16 17:43:28 +0300

    feat(clickup): drilldown to get tasks, adds listalllists getting list by folder/workspace/space

--- packages/pieces/clickup/src/lib/actions/tasks/get-tasks.ts
+++ packages/pieces/clickup/src/lib/actions/tasks/get-tasks.ts
@@ -1,6 +1,6 @@
 import { createAction } from "@activepieces/pieces-framework";
 import { getAccessTokenOrThrow } from "@activepieces/pieces-common";
-import { clickupCommon, listTasks } from "../../common";
+import { clickupCommon, listAllLists, listLists, listSpaces, listTasks } from "../../common";
 import { clickupAuth } from "../../../";
 
 export const getClickupTasks = createAction({
@@ -10,13 +10,51 @@ export const getClickupTasks = createAction({
   displayName: 'Get Tasks',
   props: {
     workspace_id: clickupCommon.workspace_id(true),
-    space_id: clickupCommon.space_id(true),
-    list_id: clickupCommon.list_id(true),
+    space_id: clickupCommon.space_id(false),
+    folder_id: clickupCommon.folder_id(false),
+    list_id: clickupCommon.list_id(false),
   },
   async run(configValue) {
-    const { list_id } = configValue.propsValue;
-    const response = await listTasks(getAccessTokenOrThrow(configValue.auth), list_id as unknown as string)
+    const { list_id, folder_id, space_id, workspace_id } = configValue.propsValue;
+    const auth = getAccessTokenOrThrow(configValue.auth)
 
-    return response.tasks;
-  },
-});
+    if (list_id) {
+      return (await listTasks(auth, list_id as unknown as string)).tasks
+    }
+
+    if (folder_id) {
+      let tasks: { id: string; name: string; }[] = []
+      
+      const lists = (await listLists(auth, folder_id as unknown as string)).lists
+      for (let k = 0; k < lists.length; k++) {
+        tasks = [...tasks, ...(await listTasks(auth, lists[k].id)).tasks]
+      }
+      return tasks
+    }
+
+    if (space_id) {
+      let tasks: { id: string; name: string; }[] = []
+      
+      const lists = await listAllLists(auth, space_id)
+      for (let k = 0; k < lists.length; k++) {
+        tasks = [...tasks, ...(await listTasks(auth, lists[k].id)).tasks]
+      }
+      
+      return tasks
+    }
+
+    if (workspace_id) {
+      const spaces = (await listSpaces(auth, workspace_id)).spaces
+      let tasks: { id: string; name: string; }[] = []
+      for (let i = 0; i < spaces.length; i++) {
+        const lists = await listAllLists(auth, spaces[i].id)
+        for (let k = 0; k < lists.length; k++) {
+          tasks = [...tasks, ...(await listTasks(auth, lists[k].id)).tasks]
+        }
+      }
+      return tasks
+    }
+
+    return []
+  }
+})
