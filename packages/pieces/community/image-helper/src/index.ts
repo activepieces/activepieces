973dd6d6ce261239756ae5261d6212c65fbeb498
Kishan Parmar <code.kishanparmar@gmail.com>
2024-01-19 12:33:55 +0530

    chore: move image-helper to commuity folder

--- packages/pieces/community/image-helper/src/index.ts
+++ packages/pieces/community/image-helper/src/index.ts
@@ -1,15 +1,14 @@
-
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-import { imageToBase64 } from "./lib/actions/image-to-base64.action";
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { imageToBase64 } from './lib/actions/image-to-base64.action';
 //import { resizeImage } from "./lib/actions/resize-image.action";
 //import { flipImage } from "./lib/actions/flip-image.action";
 
 export const imageHelper = createPiece({
-  displayName: "Image-helper",
+  displayName: 'Image-helper',
   auth: PieceAuth.None(),
   minimumSupportedRelease: '0.9.0',
-  logoUrl: "https://cdn.activepieces.com/pieces/image-helper.png",
-  authors: ["PFernandez98"],
+  logoUrl: 'https://cdn.activepieces.com/pieces/image-helper.png',
+  authors: ['PFernandez98'],
   //actions: [imageToBase64, resizeImage, flipImage],
   actions: [imageToBase64],
   triggers: [],
