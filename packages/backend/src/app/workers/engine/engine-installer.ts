6d91a0bb1f7bf8b01fefb6b79fc018eee6a336ac
Khaled Mashaly <kmmashaly@gmail.com>
2024-01-25 00:29:18 +0200

    refactor(backend): remove isolated-vm linking

--- packages/backend/src/app/workers/engine/engine-installer.ts
+++ packages/backend/src/app/workers/engine/engine-installer.ts
@@ -2,8 +2,6 @@ import { copyFile } from 'node:fs/promises'
 import { system } from '../../helper/system/system'
 import { SystemProp } from '../../helper/system/system-prop'
 import { logger } from '../../helper/logger'
-import { packageManager } from '../../helper/package-manager'
-import { CodeSandboxType } from '@activepieces/shared'
 
 const engineExecutablePath = system.getOrThrow(SystemProp.ENGINE_EXECUTABLE_PATH)
 
@@ -16,21 +14,7 @@ export const engineInstaller = {
 
         await copyFile(engineExecutablePath, `${path}/main.js`)
         await copyFile(`${engineExecutablePath}.map`, `${path}/main.js.map`)
-        await installDependencies(path)
     },
-}
-
-const installDependencies = async (path: string): Promise<void> => {
-    const codeSandboxType = system.getOrThrow<CodeSandboxType>(SystemProp.CODE_SANDBOX_TYPE)
-    const codSandboxTypeIsV8Isolate = codeSandboxType === CodeSandboxType.V8_ISOLATE
-
-    if (codSandboxTypeIsV8Isolate) {
-        await packageManager.link({
-            path,
-            global: true,
-            packageName: 'isolated-vm',
-        })
-    }
 }
 
 type InstallParams = {
