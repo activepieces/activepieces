ec94218d6e4e382713b6ff4939bfabce47542daa
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2024-01-24 17:49:09 +0300

    feat: adds heartbeat

---
+++ packages/pieces/community/heartbeat/README.md
@@ -0,0 +1,7 @@
+# pieces-community-heartbeat
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build pieces-community-heartbeat` to build the library.
---
+++ packages/pieces/community/heartbeat/src/index.ts
@@ -0,0 +1,28 @@
+
+import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { heartBeatCreateUser } from "./lib/actions/create-user";
+
+const markdownPropertyDescription = `
+  1. Login to your Heartbeat account
+  2. On the bottom-left, click on 'Admin Settings'
+  3. On the left panel, click on 'API Keys'
+  5. Click on 'Create API Key'
+  6. On the popup form, Enter the 'Label' to name the Key
+  7. Copy the API key and paste it below.
+`;
+
+export const heartbeatAuth = PieceAuth.SecretText({
+  displayName: 'API Key',
+  description: markdownPropertyDescription,
+  required: true,
+});
+
+export const Heartbeat = createPiece({
+  displayName: "Heartbeat",
+  auth: heartbeatAuth,
+  minimumSupportedRelease: '0.9.0',
+  logoUrl: "https://canny.io/images/1c45977e303cc49b7230311e0eb3e87e.png", //TODO: replace logo
+  authors: [],
+  actions: [heartBeatCreateUser],
+  triggers: [],
+});
