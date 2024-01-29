310010fbe0d1a9d711b306f76c940aae53b0dd57
Kishan Parmar <code.kishanparmar@gmail.com>
2024-01-29 10:35:56 +0530

    fix(google-calendar): fix update action props/ piece version

--- packages/pieces/community/google-calendar/src/lib/actions/delete-event.action.ts
+++ packages/pieces/community/google-calendar/src/lib/actions/delete-event.action.ts
@@ -1,39 +1,35 @@
-import { googleCalendarAuth } from '../../index';
-import { Property, createAction } from "@activepieces/pieces-framework";
+import { Property, createAction } from '@activepieces/pieces-framework';
 import { google } from 'googleapis';
 import { OAuth2Client } from 'googleapis-common';
+import { googleCalendarAuth } from '../../index';
 import { googleCalendarCommon } from '../common';
 
-
-
 export const deleteEventAction = createAction({
-    displayName: 'Delete Event',
-    auth: googleCalendarAuth,
-    name: 'delete_event',
-    description: 'Delete an event from Google Calendar',
-    props: {
-        calendar_id: googleCalendarCommon.calendarDropdown('writer'),
-        eventId: Property.ShortText({
-            displayName: 'Event ID',
-            required: true,
-        }),
-    },
-    async run(context) {
-        const authClient = new OAuth2Client();
-        authClient.setCredentials(context.auth);
+  displayName: 'Delete Event',
+  auth: googleCalendarAuth,
+  name: 'delete_event',
+  description: 'Deletes an event from Google Calendar.',
+  props: {
+    calendar_id: googleCalendarCommon.calendarDropdown('writer'),
+    eventId: Property.ShortText({
+      displayName: 'Event ID',
+      required: true,
+    }),
+  },
+  async run(context) {
+    const authClient = new OAuth2Client();
+    authClient.setCredentials(context.auth);
 
-        const calendarId = context.propsValue.calendar_id;
-        const eventId = context.propsValue.eventId;
-        
-        const calendar = google.calendar({ version: 'v3', auth: authClient });
+    const calendarId = context.propsValue.calendar_id;
+    const eventId = context.propsValue.eventId;
 
-        const response = await calendar.events.delete({
-            calendarId,
-            eventId,
-        });
+    const calendar = google.calendar({ version: 'v3', auth: authClient });
 
-        return response;
+    const response = await calendar.events.delete({
+      calendarId,
+      eventId,
+    });
 
-    }
-
+    return response.data;
+  },
 });
--- packages/pieces/community/google-calendar/src/lib/actions/update-event.ation.ts
+++ packages/pieces/community/google-calendar/src/lib/actions/update-event.ation.ts
@@ -1,63 +1,59 @@
-import { googleCalendarAuth } from '../../index';
-import { Property, createAction } from "@activepieces/pieces-framework";
+import { Property, createAction } from '@activepieces/pieces-framework';
 import { google } from 'googleapis';
 import { OAuth2Client } from 'googleapis-common';
+import { googleCalendarAuth } from '../../index';
 import { googleCalendarCommon } from '../common';
 
-
-
 export const updateEventAction = createAction({
-    displayName: 'Update Event',
-    auth: googleCalendarAuth,
-    name: 'update_event',
-    description: 'Update an event in Google Calendar',
-    props: {
-        calendar_id: googleCalendarCommon.calendarDropdown('writer'),
-        eventId: Property.ShortText({
-            displayName: 'Event ID',
-            required: true,
-        }),
-        title: Property.ShortText({
-            displayName: 'Title of the event',
-            required: true,
-        }),
-        start_date_time: Property.DateTime({
-            displayName: 'Start date time of the event',
-            required: true,
-        }),
-        end_date_time: Property.DateTime({
-            displayName: 'End date time of the event',
-            required: true,
-        }),
-    },
-    async run(context) {
-        const authClient = new OAuth2Client();
-        authClient.setCredentials(context.auth);
+  displayName: 'Update Event',
+  auth: googleCalendarAuth,
+  name: 'update_event',
+  description: 'Updates an event in Google Calendar.',
+  props: {
+    calendar_id: googleCalendarCommon.calendarDropdown('writer'),
+    eventId: Property.ShortText({
+      displayName: 'Event ID',
+      required: true,
+    }),
+    title: Property.ShortText({
+      displayName: 'Title of the event',
+      required: false,
+    }),
+    start_date_time: Property.DateTime({
+      displayName: 'Start date time of the event',
+      required: false,
+    }),
+    end_date_time: Property.DateTime({
+      displayName: 'End date time of the event',
+      required: false,
+    }),
+  },
+  async run(context) {
+    const authClient = new OAuth2Client();
+    authClient.setCredentials(context.auth);
 
-        const calendarId = context.propsValue.calendar_id;
-        const eventId = context.propsValue.eventId;
-        const summary = context.propsValue.title;
-        const start = context.propsValue.start_date_time;
-        const end = context.propsValue.end_date_time;
+    const calendarId = context.propsValue.calendar_id;
+    const eventId = context.propsValue.eventId;
+    const summary = context.propsValue.title;
+    const start = context.propsValue.start_date_time;
+    const end = context.propsValue.end_date_time;
 
-        const calendar = google.calendar({ version: 'v3', auth: authClient });
+    const calendar = google.calendar({ version: 'v3', auth: authClient });
 
-        const response = await calendar.events.update({
-            calendarId,
-            eventId,
-            requestBody: {
-                summary,
-                start: {
-                    dateTime: start,
-                },
-                end: {
-                    dateTime: end,
-                },
-            },
-        });
+    const response = await calendar.events.update({
+      calendarId,
+      eventId,
+      requestBody: {
+        summary,
+        start: {
+          dateTime: start,
+        },
+        end: {
+          dateTime: end,
+        },
+      },
+    });
 
-        return response.data;
-
-    }
-
+    return response.data;
+  },
 });
