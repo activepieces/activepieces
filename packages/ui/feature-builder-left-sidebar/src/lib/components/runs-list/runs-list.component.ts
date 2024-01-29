3ad0067ca242bfd7c7ac78b4fe1b8ddb5bd4fcb0
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-01-29 14:58:59 +0300

    chore: introduce infinite scrolling to runs

--- packages/ui/feature-builder-left-sidebar/src/lib/components/runs-list/runs-list.component.ts
+++ packages/ui/feature-builder-left-sidebar/src/lib/components/runs-list/runs-list.component.ts
@@ -3,10 +3,26 @@ import {
   Component,
   Input,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { InstanceRunService } from '@activepieces/ui/common';
-import { FlowRun } from '@activepieces/shared';
-import { Observable, map } from 'rxjs';
+import { FlowRun, Cursor } from '@activepieces/shared';
+import {
+  BehaviorSubject,
+  Observable,
+  map,
+  mergeMap,
+  scan,
+  startWith,
+  tap,
+  throttleTime,
+} from 'rxjs';
+import {
+  LeftSideBarType,
+  canvasActions,
+} from '@activepieces/ui/feature-builder-store';
+import { Store } from '@ngrx/store';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-runs-list',
@@ -14,18 +30,65 @@ import { Observable, map } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RunsListComponent implements OnInit {
+  @ViewChild(CdkVirtualScrollViewport)
+  viewport: CdkVirtualScrollViewport;
   @Input({
     required: true,
   })
   projectId!: string;
+  @Input({
+    required: true,
+  })
+  flowId!: string;
   runs$!: Observable<FlowRun[]>;
-  constructor(private instanceRunService: InstanceRunService) {}
+  cursor: Cursor = null;
+  offset$ = new BehaviorSubject<Cursor>(null);
+  constructor(
+    private instanceRunService: InstanceRunService,
+    private store: Store
+  ) {}
   ngOnInit(): void {
-    this.runs$ = this.instanceRunService
-      .list(this.projectId, {
-        cursor: '',
-        limit: 50,
+    this.runs$ = this.offset$.pipe(
+      startWith(null),
+      tap(console.log),
+      throttleTime(500),
+      mergeMap(() => this.getBatch(this.cursor, this.flowId, this.projectId)),
+      scan((acc, batch) => {
+        return [...acc, ...batch];
+      }, [] as FlowRun[])
+    );
+  }
+  closeLeftSideBar() {
+    this.store.dispatch(
+      canvasActions.setLeftSidebar({
+        sidebarType: LeftSideBarType.NONE,
       })
-      .pipe(map((res) => res.data));
+    );
+  }
+  nextBatch() {
+    if (this.cursor === null) {
+      return;
+    }
+
+    const end = this.viewport.getRenderedRange().end;
+    const total = this.viewport.getDataLength();
+    if (end === total) {
+      this.offset$.next(this.cursor);
+    }
+  }
+  getBatch(cursor: Cursor, flowId: string, projectId: string) {
+    console.log('getBatch');
+    return this.instanceRunService
+      .list(projectId, {
+        flowId: flowId,
+        cursor: cursor || '',
+        limit: 30,
+      })
+      .pipe(
+        tap((res) => {
+          this.cursor = res.next;
+        }),
+        map((res) => res.data)
+      );
   }
 }
