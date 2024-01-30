36ed2c69ef9a8c811859fb3ee81cc202e771502e
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-01-30 12:42:25 +0000

    feat: add search query

--- packages/backend/src/app/pieces/piece-metadata-service/hooks.ts
+++ packages/backend/src/app/pieces/piece-metadata-service/hooks.ts
@@ -1,13 +1,17 @@
-import { PieceMetadataModelSummary } from '../piece-metadata-entity'
+import { PieceMetadataSchema } from '../piece-metadata-entity'
 import { PlatformId } from '@activepieces/ee-shared'
+import Fuse from 'fuse.js'
 
-const defaultHooks: PieceMetadataServiceHooks = {
-    async filterPieces({ pieces }) {
-        return pieces
+const defaultPieceHooks: PieceMetadataServiceHooks = {
+    async filterPieces(params) {
+        return filterPieceBasedOnSearchQuery({
+            searchQuery: params.searchQuery,
+            pieces: params.pieces,
+        })
     },
 }
 
-let hooks = defaultHooks
+let hooks = defaultPieceHooks
 
 export const pieceMetadataServiceHooks = {
     set(newHooks: PieceMetadataServiceHooks): void {
@@ -20,11 +24,39 @@ export const pieceMetadataServiceHooks = {
 }
 
 export type PieceMetadataServiceHooks = {
-    filterPieces(p: FilterPiecesParams): Promise<PieceMetadataModelSummary[]>
+    filterPieces(p: FilterPiecesParams): Promise<PieceMetadataSchema[]>
 }
 
 export type FilterPiecesParams = {
     includeHidden?: boolean
-    pieces: PieceMetadataModelSummary[]
     platformId?: PlatformId
+    searchQuery?: string
+    pieces: PieceMetadataSchema[]
+}
+
+export const filterPieceBasedOnSearchQuery = ({ searchQuery, pieces }: { searchQuery: string | undefined, pieces: PieceMetadataSchema[] }): PieceMetadataSchema[] => {
+    if (!searchQuery) {
+        return pieces
+    }
+
+    const convertPieces = pieces.map(p => ({
+        ...p,
+        actionOrTrigger: [...Object.values(p.actions).map(a => {
+            return {
+                name: a.name,
+                description: a.description,
+            }
+        }), ...Object.values(p.triggers).map(t => {
+            return {
+                name: t.name,
+                description: t.description,
+            }
+        })],
+    }))
+    const fuse = new Fuse(convertPieces, {
+        keys: ['name', 'description', 'actionOrTrigger.name', 'actionOrTrigger.description'],
+        threshold: 0.3,
+    })
+
+    return fuse.search(searchQuery).map(({ item }) => pieces.find(p => p.name === item.name)!)
 }
