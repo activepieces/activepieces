7055c81c4c186d88484680dbe3907259e44bedda
Mario Meyer <mariomeyer@gmail.com>
2024-02-01 18:15:24 -0500

    feat: odoo piece
    feat: include Get Contacts action

---
+++ packages/pieces/community/odoo/src/index.ts
@@ -0,0 +1,66 @@
+
+import { createPiece, PieceAuth, Property } from "@activepieces/pieces-framework";
+import Odoo from "./commom/index";
+import actions from './lib/actions';
+
+export const odooAuth = PieceAuth.CustomAuth({
+  props: {
+    base_url: Property.ShortText({
+      displayName: 'Odoo URL',
+      description: 'Enter the base URL',
+      required: true,
+    }),
+    database: Property.ShortText({
+      displayName: 'Odoo Database',
+      description: 'Enter the database name',
+      required: true,
+    }),
+    username: Property.ShortText({
+      displayName: 'Odoo Username',
+      description: 'Enter the username',
+      required: true,
+    }),
+    api_key: PieceAuth.SecretText({
+      displayName: 'Odoo API Key',
+      description: 'Enter the API Key',
+      required: true
+    })
+  },
+  // Optional Validation
+  validate: async ({ auth }) => {
+    const { base_url, database, username, api_key } = auth;
+
+    const odoo = new Odoo({
+      url: base_url,
+      port: 443,
+      db: database,
+      username: username,
+      password: api_key,
+    })
+
+    try {
+      await odoo.connect();
+
+      return {
+        valid: true,
+      };
+    } catch (err) {
+      return {
+        valid: false,
+        error:
+          'Connection failed. Please check your credentials and try again.',
+      };
+    }
+  },
+  required: true
+})
+
+export const odoo = createPiece({
+  displayName: "Odoo",
+  auth: odooAuth,
+  minimumSupportedRelease: '0.9.0',
+  logoUrl: "https://peliqan.io/wp-content/uploads/2023/01/odoo_icon.png",
+  authors: ['MarioMeyer'],
+  actions,
+  triggers: [],
+});
