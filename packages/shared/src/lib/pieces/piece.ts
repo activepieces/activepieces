80c3d3bc2824ab5875d1e54d2f6edf23b0ce91fe
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-02-01 16:23:58 +0000

    test: fix tests

--- packages/shared/src/lib/pieces/piece.ts
+++ packages/shared/src/lib/pieces/piece.ts
@@ -1,53 +1,53 @@
-import { Static, Type } from '@sinclair/typebox';
+import { Static, Type } from '@sinclair/typebox'
 
 export enum PackageType {
-  ARCHIVE = 'ARCHIVE',
-  REGISTRY = 'REGISTRY',
+    ARCHIVE = 'ARCHIVE',
+    REGISTRY = 'REGISTRY',
 }
 
 export enum PieceType {
-  CUSTOM = 'CUSTOM',
-  OFFICIAL = 'OFFICIAL',
+    CUSTOM = 'CUSTOM',
+    OFFICIAL = 'OFFICIAL',
 }
 
 export const PrivatePiecePackage = Type.Object({
-  packageType: Type.Literal(PackageType.ARCHIVE),
-  pieceType: Type.Enum(PieceType),
-  pieceName: Type.String(),
-  pieceVersion: Type.String(),
-  archiveId: Type.String(),
-});
+    packageType: Type.Literal(PackageType.ARCHIVE),
+    pieceType: Type.Enum(PieceType),
+    pieceName: Type.String(),
+    pieceVersion: Type.String(),
+    archiveId: Type.String(),
+})
 
-export type PrivatePiecePackage = Static<typeof PrivatePiecePackage>;
+export type PrivatePiecePackage = Static<typeof PrivatePiecePackage>
 
 export const PublicPiecePackage = Type.Object({
-  packageType: Type.Literal(PackageType.REGISTRY),
-  pieceType: Type.Enum(PieceType),
-  pieceName: Type.String(),
-  pieceVersion: Type.String(),
-});
+    packageType: Type.Literal(PackageType.REGISTRY),
+    pieceType: Type.Enum(PieceType),
+    pieceName: Type.String(),
+    pieceVersion: Type.String(),
+})
 
-export type PublicPiecePackage = Static<typeof PublicPiecePackage>;
+export type PublicPiecePackage = Static<typeof PublicPiecePackage>
 
-export type PiecePackage = PrivatePiecePackage | PublicPiecePackage;
+export type PiecePackage = PrivatePiecePackage | PublicPiecePackage
 
 export enum PieceCategory {
-  ARTIFICIAL_INTELLIGENCE = 'ARTIFICIAL_INTELLIGENCE',
-  BUSINESS_INTELLIGENCE = 'BUSINESS_INTELLIGENCE',
-  COMMUNICATION = 'COMMUNICATION',
-  COMMERCE = 'COMMERCE',
-  CONTENT_AND_FILES = 'CONTENT_AND_FILES',
-  CORE = 'CORE',
-  DATABASES = 'DATABASES',
-  DEVELOPER_TOOLS = 'DEVELOPER_TOOLS',
-  CUSTOMER_SERVICE = 'CUSTOMER_SERVICE',
-  FILE_MANAGEMENT_AND_STORAGE = 'FILE_MANAGEMENT_AND_STORAGE',
-  FORMS_AND_SURVEYS = 'FORMS_AND_SURVEYS',
-  HUMAN_RESOURCES = 'HUMAN_RESOURCES',
-  IT_OPERATIONS = 'IT_OPERATIONS',
-  MARKETING = 'MARKETING',
-  PROJECT_MANAGEMENT = 'PROJECT_MANAGEMENT',
-  SALES_AND_CRM = 'SALES_AND_CRM',
-  OTHER = 'OTHER',
-  WEBSITE_BUILDERS = 'WEBSITE_BUILDERS',
+    ARTIFICIAL_INTELLIGENCE = 'ARTIFICIAL_INTELLIGENCE',
+    BUSINESS_INTELLIGENCE = 'BUSINESS_INTELLIGENCE',
+    COMMUNICATION = 'COMMUNICATION',
+    COMMERCE = 'COMMERCE',
+    CONTENT_AND_FILES = 'CONTENT_AND_FILES',
+    CORE = 'CORE',
+    DATABASES = 'DATABASES',
+    DEVELOPER_TOOLS = 'DEVELOPER_TOOLS',
+    CUSTOMER_SERVICE = 'CUSTOMER_SERVICE',
+    FILE_MANAGEMENT_AND_STORAGE = 'FILE_MANAGEMENT_AND_STORAGE',
+    FORMS_AND_SURVEYS = 'FORMS_AND_SURVEYS',
+    HUMAN_RESOURCES = 'HUMAN_RESOURCES',
+    IT_OPERATIONS = 'IT_OPERATIONS',
+    MARKETING = 'MARKETING',
+    PROJECT_MANAGEMENT = 'PROJECT_MANAGEMENT',
+    SALES_AND_CRM = 'SALES_AND_CRM',
+    OTHER = 'OTHER',
+    WEBSITE_BUILDERS = 'WEBSITE_BUILDERS',
 }
