a444ec1b841b9524e54fcff7a1b3d1176b1dd4b7
Maor Rozenfeld <49363375+maor-rozenfeld@users.noreply.github.com>
2024-02-06 12:45:45 +0100

    chore: move ee-shared to shared

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -56,3 +56,22 @@ export * from './lib/flows/dto/flow-template-request'
 // Look at https://github.com/sinclairzx81/typebox/issues/350
 TypeSystem.ExactOptionalPropertyTypes = false
 export * from './lib/support-url'
+export * from './lib/git-repo';
+export * from './lib/api-key';
+export * from './lib/project/project-with-usage-and-plan';
+export * from './lib/billing';
+export * from './lib/project/project-requests';
+export * from './lib/custom-domains';
+export * from './lib/platform';
+export * from './lib/project-members/project-member-role';
+export * from './lib/referral/referral';
+export * from './lib/project-members/project-member-request';
+export * from './lib/project-members/project-member';
+export * from './lib/flow-templates';
+export * from './lib/product-embed/app-credentials/index';
+export * from './lib/product-embed/connection-keys/index';
+export * from './lib/signing-key'
+export * from './lib/managed-authn'
+export * from './lib/oauth-apps'
+export * from './lib/otp'
+export * from './lib/authn'
