beecfbc0503793c339d0b74b47140696df55cf5c
Olivier Sambourg <AdamSelene@users.noreply.github.com>
2024-02-10 16:42:02 +0100

    feat: add Snowflake piece

---
+++ packages/pieces/community/snowflake/src/index.ts
@@ -0,0 +1,56 @@
+import {
+  createPiece,
+  PieceAuth,
+  Property,
+} from '@activepieces/pieces-framework';
+import { runQuery } from './lib/actions/run-query';
+import { PieceCategory } from '@activepieces/shared';
+
+export const snowflakeAuth = PieceAuth.CustomAuth({
+  props: {
+    account: Property.ShortText({
+      displayName: 'Account',
+      required: true,
+      description: 'A string indicating the Snowflake account identifier.',
+    }),
+    username: Property.ShortText({
+      displayName: 'Username',
+      required: true,
+      description: 'The login name for your Snowflake user.',
+    }),
+    password: PieceAuth.SecretText({
+      displayName: 'Password',
+      description: 'Password for the user.',
+      required: true,
+    }),
+    database: Property.ShortText({
+      displayName: 'Database',
+      description:
+        'The default database to use for the session after connecting.',
+      required: false,
+    }),
+    role: Property.ShortText({
+      displayName: 'Role',
+      description:
+        'The default security role to use for the session after connecting.',
+      required: false,
+    }),
+    warehouse: Property.ShortText({
+      displayName: 'Warehouse',
+      description:
+        'The default virtual warehouse to use for the session after connecting. Used for performing queries, loading data, etc.',
+      required: false,
+    }),
+  },
+  required: true,
+});
+export const snowflake = createPiece({
+  displayName: 'Snowflake',
+  auth: snowflakeAuth,
+  minimumSupportedRelease: '0.9.0',
+  logoUrl: 'https://cdn.activepieces.com/pieces/snowflake.png',
+  categories: [PieceCategory.DEVELOPER_TOOLS],
+  authors: ['AdamSelene'],
+  actions: [runQuery],
+  triggers: [],
+});
