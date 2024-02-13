e07b69b9f411e31c3c41c63169cc9ac7d5377802
Kishan Parmar <code.kishanparmar@gmail.com>
2024-02-13 16:10:02 +0530

    feat: add test action

--- packages/pieces/community/microsoft-dynamics-crm/src/index.ts
+++ packages/pieces/community/microsoft-dynamics-crm/src/index.ts
@@ -1,19 +1,25 @@
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-import { testAction } from "./lib/actions/test-action";
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { testAction } from './lib/actions/test-action';
 
 export const microsoftDynamicsCRMAuth = PieceAuth.OAuth2({
   description: 'OAUTH2',
   required: true,
-  scope: ['https://org77f0100e.crm8.dynamics.com/.default'],
+  scope: [
+    'https://org77f0100e.crm8.dynamics.com/.default',
+    'openid',
+    'email',
+    'profile',
+    'offline_access',
+  ],
   authUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-  tokenUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
-})
+  tokenUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+});
 
 export const microsoftDynamicsCrm = createPiece({
-  displayName: "Microsoft Dynamics CRM",
+  displayName: 'Microsoft Dynamics CRM',
   auth: microsoftDynamicsCRMAuth,
   minimumSupportedRelease: '0.9.0',
-  logoUrl: "https://cdn.activepieces.com/pieces/microsoft-dynamics-crm.png",
+  logoUrl: 'https://cdn.activepieces.com/pieces/microsoft-dynamics-crm.png',
   authors: [],
   actions: [testAction],
   triggers: [],
--- packages/pieces/community/microsoft-dynamics-crm/src/lib/actions/test-action.ts
+++ packages/pieces/community/microsoft-dynamics-crm/src/lib/actions/test-action.ts
@@ -1,14 +1,34 @@
-import { microsoftDynamicsCRMAuth } from "../..";
-import { createAction } from "@activepieces/pieces-framework";
-
+import {
+  AuthenticationType,
+  HttpMethod,
+  HttpRequest,
+  httpClient,
+} from '@activepieces/pieces-common';
+import { createAction } from '@activepieces/pieces-framework';
+import { microsoftDynamicsCRMAuth } from '../..';
 export const testAction = createAction({
-    name: 'test_Action',
-    displayName: 'Test Action',
-    description: 'Test Action',
-    auth: microsoftDynamicsCRMAuth,
-    props: {},
-    async run(context) {
-        console.log("INSIDE RUN");
-        console.log(context);
-    }
-})
+  name: 'test_Action',
+  displayName: 'Test Action',
+  description: 'Test Action',
+  auth: microsoftDynamicsCRMAuth,
+  props: {},
+  async run(context) {
+    const request: HttpRequest = {
+      url: 'https://orgac098933.crm.dynamics.com/api/data/v9.2/leads',
+      method: HttpMethod.GET,
+      authentication: {
+        type: AuthenticationType.BEARER_TOKEN,
+        token: context.auth.access_token,
+      },
+      headers: {
+        'OData-MaxVersion': '4.0',
+        'OData-Version': '4.0',
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+    const res = await httpClient.sendRequest(request);
+    console.log(res);
+    return res.body;
+  },
+});
