8168c89420fdee43a169a94c99dcbf7b49ecf66b
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-02-20 12:13:39 +0100

    chore: remove wrong test

--- packages/server/api/test/integration/cloud/signing-key/signing-key.test.ts
+++ packages/server/api/test/integration/cloud/signing-key/signing-key.test.ts
@@ -64,34 +64,6 @@ describe('Signing Key API', () => {
             expect(responseBody.algorithm).toBe('RSA')
         }, 10000)
 
-        it('Fails if platform is not found', async () => {
-            // arrange
-            const nonExistentPlatformId = apId()
-
-            const testToken = await generateMockToken({
-                type: PrincipalType.USER,
-                platform: {
-                    id: nonExistentPlatformId,
-                    role: PlatformRole.OWNER,
-                },
-            })
-
-            const mockSigningKeyName = faker.lorem.word()
-            const response = await app?.inject({
-                method: 'POST',
-                url: '/v1/signing-keys',
-                body: {
-                    displayName: mockSigningKeyName,
-                },
-                headers: {
-                    authorization: `Bearer ${testToken}`,
-                },
-            })
-
-            // assert
-            expect(response?.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
-        })
-
         it('Fails if user is not platform owner', async () => {
             // arrange
             const mockUser = createMockUser()
