7858a3873f9f314252690991460327efed430c46
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-02-21 10:18:18 +0000

    fix: add category to form

--- packages/pieces/community/forms/src/index.ts
+++ packages/pieces/community/forms/src/index.ts
@@ -3,13 +3,15 @@ import { returnFile } from './lib/actions/return-file';
 import { returnMarkdown } from './lib/actions/return-markdown';
 import { onFormSubmission } from './lib/triggers/form-trigger';
 import { onFileSubmission } from './lib/triggers/file-trigger';
+import { PieceCategory } from '@activepieces/shared';
 
 export const forms = createPiece({
-  displayName: 'Forms',
+  displayName: 'Forms (BETA)',
   description: 'Trigger a flow through form interfaces.',
   auth: PieceAuth.None(),
-  minimumSupportedRelease: '0.19.0',
-  logoUrl: 'https://cdn.activepieces.com/pieces/interfaces.png',
+  minimumSupportedRelease: '0.20.3',
+  categories: [PieceCategory.CORE],
+  logoUrl: 'https://cdn.activepieces.com/pieces/forms.png',
   authors: ['MoShizzle'],
   actions: [
     returnFile,
