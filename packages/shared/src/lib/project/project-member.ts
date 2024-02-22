9affdad8b8397d0f04ffe1bf26f306f3cd15b676
Khaled Mashaly <kmmashaly@gmail.com>
2024-02-22 17:35:32 +0200

    refactor(api): add external customer role and return role in authn response

---
+++ packages/shared/src/lib/project/project-member.ts
@@ -0,0 +1,10 @@
+export enum ProjectMemberRole {
+    ADMIN = 'ADMIN',
+    EDITOR = 'EDITOR',
+    VIEWER = 'VIEWER',
+    /**
+     * Members who are customers for our customers
+     */
+    EXTERNAL_CUSTOMER = 'EXTERNAL_CUSTOMER',
+}
+
