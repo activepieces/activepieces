00906e2b1c362f0a4859946e135292845b376b0d
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2024-02-22 10:55:24 +0300

    chore: refresh table on new connection created

--- packages/ui/common/src/lib/service/app-connections.service.ts
+++ packages/ui/common/src/lib/service/app-connections.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import {
   SeekPage,
   AppConnectionId,
@@ -15,15 +15,17 @@ import { environment } from '../environments/environment';
   providedIn: 'root',
 })
 export class AppConnectionsService {
-  constructor(private http: HttpClient) {}
-
+  constructor(private http: HttpClient) { }
+  private _newConnectionCreated$: Subject<boolean> = new Subject();
   upsert(
     request: UpsertAppConnectionRequestBody
   ): Observable<AppConnectionWithoutSensitiveData> {
-    return this.http.post<AppConnectionWithoutSensitiveData>(
-      environment.apiUrl + '/app-connections',
-      request
-    );
+    return this.http
+      .post<AppConnectionWithoutSensitiveData>(
+        environment.apiUrl + '/app-connections',
+        request
+      )
+      .pipe(tap(() => this._newConnectionCreated$.next(true)));
   }
 
   list(
@@ -55,5 +57,8 @@ export class AppConnectionsService {
     return this.http.delete<void>(
       environment.apiUrl + '/app-connections/' + id
     );
+  }
+  get newConnectionCreated$() {
+    return this._newConnectionCreated$.asObservable();
   }
 }
