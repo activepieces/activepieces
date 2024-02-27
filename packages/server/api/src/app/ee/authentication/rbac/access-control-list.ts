da18ac03c422d244e1115066255d0633aa779e35
Khaled Mashaly <kmmashaly@gmail.com>
2024-02-28 00:31:46 +0200

    refactor(api): rbac authz logic

---
+++ packages/server/api/src/app/ee/authentication/rbac/access-control-list.ts
@@ -0,0 +1,77 @@
+import { ProjectMemberRole } from '@activepieces/shared'
+import { ResourceAction, ResourceName } from './resources'
+
+export enum Permission {
+    READ_ACTIVITY = 'READ_ACTIVITY',
+    READ_APP_CONNECTION = 'READ_APP_CONNECTION',
+    WRITE_APP_CONNECTION = 'WRITE_APP_CONNECTION',
+    READ_FLOW = 'READ_FLOW',
+    WRITE_FLOW = 'WRITE_FLOW',
+    READ_PROJECT_MEMBER = 'READ_PROJECT_MEMBER',
+    WRITE_PROJECT_MEMBER = 'WRITE_PROJECT_MEMBER',
+}
+
+export const rolePermissions: Record<ProjectMemberRole, Permission[]> = {
+    [ProjectMemberRole.ADMIN]: [
+        Permission.READ_ACTIVITY,
+        Permission.READ_APP_CONNECTION,
+        Permission.WRITE_APP_CONNECTION,
+        Permission.READ_FLOW,
+        Permission.WRITE_FLOW,
+        Permission.READ_PROJECT_MEMBER,
+        Permission.WRITE_PROJECT_MEMBER,
+    ],
+    [ProjectMemberRole.EDITOR]: [
+        Permission.READ_ACTIVITY,
+        Permission.READ_APP_CONNECTION,
+        Permission.WRITE_APP_CONNECTION,
+        Permission.READ_FLOW,
+        Permission.WRITE_FLOW,
+    ],
+    [ProjectMemberRole.VIEWER]: [
+        Permission.READ_ACTIVITY,
+        Permission.READ_APP_CONNECTION,
+        Permission.READ_FLOW,
+    ],
+    [ProjectMemberRole.EXTERNAL_CUSTOMER]: [
+        Permission.READ_ACTIVITY,
+        Permission.READ_APP_CONNECTION,
+        Permission.WRITE_APP_CONNECTION,
+    ],
+}
+
+export const accessControlList: Record<Permission, AccessControl> = {
+    [Permission.READ_ACTIVITY]: {
+        resource: 'activities',
+        actions: ['GET'],
+    },
+    [Permission.READ_APP_CONNECTION]: {
+        resource: 'app-connections',
+        actions: ['GET'],
+    },
+    [Permission.WRITE_APP_CONNECTION]: {
+        resource: 'app-connections',
+        actions: ['POST', 'DELETE'],
+    },
+    [Permission.READ_FLOW]: {
+        resource: 'flows',
+        actions: ['GET'],
+    },
+    [Permission.WRITE_FLOW]: {
+        resource: 'flows',
+        actions: ['POST', 'DELETE'],
+    },
+    [Permission.READ_PROJECT_MEMBER]: {
+        resource: 'project-members',
+        actions: ['GET'],
+    },
+    [Permission.WRITE_PROJECT_MEMBER]: {
+        resource: 'project-members',
+        actions: ['POST', 'DELETE'],
+    },
+}
+
+type AccessControl = {
+    resource: ResourceName
+    actions: ResourceAction[]
+}
---
+++ packages/server/api/src/app/ee/authentication/rbac/rbac-middleware.ts
@@ -0,0 +1,148 @@
+import { FastifyRequest } from 'fastify'
+import {
+    ActivepiecesError,
+    ApEdition,
+    ErrorCode,
+    PrincipalType,
+    ProjectId,
+    ProjectMemberRole,
+    isNil,
+} from '@activepieces/shared'
+import { projectMemberService } from '../../project-members/project-member.service'
+import { getEdition } from '../../../helper/secret-helper'
+import { extractResourceName } from '../../../authentication/authorization'
+import { ResourceAction, ResourceName, accessControlledResourceNames } from './resources'
+import { accessControlList, rolePermissions } from './access-control-list'
+
+const EDITION_IS_COMMUNITY = getEdition() === ApEdition.COMMUNITY
+
+export const rbacMiddleware = async (req: FastifyRequest): Promise<void> => {
+    if (ignoreRequest(req)) {
+        return
+    }
+
+    const resourceName = extractResourceNameOrThrow(req)
+    const resourceAction = extractResourceAction(req)
+    const projectId = req.principal.projectId
+
+    const role = await getRoleOrThrow({
+        projectId,
+        userId: req.principal.id,
+        resourceName,
+        resourceAction,
+    })
+
+    const access = grantAccess({
+        role,
+        resourceName,
+        resourceAction,
+    })
+
+    if (!access) {
+        throwPermissionDenied({
+            projectId,
+            resourceName,
+            resourceAction,
+        })
+    }
+}
+
+const grantAccess = ({ role, resourceName, resourceAction }: GrantAccessArgs): boolean => {
+    const permissions = rolePermissions[role]
+
+    return permissions.some(permission => {
+        const { resource, actions } = accessControlList[permission]
+        return resource === resourceName && actions.includes(resourceAction)
+    })
+}
+
+const ignoreRequest = (req: FastifyRequest): boolean => {
+    if (EDITION_IS_COMMUNITY) {
+        return true
+    }
+
+    const ignoredPrefixes = ['/redirect', '/ui', '/v1/project-members/accept']
+    if (ignoredPrefixes.some(p => req.url.startsWith(p))) {
+        return true
+    }
+
+    if (req.principal.type === PrincipalType.SERVICE) {
+        return true
+    }
+
+    const resourceName = extractResourceNameOrThrow(req)
+
+    return !accessControlledResourceNames.includes(resourceName)
+}
+
+const getRoleOrThrow = async (params: GetRoleOrThrowArgs): Promise<ProjectMemberRole> => {
+    const { projectId, userId, resourceName, resourceAction } = params
+
+    const role = await projectMemberService.getRole({
+        projectId,
+        userId,
+    })
+
+    if (isNil(role)) {
+        throw new ActivepiecesError({
+            code: ErrorCode.PERMISSION_DENIED,
+            params: {
+                projectId,
+                resource: resourceName,
+                action: resourceAction,
+            },
+        })
+    }
+
+    return role
+
+}
+
+const extractResourceNameOrThrow = (req: FastifyRequest): ResourceName => {
+    const resourceName = extractResourceName(req.url) as ResourceName | undefined
+
+    if (isNil(resourceName)) {
+        throw new ActivepiecesError({
+            code: ErrorCode.AUTHORIZATION,
+            params: {
+                message: 'Invalid resource',
+            },
+        })
+    }
+
+    return resourceName
+}
+
+const extractResourceAction = (req: FastifyRequest): ResourceAction => {
+    return req.method as ResourceAction
+}
+
+const throwPermissionDenied = ({ projectId, resourceName, resourceAction }: ThrowPermissionDeniedArgs): never => {
+    throw new ActivepiecesError({
+        code: ErrorCode.PERMISSION_DENIED,
+        params: {
+            projectId,
+            resource: resourceName,
+            action: resourceAction,
+        },
+    })
+}
+
+type GrantAccessArgs = {
+    role: ProjectMemberRole
+    resourceName: ResourceName
+    resourceAction: ResourceAction
+}
+
+type ThrowPermissionDeniedArgs = {
+    projectId: ProjectId
+    resourceName: ResourceName
+    resourceAction: ResourceAction
+}
+
+type GetRoleOrThrowArgs = {
+    projectId: ProjectId
+    userId: string
+    resourceName: ResourceName
+    resourceAction: ResourceAction
+}
