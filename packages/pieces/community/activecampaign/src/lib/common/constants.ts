22de1c2a9f7a1e7383d96e4827ea69184cf7f306
Kishan Parmar <code.kishanparmar@gmail.com>
2024-02-28 19:10:02 +0530

    feat(activecampaign): add contact and deal actions/triggers

---
+++ packages/pieces/community/activecampaign/src/lib/common/constants.ts
@@ -0,0 +1,16 @@
+export const WEBHOOK_SOURCES = ['public', 'admin', 'api', 'system'];
+
+export enum CUSTOM_FIELD_TYPE {
+	TEXT = 'text',
+	DROPDOWN = 'dropdown',
+	TEXTAREA = 'textarea',
+	NUMBER = 'number',
+	MONEY = 'currency',
+	DATE = 'date',
+	DATETIME = 'datetime',
+	LIST_BOX = 'listbox',
+	MULTISELECT = 'multiselect',
+	RADIO = 'radio',
+	CHECKBOX = 'checkbox',
+	HIDDEN = 'hidden',
+}
