db84afc9bbb1fae8ae172c9177dcf1bd9c5e5969
Kishan Parmar <code.kishanparmar@gmail.com>
2024-03-06 11:58:24 +0530

    feat(mailerlite): add subsciber trigger

--- packages/pieces/community/mailer-lite/src/lib/actions/create-or-update-subscription.ts
+++ packages/pieces/community/mailer-lite/src/lib/actions/create-or-update-subscription.ts
@@ -2,12 +2,13 @@ import MailerLite from '@mailerlite/mailerlite-nodejs';
 import { createAction, Property } from '@activepieces/pieces-framework';
 import { mailerLiteAuth } from '../..';
 import { mailerLiteCommon } from '../common';
+import dayjs from 'dayjs';
 
 export const createOrUpdateSubscriber = createAction({
 	auth: mailerLiteAuth,
 	name: 'add_or_update_subscriber',
 	displayName: 'Add or Update subscriber',
-	description: 'Create or update a existing subscription',
+	description: 'Create a new subscriber or updates an existing one if the email already exists.',
 	props: {
 		email: Property.ShortText({
 			displayName: 'Email',
@@ -50,12 +51,10 @@ export const createOrUpdateSubscriber = createAction({
 		subscribed_at: Property.DateTime({
 			displayName: 'Subscribed On',
 			required: false,
-			description: 'Provide YYYY-MM-DD HH:mm:ss format.',
 		}),
 		opted_in_at: Property.DateTime({
 			displayName: 'Opt-in Date',
 			required: false,
-			description: 'Provide YYYY-MM-DD HH:mm:ss format.',
 		}),
 		ip_address: Property.ShortText({
 			displayName: 'Signup IP address',
@@ -73,12 +72,15 @@ export const createOrUpdateSubscriber = createAction({
 			fields: context.propsValue.subscriberFields,
 			groups: context.propsValue.subscriberGroupId,
 			status: context.propsValue.status as status,
-			subscribed_at: context.propsValue.subscribed_at,
-			opted_in_at: context.propsValue.opted_in_at,
+			subscribed_at: context.propsValue.subscribed_at
+				? dayjs(context.propsValue.subscribed_at).format('YYYY-MM-DD HH:mm:ss')
+				: undefined,
+			opted_in_at: context.propsValue.opted_in_at
+				? dayjs(context.propsValue.opted_in_at).format('YYYY-MM-DD HH:mm:ss')
+				: undefined,
 			ip_address: context.propsValue.ip_address,
 			optin_ip: context.propsValue.optin_ip,
 		});
-
 		return response.data;
 	},
 });
