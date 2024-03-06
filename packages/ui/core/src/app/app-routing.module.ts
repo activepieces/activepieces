bcfb91a4a3009cc74d6b60764aa4d31a11d3fd52
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-03-06 20:45:17 +0300

    fix: validate conneciton name in embedded connections

--- packages/ui/core/src/app/app-routing.module.ts
+++ packages/ui/core/src/app/app-routing.module.ts
@@ -48,6 +48,7 @@ export const routes: Routes = [
     component: EmbeddedConnectionDialogComponent,
     canActivate: [
       showBasedOnEditionGuard([ApEdition.ENTERPRISE, ApEdition.CLOUD]),
+      UserLoggedIn,
     ],
   },
   {
