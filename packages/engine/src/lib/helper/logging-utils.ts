5ee6a83eaaec6994b6678a0d00e5620f9b1902c0
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-03-12 03:56:52 +0000

    fix: engine log trimmer

--- packages/engine/src/lib/helper/logging-utils.ts
+++ packages/engine/src/lib/helper/logging-utils.ts
@@ -3,7 +3,7 @@ import sizeof from 'object-sizeof'
 import { isMemoryFilePath } from '../services/files.service'
 
 const TRUNCATION_TEXT_PLACEHOLDER = '(truncated)'
-const MAX_SINGLE_SIZE_FOR_SINGLE_ENTRY = 512 * 1024
+const MAX_SINGLE_SIZE_FOR_SINGLE_ENTRY = 1024 * 1024
 
 export const loggingUtils = {
     async trimExecution(steps: Record<string, StepOutput>): Promise<Record<string, StepOutput>> {
@@ -12,9 +12,11 @@ export const loggingUtils = {
             const stepOutput = steps[stepName]
             clonedSteps[stepName] = await trimStepOutput(stepOutput)
         }
-        return clonedSteps
+        // TThe above code could trim data in different steps, but their total size could still be too big
+        return trimStepsUntilSizeLimit(clonedSteps)
     },
 }
+
 
 async function trimStepOutput(stepOutput: StepOutput): Promise<StepOutput> {
     const modified: StepOutput = JSON.parse(JSON.stringify(stepOutput))
@@ -23,22 +25,71 @@ async function trimStepOutput(stepOutput: StepOutput): Promise<StepOutput> {
         case ActionType.BRANCH:
         case TriggerType.EMPTY:
         case TriggerType.PIECE:
+        case ActionType.CODE:
+        case ActionType.PIECE:
             modified.output = await applyFunctionToValues(modified.output, trim)
             break
         case ActionType.LOOP_ON_ITEMS: {
             const loopItem = modified.output
             if (loopItem) {
-                loopItem.iterations = await applyFunctionToValues(loopItem.iterations, trim)
+                const newIterations = [];
+                for (const iteration of loopItem.iterations) {
+                    const trimmedIteration = await loggingUtils.trimExecution(iteration);
+                    newIterations.push(trimmedIteration);
+                }
                 loopItem.item = await applyFunctionToValues(loopItem.item, trim)
             }
             break
         }
-        case ActionType.CODE:
-        case ActionType.PIECE:
     }
     modified.errorMessage = await applyFunctionToValues(modified.errorMessage, trim)
     return modified
 }
+
+async function trimStepsUntilSizeLimit(steps: Record<string, StepOutput>): Promise<Record<string, StepOutput>> {
+    const clonedSteps = { ...steps }
+    const entries = Object.entries(steps).sort(bySizeDesc)
+    let i = 0
+    while (i < entries.length && sizeof(clonedSteps) > MAX_SINGLE_SIZE_FOR_SINGLE_ENTRY) {
+        const [stepName, stepOutput] = entries[i]
+        clonedSteps[stepName] = await emptyStepOutputAndPreserveStructure(stepOutput)
+        i += 1
+    }
+    return clonedSteps
+}
+
+async function emptyStepOutputAndPreserveStructure(stepOutput: StepOutput): Promise<StepOutput> {
+    const modified: StepOutput = JSON.parse(JSON.stringify(stepOutput))
+    modified.input = TRUNCATION_TEXT_PLACEHOLDER
+    switch (modified.type) {
+        case ActionType.BRANCH:
+        case TriggerType.EMPTY:
+        case TriggerType.PIECE:
+        case ActionType.CODE:
+        case ActionType.PIECE:
+            modified.output = TRUNCATION_TEXT_PLACEHOLDER
+            break
+        case ActionType.LOOP_ON_ITEMS: {
+            const loopItem = modified.output
+            if (loopItem) {
+                const newIterations = [];
+                for (const iteration of loopItem.iterations) {
+                    const newTrimmedIteration: Record<string, StepOutput> = {}
+                    for (const key in iteration) {
+                        newTrimmedIteration[key] = await emptyStepOutputAndPreserveStructure(iteration[key])
+                    }
+                    newIterations.push(newTrimmedIteration);
+                }
+                loopItem.item = TRUNCATION_TEXT_PLACEHOLDER
+
+            }
+            break
+        }
+    }
+    modified.errorMessage = TRUNCATION_TEXT_PLACEHOLDER
+    return modified
+}
+
 
 const trim = async (obj: unknown): Promise<unknown> => {
     if (isMemoryFilePath(obj)) {
