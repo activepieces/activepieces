428c16de349a7847d10fe2869aa19df5de26ca91
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-03-14 15:16:21 +0300

    chore: add dropdown labels joiner pipe

---
+++ packages/ui/feature-builder-form-controls/src/lib/pipes/dropdown-labels-joiner.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+//TODO: ask about imports from @activepieces/pieces-framework in ui
+import { DropdownOption } from '@activepieces/pieces-framework';
+
+@Pipe({
+  name: 'dropdownLabelsJoiner',
+  pure: true,
+  standalone: true,
+})
+/**Since we are using virutal scrolling inside dropdown, this means that options don't get rendered until the dropdown is clicked, making the dropdown unable to show the selected value/s
+     This pipe is used to show the selected value/s in the dropdown*/
+export class DropdownLabelsJoiner implements PipeTransform {
+  transform(options: DropdownOption<unknown>[]): string {
+    return options.map((option) => option.label).join(', ');
+  }
+}
