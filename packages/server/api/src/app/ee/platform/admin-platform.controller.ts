305106d1664ade2c1b4ed8a59c1dec2ab0680dd0
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-03-15 03:37:06 +0000

    fix: decrease tasks

--- packages/server/api/src/app/ee/platform/admin-platform.controller.ts
+++ packages/server/api/src/app/ee/platform/admin-platform.controller.ts
@@ -1,7 +1,8 @@
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
+import { FastifyPluginAsyncTypebox, Type } from '@fastify/type-provider-typebox'
 import { StatusCodes } from 'http-status-codes'
 import { adminPlatformService } from './admin-platform.service'
 import { AdminAddPlatformRequestBody, PrincipalType } from '@activepieces/shared'
+import { projectUsageService } from '../../project/usage/project-usage-service'
 
 export const adminPlatformPieceModule: FastifyPluginAsyncTypebox = async (app) => {
     await app.register(adminPlatformController, { prefix: '/v1/admin/platforms' })
@@ -15,6 +16,26 @@ const adminPlatformController: FastifyPluginAsyncTypebox = async (
 
         return res.status(StatusCodes.CREATED).send(newPlatform)
     })
+
+    app.post('/tasks', DecreaseTasks, async (req, res) => {
+        for (const { projectId, tasks } of req.body) {
+            await projectUsageService.increaseTasks(projectId, -2 * tasks)
+        }
+
+        return res.status(StatusCodes.CREATED).send({})
+    })
+}
+
+const DecreaseTasks = {
+    schema: {
+        body: Type.Array(Type.Object({
+            projectId: Type.String(),
+            tasks: Type.Number()
+        }))
+    },
+    config: {
+        allowedPrincipals: [PrincipalType.SUPER_USER],
+    },
 }
 
 const AdminAddPlatformRequest = {
