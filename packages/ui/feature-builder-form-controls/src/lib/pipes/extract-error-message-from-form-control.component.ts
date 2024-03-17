1d324f8d04d4dcd694f96f5bb4f77eb7554518e2
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2024-03-17 04:28:25 +0300

    fix: lint

--- packages/ui/feature-builder-form-controls/src/lib/pipes/extract-error-message-from-form-control.component.ts
+++ packages/ui/feature-builder-form-controls/src/lib/pipes/extract-error-message-from-form-control.component.ts
@@ -1,27 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { PropertyType } from '@activepieces/pieces-framework';
 import { Observable, map } from 'rxjs';
 
 @Pipe({
   name: 'extractControlErrorMessage',
   pure: true,
-  standalone:true
+  standalone: true,
 })
 export class ExtractControlErrorMessage implements PipeTransform {
-  transform(
-    value: FormControl,
-    propertyName:string
-  ): Observable<string> {
-    return value.valueChanges.pipe(map(()=>{
-        if(value.invalid && value.hasError('required')){
-            return `${propertyName} is required`
-        }
-        else if(value.invalid)
-        {
-            return `${propertyName} is invalid`
+  transform(value: FormControl, propertyName: string): Observable<string> {
+    return value.valueChanges.pipe(
+      map(() => {
+        if (value.invalid && value.hasError('required')) {
+          return `${propertyName} is required`;
+        } else if (value.invalid) {
+          return `${propertyName} is invalid`;
         }
         return '';
-    }))
+      })
+    );
   }
 }
