abacb2c5640261a76489ec535ed5bda27fc40594
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-03-25 21:12:00 +0300

    chore: add fade in animations for video

--- packages/ui/common/src/lib/components/upgrade-note/upgrade-note.component.ts
+++ packages/ui/common/src/lib/components/upgrade-note/upgrade-note.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { fadeIn400ms } from '../../animation/fade-in.animations';
 
 @Component({
   selector: 'ap-upgrade-note',
   templateUrl: './upgrade-note.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeIn400ms],
 })
 export class UpgradeNoteComponent {
   @Input() pricingUrl = 'https://www.activepieces.com/sales';
@@ -11,6 +13,7 @@ export class UpgradeNoteComponent {
   @Input({ required: true }) featureNoteTitle = '';
   @Input({ required: true }) featureNote = '';
   @Input() videoUrl = '';
+  @Input() insideTab = false;
   openPricing() {
     window.open(this.pricingUrl, '_blank', 'noopener noreferrer');
   }
