cd297f26279a81035b8a20336f9f6fcff7d1490f
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-03-28 21:13:54 +0300

    chore: adding yaml support

--- packages/ui/common/src/lib/utils/consts.ts
+++ packages/ui/common/src/lib/utils/consts.ts
@@ -48,6 +48,19 @@ export const flowActionsUiInfo = {
   iconSizeTailWind: 'ap-w-[20px] ap-h-[20px]',
 };
 
+export const downloadYaml = (val: string, fileName: string) => {
+  const blob = new Blob([val], {
+    type: 'application/yaml',
+  });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = `${fileName}.yaml`;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+};
 export const downloadJson = (obj: any, fileName: string) => {
   const blob = new Blob([JSON.stringify(obj, null, 2)], {
     type: 'application/json',
@@ -70,6 +83,13 @@ export const jsonEditorOptionsMonaco = {
   minimap: { enabled: false },
   theme: 'apTheme',
   language: 'json',
+  readOnly: true,
+  automaticLayout: true,
+};
+export const yamlEditorOptionsMonaco = {
+  minimap: { enabled: false },
+  theme: 'apTheme',
+  language: 'yaml',
   readOnly: true,
   automaticLayout: true,
 };
