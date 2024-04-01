92d4dae8b97344105356531d2e082034fc6beb55
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-04-01 13:57:53 +0300

    fix: demo platform settings urls

--- packages/ee/ui-platform/src/lib/pages/platform-settings/platform-settings.component.ts
+++ packages/ee/ui-platform/src/lib/pages/platform-settings/platform-settings.component.ts
@@ -24,23 +24,28 @@ export class PlatformSettingsComponent implements AfterViewInit {
   readonly AuditLogTabTitle = $localize`Audit Log`;
   readonly customDomainTabTitle = $localize`Custom Domains`;
   readonly accountManagementEmailTabTitle = $localize`Mail Server`;
-  readonly tabIndexFragmentMap: { [index: number]: string } = {
-    0: 'SigningKeys',
-    1: 'MailServer',
-    2: 'CustomDomains',
-    3: 'ApiKeys',
-    4: 'SSO',
-    5: 'AuditLog',
-  };
+  readonly tabIndexFragmentMap = [
+    { fragmentName: 'SigningKeys', removeOnDemo: true },
+    { fragmentName: 'MailServer', removeOnDemo: true },
+    { fragmentName: 'CustomDomains', removeOnDemo: true },
+    { fragmentName: 'ApiKeys', removeOnDemo: false },
+    { fragmentName: 'SSO', removeOnDemo: false },
+    { fragmentName: 'AuditLog', removeOnDemo: false },
+  ];
   isDemo = false;
   platform?: Platform;
   constructor(private router: Router, private route: ActivatedRoute) {
     this.isDemo = this.route.snapshot.data[PLATFORM_DEMO_RESOLVER_KEY];
+    if (this.isDemo) {
+      this.tabIndexFragmentMap = this.tabIndexFragmentMap.filter(
+        (i) => !i.removeOnDemo
+      );
+    }
     this.platform = this.route.snapshot.data[PLATFORM_RESOLVER_KEY];
     this.fragmentChanged$ = this.route.fragment.pipe(
       tap((fragment) => {
         if (fragment === null) {
-          this.updateFragment(this.tabIndexFragmentMap[0]);
+          this.updateFragment(this.tabIndexFragmentMap[0].fragmentName);
         } else {
           this.fragmentCheck(fragment);
         }
@@ -50,7 +55,7 @@ export class PlatformSettingsComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     const fragment = this.route.snapshot.fragment;
     if (fragment === null) {
-      this.updateFragment(this.tabIndexFragmentMap[0]);
+      this.updateFragment(this.tabIndexFragmentMap[0].fragmentName);
     } else {
       this.fragmentCheck(fragment);
     }
@@ -58,8 +63,8 @@ export class PlatformSettingsComponent implements AfterViewInit {
 
   private fragmentCheck(fragment: string) {
     if (this.tabGroup) {
-      const tabIndex = Object.values(this.tabIndexFragmentMap).indexOf(
-        fragment
+      const tabIndex = this.tabIndexFragmentMap.findIndex(
+        (i) => i.fragmentName === fragment
       );
       if (tabIndex >= 0) {
         this.tabGroup.selectedIndex = tabIndex;
@@ -73,10 +78,8 @@ export class PlatformSettingsComponent implements AfterViewInit {
     });
   }
   tabChanged(event: MatTabChangeEvent) {
-    const checkIfTabIndexIsInTabsMap = (index: number) => {
-      return Object.keys(this.tabIndexFragmentMap).includes(index.toString());
-    };
-    if (!checkIfTabIndexIsInTabsMap(event.index)) return;
-    this.updateFragment(this.tabIndexFragmentMap[event.index]);
+    if (event.index < 0 || event.index >= this.tabIndexFragmentMap.length)
+      return;
+    this.updateFragment(this.tabIndexFragmentMap[event.index].fragmentName);
   }
 }
