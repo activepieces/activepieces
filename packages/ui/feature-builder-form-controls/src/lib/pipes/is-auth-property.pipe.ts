b7226db8867b545b92b2c3d2b169c0a02ecd3dc6
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-04-02 15:15:25 +0300

    chore: only show connection description inside new connection dialog

---
+++ packages/ui/feature-builder-form-controls/src/lib/pipes/is-auth-property.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { PieceProperty, PropertyType } from '@activepieces/pieces-framework';
+const AUTH_PROPERTIES = [
+  PropertyType.BASIC_AUTH,
+  PropertyType.OAUTH2,
+  PropertyType.SECRET_TEXT,
+  PropertyType.CUSTOM_AUTH,
+];
+@Pipe({
+  name: 'isAuthProperty',
+  pure: true,
+  standalone: true,
+})
+/**Since we are using virutal scrolling inside dropdown, this means that options don't get rendered until the dropdown is clicked, making the dropdown unable to show the selected value/s
+     This pipe is used to show the selected value/s in the dropdown*/
+export class IsAuthPropertyPipe implements PipeTransform {
+  transform(property: PieceProperty): boolean {
+    return AUTH_PROPERTIES.includes(property.type);
+  }
+}
