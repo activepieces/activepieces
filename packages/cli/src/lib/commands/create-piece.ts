b5c40d8d87aba841832af5199baebcc9e136613f
Kishan Parmar <code.kishanparmar@gmail.com>
2024-04-02 14:34:43 +0530

    fix(cli): fix create piece lint option error

--- packages/cli/src/lib/commands/create-piece.ts
+++ packages/cli/src/lib/commands/create-piece.ts
@@ -60,8 +60,8 @@ const nxGenerateNodeLibrary = async (
     '--buildable',
     '--projectNameAndRootFormat=as-provided',
     '--strict',
-    '--unitTestRunner=none'
-  ].join(' ')
+    '--unitTestRunner=none',
+  ].join(' ');
 
   console.log(chalk.blue(`🛠️ Executing nx command: ${nxGenerateCommand}`));
 
@@ -127,11 +127,19 @@ const updateProjectJsonConfig = async (
   projectJson.targets.build.options.updateBuildableProjectDepsInPackageJson =
     true;
 
-  const lintFilePatterns = projectJson.targets.lint.options.lintFilePatterns;
-  const patternIndex = lintFilePatterns.findIndex((item) =>
-    item.endsWith('package.json')
-  );
-  if (patternIndex !== -1) lintFilePatterns?.splice(patternIndex, 1);
+  if (projectJson.targets.lint) {
+    const lintFilePatterns = projectJson.targets.lint.options.lintFilePatterns;
+    const patternIndex = lintFilePatterns.findIndex((item) =>
+      item.endsWith('package.json')
+    );
+    if (patternIndex !== -1) lintFilePatterns?.splice(patternIndex, 1);
+  } else {
+    projectJson.targets.lint = {
+      executor: '@nx/eslint:lint',
+      outputs: ['{options.outputFile}'],
+    };
+  }
+
   await writeProjectJson(
     `packages/pieces/${pieceType}/${pieceName}`,
     projectJson
@@ -174,7 +182,6 @@ const createPiece = async (
     )
   );
 };
-
 
 export const createPieceCommand = new Command('create')
   .description('Create a new piece')
