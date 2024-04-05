365cc18e6b3584f7243ab4432e6b058fc07a61d5
Pablo Fernandez <54374282+pfernandez98@users.noreply.github.com>
2024-04-05 02:54:39 +0000

    feat: add new piece, elevenlabs

---
+++ packages/pieces/community/elevenlabs/README.md
@@ -0,0 +1,7 @@
+# pieces-elevenlabs
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build pieces-elevenlabs` to build the library.
---
+++ packages/pieces/community/elevenlabs/src/index.ts
@@ -0,0 +1,43 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { textToSpeech } from './lib/actions/text-to-speech-action';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
+
+const markdownDescription = `
+Follow these instructions to get your API Key:
+1. Visit your Elevenlabs dashboard
+2. Once there, click on your account in the bottom left corner
+3. Press Profile + API Key
+4. Copy the API Key.
+
+Please, take into consideration: We don't test your API Key validity. So make sure this is the correct one.
+`;
+
+export const elevenlabsAuth = PieceAuth.SecretText({
+  description: markdownDescription,
+  displayName: 'API Key',
+  required: true,
+  validate: async () => {
+    return {
+      valid: true,
+    };
+  },
+});
+
+export const elevenlabs = createPiece({
+  displayName: 'Elevenlabs',
+  auth: elevenlabsAuth,
+  minimumSupportedRelease: '0.20.0',
+  logoUrl: 'https://cdn.activepieces.com/pieces/elevenlabs.png',
+  authors: ['pfernandez98'],
+  actions: [
+    textToSpeech,
+    createCustomApiCallAction({
+      baseUrl: () => 'https://api.elevenlabs.io',
+      auth: elevenlabsAuth,
+      authMapping: (auth) => ({
+        'xi-api-key': `${auth}`,
+      }),
+    }),
+  ],
+  triggers: [],
+});
