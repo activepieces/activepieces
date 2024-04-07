8bad940dd30a14de0c47b4005419670e7796cc1f
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2024-04-08 01:48:38 +0300

    fix: remove circular dependency

--- packages/ui/common/src/lib/resolvers/platform.resolver.ts
+++ packages/ui/common/src/lib/resolvers/platform.resolver.ts
@@ -1,10 +1,10 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
-import { PlatformService, ProjectSelectors } from '@activepieces/ui/common';
-import { AuthenticationService } from '@activepieces/ui/common';
 import { Platform } from '@activepieces/shared';
 import { Store } from '@ngrx/store';
 import { of, take, switchMap } from 'rxjs';
+import { AuthenticationService, PlatformService } from '../service';
+import { ProjectSelectors } from '../store';
 export const PLATFORM_RESOLVER_KEY = 'platform';
 export const PlatformResolver: ResolveFn<Platform | null> = () => {
   const platformService: PlatformService = inject(PlatformService);
