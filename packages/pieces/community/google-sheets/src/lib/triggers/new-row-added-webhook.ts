eb387ab288fef75a5b622bf261e560128d4f5fbc
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-04-08 16:43:04 +0000

    fix(google-sheets): renew notification webhooks

--- packages/pieces/community/google-sheets/src/lib/triggers/new-row-added-webhook.ts
+++ packages/pieces/community/google-sheets/src/lib/triggers/new-row-added-webhook.ts
@@ -3,6 +3,7 @@ import {
   PiecePropValueSchema,
   Property,
   TriggerStrategy,
+  WebhookRenewStrategy,
   createTrigger,
 } from '@activepieces/pieces-framework';
 import { google } from 'googleapis';
@@ -11,6 +12,7 @@ import { googleSheetsAuth } from '../..';
 import { columnToLabel, googleSheetsCommon } from '../common/common';
 import { nanoid } from 'nanoid';
 import crypto from 'crypto';
+import dayjs from 'dayjs';
 
 export const newRowAddedTrigger = createTrigger({
   auth: googleSheetsAuth,
@@ -25,6 +27,10 @@ export const newRowAddedTrigger = createTrigger({
     spreadsheet_id: googleSheetsCommon.spreadsheet_id,
     sheet_id: googleSheetsCommon.sheet_id,
     include_team_drives: googleSheetsCommon.include_team_drives,
+  },
+  renewConfiguration: {
+    strategy: WebhookRenewStrategy.CRON,
+    cronExpression: '0 0 * * *',
   },
   type: TriggerStrategy.WEBHOOK,
   async onEnable(context) {
@@ -207,6 +213,7 @@ async function createFileNotification(
     fileId: fileId,
     requestBody: {
       id: channelId,
+      expiration: dayjs().add(6, 'day').toISOString(),
       type: 'web_hook',
       address: url,
     },
