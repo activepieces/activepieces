3710c4dbe91fcb7d0517536d85384ad35d43e00f
Timóteo <152414548+tintinthedev@users.noreply.github.com>
2024-04-09 19:14:47 +0000

    feat(discord): add action to ban member from discord guild

--- packages/ui/common/src/lib/environments/environment.ts
+++ packages/ui/common/src/lib/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/v1',
+  apiUrl: 'https://glorious-space-umbrella-r44pqjvvjj76hwpj5-3000.app.github.dev/v1',
   jwtTokenName: 'token',
   redirectUrl: 'http://localhost:4200/redirect',
   userPropertyNameInLocalStorage: 'currentUser',
