86937087e2a016f61fff19152f61db4cd4615558
Timóteo <152414548+tintinthedev@users.noreply.github.com>
2024-04-10 19:29:43 +0000

    feat(discord): add remove ban from user action

--- packages/pieces/community/discord/src/index.ts
+++ packages/pieces/community/discord/src/index.ts
@@ -12,6 +12,7 @@ import { discordDeleteChannel } from './lib/actions/delete-channel';
 import { discordSendApprovalMessage } from './lib/actions/send-approval-message';
 import { discordSendMessageWebhook } from './lib/actions/send-message-webhook';
 import { newMessage } from './lib/trigger/new-message';
+import { discordRemoveBanFromUser } from './lib/actions/remove-ban-from-user';
 
 const markdown = `
 To obtain a token, follow these steps:
@@ -45,6 +46,7 @@ export const discord = createPiece({
     discordCreateChannel,
     discordDeleteChannel,
     discordFindChannel,
+    discordRemoveBanFromUser,
     createCustomApiCallAction({
       baseUrl: () => {
         return 'https://discord.com/api/v9';
@@ -56,6 +58,17 @@ export const discord = createPiece({
       },
     }),
   ],
-  authors: ["creed983","TaskMagicKyle","karimkhaleel","Abdallah-Alwarawreh","kishanprmr","MoShizzle","AbdulTheActivePiecer","khaledmashaly","abuaboud"],
+  authors: [
+    'creed983',
+    'TaskMagicKyle',
+    'karimkhaleel',
+    'Abdallah-Alwarawreh',
+    'kishanprmr',
+    'MoShizzle',
+    'AbdulTheActivePiecer',
+    'khaledmashaly',
+    'abuaboud',
+    'tintinthedev',
+  ],
   triggers: [newMessage],
 });
