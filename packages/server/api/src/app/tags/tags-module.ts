3ff8f61d866ccd81e626bf11e473b4fb093b0f05
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-04-11 15:17:11 +0000

    fix: add permission to tags

--- packages/server/api/src/app/tags/tags-module.ts
+++ packages/server/api/src/app/tags/tags-module.ts
@@ -1,9 +1,9 @@
 import { FastifyPluginAsyncTypebox, Type } from '@fastify/type-provider-typebox'
 import { StatusCodes } from 'http-status-codes'
+import { platformMustBeOwnedByCurrentUser } from '../ee/authentication/ee-authorization'
 import { pieceTagService } from './pieces/piece-tag.service'
 import { tagService } from './tag-service'
 import { assertNotNullOrUndefined, EndpointScope, ListTagsRequest, PrincipalType, SeekPage, SetPieceTagsRequest, Tag, UpsertTagRequest } from '@activepieces/shared'
-import { platformMustBeOwnedByCurrentUser } from '../ee/authentication/ee-authorization'
 
 
 export const tagsModule: FastifyPluginAsyncTypebox = async (app) => {
