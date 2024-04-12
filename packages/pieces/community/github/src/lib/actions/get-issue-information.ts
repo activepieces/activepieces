3bb3199da611eb0e8b4808632b201c340ed6d33f
Kishan Parmar <code.kishanparmar@gmail.com>
2024-04-12 12:12:39 +0530

    chore: add repo dropdown

--- packages/pieces/community/github/src/lib/actions/get-issue-information.ts
+++ packages/pieces/community/github/src/lib/actions/get-issue-information.ts
@@ -13,16 +13,7 @@ export const githubGetIssueInformation = createAction({
   displayName: 'Get issue information',
   description: 'Grabs information from a specific issue',
   props: {
-    repo_owner: Property.ShortText({
-      displayName: 'Repository Owner',
-      description: 'The owner of the repository (not case sensitive)',
-      required: true,
-    }),
-    repo_name: Property.ShortText({
-      displayName: 'Repository Name',
-      description: 'The name of the repository (not case sensitive)',
-      required: true,
-    }),
+    repository: githubCommon.repositoryDropdown,
     issue_number: Property.Number({
       displayName: 'Issue Number',
       description: 'The number of the issue you want to get information from',
@@ -30,8 +21,10 @@ export const githubGetIssueInformation = createAction({
     }),
   },
   async run(configValue) {
+    const issueNumber = configValue.propsValue.issue_number;
+    const { owner, repo } = configValue.propsValue.repository!;
     const request: HttpRequest = {
-      url: `${githubCommon.baseUrl}/repos/${configValue.propsValue.repo_owner}/${configValue.propsValue.repo_name}/issues/${configValue.propsValue.issue_number}`,
+      url: `${githubCommon.baseUrl}/repos/${owner}/${repo}/issues/${issueNumber}`,
       method: HttpMethod.GET,
       headers: {
         'X-GitHub-Api-Version': '2022-11-28',
