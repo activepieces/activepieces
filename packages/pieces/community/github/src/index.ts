d8eed02187093ed5c0f14114a6eddd5c30876426
Timóteo <152414548+tintinthedev@users.noreply.github.com>
2024-04-12 17:53:32 +0000

    feat(github): add action to lock an issue

--- packages/pieces/community/github/src/index.ts
+++ packages/pieces/community/github/src/index.ts
@@ -7,6 +7,7 @@ import {
 import { PieceCategory } from '@activepieces/shared';
 import { githubCreateIssueAction } from './lib/actions/create-issue';
 import { githubTriggers } from './lib/trigger';
+import { githubLockIssueAction } from './lib/actions/lock-issue';
 
 export const githubAuth = PieceAuth.OAuth2({
   required: true,
@@ -26,6 +27,7 @@ export const github = createPiece({
   auth: githubAuth,
   actions: [
     githubCreateIssueAction,
+    githubLockIssueAction,
     createCustomApiCallAction({
       baseUrl: () => 'https://api.github.com',
       auth: githubAuth,
@@ -34,6 +36,13 @@ export const github = createPiece({
       }),
     }),
   ],
-  authors: ["kishanprmr","MoShizzle","AbdulTheActivePiecer","khaledmashaly","abuaboud"],
+  authors: [
+    'kishanprmr',
+    'MoShizzle',
+    'AbdulTheActivePiecer',
+    'khaledmashaly',
+    'abuaboud',
+    'tintinthedev',
+  ],
   triggers: githubTriggers,
 });
