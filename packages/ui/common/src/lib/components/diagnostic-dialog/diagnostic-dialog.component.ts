0337555f4cc58201acd98d68e5065d112bfd1d6f
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2024-04-14 16:39:55 +0300

    chore: added json-viewer and warning box to diagnostics dialog

--- packages/ui/common/src/lib/components/diagnostic-dialog/diagnostic-dialog.component.ts
+++ packages/ui/common/src/lib/components/diagnostic-dialog/diagnostic-dialog.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormControl } from '@angular/forms';
 import { UiCommonModule } from '../../ui-common.module';
 import { jsonEditorOptionsMonaco } from '../../utils/consts';
-import { Observable, forkJoin, of, switchMap } from 'rxjs';
+import { Observable, catchError, forkJoin, map } from 'rxjs';
 import { FlagService } from '../../service';
 import { ApFlagId } from '@activepieces/shared';
 
@@ -21,35 +20,34 @@ export type DialogData = {
 export class DiagnosticDialogComponent {
   data: DialogData;
   readonly jsonEditorOptionsMonaco = jsonEditorOptionsMonaco;
-  enrichData$: Observable<void>;
-  jsonFormControl: FormControl<unknown>;
+  enrichData$: Observable<string>;
   showSupport$: Observable<boolean>;
   constructor(
     @Inject(MAT_DIALOG_DATA) dialogData: DialogData,
     flagsService: FlagService
   ) {
     this.data = dialogData;
-    this.jsonFormControl = new FormControl();
     this.showSupport$ = flagsService.isFlagEnabled(ApFlagId.SHOW_COMMUNITY);
     this.enrichData$ = forkJoin({
       edition: flagsService.getEdition(),
       currentVersion: flagsService.getStringFlag(ApFlagId.CURRENT_VERSION),
     }).pipe(
-      switchMap(({ edition, currentVersion }) => {
-        this.jsonFormControl.setValue(
-          JSON.stringify(
-            {
-              instance: {
-                edition,
-                currentVersion,
-              },
-              error: this.data.information,
+      map(({ edition, currentVersion }) => {
+        return JSON.stringify(
+          {
+            instance: {
+              edition,
+              currentVersion,
             },
-            null,
-            2
-          )
+            error: this.data.information,
+          },
+          null,
+          2
         );
-        return of(void 0);
+      }),
+      catchError((err) => {
+        console.error(err);
+        return JSON.stringify({ error: this.data.information });
       })
     );
   }
