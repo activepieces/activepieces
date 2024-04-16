2f62775d4d63bb1fc74761175f1c08f1adaa79ad
Kishan Parmar <code.kishanparmar@gmail.com>
2024-04-16 18:33:12 +0530

    feat(google-sheets): new or updated row trigger

--- packages/pieces/community/google-sheets/src/index.ts
+++ packages/pieces/community/google-sheets/src/index.ts
@@ -1,9 +1,5 @@
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
-import {
-  OAuth2PropertyValue,
-  PieceAuth,
-  createPiece,
-} from '@activepieces/pieces-framework';
+import { OAuth2PropertyValue, PieceAuth, createPiece } from '@activepieces/pieces-framework';
 import { PieceCategory } from '@activepieces/shared';
 import { clearSheetAction } from './lib/actions/clear-sheet';
 import { deleteRowAction } from './lib/actions/delete-row.action';
@@ -14,56 +10,57 @@ import { insertRowAction } from './lib/actions/insert-row.action';
 import { updateRowAction } from './lib/actions/update-row';
 import { googleSheetsCommon } from './lib/common/common';
 import { newRowAddedTrigger } from './lib/triggers/new-row-added-webhook';
+import { newOrUpdatedRowTrigger } from './lib/triggers/new-or-updated-row.trigger';
 
 export const googleSheetsAuth = PieceAuth.OAuth2({
-  description: '',
+	description: '',
 
-  authUrl: 'https://accounts.google.com/o/oauth2/auth',
-  tokenUrl: 'https://oauth2.googleapis.com/token',
-  required: true,
-  scope: [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive.readonly',
-  ],
+	authUrl: 'https://accounts.google.com/o/oauth2/auth',
+	tokenUrl: 'https://oauth2.googleapis.com/token',
+	required: true,
+	scope: [
+		'https://www.googleapis.com/auth/spreadsheets',
+		'https://www.googleapis.com/auth/drive.readonly',
+	],
 });
 
 export const googleSheets = createPiece({
-  minimumSupportedRelease: '0.5.0',
-  logoUrl: 'https://cdn.activepieces.com/pieces/google-sheets.png',
-  categories: [PieceCategory.PRODUCTIVITY],
-  authors: [
-    'ShayPunter',
-    'Ozak93',
-    'Abdallah-Alwarawreh',
-    'Salem-Alaa',
-    'kishanprmr',
-    'MoShizzle',
-    'AbdulTheActivePiecer',
-    'khaledmashaly',
-    'abuaboud',
-  ],
-  actions: [
-    insertRowAction,
-    deleteRowAction,
-    updateRowAction,
-    findRowsAction,
-    clearSheetAction,
-    findRowByNumAction,
-    getRowsAction,
-    createCustomApiCallAction({
-      auth: googleSheetsAuth,
-      baseUrl: () => {
-        return googleSheetsCommon.baseUrl;
-      },
-      authMapping: (auth) => {
-        return {
-          Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
-        };
-      },
-    }),
-  ],
-  displayName: 'Google Sheets',
-  description: 'Create, edit, and collaborate on spreadsheets online',
-  triggers: [newRowAddedTrigger],
-  auth: googleSheetsAuth,
+	minimumSupportedRelease: '0.5.0',
+	logoUrl: 'https://cdn.activepieces.com/pieces/google-sheets.png',
+	categories: [PieceCategory.PRODUCTIVITY],
+	authors: [
+		'ShayPunter',
+		'Ozak93',
+		'Abdallah-Alwarawreh',
+		'Salem-Alaa',
+		'kishanprmr',
+		'MoShizzle',
+		'AbdulTheActivePiecer',
+		'khaledmashaly',
+		'abuaboud',
+	],
+	actions: [
+		insertRowAction,
+		deleteRowAction,
+		updateRowAction,
+		findRowsAction,
+		clearSheetAction,
+		findRowByNumAction,
+		getRowsAction,
+		createCustomApiCallAction({
+			auth: googleSheetsAuth,
+			baseUrl: () => {
+				return googleSheetsCommon.baseUrl;
+			},
+			authMapping: (auth) => {
+				return {
+					Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
+				};
+			},
+		}),
+	],
+	displayName: 'Google Sheets',
+	description: 'Create, edit, and collaborate on spreadsheets online',
+	triggers: [newRowAddedTrigger, newOrUpdatedRowTrigger],
+	auth: googleSheetsAuth,
 });
