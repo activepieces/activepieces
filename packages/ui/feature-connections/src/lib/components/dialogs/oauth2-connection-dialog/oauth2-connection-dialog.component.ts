6501a057bf28fa1f306a6282b9f7fad26e874062
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2024-04-16 21:24:27 +0300

    chore: introduce connection form control to oauth2 dialog

--- packages/ui/feature-connections/src/lib/components/dialogs/oauth2-connection-dialog/oauth2-connection-dialog.component.ts
+++ packages/ui/feature-connections/src/lib/components/dialogs/oauth2-connection-dialog/oauth2-connection-dialog.component.ts
@@ -33,8 +33,7 @@ import {
 } from '../../../models/oauth2-popup-params.interface';
 import { CloudAuthConfigsService } from '../../../services/cloud-auth-configs.service';
 import { AppConnectionsService } from '@activepieces/ui/common';
-import { ConnectionValidator } from '../../../validators/connectionNameValidator';
-import { connectionNameRegex } from '../utils';
+import { createConnectionNameControl } from '../utils';
 
 interface OAuth2PropertySettings {
   redirect_url: FormControl<string>;
@@ -73,8 +72,6 @@ export class OAuth2ConnectionDialogComponent implements OnInit {
     'Copy this URL and paste it under Redirect URL in your app on the 3rd party service. Activepieces predefines this because we manage the authentication flow.';
   scopesTooltip =
     'The permissions needed to access the endpoints you plan to work with on the 3rd party service.';
-  keyTooltip =
-    'The ID of this authentication definition. You will need to select this key whenever you want to reuse this authentication.';
   hasCloudAuthCred$: Observable<boolean>;
   upsert$: Observable<AppConnectionWithoutSensitiveData | null>;
   constructor(
@@ -110,23 +107,9 @@ export class OAuth2ConnectionDialogComponent implements OnInit {
         nonNullable: true,
         validators: [Validators.required],
       }),
-      name: new FormControl(
-        this.appConnectionsService.getConnectionNameSuggest(
-          this.dialogData.pieceName
-        ),
-        {
-          nonNullable: true,
-          validators: [
-            Validators.required,
-            Validators.pattern(connectionNameRegex),
-          ],
-          asyncValidators: [
-            ConnectionValidator.createValidator(
-              this.appConnectionsService.getAllOnce(),
-              undefined
-            ),
-          ],
-        }
+      name: createConnectionNameControl(
+        this.appConnectionsService,
+        this.dialogData.pieceName
       ),
       value: new FormControl<OAuth2PopupResponse | null>(null, {
         validators: Validators.required,
