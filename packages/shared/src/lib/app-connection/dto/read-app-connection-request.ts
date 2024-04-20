64501076be8be858e0148746037c107b057d268d
Islam Abdelfattah <islam_9t9@yahoo.com>
2024-04-20 19:23:45 +0000

    feat: add connectionName and filters in request query

--- packages/shared/src/lib/app-connection/dto/read-app-connection-request.ts
+++ packages/shared/src/lib/app-connection/dto/read-app-connection-request.ts
@@ -4,6 +4,9 @@ export const ListAppConnectionsRequestQuery = Type.Object({
     cursor: Type.Optional(Type.String({})),
     projectId: Type.String(),
     pieceName: Type.Optional(Type.String({})),
+    connectionName: Type.Optional(Type.String({})),
+    createdAfter: Type.Optional(Type.String({})),
+    createdBefore: Type.Optional(Type.String({})),
     limit: Type.Optional(Type.Number({})),
 })
 export type ListAppConnectionsRequestQuery = Static<typeof ListAppConnectionsRequestQuery>
