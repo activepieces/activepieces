b990d6fa41b38f4f139eb5cdb197ce1d5cf54068
Islam Abdelfattah <islam_9t9@yahoo.com>
2024-04-20 19:27:30 +0000

    feat: add connectionName and date filter to connection component

--- packages/ui/feature-dashboard/src/lib/pages/connections-table/connections-table.component.ts
+++ packages/ui/feature-dashboard/src/lib/pages/connections-table/connections-table.component.ts
@@ -5,24 +5,34 @@ import {
   ViewChild,
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute } from '@angular/router';
-import { map, Observable, tap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  map,
+  Observable,
+  tap,
+  Subject,
+  startWith,
+  distinctUntilChanged,
+} from 'rxjs';
 import {
   AppConnection,
   AppConnectionStatus,
   SeekPage,
 } from '@activepieces/shared';
+import { ConnectionsTableDataSource } from './connections-table.datasource';
 import {
   DeleteEntityDialogComponent,
   DeleteEntityDialogData,
+  ApPaginatorComponent,
+  DATE_RANGE_END_QUERY_PARAM,
+  DATE_RANGE_START_QUERY_PARAM,
+  AppConnectionsService,
 } from '@activepieces/ui/common';
-import { ConnectionsTableDataSource } from './connections-table.datasource';
-import { ApPaginatorComponent } from '@activepieces/ui/common';
-import { AppConnectionsService } from '@activepieces/ui/common';
 import { PieceMetadataService } from '@activepieces/ui/feature-pieces';
 import { NewConnectionDialogComponent } from '../../components/dialogs/new-connection-dialog/new-connection-dialog.component';
 import { AddEditConnectionButtonComponent } from '@activepieces/ui/feature-connections';
 import { PieceMetadataModelSummary } from '@activepieces/pieces-framework';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './connections-table.component.html',
@@ -35,25 +45,68 @@ export class ConnectionsTableComponent implements OnInit {
   paginator!: ApPaginatorComponent;
   connectionPage$: Observable<SeekPage<AppConnection>>;
   dataSource!: ConnectionsTableDataSource;
+  refreshTableForReruns$: Subject<boolean> = new Subject();
   title = $localize`Connections`;
   newConnectionPiece?: PieceMetadataModelSummary;
   newConnectionDialogClosed$?: Observable<PieceMetadataModelSummary>;
   displayedColumns = ['app', 'name', 'status', 'created', 'updated', 'action'];
   deleteConnectionDialogClosed$?: Observable<void>;
   readonly AppConnectionStatus = AppConnectionStatus;
+  filtersChanged$: Observable<void>;
+  dateFormGroup = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
   constructor(
+    private router: Router,
     private activatedRoute: ActivatedRoute,
     private pieceMetadataService: PieceMetadataService,
     private connectionService: AppConnectionsService,
     private dialogService: MatDialog
-  ) {}
+  ) {
+    const startDate = this.activatedRoute.snapshot.queryParamMap.get(
+      DATE_RANGE_START_QUERY_PARAM
+    );
+    const endDate = this.activatedRoute.snapshot.queryParamMap.get(
+      DATE_RANGE_END_QUERY_PARAM
+    );
+    this.dateFormGroup.setValue({
+      start: startDate ? new Date(startDate) : null,
+      end: endDate ? new Date(endDate) : null,
+    });
+  }
 
   ngOnInit(): void {
+    this.filtersChanged$ = this.dateFormGroup.valueChanges.pipe(
+      startWith(this.dateFormGroup.value),
+      distinctUntilChanged(
+        (prev, curr) => prev.start === curr.start && prev.end === curr.end
+      ),
+      tap((result) => {
+        const createdAfter = result.start ? new Date(result.start) : undefined;
+        const createdBefore = result.end ? new Date(result.end) : undefined;
+        if (createdBefore) {
+          createdBefore.setHours(23, 59, 59, 999);
+        }
+        this.router.navigate(['connections'], {
+          queryParams: {
+            createdAfter: createdAfter ? createdAfter.toISOString() : undefined,
+            createdBefore: createdBefore
+              ? createdBefore.toISOString()
+              : undefined,
+          },
+          queryParamsHandling: 'merge',
+        });
+      }),
+      map(() => undefined)
+    );
+
     this.dataSource = new ConnectionsTableDataSource(
       this.activatedRoute.queryParams,
       this.paginator,
       this.pieceMetadataService,
-      this.connectionService
+      this.connectionService,
+      this.refreshTableForReruns$.asObservable().pipe(startWith(true))
     );
   }
 
@@ -84,5 +137,22 @@ export class ConnectionsTableComponent implements OnInit {
           });
         })
       );
+  }
+
+  handleOnChange(e: Event) {
+    const event = e.target as HTMLInputElement;
+
+    this.dataSource = new ConnectionsTableDataSource(
+      this.activatedRoute.queryParams.pipe(
+        map((res) => ({
+          ...res,
+          connectionName: event.value,
+        }))
+      ),
+      this.paginator,
+      this.pieceMetadataService,
+      this.connectionService,
+      this.refreshTableForReruns$.asObservable().pipe(startWith(true))
+    );
   }
 }
