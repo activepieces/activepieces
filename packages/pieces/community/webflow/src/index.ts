bbc65528d9206485970757b571f561800cff37c6
Kishan Parmar <code.kishanparmar@gmail.com>
2024-04-30 19:43:33 +0530

    feat(webflow): add forms scope

--- packages/pieces/community/webflow/src/index.ts
+++ packages/pieces/community/webflow/src/index.ts
@@ -1,5 +1,9 @@
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
-import { OAuth2PropertyValue, PieceAuth, createPiece } from '@activepieces/pieces-framework';
+import {
+  OAuth2PropertyValue,
+  PieceAuth,
+  createPiece,
+} from '@activepieces/pieces-framework';
 import { PieceCategory } from '@activepieces/shared';
 import { webflowCreateCollectionItemAction } from './lib/actions/create-collection-item';
 import { webflowDeleteCollectionItem } from './lib/actions/delete-collection-item';
@@ -13,52 +17,53 @@ import { webflowUpdateCollectionItem } from './lib/actions/update-collection-ite
 import { webflowNewSubmission } from './lib/triggers/new-form-submitted';
 
 export const webflowAuth = PieceAuth.OAuth2({
-	description: '',
-	authUrl: 'https://webflow.com/oauth/authorize',
-	tokenUrl: 'https://api.webflow.com/oauth/access_token',
-	required: true,
-	scope: [
-		'sites:write',
-		'sites:read',
-		'cms:write',
-		'cms:read',
-		'ecommerce:read',
-		'ecommerce:write',
-	],
+  description: '',
+  authUrl: 'https://webflow.com/oauth/authorize',
+  tokenUrl: 'https://api.webflow.com/oauth/access_token',
+  required: true,
+  scope: [
+    'forms:read',
+    'sites:write',
+    'sites:read',
+    'cms:write',
+    'cms:read',
+    'ecommerce:read',
+    'ecommerce:write',
+  ],
 });
 
 export const webflow = createPiece({
-	displayName: 'Webflow',
-	description: 'Design, build, and launch responsive websites visually',
-	minimumSupportedRelease: '0.5.0',
-	logoUrl: 'https://cdn.activepieces.com/pieces/webflow.png',
-	categories: [PieceCategory.MARKETING],
-	authors: [
-		'Ahmad-AbuOsbeh',
-		'TaskMagicKyle',
-		'kishanprmr',
-		'MoShizzle',
-		'khaledmashaly',
-		'abuaboud',
-	],
-	auth: webflowAuth,
-	actions: [
-		webflowCreateCollectionItemAction,
-		webflowDeleteCollectionItem,
-		webflowUpdateCollectionItem,
-		webflowFindCollectionItem,
-		webflowGetCollectionItem,
-		webflowFulfillOrder,
-		webflowUnfulfillOrder,
-		webflowRefundOrder,
-		webflowFindOrder,
-		createCustomApiCallAction({
-			baseUrl: () => 'https://api.webflow.com',
-			auth: webflowAuth,
-			authMapping: (auth) => ({
-				Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
-			}),
-		}),
-	],
-	triggers: [webflowNewSubmission],
+  displayName: 'Webflow',
+  description: 'Design, build, and launch responsive websites visually',
+  minimumSupportedRelease: '0.5.0',
+  logoUrl: 'https://cdn.activepieces.com/pieces/webflow.png',
+  categories: [PieceCategory.MARKETING],
+  authors: [
+    'Ahmad-AbuOsbeh',
+    'TaskMagicKyle',
+    'kishanprmr',
+    'MoShizzle',
+    'khaledmashaly',
+    'abuaboud',
+  ],
+  auth: webflowAuth,
+  actions: [
+    webflowCreateCollectionItemAction,
+    webflowDeleteCollectionItem,
+    webflowUpdateCollectionItem,
+    webflowFindCollectionItem,
+    webflowGetCollectionItem,
+    webflowFulfillOrder,
+    webflowUnfulfillOrder,
+    webflowRefundOrder,
+    webflowFindOrder,
+    createCustomApiCallAction({
+      baseUrl: () => 'https://api.webflow.com',
+      auth: webflowAuth,
+      authMapping: (auth) => ({
+        Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
+      }),
+    }),
+  ],
+  triggers: [webflowNewSubmission],
 });
