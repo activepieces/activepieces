4cf9712f3177f8d8d1d20fb5157f8b6382ac666f
Abdelrahman Mostafa <Abdelraahmanmostafa@gmail.com>
2024-05-06 18:48:59 +0000

    fix(whatsapp): description markdown

--- packages/pieces/community/whatsapp/src/index.ts
+++ packages/pieces/community/whatsapp/src/index.ts
@@ -9,13 +9,12 @@ To Obtain a Phone Number ID and a Permanent System User Access Token, follow the
 1. Go to https://developers.facebook.com/
 2. Make a new app, Select Other for usecase.
 3. Choose Business as the type of app.
-4. Fill the App Domain with Domain in Redirect URL.
-5. Add new Product -> WhatsApp.
-6. Navigate to WhatsApp Settings > API Setup.
-7. Select a phone number and copy its Phone number ID.
-8. Login to your [Meta Business Manager](https://business.facebook.com/).
-9. Click on Settings.
-10. Create a new System User with access over the app and copy the access token.
+4. Add new Product -> WhatsApp.
+5. Navigate to WhatsApp Settings > API Setup.
+6. Select a phone number and copy its Phone number ID.
+7. Login to your [Meta Business Manager](https://business.facebook.com/).
+8. Click on Settings.
+9. Create a new System User with access over the app and copy the access token.
 `;
 
 export const whatsappAuth = PieceAuth.CustomAuth({
