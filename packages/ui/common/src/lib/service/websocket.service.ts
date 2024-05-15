56119c3b5ed3b363043cde5ec83474b866da42dd
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-05-15 11:51:51 +0300

    chore: address comments

--- packages/ui/common/src/lib/service/websocket.service.ts
+++ packages/ui/common/src/lib/service/websocket.service.ts
@@ -23,7 +23,7 @@ export class WebSocketService {
     return {
       url: resolveSocketUrl(environment.apiUrl),
       options: {
-        transports: ['websocket', 'polling'],
+        transports: ['websocket'],
         auth: {
           token: this.authenticationService.getToken(),
         },
