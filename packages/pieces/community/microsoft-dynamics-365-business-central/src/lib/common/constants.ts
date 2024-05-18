3e0fedc89b8b7dc5abd14d2f0734ae1fb507034d
Kishan Parmar <code.kishanparmar@gmail.com>
2024-05-18 17:06:55 +0530

    feat(business-central): currencies & contacts entity

--- packages/pieces/community/microsoft-dynamics-365-business-central/src/lib/common/constants.ts
+++ packages/pieces/community/microsoft-dynamics-365-business-central/src/lib/common/constants.ts
@@ -1,10 +1,18 @@
 export const ENTITY_DROPDOWN_OPTIONS = [
 	{
-		label: 'Customers',
-		value: 'customers',
-	},
-	{
 		label: 'Bank Accounts',
 		value: 'bankAccounts',
+	},
+	{
+		label: 'Contacts',
+		value: 'contacts',
+	},
+	{
+		label: 'Currencies',
+		value: 'currencies',
+	},
+	{
+		label: 'Customers',
+		value: 'customers',
 	},
 ];
--- packages/pieces/community/microsoft-dynamics-365-business-central/src/lib/common/props.ts
+++ packages/pieces/community/microsoft-dynamics-365-business-central/src/lib/common/props.ts
@@ -149,6 +149,112 @@ export const entityProps = {
 			required: false,
 		}),
 	},
+	contacts: {
+		number: Property.ShortText({
+			displayName: 'Number',
+			required: false,
+		}),
+		displayName: Property.ShortText({
+			displayName: 'Display Name',
+			required: false,
+		}),
+		type: Property.StaticDropdown({
+			displayName: 'Type',
+			required: false,
+			options: {
+				disabled: false,
+				options: [
+					{
+						label: 'Person',
+						value: 'Person',
+					},
+					{ label: 'Company', value: 'Company' },
+				],
+			},
+		}),
+		jobTitle: Property.ShortText({
+			displayName: 'Job Title',
+			required: false,
+		}),
+		companyNumber: Property.ShortText({
+			displayName: 'Company Number',
+			required: false,
+		}),
+		companyName: Property.ShortText({
+			displayName: 'Company Name',
+			required: false,
+		}),
+		addressLine1: Property.LongText({
+			displayName: 'Address Line 1',
+			required: false,
+		}),
+		addressLine2: Property.LongText({
+			displayName: 'Address Line 2',
+			required: false,
+		}),
+		city: Property.ShortText({
+			displayName: 'City',
+			required: false,
+		}),
+		state: Property.ShortText({
+			displayName: 'State',
+			required: false,
+		}),
+		country: Property.ShortText({
+			displayName: 'Country',
+			required: false,
+		}),
+		postalCode: Property.ShortText({
+			displayName: 'Postal Code',
+			required: false,
+		}),
+		phoneNumber: Property.ShortText({
+			displayName: 'Phone Number',
+			required: false,
+		}),
+		mobilePhoneNumber: Property.ShortText({
+			displayName: 'Mobile Number',
+			required: false,
+		}),
+		email: Property.ShortText({
+			displayName: 'Email',
+			required: false,
+		}),
+		website: Property.ShortText({
+			displayName: 'Website',
+			required: false,
+		}),
+		privacyBlocked: Property.Checkbox({
+			displayName: 'Privacy Blocked?',
+			description: 'Specifies whether the privacy of the contact is blocked.',
+			required: false,
+		}),
+		taxRegistrationNumber: Property.ShortText({
+			displayName: 'Tax Registration Number',
+			required: false,
+		}),
+	},
+	currencies: {
+		displayName: Property.ShortText({
+			displayName: 'Display Name',
+			required: false,
+		}),
+		code: Property.ShortText({
+			displayName: 'Code',
+			description: 'The code of the currency.',
+			required: false,
+		}),
+		amountDecimalPlaces: Property.ShortText({
+			displayName: 'Amount Decimal Places',
+			description:
+				'	Specifies the number of decimal places the system will display on amounts for this currency.',
+			required: false,
+		}),
+		amountRoundingPrecision: Property.Number({
+			displayName: 'Amount Rounding Precision',
+			required: false,
+		}),
+	},
 };
 
 export const commonProps = {
@@ -210,6 +316,12 @@ export const commonProps = {
 					break;
 				case 'bankAccounts':
 					fields = entityProps.bankAccounts;
+					break;
+				case 'customers':
+					fields = entityProps.customers;
+					break;
+				case 'currencies':
+					fields = entityProps.currencies;
 					break;
 				default:
 					break;
