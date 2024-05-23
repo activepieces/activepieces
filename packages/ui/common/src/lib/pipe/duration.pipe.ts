9a0e4418a78f765842c9f84fa16e1e942101351a
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-05-23 13:44:38 +0300

    chore: make run duration show up in side bar title

--- packages/ui/common/src/lib/pipe/duration.pipe.ts
+++ packages/ui/common/src/lib/pipe/duration.pipe.ts
@@ -6,21 +6,21 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class DurationPipe implements PipeTransform {
-  transform(from: string | Date, to: string | Date): string {
+  transform(from: string | Date, to: string | Date, short?: boolean): string {
     try {
       const fromTime = new Date(from).getTime();
       const toTime = new Date(to).getTime();
       const diff = toTime - fromTime;
       const diffInSeconds = Math.ceil(diff / 1000);
       if (diffInSeconds < 60) {
-        return $localize`${diffInSeconds} seconds`;
+        return $localize`${diffInSeconds}${short ? 's' : ' seconds'}`;
       }
       const diffInMinutes = Math.ceil(diffInSeconds / 60);
       if (diffInMinutes < 60) {
-        return $localize`${diffInMinutes} minutes`;
+        return $localize`${diffInMinutes}${short ? 'm' : ' minutes'}`;
       }
       const diffInHours = Math.ceil(diffInMinutes / 60);
-      return $localize`${diffInHours} hours`;
+      return $localize`${diffInHours}${short ? 'h' : ' hours'}`;
     } catch (ex) {
       console.error(ex);
       return '';
