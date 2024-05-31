df0075d0debc47fa0d48edcbb20d26512cb0d8dc
Islam Abdelfattah <islam_9t9@yahoo.com>
2024-05-31 01:59:42 +0000

    feat: default alert for existing projects

--- packages/server/api/src/app/database/migration/postgres/1716989780835-AddAlertsEntityPostgres.ts
+++ packages/server/api/src/app/database/migration/postgres/1716989780835-AddAlertsEntityPostgres.ts
@@ -1,3 +1,5 @@
+import { logger } from '@activepieces/server-shared'
+import { apId } from '@activepieces/shared'
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class AddAlertsEntityPostgres1716989780835 implements MigrationInterface {
@@ -16,12 +18,33 @@ export class AddAlertsEntityPostgres1716989780835 implements MigrationInterface 
         `)
         await queryRunner.query(`
             ALTER TABLE "platform"
-            ADD "alertsEnabled" boolean NOT NULL
+            ADD "alertsEnabled" boolean
         `)
         await queryRunner.query(`
             UPDATE "platform"
             SET "alertsEnabled" = false
         `)
+        await queryRunner.query(`
+            ALTER TABLE "platform"
+            ALTER COLUMN "alertsEnabled" SET NOT NULL
+        `)
+        const projects = await queryRunner.query(`
+            SELECT p."id" AS "projectId", u."email" AS "receiver"
+            FROM "project" p
+            INNER JOIN "user" u ON u."id" = p."ownerId"
+        `)
+
+        let countAlerts = 0;
+        for (const project of projects) {
+            const alertId = apId();
+            await queryRunner.query(
+                'INSERT INTO alert (id, created, updated, projectId, channel, receiver) VALUES ($1, NOW(), NOW(), $2, "EMAIL", $3)',
+                [alertId, project.projectId, project.receiver],
+            );
+            countAlerts++;
+        }
+
+        logger.info(`CreateAlerts1680986182074 Migrated ${countAlerts} alerts`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
