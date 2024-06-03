1cccbe732a9532b2d1f430fe44f6a9d3e836eb1c
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-06-03 11:34:00 +0300

    chore: add check email note for trial key

--- packages/ui/feature-dashboard/src/lib/components/request-trial/request-trial.component.ts
+++ packages/ui/feature-dashboard/src/lib/components/request-trial/request-trial.component.ts
@@ -1,14 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UiCommonModule } from '@activepieces/ui/common';
+import { UiCommonModule, fadeIn400ms } from '@activepieces/ui/common';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable, delay, of, tap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-request-trial-component',
   standalone: true,
   imports: [CommonModule, UiCommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeIn400ms],
   template: `
     <div
       class=" ap-w-full ap-h-full ap-bg-[#F5F3FF] ap-text-black ap-py-5 lg:ap-py-5 ap-overflow-scroll thin-scrollbars ap-relative"
@@ -25,8 +28,11 @@ import { MatDialog } from '@angular/material/dialog';
         <div
           class="ap-flex  ap-gap-4 lg:ap-gap-8 ap-items-center ap-justify-center ap-flex-wrap lg:ap-flex-nowrap "
         >
-          <div class="ap-flex ap-flex-col ap-gap-4 lg:ap-w-[600px] ">
+          <div
+            class="ap-flex ap-flex-col ap-gap-4 lg:ap-w-[478px] ap-flex-grow lg:ap-flex-grow-0 "
+          >
             <svg-icon src="assets/img/custom/logo/full-logo.svg"></svg-icon>
+            @if(!showCheckYourEmailNote) {
             <div
               class="ap-typography-headline-5 lg:ap-typography-headline-3 !ap-font-semibold"
             >
@@ -37,35 +43,35 @@ import { MatDialog } from '@angular/material/dialog';
               sales, operations, HR, finance and IT teams
             </div>
             <form
-              [formGroup]="form"
-              (submit)="submit()"
+              [formGroup]="sendEmailForm"
+              (submit)="submitEmail()"
               class="ap-flex ap-gap-4 ap-flex-wrap lg:ap-flex-nowrap"
             >
-              <mat-form-field class="ap-w-full" subscriptSizing="dynamic">
+              <mat-form-field class="ap-flex-grow" subscriptSizing="dynamic">
                 <mat-label>Email</mat-label>
                 <input
                   type="email"
-                  [formControl]="form.controls.email"
+                  [formControl]="sendEmailForm.controls.email"
                   matInput
                   placeholder="john@doe.com"
                 />
-                @if(form.controls.email.invalid) {
+                @if(sendEmailForm.controls.email.invalid) {
                 <mat-error>
-                  @if( form.controls.email.getError('email')) {
+                  @if( sendEmailForm.controls.email.getError('email')) {
                   <ng-container i18n>Email is invalid </ng-container>
                   } @else {
                   <ng-container i18n> Email is required </ng-container> }
                 </mat-error>
                 }
               </mat-form-field>
-              <div class="ap-min-w-[162px] ap-w-full">
+              <div class="ap-min-w-[162px] ap-w-full lg:ap-w-auto ">
                 <ap-button
                   type="submit"
                   [loading]="loading"
-                  (buttonClicked)="submit()"
+                  (buttonClicked)="submitEmail()"
                   btnColor="primary"
                   btnSize="large"
-                  class="ap-w-full lg:ap-w-auto"
+                  class="ap-w-full"
                   [fullWidthOfContainer]="true"
                   i18n
                 >
@@ -73,7 +79,54 @@ import { MatDialog } from '@angular/material/dialog';
                 </ap-button>
               </div>
             </form>
+            } @else {
 
+            <div
+              class="ap-flex ap-justify-center ap-items-center ap-my-2"
+              @fadeIn
+            >
+              <img
+                class="ap-m-auto"
+                src="/assets/img/custom/auth/mail_sent.png"
+              />
+            </div>
+            <div class="ap-text-center ap-typography-body-1 ap-mb-2" i18n>
+              Please check your email for the trial key, and paste it here to
+              start 🚀
+            </div>
+            <form
+              [formGroup]="activateTrialKeyForm"
+              (submit)="submitTrialKey()"
+              class="ap-flex ap-gap-4 ap-flex-wrap lg:ap-flex-nowrap"
+            >
+              <mat-form-field class="ap-flex-grow" subscriptSizing="dynamic">
+                <mat-label>Key</mat-label>
+                <input
+                  type="text"
+                  [formControl]="activateTrialKeyForm.controls.key"
+                  matInput
+                />
+                @if(activateTrialKeyForm.controls.key.invalid) {
+                <mat-error>Key is required </mat-error>
+                }
+              </mat-form-field>
+
+              <div class="ap-min-w-[162px] ap-w-full lg:ap-w-auto">
+                <ap-button
+                  type="submit"
+                  [loading]="loading"
+                  (buttonClicked)="submitTrialKey()"
+                  btnColor="primary"
+                  btnSize="large"
+                  class="ap-w-full"
+                  [fullWidthOfContainer]="true"
+                  i18n
+                >
+                  Activate Trial
+                </ap-button>
+              </div>
+            </form>
+            }
             <div class="ap-flex ap-gap-2 ap-text-description ap-items-center">
               <svg-icon
                 src="assets/img/custom/info.svg"
@@ -117,27 +170,65 @@ import { MatDialog } from '@angular/material/dialog';
         </div>
       </div>
     </div>
+    <img class="ap-hidden" src="/assets/img/custom/auth/mail_sent.png" />
+    @if(sendTrialKey$ | async) {} @if(activateTrialKey$ | async){}
   `,
 })
 export class RequestTrialComponent {
+  showCheckYourEmailNote = false;
+  sendTrialKey$?: Observable<null>;
+  activateTrialKey$?: Observable<null>;
   logos = [
     'https://www.activepieces.com/logos/posthog.svg',
     'https://www.activepieces.com/logos/roblox.svg',
     'https://www.activepieces.com/logos/clickup.svg',
     'https://www.activepieces.com/logos/plivo.svg',
   ];
-  form = this.fb.group({
-    email: ['', Validators.required],
+  sendEmailForm = this.fb.group({
+    email: this.fb.control('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
   });
-  constructor(private fb: FormBuilder, private matDialog: MatDialog) {}
+  activateTrialKeyForm = this.fb.group({
+    key: this.fb.control('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
+  constructor(
+    private fb: FormBuilder,
+    private matDialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {}
   loading = false;
-  submit() {
-    if (this.form.valid) {
+  submitEmail() {
+    //TODO: Actual implelentation
+    if (this.sendEmailForm.valid) {
       this.loading = true;
-      setTimeout(() => {
-        this.loading = false;
-        this.form.reset();
-      }, 2000);
+      this.sendTrialKey$ = of(null).pipe(
+        delay(1000),
+        tap(() => {
+          this.loading = false;
+          this.showCheckYourEmailNote = true;
+        })
+      );
+    }
+  }
+
+  submitTrialKey() {
+    //TODO: Actual implelentation
+    if (this.activateTrialKeyForm.valid) {
+      this.loading = true;
+      this.activateTrialKey$ = of(null).pipe(
+        delay(1000),
+        tap(() => {
+          this.loading = false;
+          this.showCheckYourEmailNote = true;
+          this.close();
+          this.snackBar.open('Trial activated successfully');
+        })
+      );
     }
   }
   close() {
