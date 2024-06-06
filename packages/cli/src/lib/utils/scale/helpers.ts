2f530f92ef283a71d0007b4f08804ac67ce48360
saifmohamed-dev <155442820+saifmohamed-dev@users.noreply.github.com>
2024-06-06 15:26:09 +0300

    feat: multi protocol support

--- packages/cli/src/lib/utils/scale/helpers.ts
+++ packages/cli/src/lib/utils/scale/helpers.ts
@@ -1,24 +1,30 @@
 import http from 'http';
+import https from 'https';
+import { URL } from 'url';
 
 export const getJsonFromUrl = (url: string): Promise<any> => {
   return new Promise((resolve, reject) => {
-    http.get(url, (res) => {
-      let data = '';
+    const parsedUrl = new URL(url);
+    const protocol = parsedUrl.protocol === 'https:' ? https : http;
+    protocol
+      .get(url, (res) => {
+        let data = '';
 
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
 
-      res.on('end', () => {
-        try {
-          resolve(JSON.parse(data));
-        } catch (e) {
-          reject(e);
-        }
+        res.on('end', () => {
+          try {
+            resolve(JSON.parse(data));
+          } catch (e) {
+            reject(e);
+          }
+        });
+      })
+      .on('error', (err) => {
+        reject(err);
       });
-    }).on('error', (err) => {
-      reject(err);
-    });
   });
 };
 
