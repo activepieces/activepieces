d4068b2853465ae91e56237590b0ee22ad5516da
Patrik Dufresne <patrik@ikus-soft.com>
2024-06-06 21:28:41 +0000

    feat: add custom get records for Odoo piece

---
+++ packages/pieces/community/odoo/src/lib/actions/get-records.ts
@@ -0,0 +1,62 @@
+import { createAction, Property, Validators } from "@activepieces/pieces-framework";
+import Odoo from "../../commom/index";
+import { odooAuth } from "../..";
+
+export default createAction({
+    name: 'get_records',
+    auth: odooAuth,
+    displayName: 'Custom Search and read records',
+    description: 'Records can be listed and filtered',
+    props: {
+        // Properties to ask from the user, in this ask we will take number of
+        model: Property.ShortText({
+            displayName: 'Model',
+            description: "Model name. e.g.: res.partner",
+            required: true,
+            defaultValue: 'res.partner',
+        }),
+        domain: Property.Json({
+            displayName: 'Search domains',
+            required: false,
+            description: 'A domain is a list of criteria, each criterion being a triple of (field_name, operator, value). See https://www.odoo.com/documentation/17.0/developer/reference/backend/orm.html#reference-orm-domains for details.',
+            defaultValue: [
+                ["is_company", "=", true],
+            ],
+        }),
+        fields: Property.Array({
+            displayName: 'Fields',
+            description: 'Returns the requested fields of the records. When undefined, returns all fields.',
+            required: false,
+          }),
+        offset: Property.Number({
+            displayName: 'Offset',
+            required: false,
+            validators: [Validators.integer],
+        }),
+        limit: Property.Number({
+            displayName: 'Limit',
+            required: false,
+            validators: [Validators.integer, Validators.minValue(1)],
+        })
+    },
+    async run(context) {
+        const odoo = new Odoo({
+            url: context.auth.base_url,
+            port: 443,
+            db: context.auth.database,
+            username: context.auth.username,
+            password: context.auth.api_key,
+        })
+
+        try {
+            await odoo.connect();
+            const domainArray = context.propsValue.domain
+            ? (context.propsValue.domain as unknown as any[])
+            : [];
+            return await odoo.getRecords({model: context.propsValue.model, domain: domainArray, fields: context.propsValue.fields, offset: context.propsValue.offset, limit: context.propsValue.limit})
+        } catch (err) {
+            return err
+        }
+    }
+});
+
