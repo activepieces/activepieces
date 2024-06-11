5f6293b70b1e859deef21adcd4342d3a09af5614
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-06-11 12:21:42 +0000

    fix: allow admin to access project owner

--- packages/server/api/src/app/ee/project-members/project-member.service.ts
+++ packages/server/api/src/app/ee/project-members/project-member.service.ts
@@ -100,6 +100,9 @@ export const projectMemberService = {
         const user = await userService.getOneOrFail({
             id: userId,
         })
+        if (user.id === project.ownerId) {
+            return ProjectMemberRole.ADMIN
+        }
         if (project.platformId === user.platformId && user.platformRole === PlatformRole.ADMIN) {
             return ProjectMemberRole.ADMIN
         }
