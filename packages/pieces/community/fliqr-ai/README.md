f77ed6228f4477af13223ea92dc6950dd725e23f
Aditya Rathore <34496554+drona2938@users.noreply.github.com>
2024-06-16 13:25:13 +0000

    Pending changes exported from your codespace

---
+++ packages/pieces/community/fliqr-ai/README.md
@@ -0,0 +1,7 @@
+# pieces-fliqr-ai
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build pieces-fliqr-ai` to build the library.
--- packages/ui/common/src/lib/environments/environment.ts
+++ packages/ui/common/src/lib/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/v1',
+  apiUrl: 'https://legendary-garbanzo-q9995r99p4pfx6px-3000.app.github.dev/v1',
   jwtTokenName: 'token',
   redirectUrl: 'http://localhost:4200/redirect',
   userPropertyNameInLocalStorage: 'currentUser',
