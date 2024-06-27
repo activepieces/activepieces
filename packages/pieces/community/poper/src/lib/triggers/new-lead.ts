e121d0b1fa1a575b3fc47660510180d184dd93a1
Kishan Parmar <code.kishanparmar@gmail.com>
2024-06-27 13:57:44 +0530

    chore(poper): update webhook instructions

--- packages/pieces/community/poper/src/lib/triggers/new-lead.ts
+++ packages/pieces/community/poper/src/lib/triggers/new-lead.ts
@@ -1,40 +1,44 @@
+import {
+  createTrigger,
+  TriggerStrategy,
+  Property,
+} from '@activepieces/pieces-framework';
 
-import { createTrigger, TriggerStrategy, Property } from '@activepieces/pieces-framework';
+const message = `
+1. Log in to your [Poper Account](https://app.poper.ai/).
+2. Click on the popup for which you want to set up a trigger.
+3. On the left-side menu, click on Integrations and search for **Webhook**.
+4. Enter an appropriate webhook name and paste the following URLs:
 
-const message = `**Live Webhook:**
-\`\`\`text
-{{webhookUrl}}
-\`\`\`
-<br>
-Use this webhook to trigger a flow when a new lead is obtained from a popup.
+  **Test URL (only generate sample data)**:
+  \`\`\`text
+  {{webhookUrl}}/test
+  \`\`\`
 
-**Test Webhook:**
-\`\`\`text
-{{webhookUrl}}/test
-\`\`\`
-<br>
-Use this webhook to test this trigger.
+  **Trigger Flow URL**:
+  \`\`\`text
+  {{webhookUrl}}
+  \`\`\`
 `;
 
 export const newLead = createTrigger({
-auth: undefined,
-name: 'newLead',
-displayName: 'New Lead',
-description: 'Triggers when a new lead is obtained from popup',
-props: {
-  markdown: Property.MarkDown({
-    value: message,
-  }),
-},
-sampleData: {},
-type: TriggerStrategy.WEBHOOK,
-async onEnable() {
-  // ignore
-},
-async onDisable() {
-  // ignore
-},
-async run(context) {
-  return [context.payload.body];
-},
+  name: 'newLead',
+  displayName: 'New Lead',
+  description: 'Triggers when a new lead is obtained from popup.',
+  props: {
+    markdown: Property.MarkDown({
+      value: message,
+    }),
+  },
+  sampleData: {},
+  type: TriggerStrategy.WEBHOOK,
+  async onEnable() {
+    // ignore
+  },
+  async onDisable() {
+    // ignore
+  },
+  async run(context) {
+    return [context.payload.body];
+  },
 });
