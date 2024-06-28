4c911f40cf974ed27551ae71147c52758cfc838d
obenazouz-wedof <144689457+obenazouz-wedof@users.noreply.github.com>
2024-06-28 11:54:50 +0200

    Update abort-certification-folder.ts

--- packages/pieces/community/wedof/src/lib/actions/certification-folders/abort-certification-folder.ts
+++ packages/pieces/community/wedof/src/lib/actions/certification-folders/abort-certification-folder.ts
@@ -25,7 +25,6 @@ export const abortCertificationFolder = createAction({
     const message = {
       comment: context.propsValue.comment,
     };
-
     return (
       await httpClient.sendRequest({
         method: HttpMethod.POST,
