5ac8aaece57a68070603e993bbf3d1413954bc49
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-07-02 23:02:43 +0000

    fix: hold connections when there is no job

--- packages/server/api/src/app/flow-worker/redis/redis-consumer.ts
+++ packages/server/api/src/app/flow-worker/redis/redis-consumer.ts
@@ -1,4 +1,4 @@
-import { flowTimeoutSandbox, JobStatus, QueueName, system, SystemProp, triggerTimeoutSandbox } from '@activepieces/server-shared'
+import { exceptionHandler, flowTimeoutSandbox, JobStatus, memoryLock, QueueName, system, SystemProp, triggerTimeoutSandbox } from '@activepieces/server-shared'
 import { apId, assertNotNullOrUndefined, isNil } from '@activepieces/shared'
 import { Job, Worker } from 'bullmq'
 import dayjs from 'dayjs'
@@ -11,15 +11,33 @@ const WORKER_CONCURRENCY = system.getNumber(SystemProp.FLOW_WORKER_CONCURRENCY) 
 
 export const redisConsumer: ConsumerManager = {
     async poll(jobType) {
-        const worker = consumers[jobType]
-        assertNotNullOrUndefined(worker, 'Queue not found')
-        const job = await worker.getNextJob(serverId)
-        if (isNil(job)) {
-            return null
+        let lock
+        try {
+            lock = await memoryLock.acquire(`poll-${jobType}`, 5000)
+            const worker = consumers[jobType]
+            assertNotNullOrUndefined(worker, 'Queue not found')
+            // The worker.getNextJob() method holds the connection until a job is available, but it can only be called once at a time.
+            // To handle multiple workers, we are storing them in memory while waiting for a job to become available.
+            const job = await worker.getNextJob(serverId)
+            if (isNil(job)) {
+                return null
+            }
+            return {
+                id: job.id!,
+                data: job.data,
+            }
         }
-        return {
-            id: job.id!,
-            data: job.data,
+        catch (e) {
+            if (memoryLock.isTimeoutError(e)) {
+                return null
+            }
+            exceptionHandler.handle(e)
+            throw e
+        }
+        finally {
+            if (lock) {
+                await lock.release()
+            }
         }
     },
     async update({ queueName, jobId, status, message }): Promise<void> {
@@ -45,15 +63,11 @@ export const redisConsumer: ConsumerManager = {
                 connection: createRedisClient(),
                 lockDuration,
                 maxStalledCount: 5,
-                drainDelay: 5,
                 stalledInterval: 30000,
             })
         }
         await Promise.all(Object.values(consumers).map((consumer) => consumer.waitUntilReady()))
-        for (const queueName of Object.values(QueueName)) {
-            const worker = consumers[queueName]
-            await worker.startStalledCheckTimer()
-        }
+
     },
     async close(): Promise<void> {
         if (WORKER_CONCURRENCY === 0) {
@@ -67,10 +81,10 @@ export const redisConsumer: ConsumerManager = {
 function getLockDurationInMs(queueName: QueueName) {
     switch (queueName) {
         case QueueName.WEBHOOK:
-            return  dayjs.duration(triggerTimeoutSandbox, 'seconds').add(5, 'seconds').asMilliseconds()
+            return dayjs.duration(triggerTimeoutSandbox, 'seconds').add(5, 'seconds').asMilliseconds()
         case QueueName.ONE_TIME:
-            return  dayjs.duration(flowTimeoutSandbox, 'seconds').add(5, 'seconds').asMilliseconds()
+            return dayjs.duration(flowTimeoutSandbox, 'seconds').add(5, 'seconds').asMilliseconds()
         case QueueName.SCHEDULED:
-            return  dayjs.duration(triggerTimeoutSandbox, 'seconds').add(5, 'seconds').asMilliseconds()
+            return dayjs.duration(triggerTimeoutSandbox, 'seconds').add(5, 'seconds').asMilliseconds()
     }
 }
