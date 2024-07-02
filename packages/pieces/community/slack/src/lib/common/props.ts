8e3e716d33389367cd98beca65699ca68f28652d
Kishan Parmar <code.kishanparmar@gmail.com>
2024-07-02 15:29:53 +0530

    feat(slack): update channel dropdown limit

--- packages/pieces/community/slack/src/lib/common/props.ts
+++ packages/pieces/community/slack/src/lib/common/props.ts
@@ -12,48 +12,71 @@ export const slackInfo = Property.MarkDown({
 	  2. Click on Add apps to this channel.
 	  3. Search for and add the bot.`,
 });
-export const slackChannel = <R extends boolean>(required: R) => Property.Dropdown<string, R>({
-  displayName: 'Channel',
-  description: 'Channel, private group, or IM channel to send message to.',
-  required,
-  refreshers: [],
-  async options({ auth }) {
-    if (!auth) {
-      return {
-        disabled: true,
-        placeholder: 'connect slack account',
-        options: [],
-      };
-    }
-    const authentication = auth as OAuth2PropertyValue;
-    const accessToken = authentication['access_token'];
-    const client = new WebClient(accessToken);
-    const channels: { label: string; value: string }[] = [];
-    for await (const page of client.paginate('conversations.list', {
-      types: 'public_channel,private_channel',
-      exclude_archived: true,
-      // Only limits page size, not total number of results.
-      // We use a high number to avoid rate limiting and because Slack filters archived channels AFTER computing each page
-      // i.e. you can get many pages with only 10-50 results even though you're asking for 1000 per page
-      limit: 1000,
-    })) {
-      const response = page as ConversationsListResponse;
-      if (response.channels) {
-        channels.push(
-          ...response.channels.map((channel) => {
-            return { label: channel.name || '', value: channel.id || '' };
-          })
-        );
+export const slackChannel = <R extends boolean>(required: R) =>
+  Property.Dropdown<string, R>({
+    displayName: 'Channel',
+    description: `If you can't find the desired channel, you can obtain the Channel ID by right-clicking on the channel in Slack, selecting **View Channel Details**, and copying the Channel ID. Then, press **(X)** to enter the Channel ID in the dropdown and paste it.`,
+    required,
+    refreshers: [],
+    async options({ auth }) {
+      if (!auth) {
+        return {
+          disabled: true,
+          placeholder: 'connect slack account',
+          options: [],
+        };
       }
-    }
+      const authentication = auth as OAuth2PropertyValue;
+      const accessToken = authentication['access_token'];
+      const client = new WebClient(accessToken);
+      const channels: { label: string; value: string }[] = [];
+      const CHANNELS_LIMIT = 2000;
 
-    return {
-      disabled: false,
-      placeholder: 'Select channel',
-      options: channels,
-    };
-  },
-});
+      let cursor;
+      do {
+        const response = await client.conversations.list({
+          types: 'public_channel,private_channel',
+          exclude_archived: true,
+          limit: 1000,
+          cursor,
+        });
+
+        if (response.channels) {
+          channels.push(
+            ...response.channels.map((channel) => {
+              return { label: channel.name || '', value: channel.id || '' };
+            })
+          );
+        }
+
+        cursor = response.response_metadata?.next_cursor;
+      } while (cursor && channels.length < CHANNELS_LIMIT);
+
+      // for await (const page of client.paginate('conversations.list', {
+      //   types: 'public_channel,private_channel',
+      //   exclude_archived: true,
+      //   // Only limits page size, not total number of results.
+      //   // We use a high number to avoid rate limiting and because Slack filters archived channels AFTER computing each page
+      //   // i.e. you can get many pages with only 10-50 results even though you're asking for 1000 per page
+      //   limit: 1000,
+      // })) {
+      //   const response = page as ConversationsListResponse;
+      //   if (response.channels) {
+      //     channels.push(
+      //       ...response.channels.map((channel) => {
+      //         return { label: channel.name || '', value: channel.id || '' };
+      //       })
+      //     );
+      //   }
+      // }
+
+      return {
+        disabled: false,
+        placeholder: 'Select channel',
+        options: channels,
+      };
+    },
+  });
 
 export const username = Property.ShortText({
   displayName: 'Username',
