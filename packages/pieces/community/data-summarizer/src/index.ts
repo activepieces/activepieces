c91d32f9aa2b317eaba101e45053196365a24d1b
Ali Tahboub <ali@activepieces.com>
2024-07-03 08:27:38 +0000

    chore: added actions to index.ts in data-summarizer

--- packages/pieces/community/data-summarizer/src/index.ts
+++ packages/pieces/community/data-summarizer/src/index.ts
@@ -1,13 +1,15 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { calculateAverage } from './lib/actions/calculate-average';
+import { calculateSum } from './lib/actions/calculate-sum';
+import { countUniques } from './lib/actions/count-uniques';
+import { getMinMax } from './lib/actions/get-min-max';
 
-    import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-    
-    export const dataSummarizer = createPiece({
-      displayName: "Data-summarizer",
-      auth: PieceAuth.None(),
-      minimumSupportedRelease: '0.20.0',
-      logoUrl: "https://cdn.activepieces.com/pieces/data-summarizer.png",
-      authors: [],
-      actions: [],
-      triggers: [],
-    });
-    
+export const dataSummarizer = createPiece({
+  displayName: 'Data Summarizer',
+  auth: PieceAuth.None(),
+  minimumSupportedRelease: '0.20.0',
+  logoUrl: 'https://cdn.activepieces.com/pieces/data-summarizer.png',
+  authors: ['tahboubali'],
+  actions: [calculateAverage, calculateSum, countUniques, getMinMax],
+  triggers: []
+});
