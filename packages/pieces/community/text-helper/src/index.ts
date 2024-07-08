567bdb28094627eb7ff655b862d071ee3501efd4
Olivier Sambourg <AdamSelene@users.noreply.github.com>
2024-07-09 00:57:33 +0200

    feat(text-helper): add action to strip html tags

--- packages/pieces/community/text-helper/src/index.ts
+++ packages/pieces/community/text-helper/src/index.ts
@@ -6,14 +6,30 @@ import { htmlToMarkdown } from './lib/actions/html-to-markdown';
 import { markdownToHTML } from './lib/actions/markdown-to-html';
 import { replace } from './lib/actions/replace';
 import { split } from './lib/actions/split';
+import { stripHtmlContent } from './lib/actions/strip-html';
 
 export const textHelper = createPiece({
   displayName: 'Text Helper',
   description: 'Tools for text processing',
   auth: PieceAuth.None(),
   logoUrl: 'https://cdn.activepieces.com/pieces/text-helper.svg',
-  authors: ["joeworkman","kishanprmr","MoShizzle","AbdulTheActivePiecer","abuaboud"],
+  authors: [
+    'joeworkman',
+    'kishanprmr',
+    'MoShizzle',
+    'AbdulTheActivePiecer',
+    'abuaboud',
+    'AdamSelene',
+  ],
   categories: [PieceCategory.CORE],
-  actions: [concat, replace, split, find, markdownToHTML, htmlToMarkdown],
+  actions: [
+    concat,
+    replace,
+    split,
+    find,
+    markdownToHTML,
+    htmlToMarkdown,
+    stripHtmlContent,
+  ],
   triggers: [],
 });
