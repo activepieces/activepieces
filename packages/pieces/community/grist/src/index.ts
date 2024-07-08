2c7c50f09b9a35b4b2f7d01c69f47f078b0a791e
Kishan Parmar <code.kishanparmar@gmail.com>
2024-07-09 00:33:28 +0530

    chore(grist): remove console messages

--- packages/pieces/community/grist/src/index.ts
+++ packages/pieces/community/grist/src/index.ts
@@ -1,8 +1,8 @@
 import {
-	createPiece,
-	PieceAuth,
-	PiecePropValueSchema,
-	Property,
+  createPiece,
+  PieceAuth,
+  PiecePropValueSchema,
+  Property,
 } from '@activepieces/pieces-framework';
 import { gristCreateRecordAction } from './lib/actions/create-record.action';
 import { gristUpdateRecordAction } from './lib/actions/update-record.action';
@@ -15,63 +15,70 @@ import { GristAPIClient } from './lib/common/helpers';
 import { PieceCategory } from '@activepieces/shared';
 
 export const gristAuth = PieceAuth.CustomAuth({
-	required: true,
-	description: `
+  required: true,
+  description: `
 	Log in to your Grist account.avigate to the account menu at the top right, and select **Profile Settings** to manage or create your API Key.
 	In the **Domain URL** field, enter the domain URL of your Grist instance.For example,if you have team site it will be "https://team.getgist.com".`,
-	props: {
-		apiKey: PieceAuth.SecretText({
-			displayName: 'API Key',
-			required: true,
-		}),
-		domain: Property.ShortText({
-			displayName: 'Domain URL',
-			required: true,
-			defaultValue: 'https://docs.getgrist.com',
-		}),
-	},
-	validate: async ({ auth }) => {
-		try {
-			const authValue = auth as PiecePropValueSchema<typeof gristAuth>;
+  props: {
+    apiKey: PieceAuth.SecretText({
+      displayName: 'API Key',
+      required: true,
+    }),
+    domain: Property.ShortText({
+      displayName: 'Domain URL',
+      required: true,
+      defaultValue: 'https://docs.getgrist.com',
+    }),
+  },
+  validate: async ({ auth }) => {
+    try {
+      const authValue = auth as PiecePropValueSchema<typeof gristAuth>;
 
-			const client = new GristAPIClient({ domainUrl: authValue.domain, apiKey: authValue.apiKey });
+      const client = new GristAPIClient({
+        domainUrl: authValue.domain,
+        apiKey: authValue.apiKey,
+      });
 
-			await client.listWorkspaces('current');
+      await client.listWorkspaces('current');
 
-			return {
-				valid: true,
-			};
-		} catch (error) {
-			return {
-				valid: false,
-				error: 'Please provide valid API key and domain URL.',
-			};
-		}
-	},
+      return {
+        valid: true,
+      };
+    } catch (error) {
+      return {
+        valid: false,
+        error: 'Please provide valid API key and domain URL.',
+      };
+    }
+  },
 });
 
 export const grist = createPiece({
-	displayName: 'Grist',
-	auth: gristAuth,
-	minimumSupportedRelease: '0.20.0',
-	logoUrl: 'https://cdn.activepieces.com/pieces/grist.png',
-	description: 'open source spreadsheet',
-	categories: [PieceCategory.PRODUCTIVITY],
-	authors: ['kishanprmr'],
-	actions: [
-		gristCreateRecordAction,
-		gristSearchRecordAction,
-		gristUpdateRecordAction,
-		gristUploadAttachmentsToDocumnetAction,
-		createCustomApiCallAction({
-			auth: gristAuth,
-			baseUrl: (auth) => {
-				return `${(auth as PiecePropValueSchema<typeof gristAuth>).domain}/api/`;
-			},
-			authMapping: async (auth) => ({
-				Authorization: `Bearer ${(auth as PiecePropValueSchema<typeof gristAuth>).apiKey}`,
-			}),
-		}),
-	],
-	triggers: [gristNewRecordTrigger, gristUpdatedRecordTrigger],
+  displayName: 'Grist',
+  auth: gristAuth,
+  minimumSupportedRelease: '0.20.0',
+  logoUrl: 'https://cdn.activepieces.com/pieces/grist.png',
+  description: 'open source spreadsheet',
+  categories: [PieceCategory.PRODUCTIVITY],
+  authors: ['kishanprmr'],
+  actions: [
+    gristCreateRecordAction,
+    gristSearchRecordAction,
+    gristUpdateRecordAction,
+    gristUploadAttachmentsToDocumnetAction,
+    createCustomApiCallAction({
+      auth: gristAuth,
+      baseUrl: (auth) => {
+        return `${
+          (auth as PiecePropValueSchema<typeof gristAuth>).domain
+        }/api/`;
+      },
+      authMapping: async (auth) => ({
+        Authorization: `Bearer ${
+          (auth as PiecePropValueSchema<typeof gristAuth>).apiKey
+        }`,
+      }),
+    }),
+  ],
+  triggers: [gristNewRecordTrigger, gristUpdatedRecordTrigger],
 });
--- packages/pieces/community/grist/src/lib/common/types.ts
+++ packages/pieces/community/grist/src/lib/common/types.ts
@@ -1,93 +1,93 @@
 export type GristAPIClientOptions = {
-	domainUrl: string;
-	apiKey: string;
+  domainUrl: string;
+  apiKey: string;
 };
 
 export type GristOrganizationResponse = {
-	name: string;
-	id: string;
-	createdAt: string;
-	updatedAt: string;
+  name: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export type GristDocumentResponse = {
-	name: string;
-	id: string;
-	createdAt: string;
-	updatedAt: string;
+  name: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export type GristWorkspaceResponse = {
-	name: string;
-	id: number;
-	createdAt: string;
-	updatedAt: string;
-	docs: Array<GristDocumentResponse>;
+  name: string;
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  docs: Array<GristDocumentResponse>;
 };
 
 export type GristTableResponse = {
-	id: string;
+  id: string;
 };
 
 export type GristTableColumnsResponse = {
-	id: string;
-	fields: {
-		type:
-			| 'Any'
-			| 'Text'
-			| 'Numeric'
-			| 'Int'
-			| 'Bool'
-			| 'Date'
-			| `DateTime:${string}`
-			| `Ref:${string}`
-			| `RefList:${string}`
-			| 'Choice'
-			| 'ChoiceList'
-			| 'Attachments';
-		label: string;
-		widgetOptions: string;
-		isFormula: boolean;
-	};
+  id: string;
+  fields: {
+    type:
+      | 'Any'
+      | 'Text'
+      | 'Numeric'
+      | 'Int'
+      | 'Bool'
+      | 'Date'
+      | `DateTime:${string}`
+      | `Ref:${string}`
+      | `RefList:${string}`
+      | 'Choice'
+      | 'ChoiceList'
+      | 'Attachments';
+    label: string;
+    widgetOptions: string;
+    isFormula: boolean;
+  };
 };
 
 export type GristTableRecordResponse = {
-	id: number;
-	fields: Record<string, any>;
+  id: number;
+  fields: Record<string, any>;
 };
 
 export type GristCreateRecordsRequest = {
-	records: Array<Omit<GristTableRecordResponse, 'id'>>;
+  records: Array<Omit<GristTableRecordResponse, 'id'>>;
 };
 
 export type GristUpdateRecordsRequest = {
-	records: Array<GristTableRecordResponse>;
+  records: Array<GristTableRecordResponse>;
 };
 
 export type GristCreateRecordsResponse = {
-	records: Array<{ id: number }>;
+  records: Array<{ id: number }>;
 };
 
 export type GristListRecordsResponse = {
-	records: Array<GristTableRecordResponse>;
+  records: Array<GristTableRecordResponse>;
 };
 
 export type GristCreateWebhookRequest = {
-	webhooks: Array<{
-		fields: {
-			name?: string;
-			memo?: string;
-			url: string;
-			enabled: boolean;
-			eventTypes: Array<string>;
-			isReadyColumn?: string;
-			tableId: string;
-		};
-	}>;
+  webhooks: Array<{
+    fields: {
+      name?: string;
+      memo?: string;
+      url: string;
+      enabled: boolean;
+      eventTypes: Array<string>;
+      isReadyColumn?: string;
+      tableId: string;
+    };
+  }>;
 };
 
 export type GristCreateWebhookResponse = {
-	webhooks: Array<{ id: number }>;
+  webhooks: Array<{ id: number }>;
 };
 
 export type GristWebhookPayload = Record<string, any>;
