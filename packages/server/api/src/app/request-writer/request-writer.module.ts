85bbe51bb111149fc5909d081cd93c977f758045
saifmohamedsv <saifmohamed.dev@gmail.com>
2024-07-11 15:43:14 +0300

    fix: fix linting

--- packages/server/api/src/app/request-writer/request-writer.module.ts
+++ packages/server/api/src/app/request-writer/request-writer.module.ts
@@ -1,7 +1,7 @@
+import { GenerateCodeRequest, GenerateCodeResponse, WebsocketClientEvent, WebsocketServerEvent } from '@activepieces/shared'
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
 import { websocketService } from '../websockets/websockets.service'
 import { requestWriterService } from './request-writer.service'
-import { GenerateCodeRequest, GenerateCodeResponse, WebsocketClientEvent, WebsocketServerEvent } from '@activepieces/shared'
 
 export const requestWriterModule: FastifyPluginAsyncTypebox = async () => {
     websocketService.addListener(WebsocketServerEvent.GENERATE_HTTP_REQUEST, (socket) => {
