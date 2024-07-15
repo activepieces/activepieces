c8b962792e874d6ed9e4a7f421d28261a80ac59f
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-07-15 14:57:22 +0300

    chore: add getUser from localStorage in authenticationSession

--- packages/react-ui/src/features/authentication/lib/authentication-session.ts
+++ packages/react-ui/src/features/authentication/lib/authentication-session.ts
@@ -1,31 +1,54 @@
 import { projectApi } from "@/features/projects/lib/project-api";
-import { AuthenticationResponse, assertNotNullOrUndefined } from "@activepieces/shared";
+import { AuthenticationResponse } from "@activepieces/shared";
 
+const currentUserKey = 'currentUser';
+const tokenKey = 'token';
+const projectIdKey = 'projectId';
+export const platformCacheKey = 'platform';
 export const authenticationSession = {
     saveResponse(response: AuthenticationResponse) {
-        localStorage.setItem('token', response.token);
-        localStorage.setItem('currentUser', JSON.stringify(response));
-        localStorage.setItem('projectId', response.projectId);
+        localStorage.setItem(tokenKey, response.token);
+        localStorage.setItem(currentUserKey, JSON.stringify(response));
+        localStorage.setItem(projectIdKey, response.projectId);
+
     },
     getToken(): string | null {
-        return localStorage.getItem('token') ?? null;
+        return localStorage.getItem(tokenKey) ?? null;
     },
     getProjectId(): string  {
-        const currentProjectId = localStorage.getItem('projectId')!
+        const currentProjectId = localStorage.getItem(projectIdKey)!
         return currentProjectId
+    },
+    getPlatformId(): string | null {
+        return this.getCurrentUser()?.platformId ?? null;
     },
     async switchToSession(projectId: string) {
         const result = await projectApi.getTokenForProject(projectId);
-        localStorage.setItem('token', result.token);
-        localStorage.setItem('projectId', projectId);
+        localStorage.setItem(tokenKey, result.token);
+        localStorage.setItem(projectIdKey, projectId);
+
     },
     isLoggedIn(): boolean {
         return !!this.getToken();
     },
     LogOut() {
-        localStorage.removeItem('token');
-        localStorage.removeItem('currentUser');
-        localStorage.removeItem('projectId');
+        localStorage.removeItem(tokenKey);
+        localStorage.removeItem(currentUserKey);
+        localStorage.removeItem(projectIdKey);
         window.location.href = '/sign-in';
+    },
+    getCurrentUser(): AuthenticationResponse|null {
+        const user = localStorage.getItem(currentUserKey);
+        if (user) {
+            try {
+                return JSON.parse(user);
+
+            }
+            catch (e) {
+                console.error(e);
+                return null;
+            }
+        }
+        return null;
     }
 }
