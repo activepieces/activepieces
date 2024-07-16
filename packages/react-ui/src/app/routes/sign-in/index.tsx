5fafcf880ca625a9c1add98c7831e84f614a9ae2
Islam Abdelfattah <islam_9t9@yahoo.com>
2024-07-16 14:54:20 +0000

    fix: use theme logos

--- packages/react-ui/src/app/routes/sign-in/index.tsx
+++ packages/react-ui/src/app/routes/sign-in/index.tsx
@@ -1,10 +1,11 @@
 import { AuthFormTemplate } from '@/features/authentication/components/auth-form-template';
+import { theme } from '@/lib/theme';
 
 const SignInPage: React.FC = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-2">
       <img
-        src="https://cdn.activepieces.com/brand/full-logo.png"
+        src={theme.fullLogoUrl}
         alt="activepieces-logo"
         width={205}
         height={205}
