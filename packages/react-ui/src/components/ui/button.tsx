89b4752f788de23ea9fe90fda88aad8d847dcf90
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-07-16 13:10:17 +0300

    chore: adjust default styling for input and button

--- packages/react-ui/src/components/ui/button.tsx
+++ packages/react-ui/src/components/ui/button.tsx
@@ -2,12 +2,12 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
-import { cn } from '@/lib/utils';
-
 import { LoadingSpinner } from './spinner';
 
+import { cn } from '@/lib/utils';
+
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'border-input bg-background hover:bg-accent hover:text-accent-foreground border',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
--- packages/react-ui/src/components/ui/dialog.tsx
+++ packages/react-ui/src/components/ui/dialog.tsx
@@ -57,7 +57,7 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      'flex flex-col space-y-1.5 gap-3 text-center sm:text-left',
+      'flex flex-col space-y-1.5 gap-3 text-center sm:text-left mb-4',
       className
     )}
     {...props}
