5c24a43f51a813aa672840f4af86e12fc5d86270
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-07-16 19:04:00 +0300

    chore: activate delete connections

--- packages/react-ui/src/features/connections/lib/app-connections-api.ts
+++ packages/react-ui/src/features/connections/lib/app-connections-api.ts
@@ -1,14 +1,18 @@
-import { api } from '@/lib/api';
 import {
   AppConnection,
   ListAppConnectionsRequestQuery,
   SeekPage,
 } from '@activepieces/shared';
 
+import { api } from '@/lib/api';
+
 export const appConnectionsApi = {
   list(
-    request: ListAppConnectionsRequestQuery,
+    request: ListAppConnectionsRequestQuery
   ): Promise<SeekPage<AppConnection>> {
     return api.get<SeekPage<AppConnection>>('/v1/app-connections', request);
+  },
+  delete(id: string): Promise<void> {
+    return api.delete<void>(`/v1/app-connections/${id}`);
   },
 };
