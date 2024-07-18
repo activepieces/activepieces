94bc38e32d3aabc26fc2591de7de1d2974c5f037
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-07-18 16:23:20 +0200

    fix: cache builder

--- packages/react-ui/src/app/builder/run-list/flow-run-card.tsx
+++ packages/react-ui/src/app/builder/run-list/flow-run-card.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { CardListItem } from '@/components/ui/card-list';
 import { LoadingSpinner } from '@/components/ui/spinner';
+import { INTERNAL_ERROR_TOAST, toast } from '@/components/ui/use-toast';
 import { flowRunsApi } from '@/features/flow-runs/lib/flow-runs-api';
 import { LeftSideBarType, useBuilderStateContext } from '@/hooks/builder-hooks';
 import { formatUtils } from '@/lib/utils';
@@ -22,11 +23,11 @@ const FlowRunCard = React.memo((params: FlowRunCardProps) => {
   const { mutate, isPending } = useMutation<FlowRun, Error, string>({
     mutationFn: (flowRunId) => flowRunsApi.getPopulated(flowRunId),
     onSuccess: (run) => {
-      // TODO handling loading
       setRun(run);
       setLeftSidebar(LeftSideBarType.RUN_DETAILS);
     },
     onError: (error) => {
+      toast(INTERNAL_ERROR_TOAST);
       console.error(error);
     },
   });
