f13c552843d534148a664603a83dc7c1a4721be8
Olivier Sambourg <AdamSelene@users.noreply.github.com>
2024-07-19 09:54:39 +0200

    feat(intercom): add various conversation triggers

---
+++ packages/pieces/community/intercom/src/lib/triggers/conversation-closed.ts
@@ -0,0 +1,34 @@
+import {
+  createTrigger,
+  TriggerStrategy,
+} from '@activepieces/pieces-framework';
+import { intercomAuth } from '../..';
+import { intercomClient } from '../common';
+
+export const conversationClosed = createTrigger({
+  // auth: check https://www.activepieces.com/docs/developers/piece-reference/authentication,
+  name: 'conversationClosed',
+  displayName: 'Conversation closed',
+  description:
+    'Triggers when a conversation is closed',
+  props: {},
+  sampleData: {},
+  auth: intercomAuth,
+  type: TriggerStrategy.APP_WEBHOOK,
+  async onEnable(context) {
+    const client = intercomClient(context.auth);
+    const response: { app: { id_code: string } } = await client.get({
+      url: '/me',
+    });
+    context.app.createListeners({
+      events: ['conversation.admin.closed'],
+      identifierValue: response['app']['id_code'],
+    });
+  },
+  async onDisable(context) {
+    // implement webhook deletion logic
+  },
+  async run(context) {
+    return [context.payload.body];
+  },
+});
