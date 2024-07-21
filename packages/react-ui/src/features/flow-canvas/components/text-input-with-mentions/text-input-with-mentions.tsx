756113ceb791c84f707afaa6a9cee9620273ed33
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2024-07-21 15:52:45 +0300

    chore: render mention the way we want to

--- packages/react-ui/src/features/flow-canvas/components/text-input-with-mentions/text-input-with-mentions.tsx
+++ packages/react-ui/src/features/flow-canvas/components/text-input-with-mentions/text-input-with-mentions.tsx
@@ -4,21 +4,47 @@ import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
 import { useEditor, EditorContent } from '@tiptap/react';
 
-import suggestion from './suggestion.js';
+
+
 // figure out a way to insert mentions dynamically in the editor
 //figure out a way to parse the output of the editor to get text
 // figure out a way to parse the input into a json object for the editor
-//figure out a way to render the mentions correctly
+
 // define your extension array
 const extensions = [
   Document,
-  Paragraph,
-  Text,
-  Mention.configure({
+  Paragraph.configure({
     HTMLAttributes: {
-      class: 'mention',
+      class: 'text-base leading-[30px]',
+    }
+  }),
+  Text,
+  
+  Mention.configure({
+    renderHTML({ options, node }) {
+      const imageUrl = `https://cdn.activepieces.com/pieces/webhook.svg`;//TODO: replace with the actual image url
+      
+      // Creating the main div element
+      const mentionElement = document.createElement('span');
+      mentionElement.className = 'inline-flex bg-[#fafafa] border border-[#9e9e9e] border-solid items-center gap-2 py-1 px-2 rounded-[3px]  ';
+     
+      // Creating the image element
+      const imgElement = document.createElement('img');
+      imgElement.src = imageUrl;
+      imgElement.className = 'object-fit w-4 h-4';
+      
+      // Adding the image element to the main div
+      mentionElement.appendChild(imgElement);
+      
+      // Creating the second child div element
+      const mentiontextDiv = document.createElement('div');
+      mentiontextDiv.className = 'text-base text-accent-foreground leading-[18px]';
+      mentiontextDiv.textContent = node.attrs.id;
+      
+      // Adding the text div to the main div
+      mentionElement.appendChild(mentiontextDiv);
+      return mentionElement;
     },
-    suggestion,
   }),
 ];
 
