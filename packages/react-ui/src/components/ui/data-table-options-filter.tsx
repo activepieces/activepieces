aae176402ec137473bfee33b6a595a125a0224ed
Islam Abdelfattah <islam_9t9@yahoo.com>
2024-07-28 15:59:11 +0300

    feat: separate input and select options in separate components

--- packages/react-ui/src/components/ui/data-table-options-filter.tsx
+++ packages/react-ui/src/components/ui/data-table-options-filter.tsx
@@ -1,24 +1,9 @@
-import { CheckIcon, PlusCircledIcon } from '@radix-ui/react-icons';
 import { Column } from '@tanstack/react-table';
 import * as React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-import { Badge } from './badge';
-import { Button } from './button';
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-} from './command';
-import { DebouncedInput } from './debounced-input';
-import { Popover, PopoverContent, PopoverTrigger } from './popover';
-import { Separator } from './seperator';
-
-import { cn } from '@/lib/utils';
+import { DataTableInputPopover } from './data-table-input-popover';
+import { DataTableSelectPopover } from './data-table-select-popover';
 
 interface DataTableFacetedFilterProps<TData, TValue> {
   type: string;
@@ -70,137 +55,22 @@ export function DataTableFacetedFilter<TData, TValue>({
     case 'input': {
       const filterValue = (column?.getFilterValue() || '') as string;
       return (
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button variant="outline" size="sm" className="h-8 border-dashed">
-              <PlusCircledIcon className="mr-2 size-4" />
-              {title}
-              {filterValue.length > 0 && (
-                <>
-                  <Separator orientation="vertical" className="mx-2 h-4" />
-                  <Badge
-                    variant="secondary"
-                    className="rounded-sm px-1 font-normal max-w-40 truncate"
-                  >
-                    {filterValue}
-                  </Badge>
-                </>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[200px] p-0" align="start">
-            <DebouncedInput
-              placeholder="Name"
-              value={filterValue}
-              onChange={handleFilterChange}
-            />
-          </PopoverContent>
-        </Popover>
+        <DataTableInputPopover
+          title={title}
+          filterValue={filterValue}
+          handleFilterChange={handleFilterChange}
+        />
       );
     }
     case 'select':
       return (
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button variant="outline" size="sm" className="h-8 border-dashed">
-              <PlusCircledIcon className="mr-2 size-4" />
-              {title}
-              {selectedValues?.size > 0 && (
-                <>
-                  <Separator orientation="vertical" className="mx-2 h-4" />
-                  <Badge
-                    variant="secondary"
-                    className="rounded-sm px-1 font-normal lg:hidden"
-                  >
-                    {selectedValues.size}
-                  </Badge>
-                  <div className="hidden space-x-1 lg:flex">
-                    {selectedValues.size > 2 ? (
-                      <Badge
-                        variant="secondary"
-                        className="rounded-sm px-1 font-normal"
-                      >
-                        {selectedValues.size} selected
-                      </Badge>
-                    ) : (
-                      options
-                        .filter((option) => selectedValues.has(option.value))
-                        .map((option) => (
-                          <Badge
-                            variant="secondary"
-                            key={option.value}
-                            className="rounded-sm px-1 font-normal"
-                          >
-                            {option.label}
-                          </Badge>
-                        ))
-                    )}
-                  </div>
-                </>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[200px] p-0" align="start">
-            <Command>
-              <CommandInput placeholder={title} />
-              <CommandList>
-                <CommandEmpty>No results found.</CommandEmpty>
-                <CommandGroup>
-                  {options.map((option) => {
-                    const isSelected = selectedValues.has(option.value);
-                    return (
-                      <CommandItem
-                        key={option.value}
-                        onSelect={() => {
-                          if (isSelected) {
-                            selectedValues.delete(option.value);
-                          } else {
-                            selectedValues.add(option.value);
-                          }
-                          const filterValues = Array.from(selectedValues);
-                          handleFilterChange(filterValues);
-                        }}
-                      >
-                        <div
-                          className={cn(
-                            'mr-2 flex h-4 w-4 items-center justify-center rounded border border-primary',
-                            isSelected
-                              ? 'bg-primary text-primary-foreground'
-                              : 'opacity-50 [&_svg]:invisible',
-                          )}
-                        >
-                          <CheckIcon className={cn('h-4 w-4')} />
-                        </div>
-                        {option.icon && (
-                          <option.icon className="mr-2 size-4 text-muted-foreground" />
-                        )}
-                        <span>{option.label}</span>
-                        {facets?.get(option.value) && (
-                          <span className="ml-auto flex size-4 items-center justify-center font-mono text-xs">
-                            {facets.get(option.value)}
-                          </span>
-                        )}
-                      </CommandItem>
-                    );
-                  })}
-                </CommandGroup>
-                {selectedValues.size > 0 && (
-                  <>
-                    <CommandSeparator />
-                    <CommandGroup>
-                      <CommandItem
-                        onSelect={() => handleFilterChange([])}
-                        className="justify-center text-center"
-                      >
-                        Clear filters
-                      </CommandItem>
-                    </CommandGroup>
-                  </>
-                )}
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
+        <DataTableSelectPopover
+          title={title}
+          selectedValues={selectedValues}
+          options={options}
+          handleFilterChange={handleFilterChange}
+          facets={facets}
+        />
       );
 
     default:
