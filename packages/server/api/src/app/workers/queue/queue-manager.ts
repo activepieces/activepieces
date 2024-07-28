65faee5488f080e0c8c2e03c0f5b852a328a9555
Hazem Adel <74595174+hazemadelkhalel@users.noreply.github.com>
2024-07-28 18:34:14 +0300

    feat: optimize get job priority & update retryExponential value

--- packages/server/api/src/app/workers/queue/queue-manager.ts
+++ packages/server/api/src/app/workers/queue/queue-manager.ts
@@ -24,8 +24,8 @@ export const DEFAULT_PRIORITY: keyof typeof JOB_PRIORITY = 'low'
 
 // TODO remove after adding rate limting per user
 export async function getJobPriority(projectId: string, synchronousHandlerId: string | null | undefined): Promise<keyof typeof JOB_PRIORITY> {
-    const project = await projectService.getOneOrThrow(projectId)
-    const isCloudPlatform = flagService.isCloudPlatform(project?.platformId)
+    const platformId = await projectService.getPlatformId(projectId)
+    const isCloudPlatform = flagService.isCloudPlatform(platformId)
     const edition = system.getEdition()
     if (!isCloudPlatform && edition === ApEdition.CLOUD) {
         return ENTERPRISE_FLOW_PRIORITY
