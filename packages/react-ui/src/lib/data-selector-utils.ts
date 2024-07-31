9f65ba2e317d80e994f7e6217462a83d48be22a8
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-07-31 18:32:11 +0300

    chore: comments follow up

--- packages/react-ui/src/lib/data-selector-utils.ts
+++ packages/react-ui/src/lib/data-selector-utils.ts
@@ -11,53 +11,38 @@ export type MentionTreeNode = {
   };
   children?: MentionTreeNode[];
 };
+
+type HandleStepOutputProps = {
+  stepOutput: unknown;
+  propertyPath: string;
+  displayName: string;
+};
+
 /**Traverses an object to find its child properties and their paths, stepOutput has to be an object on first invocation */
 function traverseStepOutputAndReturnMentionTree({
   stepOutput,
   propertyPath,
   displayName,
-}: {
-  stepOutput: unknown;
-  propertyPath: string;
-  displayName: string;
-}): MentionTreeNode {
-  if (stepOutput && typeof stepOutput === 'object') {
-    if (Array.isArray(stepOutput)) {
-      return handlingArrayStepOutput(stepOutput, propertyPath, displayName);
-    }
-    return {
-      key: propertyPath,
-      data: {
-        propertyPath: propertyPath,
-        displayName: displayName,
-        isSlice: false,
-        value: Object.keys(stepOutput).length === 0 ? 'Empty List' : undefined,
-      },
-      children: Object.keys(stepOutput).map((k) => {
-        const escapedKey = k
-          .replaceAll(/\\/g, '\\')
-          .replaceAll(/"/g, '\\"')
-          .replaceAll(/'/g, "\\'")
-          .replaceAll(/\n/g, '\\n')
-          .replaceAll(/\r/g, '\\r')
-          .replaceAll(/\t/g, '\\t')
-          .replaceAll(/’/g, '\\’');
-        const newPath = `${propertyPath}['${escapedKey}']`;
-        const newDisplayName = k;
-        return traverseStepOutputAndReturnMentionTree({
-          stepOutput: (stepOutput as Record<string, unknown>)[k],
-          propertyPath: newPath,
-          displayName: newDisplayName,
-        });
-      }),
-    };
+}: HandleStepOutputProps): MentionTreeNode {
+  if (Array.isArray(stepOutput)) {
+    return handlingArrayStepOutput({
+      stepOutput,
+      lastDisplayName: displayName,
+      path: propertyPath,
+      startingIndex: 0,
+    });
+  }
+  const isObject = stepOutput && typeof stepOutput === 'object';
+
+  if (isObject) {
+    return handleObjectStepOutput({ propertyPath, displayName, stepOutput });
   } else {
     const value = formatStepOutput(stepOutput);
     return {
       key: propertyPath,
       data: {
-        propertyPath: propertyPath,
-        displayName: displayName,
+        propertyPath,
+        displayName,
         value,
         isSlice: false,
       },
@@ -66,12 +51,17 @@ function traverseStepOutputAndReturnMentionTree({
   }
 }
 
-const handlingArrayStepOutput = (
-  stepOutput: unknown[],
-  path: string,
-  lastDisplayName: string,
-  startingIndex = 0,
-): MentionTreeNode => {
+const handlingArrayStepOutput = ({
+  lastDisplayName,
+  startingIndex,
+  stepOutput,
+  path,
+}: {
+  stepOutput: unknown[];
+  path: string;
+  lastDisplayName: string;
+  startingIndex: number;
+}): MentionTreeNode => {
   const MAX_ARRAY_LENGTH_BEFORE_SLICING = 100;
   if (stepOutput.length <= MAX_ARRAY_LENGTH_BEFORE_SLICING) {
     return {
@@ -104,12 +94,12 @@ const handlingArrayStepOutput = (
       Math.min((i + 1) * MAX_ARRAY_LENGTH_BEFORE_SLICING, stepOutput.length) -
       1;
     const displayName = `${lastDisplayName} ${startingIndex}-${endingIndex}`;
-    const sliceOutput = handlingArrayStepOutput(
-      stepOutput.slice(startingIndex, endingIndex),
+    const sliceOutput = handlingArrayStepOutput({
+      stepOutput: stepOutput.slice(startingIndex, endingIndex),
       path,
       lastDisplayName,
       startingIndex,
-    );
+    });
     children.push({
       ...sliceOutput,
       key: displayName,
@@ -131,6 +121,38 @@ const handlingArrayStepOutput = (
     children: children,
   };
 };
+
+function handleObjectStepOutput({
+  displayName,
+  propertyPath,
+  stepOutput,
+}: {
+  propertyPath: string;
+  displayName: string;
+  stepOutput: object;
+}): MentionTreeNode {
+  return {
+    key: propertyPath,
+    data: {
+      propertyPath: propertyPath,
+      displayName: displayName,
+      isSlice: false,
+      value: Object.keys(stepOutput).length === 0 ? 'Empty List' : undefined,
+    },
+    children: Object.keys(stepOutput).map((childPropertyKey) => {
+      const escapedKey = childPropertyKey.replaceAll(
+        /[\\"'\n\r\t’]/g,
+        (char) => `\\${char}`,
+      );
+
+      return traverseStepOutputAndReturnMentionTree({
+        stepOutput: (stepOutput as Record<string, unknown>)[childPropertyKey],
+        propertyPath: `${propertyPath}['${escapedKey}']`,
+        displayName: childPropertyKey,
+      });
+    }),
+  };
+}
 
 function formatStepOutput(stepOutput: unknown) {
   if (stepOutput === null) {
