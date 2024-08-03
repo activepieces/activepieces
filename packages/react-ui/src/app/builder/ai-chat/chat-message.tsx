983f6a2f7bee08f813a30db10dd0e87d4f4ed1b1
A <abdullahbitar02@gmail.com>
2024-08-03 04:15:14 +0300

    feat: add code editor for bot reply

--- packages/react-ui/src/app/builder/ai-chat/chat-message.tsx
+++ packages/react-ui/src/app/builder/ai-chat/chat-message.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Bot } from 'lucide-react';
+import { CodeEditior } from '../step-settings/code-settings/code-editior';
 
 interface ChatMessageProps {
     message: string;
@@ -7,6 +8,12 @@ interface ChatMessageProps {
 }
 
 export const ChatMessage: React.FC<ChatMessageProps> = ({ message, userType }) => {
+
+    const sourceCode = {
+        code: message,
+        packageJson: ''
+    };
+    
     return (
         <div className={`flex ${userType === 'user' ? 'justify-end' : 'justify-start'} m-4`}>
             {userType === 'bot' && (
@@ -14,12 +21,14 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({ message, userType }) =
                     <div className="w-7 h-7 border rounded-full border-gray-300 flex items-center justify-center">
                         <Bot className="w-5 h-5 text-gray-500" />
                     </div>
-                    <p className={`flex max-w-xs lg:max-w-md rounded-2xl pl-5 pr-5 pt-2 pb-2`}>{message}</p>
+                    <div className={`w-full pl-7 pr-7 mb-6`}>
+                        <CodeEditior sourceCode={sourceCode} readonly={true} onChange={() => {}}></CodeEditior>
+                    </div>
                 </>
             )}
             {userType === 'user' && (
                 <>
-                    <p className={`flex max-w-xs lg:max-w-md bg-gray-100 rounded-2xl pl-5 pr-5 pt-2 pb-2`}>{message}</p>
+                    <p className={`flex max-w-xs lg:max-w-md bg-gray-100 rounded-2xl pl-5 pr-5 pt-2 pb-2 break-all`}>{message}</p>
                 </>
             )}
         </div>
