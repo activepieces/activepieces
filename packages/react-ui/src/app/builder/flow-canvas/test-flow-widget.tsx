3a3330e1ece21302ff07a1a89ede7b58c2c29bcf
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-08-07 15:18:00 +0200

    fix: test flow widget

--- packages/react-ui/src/app/builder/flow-canvas/test-flow-widget.tsx
+++ packages/react-ui/src/app/builder/flow-canvas/test-flow-widget.tsx
@@ -11,15 +11,16 @@ import {
 } from '@/components/ui/tooltip';
 import { INTERNAL_ERROR_TOAST, toast } from '@/components/ui/use-toast';
 import { flowsApi } from '@/features/flows/lib/flows-api';
-import { TriggerType, flowHelper, isNil } from '@activepieces/shared';
+import { StepLocationRelativeToParent, TriggerType, flowHelper, isNil } from '@activepieces/shared';
 
 import { useBuilderStateContext } from '../builder-hooks';
 
 const TestFlowWidget = React.memo(() => {
-  const [flowVersion, setRun, selectStepByName] = useBuilderStateContext((state) => [
+  const [flowVersion, setRun, selectStepByName, clickOnNewNodeButton] = useBuilderStateContext((state) => [
     state.flowVersion,
     state.setRun,
-    state.selectStepByName
+    state.selectStepByName,
+    state.clickOnNewNodeButton
   ]);
 
   const triggerHasSampleData =
@@ -38,17 +39,25 @@ const TestFlowWidget = React.memo(() => {
           setRun(run, flowVersion);
         },
       ),
-    onSuccess: () => {},
+    onSuccess: () => { },
     onError: (error) => {
       console.log(error);
       toast(INTERNAL_ERROR_TOAST);
     },
   });
-  
+
   function handleCompleteSettings() {
     const invalidSteps = flowHelper.getAllSteps(flowVersion.trigger).filter(step => !step.valid)
     if (invalidSteps.length > 0) {
-      selectStepByName(invalidSteps[0].name);
+      if (invalidSteps[0].type === TriggerType.EMPTY) {
+        clickOnNewNodeButton(
+          'trigger',
+          invalidSteps[0].name,
+          StepLocationRelativeToParent.AFTER,
+        );
+      } else {
+        selectStepByName(invalidSteps[0].name);
+      }
       return;
     }
   }
