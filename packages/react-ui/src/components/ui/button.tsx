522fed803b6451083404b8e62b6adffe30600db6
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2024-08-11 17:23:15 +0300

    chore: add transparent button variant

--- packages/react-ui/src/components/ui/button.tsx
+++ packages/react-ui/src/components/ui/button.tsx
@@ -2,9 +2,9 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
-import { cn } from '@/lib/utils';
-
 import { LoadingSpinner } from './spinner';
+
+import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
   'ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -20,6 +20,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        transparent: 'text-primary hover:bg-primary/10',
       },
       size: {
         default: 'h-10 px-4 py-2',
