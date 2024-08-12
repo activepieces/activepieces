d9f84eb6a16255e7e3e5a6f4f41d3ac8d6d05512
obenazouz-wedof <144689457+obenazouz-wedof@users.noreply.github.com>
2024-08-12 12:00:07 +0000

    feat(wedof): update wedof.ts

--- packages/pieces/community/wedof/src/lib/common/wedof.ts
+++ packages/pieces/community/wedof/src/lib/common/wedof.ts
@@ -1,8 +1,8 @@
 import { Property } from '@activepieces/pieces-framework';
 
 export const wedofCommon = {
-   baseUrl: 'https://staging.wedof.fr/api',
-  host: 'https://staging.wedof.fr/api',
+   baseUrl: 'https://www.wedof.fr/api',
+  host: 'https://www.wedof.fr/api',
  
 
   state: Property.StaticMultiSelectDropdown({
