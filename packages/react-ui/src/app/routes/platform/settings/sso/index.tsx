7b17f2719a8bd5d119b7c8c1839f594afa3b9483
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-08-19 13:32:23 +0000

    feat: add allowed domains

--- packages/react-ui/src/app/routes/platform/settings/sso/index.tsx
+++ packages/react-ui/src/app/routes/platform/settings/sso/index.tsx
@@ -1,8 +1,13 @@
+import { isNil } from '@activepieces/shared';
 import { useMutation } from '@tanstack/react-query';
-import { LockIcon, MailIcon } from 'lucide-react';
+import { LockIcon, MailIcon, Earth } from 'lucide-react';
 import React from 'react';
 
+import GithubIcon from '../../../../../assets/img/custom/auth/github.svg';
+import GoogleIcon from '../../../../../assets/img/custom/auth/google-icon.svg';
+
 import LockedFeatureGuard from '@/app/components/locked-feature-guard';
+import { AllowedDomainDialog } from '@/app/routes/platform/settings/sso/allowed-domain';
 import { NewOAuth2Dialog } from '@/app/routes/platform/settings/sso/oauth2-dialog';
 import { ConfigureSamlDialog } from '@/app/routes/platform/settings/sso/saml-dialog';
 import { Card } from '@/components/ui/card';
@@ -10,22 +15,21 @@ import { Switch } from '@/components/ui/switch';
 import { INTERNAL_ERROR_TOAST, toast } from '@/components/ui/use-toast';
 import { platformHooks } from '@/hooks/platform-hooks';
 import { platformApi } from '@/lib/platforms-api';
-import { isNil } from '@activepieces/shared';
-
-import GithubIcon from '../../../../../assets/img/custom/auth/github.svg';
-import GoogleIcon from '../../../../../assets/img/custom/auth/google-icon.svg';
+import { Badge } from '@/components/ui/badge';
 
 type ProviderCardProps = {
   providerName: string;
   providerDescription?: string;
   providerIcon?: React.ReactNode;
   button: React.ReactNode;
+  badgesText?: string[];
 };
 const ProviderCard = ({
   providerName,
   providerIcon,
   providerDescription,
   button,
+  badgesText
 }: ProviderCardProps) => {
   return (
     <Card className="w-full px-4 py-4">
@@ -39,6 +43,14 @@ const ProviderCard = ({
             {providerDescription ??
               `Allow logins through ${providerName.toLowerCase()}'s single sign-on functionality.`}
           </div>
+          {badgesText && <div className='mt-2 gap-2 flex '>
+            {badgesText.map((text, index) => (
+              <Badge key={index} variant={"outline"}>
+                {text}
+              </Badge>
+            ))}
+          </div>}
+
         </div>
         <div className="flex flex-col justify-center items-center">
           {button}
@@ -77,6 +89,7 @@ const SSOPage = () => {
       toast(INTERNAL_ERROR_TOAST);
     },
   });
+
   return (
     <LockedFeatureGuard
       locked={!platform.ssoEnabled}
@@ -92,6 +105,15 @@ const SSOPage = () => {
           </div>
         </div>
         <div className="flex flex-col gap-4">
+          <ProviderCard
+            providerName="Allowed Domains"
+            providerDescription={`Allow users to authenticate with specific domains. Leave empty to allow all domains.`}
+            providerIcon={<Earth className="w-[32px] h-[32px]" />}
+            badgesText={platform?.allowedAuthDomains ?? []}
+            button={
+              <AllowedDomainDialog platform={platform} refetch={refetch} />
+            }
+          />
           <ProviderCard
             providerName="Google"
             providerIcon={
