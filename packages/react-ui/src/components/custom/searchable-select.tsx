1aea5bb18ad7cbfb2526b559bba227d8c83c7029
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-08-20 16:39:58 +0300

    chore: make actions/trigger dropdown searchable

--- packages/react-ui/src/components/custom/searchable-select.tsx
+++ packages/react-ui/src/components/custom/searchable-select.tsx
@@ -25,6 +25,7 @@ import { ScrollArea } from '../ui/scroll-area';
 type SelectOption<T> = {
   value: T;
   label: string;
+  description?: string; 
 };
 
 type SearchableSelectProps<T> = {
@@ -69,10 +70,11 @@ export const SearchableSelect = <T extends React.Key>({
             label: option.label,
             value: option.value,
             index: index,
+            description: option.description?? '',
           };
         })
         .filter((option) => {
-          return option.label.toLowerCase().includes(searchTerm.toLowerCase());
+          return option.label.toLowerCase().includes(searchTerm.toLowerCase()) ||   option.description.toLowerCase().includes(searchTerm.toLowerCase());
         });
       setFilteredOptions(filteredOptions.map((op) => op.index));
     }
@@ -152,19 +154,19 @@ export const SearchableSelect = <T extends React.Key>({
                           setOpen(false);
                           onSelect(currentValue);
                         }}
-                        className="flex gap-2 items-center"
+                        className="flex gap-2 flex-col items-start"
                       >
+                        <div className='flex gap-2 items-center'>
                         <Check
-                          width={16}
-                          height={16}
+                      
                           className={cn(
-                            'flex-shrink-0',
-                            selectedIndex === filterIndex
-                              ? 'opacity-100'
-                              : 'opacity-0',
+                            'flex-shrink-0 w-4 h-4',
+                           { 'hidden': selectedIndex !== filterIndex },
                           )}
                         />
                         {option.label}
+                        </div>
+                        {option.description && <div className='text-sm text-muted-foreground'>{option.description}</div>}
                       </CommandItem>
                     );
                   })}
