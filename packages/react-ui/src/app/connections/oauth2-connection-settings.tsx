48631c7206cf491adcec1a9ff3b7a21d5acb9d39
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-09-02 22:51:29 +0000

    fix: oauth2 connect button

--- packages/react-ui/src/app/connections/oauth2-connection-settings.tsx
+++ packages/react-ui/src/app/connections/oauth2-connection-settings.tsx
@@ -239,7 +239,7 @@ const OAuth2ConnectionSettings = ({
           />
         )}
 
-        <div className="border border-solid p-2 rounded-lg gap-2 flex text-center items-center justify-center h-ful">
+        <div className="border border-solid p-2 rounded-lg gap-2 flex text-center items-center justify-center h-full">
           <div className="rounded-full border border-solid p-1 flex items-center justify-center">
             <img src={piece.logoUrl} className="w-5 h-5"></img>
           </div>
