8ba6d256d73d411ce8d747e8fd0f004651b7b081
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-09-03 22:38:28 +0000

    fix: email validation

--- packages/react-ui/src/features/authentication/lib/password-validation-utils.ts
+++ packages/react-ui/src/features/authentication/lib/password-validation-utils.ts
@@ -60,6 +60,5 @@ const passwordValidation = {
     NUMBER_REGEX.test(value) || validationMessages.number,
 };
 
-const emailRegex =
-  /^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export { passwordValidation, emailRegex, passwordRules };
