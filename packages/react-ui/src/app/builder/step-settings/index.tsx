5d555de10074960e131f195ec7b9b5802f7706c6
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-09-04 15:56:40 +0200

    fix: lint current

--- packages/react-ui/src/app/builder/step-settings/index.tsx
+++ packages/react-ui/src/app/builder/step-settings/index.tsx
@@ -196,9 +196,19 @@ const StepSettingsContainer = React.memo(
       }
     }, [actionName, triggerName]);
 
+    const previousStep = useRef<Action | Trigger | null>(null);
 
     useUpdateEffect(() => {
-      const currentStep = form.getValues();
+      const currentStep = JSON.parse(JSON.stringify(form.getValues()));
+      if (previousStep.current === null) {
+        previousStep.current = currentStep;
+        return;
+      }
+      if (deepEqual(currentStep, previousStep.current)) {
+        return;
+      }
+      previousStep.current = currentStep;
+
       if (currentStep.type === TriggerType.PIECE) {
         debouncedTrigger(currentStep as Trigger);
       } else {
@@ -273,18 +283,18 @@ const StepSettingsContainer = React.memo(
                   {[ActionType.CODE, ActionType.PIECE].includes(
                     modifiedStep.type as ActionType,
                   ) && (
-                      <ActionErrorHandlingForm
-                        hideContinueOnFailure={
-                          modifiedStep.settings.errorHandlingOptions
-                            ?.continueOnFailure?.hide
-                        }
-                        disabled={readonly}
-                        hideRetryOnFailure={
-                          modifiedStep.settings.errorHandlingOptions
-                            ?.retryOnFailure?.hide
-                        }
-                      ></ActionErrorHandlingForm>
-                    )}
+                    <ActionErrorHandlingForm
+                      hideContinueOnFailure={
+                        modifiedStep.settings.errorHandlingOptions
+                          ?.continueOnFailure?.hide
+                      }
+                      disabled={readonly}
+                      hideRetryOnFailure={
+                        modifiedStep.settings.errorHandlingOptions
+                          ?.retryOnFailure?.hide
+                      }
+                    ></ActionErrorHandlingForm>
+                  )}
                 </div>
               </ScrollArea>
             </ResizablePanel>
