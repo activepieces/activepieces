76cb1f3aa2246b56ff3a7f4444f9b2f5ec728d64
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-09-07 10:03:15 +0000

    fix: add indices

--- packages/server/api/src/app/database/migration/postgres/1725699690971-AddLogsFileIdIndex.ts
+++ packages/server/api/src/app/database/migration/postgres/1725699690971-AddLogsFileIdIndex.ts
@@ -3,31 +3,40 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class AddLogsFileIdIndex1725699690971 implements MigrationInterface {
     name = 'AddLogsFileIdIndex1725699690971'
-
+    transaction = false
     public async up(queryRunner: QueryRunner): Promise<void> {
+
+        // Create indexes concurrently does not run in a transaction
         await queryRunner.query(`
-            CREATE INDEX "idx_file_project_id" ON "file" ("projectId")
-        `)
-        await queryRunner.query(`
-            CREATE INDEX "idx_file_type_created_desc" ON "file" ("type", "created")
-        `)
-        await queryRunner.query(`
-            CREATE INDEX "idx_run_logs_file_id" ON "flow_run" ("logsFileId")
+            CREATE INDEX CONCURRENTLY "idx_file_project_id" ON "file" ("projectId")
         `)
         logger.info({
             name: this.name,
-        }, 'is up')
+        }, 'idx_file_project_id is up')
+        await queryRunner.query(`
+            CREATE INDEX CONCURRENTLY "idx_file_type_created_desc" ON "file" ("type", "created")
+        `)
+        logger.info({
+            name: this.name,
+        }, 'idx_file_type_created_desc is up')
+
+        await queryRunner.query(`
+            CREATE INDEX CONCURRENTLY "idx_run_logs_file_id" ON "flow_run" ("logsFileId")
+        `)
+        logger.info({
+            name: this.name,
+        }, 'idx_run_logs_file_id is up')
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            DROP INDEX "public"."idx_run_logs_file_id"
+            DROP INDEX CONCURRENTLY "public"."idx_run_logs_file_id"
         `)
         await queryRunner.query(`
-            DROP INDEX "public"."idx_file_type_created_desc"
+            DROP INDEX CONCURRENTLY "public"."idx_file_type_created_desc"
         `)
         await queryRunner.query(`
-            DROP INDEX "public"."idx_file_project_id"
+            DROP INDEX CONCURRENTLY "public"."idx_file_project_id"
         `)
         logger.info({
             name: this.name,
