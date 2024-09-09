9dcff6cd1c22ebb8be25d1f659f248cc3402c8cb
Kishan Parmar <code.kishanparmar@gmail.com>
2024-09-09 13:38:32 +0530

    chore: remove extra files

--- packages/pieces/community/google-search-console/src/index.ts
+++ packages/pieces/community/google-search-console/src/index.ts
@@ -10,37 +10,34 @@ import { deleteSite } from './lib/actions/delete-a-site';
 
 // OAuth 2.0 Authentication Configuration
 export const googleSearchConsoleAuth = PieceAuth.OAuth2({
-    description: '',
-    authUrl: 'https://accounts.google.com/o/oauth2/auth',
-    tokenUrl: 'https://oauth2.googleapis.com/token',
-    scope: ['https://www.googleapis.com/auth/webmasters'],
-    required: true
+  description: '',
+  authUrl: 'https://accounts.google.com/o/oauth2/auth',
+  tokenUrl: 'https://oauth2.googleapis.com/token',
+  scope: ['https://www.googleapis.com/auth/webmasters'],
+  required: true,
 });
 
 export const createAuthClient = (accessToken: string) => {
-    const auth = new google.auth.OAuth2();
-    auth.setCredentials({ access_token: accessToken });
-    return google.webmasters({ version: 'v3', auth });
+  const auth = new google.auth.OAuth2();
+  auth.setCredentials({ access_token: accessToken });
+  return google.webmasters({ version: 'v3', auth });
 };
 
 // Create the Google Search Console Piece
 export const googleSearchConsolePiece = createPiece({
-    displayName: 'Google Search Console',
-    minimumSupportedRelease: '0.20.0',
-    logoUrl: "",
-    triggers: [],
-    description: 'Interact with the Google Search Console API to manage your websites.',
-    authors: [
-        'Gushkool'
-    ],
-    auth: googleSearchConsoleAuth,
-    actions: [
-        searchAnalytics,
-        listSitemaps,
-        submitSitemap,
-        listSites,
-        addSite,
-        deleteSite,
-        urlInspection,
-    ]
+  displayName: 'Google Search Console',
+  minimumSupportedRelease: '0.20.0',
+  auth: googleSearchConsoleAuth,
+  logoUrl: 'https://cdn.activepieces.com/pieces/google-search-console.png',
+  authors: ['Gushkool'],
+  triggers: [],
+  actions: [
+    searchAnalytics,
+    listSitemaps,
+    submitSitemap,
+    listSites,
+    addSite,
+    deleteSite,
+    urlInspection,
+  ],
 });
