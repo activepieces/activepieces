c2438712b958eb13b0d5cc43b860e858ff85dc67
Thibaut Caillierez <thibaut.caillierez@alan.eu>
2024-09-12 11:38:45 +0200

    feat(metabase): handle query errors

--- packages/pieces/community/metabase/src/lib/actions/get-question.ts
+++ packages/pieces/community/metabase/src/lib/actions/get-question.ts
@@ -33,7 +33,7 @@ export const getQuestion = createAction({
     );
     const parameters = card['parameters'] as MetabaseParam[];
 
-    return queryMetabaseApi(
+    const response = await queryMetabaseApi(
       {
         endpoint: `card/${propsValue.questionId}/query`,
         method: HttpMethod.POST,
@@ -59,5 +59,10 @@ export const getQuestion = createAction({
       },
       auth
     );
+    if (response.error) {
+      throw new Error(response.error);
+    } else {
+      return response;
+    }
   },
 });
