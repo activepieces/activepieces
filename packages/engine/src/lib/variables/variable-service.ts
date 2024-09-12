f8828b527da5075493aa3fd2731b210270136d8d
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-09-12 16:43:21 +0000

    fix: empty variable on resolving with v8 isolate

--- packages/engine/src/lib/variables/variable-service.ts
+++ packages/engine/src/lib/variables/variable-service.ts
@@ -131,7 +131,7 @@ export class VariableService {
             return result ?? ''
         }
         catch (exception) {
-            console.error('[evalInScope] Error evaluating variable', exception)
+            console.warn('[evalInScope] Error evaluating variable', exception)
             return ''
         }
     }
