5882612fdc2c727845de3fd11fdd5b0fef76db15
moshe <aharonshor@gmail.com>
2024-09-19 03:33:33 +0300

    feat: add custom create and write actions in odoo

--- packages/pieces/community/odoo/src/lib/actions/index.ts
+++ packages/pieces/community/odoo/src/lib/actions/index.ts
@@ -2,10 +2,14 @@ import createContact from './create-contact';
 import createCompany from './create-company';
 import getContacts from './get-contacts';
 import getRecords from './get-records';
+import createRecord from './create-record';
+import updateRecord from './update-record';
 
 export default [
     getContacts,
     createContact,
     createCompany,
     getRecords,
+    createRecord,
+    updateRecord,
 ];
