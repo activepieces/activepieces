062d2868d8939bc1dfe22cb2123fef76c2b1b221
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-09-23 11:18:25 +0300

    chore: add telemetry for ai providers

--- packages/server/api/src/app/ai/ai-provider.module.ts
+++ packages/server/api/src/app/ai/ai-provider.module.ts
@@ -1,92 +1,129 @@
-import { AiProviderConfig, AiProviderWithoutSensitiveData, EnginePrincipal, PrincipalType, SeekPage } from '@activepieces/shared'
-import { FastifyPluginAsyncTypebox, FastifyPluginCallbackTypebox, Type } from '@fastify/type-provider-typebox'
-import { StatusCodes } from 'http-status-codes'
-import { platformMustBeOwnedByCurrentUser } from '../ee/authentication/ee-authorization'
-import { projectService } from '../project/project-service'
-import { proxyController } from './ai-provider-proxy'
-import { aiProviderService } from './ai-provider.service'
+import {
+  AiProviderConfig,
+  AiProviderWithoutSensitiveData,
+  EnginePrincipal,
+  PrincipalType,
+  SeekPage,
+  TelemetryEventName,
+} from '@activepieces/shared';
+import {
+  FastifyPluginAsyncTypebox,
+  FastifyPluginCallbackTypebox,
+  Type,
+} from '@fastify/type-provider-typebox';
+import { StatusCodes } from 'http-status-codes';
+import { platformMustBeOwnedByCurrentUser } from '../ee/authentication/ee-authorization';
+import { projectService } from '../project/project-service';
+import { proxyController } from './ai-provider-proxy';
+import { aiProviderService } from './ai-provider.service';
+import { telemetry } from '../helper/telemetry.utils';
+import { logger } from '../../../../shared/src';
 
 export const aiProviderModule: FastifyPluginAsyncTypebox = async (app) => {
-    await app.register(proxyController, { prefix: '/v1/ai-providers/proxy' })
-    await app.register(aiProviderController, { prefix: '/v1/ai-providers' })
-    await app.register(engineAiProviderController, { prefix: '/v1/ai-providers' })
-}
+  await app.register(proxyController, { prefix: '/v1/ai-providers/proxy' });
+  await app.register(aiProviderController, { prefix: '/v1/ai-providers' });
+  await app.register(engineAiProviderController, {
+    prefix: '/v1/ai-providers',
+  });
+};
 
 const engineAiProviderController: FastifyPluginCallbackTypebox = (
-    fastify,
-    _opts,
-    done,
+  fastify,
+  _opts,
+  done
 ) => {
+  fastify.get('/', ListProxyConfigRequest, async (request) => {
+    const projectId = (request.principal as unknown as EnginePrincipal)
+      .projectId;
+    const platformId = await projectService.getPlatformId(projectId);
+    return aiProviderService.list(platformId);
+  });
 
-    fastify.get('/', ListProxyConfigRequest, async (request) => {
-        const projectId = (request.principal as unknown as EnginePrincipal).projectId
-        const platformId = await projectService.getPlatformId(projectId)
-        return aiProviderService.list(platformId)
-    })  
-
-    done()
-}   
+  done();
+};
 
 const aiProviderController: FastifyPluginCallbackTypebox = (
-    fastify,
-    _opts,
-    done,
+  fastify,
+  _opts,
+  done
 ) => {
+  fastify.addHook('preHandler', platformMustBeOwnedByCurrentUser);
+  fastify.post('/', CreateProxyConfigRequest, async (request) => {
+    telemetry
+      .trackProject(request.principal.projectId, {
+        name: TelemetryEventName.AI_PROVIDER_CONFIGURED,
+        payload: {
+          projectId: request.principal.projectId,
+          platformId: request.principal.platform.id,
+          provider: request.body.provider,
+        },
+      })
+      .catch((e) =>
+        logger.error(
+          e,
+          '[ConfigureAiProvider#telemetry] telemetry.trackProject'
+        )
+      );
+    return aiProviderService.upsert(request.principal.platform.id, {
+      config: request.body.config,
+      baseUrl: request.body.baseUrl,
+      provider: request.body.provider,
+    });
+  });
 
-    fastify.addHook('preHandler', platformMustBeOwnedByCurrentUser)
-    fastify.post('/', CreateProxyConfigRequest, async (request) => {
-        return aiProviderService.upsert(request.principal.platform.id, {
-            config: request.body.config,
-            baseUrl: request.body.baseUrl,
-            provider: request.body.provider,
-        })
-    })
+  fastify.delete(
+    '/:provider',
+    DeleteProxyConfigRequest,
+    async (request, reply) => {
+      await aiProviderService.delete({
+        platformId: request.principal.platform.id,
+        provider: request.params.provider,
+      });
+      await reply.status(StatusCodes.NO_CONTENT).send();
+    }
+  );
 
-    fastify.delete('/:provider', DeleteProxyConfigRequest, async (request, reply) => {
-        await aiProviderService.delete({
-            platformId: request.principal.platform.id,
-            provider: request.params.provider,
-        })
-        await reply.status(StatusCodes.NO_CONTENT).send()
-    })
-
-    done()
-}
-
+  done();
+};
 
 const ListProxyConfigRequest = {
-    config: {
-        allowedPrincipals: [PrincipalType.USER, PrincipalType.ENGINE],
+  config: {
+    allowedPrincipals: [PrincipalType.USER, PrincipalType.ENGINE],
+  },
+  schema: {
+    tags: ['ai-providers'],
+    description: 'List ai provider configs',
+    response: {
+      [StatusCodes.OK]: SeekPage(AiProviderWithoutSensitiveData),
     },
-    schema: {
-        tags: ['ai-providers'],
-        description: 'List ai provider configs',
-        response: {
-            [StatusCodes.OK]: SeekPage(AiProviderWithoutSensitiveData),
-        },
-    },
-}
+  },
+};
 
 const CreateProxyConfigRequest = {
-    config: {
-        allowedPrincipals: [PrincipalType.USER],
-    },
-    schema: {
-        tags: ['ai-providers'],
-        description: 'Create ai provider config',
-        body: Type.Omit(AiProviderConfig, ['id', 'created', 'updated', 'platformId']),
-    },
-}
+  config: {
+    allowedPrincipals: [PrincipalType.USER],
+  },
+  schema: {
+    tags: ['ai-providers'],
+    description: 'Create ai provider config',
+    body: Type.Omit(AiProviderConfig, [
+      'id',
+      'created',
+      'updated',
+      'platformId',
+    ]),
+  },
+};
 
 const DeleteProxyConfigRequest = {
-    config: {
-        allowedPrincipals: [PrincipalType.USER],
-    },
-    schema: {
-        params: Type.Object({
-            provider: Type.String(),
-        }),
-        tags: ['ai-providers'],
-        description: 'Delete ai provider config',
-    },
-}
+  config: {
+    allowedPrincipals: [PrincipalType.USER],
+  },
+  schema: {
+    params: Type.Object({
+      provider: Type.String(),
+    }),
+    tags: ['ai-providers'],
+    description: 'Delete ai provider config',
+  },
+};
