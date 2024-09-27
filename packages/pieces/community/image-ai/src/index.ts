d6a6c1c10168b957e0ca9f655f352789e555ed27
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-09-28 00:41:18 +0200

    fix: bump common package

--- packages/pieces/community/image-ai/src/index.ts
+++ packages/pieces/community/image-ai/src/index.ts
@@ -6,7 +6,7 @@ export const imageAi = createPiece({
   displayName: 'Image AI',
   auth: PieceAuth.None(),
   minimumSupportedRelease: '0.20.0',
-  logoUrl: 'https://cdn.activepieces.com/pieces/image-ai.png',
+  logoUrl: 'https://cdn.activepieces.com/pieces/image-ai.svg',
   categories: [PieceCategory.ARTIFICIAL_INTELLIGENCE],
   authors: ['kishanprmr'],
   actions: [generateImage],
