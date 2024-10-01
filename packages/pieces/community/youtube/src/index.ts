e63fa039c1d056d9810d2dce0b57621160a43ded
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-10-01 00:03:57 +0000

    fix: upgrade minimum version

--- packages/pieces/community/youtube/src/index.ts
+++ packages/pieces/community/youtube/src/index.ts
@@ -12,7 +12,7 @@ export const youtube = createPiece({
   description:
     'Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube',
 
-  minimumSupportedRelease: '0.5.0',
+  minimumSupportedRelease: '0.33.0',
   logoUrl: 'https://cdn.activepieces.com/pieces/youtube.png',
   categories: [PieceCategory.CONTENT_AND_FILES],
   auth: youtubeAuth,
