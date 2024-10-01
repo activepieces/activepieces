b13ae34a1e1d2f98d0584a5ed66003d5a6c72107
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-10-01 17:29:45 +0300

    fix: stop emitting saving on setting initial value for dynamic props

--- packages/react-ui/src/app/builder/piece-properties/text-input-with-mentions/index.tsx
+++ packages/react-ui/src/app/builder/piece-properties/text-input-with-mentions/index.tsx
@@ -111,7 +111,7 @@ export const TextInputWithMentions = ({
       attributes: {
         class: cn(
           className ??
-            ' w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50',
+            ' w-full rounded-md border border-input bg-background px-3 min-h-10 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50',
           textMentionUtils.inputThatUsesMentionClass,
           {
             'cursor-not-allowed opacity-50': disabled,
