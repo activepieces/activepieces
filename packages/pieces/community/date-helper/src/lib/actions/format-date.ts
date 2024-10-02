e52c292b65ff625d11a112fba4c122051788d4db
Kamal Mahyuddin <kamal@envoy.com>
2024-10-02 09:34:08 -0700

    fix(date-helper): extend dayjs with advancedFormat to support X format

--- packages/pieces/community/date-helper/src/lib/actions/format-date.ts
+++ packages/pieces/community/date-helper/src/lib/actions/format-date.ts
@@ -8,10 +8,11 @@ import {
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-
+dayjs.extend(advancedFormat);
 
 export const formatDateAction = createAction({
   name: 'format_date',
--- packages/pieces/community/date-helper/src/lib/actions/next-day-of-week.ts
+++ packages/pieces/community/date-helper/src/lib/actions/next-day-of-week.ts
@@ -12,9 +12,11 @@ import {
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(advancedFormat);
 
 export const nextDayofWeek = createAction({
   name: 'next_day_of_week',
