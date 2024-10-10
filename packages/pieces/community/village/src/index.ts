ee0c129ab83d965e10245690b05d05492f204b94
Rafael Muttoni <rafael@muttoni.com.br>
2024-10-10 19:51:56 +0000

    feat(village-piece): add auth

--- packages/pieces/community/village/src/index.ts
+++ packages/pieces/community/village/src/index.ts
@@ -1,9 +1,16 @@
 
     import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+
+    export const villageAuth = PieceAuth.SecretText({
+      displayName: 'API Key',
+      required: true,
+      description: 'Your Village API Key',
+    });
+    
     
     export const village = createPiece({
       displayName: "Village",
-      auth: PieceAuth.None(),
+      auth: villageAuth,
       minimumSupportedRelease: '0.20.0',
       logoUrl: "https://cdn.activepieces.com/pieces/village.png",
       authors: [],
