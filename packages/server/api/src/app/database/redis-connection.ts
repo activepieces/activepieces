2e08bf948b3b2da3d68dec22c5accff744efb338
Hazem Adel <74595174+hazemadelkhalel@users.noreply.github.com>
2024-10-15 13:18:12 +0300

    feat: add redis sentinels

--- packages/server/api/src/app/database/redis-connection.ts
+++ packages/server/api/src/app/database/redis-connection.ts
@@ -1,56 +1,71 @@
-import { AppSystemProp, system } from '@activepieces/server-shared'
-import { isNil } from '@activepieces/shared'
-import Redis, { RedisOptions } from 'ioredis'
+import { AppSystemProp, system } from '@activepieces/server-shared';
+import { isNil } from '@activepieces/shared';
+import Redis, { RedisOptions } from 'ioredis';
 
-const url = system.get(AppSystemProp.REDIS_URL)
-const username = system.get(AppSystemProp.REDIS_USER)
-const password = system.get(AppSystemProp.REDIS_PASSWORD)
-const useSsl = system.getBoolean(AppSystemProp.REDIS_USE_SSL) ?? false
-const db = system.getNumber(AppSystemProp.REDIS_DB) ?? 0
+const url = system.get(AppSystemProp.REDIS_URL);
+const username = system.get(AppSystemProp.REDIS_USER);
+const password = system.get(AppSystemProp.REDIS_PASSWORD);
+const useSsl = system.getBoolean(AppSystemProp.REDIS_USE_SSL) ?? false;
+const db = system.getNumber(AppSystemProp.REDIS_DB) ?? 0;
+const sentinelList = system.get(AppSystemProp.REDIS_SENTINELS);
 
 export const createRedisClient = (params?: CreateRedisClientParams): Redis => {
-    const config: Partial<RedisOptions> = {
-        maxRetriesPerRequest: null,
-        ...params,
-    }
+  const config: Partial<RedisOptions> = {
+    maxRetriesPerRequest: null,
+    ...params,
+  };
 
-    if (url) {
-        return new Redis(url, {
-            ...config,
-        })
-    }
-
-    const host = system.getOrThrow(AppSystemProp.REDIS_HOST)
-    const serializedPort = system.getOrThrow(AppSystemProp.REDIS_PORT)
-    const port = Number.parseInt(serializedPort, 10)
-
+  if (sentinelList) {
+    const sentinels = sentinelList.split(',').map((sentinel) => {
+      const [host, port] = sentinel.split(':');
+      return { host, port: Number.parseInt(port, 10) };
+    });
+    const name = sentinels[0].host;
     return new Redis({
-        ...config,
-        host,
-        port,
-        username,
-        password,
-        db,
-        tls: useSsl ? {} : undefined,
-    })
-}
+      ...config,
+      sentinels,
+      name,
+      enableTLSForSentinelMode: useSsl ? true : undefined,
+    });
+  }
+
+  if (url) {
+    return new Redis(url, {
+      ...config,
+    });
+  }
+
+  const host = system.getOrThrow(AppSystemProp.REDIS_HOST);
+  const serializedPort = system.getOrThrow(AppSystemProp.REDIS_PORT);
+  const port = Number.parseInt(serializedPort, 10);
+
+  return new Redis({
+    ...config,
+    host,
+    port,
+    username,
+    password,
+    db,
+    tls: useSsl ? {} : undefined,
+  });
+};
 
 type CreateRedisClientParams = {
-    /**
+  /**
    * connection timeout in milliseconds
    */
-    connectTimeout?: number
-    maxRetriesPerRequest?: number
-}
+  connectTimeout?: number;
+  maxRetriesPerRequest?: number;
+};
 
 export const getRedisConnection = (() => {
-    let redis: Redis | null = null
+  let redis: Redis | null = null;
 
-    return (): Redis => {
-        if (!isNil(redis)) {
-            return redis
-        }
-        redis = createRedisClient()
-        return redis
+  return (): Redis => {
+    if (!isNil(redis)) {
+      return redis;
     }
-})()
+    redis = createRedisClient();
+    return redis;
+  };
+})();
