07d37fc5b6a34c448f406c0c17fd19f66159a30d
Mohammad AbuAboud <mo@activepieces.com>
2024-10-17 16:33:52 +0000

    fix: sentinel

--- packages/server/api/src/app/database/redis-connection.ts
+++ packages/server/api/src/app/database/redis-connection.ts
@@ -1,19 +1,14 @@
 import fs from 'fs'
 
-import { AppSystemProp,  RedisType, system } from '@activepieces/server-shared'
-import { assertNotNullOrUndefined, isNil } from '@activepieces/shared'
+import { AppSystemProp, RedisType, system } from '@activepieces/server-shared'
+import { isNil } from '@activepieces/shared'
 import Redis, { RedisOptions } from 'ioredis'
 
-const redisConnectionMode = system.get(AppSystemProp.REDIS_TYPE)
+const redisConnectionMode = system.getOrThrow(AppSystemProp.REDIS_TYPE)
 const url = system.get(AppSystemProp.REDIS_URL)
-const username = system.get(AppSystemProp.REDIS_USER)
-const password = system.getOrThrow(AppSystemProp.REDIS_PASSWORD)
-const role = system.get<'master' | 'slave'>(AppSystemProp.REDIS_SENTINEL_ROLE)
+const password = system.get(AppSystemProp.REDIS_PASSWORD)
 const useSsl = system.getBoolean(AppSystemProp.REDIS_USE_SSL) ?? false
 const sslCaFile = system.get(AppSystemProp.REDIS_SSL_CA_FILE)
-const sentinelList = system.get(AppSystemProp.REDIS_SENTINEL_HOSTS)
-const sentinelName = system.get(AppSystemProp.REDIS_SENTINEL_NAME)
-const db = system.getNumber(AppSystemProp.REDIS_DB) ?? 0
 
 const readCAFile = (file: string | undefined): string | undefined => {
     if (isNil(file)) {
@@ -31,7 +26,9 @@ const createStandaloneClient = (config: Partial<RedisOptions>): Redis => {
 
     const host = system.getOrThrow(AppSystemProp.REDIS_HOST)
     const serializedPort = system.getOrThrow(AppSystemProp.REDIS_PORT)
+    const username = system.getOrThrow(AppSystemProp.REDIS_USER)
     const port = Number.parseInt(serializedPort, 10)
+    const db = system.getNumber(AppSystemProp.REDIS_DB) ?? 0
 
     return new Redis({
         ...config,
@@ -68,7 +65,9 @@ export const createRedisClient = (params?: CreateRedisClientParams): Redis => {
 
 
 const createSentinelClient = (config: Partial<RedisOptions>): Redis => {
-    assertNotNullOrUndefined(sentinelList, 'REDIS_SENTINEL_HOSTS')
+    const sentinelList = system.getOrThrow(AppSystemProp.REDIS_SENTINEL_HOSTS)
+    const sentinelName = system.getOrThrow(AppSystemProp.REDIS_SENTINEL_NAME)
+    const sentinelrole = system.get<'master' | 'slave'>(AppSystemProp.REDIS_SENTINEL_ROLE)
 
     const sentinels = sentinelList.split(',').map((sentinel) => {
         const [host, port] = sentinel.split(':')
@@ -80,7 +79,7 @@ const createSentinelClient = (config: Partial<RedisOptions>): Redis => {
         ...({ sentinels }),
         name: sentinelName,
         password,
-        role,
+        role: sentinelrole,
         ...getTlsOptionsForSentinel(useSsl, tlsCa),
         lazyConnect: true,
     }
