ee158e8724b4ab1b140f9e79efe8b4c02a0e2043
Anas Barghouthy <anasbar97@gmail.com>
2024-10-27 21:10:22 +0000

    chore: enhance logo, send button sizes + enhance animation

--- packages/react-ui/src/app/routes/chat/index.tsx
+++ packages/react-ui/src/app/routes/chat/index.tsx
@@ -204,10 +204,13 @@ export function ChatPage() {
         <div className="flex flex-col items-center justify-center">
           <div className="flex items-center justify-center py-8 ps-4 font-bold">
             <div className="flex flex-col items-center gap-1">
-              <ChatBubbleAvatar
-                src={chatUI?.platformLogoUrl}
-                fallback={<BotIcon className="size-5" />}
-              />
+              <div className="flex items-center justify-center p-3 rounded-full">
+                <img
+                  src={chatUI?.platformLogoUrl}
+                  alt="Bot Avatar"
+                  className="w-10 h-10"
+                />
+              </div>
               <div className="flex items-center gap-1 justify-center">
                 <p className="animate-typing overflow-hidden whitespace-nowrap pr-1 hidden lg:block lg:text-xl text-foreground leading-8">
                   Hi I&apos;m {botName} 👋 What can I help you with today?
@@ -215,7 +218,7 @@ export function ChatPage() {
                 <p className="animate-typing-sm overflow-hidden whitespace-nowrap pr-1 lg:hidden text-xl text-foreground leading-8">
                   Hi I&apos;m {botName} 👋
                 </p>
-                <span className="w-4 h-4 rounded-full animate-blink" />
+                <span className="w-4 h-4 rounded-full bg-foreground animate-[fade_0.15s_ease-out_forwards_0.7s_reverse]" />
               </div>
             </div>
           </div>
