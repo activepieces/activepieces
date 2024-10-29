607c9f7d6e37464e4e1bb76d09a63733363801c6
Kishan Parmar <code.kishanparmar@gmail.com>
2024-10-29 15:14:52 +0530

    fix(ai-utility): structured data

--- packages/pieces/community/ai-utility/src/lib/actions/extract-structured-data.ts
+++ packages/pieces/community/ai-utility/src/lib/actions/extract-structured-data.ts
@@ -1,9 +1,4 @@
-import {
-  ApFile,
-  createAction,
-  DynamicPropsValue,
-  Property,
-} from '@activepieces/pieces-framework';
+import { ApFile, createAction, Property } from '@activepieces/pieces-framework';
 import { isNil } from '@activepieces/shared';
 import {
   AI,
@@ -19,58 +14,73 @@ export const extractStructuredData = createAction({
   props: {
     provider: aiProps('function').provider,
     model: aiProps('function').model,
-    inputType: Property.StaticDropdown({
-      displayName: 'Input Type',
-      description: 'Type of input to be extract information from.',
-      required: true,
-      defaultValue: 'text',
-      options: {
-        disabled: false,
-        options: [
-          { label: 'Text', value: 'text' },
-          { label: 'Image', value: 'image' },
-        ],
-      },
+    text: Property.LongText({
+      displayName: 'Text',
+      description: 'Text to extract structured data from.',
+      required: false,
     }),
-    body: Property.DynamicProperties({
-      displayName: 'Body',
-      required: true,
-      refreshers: ['inputType'],
-      props: async ({ inputType }) => {
-        const type = inputType as unknown as string;
-        const fields: DynamicPropsValue = {};
-
-        if (type === 'text') {
-          fields['input'] = Property.LongText({
-            displayName: 'Unstructured Text',
-            required: true,
-          });
-          fields['prompt'] = Property.LongText({
-            displayName: 'Guide Prompt',
-            description:
-              'A prompt to guide the AI in extracting data from the text.',
-            required: false,
-            defaultValue: 'Extract the following data from the provided text.',
-          });
-        } else if (type === 'image') {
-          fields['input'] = Property.File({
-            displayName: 'Image',
-            description: 'The image file you want extract data from.',
-            required: true,
-          });
-          fields['prompt'] = Property.LongText({
-            displayName: 'Guide Prompt',
-            description:
-              'A prompt to guide the AI in extracting data from the image.',
-            required: false,
-            defaultValue:
-              'Use optical character recognition (OCR) to extract from provided image.',
-          });
-        }
-
-        return fields;
-      },
+    image: Property.File({
+      displayName: 'Image',
+      description: 'Image to extract structured data from.',
+      required: false,
     }),
+    prompt: Property.LongText({
+      displayName: 'Guide Prompt',
+      description: 'Prompt to guide the AI.',
+      required: false,
+    }),
+    // inputType: Property.StaticDropdown({
+    //   displayName: 'Input Type',
+    //   description: 'Type of input to be extract information from.',
+    //   required: true,
+    //   defaultValue: 'text',
+    //   options: {
+    //     disabled: false,
+    //     options: [
+    //       { label: 'Text', value: 'text' },
+    //       { label: 'Image', value: 'image' },
+    //     ],
+    //   },
+    // }),
+    // body: Property.DynamicProperties({
+    //   displayName: 'Body',
+    //   required: true,
+    //   refreshers: ['inputType'],
+    //   props: async ({ inputType }) => {
+    //     const type = inputType as unknown as string;
+    //     const fields: DynamicPropsValue = {};
+
+    //     if (type === 'text') {
+    //       fields['input'] = Property.LongText({
+    //         displayName: 'Unstructured Text',
+    //         required: true,
+    //       });
+    //       fields['prompt'] = Property.LongText({
+    //         displayName: 'Guide Prompt',
+    //         description:
+    //           'A prompt to guide the AI in extracting data from the text.',
+    //         required: false,
+    //         defaultValue: 'Extract the following data from the provided text.',
+    //       });
+    //     } else if (type === 'image') {
+    //       fields['input'] = Property.File({
+    //         displayName: 'Image',
+    //         description: 'The image file you want extract data from.',
+    //         required: true,
+    //       });
+    //       fields['prompt'] = Property.LongText({
+    //         displayName: 'Guide Prompt',
+    //         description:
+    //           'A prompt to guide the AI in extracting data from the image.',
+    //         required: false,
+    //         defaultValue:
+    //           'Use optical character recognition (OCR) to extract from provided image.',
+    //       });
+    //     }
+
+    //     return fields;
+    //   },
+    // }),
     params: Property.Array({
       displayName: 'Data Definition',
       required: true,
@@ -117,14 +127,19 @@ export const extractStructuredData = createAction({
   async run(context) {
     const provider = context.propsValue.provider;
     const model = context.propsValue.model;
-    const inputType = context.propsValue.inputType;
-    const body = context.propsValue.body;
+    const text = context.propsValue.text;
+    const image = context.propsValue.image;
+    const prompt = context.propsValue.prompt;
     const params = context.propsValue.params;
+
+    if (!text && !image) {
+      throw new Error('Please provide text or image to extract data from.');
+    }
 
     const ai = AI({ provider, server: context.server });
 
     let response;
-    if (inputType === 'text') {
+    if (text) {
       const functionCalling = ai.chat.function;
       if (!functionCalling) {
         throw new Error(
@@ -137,20 +152,19 @@ export const extractStructuredData = createAction({
         messages: [
           {
             role: AIChatRole.USER,
-            content: body['input'] as string,
+            content: text,
           },
         ],
         functions: [
           {
             name: 'extract_structured_data',
             description:
-              (body['prompt'] as string) ??
-              'Extract the following data from the provided text.',
+              prompt ?? 'Extract the following data from the provided text.',
             arguments: params as AIFunctionArgumentDefinition[],
           },
         ],
       });
-    } else if (inputType === 'image') {
+    } else {
       const functionCalling = ai.image?.function;
       if (!functionCalling) {
         throw new Error(`Model ${model} does not support image generation.`);
@@ -158,12 +172,12 @@ export const extractStructuredData = createAction({
 
       response = await functionCalling({
         model,
-        image: body['input'] as ApFile,
+        image: image as ApFile,
         messages: [
           {
             role: AIChatRole.USER,
             content:
-              (body['prompt'] as string) ??
+              prompt ??
               'Use optical character recognition (OCR) to extract from provided image.',
           },
         ],
