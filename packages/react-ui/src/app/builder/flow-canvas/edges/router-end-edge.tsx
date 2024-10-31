03afde2205f116417f6be356256d10962e3c316d
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-10-31 14:29:05 +0300

    fix: after branch add button position

--- packages/react-ui/src/app/builder/flow-canvas/edges/router-end-edge.tsx
+++ packages/react-ui/src/app/builder/flow-canvas/edges/router-end-edge.tsx
@@ -26,32 +26,27 @@ export const ApRouterEndCanvasEdge = ({
   const distanceBetweenTargetAndSource = Math.abs(targetX - sourceX);
 
   const path = `
-    M ${sourceX - 0.5} ${
-    sourceY - flowUtilConsts.VERTICAL_SPACE_BETWEEN_STEP_AND_LINE * 2
-  }
+    M ${sourceX - 0.5} ${sourceY - flowUtilConsts.VERTICAL_SPACE_BETWEEN_STEP_AND_LINE * 2
+    }
     v ${data.verticalSpaceBetweenLastNodeInBranchAndEndLine}
    
-    ${
-      distanceBetweenTargetAndSource >= flowUtilConsts.ARC_LENGTH
-        ? `${
-            targetX > sourceX
-              ? flowUtilConsts.ARC_RIGHT_DOWN
-              : flowUtilConsts.ARC_LEFT_DOWN
-          }`
-        : `
-     v ${
-       flowUtilConsts.ARC_LENGTH +
-       flowUtilConsts.VERTICAL_SPACE_BETWEEN_STEP_AND_LINE +
-       2
-     }
+    ${distanceBetweenTargetAndSource >= flowUtilConsts.ARC_LENGTH
+      ? `${targetX > sourceX
+        ? flowUtilConsts.ARC_RIGHT_DOWN
+        : flowUtilConsts.ARC_LEFT_DOWN
+      }`
+      : `
+     v ${flowUtilConsts.ARC_LENGTH +
+      flowUtilConsts.VERTICAL_SPACE_BETWEEN_STEP_AND_LINE +
+      2
+      }
     `
     } 
 
 
 
-       ${
-         data.drawHorizontalLine
-           ? `h ${horizontalLineLength}  
+       ${data.drawHorizontalLine
+      ? `h ${horizontalLineLength}  
 
 
         
@@ -59,16 +54,14 @@ export const ApRouterEndCanvasEdge = ({
         
        
        `
-           : ``
-       }
+      : ``
+    }
       
 
-    ${
-      data.drawEndingVerticalLine
-        ? `v${verticalLineLength} ${
-            !data.isNextStepEmpty ? flowUtilConsts.ARROW_DOWN : ''
-          }`
-        : ''
+    ${data.drawEndingVerticalLine
+      ? `v${verticalLineLength} ${!data.isNextStepEmpty ? flowUtilConsts.ARROW_DOWN : ''
+      }`
+      : ''
     }
   `;
   return (
@@ -80,7 +73,7 @@ export const ApRouterEndCanvasEdge = ({
 
       {data.drawEndingVerticalLine && (
         <foreignObject
-          x={targetX - flowUtilConsts.AP_NODE_SIZE.ADD_BUTTON.width / 2}
+          x={targetX - flowUtilConsts.AP_NODE_SIZE.ADD_BUTTON.width / 2 - flowUtilConsts.LINE_WIDTH / 2}
           y={
             targetY -
             verticalLineLength +
