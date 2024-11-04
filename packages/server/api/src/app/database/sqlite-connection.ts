9cc9f4113f9afb5e8eee152b9e136f3866d1593f
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-11-04 22:53:08 +0000

    feat: add schema version for the flow version

--- packages/server/api/src/app/database/sqlite-connection.ts
+++ packages/server/api/src/app/database/sqlite-connection.ts
@@ -66,6 +66,7 @@ import { RemovePremiumPiecesSqlite1727865697005 } from './migration/sqlite/17278
 import { UpdatePlaformInSqlite1729330108485 } from './migration/sqlite/1729330108485-UpdatePlaformInSqlite'
 import { AddConnectionOwnerSqlite1730121414658 } from './migration/sqlite/1730121414658-AddConnectionOwnerSqlite'
 import { AppConnectionsSetNullSqlite1730627777709 } from './migration/sqlite/1730627777709-AppConnectionsSetNullSqlite'
+import { AddFlowSchemaVersionSqlite1730760312426 } from './migration/sqlite/1730760312426-AddFlowSchemaVersionSqlite'
 
 const getSqliteDatabaseFilePath = (): string => {
     const apConfigDirectoryPath = system.getOrThrow(AppSystemProp.CONFIG_PATH)
@@ -150,6 +151,7 @@ const getMigrations = (): (new () => MigrationInterface)[] => {
         AddPinnedPiecesSqlite1729774033945,
         AddConnectionOwnerSqlite1730121414658,
         AppConnectionsSetNullSqlite1730627777709,
+        AddFlowSchemaVersionSqlite1730760312426,
     ]
     const edition = system.getEdition()
     if (edition !== ApEdition.COMMUNITY) {
