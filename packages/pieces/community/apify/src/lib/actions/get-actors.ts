de8bcb8c775f9307868b0d065d5135b6438c3528
Mark van Bellen <mark@all-tech-plus.com>
2024-11-16 14:48:48 +0000

    feat: new piece 3 actions

---
+++ packages/pieces/community/apify/src/lib/actions/get-actors.ts
@@ -0,0 +1,31 @@
+import { createAction } from '@activepieces/pieces-framework';
+import { apifyAuth } from '../..';
+import { httpClient, HttpMethod } from '@activepieces/pieces-common';
+
+export const getActors = createAction({
+
+  name: 'getActors',
+  auth: apifyAuth,
+  displayName: 'Get users Actors',
+  description: 'Gets the list of Actors the user has available',
+  props: {},
+  async run(context) {
+    const apifyToken = context.auth.apikey;
+    const headers = {
+      'Authorization': 'Bearer ' + apifyToken,
+      'Content-Type': 'application/json',
+    };
+    
+    const url = 'https://api.apify.com/v2/acts/'
+    
+    const httprequestdata = {
+      method: HttpMethod.GET,
+      url,
+      headers,
+  
+    };
+
+    const response = await httpClient.sendRequest(httprequestdata);
+    return response.body;
+  },
+});
