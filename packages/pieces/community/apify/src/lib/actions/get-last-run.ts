2e5844ee17d7ac7068ce8a2bfcc53ceaa841d0bc
Kishan Parmar <code.kishanparmar@gmail.com>
2024-11-19 14:52:48 +0530

    fix(apify): update auth

--- packages/pieces/community/apify/src/lib/actions/get-last-run.ts
+++ packages/pieces/community/apify/src/lib/actions/get-last-run.ts
@@ -9,23 +9,27 @@ export const getLastRun = createAction({
   description: 'Gets last run details for a given Actor.',
   props: {
     actorid: Property.ShortText({
-        displayName: 'The id of the Actor (alphanumeric)',
-        // updated the description as can also use the actors user or ownername a tilde and the actor name in place of the id
-        description: 'The id [defaultDatasetId] of the Actor to get run information [you can also use the username then ~ then the name] (compulsory)',
-        required: true,
-      }),
+      displayName: 'The id of the Actor (alphanumeric)',
+      // updated the description as can also use the actors user or ownername a tilde and the actor name in place of the id
+      description:
+        'The id [defaultDatasetId] of the Actor to get run information [you can also use the username then ~ then the name] (compulsory)',
+      required: true,
+    }),
   },
   async run(context) {
-    const apifyToken = context.auth;
+    const apifyToken = context.auth.apikey;
     const headers = {
       Authorization: 'Bearer ' + apifyToken,
       'Content-Type': 'application/json',
     };
-    
+
     // removed ?status=SUCCEEDED as we might need to know if a particular actor failed
-    const url = 'https://api.apify.com/v2/acts/' + context.propsValue.actorid + '/runs/last';
+    const url =
+      'https://api.apify.com/v2/acts/' +
+      context.propsValue.actorid +
+      '/runs/last';
     //?status=SUCCEEDED'
-    
+
     const httprequestdata = {
       method: HttpMethod.GET,
       url,
--- packages/pieces/community/apify/src/lib/actions/start-actor.ts
+++ packages/pieces/community/apify/src/lib/actions/start-actor.ts
@@ -3,33 +3,36 @@ import { apifyAuth } from '../..';
 import { httpClient, HttpMethod } from '@activepieces/pieces-common';
 
 export const startActor = createAction({
-// https://api.apify.com/v2/acts/{actorId}/run-sync 
-// https://api.apify.com/v2/acts/{actorId}/runs
+  // https://api.apify.com/v2/acts/{actorId}/run-sync
+  // https://api.apify.com/v2/acts/{actorId}/runs
   name: 'startActor',
   auth: apifyAuth,
   displayName: 'Start an Apify Actor',
   description: 'Starts an Apify Actor web scraper',
   props: {
     actorid: Property.ShortText({
-        displayName: 'The id or name of the Actor (alphanumeric)',
-        description: 'The id of the Actor to run [Use either id, or the username then ~ then the name] (compulsory)',
-        required: true,
-      }),
+      displayName: 'The id or name of the Actor (alphanumeric)',
+      description:
+        'The id of the Actor to run [Use either id, or the username then ~ then the name] (compulsory)',
+      required: true,
+    }),
     jsonbody: Property.Json({
-        displayName: 'JSON input',
-        description: 'The JSON input to pass to the Actor [you can get the JSON from a run in your Apify account]. If left blank will use defaults. (optional)',
-        required: true,
+      displayName: 'JSON input',
+      description:
+        'The JSON input to pass to the Actor [you can get the JSON from a run in your Apify account]. If left blank will use defaults. (optional)',
+      required: true,
     }),
   },
   async run(context) {
     const apifyToken = context.auth.apikey;
     const headers = {
-      'Authorization': 'Bearer ' + apifyToken,
+      Authorization: 'Bearer ' + apifyToken,
       'Content-Type': 'application/json',
     };
 
-    const url = 'https://api.apify.com/v2/acts/' + context.propsValue.actorid + '/runs/';
- 
+    const url =
+      'https://api.apify.com/v2/acts/' + context.propsValue.actorid + '/runs/';
+
     const httprequestdata = {
       method: HttpMethod.POST,
       url,
