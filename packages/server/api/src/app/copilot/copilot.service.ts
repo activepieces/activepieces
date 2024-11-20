09ce6e5738272ebaba25502919b65f6aa7dd6224
AbdulTheActivePiecer <abdulyki@activepieces.com>
2024-11-20 19:12:18 +0300

    fix: make copilot service generate code for now

--- packages/server/api/src/app/copilot/copilot.service.ts
+++ packages/server/api/src/app/copilot/copilot.service.ts
@@ -1,8 +1,9 @@
 import { AskCopilotRequest, AskCopilotResponse } from '@activepieces/shared'
+import { codeGeneratorTool } from './tools/code-generate'
 
 
 export const copilotService = {
     async ask(request: AskCopilotRequest): Promise<AskCopilotResponse | null> {
-        return null;
+        return codeGeneratorTool.generateCode(request)
     },
 }
