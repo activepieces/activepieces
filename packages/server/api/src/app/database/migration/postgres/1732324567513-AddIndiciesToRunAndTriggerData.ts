8d17a29bee8b7cd7f3298b4099d348e27fa0c79e
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-11-23 01:27:34 +0000

    fix: add if not exists for new indices

--- packages/server/api/src/app/database/migration/postgres/1732324567513-AddIndiciesToRunAndTriggerData.ts
+++ packages/server/api/src/app/database/migration/postgres/1732324567513-AddIndiciesToRunAndTriggerData.ts
@@ -6,25 +6,25 @@ export class AddIndiciesToRunAndTriggerData1732324567513 implements MigrationInt
     
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            CREATE INDEX CONCURRENTLY "idx_trigger_event_project_id_flow_id" ON "trigger_event" ("projectId", "flowId")
+            CREATE INDEX CONCURRENTLY IF NOT EXISTS "idx_trigger_event_project_id_flow_id" ON "trigger_event" ("projectId", "flowId")
         `)
         await queryRunner.query(`
-            CREATE UNIQUE INDEX CONCURRENTLY "idx_trigger_event_file_id" ON "trigger_event" ("fileId")
+            CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS "idx_trigger_event_file_id" ON "trigger_event" ("fileId")
         `)
         await queryRunner.query(`
-            CREATE INDEX CONCURRENTLY "idx_run_flow_id" ON "flow_run" ("flowId")
+            CREATE INDEX CONCURRENTLY IF NOT EXISTS "idx_run_flow_id" ON "flow_run" ("flowId")
         `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            DROP INDEX CONCURRENTLY "public"."idx_run_flow_id"
+            DROP INDEX IF EXISTS CONCURRENTLY "public"."idx_run_flow_id"
         `)
         await queryRunner.query(`
-            DROP INDEX CONCURRENTLY "public"."idx_trigger_event_file_id"
+            DROP INDEX IF EXISTS CONCURRENTLY "public"."idx_trigger_event_file_id"
         `)
         await queryRunner.query(`
-            DROP INDEX CONCURRENTLY "public"."idx_trigger_event_project_id_flow_id"
+            DROP INDEX IF EXISTS CONCURRENTLY "public"."idx_trigger_event_project_id_flow_id"
         `)
     }
 
