a2dceeeb5b6a527c7e9de6d45c11fd2084965122
Matthieu Lombard <matthieu.lombard@volubile.ai>
2024-12-03 16:55:10 +0100

    fix: remove auth for secret key and replace with a simple text input

--- packages/pieces/community/crypto/src/index.ts
+++ packages/pieces/community/crypto/src/index.ts
@@ -4,15 +4,10 @@ import { generatePassword } from './lib/actions/generate-password';
 import { hashText } from './lib/actions/hash-text';
 import { hmacSignature } from './lib/actions/hmac-signature';
 
-export const hmacSecretKey = PieceAuth.SecretText({
-  displayName: 'Secret key',
-  required: true,
-});
-
 export const Crypto = createPiece({
   displayName: 'Crypto',
   description: 'Generate random passwords and hash existing text',
-  auth: hmacSecretKey,
+  auth: PieceAuth.None(),
   minimumSupportedRelease: '0.30.0',
   logoUrl: 'https://cdn.activepieces.com/pieces/crypto.png',
   categories: [PieceCategory.CORE],
--- packages/pieces/community/crypto/src/lib/actions/hmac-signature.ts
+++ packages/pieces/community/crypto/src/lib/actions/hmac-signature.ts
@@ -1,16 +1,19 @@
 import { createAction, Property } from '@activepieces/pieces-framework';
 import Crypto from 'crypto';
 import { Buffer } from 'buffer';
-import { hmacSecretKey } from '../..';
 
 export const hmacSignature = createAction({
-  auth: hmacSecretKey,
   name: 'hmac-signature',
   description:
     'Converts text to a HMAC signed hash value using various hashing algorithms',
   displayName: 'Generate HMAC Signature',
   props: {
-    encoding: Property.StaticDropdown<'utf-8' | 'hex' | 'base64'>({
+    secretKey: Property.ShortText({
+      displayName: 'Secret key',
+      description: 'The secret key to encrypt',
+      required: true,
+    }),
+    secretKeyEncoding: Property.StaticDropdown<'utf-8' | 'hex' | 'base64'>({
       displayName: 'Secret key encoding',
       description: 'The secret key encoding to use',
       required: true,
@@ -43,7 +46,10 @@ export const hmacSignature = createAction({
   async run(context) {
     const hashAlgorithm = Crypto.createHmac(
       context.propsValue.method,
-      Buffer.from(context.auth, context.propsValue.encoding)
+      Buffer.from(
+        context.propsValue.secretKey,
+        context.propsValue.secretKeyEncoding
+      )
     );
 
     const text = context.propsValue.text;
