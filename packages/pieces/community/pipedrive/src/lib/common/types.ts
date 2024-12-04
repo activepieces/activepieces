07310d072ed51854e57245b3916ebbf602e4cb34
Kishan Parmar <code.kishanparmar@gmail.com>
2024-12-04 15:09:28 +0530

    feat(pipedrive): update deal trigger

---
+++ packages/pieces/community/pipedrive/src/lib/common/types.ts
@@ -0,0 +1,41 @@
+type GetField = {
+	id: string;
+	name: string;
+	key:string
+};
+
+type AdditionalData = {
+	start: number;
+	limit: number;
+	more_items_in_collection: boolean;
+};
+
+export type FieldsResponse = {
+	success: boolean;
+	data: GetField[];
+	additional_data: AdditionalData;
+};
+
+type StageWithPipelineInfo = {
+	id: number;
+	name: string;
+	pipeline_id: number;
+};
+
+export type GetStagesResponse = {
+	success: boolean;
+	data: StageWithPipelineInfo[];
+};
+
+export type ListDealsResponse = {
+	success: boolean;
+	data: Record<string, unknown>[];
+	additional_data: AdditionalData;
+};
+
+export type GetDealResponse=
+{
+	success: boolean;
+	data: Record<string, unknown>;
+	additional_data: AdditionalData;
+}
