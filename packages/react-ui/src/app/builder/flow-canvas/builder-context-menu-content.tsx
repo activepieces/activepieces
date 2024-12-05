37e5a64406cf21b0a787f40547fa545847bcaa46
Mohammad AbuAboud <moabuaboud@gmail.com>
2024-12-05 13:04:43 +0000

    fix: remove commented code

--- packages/react-ui/src/app/builder/flow-canvas/builder-context-menu-content.tsx
+++ packages/react-ui/src/app/builder/flow-canvas/builder-context-menu-content.tsx
@@ -8,55 +8,46 @@ import { BuilderState, RightSideBarType } from "../builder-hooks";
 import { FlowOperationType } from "../../../../../shared/src";
 import { INTERNAL_ERROR_TOAST, toast } from "@/components/ui/use-toast";
 
-const BuilderContextMenuItemWrapper = ({showTooltip,children}:{showTooltip:boolean, children:React.ReactNode}) =>{
+const BuilderContextMenuItemWrapper = ({ showTooltip, children }: { showTooltip: boolean, children: React.ReactNode }) => {
     return <Tooltip >
         <TooltipTrigger asChild>
             <div>
-            {children}
+                {children}
 
             </div>
         </TooltipTrigger>
         {
-            showTooltip &&  <TooltipContent>
-            {t("You need to select a step")}
-        </TooltipContent>
+            showTooltip && <TooltipContent>
+                {t("You need to select a step")}
+            </TooltipContent>
         }
     </Tooltip>
 }
 
-export const BuilderContextMenuContent = ({selectedNodes,applyOperation, selectedStep, setRightSidebar}:{ selectedNodes: ApNode[], applyOperation:BuilderState['applyOperation'], setRightSidebar: BuilderState['setRightSidebar'], selectedStep:BuilderState['selectedStep']})=>{
-    const disabled=selectedNodes.length===0;
+export const BuilderContextMenuContent = ({ selectedNodes, applyOperation, selectedStep, setRightSidebar }: { selectedNodes: ApNode[], applyOperation: BuilderState['applyOperation'], setRightSidebar: BuilderState['setRightSidebar'], selectedStep: BuilderState['selectedStep'] }) => {
+    const disabled = selectedNodes.length === 0;
 
-    return <>
-        {/* <BuilderContextMenuItemWrapper showTooltip={disabled}>
-        <ContextMenuItem disabled={disabled} className="flex gap-2 items-center">
-            <Copy className="w-4 h-4"></Copy> {t('Copy')}
-        </ContextMenuItem>
-        </BuilderContextMenuItemWrapper> */}
-        <BuilderContextMenuItemWrapper showTooltip={disabled}>
-        <ContextMenuItem disabled={disabled} onClick={()=>{
-            selectedNodes.forEach(node=>{
-                    if('step' in  node.data)
-                    {
-                        applyOperation({
-                            type:FlowOperationType.DELETE_ACTION,
-                            request: {
-                                name: node.data.step.name
-                            }
-                        },()=>{
-                            toast(INTERNAL_ERROR_TOAST);
-                        })
-                        if(selectedStep === node.data.step.name)
-                        {
-                            setRightSidebar(RightSideBarType.NONE);
+    return <BuilderContextMenuItemWrapper showTooltip={disabled}>
+        <ContextMenuItem disabled={disabled} onClick={() => {
+            selectedNodes.forEach(node => {
+                if ('step' in node.data) {
+                    applyOperation({
+                        type: FlowOperationType.DELETE_ACTION,
+                        request: {
+                            name: node.data.step.name
                         }
+                    }, () => {
+                        toast(INTERNAL_ERROR_TOAST);
+                    })
+                    if (selectedStep === node.data.step.name) {
+                        setRightSidebar(RightSideBarType.NONE);
                     }
+                }
             })
         }}>
             <div className="flex gap-2 items-center text-destructive">
-            <Trash className="w-4 stroke-destructive h-4"></Trash> {t('Delete')}
+                <Trash className="w-4 stroke-destructive h-4"></Trash> {t('Delete')}
             </div>
-        </ContextMenuItem> 
-        </BuilderContextMenuItemWrapper>
-        </>
+        </ContextMenuItem>
+    </BuilderContextMenuItemWrapper>
 }
