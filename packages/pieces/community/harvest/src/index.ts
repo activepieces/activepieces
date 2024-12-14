5285a6379d9d669fe068bbc0685d778bd563777a
D-Rowe-FS <142934784+D-Rowe-FS@users.noreply.github.com>
2024-12-14 01:54:43 +0000

    feat(harvest): implement get-users action

--- packages/pieces/community/harvest/src/index.ts
+++ packages/pieces/community/harvest/src/index.ts
@@ -9,6 +9,7 @@
     import { getExpenses } from './lib/actions/get-expenses';
     import { getTime_entries } from './lib/actions/get-time_entries';
     import { getRoles } from './lib/actions/get-roles';
+    import { getUsers } from './lib/actions/get-users';
 
     export const harvestAuth = PieceAuth.OAuth2({
       required: true,
@@ -24,7 +25,7 @@
       minimumSupportedRelease: '0.36.1',
       logoUrl: "https://cdn.activepieces.com/pieces/harvest.png",
       authors: ["drowe"],
-      actions: [getClients, getEstimates, getExpenses, getInvoices, getProjects, getRoles, getTasks, getTime_entries],
+      actions: [getClients, getEstimates, getExpenses, getInvoices, getProjects, getRoles, getTasks, getTime_entries, getUsers],
       triggers: [],
     });
     
