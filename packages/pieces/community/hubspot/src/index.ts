c577039282ea3edbaa6cf1e3686b9208bc40bfdd
Kishan Parmar <code.kishanparmar@gmail.com>
2024-12-22 17:33:53 +0530

    feat(hubspot): update company action

--- packages/pieces/community/hubspot/src/index.ts
+++ packages/pieces/community/hubspot/src/index.ts
@@ -32,6 +32,7 @@ import { updateProductAction } from './lib/actions/update-product';
 import { findProductAction } from './lib/actions/find-product';
 import { createCompanyAction } from './lib/actions/create-company';
 import { findCompanyAction } from './lib/actions/find-company';
+import { updateCompanyAction } from './lib/actions/update-company';
 
 export const hubspotAuth = PieceAuth.OAuth2({
 	authUrl: 'https://app.hubspot.com/oauth/authorize',
@@ -82,6 +83,7 @@ export const hubspot = createPiece({
 		getContactAction,
 		getDealAction,
 		createCompanyAction,
+		updateCompanyAction,
 		findCompanyAction,
 		getCompanyAction,
 		createTicketAction,
