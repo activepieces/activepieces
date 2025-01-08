c1b8078859883f640a6908b20685f4fcc5fc081e
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-01-08 13:51:35 +0000

    fix: saml

--- packages/server/api/src/app/ee/authentication/saml-authn/authn-sso-saml-module.ts
+++ packages/server/api/src/app/ee/authentication/saml-authn/authn-sso-saml-module.ts
@@ -1,8 +1,6 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
-import { platformMustHaveFeatureEnabled } from '../ee-authorization'
 import { authnSsoSamlController } from './authn-sso-saml-controller'
 
 export const authnSsoSamlModule: FastifyPluginAsyncTypebox = async (app) => {
-    app.addHook('preHandler', platformMustHaveFeatureEnabled((platform) => platform.ssoEnabled))
     await app.register(authnSsoSamlController, { prefix: '/v1/authn/saml' })
 }
