9659ff42cec163b2deeeb1c72f41aa47bbb7365a
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-01-12 16:56:50 +0300

    fix: simplify array zipper

--- packages/engine/src/lib/variables/processors/array-zipper.ts
+++ packages/engine/src/lib/variables/processors/array-zipper.ts
@@ -1,33 +1,27 @@
 import { isObject } from '@activepieces/shared'
 import { ProcessorFn } from './types'
 
-function getMaxArrayLength<T extends object>(props: T): number {
+function getLongestArrayLengthInObject(props: Record<string, unknown>): number {
     return Math.max(
-        ...Object.keys(props).map(key =>
-            Array.isArray(props[key as keyof T]) ? (props[key as keyof T] as unknown as Array<unknown>).length : 1,
+        ...Object.values(props).map(value =>
+            Array.isArray(value) ? value.length : 1,
         ),
     )
 }
 
-function constructResultForIndex<T>(props: T, inputsName: (keyof T)[], index: number): T {
-    const result = { ...props }
-    inputsName.forEach(input => {
-        const value = props[input];
-        (result[input] as unknown) = Array.isArray(value) ? value[index] : value
-    })
-    return result
+function constructResultForIndex(props: Record<string, unknown>, index: number): Record<string, unknown> {
+    return Object.entries(props).reduce((result, [key, value]) => {
+            result[key] = Array.isArray(value) ? value[index] : value
+            return result
+        }, {} as Record<string, unknown>)
 }
 
 export const arrayZipperProcessor: ProcessorFn = (_property, value) => {
-    if (Array.isArray(value)) {
+    if (Array.isArray(value) || !isObject(value)) {
         return value
     }
-    if (!isObject(value)) {
-        return value
-    }
-    const inputsName = Object.keys(value)
-    const maxLength = getMaxArrayLength(value)
-    return Array.from({ length: maxLength }, (_, index) =>
-        constructResultForIndex(value, inputsName, index),
+  
+    return Array.from({ length: getLongestArrayLengthInObject(value) },
+        (_, index) => constructResultForIndex(value, index),
     )
 }
