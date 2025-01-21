6d0d6caba7a17a73526542cdae31f2c7fcb1edbf
Amr abu aza <amrabuaza@gmail.com>
2025-01-21 19:46:10 +0000

    feat(move-stripe-webhook-to-public-controller): add new endpoint for public stripe callback

--- packages/server/api/src/app/ee/platform-billing/platform-billing.controller.ts
+++ packages/server/api/src/app/ee/platform-billing/platform-billing.controller.ts
@@ -1,15 +1,13 @@
 import { ApSubscriptionStatus } from '@activepieces/ee-shared'
-import { exceptionHandler } from '@activepieces/server-shared'
-import { ALL_PRINCIPAL_TYPES, assertNotNullOrUndefined, PrincipalType } from '@activepieces/shared'
+import { assertNotNullOrUndefined, PrincipalType } from '@activepieces/shared'
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
 import { Type } from '@sinclair/typebox'
 import { FastifyRequest } from 'fastify'
 import { StatusCodes } from 'http-status-codes'
-import Stripe from 'stripe'
 import { platformService } from '../../platform/platform.service'
 import { platformBillingService } from './platform-billing.service'
  
-import { stripeHelper, stripeWebhookSecret } from './stripe-helper'
+import { stripeHelper } from './stripe-helper'
 import { BillingEntityType, usageService } from './usage/usage-service'
 
 export const platformBillingController: FastifyPluginAsyncTypebox = async (fastify) => {
