a2ea1763c58ab9247cdbf1c0bfb42663d8ed8578
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-01-23 17:48:17 +0000

    fix: add billing back

--- packages/server/api/src/app/ee/platform-billing/usage/usage-service.ts
+++ packages/server/api/src/app/ee/platform-billing/usage/usage-service.ts
@@ -48,8 +48,30 @@ export const usageService = (log: FastifyBaseLogger) => ({
 
 
     async tasksExceededLimit(projectId: string): Promise<boolean> {
-        return false
+        if (![ApEdition.CLOUD, ApEdition.ENTERPRISE].includes(edition)) {
+            return false
+        }
 
+        try {
+            const projectPlan = await projectLimitsService.getPlanByProjectId(projectId)
+            if (!projectPlan) {
+                return false
+            }
+            const platformId = await projectService.getPlatformId(projectId)
+            const { consumedProjectUsage, consumedPlatformUsage } = await increaseProjectAndPlatformUsage({ projectId, incrementBy: 0, usageType: BillingUsageType.TASKS })
+            const shouldLimitFromProjectPlan = !isNil(projectPlan.tasks) && consumedProjectUsage >= projectPlan.tasks
+            const platformBilling = await platformBillingService(log).getOrCreateForPlatform(platformId)
+            const shouldLimitFromPlatformBilling = !isNil(platformBilling.tasksLimit) && consumedPlatformUsage >= platformBilling.tasksLimit
+            const platform = await platformService.getOneOrThrow(platformId)
+            if (!platform.manageProjectsEnabled) {
+                return shouldLimitFromPlatformBilling
+            }
+            return shouldLimitFromProjectPlan || shouldLimitFromPlatformBilling
+        }
+        catch (e) {
+            exceptionHandler.handle(e, log)
+            return false
+        }
     },
 
     async aiTokensExceededLimit(projectId: string, tokensToConsume: number): Promise<boolean> {
