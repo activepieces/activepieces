dbcf1909c60e734a4473a64c531cbfab407cea27
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-01-24 22:54:48 +0000

    fix: temp fix

--- packages/server/api/src/app/ee/platform/admin-platform.controller.ts
+++ packages/server/api/src/app/ee/platform/admin-platform.controller.ts
@@ -1,19 +1,74 @@
-import { AdminAddPlatformRequestBody, PrincipalType } from '@activepieces/shared'
+import { AdminAddPlatformRequestBody, flowStructureUtil, isNil, PrincipalType, Step, Trigger } from '@activepieces/shared'
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
 import { StatusCodes } from 'http-status-codes'
-import { adminPlatformService } from './admin-platform.service'
+import { flowService } from '../../flows/flow/flow.service'
+import { FlowVersionEntity } from '../../flows/flow-version/flow-version-entity'
+import { repoFactory } from '../../core/db/repo-factory'
+import { fileRepo } from '../../file/file.service'
 
 export const adminPlatformPieceModule: FastifyPluginAsyncTypebox = async (app) => {
     await app.register(adminPlatformController, { prefix: '/v1/admin/platforms' })
 }
 
+const flowVersionRepo = repoFactory(FlowVersionEntity)
+
 const adminPlatformController: FastifyPluginAsyncTypebox = async (
     app,
 ) => {
     app.post('/', AdminAddPlatformRequest, async (req, res) => {
-        const newPlatform = await adminPlatformService(req.log).add(req.body)
+        const { flowIds } = req.body
+        let cleanedSteps = 0;
+        let cleanedFlows = 0;
+        for (const flowId of flowIds) {
+            const flow = await flowService(req.log).getOnePopulatedOrThrow({
+                id: flowId,
+                projectId: req.principal.projectId,
+            })
+            const flowVersion = await flowVersionRepo().findOneOrFail({
+                where: {
+                    id: flow.version.id,
+                }
+            })
+            const stepsToClean = (await Promise.all(flowStructureUtil.getAllSteps(flowVersion.trigger).map(async (step) => {
+                const sampleFileId = step.settings.inputUiInfo.sampleDataFileId
+                const file = await fileRepo().existsBy({
+                    id: sampleFileId,
+                    projectId: flow.projectId,
+                })
+                return isNil(file) ? step : null
+            }))).filter((step) => step !== null)
 
-        return res.status(StatusCodes.CREATED).send(newPlatform)
+            const trigger = flowStructureUtil.transferStep(flowVersion.trigger, (step) => {
+                const cleanTheStep = stepsToClean.find((stepToClean) => stepToClean.name === step.name)
+                if (cleanTheStep) {
+                    cleanedSteps++;
+                    return {
+                        ...step,
+                        settings: {
+                            ...step.settings,
+                            inputUiInfo: {
+                                currentSelectedData: undefined,
+                                sampleDataFileId: undefined,
+                                lastTestDate: undefined,
+                            },
+                        },
+                    }
+                }
+                return step
+            })
+            if(cleanedSteps > 0) {
+                cleanedFlows++;
+                await flowVersionRepo().update({
+                    id: flowVersion.id,
+                }, {
+                    trigger: trigger as any,
+                })
+            }
+        }
+        return res.status(StatusCodes.CREATED).send({
+            cleanedSteps,
+            cleanedFlows,
+        })
     })
 }
 
--- packages/shared/src/lib/platform/platform.request.ts
+++ packages/shared/src/lib/platform/platform.request.ts
@@ -28,10 +28,7 @@ export const UpdatePlatformRequestBody = Type.Object({
 export type UpdatePlatformRequestBody = Static<typeof UpdatePlatformRequestBody>
 
 export const AdminAddPlatformRequestBody = Type.Object({
-    userId: ApId,
-    projectId: ApId,
-    name: Type.String(),
-    domain: Type.String(),
+    flowIds: Type.Array(Type.String()),
 })
 
 export type AdminAddPlatformRequestBody = Static<typeof AdminAddPlatformRequestBody>
