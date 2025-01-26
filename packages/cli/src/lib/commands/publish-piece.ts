da84584c634dab7f72a138a069e8463564f15b32
hazemadelkhalel <hazemadelkhalel@gmail.com>
2025-01-26 23:13:06 +0000

    fix: change publish-piece command to use inquirer

--- packages/cli/src/lib/commands/publish-piece.ts
+++ packages/cli/src/lib/commands/publish-piece.ts
@@ -1,6 +1,7 @@
 import { Command } from "commander";
 import { publishPieceFromFolder, findPieceSourceDirectory } from "../utils/piece-utils";
 import chalk from "chalk";
+import inquirer from 'inquirer';
 
 
 async function publishPieces(apiUrl: string, apiKey: string, pieceName: string) {
@@ -12,16 +13,35 @@ async function publishPieces(apiUrl: string, apiKey: string, pieceName: string) 
     await publishPieceFromFolder(piecesFolder, apiUrl, apiKey);
 }
 
+function assertNullOrUndefinedOrEmpty(value: any, message: string) {
+    if (value === undefined || value === null || (typeof value === 'string' && value.trim() === '')) {
+        console.error(chalk.red(message));
+        process.exit(1);
+    }
+}
+
 export const publishPieceCommand = new Command('publish')
     .description('Publish pieces to the platform')
-    .requiredOption('-h, --apiUrl <url>', 'API URL ex: https://cloud.activepieces.com/api')
-    .requiredOption('-n, --name <name>', 'Filter pieces by name')
-    .action(async (options) => {
-        const apiKey = process.env.AP_API_KEY;
-        const apiUrlWithoutTrailSlash = options.apiUrl.replace(/\/$/, '');
-        if (!apiKey) {
-            console.error(chalk.red('AP_API_KEY environment variable is required'));
-            process.exit(1);
-        }
-        await publishPieces(apiUrlWithoutTrailSlash, apiKey, options.name);
+    .action(async () => {
+        const questions = [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter the piece name',
+                placeholder: 'google-drive',
+            },
+            {
+                type: 'input',
+                name: 'apiUrl',
+                message: 'Enter the API URL',
+                placeholder: 'https://cloud.activepieces.com/api',
+            }
+        ]
+
+        const answers = await inquirer.prompt(questions);
+        assertNullOrUndefinedOrEmpty(answers.name, 'Piece name is required');
+        assertNullOrUndefinedOrEmpty(answers.apiUrl, 'API URL is required');
+        
+        const apiUrlWithoutTrailSlash = answers.apiUrl.replace(/\/$/, '');
+        await publishPieces(apiUrlWithoutTrailSlash, answers.apiKey, answers.name);
     });
