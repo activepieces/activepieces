c1865b857ca66ce241cedb0d1b502fe78a0bc550
Kishan Parmar <code.kishanparmar@gmail.com>
2025-02-05 14:11:36 +0530

    feat: new document trigger

--- packages/pieces/community/google-docs/src/lib/common/props.ts
+++ packages/pieces/community/google-docs/src/lib/common/props.ts
@@ -1,44 +1,55 @@
-import { googleDocsAuth } from "../../index";
-import { DropdownOption, PiecePropValueSchema, Property } from "@activepieces/pieces-framework";
-import { google } from 'googleapis';
+import { googleDocsAuth } from '../../index';
+import { DropdownOption, PiecePropValueSchema, Property } from '@activepieces/pieces-framework';
+import { google, drive_v3 } from 'googleapis';
 import { OAuth2Client } from 'googleapis-common';
 
 export const folderIdProp = Property.Dropdown({
-    displayName:'Folder',
-    refreshers:[],
-    required:false,
-    async options({auth}) {
-        if(!auth)
-        {
-            return{
-                disabled:true,
-                placeholder:'Please connect to your Google Drive account.'
-            }
-        }
-        const authValue = auth as PiecePropValueSchema<typeof googleDocsAuth>;
+	displayName: 'Folder',
+	refreshers: [],
+	required: false,
+	options: async ({ auth }) => {
+		if (!auth) {
+			return {
+				disabled: true,
+				placeholder: 'Please connect to your Google Drive account.',
+				options: [],
+			};
+		}
+		const authValue = auth as PiecePropValueSchema<typeof googleDocsAuth>;
 
-        const authClient = new OAuth2Client();
-        authClient.setCredentials(authValue);
+		const authClient = new OAuth2Client();
+		authClient.setCredentials(authValue);
 
-        const drive = google.drive({ version: 'v3', auth: authClient });
+		const drive = google.drive({ version: 'v3', auth: authClient });
 
-        const options :DropdownOption<string>[] = [];
+		const options: DropdownOption<string>[] = [];
 
-        let nextPageToken;
+		let nextPageToken;
 
-        do
-        {
-            const response = await drive.files.list({
-                q:"mimeType='application/vnd.google-apps.folder' and trashed = false",
-                supportsAllDrives:true,
-                includeItemsFromAllDrives:true,
-                pageToken:nextPageToken
-            });
+		do {
+			const response: any = await drive.files.list({
+				q: "mimeType='application/vnd.google-apps.folder' and trashed = false",
+				supportsAllDrives: true,
+                orderBy:'createdTime desc',
+				includeItemsFromAllDrives: true,
+				pageToken: nextPageToken,
+			});
 
-            
+			const fileList: drive_v3.Schema$FileList = response.data;
 
-            nextPageToken = response.data.nextPageToken;
-        }while(nextPageToken)
-        
-    },
-})
+			if (fileList.files) {
+				for (const file of fileList.files)
+					options.push({
+						label: file.name!,
+						value: file.id!,
+					});
+			}
+			nextPageToken = response.data.nextPageToken;
+		} while (nextPageToken);
+
+		return {
+			disabled: false,
+			options,
+		};
+	},
+});
