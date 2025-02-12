155808bb8f886910545ba1753040a9952c7d3c0d
Amr abu aza <amrabuaza@gmail.com>
2025-02-12 17:23:47 +0000

    feat(remove-current-user-from-localstorage): remove current user from localstorage and apply changes

--- packages/react-ui/src/app/components/header.tsx
+++ packages/react-ui/src/app/components/header.tsx
@@ -8,7 +8,7 @@ import { PlatformSwitcher } from '@/features/platform-switcher/components/platfo
 import { ProjectSwitcher } from '@/features/projects/components/project-switcher';
 import { InviteUserDialog } from '@/features/team/component/invite-user-dialog';
 import { useShowPlatformAdminDashboard } from '@/hooks/authorization-hooks';
-import { authenticationSession } from '@/lib/authentication-session';
+import { userHooks } from '@/hooks/user-hooks';
 import { PlatformRole } from '@activepieces/shared';
 
 import { useEmbedding } from '../../components/embed-provider';
@@ -17,14 +17,13 @@ import { notificationHooks } from '../routes/platform/notifications/hooks/notifi
 import { PlatformDialog } from '../routes/platform/notifications/paltform-dialog';
 
 import UsageLimitsButton from './usage-limits-button';
-
 export const Header = () => {
   const history = useLocation();
   const isInPlatformAdmin = history.pathname.startsWith('/platform');
   const showPlatformAdminDashboard = useShowPlatformAdminDashboard();
   const { embedState } = useEmbedding();
   const messages = notificationHooks.useNotifications();
-  const platformRole = authenticationSession.getUserPlatformRole();
+  const platformRole = userHooks.getCurrentUserPlatformRole();
 
   return (
     !embedState.isEmbedded && (
