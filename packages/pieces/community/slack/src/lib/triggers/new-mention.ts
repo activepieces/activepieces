90b889984a8b52c3e76ca6855d779136b8c5ec82
Kishan Parmar <code.kishanparmar@gmail.com>
2025-02-19 11:04:10 +0530

    fix: fix empty ts messages

--- packages/pieces/community/slack/src/lib/triggers/new-mention.ts
+++ packages/pieces/community/slack/src/lib/triggers/new-mention.ts
@@ -1,165 +1,157 @@
 import {
-  OAuth2PropertyValue,
-  Property,
-  TriggerStrategy,
-  createTrigger,
+	OAuth2PropertyValue,
+	Property,
+	TriggerStrategy,
+	createTrigger,
 } from '@activepieces/pieces-framework';
 import { getChannels, multiSelectChannelInfo, userId } from '../common/props';
 import { slackAuth } from '../../';
 import { WebClient } from '@slack/web-api';
 import { getFirstFiveOrAll } from '../common/utils';
+import { isNil } from '@activepieces/shared';
 
 const sampleData = {
-  client_msg_id: '2767cf34-0651-44e0-b9c8-1b167ce9b7a9',
-  type: 'message',
-  text: 'heeeelllo\n<@U07BN652T52>',
-  user: 'U037UG6FKPU',
-  ts: '1678231735.586539',
-  blocks: [
-    {
-      type: 'rich_text',
-      block_id: 'jCFSh',
-      elements: [
-        {
-          type: 'rich_text_section',
-          elements: [
-            {
-              type: 'text',
-              text: 'heeeelllo\n',
-            },
-            {
-              type: 'user',
-              user_id: 'U07BN652T52',
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  team: 'T037MS4FGDC',
-  channel: 'C037RTX2ZDM',
-  event_ts: '1678231735.586539',
-  channel_type: 'channel',
+	client_msg_id: '2767cf34-0651-44e0-b9c8-1b167ce9b7a9',
+	type: 'message',
+	text: 'heeeelllo\n<@U07BN652T52>',
+	user: 'U037UG6FKPU',
+	ts: '1678231735.586539',
+	blocks: [
+		{
+			type: 'rich_text',
+			block_id: 'jCFSh',
+			elements: [
+				{
+					type: 'rich_text_section',
+					elements: [
+						{
+							type: 'text',
+							text: 'heeeelllo\n',
+						},
+						{
+							type: 'user',
+							user_id: 'U07BN652T52',
+						},
+					],
+				},
+			],
+		},
+	],
+	team: 'T037MS4FGDC',
+	channel: 'C037RTX2ZDM',
+	event_ts: '1678231735.586539',
+	channel_type: 'channel',
 };
 
 export const newMention = createTrigger({
-  auth: slackAuth,
-  name: 'new_mention',
-  displayName: 'New Mention',
-  description: 'Triggers when a username is mentioned.',
-  props: {
-    info: multiSelectChannelInfo,
-    user: userId,
-    channels: Property.MultiSelectDropdown({
-      displayName: 'Channels',
-      description:
-        'If no channel is selected, the flow will be triggered for username mentions in all channels',
-      required: false,
-      refreshers: [],
-      async options({ auth }) {
-        if (!auth) {
-          return {
-            disabled: true,
-            placeholder: 'connect slack account',
-            options: [],
-          };
-        }
-        const authentication = auth as OAuth2PropertyValue;
-        const accessToken = authentication['access_token'];
-        const channels = await getChannels(accessToken);
-        return {
-          disabled: false,
-          placeholder: 'Select channel',
-          options: channels,
-        };
-      },
-    }),
-    ignoreBots: Property.Checkbox({
-      displayName: 'Ignore Bot Messages ?',
-      required: true,
-      defaultValue: false,
-    }),
-  },
-  type: TriggerStrategy.APP_WEBHOOK,
-  sampleData: sampleData,
-  onEnable: async (context) => {
-    // Older OAuth2 has team_id, newer has team.id
-    const teamId =
-      context.auth.data['team_id'] ?? context.auth.data['team']['id'];
-    context.app.createListeners({
-      events: ['message'],
-      identifierValue: teamId,
-    });
-  },
-  onDisable: async (context) => {
-    // Ignored
-  },
+	auth: slackAuth,
+	name: 'new_mention',
+	displayName: 'New Mention',
+	description: 'Triggers when a username is mentioned.',
+	props: {
+		info: multiSelectChannelInfo,
+		user: userId,
+		channels: Property.MultiSelectDropdown({
+			displayName: 'Channels',
+			description:
+				'If no channel is selected, the flow will be triggered for username mentions in all channels',
+			required: false,
+			refreshers: [],
+			async options({ auth }) {
+				if (!auth) {
+					return {
+						disabled: true,
+						placeholder: 'connect slack account',
+						options: [],
+					};
+				}
+				const authentication = auth as OAuth2PropertyValue;
+				const accessToken = authentication['access_token'];
+				const channels = await getChannels(accessToken);
+				return {
+					disabled: false,
+					placeholder: 'Select channel',
+					options: channels,
+				};
+			},
+		}),
+		ignoreBots: Property.Checkbox({
+			displayName: 'Ignore Bot Messages ?',
+			required: true,
+			defaultValue: false,
+		}),
+	},
+	type: TriggerStrategy.APP_WEBHOOK,
+	sampleData: sampleData,
+	onEnable: async (context) => {
+		// Older OAuth2 has team_id, newer has team.id
+		const teamId = context.auth.data['team_id'] ?? context.auth.data['team']['id'];
+		context.app.createListeners({
+			events: ['message'],
+			identifierValue: teamId,
+		});
+	},
+	onDisable: async (context) => {
+		// Ignored
+	},
 
-  test: async (context) => {
-    const channels = context.propsValue.channels as string[];
+	test: async (context) => {
+		const channels = context.propsValue.channels as string[];
 
-    if (!channels || (Array.isArray(channels) && channels.length === 0)) {
-      return [sampleData];
-    }
-    const client = new WebClient(context.auth.access_token);
-    const response = await client.conversations.history({
-      channel: channels[0],
-      limit: 100,
-      
-    });
-    if (!response.messages) {
-      return [];
-    }
-    const messages =  response.messages
-      .filter(
-        (message) =>
-          message.text && message.text.includes(`<@${context.propsValue.user}>`)
-      )
-      .filter((message) => !(context.propsValue.ignoreBots && message.bot_id))
-      .map((message) => {
-        return {
-          ...message,
-          channel: channels[0],
-          event_ts: '1678231735.586539',
-          channel_type: 'channel',
-        };
-      }).sort((a,b)=>{
-        const tsA = a.ts ? parseFloat(a.ts) : Number.MAX_VALUE;
-        const tsB = b.ts ? parseFloat(b.ts) : Number.MAX_VALUE;
-        return tsB - tsA;
-      });
+		if (!channels || (Array.isArray(channels) && channels.length === 0)) {
+			return [sampleData];
+		}
+		const client = new WebClient(context.auth.access_token);
+		const response = await client.conversations.history({
+			channel: channels[0],
+			limit: 100,
+		});
+		if (!response.messages) {
+			return [];
+		}
+		const messages = response.messages
+			.filter((message) => !isNil(message.ts))
+			.filter((message) => message.text && message.text.includes(`<@${context.propsValue.user}>`))
+			.filter((message) => !(context.propsValue.ignoreBots && message.bot_id))
+			.map((message) => {
+				return {
+					...message,
+					channel: channels[0],
+					event_ts: '1678231735.586539',
+					channel_type: 'channel',
+				};
+			})
+			.sort((a, b) => parseFloat(b.ts!) - parseFloat(a.ts!));
 
-    return getFirstFiveOrAll(messages);
+		return getFirstFiveOrAll(messages);
+	},
 
+	run: async (context) => {
+		const payloadBody = context.payload.body as PayloadBody;
+		const channels = context.propsValue.channels as string[];
 
-  },
+		if (channels.length === 0 || channels.includes(payloadBody.event.channel)) {
+			// check for bot messages
+			if (context.propsValue.ignoreBots && payloadBody.event.bot_id) {
+				return [];
+			}
+			// check for mention
+			if (
+				context.propsValue.user &&
+				payloadBody.event.text?.includes(`<@${context.propsValue.user}>`)
+			) {
+				return [payloadBody.event];
+			}
+		}
 
-  run: async (context) => {
-    const payloadBody = context.payload.body as PayloadBody;
-    const channels = context.propsValue.channels as string[];
-
-    if (channels.length === 0 || channels.includes(payloadBody.event.channel)) {
-      // check for bot messages
-      if (context.propsValue.ignoreBots && payloadBody.event.bot_id) {
-        return [];
-      }
-      // check for mention
-      if (
-        context.propsValue.user &&
-        payloadBody.event.text?.includes(`<@${context.propsValue.user}>`)
-      ) {
-        return [payloadBody.event];
-      }
-    }
-
-    return [];
-  },
+		return [];
+	},
 });
 
 type PayloadBody = {
-  event: {
-    channel: string;
-    bot_id?: string;
-    text?: string;
-  };
+	event: {
+		channel: string;
+		bot_id?: string;
+		text?: string;
+	};
 };
--- packages/pieces/community/slack/src/lib/triggers/new-message.ts
+++ packages/pieces/community/slack/src/lib/triggers/new-message.ts
@@ -1,122 +1,113 @@
-import {
-  Property,
-  TriggerStrategy,
-  createTrigger,
-} from '@activepieces/pieces-framework';
+import { Property, TriggerStrategy, createTrigger } from '@activepieces/pieces-framework';
 import { singleSelectChannelInfo, slackChannel } from '../common/props';
 import { slackAuth } from '../../';
 import { WebClient } from '@slack/web-api';
 import { getFirstFiveOrAll } from '../common/utils';
+import { isNil } from '@activepieces/shared';
 
 const sampleData = {
-  client_msg_id: '2767cf34-0651-44e0-b9c8-1b167ce9b7a9',
-  type: 'message',
-  text: 'f',
-  user: 'U037UG6FKPU',
-  ts: '1678231735.586539',
-  blocks: [
-    {
-      type: 'rich_text',
-      block_id: '4CM',
-      elements: [
-        {
-          type: 'rich_text_section',
-          elements: [
-            {
-              type: 'text',
-              text: 'f',
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  team: 'T037MS4FGDC',
-  channel: 'C037RTX2ZDM',
-  event_ts: '1678231735.586539',
-  channel_type: 'channel',
+	client_msg_id: '2767cf34-0651-44e0-b9c8-1b167ce9b7a9',
+	type: 'message',
+	text: 'f',
+	user: 'U037UG6FKPU',
+	ts: '1678231735.586539',
+	blocks: [
+		{
+			type: 'rich_text',
+			block_id: '4CM',
+			elements: [
+				{
+					type: 'rich_text_section',
+					elements: [
+						{
+							type: 'text',
+							text: 'f',
+						},
+					],
+				},
+			],
+		},
+	],
+	team: 'T037MS4FGDC',
+	channel: 'C037RTX2ZDM',
+	event_ts: '1678231735.586539',
+	channel_type: 'channel',
 };
 
 export const newMessage = createTrigger({
-  auth: slackAuth,
-  name: 'new_message',
-  displayName: 'New Message',
-  description: 'Triggers when a new message is received',
-  props: {
-    info: singleSelectChannelInfo,
-    channel: slackChannel(false),
-    ignoreBots: Property.Checkbox({
-      displayName: 'Ignore Bot Messages ?',
-      required: true,
-      defaultValue: false,
-    }),
-  },
-  type: TriggerStrategy.APP_WEBHOOK,
-  sampleData: sampleData,
-  onEnable: async (context) => {
-    // Older OAuth2 has team_id, newer has team.id
-    const teamId =
-      context.auth.data['team_id'] ?? context.auth.data['team']['id'];
-    context.app.createListeners({
-      events: ['message'],
-      identifierValue: teamId,
-    });
-  },
-  onDisable: async (context) => {
-    // Ignored
-  },
+	auth: slackAuth,
+	name: 'new_message',
+	displayName: 'New Message',
+	description: 'Triggers when a new message is received',
+	props: {
+		info: singleSelectChannelInfo,
+		channel: slackChannel(false),
+		ignoreBots: Property.Checkbox({
+			displayName: 'Ignore Bot Messages ?',
+			required: true,
+			defaultValue: false,
+		}),
+	},
+	type: TriggerStrategy.APP_WEBHOOK,
+	sampleData: sampleData,
+	onEnable: async (context) => {
+		// Older OAuth2 has team_id, newer has team.id
+		const teamId = context.auth.data['team_id'] ?? context.auth.data['team']['id'];
+		context.app.createListeners({
+			events: ['message'],
+			identifierValue: teamId,
+		});
+	},
+	onDisable: async (context) => {
+		// Ignored
+	},
 
-  test: async (context) => {
-    if (!context.propsValue.channel) {
-      return [sampleData];
-    }
-    const client = new WebClient(context.auth.access_token);
-    const response = await client.conversations.history({
-      channel: context.propsValue.channel,
-      limit: 100,
-    });
+	test: async (context) => {
+		if (!context.propsValue.channel) {
+			return [sampleData];
+		}
+		const client = new WebClient(context.auth.access_token);
+		const response = await client.conversations.history({
+			channel: context.propsValue.channel,
+			limit: 100,
+		});
 
-    if (!response.messages) {
-      return [];
-    }
-    const messages =  response.messages
-      .filter((message) => !(context.propsValue.ignoreBots && message.bot_id))
-      .map((message) => {
-        return {
-          ...message,
-          channel: context.propsValue.channel,
-          event_ts: '1678231735.586539',
-          channel_type: 'channel',
-        };
-      }).sort((a,b)=>{
-        const tsA = a.ts ? parseFloat(a.ts) : Number.MAX_VALUE;
-        const tsB = b.ts ? parseFloat(b.ts) : Number.MAX_VALUE;
-        return tsB - tsA;
-      });
+		if (!response.messages) {
+			return [];
+		}
+		const messages = response.messages
+			.filter((message) => !isNil(message.ts))
+			.filter((message) => !(context.propsValue.ignoreBots && message.bot_id))
+			.map((message) => {
+				return {
+					...message,
+					channel: context.propsValue.channel,
+					event_ts: '1678231735.586539',
+					channel_type: 'channel',
+				};
+			})
+			.sort((a, b) => parseFloat(b.ts!) - parseFloat(a.ts!));
 
-    return getFirstFiveOrAll(messages);
-  },
+		return getFirstFiveOrAll(messages);
+	},
 
-  run: async (context) => {
-    const payloadBody = context.payload.body as PayloadBody;
-    if (
-      !context.propsValue.channel ||
-      payloadBody.event.channel === context.propsValue.channel
-    ) {
-      // check for bot messages
-      if (context.propsValue.ignoreBots && payloadBody.event.bot_id) {
-        return [];
-      }
-      return [payloadBody.event];
-    }
+	run: async (context) => {
+		const payloadBody = context.payload.body as PayloadBody;
+		if (!context.propsValue.channel || payloadBody.event.channel === context.propsValue.channel) {
+			// check for bot messages
+			if (context.propsValue.ignoreBots && payloadBody.event.bot_id) {
+				return [];
+			}
+			return [payloadBody.event];
+		}
 
-    return [];
-  },
+		return [];
+	},
 });
 
 type PayloadBody = {
-  event: {
-    channel: string;
-    bot_id?: string;
-  };
+	event: {
+		channel: string;
+		bot_id?: string;
+	};
 };
