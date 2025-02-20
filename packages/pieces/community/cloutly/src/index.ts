6b3ecc06e6fb884466d04983db06576254e573eb
Kishan Parmar <code.kishanparmar@gmail.com>
2025-02-20 12:44:23 +0530

    fix: fix logo, add custom api action

--- packages/pieces/community/cloutly/src/index.ts
+++ packages/pieces/community/cloutly/src/index.ts
@@ -1,6 +1,7 @@
-
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-import { sendReviewInvite } from "./lib/actions/send-review-invite";
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { sendReviewInvite } from './lib/actions/send-review-invite';
+import { PieceCategory } from '@activepieces/shared';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
 
 export const cloutlyAuth = PieceAuth.SecretText({
   displayName: 'API Key',
@@ -9,11 +10,25 @@ export const cloutlyAuth = PieceAuth.SecretText({
 });
 
 export const cloutly = createPiece({
-  displayName: "Cloutly Reviews",
+  displayName: 'Cloutly',
+  description: 'Review Management Tool',
   auth: cloutlyAuth,
   minimumSupportedRelease: '0.36.1',
-  logoUrl: "https://app.cloutly.com/assets/images/icon-cloutly.svg",
+  logoUrl: 'https://cdn.activepieces.com/pieces/cloutly.svg',
+  categories: [PieceCategory.MARKETING],
   authors: ['joshuaheslin'],
-  actions: [sendReviewInvite],
+  actions: [
+    sendReviewInvite,
+    createCustomApiCallAction({
+      baseUrl: () => {
+        return 'https://app.cloutly.com/api/v1';
+      },
+      auth: cloutlyAuth,
+      authMapping: async (auth) => ({
+        'x-app': 'activepieces',
+        'x-api-key': auth as string,
+      }),
+    }),
+  ],
   triggers: [],
 });
