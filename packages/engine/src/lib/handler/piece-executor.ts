5510961508e405d483e17cbd57fd11ce1f276d8f
hazemadelkhalel <hazemadelkhalel@gmail.com>
2025-03-02 13:17:23 +0000

    feat: add respond piece framework and return response action

--- packages/engine/src/lib/handler/piece-executor.ts
+++ packages/engine/src/lib/handler/piece-executor.ts
@@ -1,5 +1,5 @@
 import { URL } from 'url'
-import { ActionContext, ConnectionsManager, PauseHook, PauseHookParams, PiecePropertyMap, StaticPropsValue, StopHook, StopHookParams, TagsManager } from '@activepieces/pieces-framework'
+import { ActionContext, ConnectionsManager, PauseHook, PauseHookParams, PiecePropertyMap, RespondHook, RespondHookParams, StaticPropsValue, StopHook, StopHookParams, TagsManager } from '@activepieces/pieces-framework'
 import { ActionType, assertNotNullOrUndefined, AUTHENTICATION_PROPERTY_NAME, ExecutionType, FlowRunStatus, GenericStepOutput, isNil, PauseType, PieceAction, StepOutputStatus } from '@activepieces/shared'
 import dayjs from 'dayjs'
 import { continueIfFailureHandler, handleExecutionError, runWithExponentialBackoff } from '../helper/error-handling'
@@ -13,7 +13,7 @@ import { propsProcessor } from '../variables/props-processor'
 import { ActionHandler, BaseExecutor } from './base-executor'
 import { ExecutionVerdict } from './context/flow-execution-context'
 
-type HookResponse = { stopResponse: StopHookParams | undefined, pauseResponse: PauseHookParams | undefined, tags: string[], stopped: boolean, paused: boolean }
+type HookResponse = { stopResponse: StopHookParams | undefined, pauseResponse: PauseHookParams | undefined, respondResponse: RespondHookParams | undefined, tags: string[], stopped: boolean, paused: boolean }
 
 const AP_PAUSED_FLOW_TIMEOUT_DAYS = Number(process.env.AP_PAUSED_FLOW_TIMEOUT_DAYS)
 
@@ -65,6 +65,7 @@ const executeAction: ActionHandler<PieceAction> = async ({ action, executionStat
             pauseResponse: undefined,
             paused: false,
             tags: [],
+            respondResponse: undefined,
         }
         const isPaused = executionState.isPaused({ stepName: action.name })
         const context: ActionContext = {
@@ -110,6 +111,7 @@ const executeAction: ActionHandler<PieceAction> = async ({ action, executionStat
                 id: constants.flowRunId,
                 stop: createStopHook(hookResponse),
                 pause: createPauseHook(hookResponse, executionState.pauseRequestId),
+                respond: createRespondHook(hookResponse),
             },
             project: {
                 id: constants.projectId,
@@ -187,6 +189,12 @@ function createStopHook(hookResponse: HookResponse): StopHook {
     return (req?: StopHookParams) => {
         hookResponse.stopped = true
         hookResponse.stopResponse = req ?? { response: {} }
+    }
+}
+
+function createRespondHook(hookResponse: HookResponse): RespondHook {
+    return (req?: RespondHookParams) => {
+        hookResponse.respondResponse = req ?? { response: {} }
     }
 }
 
