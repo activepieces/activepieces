61da917e837d0ce99faf40d749bd02fe6fbfbe35
hazemadelkhalel <hazemadelkhalel@gmail.com>
2025-03-04 12:03:48 +0000

    revert: remove respond from webhook piece

--- packages/pieces/community/webhook/src/lib/actions/return-response.ts
+++ packages/pieces/community/webhook/src/lib/actions/return-response.ts
@@ -85,22 +85,10 @@ export const returnResponse = createAction({
         return fields;
       },
     }),
-    respond: Property.StaticDropdown({
-      displayName: 'Flow Execution',
-      required: false,
-      defaultValue: 'stop',
-      options: {
-        disabled: false,
-        options: [
-          { label: 'Stop', value: 'stop' },
-          { label: 'Respond and Continue', value: 'respond' },
-        ],
-      },
-    }),
   },
 
   async run(context) {
-    const { fields, responseType, respond } = context.propsValue;
+    const { fields, responseType } = context.propsValue;
     const bodyInput = fields ['body'];
     const headers = fields['headers'];
     const status = fields['status'];
@@ -123,15 +111,9 @@ export const returnResponse = createAction({
         break;
     }
     
-    if (respond === 'respond') {
-      context.run.respond({
-        response: response,
-      });
-    } else {
-      context.run.stop({
-        response: response,
-      });
-    }
+    context.run.stop({
+      response: response,
+    });
     return response;
   },
 });
