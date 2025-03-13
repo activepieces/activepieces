2cca1e3c995f169cf1de70ec6256e676bd16e85d
Wunna <mgwunna.mw@icloud.com>
2025-03-13 10:56:41 +0630

    fix: 🐛 condition order wrong in get api endpoint utilty

--- packages/pieces/community/returning-ai/src/utilities.ts
+++ packages/pieces/community/returning-ai/src/utilities.ts
@@ -1,12 +1,12 @@
 export function getApiEndpoint(authToken?: string) {
   if (!authToken) return 'https://integration.genesiv.com';
-  
+
   if (authToken.includes('local:')) {
     return 'http://localhost:3333';
-  } else if (authToken.includes('staging:')) {
-    return 'https://staging-integration.genesiv.org';
   } else if (authToken.includes('pre-staging:')) {
     return 'https://sgtr-integration.genesiv.org';
+  } else if (authToken.includes('staging:')) {
+    return 'https://staging-integration.genesiv.org';
   } else if (authToken.includes('playground:')) {
     return 'https://playground-integration.genesiv.org';
   } else {
