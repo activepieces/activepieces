6229263a10a04391c7f15e3dc1e95832b321dc2f
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-03-28 03:44:03 +0000

    fix: tab style

--- packages/react-ui/src/components/ui/tabs.tsx
+++ packages/react-ui/src/components/ui/tabs.tsx
@@ -23,7 +23,7 @@ const tabsTriggerVariants = cva('inline-flex items-center justify-center', {
   variants: {
     variant: {
       default:
-        'whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+        'whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
       outline:
         'px-3 py-1.5 text-sm font-medium ring-offset-background transition-all border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:text-foreground text-muted-foreground',
     },
@@ -71,7 +71,7 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      'mt-5 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+      'mt-5 ring-offset-background focus-visible:outline-none',
       className,
     )}
     {...props}
