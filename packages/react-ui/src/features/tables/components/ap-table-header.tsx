365065bb9eb7991ee14ac3bbf3d2f544cd7b2b1a
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-04-13 15:25:38 +0300

    chore: show actions button always instead of showing it only when a record is selected

--- packages/react-ui/src/features/tables/components/ap-table-header.tsx
+++ packages/react-ui/src/features/tables/components/ap-table-header.tsx
@@ -1,5 +1,5 @@
 import { t } from 'i18next';
-import { RefreshCw, Trash2 } from 'lucide-react';
+import { ChevronDown, DownloadIcon, RefreshCw, Trash2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
@@ -8,13 +8,14 @@ import { Button } from '@/components/ui/button';
 import { HomeButton } from '@/components/ui/home-button';
 import { PermissionNeededTooltip } from '@/components/ui/permission-needed-tooltip';
 import { useAuthorization } from '@/hooks/authorization-hooks';
-import { cn, NEW_TABLE_QUERY_PARAM } from '@/lib/utils';
+import { NEW_TABLE_QUERY_PARAM } from '@/lib/utils';
 import { Permission } from '@activepieces/shared';
 
 import ApTableName from './ap-table-name';
 import { useTableState } from './ap-table-state-provider';
-import { ExportCsvButton } from './export-csv-button';
 import { ImportCsvDialog } from './import-csv-dialog';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { tablesUtils } from '../lib/utils';
 
 type ApTableHeaderProps = {
   isFetchingNextPage: boolean;
@@ -26,12 +27,16 @@ const ApTableHeader = ({ isFetchingNextPage }: ApTableHeaderProps) => {
     setSelectedRecords,
     deleteRecords,
     records,
+    table,
+    fields
   ] = useTableState((state) => [
     state.isSaving,
     state.selectedRecords,
     state.setSelectedRecords,
     state.deleteRecords,
     state.records,
+    state.table,
+    state.fields
   ]);
   const [searchParams] = useSearchParams();
   const userHasTableWritePermission = useAuthorization().checkAccess(
@@ -43,7 +48,14 @@ const ApTableHeader = ({ isFetchingNextPage }: ApTableHeaderProps) => {
     setIsEditingTableName(searchParams.get(NEW_TABLE_QUERY_PARAM) === 'true');
   }, []);
 
-  const tableData = useTableState((state) => state.table);
+ const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const exportRecords = () => {
+    tablesUtils.exportRecords({
+      tableName: table.name,
+      records: records,
+      fields: fields,
+    })
+  }
   return (
     <div className="flex flex-col gap-4 flex-none px-4">
       <div className="flex items-center justify-between">
@@ -52,7 +64,7 @@ const ApTableHeader = ({ isFetchingNextPage }: ApTableHeaderProps) => {
             <HomeButton showBackButton={true} route={'/tables'}></HomeButton>
 
             <ApTableName
-              tableName={tableData?.name ?? ''}
+              tableName={table.name}
               isEditingTableName={isEditingTableName}
               setIsEditingTableName={setIsEditingTableName}
             />
@@ -74,39 +86,67 @@ const ApTableHeader = ({ isFetchingNextPage }: ApTableHeaderProps) => {
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
             <ImportCsvDialog />
-            <ExportCsvButton />
             <div onClick={(e) => e.stopPropagation()}>
               <PermissionNeededTooltip
                 hasPermission={userHasTableWritePermission}
               >
-                <ConfirmationDeleteDialog
-                  title={t('Delete Records')}
-                  message={t(
-                    'Are you sure you want to delete the selected records? This action cannot be undone.',
-                  )}
-                  entityName={
-                    selectedRecords.size === 1 ? t('record') : t('records')
-                  }
-                  mutationFn={async () => {
-                    const indices = Array.from(selectedRecords).map((row) =>
-                      records.findIndex((r) => r.uuid === row),
-                    );
-                    deleteRecords(indices.map((index) => index.toString()));
-                    setSelectedRecords(new Set());
-                  }}
-                >
+               <DropdownMenu  open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+                <DropdownMenuTrigger asChild>
                   <Button
-                    size="sm"
-                    className={cn({
-                      hidden: selectedRecords.size === 0,
-                    })}
-                    variant="destructive"
-                    disabled={!userHasTableWritePermission}
-                  >
-                    <Trash2 className="mr-2 w-4" />
-                    {`${t('Delete')} (${selectedRecords.size})`}
-                  </Button>
-                </ConfirmationDeleteDialog>
+                      size="sm"
+                      className='flex gap-2 items-center'
+                      disabled={!userHasTableWritePermission}
+                    >
+                      {`${t('Actions')}`}
+                      <ChevronDown className="w-4" />
+                    </Button>
+                    
+
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align='end'>
+                      <>
+                      <DropdownMenuItem onSelect={()=> {
+                        exportRecords()
+                      }}
+                      disabled={selectedRecords.size === 0}
+                      className='flex gap-2 items-center'
+                      >
+                        <DownloadIcon className='size-4'></DownloadIcon>
+                        {t('Export Records')} {selectedRecords.size > 0 ? `(${selectedRecords.size})` : ''}
+                      </DropdownMenuItem>
+                    <ConfirmationDeleteDialog
+                    title={t('Delete Records')}
+                    message={t(
+                      'Are you sure you want to delete the selected records? This action cannot be undone.',
+                    )}
+                    entityName={
+                      selectedRecords.size === 1 ? t('record') : t('records')
+                    }
+                    mutationFn={async () => {
+                      const indices = Array.from(selectedRecords).map((row) =>
+                        records.findIndex((r) => r.uuid === row),
+                      );
+                      deleteRecords(indices.map((index) => index.toString()));
+                      setSelectedRecords(new Set());
+                      setIsMenuOpen(false)
+
+                    }}
+                    >
+                      <DropdownMenuItem className='text-destructive flex gap-2 items-center' onSelect={(e)=> {
+                        e.preventDefault();
+                        e.stopPropagation();
+                      }} disabled={selectedRecords.size === 0}> 
+                        <Trash2 className='size-4'></Trash2>
+                        {t('Delete Records')} {selectedRecords.size > 0 ? `(${selectedRecords.size})` : ''}
+                      </DropdownMenuItem>
+                    </ConfirmationDeleteDialog>
+                      </>
+                 
+
+                    </DropdownMenuContent>
+               </DropdownMenu>
+               
+                
               </PermissionNeededTooltip>
             </div>
           </div>
@@ -119,3 +159,6 @@ const ApTableHeader = ({ isFetchingNextPage }: ApTableHeaderProps) => {
 ApTableHeader.displayName = 'ApTableHeader';
 
 export default ApTableHeader;
+
+
+
