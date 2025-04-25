6fd2373d6d095a7749acfbca334e0fd3ed0bdbd6
Hazem Adel <hazemadelkhalel@gmail.com>
2025-04-25 00:51:49 +0000

    fix: remove unnecessary code

--- packages/server/api/src/app/database/migration/postgres/1745530653784-AddConnectionIdsToFlowVersion.ts
+++ packages/server/api/src/app/database/migration/postgres/1745530653784-AddConnectionIdsToFlowVersion.ts
@@ -1,40 +1,5 @@
-import { flowStructureUtil, FlowVersion } from '@activepieces/shared'
+import { flowStructureUtil } from '@activepieces/shared'
 import { MigrationInterface, QueryRunner } from 'typeorm'
-
-
-function extractConnectionIds(
-    flowVersion: FlowVersion,
-): string[] {
-    const uniqueConnectionIds = new Set<string>()
-
-    function extractIdFromConnectionString(authString: string): string[] {
-        const match = authString.match(/{{connections\['([^']*(?:'\s*,\s*'[^']*)*)'\]}}/)
-        if (!match) return []
-        return match[1].split(/'\s*,\s*'/).map(id => id.trim())
-    }
-
-    function processAuth(auth: string) {
-        const connectionIds = extractIdFromConnectionString(auth)
-        for (const id of connectionIds) {
-            uniqueConnectionIds.add(id)
-        }
-    }
-
-    // Extract from trigger settings
-    if (flowVersion.trigger.settings?.input?.auth) {
-        processAuth(flowVersion.trigger.settings.input.auth)
-    }
-
-    // Extract from all steps
-    const steps = flowStructureUtil.getAllSteps(flowVersion.trigger)
-    for (const step of steps) {
-        if (step.settings?.input?.auth) {
-            processAuth(step.settings.input.auth)
-        }
-    }
-
-    return Array.from(uniqueConnectionIds)
-}
 
 export class AddConnectionIdsToFlowVersion1745530653784 implements MigrationInterface {
     name = 'AddConnectionIdsToFlowVersion1745530653784'
@@ -49,7 +14,7 @@ export class AddConnectionIdsToFlowVersion1745530653784 implements MigrationInte
         // Update existing rows
         const flowVersions = await queryRunner.query('SELECT * FROM flow_version')
         for (const flowVersion of flowVersions) {
-            const connectionIds = extractConnectionIds(flowVersion)
+            const connectionIds = flowStructureUtil.extractConnectionIds(flowVersion)
 
             await queryRunner.query('UPDATE flow_version SET "connectionIds" = $1 WHERE id = $2', [connectionIds, flowVersion.id])
         }
