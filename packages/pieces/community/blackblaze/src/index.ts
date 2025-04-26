467547fcb3086d748480d84c397f7c4f109ee743
e4ejaz <ejazakbar851@gmail.com>
2025-04-27 02:43:44 +0500

    feat(piece): add BlackBlaze Bucket Piece

---
+++ packages/pieces/community/blackblaze/src/index.ts
@@ -0,0 +1,199 @@
+import {
+  PieceAuth,
+  Property,
+  createPiece,
+} from '@activepieces/pieces-framework';
+import { PieceCategory } from '@activepieces/shared';
+import { readBlackBlazeFile } from './lib/actions/read-file';
+import { blackBlazes3UploadFile } from './lib/actions/upload-file';
+import { createBlackBlazeS3 } from './lib/common';
+import { newBlackBlazeFile } from './lib/triggers/new-file';
+
+const description = `
+This piece allows you to upload files to BlackBlaze Bucket compatible services.
+
+BlackBlaze Settings:
+Regions: https://www.backblaze.com/apidocs/introduction-to-the-s3-compatible-api
+Endpoint: leave blank
+`;
+
+export const blackBlazeS3Auth = PieceAuth.CustomAuth({
+  description: description,
+  props: {
+    accessKeyId: Property.ShortText({
+      displayName: 'Access Key ID',
+      required: true,
+    }),
+    secretAccessKey: PieceAuth.SecretText({
+      displayName: 'Secret Access Key',
+      required: true,
+    }),
+    bucket: Property.ShortText({
+      displayName: 'Bucket',
+      required: true,
+    }),
+    endpoint: Property.ShortText({
+      displayName: 'Endpoint',
+      required: false,
+    }),
+    region: Property.StaticDropdown({
+      displayName: 'Region',
+      options: {
+        options: [
+          {
+            label: 'Default',
+            value: 'us-east-1',
+          },
+          {
+            label: 'US East (N. Virginia) [us-east-1]',
+            value: 'us-east-1',
+          },
+          {
+            label: 'US East (Ohio) [us-east-2]',
+            value: 'us-east-2',
+          },
+          {
+            label: 'US West (N. California) [us-west-1]',
+            value: 'us-west-1',
+          },
+          {
+            label: 'US West (Oregon) [us-west-2]',
+            value: 'us-west-2',
+          },
+          {
+            label: 'Africa (Cape Town) [af-south-1]',
+            value: 'af-south-1',
+          },
+          {
+            label: 'Asia Pacific (Hong Kong) [ap-east-1]',
+            value: 'ap-east-1',
+          },
+          {
+            label: 'Asia Pacific (Mumbai) [ap-south-1]',
+            value: 'ap-south-1',
+          },
+          {
+            label: 'Asia Pacific (Osaka-Local) [ap-northeast-3]',
+            value: 'ap-northeast-3',
+          },
+          {
+            label: 'Asia Pacific (Seoul) [ap-northeast-2]',
+            value: 'ap-northeast-2',
+          },
+          {
+            label: 'Asia Pacific (Singapore) [ap-southeast-1]',
+            value: 'ap-southeast-1',
+          },
+          {
+            label: 'Asia Pacific (Sydney) [ap-southeast-2]',
+            value: 'ap-southeast-2',
+          },
+          {
+            label: 'Asia Pacific (Tokyo) [ap-northeast-1]',
+            value: 'ap-northeast-1',
+          },
+          {
+            label: 'Canada (Central) [ca-central-1]',
+            value: 'ca-central-1',
+          },
+          {
+            label: 'Europe (Frankfurt) [eu-central-1]',
+            value: 'eu-central-1',
+          },
+          {
+            label: 'Europe (Ireland) [eu-west-1]',
+            value: 'eu-west-1',
+          },
+          {
+            label: 'Europe (London) [eu-west-2]',
+            value: 'eu-west-2',
+          },
+          {
+            label: 'Europe (Milan) [eu-south-1]',
+            value: 'eu-south-1',
+          },
+          {
+            label: 'Europe (Paris) [eu-west-3]',
+            value: 'eu-west-3',
+          },
+          {
+            label: 'Europe (Stockholm) [eu-north-1]',
+            value: 'eu-north-1',
+          },
+          {
+            label: 'Middle East (Bahrain) [me-south-1]',
+            value: 'me-south-1',
+          },
+          {
+            label: 'South America (São Paulo) [sa-east-1]',
+            value: 'sa-east-1',
+          },
+          {
+            label: 'Europe (Spain) [eu-south-2]',
+            value: 'eu-south-2',
+          },
+          {
+            label: 'Asia Pacific (Hyderabad) [ap-south-2]',
+            value: 'ap-south-2',
+          },
+          {
+            label: 'Asia Pacific (Jakarta) [ap-southeast-3]',
+            value: 'ap-southeast-3',
+          },
+          {
+            label: 'Asia Pacific (Melbourne) [ap-southeast-4]',
+            value: 'ap-southeast-4',
+          },
+          {
+            label: 'China (Beijing) [cn-north-1]',
+            value: 'cn-north-1',
+          },
+          {
+            label: 'China (Ningxia) [cn-northwest-1]',
+            value: 'cn-northwest-1',
+          },
+          {
+            label: 'Europe (Zurich) [eu-central-2]',
+            value: 'eu-central-2',
+          },
+          {
+            label: 'Middle East (UAE) [me-central-1]',
+            value: 'me-central-1',
+          },
+        ],
+      },
+      required: true,
+    }),
+  },
+  validate: async ({ auth }) => {
+    const s3 = createBlackBlazeS3(auth);
+    try {
+      await s3.listObjectsV2({
+        Bucket: auth.bucket,
+        MaxKeys: 1,
+      });
+      return {
+        valid: true,
+      };
+    } catch (e) {
+      return {
+        valid: false,
+        error: (e as Error)?.message,
+      };
+    }
+  },
+  required: true,
+});
+
+export const blackblaze = createPiece({
+  displayName: 'Black Blaze',
+  description: 'Scalable storage in the cloud',
+
+  logoUrl: 'https://companieslogo.com/img/orig/BLZE-59480c3d.png?t=1720244491',
+  minimumSupportedRelease: '0.30.0',
+  authors: ["nhnansari","Willianwg","kishanprmr","MoShizzle","AbdulTheActivePiecer","khaledmashaly","abuaboud"],
+  categories: [PieceCategory.DEVELOPER_TOOLS],
+  auth: blackBlazeS3Auth,
+  actions: [blackBlazes3UploadFile, readBlackBlazeFile],
+  triggers: [newBlackBlazeFile],
+});
