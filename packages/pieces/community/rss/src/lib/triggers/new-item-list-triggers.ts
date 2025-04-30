907e5ae0d49fe6a022373a94faa2be6e945a57cc
Kevin Yu <kevin.yu@alan.eu>
2025-04-30 16:15:34 +0200

    refactor(rss): sampleData in a common file

--- packages/pieces/community/rss/src/lib/triggers/new-item-list-triggers.ts
+++ packages/pieces/community/rss/src/lib/triggers/new-item-list-triggers.ts
@@ -6,8 +6,6 @@ import {
 import {
   PieceAuthProperty,
   PiecePropValueSchema,
-  Store,
-  StoreScope,
   TriggerStrategy,
   createTrigger,
 } from '@activepieces/pieces-framework';
@@ -17,6 +15,7 @@ import axios from 'axios';
 import { isNil } from '@activepieces/shared';
 import dayjs from 'dayjs';
 import { getId } from '../common/getId';
+import { sampleData } from '../common/sampleData';
 
 type PollingProps = {
   rss_feed_urls: string[];
@@ -27,119 +26,7 @@ export const rssNewItemListTrigger = createTrigger({
   displayName: 'New Items in Multiple Feeds',
   description: 'Runs when a new item is added in one of the RSS feed',
   type: TriggerStrategy.POLLING,
-  sampleData: {
-    title: 'AWS Cloud Quest: Container Services',
-    description:
-      '<p>This is the DIY challenge of the Container Services in AWS Cloud Quest.</p>\n\n<p></ol>',
-    summary:
-      '<p>This is the DIY challenge of the Container Services in AWS Cloud Quest.</ol>',
-    date: '2023-03-08T21:57:48.000Z',
-    pubdate: '2023-03-08T21:57:48.000Z',
-    pubDate: '2023-03-08T21:57:48.000Z',
-    link: 'https://dev.to/arc/aws-cloud-quest-container-services-1hi7',
-    guid: 'https://dev.to/arc/aws-cloud-quest-container-services-1hi7',
-    author: 'architec',
-    comments: null,
-    origlink: null,
-    image: {},
-    source: {},
-    categories: ['aws'],
-    enclosures: [],
-    'rss:@': {},
-    'rss:title': {
-      '@': {},
-      '#': 'AWS Cloud Quest: Container Services',
-    },
-    'dc:creator': {
-      '@': {},
-      '#': 'architec',
-    },
-    'rss:pubdate': {
-      '@': {},
-      '#': 'Wed, 08 Mar 2023 21:57:48 +0000',
-    },
-    'rss:link': {
-      '@': {},
-      '#': 'https://dev.to/arc/aws-cloud-quest-container-services-1hi7',
-    },
-    permalink: 'https://dev.to/arc/aws-cloud-quest-container-services-1hi7',
-    'rss:guid': {
-      '@': {},
-      '#': 'https://dev.to/arc/aws-cloud-quest-container-services-1hi7',
-    },
-    'rss:description': {
-      '@': {},
-      '#': '<p>This is the DIY challenge of the Container Services in AWS Cloud Quest.</p>\n\n<p><a href="https://res.cloudinary.com/practicaldev/image/fetch/s--pZTG6rga--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/993bebzvmiomak17lm98.png" class="article-body-image-wrapper"><img src="https://res.cloudinary.com/practicaldev/image/fetch/s--pZTG6rga--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/993bebzvmiomak17lm98.png" alt="Image description" width="880" height="419"></a></p>\n\n<h3>\n  \n  \n  DIY Steps:\n</h3>\n\n<ol>\n<li>Repeat step 28-42</li>\n</ol>',
-    },
-    'rss:category': {
-      '@': {},
-      '#': 'aws',
-    },
-    meta: {
-      '#ns': [
-        {
-          'xmlns:atom': 'http://www.w3.org/2005/Atom',
-        },
-        {
-          'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
-        },
-      ],
-      '@': [
-        {
-          'xmlns:atom': 'http://www.w3.org/2005/Atom',
-        },
-        {
-          'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
-        },
-      ],
-      '#xml': {
-        version: '1.0',
-        encoding: 'UTF-8',
-      },
-      '#type': 'rss',
-      '#version': '2.0',
-      title: 'DEV Community',
-      description: 'The most recent home feed on DEV Community.',
-      date: null,
-      pubdate: null,
-      pubDate: null,
-      link: 'https://dev.to/',
-      xmlurl: 'https://dev.to/feed/',
-      xmlUrl: 'https://dev.to/feed/',
-      author: null,
-      language: 'en',
-      favicon: null,
-      copyright: null,
-      generator: null,
-      cloud: {},
-      image: {},
-      categories: [],
-      'rss:@': {},
-      'rss:title': {
-        '@': {},
-        '#': 'DEV Community',
-      },
-      'rss:description': {
-        '@': {},
-        '#': 'The most recent home feed on DEV Community.',
-      },
-      'rss:link': {
-        '@': {},
-        '#': 'https://dev.to/',
-      },
-      'atom:link': {
-        '@': {
-          rel: 'self',
-          type: 'application/rss+xml',
-          href: 'https://dev.to/feed/',
-        },
-      },
-      'rss:language': {
-        '@': {},
-        '#': 'en',
-      },
-    },
-  },
+  sampleData: sampleData,
   props: {
     rss_feed_urls: rssFeedUrls,
   },
