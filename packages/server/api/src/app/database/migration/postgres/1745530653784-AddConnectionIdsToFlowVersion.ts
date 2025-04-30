009ee7a354b8ef1ef66f943983d80d4f30746af3
Hazem Adel <hazemadelkhalel@gmail.com>
2025-04-30 11:37:11 +0000

    fix: do the migration for database only for any edition except cloud

--- packages/server/api/src/app/database/migration/postgres/1745530653784-AddConnectionIdsToFlowVersion.ts
+++ packages/server/api/src/app/database/migration/postgres/1745530653784-AddConnectionIdsToFlowVersion.ts
@@ -1,12 +1,31 @@
+import { ApEdition, flowStructureUtil } from '@activepieces/shared'
 import { MigrationInterface, QueryRunner } from 'typeorm'
+import { system } from '../../../helper/system/system'
 
 export class AddConnectionIdsToFlowVersion1745530653784 implements MigrationInterface {
     name = 'AddConnectionIdsToFlowVersion1745530653784'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        const edition = system.getEdition()
+
+        // First add the column as nullable
         await queryRunner.query(`
             ALTER TABLE "flow_version"
-            ADD "connectionIds" character varying array NOT NULL DEFAULT '{}'
+            ADD "connectionIds" character varying array
+        `)
+
+        // Update existing rows
+        const flowVersions = await queryRunner.query('SELECT * FROM flow_version')
+        for (const flowVersion of flowVersions) {
+            const connectionIds = edition !== ApEdition.CLOUD ? flowStructureUtil.extractConnectionIds(flowVersion) : []
+
+            await queryRunner.query('UPDATE flow_version SET "connectionIds" = $1 WHERE id = $2', [connectionIds, flowVersion.id])
+        }
+
+        // Now make the column NOT NULL
+        await queryRunner.query(`
+            ALTER TABLE "flow_version"
+            ALTER COLUMN "connectionIds" SET NOT NULL
         `)
     }
 
