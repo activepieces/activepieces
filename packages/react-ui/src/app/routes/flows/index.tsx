75b37af5cc2c582437178a1769fbfef4e0b0514a
louai <boumedienelouai2@gmail.com>
2025-05-06 07:45:59 +0100

    refactor(ui): move import flow button to create flow dropdown

--- packages/react-ui/src/app/routes/flows/index.tsx
+++ packages/react-ui/src/app/routes/flows/index.tsx
@@ -132,188 +132,188 @@ const FlowsPage = () => {
   const columns: (ColumnDef<RowDataWithActions<PopulatedFlow>> & {
     accessorKey: string;
   })[] = [
-    {
-      id: 'select',
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            table.getIsSomePageRowsSelected()
-          }
-          onCheckedChange={(value) => {
-            const isChecked = !!value;
-            table.toggleAllPageRowsSelected(isChecked);
-
-            if (isChecked) {
-              const allRowIds = table
-                .getRowModel()
-                .rows.map((row) => row.original);
-
-              const newSelectedRowIds = [...allRowIds, ...selectedRows];
-
-              const uniqueRowIds = Array.from(
-                new Map(
-                  newSelectedRowIds.map((item) => [item.id, item]),
-                ).values(),
-              );
-
-              setSelectedRows(uniqueRowIds);
-            } else {
-              const filteredRowIds = selectedRows.filter((row) => {
-                return !table
-                  .getRowModel()
-                  .rows.some((r) => r.original.version.id === row.version.id);
-              });
-              setSelectedRows(filteredRowIds);
-            }
-          }}
-        />
-      ),
-      cell: ({ row }) => {
-        const isChecked = selectedRows.some(
-          (selectedRow) =>
-            selectedRow.id === row.original.id &&
-            selectedRow.status === row.original.status,
-        );
-        return (
+      {
+        id: 'select',
+        header: ({ table }) => (
           <Checkbox
-            checked={isChecked}
+            checked={
+              table.getIsAllPageRowsSelected() ||
+              table.getIsSomePageRowsSelected()
+            }
             onCheckedChange={(value) => {
               const isChecked = !!value;
-              let newSelectedRows = [...selectedRows];
+              table.toggleAllPageRowsSelected(isChecked);
+
               if (isChecked) {
-                const exists = newSelectedRows.some(
-                  (selectedRow) => selectedRow.id === row.original.id,
+                const allRowIds = table
+                  .getRowModel()
+                  .rows.map((row) => row.original);
+
+                const newSelectedRowIds = [...allRowIds, ...selectedRows];
+
+                const uniqueRowIds = Array.from(
+                  new Map(
+                    newSelectedRowIds.map((item) => [item.id, item]),
+                  ).values(),
                 );
-                if (!exists) {
-                  newSelectedRows.push(row.original);
-                }
+
+                setSelectedRows(uniqueRowIds);
               } else {
-                newSelectedRows = newSelectedRows.filter(
-                  (selectedRow) => selectedRow.id !== row.original.id,
-                );
+                const filteredRowIds = selectedRows.filter((row) => {
+                  return !table
+                    .getRowModel()
+                    .rows.some((r) => r.original.version.id === row.version.id);
+                });
+                setSelectedRows(filteredRowIds);
               }
-              setSelectedRows(newSelectedRows);
-              row.toggleSelected(!!value);
             }}
           />
-        );
-      },
-      accessorKey: 'select',
-    },
-    {
-      accessorKey: 'name',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title={t('Name')} />
-      ),
-      cell: ({ row }) => {
-        const status = row.original.version.displayName;
-        return <div className="text-left">{status}</div>;
-      },
-    },
-    {
-      accessorKey: 'steps',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title={t('Steps')} />
-      ),
-      cell: ({ row }) => {
-        return (
-          <PieceIconList
-            trigger={row.original.version.trigger}
-            maxNumberOfIconsToShow={2}
-          />
-        );
-      },
-    },
-    {
-      accessorKey: 'folderId',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title={t('Folder')} />
-      ),
-      cell: ({ row }) => {
-        const folderId = row.original.folderId;
-        return (
-          <div className="text-left min-w-[150px]">
-            {folderId ? (
-              <FolderBadge folderId={folderId} />
-            ) : (
-              <span>{t('Uncategorized')}</span>
-            )}
-          </div>
-        );
-      },
-    },
-    {
-      accessorKey: 'created',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title={t('Created')} />
-      ),
-      cell: ({ row }) => {
-        const created = row.original.created;
-        return (
-          <div className="text-left font-medium min-w-[150px]">
-            {formatUtils.formatDate(new Date(created))}
-          </div>
-        );
-      },
-    },
-    {
-      accessorKey: 'status',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title={t('Status')} />
-      ),
-      cell: ({ row }) => {
-        return (
-          <div
-            className="flex items-center space-x-2"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <FlowStatusToggle
-              flow={row.original}
-              flowVersion={row.original.version}
-            ></FlowStatusToggle>
-          </div>
-        );
-      },
-    },
-    {
-      accessorKey: 'actions',
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="" />
-      ),
-      cell: ({ row }) => {
-        const flow = row.original;
-        return (
-          <div onClick={(e) => e.stopPropagation()}>
-            <FlowActionMenu
-              insideBuilder={false}
-              flow={flow}
-              readonly={false}
-              flowVersion={flow.version}
-              onRename={() => {
-                setRefresh(refresh + 1);
-                refetch();
+        ),
+        cell: ({ row }) => {
+          const isChecked = selectedRows.some(
+            (selectedRow) =>
+              selectedRow.id === row.original.id &&
+              selectedRow.status === row.original.status,
+          );
+          return (
+            <Checkbox
+              checked={isChecked}
+              onCheckedChange={(value) => {
+                const isChecked = !!value;
+                let newSelectedRows = [...selectedRows];
+                if (isChecked) {
+                  const exists = newSelectedRows.some(
+                    (selectedRow) => selectedRow.id === row.original.id,
+                  );
+                  if (!exists) {
+                    newSelectedRows.push(row.original);
+                  }
+                } else {
+                  newSelectedRows = newSelectedRows.filter(
+                    (selectedRow) => selectedRow.id !== row.original.id,
+                  );
+                }
+                setSelectedRows(newSelectedRows);
+                row.toggleSelected(!!value);
               }}
-              onMoveTo={() => {
-                setRefresh(refresh + 1);
-                refetch();
-              }}
-              onDuplicate={() => {
-                setRefresh(refresh + 1);
-                refetch();
-              }}
-              onDelete={() => {
-                setRefresh(refresh + 1);
-                refetch();
-              }}
+            />
+          );
+        },
+        accessorKey: 'select',
+      },
+      {
+        accessorKey: 'name',
+        header: ({ column }) => (
+          <DataTableColumnHeader column={column} title={t('Name')} />
+        ),
+        cell: ({ row }) => {
+          const status = row.original.version.displayName;
+          return <div className="text-left">{status}</div>;
+        },
+      },
+      {
+        accessorKey: 'steps',
+        header: ({ column }) => (
+          <DataTableColumnHeader column={column} title={t('Steps')} />
+        ),
+        cell: ({ row }) => {
+          return (
+            <PieceIconList
+              trigger={row.original.version.trigger}
+              maxNumberOfIconsToShow={2}
+            />
+          );
+        },
+      },
+      {
+        accessorKey: 'folderId',
+        header: ({ column }) => (
+          <DataTableColumnHeader column={column} title={t('Folder')} />
+        ),
+        cell: ({ row }) => {
+          const folderId = row.original.folderId;
+          return (
+            <div className="text-left min-w-[150px]">
+              {folderId ? (
+                <FolderBadge folderId={folderId} />
+              ) : (
+                <span>{t('Uncategorized')}</span>
+              )}
+            </div>
+          );
+        },
+      },
+      {
+        accessorKey: 'created',
+        header: ({ column }) => (
+          <DataTableColumnHeader column={column} title={t('Created')} />
+        ),
+        cell: ({ row }) => {
+          const created = row.original.created;
+          return (
+            <div className="text-left font-medium min-w-[150px]">
+              {formatUtils.formatDate(new Date(created))}
+            </div>
+          );
+        },
+      },
+      {
+        accessorKey: 'status',
+        header: ({ column }) => (
+          <DataTableColumnHeader column={column} title={t('Status')} />
+        ),
+        cell: ({ row }) => {
+          return (
+            <div
+              className="flex items-center space-x-2"
+              onClick={(e) => e.stopPropagation()}
             >
-              <EllipsisVertical className="h-10 w-10" />
-            </FlowActionMenu>
-          </div>
-        );
+              <FlowStatusToggle
+                flow={row.original}
+                flowVersion={row.original.version}
+              ></FlowStatusToggle>
+            </div>
+          );
+        },
       },
-    },
-  ];
+      {
+        accessorKey: 'actions',
+        header: ({ column }) => (
+          <DataTableColumnHeader column={column} title="" />
+        ),
+        cell: ({ row }) => {
+          const flow = row.original;
+          return (
+            <div onClick={(e) => e.stopPropagation()}>
+              <FlowActionMenu
+                insideBuilder={false}
+                flow={flow}
+                readonly={false}
+                flowVersion={flow.version}
+                onRename={() => {
+                  setRefresh(refresh + 1);
+                  refetch();
+                }}
+                onMoveTo={() => {
+                  setRefresh(refresh + 1);
+                  refetch();
+                }}
+                onDuplicate={() => {
+                  setRefresh(refresh + 1);
+                  refetch();
+                }}
+                onDelete={() => {
+                  setRefresh(refresh + 1);
+                  refetch();
+                }}
+              >
+                <EllipsisVertical className="h-10 w-10" />
+              </FlowActionMenu>
+            </div>
+          );
+        },
+      },
+    ];
 
   const bulkActions = useFlowsBulkActions({
     selectedRows,
@@ -335,26 +335,7 @@ const FlowsPage = () => {
             {t('Flows')}
           </TableTitle>
           <div className="ml-auto flex flex-row gap-2">
-            <PermissionNeededTooltip
-              hasPermission={doesUserHavePermissionToWriteFlow}
-            >
-              <ImportFlowDialog
-                insideBuilder={false}
-                onRefresh={() => {
-                  setRefresh(refresh + 1);
-                  refetch();
-                }}
-              >
-                <Button
-                  disabled={!doesUserHavePermissionToWriteFlow}
-                  variant="outline"
-                  className="flex gap-2 items-center"
-                >
-                  <Import className="w-4 h-4" />
-                  {t('Import Flow')}
-                </Button>
-              </ImportFlowDialog>
-            </PermissionNeededTooltip>
+
 
             <PermissionNeededTooltip
               hasPermission={doesUserHavePermissionToWriteFlow}
@@ -367,14 +348,14 @@ const FlowsPage = () => {
                   <Button
                     disabled={!doesUserHavePermissionToWriteFlow}
                     variant="default"
-                    className="flex gap-2 items-center"
+                    className="flex gap-2 min-w-40 items-center"
                     loading={isCreateFlowPending}
                   >
-                    <span>{t('New Flow')}</span>
+                    <span>{t('Create flow')}</span>
                     <ChevronDown className="h-4 w-4 " />
                   </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent>
+                <DropdownMenuContent className='min-w-40'>
                   <DropdownMenuItem
                     onSelect={(e) => {
                       e.preventDefault();
@@ -394,6 +375,23 @@ const FlowsPage = () => {
                       <span>{t('Use a template')}</span>
                     </DropdownMenuItem>
                   </SelectFlowTemplateDialog>
+
+                  <ImportFlowDialog
+                    insideBuilder={false}
+                    onRefresh={() => {
+                      setRefresh(refresh + 1);
+                      refetch();
+                    }}
+                  >
+                    <DropdownMenuItem
+                      onSelect={(e) => e.preventDefault()}
+                      disabled={!doesUserHavePermissionToWriteFlow}
+                    >
+                      <Import className="h-4 w-4 me-2" />
+                      {t('From local file')}
+                    </DropdownMenuItem>
+                  </ImportFlowDialog>
+
                 </DropdownMenuContent>
               </DropdownMenu>
             </PermissionNeededTooltip>
