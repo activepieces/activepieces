e8e27b9256af80e3ff3fcf162416f67d66c57d16
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-05-08 12:11:29 +0300

    fix: lint

--- packages/react-ui/src/app/routes/runs/index.tsx
+++ packages/react-ui/src/app/routes/runs/index.tsx
@@ -1,15 +1,11 @@
 import { Navigate } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
 
 import { ACTIVE_TAB_QUERY_PARAM, FlowsPageTabs } from '../flows';
-import { useSearchParams } from 'react-router-dom';
 
 export const FlowsRunsPageReroute = () => {
   const [searchParams] = useSearchParams();
   const searchParamsWithActiveTab = new URLSearchParams(searchParams);
   searchParamsWithActiveTab.set(ACTIVE_TAB_QUERY_PARAM, FlowsPageTabs.HISTORY);
-  return (
-    <Navigate
-      to={`/flows?${searchParamsWithActiveTab.toString()}`}
-    />
-  );
+  return <Navigate to={`/flows?${searchParamsWithActiveTab.toString()}`} />;
 };
