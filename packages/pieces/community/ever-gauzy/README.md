c26a095dd969f78ee9924107aa2c54981c1aa1f9
rolandm99 <manfulmweze99@gmail.com>
2025-05-08 12:51:32 +0200

    feat: implement ever-gauzy piece

---
+++ packages/pieces/community/ever-gauzy/README.md
@@ -0,0 +1,7 @@
+# pieces-ever-gauzy
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build pieces-ever-gauzy` to build the library.
---
+++ packages/pieces/community/ever-gauzy/src/index.ts
@@ -0,0 +1,62 @@
+import { createPiece, PieceAuth, OAuth2PropertyValue } from "@activepieces/pieces-framework";
+import { OAuth2GrantType, PieceCategory } from "@activepieces/shared";
+import { createEmployee, deleteEmployee, updateEmployee, getEmployee, listEmployees } from './lib/actions/employee';
+import { startTimer, stopTimer, getTimerStatus } from "./lib/actions/timer";
+import { createTask, deleteTask, updateTask, getTask, getListOfTasks } from "./lib/actions/tasks";
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
+
+export const gauzyAuth = PieceAuth.OAuth2({
+  required: true,
+  grantType: OAuth2GrantType.AUTHORIZATION_CODE,
+  authUrl: 'https://api.gauzy.co/auth/auth0',
+  tokenUrl: 'https://auth.gauzy.co/oauth/token',
+  scope: ['gauzy:all'],
+});
+
+export const everGauzy = createPiece({
+  displayName: "Ever-gauzy",
+  auth: gauzyAuth,
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: "https://cdn.activepieces.com/pieces/ever-gauzy.png",
+  categories: [
+    PieceCategory.PRODUCTIVITY,
+    PieceCategory.HUMAN_RESOURCES,
+    PieceCategory.BUSINESS_INTELLIGENCE
+  ],
+  description: "Ever Gauzy is a powerful open-source business management platform that helps you manage your projects, teams, and clients effectively. Automate employee management, time tracking, task management, and more.",
+  authors: ["RolandM"],
+  actions: [
+    // Employee Actions
+    createEmployee,
+    updateEmployee,
+    deleteEmployee,
+    getEmployee,
+    listEmployees,
+
+    // Timer Actions
+    startTimer,
+    stopTimer,
+    getTimerStatus,
+
+    // Task Actions
+    createTask,
+    updateTask,
+    deleteTask,
+    getTask,
+    getListOfTasks,
+    
+    // Custom API Call
+    createCustomApiCallAction({
+      baseUrl: (auth) => {
+        const authObj = auth as OAuth2PropertyValue;
+        return authObj.props?.["baseUrl"] as string || 'https://api.gauzy.co';
+      },
+      auth: gauzyAuth,
+      authMapping: async (auth) => ({
+        Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
+        'Content-Type': 'application/json',
+      }),
+    }),
+  ],
+  triggers: [],
+});
