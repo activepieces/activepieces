1026f1b2833b3fac71278b2016fe98f8993b29f1
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-05-08 12:06:12 +0300

    fix: reroute to runs page with searchparams included

--- packages/react-ui/src/app/routes/runs/index.tsx
+++ packages/react-ui/src/app/routes/runs/index.tsx
@@ -1,11 +1,15 @@
 import { Navigate } from 'react-router';
 
 import { ACTIVE_TAB_QUERY_PARAM, FlowsPageTabs } from '../flows';
+import { useSearchParams } from 'react-router-dom';
 
 export const FlowsRunsPageReroute = () => {
+  const [searchParams] = useSearchParams();
+  const searchParamsWithActiveTab = new URLSearchParams(searchParams);
+  searchParamsWithActiveTab.set(ACTIVE_TAB_QUERY_PARAM, FlowsPageTabs.HISTORY);
   return (
     <Navigate
-      to={`/flows?${ACTIVE_TAB_QUERY_PARAM}=${FlowsPageTabs.HISTORY}`}
+      to={`/flows?${searchParamsWithActiveTab.toString()}`}
     />
   );
 };
