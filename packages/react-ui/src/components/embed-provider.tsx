40c188136c5273a2a08f75a67d054c86555e5735
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-05-13 16:48:52 +0300

    fix: revert embed default state

--- packages/react-ui/src/components/embed-provider.tsx
+++ packages/react-ui/src/components/embed-provider.tsx
@@ -23,7 +23,7 @@ const defaultState: EmbeddingState = {
   hideSideNav: false,
   hideLogoInBuilder: false,
   prefix: '',
-  disableNavigationInBuilder: true,
+  disableNavigationInBuilder: false,
   hideFolders: false,
   hideFlowNameInBuilder: false,
   useDarkBackground: window.opener !== null,
