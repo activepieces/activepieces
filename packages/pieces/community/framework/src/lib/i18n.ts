18617523eecf05630c648fab2542f7b800242d06
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-05-14 17:56:46 +0300

    chore: bump framework version

--- packages/pieces/community/framework/src/lib/i18n.ts
+++ packages/pieces/community/framework/src/lib/i18n.ts
@@ -37,6 +37,7 @@ const readLocaleFile = async (locale: LocalesEnum, pieceOutputPath: string) => {
   const filePath = path.join(pieceOutputPath, 'src', 'i18n', `${locale}.json`);
 
   if (!(await fileExists(filePath))) {
+    console.log(`No i18n file found for ${locale} in piece ${pieceOutputPath}`);
     return null;
   }
 
@@ -106,9 +107,11 @@ const translatePiece = <T extends PieceMetadataModelSummary | PieceMetadataModel
   }
 }
 const initializeI18n =  async (pieceName: string): Promise<I18nForPiece | undefined> => {
+  console.log(`pieceName to initialize i18n ${pieceName}`)
   const locales = Object.values(LocalesEnum);
   const i18n: I18nForPiece = {};
   const pieceOutputPath = await extractPiecePath(pieceName)
+  console.log(`pieceOutputPath to get i18n for ${pieceName} : ${pieceOutputPath}`)
   if (!pieceOutputPath) {
     return undefined
   }
@@ -121,7 +124,8 @@ const initializeI18n =  async (pieceName: string): Promise<I18nForPiece | undefi
   return (Object.keys(i18n).length > 0) ? i18n : undefined;
 }
 
+
 export const pieceTranslation = {
   translatePiece,
-  initializeI18n
+  initializeI18n,
 }
