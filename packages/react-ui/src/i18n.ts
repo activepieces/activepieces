448122ebc587c9f65245073082ee5d145e6cc8b2
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-05-15 16:11:56 +0300

    chore: regenarate missing translation keys for i18n

--- packages/react-ui/src/i18n.ts
+++ packages/react-ui/src/i18n.ts
@@ -1,9 +1,9 @@
+import { LocalesEnum } from '@activepieces/shared';
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import ICU from 'i18next-icu';
 import { initReactI18next } from 'react-i18next';
-
 i18n
   .use(ICU)
   .use(Backend)
@@ -12,10 +12,11 @@ i18n
   .init({
     fallbackLng: 'en',
     debug: false,
-
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
+    supportedLngs: Object.values(LocalesEnum),
+    keySeparator: false,
+    nsSeparator: false,
   });
-
 export default i18n;
