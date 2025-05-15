0894b927177c584c7e68e63fb613fddbc0a9acaa
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-05-15 12:59:32 +0300

    fix: remove unused types

--- packages/pieces/community/framework/src/lib/piece-metadata.ts
+++ packages/pieces/community/framework/src/lib/piece-metadata.ts
@@ -130,9 +130,3 @@ export const PieceMetadataModelSummary = Type.Composite([
   PiecePackageMetadata
 ])
 export type PieceMetadataModelSummary = PieceMetadataSummary & PiecePackageMetadata;
-
-export const PieceMetadataModelWithoutI18n = Type.Omit(PieceMetadataModel, ["i18n"])
-export type PieceMetadataModelWithoutI18n = Omit<PieceMetadataModel, "i18n">
-
-export const PieceMetadataModelWithoutI18nSummary = Type.Omit(PieceMetadataModelSummary, ["i18n"])
-export type PieceMetadataModelWithoutI18nSummary = Omit<PieceMetadataModelSummary, "i18n">
