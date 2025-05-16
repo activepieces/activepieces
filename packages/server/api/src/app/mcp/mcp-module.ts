7dc99d2526f9aec1b35fbd12f5308ca44c535e0f
Mohammed Abu Aboud <moabuaboud@gmail.com>
2025-05-16 13:38:52 +0200

    Revert "feat: change mcp to new auth spec"

--- packages/server/api/src/app/mcp/mcp-module.ts
+++ packages/server/api/src/app/mcp/mcp-module.ts
@@ -1,10 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
 import { mcpPieceController } from './mcp-piece-controller'
 import { mcpServerController } from './mcp-server-controller'
+import { mcpSessionManager } from './mcp-session-manager'
 import { mcpSseController } from './mcp-sse-controller'
 
 export const mcpModule: FastifyPluginAsyncTypebox = async (app) => {
     await app.register(mcpServerController, { prefix: '/v1/mcp-servers' })
     await app.register(mcpSseController, { prefix: '/v1/mcp' })
     await app.register(mcpPieceController, { prefix: '/v1/mcp-pieces' })
+    await mcpSessionManager(app.log).init()
 }
