7a09d029386d09b2a85e5f1c8e39e37a08599c20
Olivier Sambourg <AdamSelene@users.noreply.github.com>
2025-05-16 13:57:35 +0200

    feat(docusign): move consent process to auth validation method

--- packages/pieces/community/docusign/src/lib/common.ts
+++ packages/pieces/community/docusign/src/lib/common.ts
@@ -1,12 +1,7 @@
 import { ApiClient } from 'docusign-esign';
 import { DocusignAuthType } from '../index';
-import { AxiosError } from 'axios';
-import { ServerContext } from '@activepieces/pieces-framework';
 
-export async function createApiClient(
-  auth: DocusignAuthType,
-  server: ServerContext
-) {
+export async function createApiClient(auth: DocusignAuthType) {
   const oAuthBasePath =
     auth.environment === 'demo'
       ? 'account-d.docusign.com'
@@ -15,47 +10,15 @@ export async function createApiClient(
     basePath: `https://${auth.environment}.docusign.net/restapi`,
     oAuthBasePath,
   });
-  try {
-    const results = await dsApi.requestJWTUserToken(
-      auth.clientId,
-      auth.impersonatedUserId,
-      auth.scopes.split(','),
-      Buffer.from(auth.privateKey.replace(/\\n/g, '\n'), 'utf-8'),
-      10 * 60 // 10mn lifetime
-    );
-    const accessToken = results.body.access_token;
-    dsApi.addDefaultHeader('Authorization', `Bearer ${accessToken}`);
-    return dsApi;
-  } catch (error) {
-    if (
-      error instanceof AxiosError &&
-      error.response &&
-      error.response.status === 400 &&
-      error.response.data &&
-      error.response.data.error === 'consent_required'
-    ) {
-      const formattedScopes = auth.scopes.split(',').join(encodeURI(' '));
-      // We don't use the built-in getAuthorizationUri method from docusign
-      // because it currently limits the scopes that can be requested to signature, extended, and impersonation
-      const consentUrl =
-        'https://' +
-        dsApi.getOAuthBasePath() +
-        '/oauth/auth' +
-        '?response_type=code' +
-        '&scope=' +
-        formattedScopes +
-        '&client_id=' +
-        auth.clientId +
-        '&redirect_uri=' +
-        encodeURIComponent(`${server.publicUrl}/redirect`);
-      throw new Error(
-        JSON.stringify({
-          message:
-            'Consent is required, please visit this URL and grant consent',
-          url: consentUrl,
-        })
-      );
-    }
-    throw error;
-  }
+
+  const results = await dsApi.requestJWTUserToken(
+    auth.clientId,
+    auth.impersonatedUserId,
+    auth.scopes.split(','),
+    Buffer.from(auth.privateKey.replace(/\\n/g, '\n'), 'utf-8'),
+    10 * 60 // 10mn lifetime
+  );
+  const accessToken = results.body.access_token;
+  dsApi.addDefaultHeader('Authorization', `Bearer ${accessToken}`);
+  return dsApi;
 }
