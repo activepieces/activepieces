537de65157015b184c89c58a4351408754da2aee
Kishan Parmar <code.kishanparmar@gmail.com>
2025-05-26 17:00:11 +0530

    feat(smartsuite): add custom api call

--- packages/pieces/community/smartsuite/src/index.ts
+++ packages/pieces/community/smartsuite/src/index.ts
@@ -1,4 +1,7 @@
-import { createPiece } from '@activepieces/pieces-framework';
+import {
+  createPiece,
+  PiecePropValueSchema,
+} from '@activepieces/pieces-framework';
 import { PieceCategory } from '@activepieces/shared';
 import { smartsuiteAuth } from './lib/auth';
 
@@ -13,15 +16,18 @@ import { getRecord } from './lib/actions/get-record';
 // Triggers
 import { newRecord } from './lib/triggers/new-record';
 import { updatedRecord } from './lib/triggers/updated-record';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { SMARTSUITE_API_URL } from './lib/common/constants';
 
 export const smartsuite = createPiece({
   displayName: 'SmartSuite',
-  description: 'Collaborative work management platform combining databases with spreadsheets',
+  description:
+    'Collaborative work management platform combining databases with spreadsheets.',
   logoUrl: 'https://cdn.activepieces.com/pieces/smartsuite.png',
   categories: [PieceCategory.PRODUCTIVITY],
   auth: smartsuiteAuth,
   minimumSupportedRelease: '0.30.0',
-  authors: ['Kunal-Darekar','kishanprmr'],
+  authors: ['Kunal-Darekar', 'kishanprmr'],
   actions: [
     createRecord,
     updateRecord,
@@ -29,9 +35,17 @@ export const smartsuite = createPiece({
     uploadFile,
     findRecords,
     getRecord,
+    createCustomApiCallAction({
+      auth: smartsuiteAuth,
+      baseUrl: () => SMARTSUITE_API_URL,
+      authMapping: async (auth) => {
+        const authValue = auth as PiecePropValueSchema<typeof smartsuiteAuth>;
+        return {
+          Authorization: `Token ${authValue.apiKey}`,
+          'ACCOUNT-ID': authValue.accountId,
+        };
+      },
+    }),
   ],
-  triggers: [
-    newRecord,
-    updatedRecord,
-  ],
+  triggers: [newRecord, updatedRecord],
 });
