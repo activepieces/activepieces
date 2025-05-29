13445cb8ffbf2f20333896579945603adeb7f6ad
Sanket6652 <sanketnannaware96@gmail.com>
2025-05-29 20:00:23 +0530

    feat: heygen actions

---
+++ packages/pieces/community/heygen/src/lib/actions/create-avatar-video.ts
@@ -0,0 +1,12 @@
+import { createAction, Property } from '@activepieces/pieces-framework';
+
+export const createAvatarVideo = createAction({
+  // auth: check https://www.activepieces.com/docs/developers/piece-reference/authentication,
+  name: 'createAvatarVideo',
+  displayName: 'Create Avatar Video',
+  description: '',
+  props: {},
+  async run() {
+    // Action logic here
+  },
+});
