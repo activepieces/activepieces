a7c8e538e7cfbb84ed123732953093b2e613f11b
Ani-4x <gawandeanimesh@gmail.com>
2025-06-02 14:57:46 +0530

    feat(crisp):  wip

--- packages/pieces/community/crisp/src/index.ts
+++ packages/pieces/community/crisp/src/index.ts
@@ -4,7 +4,7 @@ import { crispAuth } from './lib/common/common';
 
 export const crisp = createPiece({
   displayName: 'Crisp',
-  logoUrl: 'https://website.crisp.chat/favicon-32x32.png',
+  logoUrl: 'https://cdn.activepieces.com/pieces/crisp.png',
   auth: crispAuth,
   authors: [''],
   description: 'Crisp is a customer messaging platform that allows businesses to communicate with their customers through various channels such as live chat, email, and social media.',
