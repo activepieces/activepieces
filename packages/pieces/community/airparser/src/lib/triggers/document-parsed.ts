d82660a223dbce47b24fcc64b94718a65b349447
Kishan Parmar <code.kishanparmar@gmail.com>
2025-06-09 21:42:09 +0530

    fix(airparser): fix lint issue

--- packages/pieces/community/airparser/src/lib/triggers/document-parsed.ts
+++ packages/pieces/community/airparser/src/lib/triggers/document-parsed.ts
@@ -1,20 +1,24 @@
 import { HttpMethod } from '@activepieces/pieces-common';
-import { createTrigger, Property, TriggerStrategy } from '@activepieces/pieces-framework';
+import {
+	createTrigger,
+	Property,
+	TriggerStrategy,
+} from '@activepieces/pieces-framework';
 import { isNil } from '@activepieces/shared';
 import { airparserAuth } from '../../index';
 import { airparserApiCall, GetDocumentResponse } from '../common';
 import { inboxIdDropdown } from '../common/props';
 
 export const documentParsedTrigger = createTrigger({
-	auth: airparserAuth,
-	name: 'document_parsed',
-	displayName: 'Document Parsed',
-	description: 'Triggers when a new document is parsed in a specific inbox.',
-	type: TriggerStrategy.WEBHOOK,
-	props: {
-		inboxId: inboxIdDropdown,
-		markdown: Property.MarkDown({
-			value: `## Airparser Webhook Setup
+  auth: airparserAuth,
+  name: 'document_parsed',
+  displayName: 'Document Parsed',
+  description: 'Triggers when a new document is parsed in a specific inbox.',
+  type: TriggerStrategy.WEBHOOK,
+  props: {
+    inboxId: inboxIdDropdown,
+    markdown: Property.MarkDown({
+      value: `## Airparser Webhook Setup
 			To use this trigger, you need to manually set up a webhook in your Airparser account:
 
 			1. Login to your Airparser account.
@@ -25,72 +29,81 @@ export const documentParsedTrigger = createTrigger({
 			\`\`\`
 			4. Click Save to register the webhook.
 			`,
-		}),
-	},
-	async onEnable(context) {},
+    }),
+  },
+  async onEnable(context) {
+    // No need to register webhooks programmatically as user will do it manually
+  },
+  async onDisable(context) {
+    // No need to unregister webhooks as user will do it manually
+  },
+  async run(context) {
+    const payload = context.payload.body as {
+      inbox_id: string;
+      doc_id: string;
+      event: string;
+    };
 
-	async onDisable(context) {},
+    if (
+      payload.event === 'doc.parsed' &&
+      payload.inbox_id === context.propsValue.inboxId
+    ) {
+      return [payload];
+    }
+    return [];
+  },
 
-	async run(context) {
-		const payload = context.payload.body as { inbox_id: string; doc_id: string; event: string };
+  async test(context) {
+    const { inboxId } = context.propsValue;
+    const listDocResponse = await airparserApiCall<{
+      hasPrevPage: boolean;
+      hasNextPage: boolean;
+      docs: { _id: string; name: string }[];
+    }>({
+      apiKey: context.auth as string,
+      method: HttpMethod.GET,
+      resourceUri: `/inboxes/${inboxId}/docs`,
+      query: {
+        statuses: 'parsed',
+      },
+    });
 
-		if (payload.event === 'doc.parsed' && payload.inbox_id === context.propsValue.inboxId) {
-			return [payload];
-		}
-		return [];
-	},
+    if (isNil(listDocResponse.docs)) return [];
 
-	async test(context) {
-		const { inboxId } = context.propsValue;
-		const listDocResponse = await airparserApiCall<{
-			hasPrevPage: boolean;
-			hasNextPage: boolean;
-			docs: { _id: string; name: string }[];
-		}>({
-			apiKey: context.auth as string,
-			method: HttpMethod.GET,
-			resourceUri: `/inboxes/${inboxId}/docs`,
-			query: {
-				statuses: 'parsed',
-			},
-		});
+    const items = [];
+    for (const doc of listDocResponse.docs) {
+      const response = await airparserApiCall<GetDocumentResponse>({
+        apiKey: context.auth,
+        method: HttpMethod.GET,
+        resourceUri: `/docs/${doc._id}/extended`,
+      });
 
-		if (isNil(listDocResponse.docs)) return [];
+      items.push({
+        inbox_id: inboxId,
+        doc_id: doc._id,
+        event: 'doc.parsed',
+        payload: {
+          filename: response.filename,
+          parsed: response.json,
+        },
+      });
+    }
 
-		const items = [];
-		for (const doc of listDocResponse.docs) {
-			const response = await airparserApiCall<GetDocumentResponse>({
-				apiKey: context.auth,
-				method: HttpMethod.GET,
-				resourceUri: `/docs/${doc._id}/extended`,
-			});
+    return items;
+  },
 
-			items.push({
-				inbox_id: inboxId,
-				doc_id: doc._id,
-				event: 'doc.parsed',
-				payload: {
-					filename: response.filename,
-					parsed: response.json,
-				},
-			});
-		}
-
-		return items;
-	},
-
-	sampleData: {
-		inbox_id: '6846e11bb1abe002cb1ada14',
-		doc_id: '6846ee9db1abe002cb1b05ad',
-		event: 'doc.parsed',
-		payload: {
-			filename: 'sample.pdf',
-			parsed: {
-				billing_address: 'Your Company Name\nYour Address City, State Zip',
-				shipping_address: 'Client Name Address City, State Zip',
-				totalamount: '200.00',
-				created_at: '2025-06-09T14:24:29.099Z',
-			},
-		},
-	},
+  sampleData: {
+    inbox_id: '6846e11bb1abe002cb1ada14',
+    doc_id: '6846ee9db1abe002cb1b05ad',
+    event: 'doc.parsed',
+    payload: {
+      filename: 'sample.pdf',
+      parsed: {
+        billing_address: 'Your Company Name\nYour Address City, State Zip',
+        shipping_address: 'Client Name Address City, State Zip',
+        totalamount: '200.00',
+        created_at: '2025-06-09T14:24:29.099Z',
+      },
+    },
+  },
 });
