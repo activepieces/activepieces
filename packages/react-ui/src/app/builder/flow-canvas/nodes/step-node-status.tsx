9501fa8c9091313ef1a55d77bd9dea44ce61d4e8
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-06-19 11:58:01 +0300

    fix: refactor step node status

---
+++ packages/react-ui/src/app/builder/flow-canvas/nodes/step-node-status.tsx
@@ -0,0 +1,76 @@
+import { t } from 'i18next';
+import { RouteOff } from 'lucide-react';
+import { useMemo } from 'react';
+
+import { InvalidStepIcon } from '@/components/custom/alert-icon';
+import { LoadingSpinner } from '@/components/ui/spinner';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { StepStatusIcon } from '@/features/flow-runs/components/step-status-icon';
+import { FlowRunStatus, isNil, Step } from '@activepieces/shared';
+
+import { useBuilderStateContext } from '../../builder-hooks';
+import { flowCanvasUtils } from '../utils/flow-canvas-utils';
+
+const ApStepNodeStatus = ({ step }: { step: Step }) => {
+  const [run, loopIndexes, flowVersion] = useBuilderStateContext((state) => [
+    state.run,
+    state.loopsIndexes,
+    state.flowVersion,
+  ]);
+
+  const stepOutputStatus = useMemo(() => {
+    return flowCanvasUtils.getStepStatus(
+      step.name,
+      run,
+      loopIndexes,
+      flowVersion,
+    );
+  }, [step.name, run, loopIndexes, flowVersion]);
+  const isSkipped = flowCanvasUtils.isSkipped(step.name, flowVersion.trigger);
+  const showRunningIcon =
+    isNil(stepOutputStatus) &&
+    run?.status === FlowRunStatus.RUNNING &&
+    !isSkipped;
+
+  return (
+    <div className="w-4 flex mt-0.5 items-center justify-center h-[20px]">
+      {stepOutputStatus && (
+        <StepStatusIcon status={stepOutputStatus} size="4"></StepStatusIcon>
+      )}
+      {showRunningIcon && (
+        <LoadingSpinner className="w-4 h-4 "></LoadingSpinner>
+      )}
+      {isSkipped && (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <RouteOff className="w-4 h-4"> </RouteOff>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">{t('Skipped')}</TooltipContent>
+        </Tooltip>
+      )}
+      {!step.valid && !isSkipped && (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="mr-3">
+              <InvalidStepIcon
+                size={16}
+                viewBox="0 0 16 15"
+                className="stroke-0 animate-fade"
+              ></InvalidStepIcon>
+            </div>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">
+            {t('Incomplete settings')}
+          </TooltipContent>
+        </Tooltip>
+      )}
+    </div>
+  );
+};
+ApStepNodeStatus.displayName = 'ApStepNodeStatus';
+
+export { ApStepNodeStatus };
--- packages/react-ui/src/app/builder/flow-canvas/nodes/step-node.tsx
+++ packages/react-ui/src/app/builder/flow-canvas/nodes/step-node.tsx
@@ -1,109 +1,53 @@
 import { useDraggable } from '@dnd-kit/core';
 import { Handle, NodeProps, Position } from '@xyflow/react';
-import { t } from 'i18next';
-import { ChevronDown, RouteOff } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import React, { useMemo } from 'react';
 
 import { useBuilderStateContext } from '@/app/builder/builder-hooks';
 import { PieceSelector } from '@/app/builder/pieces-selector';
-import { InvalidStepIcon } from '@/components/custom/alert-icon';
 import { Button } from '@/components/ui/button';
-import { LoadingSpinner } from '@/components/ui/spinner';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
-import { flowRunUtils } from '@/features/flow-runs/lib/flow-run-utils';
 import { PieceIcon } from '@/features/pieces/components/piece-icon';
 import { piecesHooks } from '@/features/pieces/lib/pieces-hook';
 import { cn } from '@/lib/utils';
 import {
-  Action,
-  ActionType,
   FlowOperationType,
-  FlowRun,
-  FlowRunStatus,
-  FlowVersion,
-  Trigger,
+  Step,
   TriggerType,
   flowStructureUtil,
-  isNil,
 } from '@activepieces/shared';
 
-import { StepStatusIcon } from '../../../../features/flow-runs/components/step-status-icon';
 import { flowUtilConsts, STEP_CONTEXT_MENU_ATTRIBUTE } from '../utils/consts';
+import { flowCanvasUtils } from '../utils/flow-canvas-utils';
 import { ApStepNode } from '../utils/types';
 
-function hasSkippedParent(stepName: string, trigger: Trigger): boolean {
-  const step = flowStructureUtil.getStep(stepName, trigger);
-  if (!step) {
-    return false;
+import { ApStepNodeStatus } from './step-node-status';
+
+const getPieceSelectorOperationType = (step: Step) => {
+  if (flowStructureUtil.isTrigger(step.type)) {
+    return FlowOperationType.UPDATE_TRIGGER;
   }
+  return FlowOperationType.UPDATE_ACTION;
+};
 
-  const skippedParents = flowStructureUtil
-    .findPathToStep(trigger, stepName)
-    .filter(
-      (p) =>
-        (p.type === ActionType.LOOP_ON_ITEMS || p.type === ActionType.ROUTER) &&
-        flowStructureUtil.isChildOf(p, stepName) &&
-        p.skip,
-    );
-  return skippedParents.length > 0;
-}
-
-function getStepStatus(
-  stepName: string | undefined,
-  run: FlowRun | null,
-  loopIndexes: Record<string, number>,
-  flowVersion: FlowVersion,
-) {
-  if (!run || !stepName || !run.steps) {
-    return undefined;
-  }
-  const stepOutput = flowRunUtils.extractStepOutput(
-    stepName,
-    loopIndexes,
-    run.steps,
-    flowVersion.trigger,
-  );
-  return stepOutput?.status;
-}
-
-const StepActionWrapper = React.memo(
-  ({ children }: { children: React.ReactNode }) => {
-    return (
-      <div className="flex items-center gap-2 cursor-pointer">{children}</div>
-    );
-  },
-);
-
-StepActionWrapper.displayName = 'StepActionWrapper';
 const ApStepCanvasNode = React.memo(
   ({ data }: NodeProps & Omit<ApStepNode, 'position'>) => {
     const [
       selectStepByName,
       isSelected,
       isDragging,
-      selectedStep,
-      run,
       readonly,
       exitStepSettings,
       flowVersion,
-      loopIndexes,
       setSelectedBranchIndex,
       setPieceSelectorStep,
       pieceSelectorStep,
     ] = useBuilderStateContext((state) => [
       state.selectStepByName,
-      !isNil(state.selectedStep) && state.selectedStep === data.step?.name,
+      state.selectedStep === data.step?.name,
       state.activeDraggingStep === data.step?.name,
-      state.selectedStep,
-      state.run,
       state.readonly,
       state.exitStepSettings,
       state.flowVersion,
-      state.loopsIndexes,
       state.setSelectedBranchIndex,
       state.setPieceSelectorStep,
       state.pieceSelectorStep,
@@ -122,14 +66,9 @@ const ApStepCanvasNode = React.memo(
     }, [data, flowVersion]);
 
     const isTrigger = flowStructureUtil.isTrigger(step.type);
-    const isAction = flowStructureUtil.isAction(step.type);
 
-    const pieceSelectorOperation = isAction
-      ? FlowOperationType.UPDATE_ACTION
-      : FlowOperationType.UPDATE_TRIGGER;
-    const isEmptyTriggerSelected =
-      selectedStep === 'trigger' && step.type === TriggerType.EMPTY;
-    const isSkipped = (step as Action).skip;
+    const isEmptyTriggerSelected = step.type === TriggerType.EMPTY;
+    const isSkipped = flowCanvasUtils.isSkipped(step.name, flowVersion.trigger);
 
     const { attributes, listeners, setNodeRef } = useDraggable({
       id: step.name,
@@ -139,16 +78,9 @@ const ApStepCanvasNode = React.memo(
       },
     });
 
-    const stepOutputStatus = useMemo(() => {
-      return getStepStatus(step.name, run, loopIndexes, flowVersion);
-    }, [step.name, run, loopIndexes, flowVersion]);
-
-    const showRunningIcon =
-      isNil(stepOutputStatus) &&
-      run?.status === FlowRunStatus.RUNNING &&
-      !hasSkippedParent(step.name, flowVersion.trigger) &&
-      !isSkipped;
-
+    const initialSelectedPieceDisplayName = isEmptyTriggerSelected
+      ? undefined
+      : stepMetadata?.displayName;
     const handleStepClick = (
       e: React.MouseEvent<HTMLDivElement, MouseEvent>,
     ) => {
@@ -203,21 +135,15 @@ const ApStepCanvasNode = React.memo(
         <div className="px-3 h-full w-full  overflow-hidden">
           {!isDragging && (
             <PieceSelector
-              initialSelectedPiece={
-                step.type === TriggerType.EMPTY
-                  ? undefined
-                  : stepMetadata?.displayName
-              }
+              initialSelectedPieceDisplayName={initialSelectedPieceDisplayName}
               operation={{
-                type: isEmptyTriggerSelected
-                  ? FlowOperationType.UPDATE_TRIGGER
-                  : pieceSelectorOperation,
+                type: getPieceSelectorOperationType(step),
                 stepName: step.name,
               }}
               open={openPieceSelector || isEmptyTriggerSelected}
               onOpenChange={(open) => {
                 setPieceSelectorStep(open ? step.name : null);
-                if (!open && step.type === TriggerType.EMPTY) {
+                if (!open && isEmptyTriggerSelected) {
                   exitStepSettings();
                 }
               }}
@@ -288,43 +214,7 @@ const ApStepCanvasNode = React.memo(
                       <div className="text-xs truncate text-muted-foreground text-ellipsis overflow-hidden whitespace-nowrap w-full">
                         {stepMetadata?.displayName}
                       </div>
-                      <div className="w-4 flex mt-0.5 items-center justify-center h-[20px]">
-                        {stepOutputStatus && (
-                          <StepStatusIcon
-                            status={stepOutputStatus}
-                            size="4"
-                          ></StepStatusIcon>
-                        )}
-                        {showRunningIcon && (
-                          <LoadingSpinner className="w-4 h-4 "></LoadingSpinner>
-                        )}
-                        {isSkipped && (
-                          <Tooltip>
-                            <TooltipTrigger asChild>
-                              <RouteOff className="w-4 h-4"> </RouteOff>
-                            </TooltipTrigger>
-                            <TooltipContent side="bottom">
-                              {t('Skipped')}
-                            </TooltipContent>
-                          </Tooltip>
-                        )}
-                        {!step.valid && !isSkipped && (
-                          <Tooltip>
-                            <TooltipTrigger asChild>
-                              <div className="mr-3">
-                                <InvalidStepIcon
-                                  size={16}
-                                  viewBox="0 0 16 15"
-                                  className="stroke-0 animate-fade"
-                                ></InvalidStepIcon>
-                              </div>
-                            </TooltipTrigger>
-                            <TooltipContent side="bottom">
-                              {t('Incomplete settings')}
-                            </TooltipContent>
-                          </Tooltip>
-                        )}
-                      </div>
+                      <ApStepNodeStatus step={step} />
                     </div>
                   </div>
                 </div>
