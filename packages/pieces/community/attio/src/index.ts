c92d5392404ce4936ba7d6aa01e7cd0cf864813e
Kishan Parmar <code.kishanparmar@gmail.com>
2025-06-20 15:35:03 +0530

    fix(attio): update desc for auth

--- packages/pieces/community/attio/src/index.ts
+++ packages/pieces/community/attio/src/index.ts
@@ -18,17 +18,17 @@ import { createCustomApiCallAction } from '@activepieces/pieces-common';
 import { BASE_URL } from './lib/common/client';
 
 const markdownDescription = `
-To use Attio, you need to generate an API key:
+To use Attio, you need to generate an Access Token:
 1. Login to your Attio account at https://app.attio.com.
 2. From the dropdown beside your workspace name, click Workspace settings.
 3. Click the Developers tab.
 4. Click on the "New Access Token" button.
 5. Set the appropriate Scopes for the integration.
-6. Copy the generated API key.
+6. Copy the generated Access Token.
 `;
 
 export const attioAuth = PieceAuth.SecretText({
-	displayName: 'API Key',
+	displayName: 'Access Token',
 	description: markdownDescription,
 	required: true,
 });
