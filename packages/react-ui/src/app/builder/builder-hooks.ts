f3bd3250254e53005d12097af5c0d585e32845b8
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-06-20 02:27:34 +0000

    fix: react-ui typo

--- packages/react-ui/src/app/builder/builder-hooks.ts
+++ packages/react-ui/src/app/builder/builder-hooks.ts
@@ -738,9 +738,7 @@ export const useIsFocusInsideListMapperModeInput = ({
 };
 export const useFocusOnStep = () => {
   const currentRun = useBuilderStateContext((state) => state.run);
-  const setSelectedStep = useBuilderStateContext(
-    (state) => state.setSelectedStep,
-  );
+  const setSelectedStep = useBuilderStateContext((state) => state.selectStepByName);
   const previousStatus = usePrevious(currentRun?.status);
   const currentStep = flowRunUtils.findLastStepWithStatus(
     previousStatus ?? FlowRunStatus.RUNNING,
