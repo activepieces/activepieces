d2257622047dce5092edc3ca8baa9c651c2296ab
Hazem Adel <hazemadelkhalel@gmail.com>
2025-06-24 11:43:32 +0000

    refactor: names of variables in test step utils

--- packages/react-ui/src/app/builder/test-step/test-step-utils.ts
+++ packages/react-ui/src/app/builder/test-step/test-step-utils.ts
@@ -4,15 +4,15 @@ function formatErrorMessage(errorMessage: string): string {
     return errorMessage;
   }
 
-  const filteredParts = errorMessagesSplit.filter((part) => part.trim() !== '');
+  const trimmedErrorMessages = errorMessagesSplit.filter((part) => part.trim() !== '');
 
   const indentationStep = '  ';
-  return filteredParts.reduce((acc, current, index) => {
+  return trimmedErrorMessages.reduce((acc, current, index) => {
     const indentation = indentationStep.repeat(index);
-    const cleanedPart = current.trim().replace(/^\n+|\n+$/g, '');
+    const errorMessageWithoutEndlines = current.trim().replace(/^\n+|\n+$/g, '');
     const errorLabel =
-      filteredParts.length === 1 ? 'Error' : `Error ${index + 1}`;
-    return `${acc}${indentation}${errorLabel}: ${cleanedPart}\n`;
+    trimmedErrorMessages.length === 1 ? 'Error' : `Error ${index + 1}`;
+    return `${acc}${indentation}${errorLabel}: ${errorMessageWithoutEndlines}\n`;
   }, '');
 }
 
