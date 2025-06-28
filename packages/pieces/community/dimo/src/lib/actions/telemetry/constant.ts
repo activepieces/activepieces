771ff9166b0c5186505d8e5cf8b614883f73a148
Yusuf Çırak <yusuf10901@hotmail.com>
2025-06-28 20:10:26 +0300

    refactor(pieces.dimo): update date properties to use DateTime type and improve query syntax

--- packages/pieces/community/dimo/src/lib/actions/telemetry/constant.ts
+++ packages/pieces/community/dimo/src/lib/actions/telemetry/constant.ts
@@ -4,7 +4,7 @@ export const commonQueries = {
     label: "Getting a list of available signals for a specific vehicle",
     query: `
     query {
-      availableSignals(tokenId: $tokenId)
+      availableSignals(tokenId: <tokenId>)
     }
     `,
   },
@@ -13,9 +13,9 @@ export const commonQueries = {
     query: `
       query {
         signals(
-          tokenId: $tokenId,
+          tokenId: <tokenId>,
           interval: "24h",
-          from: $startDate, to: $endDate
+          from: "<startDate>", to: "<endDate>"
         ) {
           speed(agg: MED)
           powertrainType(agg: RAND)
@@ -33,8 +33,8 @@ export const commonQueries = {
     query: `
       query {
         signals(
-          tokenId: $tokenId,
-          from: $startDate, to: $endDate,
+          tokenId: <tokenId>,
+          from: "<startDate>", to: "<endDate>",
           interval: "24h"
         ) {
           timestamp
@@ -48,9 +48,9 @@ export const commonQueries = {
     query: `
       query {
         signals(
-          tokenId: $tokenId,
-          from: $startDate, to: $endDate,
-          interval: $interval
+          tokenId: <tokenId>,
+          from: "<startDate>", to: "<endDate>",
+          interval: "$interval"
         ) {
           timestamp
           maxSpeed: speed(agg: MAX)
@@ -62,7 +62,7 @@ export const commonQueries = {
     label: "Getting the latest VIN and Vehicle Configuration for a specific vehicle. You should use this after generating vinVc from the Attestation API",
     query: `
       query {
-        vinVCLatest(tokenId: $tokenId) {
+        vinVCLatest(tokenId: <tokenId>) {
           vin
         }
       }
