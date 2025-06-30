6b639c97c46ea61c8e160edfa00faa81aebca82d
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-06-30 19:50:46 +0000

    fix: truncate ai usage

--- packages/server/api/src/app/database/migration/postgres/1750526457504-AddPlatformIdToAiUsage.ts
+++ packages/server/api/src/app/database/migration/postgres/1750526457504-AddPlatformIdToAiUsage.ts
@@ -5,8 +5,12 @@ export class AddPlatformIdToAiUsage1750526457504 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
+            TRUNCATE TABLE "ai_usage"
+        `)
+        await queryRunner.query(`
             DROP INDEX "public"."idx_ai_usage_project_created"
         `)
+
         await queryRunner.query(`
             ALTER TABLE "ai_usage"
             ADD "platformId" character varying NOT NULL
