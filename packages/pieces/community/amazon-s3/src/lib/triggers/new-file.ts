a72d29ac06297e1fa396a43488b41a06783a34c3
Kishan Parmar <code.kishanparmar@gmail.com>
2025-07-04 14:26:47 +0530

    fix(s3): update trigger stratery to time based for duplicate  file issue

--- packages/pieces/community/amazon-s3/src/lib/triggers/new-file.ts
+++ packages/pieces/community/amazon-s3/src/lib/triggers/new-file.ts
@@ -1,94 +1,105 @@
-import {
-  PiecePropValueSchema,
-  Property,
-  createTrigger,
-} from '@activepieces/pieces-framework';
+import { PiecePropValueSchema, Property, createTrigger } from '@activepieces/pieces-framework';
 import { TriggerStrategy } from '@activepieces/pieces-framework';
-import {
-  DedupeStrategy,
-  Polling,
-  pollingHelper,
-} from '@activepieces/pieces-common';
+import { DedupeStrategy, Polling, pollingHelper } from '@activepieces/pieces-common';
 
 import { amazonS3Auth } from '../..';
 import { createS3 } from '../common';
+import dayjs from 'dayjs';
 
-const polling: Polling<
-  PiecePropValueSchema<typeof amazonS3Auth>,
-  { folderPath?: string }
-> = {
-  strategy: DedupeStrategy.LAST_ITEM,
-  items: async ({ auth, lastItemId, propsValue }) => {
-    const s3 = createS3(auth);
-    const params: any = {
-      Bucket: auth.bucket,
-      MaxKeys: 100,
-      StartAfter: lastItemId,
-    };
-    if (propsValue.folderPath)
-      params.Prefix = `${
-        propsValue.folderPath.endsWith('/')
-          ? propsValue.folderPath.slice(0, -1)
-          : propsValue.folderPath
-      }`;
+const polling: Polling<PiecePropValueSchema<typeof amazonS3Auth>, { folderPath?: string }> = {
+	strategy: DedupeStrategy.TIMEBASED,
+	items: async ({ auth, lastFetchEpochMS, propsValue }) => {
+		const isTest = lastFetchEpochMS === 0;
 
-    const currentValues = (await s3.listObjectsV2(params)).Contents ?? [];
-    const items = (currentValues as any[]).map((item: { Key: string }) => ({
-      id: item.Key,
-      data: item,
-    }));
-    return items;
-  },
+		const s3 = createS3(auth);
+
+		const bucketFiles = [];
+
+		let hasMore = true;
+		let nextToken: string | undefined;
+
+		do {
+			const params: any = {
+				Bucket: auth.bucket,
+				MaxKeys: isTest ? 10 : 1000,
+				ContinuationToken: nextToken,
+			};
+
+			if (propsValue.folderPath)
+				params.Prefix = `${
+					propsValue.folderPath.endsWith('/')
+						? propsValue.folderPath.slice(0, -1)
+						: propsValue.folderPath
+				}`;
+
+			const response = await s3.listObjectsV2(params);
+
+			const items = response.Contents ?? [];
+
+			bucketFiles.push(...items);
+
+			if (isTest) break;
+
+			hasMore = !!response.IsTruncated;
+			nextToken = response.NextContinuationToken ?? undefined;
+
+		} while (hasMore);
+
+		return bucketFiles.map((file) => ({
+			epochMilliSeconds: dayjs(file.LastModified).valueOf(),
+			data: file,
+		}));
+	},
 };
 
 export const newFile = createTrigger({
-  auth: amazonS3Auth,
-  name: 'new_file',
-  displayName: 'New File',
-  description: 'Trigger when a new file is uploaded.',
-  props: {
-    folderPath: Property.ShortText({
-      displayName: 'Folder Path',
-      required: false,
-    }),
-  },
-  type: TriggerStrategy.POLLING,
-  onEnable: async (context) => {
-    await pollingHelper.onEnable(polling, {
-      auth: context.auth,
-      store: context.store,
-      propsValue: context.propsValue,
-    });
-  },
-  onDisable: async (context) => {
-    await pollingHelper.onDisable(polling, {
-      auth: context.auth,
-      store: context.store,
-      propsValue: context.propsValue,
-    });
-  },
-  run: async (context) => {
-    return await pollingHelper.poll(polling, {
-      auth: context.auth,
-      store: context.store,
-      propsValue: context.propsValue,
-      files: context.files,
-    });
-  },
-  test: async (context) => {
-    return await pollingHelper.test(polling, {
-      auth: context.auth,
-      store: context.store,
-      propsValue: context.propsValue, 
-      files: context.files,
-    });
-  },
+	auth: amazonS3Auth,
+	name: 'new_file',
+	displayName: 'New or Updated File',
+	description: 'Trigger when a new file is added or an existing file is updated.',
+	props: {
+		folderPath: Property.ShortText({
+			displayName: 'Folder Path',
+			required: false,
+		}),
+	},
+	type: TriggerStrategy.POLLING,
+	onEnable: async (context) => {
+		await pollingHelper.onEnable(polling, {
+			auth: context.auth,
+			store: context.store,
+			propsValue: context.propsValue,
+		});
+	},
+	onDisable: async (context) => {
+		await pollingHelper.onDisable(polling, {
+			auth: context.auth,
+			store: context.store,
+			propsValue: context.propsValue,
+		});
+	},
+	run: async (context) => {
+		return await pollingHelper.poll(polling, {
+			auth: context.auth,
+			store: context.store,
+			propsValue: context.propsValue,
+			files: context.files,
+		});
+	},
+	test: async (context) => {
+		return await pollingHelper.test(polling, {
+			auth: context.auth,
+			store: context.store,
+			propsValue: context.propsValue,
+			files: context.files,
+		});
+	},
 
-  sampleData: {
-    Key: 'myfolder/100-3.png',
-    LastModified: '2023-08-04T13:51:26.000Z',
-    ETag: '"e9f16cce12352322272525f5af65a2e"',
-    Size: 40239,
-    StorageClass: 'STANDARD',
-  },
+	sampleData: {
+		Key: 'myfolder/100-3.png',
+		LastModified: '2023-08-04T13:51:26.000Z',
+		ETag: '"e9f16cce12352322272525f5af65a2e"',
+		Size: 40239,
+		StorageClass: 'STANDARD',
+	},
 });
