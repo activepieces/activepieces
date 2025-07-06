570e13cd3bcf120ea20e566745a65910de22677f
Sanket6652 <sanketnannaware96@gmail.com>
2025-07-06 21:21:31 +0530

    fix: airtop action

--- packages/pieces/community/airtop/src/lib/common/auth.ts
+++ packages/pieces/community/airtop/src/lib/common/auth.ts
@@ -1,14 +1,29 @@
-import { PieceAuth } from "@activepieces/pieces-framework";
-import { makeRequest } from ".";
-import { HttpMethod } from "@activepieces/pieces-common";
+import { PieceAuth } from '@activepieces/pieces-framework';
+import { makeRequest } from '.';
+import { HttpMethod } from '@activepieces/pieces-common';
 
 export const airtopAuth = PieceAuth.SecretText({
   displayName: 'API Key',
   description: `You can get your API key from [Airtop Dashboard](https://portal.airtop.ai/api-keys).`,
   required: true,
   validate: async ({ auth }) => {
-    const apiKey = auth as string;
-    const response = await makeRequest(apiKey, HttpMethod.GET, '/sessions');
-    return response;
+    try {
+      const apiKey = auth as string;
+      await makeRequest(
+        apiKey,
+        HttpMethod.GET,
+        '/sessions',
+        undefined,
+        undefined
+      );
+      return {
+        valid: true,
+      };
+    } catch {
+      return {
+        valid: false,
+        error: 'Invalid API Key.',
+      };
+    }
   },
 });
--- packages/pieces/community/airtop/src/lib/common/props.ts
+++ packages/pieces/community/airtop/src/lib/common/props.ts
@@ -2,8 +2,6 @@ import { Property, DropdownOption } from '@activepieces/pieces-framework';
 import { makeRequest } from '.';
 import { HttpMethod } from '@activepieces/pieces-common';
 
-
-
 export const sessionIdDropdown = Property.Dropdown({
   displayName: 'Session',
   description: 'Select a session to create the window in',
@@ -26,10 +24,11 @@ export const sessionIdDropdown = Property.Dropdown({
       undefined
     );
 
-    const options: DropdownOption<string>[] = sessions.data?.map((session: any) => ({
-      label: `Session ${session.id} (${session.status})`,
-      value: session.id,
-    })) || [];
+    const options: DropdownOption<string>[] =
+      sessions.data.sessions?.map((session: any) => ({
+        label: `Session ${session.id} (${session.status})`,
+        value: session.id,
+      })) || [];
 
     return {
       disabled: false,
@@ -60,10 +59,81 @@ export const windowIdDropdown = Property.Dropdown({
       undefined
     );
 
-    const options: DropdownOption<string>[] = windows.data?.map((window: any) => ({
-      label: `Window ${window.id} (${window.url || 'No URL'})`,
-      value: window.id,
-    })) || [];
+    const options: DropdownOption<string>[] =
+      windows.data.windows?.map((window: any) => ({
+        label: `Window ${window.windowId} (${window.url || 'No URL'})`,
+        value: window.windowId,
+      })) || [];
+
+    return {
+      disabled: false,
+      options,
+    };
+  },
+});
+
+export const fileIdDropdown = Property.Dropdown({
+  displayName: 'File',
+  description: 'Select a file to use in the action',
+  required: true,
+  refreshers: [],
+  options: async ({ auth }) => {
+    if (!auth) {
+      return {
+        disabled: true,
+        placeholder: 'Please connect your airtop account',
+        options: [],
+      };
+    }
+
+    const apiKey = auth as string;
+    const files = await makeRequest(
+      apiKey,
+      HttpMethod.GET,
+      '/files?offset=0&limit=50',
+      undefined
+    );
+
+    const options: DropdownOption<string>[] =
+      files.data.files?.map((file: any) => ({
+        label: `${file.fileName} (${file.fileType}) - ${file.id}`,
+        value: file.id,
+      })) || [];
+
+    return {
+      disabled: false,
+      options,
+    };
+  },
+});
+
+export const sessionIdsMultiselectDropdown = Property.MultiSelectDropdown({
+  displayName: 'Session IDs',
+  description: 'Select one or more sessions to make the file available on',
+  required: true,
+  refreshers: [],
+  options: async ({ auth }) => {
+    if (!auth) {
+      return {
+        disabled: true,
+        placeholder: 'Please connect your airtop account',
+        options: [],
+      };
+    }
+
+    const apiKey = auth as string;
+    const sessions = await makeRequest(
+      apiKey,
+      HttpMethod.GET,
+      '/sessions?offset=0&limit=50',
+      undefined
+    );
+
+    const options =
+      sessions.data.sessions?.map((session: any) => ({
+        label: `Session ${session.id} (${session.status})`,
+        value: session.id,
+      })) || [];
 
     return {
       disabled: false,
