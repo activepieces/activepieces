0068ac5c7313bfed90a9f2d1af4c9d687111187f
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-07-08 14:51:00 +0300

    chore: remove warning color for testing action/trigger note

--- packages/react-ui/src/app/builder/test-step/test-trigger-section/simulation-section.tsx
+++ packages/react-ui/src/app/builder/test-step/test-trigger-section/simulation-section.tsx
@@ -38,7 +38,7 @@ export const SimulationNote = ({
       </div>
 
       {note && (
-        <Alert className="bg-warning/5 border-warning/5 ">
+        <Alert >
           <AlertCircle className="h-4 w-4 text-warning" />
           <div className="flex flex-col gap-1">
             <AlertTitle>{t('Action Required')}:</AlertTitle>
