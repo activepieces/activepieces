cc2601c2eb09291bcc3151f31d0b79049c19cfa6
Rupal Barman <rupalbarman@gmail.com>
2025-07-09 11:40:46 +0700

    fix: change support url to point to promptx (#164)

--- packages/shared/src/lib/support-url.ts
+++ packages/shared/src/lib/support-url.ts
@@ -1,1 +1,2 @@
-export const supportUrl = 'https://community.activepieces.com'
+// export const supportUrl = 'https://community.activepieces.com'
+export const supportUrl = 'https://promptxai.com/promptx/contact.html'
