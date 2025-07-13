1cad0e40a19512da4d358db6559d38512d80cf61
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-07-13 16:54:21 +0000

    fix: update piece version in migrate-v2-agent-piece migration to 0.2.0

--- packages/shared/src/lib/flows/operations/migrations/migrate-v2-agent-piece.ts
+++ packages/shared/src/lib/flows/operations/migrations/migrate-v2-agent-piece.ts
@@ -9,7 +9,7 @@ export const migrateAgentPiece: Migration = {
             const newVersion = flowStructureUtil.transferFlow(flowVersion, (step) => {
                 if ((step.type === 'PIECE' || step.type === 'PIECE_TRIGGER') && step.settings.pieceName === '@activepieces/piece-agent') {
                     const pieceStep = step
-                    pieceStep.settings.pieceVersion = '2.0.0'
+                    pieceStep.settings.pieceVersion = '0.2.0'
                     return pieceStep
                 }
                 return step
