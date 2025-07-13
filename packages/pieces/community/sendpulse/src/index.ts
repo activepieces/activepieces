030e3f307ed64ff1a36d9351da1abcc860ebd933
sparkybug <ukaegbuosinachi15@gmail.com>
2025-07-13 16:22:01 +0100

    feat: sendpulse piece

--- packages/pieces/community/sendpulse/src/index.ts
+++ packages/pieces/community/sendpulse/src/index.ts
@@ -1,22 +1,32 @@
+import { createPiece } from "@activepieces/pieces-framework";
+import { addSubscriber } from './lib/actions/add-subscriber';
+import { updateSubscriber } from './lib/actions/update-subscriber';
+import { unsubscribeUser } from './lib/actions/unsubscribe-user';
+import { deleteContact } from './lib/actions/delete-contact';
+import { changeVariableForSubscriber } from './lib/actions/change-variable-for-subscriber';
+import { newSubscriberTrigger } from './lib/triggers/trigger-new-subscriber';
+import { updatedSubscriberTrigger } from './lib/triggers/trigger-updated-subscriber';
+import { unsubscriberTrigger } from './lib/triggers/trigger-unsubscriber';
+import { sendPulseAuth } from './lib/common/auth';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
 
-    import { createPiece } from "@activepieces/pieces-framework";
-    import { addSubscriber } from './lib/actions/add-subscriber';
-    import { updateSubscriber } from './lib/actions/update-subscriber';
-    import { unsubscribeUser } from './lib/actions/unsubscribe-user';
-    import { deleteContact } from './lib/actions/delete-contact';
-    import { changeVariableForSubscriber } from './lib/actions/change-variable-for-subscriber';
-    import { newSubscriberTrigger } from './lib/triggers/trigger-new-subscriber';
-    import { updatedSubscriberTrigger } from './lib/triggers/trigger-updated-subscriber';
-    import { unsubscriberTrigger } from './lib/triggers/trigger-unsubscriber';
-    import { sendPulseAuth } from './lib/common/auth';
-
-    export const sendpulse = createPiece({
-      displayName: "Sendpulse",
-      minimumSupportedRelease: '0.36.1',
-      logoUrl: "https://cdn.activepieces.com/pieces/sendpulse.png",
-      authors: ['sparkybug'],
+export const sendpulse = createPiece({
+  displayName: "Sendpulse",
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: "https://cdn.activepieces.com/pieces/sendpulse.png",
+  authors: ['sparkybug'],
+  auth: sendPulseAuth,
+  actions: [
+    addSubscriber,
+    updateSubscriber,
+    unsubscribeUser,
+    deleteContact,
+    changeVariableForSubscriber,
+    createCustomApiCallAction({
       auth: sendPulseAuth,
-      actions: [addSubscriber, updateSubscriber, unsubscribeUser, deleteContact, changeVariableForSubscriber],
-      triggers: [newSubscriberTrigger, updatedSubscriberTrigger, unsubscriberTrigger],
-    });
+      baseUrl: () => 'https://api.sendpulse.com',
+    }),
+  ],
+  triggers: [newSubscriberTrigger, updatedSubscriberTrigger, unsubscriberTrigger],
+});
     
