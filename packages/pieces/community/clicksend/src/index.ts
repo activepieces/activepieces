a77233c982542e70b058a7d4e00acb6e2cd656af
Sanket6652 <sanketnannaware96@gmail.com>
2025-07-14 16:29:06 +0530

    feat: clicksend auth validation

--- packages/pieces/community/clicksend/src/index.ts
+++ packages/pieces/community/clicksend/src/index.ts
@@ -13,9 +13,13 @@ import { searchContactByEmail } from './lib/actions/search-contact-by-email';
 import { searchContactByPhone } from './lib/actions/search-contact-by-phone';
 import { searchContactLists } from './lib/actions/search-contact-lists';
 import { newIncomingSms } from './lib/triggers/new-incoming-sms';
+import { makeRequest } from './lib/common';
+import { HttpMethod } from '@activepieces/pieces-common';
 
 export const clicksendAuth = PieceAuth.BasicAuth({
   required: true,
+  description: `username: Your API username
+             password: Your API key You can get your API credentials by clicking 'API Credentials' on the top right of the dashboard.`,
   username: Property.ShortText({
     displayName: 'Username',
     description: 'Your ClickSend API username',
@@ -26,6 +30,28 @@ export const clicksendAuth = PieceAuth.BasicAuth({
     description: 'Your ClickSend API key',
     required: true,
   }),
+  validate: async ({ auth }) => {
+    if (auth) {
+      try {
+        const { username, password } = auth;
+        const apiKey = `${username}:${password}`;
+        await makeRequest(apiKey, HttpMethod.GET, '/account')
+        return {
+          valid: true,
+        }
+      } catch {
+        return {
+          valid: false,
+          error: 'Invalid Api Key'
+        }
+      }
+
+    }
+    return {
+      valid: false,
+      error: 'Enter Api Key'
+    }
+  }
 });
 
 export const clicksend = createPiece({
