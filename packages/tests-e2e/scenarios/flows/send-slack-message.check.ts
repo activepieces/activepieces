1fda30ca3e9835b7694fa0b01811c169e60c527e
Mo AbuAboud <moabuaboud@gmail.com>
2025-07-17 18:37:37 +0200

    fix: update location

--- packages/tests-e2e/scenarios/flows/send-slack-message.check.ts
+++ packages/tests-e2e/scenarios/flows/send-slack-message.check.ts
@@ -4,7 +4,7 @@ import { BrowserCheck } from 'checkly/constructs'
 new BrowserCheck('send-slack-message-check', {
   name: 'Send Slack Message',
   frequency: 10, // every 10 minutes
-  locations: ['us-east-1', 'eu-west-1'],
+  locations: ['eu-west-1'],
   code: {
     entrypoint: path.join(__dirname, 'send-slack-message.spec.ts')
   },
