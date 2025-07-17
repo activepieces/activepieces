73a14899c521448b0bbc810c557a11ebcdae140d
Daniel Poon <17039704+danielpoonwj@users.noreply.github.com>
2025-07-17 19:05:22 +0800

    feat: add new action to pdf piece - pdfPageCount

--- packages/pieces/community/pdf/src/index.ts
+++ packages/pieces/community/pdf/src/index.ts
@@ -3,14 +3,20 @@ import { extractText } from './lib/actions/extract-text';
 import { convertToImage } from './lib/actions/convert-to-image';
 import { textToPdf } from './lib/actions/text-to-pdf';
 import { imageToPdf } from './lib/actions/image-to-pdf';
+import { pdfPageCount } from './lib/actions/pdf-page-count';
 
 export const PDF = createPiece({
   displayName: 'PDF',
   auth: PieceAuth.None(),
   minimumSupportedRelease: '0.34.2',
   logoUrl: 'https://cdn.activepieces.com/pieces/pdf.svg',
-  authors: ['nyamkamunhjin', 'abuaboud','AbdulTheActivepiecer','jmgb27'],
-  actions: [extractText, convertToImage, textToPdf,imageToPdf],
+  authors: [
+    'nyamkamunhjin',
+    'abuaboud',
+    'AbdulTheActivepiecer',
+    'jmgb27',
+    'danielpoonwj',
+  ],
+  actions: [extractText, convertToImage, textToPdf, imageToPdf, pdfPageCount],
   triggers: [],
 });
-
