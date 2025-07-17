8ee027dfeed6f228aff9fc068e452e1884b1586e
Mo AbuAboud <moabuaboud@gmail.com>
2025-07-17 18:37:57 +0200

    fix: update location

--- packages/tests-e2e/scenarios/flows/webhook-should-return-response.check.ts
+++ packages/tests-e2e/scenarios/flows/webhook-should-return-response.check.ts
@@ -4,7 +4,7 @@ import { BrowserCheck } from 'checkly/constructs'
 new BrowserCheck('webhook-should-return-response-check', {
   name: 'Webhook Should Return Response',
   frequency: 30, // every 30 minutes
-  locations: ['us-east-1', 'eu-west-1'],
+  locations: ['eu-west-1'],
   code: {
     entrypoint: path.join(__dirname, 'webhook-should-return-response.spec.ts')
   }
