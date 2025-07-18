ea99ce407f1655d3cf61c85aad1e54c36423ca69
Jérémie Preault <89027122+Jeremie-Preault@users.noreply.github.com>
2025-07-18 10:04:53 +0200

    Update index.ts

--- packages/pieces/community/bamboohr/src/index.ts
+++ packages/pieces/community/bamboohr/src/index.ts
@@ -4,7 +4,7 @@ import {
   Property,
 } from '@activepieces/pieces-framework';
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
-import { reportFieldChanged } from './lib/triggers/report-field-changed'; // Add this import
+import { reportFieldChanged } from './lib/triggers/report-field-changed';
 
 export const bambooHrAuth = PieceAuth.CustomAuth({
   required: true,
