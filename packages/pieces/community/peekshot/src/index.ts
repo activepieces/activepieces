16d26792e530b6ae9e25030d82afe605b276dda1
Kishan Parmar <code.kishanparmar@gmail.com>
2025-07-22 17:40:48 +0530

    fix(peekshot): fix action

--- packages/pieces/community/peekshot/src/index.ts
+++ packages/pieces/community/peekshot/src/index.ts
@@ -1,19 +1,51 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { captureScreenshot } from './lib/actions/capture-screenshot';
+import { PieceCategory } from '@activepieces/shared';
+import {
+  createCustomApiCallAction,
+  httpClient,
+  HttpMethod,
+} from '@activepieces/pieces-common';
 
-    import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-    import { captureScreenshot } from "./lib/actions/capture-screenshot";
+export const peekshotAuth = PieceAuth.SecretText({
+  displayName: 'API Key',
+  required: true,
+  description: `You can obtain your API key by navigating to [API Keys](https://dashboard.peekshot.com/dashboard/api-keys) menu.`,
+  validate: async ({ auth }) => {
+    try {
+      await httpClient.sendRequest({
+        method: HttpMethod.GET,
+        url: 'https://api.peekshot.com/api/v1/projects',
+        headers: {
+          'x-api-key': auth as string,
+          'Content-Type': 'application/json',
+        },
+      });
+      return { valid: true };
+    } catch {
+      return { valid: false, error: 'Invalid API Key.' };
+    }
+  },
+});
 
-    export const peekshotAuth = PieceAuth.SecretText({
-      displayName: 'API Key',
-      required: true,
-      description: 'Please provide your API Key',
-    });
-
-    export const peekshot = createPiece({
-      displayName: 'PeekShot',
+export const peekshot = createPiece({
+  displayName: 'PeekShot',
+  auth: peekshotAuth,
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: 'https://cdn.activepieces.com/pieces/peekshot.png',
+  categories: [PieceCategory.PRODUCTIVITY],
+  authors: ['balwant1707'],
+  actions: [
+    captureScreenshot,
+    createCustomApiCallAction({
       auth: peekshotAuth,
-      minimumSupportedRelease: '0.36.1',
-      logoUrl: 'https://peekshot.com/logo.png',
-      authors: [],
-      actions: [captureScreenshot],
-      triggers: [],
-    });
+      baseUrl: () => 'https://api.peekshot.com/api/v1',
+      authMapping: async (auth) => {
+        return {
+          'x-api-key': auth as string,
+        };
+      },
+    }),
+  ],
+  triggers: [],
+});
