566e7b09c3117aa8494d7ea0a91b1dcc8feee181
Sanket6652 <sanketnannaware96@gmail.com>
2025-07-23 18:52:06 +0530

    fix: bitly action fixed

--- packages/pieces/community/bitly/src/index.ts
+++ packages/pieces/community/bitly/src/index.ts
@@ -1,5 +1,11 @@
 import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 import { BitlyAuth } from './lib/common/auth';
+import { arch } from 'os';
+import { getBitlinkDetails } from './lib/actions/get-bitlink-details';
+import { createBitlink } from './lib/actions/create-bitlink';
+import { archiveBitlink } from './lib/actions/archive-bitlink';
+import { createQrCode } from './lib/actions/create-qr-code';
+import { updateBitlink } from './lib/actions/update-bitlink';
 
 export const bitly = createPiece({
   displayName: 'Bitly',
@@ -7,6 +13,12 @@ export const bitly = createPiece({
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/bitly.png',
   authors: ['Sanket6652'],
-  actions: [],
+  actions: [
+    archiveBitlink,
+    createBitlink,
+    createQrCode,
+    getBitlinkDetails,
+    updateBitlink,  
+  ],
   triggers: [],
 });
