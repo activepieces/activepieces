cb1362a0b1b029782bb78b44f309ccc2e7cf4275
aryel780 <aryelvalen306@gmail.com>
2025-07-24 20:31:13 +0530

    fix(hunter): index.ts

--- packages/pieces/community/hunter/src/index.ts
+++ packages/pieces/community/hunter/src/index.ts
@@ -2,6 +2,15 @@ import { createCustomApiCallAction } from '@activepieces/pieces-common';
 import { createPiece } from '@activepieces/pieces-framework';
 import { hunterIoAuth } from './lib/common/auth';
 import { addRecipientsAction } from './lib/actions/add-recipients';
+import { countEmailsAction } from './lib/actions/count-emails';
+import { createLeadAction } from './lib/actions/create-lead';
+import { deleteLeadAction } from './lib/actions/delete-lead';
+import { findEmailAction } from './lib/actions/find-emai';
+import { getLeadAction } from './lib/actions/get-lead';
+import { searchLeadsAction } from './lib/actions/search-leads';
+import { updateLeadAction } from './lib/actions/update-lead';
+import { verifyEmailAction } from './lib/actions/verify-email';
+import { newLeadCreatedTrigger } from './lib/triggers/new-lead';
 
 export const hunterIo = createPiece({
   displayName: 'Hunter',
@@ -11,17 +20,24 @@ export const hunterIo = createPiece({
   authors: ['activepieces-community'],
   actions: [
     addRecipientsAction,
-    
+    countEmailsAction,
+    createLeadAction,
+    deleteLeadAction,
+    findEmailAction,
+    getLeadAction,
+    searchLeadsAction,
+    updateLeadAction,
+    verifyEmailAction,
     createCustomApiCallAction({
       auth: hunterIoAuth,
       baseUrl: () => 'https://api.hunter.io/v2',
       authMapping: async (auth) => {
         const { apiKey } = auth as { apiKey: string };
         return {
-          'api_key': apiKey,
+          api_key: apiKey,
         };
       },
     }),
   ],
-  triggers: [],
+  triggers: [ newLeadCreatedTrigger ],
 });
