7c6f73858c4ad46eb24958a3835c12200420d812
David Anyatonwu <davidanyatonwu@gmail.com>
2025-07-24 04:06:21 +0100

    feat(bitly): add description and marketing category

--- packages/pieces/community/bitly/src/index.ts
+++ packages/pieces/community/bitly/src/index.ts
@@ -1,5 +1,6 @@
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
 import { createPiece } from '@activepieces/pieces-framework';
+import { PieceCategory } from '@activepieces/shared';
 import { bitlyAuth } from './lib/common/auth';
 import { newBitlinkCreatedTrigger } from './lib/triggers/new-bitlink-created';
 import { archiveBitlinkAction } from './lib/actions/archive-bitlink';
@@ -10,10 +11,12 @@ import { updateBitlinkAction } from './lib/actions/update-bitlink';
 
 export const bitly = createPiece({
   displayName: 'Bitly',
+  description: 'URL shortening and link management platform with analytics.',
   auth: bitlyAuth,
   minimumSupportedRelease: '0.20.0',
   logoUrl: 'https://cdn.activepieces.com/pieces/bitly.png',
   authors: ['aryel780'],
+  categories: [PieceCategory.MARKETING],
   actions: [
     archiveBitlinkAction,
     createBitlinkAction,
