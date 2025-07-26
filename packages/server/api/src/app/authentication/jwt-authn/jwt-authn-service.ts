66dd3f6aede780d3c2a07767f51c466f6256a9d3
ProPhone <dallas@thereynoldsgroup.net>
2025-07-26 15:49:57 -0500

    update

---
+++ packages/server/api/src/app/authentication/jwt-authn/jwt-authn-service.ts
@@ -0,0 +1,160 @@
+import { createHash } from 'crypto'
+import { cryptoUtils } from '@activepieces/server-shared'
+import {
+    AuthenticationResponse,
+    isNil,
+    PlatformRole,
+    PrincipalType,
+    Project,
+    User,
+    UserIdentity,
+    UserIdentityProvider,
+} from '@activepieces/shared'
+import { FastifyBaseLogger } from 'fastify'
+import { accessTokenManager } from '../lib/access-token-manager'
+import { userIdentityService } from '../user-identity/user-identity-service'
+import { platformService } from '../../platform/platform.service'
+import { projectService } from '../../project/project-service'
+import { userService } from '../../user/user-service'
+import { jwtTokenExtractor } from './lib/jwt-token-extractor'
+
+export const jwtAuthnService = (log: FastifyBaseLogger) => ({
+    async externalToken({
+        externalAccessToken,
+    }: AuthenticateParams): Promise<AuthenticationResponse> {
+        const externalPrincipal = await jwtTokenExtractor(log).extract(
+            externalAccessToken,
+        )
+
+        const { project, isNewProject } = await getOrCreateProject({
+            platformId: externalPrincipal.platformId,
+            externalProjectId: externalPrincipal.externalProjectId,
+        })
+
+        const user = await getOrCreateUser(externalPrincipal, log)
+
+        const identity = await userIdentityService(log).getOneOrFail({
+            id: user.identityId,
+        })
+
+        const token = await accessTokenManager.generateToken({
+            id: user.id,
+            type: PrincipalType.USER,
+            projectId: project.id,
+            platform: {
+                id: externalPrincipal.platformId,
+            },
+            tokenVersion: identity.tokenVersion,
+        }, 7 * 24 * 60 * 60)
+        return {
+            id: user.id,
+            platformRole: user.platformRole,
+            status: user.status,
+            externalId: user.externalId,
+            platformId: user.platformId,
+            firstName: identity.firstName,
+            lastName: identity.lastName,
+            email: identity.email,
+            trackEvents: identity.trackEvents,
+            newsLetter: identity.newsLetter,
+            verified: identity.verified,
+            token,
+            projectId: project.id,
+        }
+    },
+})
+
+async function getOrCreateProject({
+    platformId,
+    externalProjectId,
+}: GetOrCreateProjectParams): Promise<{ project: Project, isNewProject: boolean }> {
+    const existingProject = await projectService.getByPlatformIdAndExternalId({
+        platformId,
+        externalId: externalProjectId,
+    })
+
+    if (existingProject) {
+        return { project: existingProject, isNewProject: false }
+    }
+
+    const platform = await platformService.getOneOrThrow(platformId)
+
+    const project = await projectService.create({
+        displayName: externalProjectId,
+        ownerId: platform.ownerId,
+        platformId,
+        externalId: externalProjectId,
+    })
+
+    return { project, isNewProject: true }
+}
+
+const getOrCreateUser = async (
+    params: GetOrCreateUserParams,
+    log: FastifyBaseLogger,
+): Promise<User> => {
+    const existingUser = await userService.getByPlatformAndExternalId({
+        platformId: params.platformId,
+        externalId: params.externalUserId,
+    })
+
+    if (!isNil(existingUser)) {
+        return existingUser
+    }
+    const identity = await getOrCreateUserIdentity(params, log)
+    const user = await userService.create({
+        externalId: params.externalUserId,
+        platformId: params.platformId,
+        identityId: identity.id,
+        platformRole: PlatformRole.MEMBER,
+    })
+    return user
+}
+
+const getOrCreateUserIdentity = async (
+    params: GetOrCreateUserParams,
+    log: FastifyBaseLogger,
+): Promise<UserIdentity> => {
+    const cleanedEmail = generateEmailHash(params)
+    const existingIdentity = await userIdentityService(log).getIdentityByEmail(cleanedEmail)
+    if (!isNil(existingIdentity)) {
+        return existingIdentity
+    }
+    const identity = await userIdentityService(log).create({
+        email: cleanedEmail,
+        password: await cryptoUtils.generateRandomPassword(),
+        firstName: params.externalFirstName,
+        lastName: params.externalLastName,
+        trackEvents: true,
+        newsLetter: false,
+        provider: UserIdentityProvider.JWT,
+        verified: true,
+    })
+    return identity
+}
+
+function generateEmailHash(params: { platformId: string, externalUserId: string }): string {
+    const inputString = `jwt_${params.platformId}_${params.externalUserId}`
+    return cleanEmailOtherwiseCompareFails(createHash('sha256').update(inputString).digest('hex'))
+}
+
+function cleanEmailOtherwiseCompareFails(email: string): string {
+    return email.trim().toLowerCase()
+}
+
+type AuthenticateParams = {
+    externalAccessToken: string
+}
+
+type GetOrCreateUserParams = {
+    platformId: string
+    externalUserId: string
+    externalProjectId: string
+    externalFirstName: string
+    externalLastName: string
+}
+
+type GetOrCreateProjectParams = {
+    platformId: string
+    externalProjectId: string
+} 
