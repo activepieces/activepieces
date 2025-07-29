0875b40c408b0605efede7563d24ef335bc3456c
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-07-29 12:35:48 +0300

    fix: drag overlay

--- packages/react-ui/src/app/builder/flow-canvas/step-drag-overlay.tsx
+++ packages/react-ui/src/app/builder/flow-canvas/step-drag-overlay.tsx
@@ -1,9 +1,10 @@
 import { t } from 'i18next';
 
 import { stepsHooks } from '@/features/pieces/lib/steps-hooks';
-import { Action, Trigger } from '@activepieces/shared';
+import { Action, PieceCategory, Trigger } from '@activepieces/shared';
 
 import { flowUtilConsts } from './utils/consts';
+import { cn } from '@/lib/utils';
 
 const StepDragOverlay = ({
   step,
@@ -19,15 +20,18 @@ const StepDragOverlay = ({
     flowUtilConsts.STEP_DRAG_OVERLAY_WIDTH / 2 -
     lefSideBarContainerWidth
   }px`;
-  const top = `${cursorPosition.y - flowUtilConsts.STEP_DRAG_OVERLAY_HEIGHT}px`;
+  const top = `${cursorPosition.y - flowUtilConsts.STEP_DRAG_OVERLAY_HEIGHT - 15}px`;
   const { stepMetadata } = stepsHooks.useStepMetadata({
     step,
   });
+  const isRouneded = stepMetadata?.categories.includes(PieceCategory.FLOW_CONTROL);
 
   return (
     <div
       className={
-        'p-4 absolute left-0 top-0  opacity-75  flex items-center justify-center rounded-lg border border-solid border bg-white'
+        cn('p-1 absolute left-0 top-0  opacity-75  flex items-center justify-center rounded-lg border border-solid border bg-white', {
+          'rounded-full': isRouneded,
+        })
       }
       style={{
         left,
@@ -38,7 +42,9 @@ const StepDragOverlay = ({
     >
       <img
         id={t('logo')}
-        className={'object-contain left-0 right-0 static'}
+        className={cn('object-contain left-0 right-0 static size-[48px]', {
+          'rounded-full': isRouneded,
+        })}
         src={
           step.settings?.inputUiInfo?.customizedInputs?.logoUrl ??
           stepMetadata?.logoUrl
