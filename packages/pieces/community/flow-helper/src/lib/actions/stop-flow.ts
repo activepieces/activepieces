eff4d294dd30adf615f88961dd82263d7fe691ee
Kishan Parmar <code.kishanparmar@gmail.com>
2025-07-29 13:48:37 +0530

    fix(flow-helper): bump piece version to 0.0.4

--- packages/pieces/community/flow-helper/src/lib/actions/stop-flow.ts
+++ packages/pieces/community/flow-helper/src/lib/actions/stop-flow.ts
@@ -3,14 +3,14 @@ import { createAction} from '@activepieces/pieces-framework';
 export const stopFlow = createAction({
   name: 'stopFlow',
   displayName: 'Stop Flow',
-  description: 'Stops the flow immediately this step is reached',
+  description: 'Stops the flow immediately this step is reached.',
   props: {},
   async run(context) {
     context.run.stop();
 
     return {
       success: true,
-      message: 'Flow stopped successfully',
+      message: 'Flow stopped successfully.',
     };
   },
 });
