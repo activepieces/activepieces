bb1b6080839a449245b56d3e4077ab06f9172f15
Sanket6652 <sanketnannaware96@gmail.com>
2025-08-01 00:23:24 +0530

    feat: action added to indexfile

--- packages/pieces/community/paperform/src/index.ts
+++ packages/pieces/community/paperform/src/index.ts
@@ -1,5 +1,18 @@
 import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 import { PaperformAuth } from './lib/common/auth';
+import { createFormCoupon } from './lib/actions/create-form-coupon';
+import { createFormProduct } from './lib/actions/create-form-product';
+import { createSpace } from './lib/actions/create-space';
+import { deleteFormCoupon } from './lib/actions/delete-form-coupon';
+import { deleteFormProduct } from './lib/actions/delete-form-product';
+import { deleteFormSubmission } from './lib/actions/delete-form-submission';
+import { deletePartialFormSubmission } from './lib/actions/delete-partial-form-submission';
+import { findFormProduct } from './lib/actions/find-form-product';
+import { findForm } from './lib/actions/find-form';
+import { findSpace } from './lib/actions/find-space';
+import { updateFormCoupon } from './lib/actions/update-form-coupon';
+import { updateFormProduct } from './lib/actions/update-form-product';
+import { updateSpace } from './lib/actions/update-space';
 
 export const paperform = createPiece({
   displayName: 'Paperform',
@@ -7,6 +20,20 @@ export const paperform = createPiece({
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/paperform.png',
   authors: ['Sanket6652'],
-  actions: [],
+  actions: [
+    createFormCoupon,
+    createFormProduct,
+    createSpace,
+    deleteFormCoupon,
+    deleteFormProduct,
+    deleteFormSubmission,
+    deletePartialFormSubmission,
+    findFormProduct,
+    findForm,
+    findSpace,
+    updateFormCoupon,
+    updateFormProduct,
+    updateSpace
+  ],
   triggers: [],
 });
