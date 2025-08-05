1dc97152ed7f76ea0a4ce5a761830948cb2fb684
Hazem Adel <hazemadelkhalel@gmail.com>
2025-08-05 00:58:46 +0000

    fix: add sqlite migration

--- packages/server/api/src/app/database/sqlite-connection.ts
+++ packages/server/api/src/app/database/sqlite-connection.ts
@@ -137,6 +137,7 @@ import { AddExternalAgentIdSqlite1753643287673 } from './migration/sqlite/175364
 import { AddParentRunIdToFlowRunSqlite1753719777841 } from './migration/sqlite/1753719777841-AddParentRunIdToFlowRunSqlite'
 import { AddCascadeOnAgentsSqlite1753727589109 } from './migration/sqlite/1753727589109-AddCascadeOnAgentsSqlite'
 import { AddExternalIdToMCPSqlite1753786833156 } from './migration/sqlite/1753786833156-AddExternalIdToMCPSqlite'
+import { AddStepNameToTestInFlowRunEntitySqlite1754355397885 } from './migration/sqlite/1754355397885-AddStepNameToTestInFlowRunEntitySqlite'
 
 const getSqliteDatabaseFilePath = (): string => {
     const apConfigDirectoryPath = system.getOrThrow(AppSystemProp.CONFIG_PATH)
@@ -290,6 +291,7 @@ const getMigrations = (): (new () => MigrationInterface)[] => {
         AddParentRunIdToFlowRunSqlite1753719777841,
         AddCascadeOnAgentsSqlite1753727589109,
         AddExternalIdToMCPSqlite1753786833156,
+        AddStepNameToTestInFlowRunEntitySqlite1754355397885
     ]
     const edition = system.getEdition()
     if (edition !== ApEdition.COMMUNITY) {
