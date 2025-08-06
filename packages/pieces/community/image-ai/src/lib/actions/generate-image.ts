3509aa11fb06f86f37684f991973049d386a3f3a
Amr Elmohamady <amr.osama.elmohamady@gmail.com>
2025-08-06 13:24:43 +0000

    feat(image-ai): add gpt-image-1

--- packages/pieces/community/image-ai/src/lib/actions/generate-image.ts
+++ packages/pieces/community/image-ai/src/lib/actions/generate-image.ts
@@ -16,50 +16,6 @@ export const generateImageAction = createAction({
       required: true,
     }),
     advancedOptions: aiProps({ modelType: 'image' }).advancedOptions,
-    resolution: Property.Dropdown({
-      displayName: 'Resolution',
-      description: 'The resolution to generate the image in.',
-      required: true,
-      refreshers: ['model'],
-      defaultValue: '1024x1024',
-      options: async (propsValue) => {
-        const model = propsValue['model'] as ImageModel;
-
-        let options = [
-          {
-            label: '1024x1024',
-            value: '1024x1024',
-          },
-          {
-            label: '512x512',
-            value: '512x512',
-          },
-          {
-            label: '256x256',
-            value: '256x256',
-          },
-        ];
-        if (model.modelId == 'dall-e-3')
-          options = [
-            {
-              label: '1024x1024',
-              value: '1024x1024',
-            },
-            {
-              label: '1024x1792',
-              value: '1024x1792',
-            },
-            {
-              label: '1792x1024',
-              value: '1792x1024',
-            },
-          ];
-
-        return {
-          options: options,
-        };
-      },
-    }),
   },
   async run(context) {
     const providerName = context.propsValue.provider as string;
@@ -85,9 +41,10 @@ export const generateImageAction = createAction({
     const response = await generateImage({
       model: provider,
       prompt: context.propsValue.prompt,
-      size: context.propsValue.resolution as `${number}x${number}`,
       providerOptions: {
-        ...context.propsValue.advancedOptions,
+        [providerName]: {
+          ...context.propsValue.advancedOptions,
+        },
       },
     });
 
