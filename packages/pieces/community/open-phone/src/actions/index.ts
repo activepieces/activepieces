8838ca008de610bb04ca98f8a99298c7c608474c
Ani-4x <gawandeanimesh@gmail.com>
2025-08-07 18:46:04 +0530

    new file:   packages/pieces/community/mollie/.eslintrc.json 	new file:   packages/pieces/community/mollie/README.md 	new file:   packages/pieces/community/mollie/package.json 	new file:   packages/pieces/community/mollie/project.json 	new file:   packages/pieces/community/mollie/src/index.ts 	new file:   packages/pieces/community/mollie/src/lib/actions/create-customer.ts 	new file:   packages/pieces/community/mollie/src/lib/actions/create-order.ts 	new file:   packages/pieces/community/mollie/src/lib/actions/create-payment-link.ts 	new file:   packages/pieces/community/mollie/src/lib/actions/create-payment-refund.ts 	new file:   packages/pieces/community/mollie/src/lib/actions/create-payment.ts 	new file:   packages/pieces/community/mollie/src/lib/actions/search-customer.ts 	new file:   packages/pieces/community/mollie/src/lib/actions/search-order.ts 	new file:   packages/pieces/community/mollie/src/lib/actions/search-payment.ts 	new file:   packages/pieces/community/mollie/src/lib/triggers/new-customer.ts 	new file:   packages/pieces/community/mollie/src/lib/triggers/new-invoice.ts 	new file:   packages/pieces/community/mollie/src/lib/triggers/new-order.ts 	new file:   packages/pieces/community/mollie/src/lib/triggers/new-payment-chargeback.ts 	new file:   packages/pieces/community/mollie/src/lib/triggers/new-payment.ts 	new file:   packages/pieces/community/mollie/src/lib/triggers/new-refund.ts 	new file:   packages/pieces/community/mollie/src/lib/triggers/new-settlement.ts 	renamed:    packages/pieces/community/open-phone/tsconfig.json -> packages/pieces/community/mollie/tsconfig.json 	renamed:    packages/pieces/community/open-phone/tsconfig.lib.json -> packages/pieces/community/mollie/tsconfig.lib.json 	deleted:    packages/pieces/community/open-phone/.eslintrc.json 	deleted:    packages/pieces/community/open-phone/README.md 	deleted:    packages/pieces/community/open-phone/package.json 	deleted:    packages/pieces/community/open-phone/project.json 	deleted:    packages/pieces/community/open-phone/src/actions/create-contact.ts 	deleted:    packages/pieces/community/open-phone/src/actions/get-call-summary.ts 	deleted:    packages/pieces/community/open-phone/src/actions/get-call-transcription.ts 	deleted:    packages/pieces/community/open-phone/src/actions/index.ts 	deleted:    packages/pieces/community/open-phone/src/actions/send-a-message.ts 	deleted:    packages/pieces/community/open-phone/src/actions/update-contact.ts 	deleted:    packages/pieces/community/open-phone/src/common/auth.ts 	deleted:    packages/pieces/community/open-phone/src/common/common.ts 	deleted:    packages/pieces/community/open-phone/src/index.ts 	deleted:    packages/pieces/community/open-phone/src/triggers/call-recording-completed.ts 	deleted:    packages/pieces/community/open-phone/src/triggers/incoming-call-completed.ts 	deleted:    packages/pieces/community/open-phone/src/triggers/incoming-message-recieved.ts 	deleted:    packages/pieces/community/open-phone/src/triggers/index.ts 	deleted:    packages/pieces/community/open-phone/src/triggers/outgoing-call-completed.ts 	deleted:    packages/pieces/community/open-phone/src/triggers/outgoing-message-delivered.ts

--- packages/pieces/community/open-phone/src/actions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './create-contact';
-export * from './update-contact';
-export * from './send-a-message';
-export * from './get-call-summary';
-export * from './get-call-transcription';
