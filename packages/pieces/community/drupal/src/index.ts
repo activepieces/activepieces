41203d2c3319e82e2dd60721d3b87324c92384b3
Jürgen Haas <juergen.haas@lakedrops.com>
2025-08-08 13:25:35 +0200

    Initial version of the Drupal Piece

---
+++ packages/pieces/community/drupal/src/index.ts
@@ -0,0 +1,91 @@
+import {
+  HttpMethod,
+  httpClient,
+} from '@activepieces/pieces-common';
+import {
+  PieceAuth,
+  Property,
+  createPiece,
+} from '@activepieces/pieces-framework';
+import { PieceCategory } from '@activepieces/shared';
+import { drupalCallToolAction } from './lib/actions/tools';
+import { drupalPolling } from './lib/triggers/polling';
+import { drupalWebhook } from './lib/triggers/webhook';
+
+const markdownPropertyDescription = `
+First, install the [Drupal Modeler API](https://www.drupal.org/project/modeler_api) module.
+
+Then, after you've enabled the **modeler_api_connect** module, you get your API key from your user profile in Drupal.
+
+Provide the website URL in the format https://www.example.com.
+`;
+
+export const drupalAuth = PieceAuth.CustomAuth({
+  description: markdownPropertyDescription,
+  required: true,
+  props: {
+    website_url: Property.ShortText({
+      displayName: 'Website URL',
+      required: true,
+      description:
+        'URL of the Drupal website, e.g. https://www.example.com',
+    }),
+    api_key: Property.ShortText({
+      displayName: 'API Key',
+      required: true,
+    }),
+  },
+  validate: async ({ auth }) => {
+    const { website_url, api_key } = auth;
+    if (!website_url || !api_key) {
+      return {
+        valid: false,
+        error: 'Please fill all the fields [website_url, api_key]',
+      };
+    }
+    try {
+      const response = await httpClient.sendRequest({
+        method: HttpMethod.GET,
+        url: website_url + `/modeler_api`,
+        headers: {
+          'x-api-key': api_key,
+        },
+      });
+      console.debug('Auth validation response', response);
+      if (response.status === 200) {
+        return {
+          valid: true,
+        };
+      }
+      return {
+        valid: false,
+        error: 'Validation failed with response code ' + response.status,
+      };
+    } catch (e: any) {
+      return {
+        valid: false,
+        error: 'Validation failed: ' + e.message,
+      };
+    }
+  },
+});
+
+export const drupal = createPiece({
+  displayName: 'Drupal',
+  auth: drupalAuth,
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: 'https://www.drupal.org/sites/all/themes/bluecheese/images/drupal-drop-062025.svg',
+  categories: [
+    PieceCategory.ARTIFICIAL_INTELLIGENCE,
+    PieceCategory.BUSINESS_INTELLIGENCE,
+    PieceCategory.COMMERCE,
+    PieceCategory.CONTENT_AND_FILES,
+    PieceCategory.DEVELOPER_TOOLS,
+    PieceCategory.FLOW_CONTROL,
+    PieceCategory.FORMS_AND_SURVEYS,
+    PieceCategory.MARKETING,
+  ],
+  authors: ['jurgenhaas'],
+  actions: [drupalCallToolAction],
+  triggers: [drupalPolling, drupalWebhook],
+});
---
+++ packages/pieces/community/drupal/src/lib/actions/tools.ts
@@ -0,0 +1,123 @@
+import {
+  PiecePropValueSchema,
+  Property,
+  createAction,
+} from '@activepieces/pieces-framework';
+import {
+  httpClient,
+  HttpMethod,
+  HttpRequest,
+} from '@activepieces/pieces-common';
+import { drupalAuth } from '../../';
+type DrupalAuthType = PiecePropValueSchema<typeof drupalAuth>;
+
+export const drupalCallToolAction = createAction({
+  auth: drupalAuth,
+  name: 'drupal-call-tool',
+  displayName: 'Call Tool',
+  description: 'Call a tool on the Drupal site',
+  props: {
+    tool: Property.Dropdown({
+      displayName: 'Tool',
+      description: 'The tool to call.',
+      required: true,
+      refreshers: [],
+      options: async ({ auth }) => {
+        const { website_url, api_key } = (auth as DrupalAuthType);
+        if (!auth) {
+          return {
+            disabled: true,
+            options: [],
+            placeholder: 'Please authenticate first.',
+          };
+        }
+
+        try {
+          const response = await httpClient.sendRequest<DrupalTool[]>({
+            method: HttpMethod.GET,
+            url: website_url + `/modeler_api/tools`,
+            headers: {
+              'x-api-key': api_key,
+            },
+          });
+          console.debug('Tool response', response);
+          if (response.status === 200) {
+            return {
+              disabled: false,
+              options: response.body.map((tool) => {
+                return {
+                  label: tool.label,
+                  description: tool.description,
+                  value: tool,
+                };
+              }),
+            };
+          }
+        } catch (e: any) {
+          console.debug('Tool error', e);
+        }
+        return {
+          disabled: true,
+          options: [],
+          placeholder: 'Error processing tools',
+        };
+      },
+    }),
+    config: Property.DynamicProperties({
+      displayName: 'Tool configuration',
+      refreshers: ['tool'],
+      required: true,
+      props: async ({ tool }) => {
+        console.debug('Tool config input', tool);
+        const fields: Record<string, any> = {};
+        const items = tool['config'] as DrupalToolConfig[];
+        items.forEach((config: any) => {
+          fields[config.key] = Property.ShortText({
+            displayName: config.label,
+            description: config.description,
+            required: config.required,
+          });
+        });
+        console.debug('Field for this tool', fields);
+        return fields;
+      },
+    }),
+  },
+  async run({ auth, propsValue }) {
+    const { website_url, api_key } = (auth as DrupalAuthType);
+    const request: HttpRequest = {
+      method: HttpMethod.POST,
+      url: website_url + `/modeler_api/tool/execute`,
+      body: {
+        tool: propsValue.tool.id,
+        config: propsValue.config,
+      },
+      headers: {
+        'x-api-key': api_key,
+      },
+    };
+
+    const result = await httpClient.sendRequest<DrupalTool>(request);
+    console.debug('Tool call completed', result);
+
+    if (result.status === 200 || result.status === 202) {
+      return result.body;
+    } else {
+      return result;
+    }
+  },
+});
+
+interface DrupalTool {
+  id: string;
+  label: string;
+  description: string;
+  config: DrupalToolConfig[];
+}
+
+interface DrupalToolConfig {
+  key: string;
+  label: string;
+  description: string;
+  required: boolean;
+}
