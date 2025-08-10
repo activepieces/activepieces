ffbe480a1a5561b806ab1ab58d5e8e7676ec7f46
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-08-10 23:22:34 +0000

    fix: padding

--- packages/react-ui/src/app/components/sidebar/index.tsx
+++ packages/react-ui/src/app/components/sidebar/index.tsx
@@ -338,7 +338,7 @@ function ApSidebarMenuGroup(item: SidebarGroup) {
   const location = useLocation();
   return (
     <React.Fragment key={item.label}>
-      <SidebarGroup key={item.name} className="py-2">
+      <SidebarGroup key={item.name} className="py-0.5">
         {item.name && <SidebarGroupLabel>{item.name}</SidebarGroupLabel>}
         <SidebarMenu className="py-0">
           <Collapsible
