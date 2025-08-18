99f8340ae23bb9baa88de2c545c684c617254020
Daniel Poon <17039704+danielpoonwj@users.noreply.github.com>
2025-08-18 18:31:42 +0800

    feat: add maxResults field for unzipFile action

--- packages/pieces/community/file-helper/src/lib/actions/unzip-file.ts
+++ packages/pieces/community/file-helper/src/lib/actions/unzip-file.ts
@@ -6,6 +6,12 @@ interface Result {
   filePath: string;
 }
 
+const maxResultsDescription = `
+Throw an error if zip file has more than expected entries.
+- This is a safe measure when working with untrusted zip files.
+- Exclude this field or set to 0 to skip this check.
+`;
+
 export const unzipFile = createAction({
   name: 'unzipFile',
   displayName: 'Unzip File',
@@ -15,9 +21,21 @@ export const unzipFile = createAction({
       displayName: 'Zip File',
       required: true,
     }),
+    maxResults: Property.Number({
+      displayName: 'Max Results',
+      description: maxResultsDescription,
+      defaultValue: 0,
+      required: false,
+    }),
   },
   async run(context) {
     const zipFile = new AdmZip(context.propsValue.file.data);
+
+    const maxResults = context.propsValue.maxResults || 0;
+
+    if (maxResults !== 0 && zipFile.getEntryCount() > maxResults) {
+      throw `Zip file contains more entries than allowed: ${zipFile.getEntryCount()}`;
+    }
 
     const results: Result[] = [];
     zipFile.forEach(async (zipEntry) => {
