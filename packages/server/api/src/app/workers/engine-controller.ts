0bcb700c4368fc3fa9ad054c6fb7b8f90c791b9c
louai <boumedienelouai2@gmail.com>
2025-08-27 09:23:20 +0000

    fix: never increase tasks on test flow and test step

--- packages/server/api/src/app/workers/engine-controller.ts
+++ packages/server/api/src/app/workers/engine-controller.ts
@@ -101,7 +101,7 @@ export const flowEngineWorker: FastifyPluginAsyncTypebox = async (app) => {
         const runWithoutSteps = await flowRunService(request.log).updateRun({
             flowRunId: runId,
             status: runDetails.status,
-            tasks: runDetails.tasks,
+            tasks: progressUpdateType === ProgressUpdateType.TEST_FLOW ? 0 : runDetails.tasks,
             duration: runDetails.duration,
             projectId: request.principal.projectId,
             tags: runDetails.tags ?? [],
