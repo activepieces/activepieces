c13ee6e68d862f3a1a258114ff69de39a42d7910
codegino <carloginocatapang@gmail.com>
2025-09-02 20:42:09 +0200

    fix(predict-leads): update api used for technology

--- packages/pieces/community/predict-leads/src/lib/actions/technology.ts
+++ packages/pieces/community/predict-leads/src/lib/actions/technology.ts
@@ -3,33 +3,36 @@ import {
   Property,
   createAction,
 } from '@activepieces/pieces-framework';
-import { limitField, makeClient, pageField } from '../common';
+import { firstSeenAtFromField, firstSeenAtUntilField, lastSeenAtFromField, lastSeenAtUntilField, limitField, makeClient, pageField } from '../common';
 import { PredictLeadsAuth } from '../../index';
 import { prepareQuery } from '../common/client';
 
-export const findTechnologiesAction = createAction({
+export const findTechnologiesByCompanyAction = createAction({
   auth: PredictLeadsAuth,
-  name: 'predict-leads_find_technologies',
-  displayName: 'List Technologies',
-  description: 'Retrieves all tracked technologies',
+  name: 'predict-leads_find_technologies_by_domain',
+  displayName: 'List Technologies by domain',
+  description: 'Retrieve technologies used by specific company',
   props: {
-    order_by: Property.StaticDropdown({
-      displayName: 'Order By',
-      required: false,
-      options: {
-        options: [
-          { value: 'created_at_asc', label: 'Created At - Ascending' },
-          { value: 'created_at_desc', label: 'Created At - Descending' },
-        ]
-      }
+    domain: Property.ShortText({
+      displayName: 'Domain',
+      description: 'The domain of the company to find.',
+      required: true,
     }),
+    first_seen_at_from: firstSeenAtFromField,
+    first_seen_at_until: firstSeenAtUntilField,
+    last_seen_at_from: lastSeenAtFromField,
+    last_seen_at_until: lastSeenAtUntilField,
     page: pageField,
     limit: limitField,
   },
   async run(context) {
+    const domain = context.propsValue.domain;
+    const first_seen_at_from = context.propsValue.first_seen_at_from;
+    const first_seen_at_until = context.propsValue.first_seen_at_until;
+    const last_seen_at_from = context.propsValue.last_seen_at_from;
+    const last_seen_at_until = context.propsValue.last_seen_at_until;
     const page = context.propsValue.page ?? 1;
     const limit = context.propsValue.limit ?? 1000;
-    const order_by = context.propsValue.order_by ?? undefined;
 
     const client = makeClient(
       context.auth as PiecePropValueSchema<typeof PredictLeadsAuth>
@@ -37,8 +40,12 @@ export const findTechnologiesAction = createAction({
 
     try {
       const response = await client.findTechnologies(
+        domain,
         prepareQuery({
-          order_by,
+          first_seen_at_from,
+          first_seen_at_until,
+          last_seen_at_from,
+          last_seen_at_until,
           page,
           limit,
         })
@@ -50,27 +57,48 @@ export const findTechnologiesAction = createAction({
   },
 });
 
-export const findTechnologyByIdAction = createAction({
+export const findCompaniesByTechnologyIdAction = createAction({
   auth: PredictLeadsAuth,
-  name: 'predict-leads_find_technology_by_id',
-  displayName: 'Get Technology',
-  description: 'Retrieves a single technology by ID.',
+  name: 'predict-leads_find_companies_by_technology_id',
+  displayName: 'Retrieve companies by technology ID',
+  description: 'Retrieves company using specific technology ID',
   props: {
     id: Property.ShortText({
       displayName: 'ID',
       description: 'The ID of the technology to find.',
       required: true,
     }),
+    first_seen_at_from: firstSeenAtFromField,
+    first_seen_at_until: firstSeenAtUntilField,
+    last_seen_at_from: lastSeenAtFromField,
+    last_seen_at_until: lastSeenAtUntilField,
+    page: pageField,
+    limit: limitField,
   },
   async run(context) {
     const id = context.propsValue.id;
+    const first_seen_at_from = context.propsValue.first_seen_at_from;
+    const first_seen_at_until = context.propsValue.first_seen_at_until;
+    const last_seen_at_from = context.propsValue.last_seen_at_from;
+    const last_seen_at_until = context.propsValue.last_seen_at_until;
+    const page = context.propsValue.page ?? 1;
+    const limit = context.propsValue.limit ?? 1000;
 
     const client = makeClient(
       context.auth as PiecePropValueSchema<typeof PredictLeadsAuth>
     );
 
     try {
-      const response = await client.findTechnologyById(id);
+      const response = await client.findCompaniesTechnologyById(id,
+        prepareQuery({
+          first_seen_at_from,
+          first_seen_at_until,
+          last_seen_at_from,
+          last_seen_at_until,
+          page,
+          limit,
+        })
+      );
       return response;
     } catch (error) {
       throw new Error(JSON.stringify(error, undefined, 2));
