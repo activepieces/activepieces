59eaf8bf61268350e90fadc86922af74900bd682
Olivier Sambourg <AdamSelene@users.noreply.github.com>
2025-09-02 12:19:18 +0200

    Revert "feat: add new action set-metadata to image-helper (#127)" (#130)
    This reverts commit 8a4fc9283118ecce0541df02e2278598d1e90a4c.

--- packages/pieces/community/image-helper/src/index.ts
+++ packages/pieces/community/image-helper/src/index.ts
@@ -5,9 +5,7 @@ import { getMetaData } from './lib/actions/get-metadata.action';
 import { cropImage } from './lib/actions/crop-image.action';
 import { rotateImage } from './lib/actions/rotate-image.action';
 import { resizeImage } from './lib/actions/resize-Image.action';
-import { setMetaData } from './lib/actions/set-metadata.action';
 import { compressImage } from './lib/actions/compress-image.actions';
-import { is_exiftool_installed } from './helpers';
 
 export const imageHelper = createPiece({
   displayName: 'Image Helper',
@@ -16,16 +14,8 @@ export const imageHelper = createPiece({
   auth: PieceAuth.None(),
   minimumSupportedRelease: '0.30.0',
   logoUrl: 'https://cdn.activepieces.com/pieces/image-helper.png',
-  authors: ['AbdullahBitar', 'kishanprmr', 'abuaboud'],
+  authors: ["AbdullahBitar","kishanprmr","abuaboud"],
   categories: [PieceCategory.CORE],
-  actions: [
-    imageToBase64,
-    getMetaData,
-    cropImage,
-    rotateImage,
-    resizeImage,
-    compressImage,
-    ...(is_exiftool_installed() ? [setMetaData] : []),
-  ],
+  actions: [imageToBase64, getMetaData, cropImage, rotateImage, resizeImage, compressImage],
   triggers: [],
 });
