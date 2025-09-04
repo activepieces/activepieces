a24c878bf25b1c338fb2f128dfb74119a3764999
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-09-04 13:21:32 +0000

    feat: enhance refillPollingJobs to include piece metadata and renew configuration checks

--- packages/server/api/src/app/workers/queue/migration/refill-paused-jobs.ts
+++ packages/server/api/src/app/workers/queue/migration/refill-paused-jobs.ts
@@ -50,3 +50,4 @@ export const refillPausedRuns = (log: FastifyBaseLogger) => ({
     },
 })
 
+
--- packages/server/api/src/app/workers/queue/migration/refill-polling-jobs.ts
+++ packages/server/api/src/app/workers/queue/migration/refill-polling-jobs.ts
@@ -1,19 +1,44 @@
-import { FlowTriggerType, LATEST_JOB_DATA_SCHEMA_VERSION, TriggerSourceScheduleType, TriggerStrategy, WorkerJobType } from '@activepieces/shared'
+import { WebhookRenewStrategy } from '@activepieces/pieces-framework'
+import { FlowTriggerType, isNil, LATEST_JOB_DATA_SCHEMA_VERSION, TriggerSourceScheduleType, TriggerStrategy, WorkerJobType } from '@activepieces/shared'
 import { FastifyBaseLogger } from 'fastify'
+import { IsNull } from 'typeorm'
 import { jobQueue } from '..'
+import { flowVersionService } from '../../../flows/flow-version/flow-version.service'
+import { pieceMetadataService } from '../../../pieces/piece-metadata-service'
 import { projectService } from '../../../project/project-service'
 import { triggerSourceRepo } from '../../../trigger/trigger-source/trigger-source-service'
 import { JobType } from '../queue-manager'
 
 export const refillPollingJobs = (log: FastifyBaseLogger) => ({
     async run(): Promise<void> {
-        const triggerSources = await triggerSourceRepo().find({})
+        const triggerSources = await triggerSourceRepo().find({
+            where: {
+                deleted: IsNull(),
+                simulate: false,
+            },
+        })
         for (const triggerSource of triggerSources) {
             if (!triggerSource.schedule) {
                 continue
             }
             switch (triggerSource.type) {
                 case TriggerStrategy.WEBHOOK: {
+                    const pieceMetadata = await pieceMetadataService(log).get({
+                        name: triggerSource.pieceName,
+                        version: triggerSource.pieceVersion,
+                        projectId: triggerSource.projectId,
+                        platformId: await projectService.getPlatformId(triggerSource.projectId),
+                    })
+                    const flowVersion = await flowVersionService(log).getOne(triggerSource.flowVersionId)
+                    if (isNil(flowVersion)) {
+                        continue
+                    }
+                    const pieceTrigger = pieceMetadata?.triggers?.[flowVersion?.trigger.settings.triggerName]
+                    if (isNil(pieceTrigger)
+                        || isNil(pieceTrigger.renewConfiguration)
+                        || pieceTrigger.renewConfiguration.strategy !== WebhookRenewStrategy.CRON) {
+                        continue
+                    }
                     await jobQueue(log).add({
                         id: triggerSource.flowVersionId,
                         type: JobType.REPEATING,
@@ -27,8 +52,8 @@ export const refillPollingJobs = (log: FastifyBaseLogger) => ({
                         },
                         scheduleOptions: {
                             type: TriggerSourceScheduleType.CRON_EXPRESSION,
-                            cronExpression: triggerSource.schedule.cronExpression,
-                            timezone: triggerSource.schedule.timezone,
+                            cronExpression: pieceTrigger.renewConfiguration.cronExpression,
+                            timezone: 'UTC',
                         },
                     })
                     continue
