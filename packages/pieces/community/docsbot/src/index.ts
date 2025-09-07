8a57d63f4f7519cddb753ef882cf7ca23fa7eef9
Kishan Parmar <code.kishanparmar@gmail.com>
2025-09-07 13:19:35 +0530

    fix(docsbot): add piece category and custom api call

--- packages/pieces/community/docsbot/src/index.ts
+++ packages/pieces/community/docsbot/src/index.ts
@@ -4,7 +4,9 @@ import { createBot } from './lib/actions/create-bot';
 import { createSource } from './lib/actions/create-source';
 import { findBot } from './lib/actions/find-bot';
 import { uploadSourceFile } from './lib/actions/upload-source-file';
-import { docsbotAuth } from './lib/common';
+import { docsbotAuth, docsbotCommon } from './lib/common';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { PieceCategory } from '@activepieces/shared';
 
 export const docsbot = createPiece({
   displayName: 'DocsBot',
@@ -12,6 +14,7 @@ export const docsbot = createPiece({
     'DocsBot AI allows you to build AI-powered chatbots that pull answers from your existing documentation and content. This integration enables workflows to ask DocsBot questions and update its training sources dynamically.',
   auth: docsbotAuth,
   minimumSupportedRelease: '0.36.1',
+  categories:[PieceCategory.ARTIFICIAL_INTELLIGENCE,PieceCategory.PRODUCTIVITY],
   logoUrl: 'https://cdn.activepieces.com/pieces/docsbot.png',
   authors: ['LuizDMM'],
   actions: [
@@ -20,6 +23,15 @@ export const docsbot = createPiece({
     uploadSourceFile,
     createBot,
     findBot,
+    createCustomApiCallAction({
+      baseUrl: () => docsbotCommon.baseUrl,
+      auth: docsbotAuth,
+      authMapping: async (auth) => {
+        return {
+          Authorization: `Bearer ${auth}`,
+        };
+      },
+    }),
   ],
   triggers: [],
 });
