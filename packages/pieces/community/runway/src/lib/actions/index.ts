ea0aa3bcdfe7d2bfd129893162eb9ecf2bb7c8bf
naaa760 <neh6a683@gmail.com>
2025-09-08 20:30:30 +0530

    feat: add Runway actions exports index

---
+++ packages/pieces/community/runway/src/lib/actions/index.ts
@@ -0,0 +1,4 @@
+export { generateImageFromText } from './generate-image-from-text';
+export { generateVideoFromImage } from './generate-video-from-image';
+export { getTaskDetails } from './get-task-details';
+export { cancelOrDeleteTask } from './cancel-or-delete-task';
