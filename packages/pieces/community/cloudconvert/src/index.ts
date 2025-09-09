e822f0b2f702cd33b4300a35576be17c35d3f4bd
Prabhukiran161 <prabhukiran161@gmail.com>
2025-09-09 20:43:27 +0530

    feat: adding index

--- packages/pieces/community/cloudconvert/src/index.ts
+++ packages/pieces/community/cloudconvert/src/index.ts
@@ -1,13 +1,29 @@
 
     import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+    import { jobFinished } from './lib/triggers/job-finished.trigger';
+    import { newJobEvent } from './lib/triggers/new-job-event';
+    import { jobFailed } from './lib/triggers/job-failed';
+    import { convertFile } from './lib/actions/convert-file';
+    import { captureWebsite } from './lib/actions/capture-website';
+    import { mergeFiles } from './lib/actions/merge-files';
+    import { downloadFile } from './lib/actions/download-file';
+    import { archiveFiles } from './lib/actions/archive-files';
+    import { optimizeFile } from './lib/actions/optimize-file';
 
     export const cloudconvert = createPiece({
-      displayName: "Cloudconvert",
+      displayName: 'Cloudconvert',
       auth: PieceAuth.None(),
       minimumSupportedRelease: '0.36.1',
-      logoUrl: "https://cdn.activepieces.com/pieces/cloudconvert.png",
-      authors: [],
-      actions: [],
-      triggers: [],
+      logoUrl: 'https://cdn.activepieces.com/pieces/cloudconvert.png',
+      authors: ['Prabhukiran161'],
+      actions: [
+        convertFile,
+        captureWebsite,
+        mergeFiles,
+        downloadFile,
+        archiveFiles,
+        optimizeFile,
+      ],
+      triggers: [jobFinished, newJobEvent, jobFailed],
     });
     
