edb2f377e3e70ed78655e4141949e79769a67ef0
nemian <229107199+0xmoner@users.noreply.github.com>
2025-09-09 09:01:09 -0600

    feat: add convert lead action

--- packages/pieces/community/copper-crm/src/lib/actions/lead.ts
+++ packages/pieces/community/copper-crm/src/lib/actions/lead.ts
@@ -1,9 +1,12 @@
-import { createAction } from "@activepieces/pieces-framework";
+import { createAction, Property } from "@activepieces/pieces-framework";
 import { copperAuth } from "../common/auth";
 import { leadAddress, leadAssigneeId, leadCompanyName, leadCustomerSourceId, leadCustomFields, leadDetails, leadEmails, leadId, leadMonetaryValue, leadName, leadPhoneNumbers, leadSocials, leadStatus, leadTags, leadTitle, leadWebsites } from "../common/lead";
 import { makeCopperRequest } from "../common/request";
 import { HttpMethod } from "@activepieces/pieces-common";
 import { LEADS_API_ENDPOINT } from "../common/constants";
+import { companyId, companyName } from "../common/company";
+import { personAssigneeId, personContactTypeId, personName } from "../common/people";
+import { opportunityAssigneeId, opportunityMonetaryValue, opportunityName, opportunityPipelineId } from "../common/opportunity";
 
 export const createLead = createAction({
     auth: copperAuth,
@@ -96,146 +99,114 @@ export const updateLead = createAction({
     },
 });
 
-// export const convertLead = createAction({
-//     auth: copperAuth,
-//     name: 'convert_lead',
-//     displayName: 'Convert Lead',
-//     description: 'Converts an existing Lead into a Person, and optionally creates a Company and/or Opportunity.',
-//     props: {
-//       lead_id: leadId,
-//       // Person Details
-//       person_name: personName,
-//       person_contact_type_id: personContactTypeId,
-//       on_assignee_id: personAssigneeId,
-//     // Company Details (mutually exclusive company_id / company_name)
-//     //   company_id: company
-//     //   company_name: Property.ShortText({
-//     //     displayName: 'Company Name',
-//     //     description: 'The name of an existing Company to associate, or a new Company to create. Mutually exclusive with "Company ID". Provide "" (empty string) to explicitly *not* create/associate a Company.',
-//     //     required: false,
-//     //   }),
-//     //   company_exact_match: Property.Checkbox({
-//     //     displayName: 'Company Exact Match',
-//     //     description: 'If using "Company Name", set to true for an exact match, otherwise fuzzy matching applies.',
-//     //     required: false,
-//     //     defaultValue: false,
-//     //   }),
-//       // Opportunity Details
-//     //   opportunity_name: Property.ShortText({
-//     //     displayName: 'Opportunity Name',
-//     //     description: 'The name for the new Opportunity record. If not provided, no Opportunity will be created.',
-//     //     required: false,
-//     //   }),
-//     //   opportunity_pipeline_id: Property.Number({
-//     //     displayName: 'Opportunity Pipeline ID',
-//     //     description: 'The unique ID of the Pipeline for the new Opportunity. Required if an Opportunity Name is provided.',
-//     //     required: false,
-//     //   }),
-//     //   opportunity_pipeline_stage_id: Property.Number({
-//     //     displayName: 'Opportunity Pipeline Stage ID',
-//     //     description: 'The unique ID of the Pipeline Stage for the new Opportunity. Defaults to the first stage in the pipeline if Pipeline ID is provided but this is not.',
-//     //     required: false,
-//     //   }),
-//     //   opportunity_monetary_value: Property.Number({
-//     //     displayName: 'Opportunity Monetary Value',
-//     //     description: 'The expected monetary value of the new Opportunity.',
-//     //     required: false,
-//     //   }),
-//     //   opportunity_assignee_id: Property.Number({
-//     //     displayName: 'Opportunity Assignee ID',
-//     //     description: 'The unique ID of the User who will be the owner of the new Opportunity.',
-//     //     required: false,
-//     //   }),
-//     },
-//     async run(context) {
-//       const {
-//         lead_id,
-//         person_name,
-//         person_contact_type_id,
-//         // person_assignee_id,
-//         // company_id,
-//         // company_name,
-//         // company_exact_match,
-//         // opportunity_name,
-//         // opportunity_pipeline_id,
-//         // opportunity_pipeline_stage_id,
-//         // opportunity_monetary_value,
-//         // opportunity_assignee_id,
-//       } = context.propsValue;
+export const convertLead = createAction({
+    auth: copperAuth,
+    name: 'convert_lead',
+    displayName: 'Convert Lead',
+    description: 'Converts an existing Lead into a Person, and optionally creates a Company and/or Opportunity.',
+    props: {
+      lead_id: leadId,
+      person_name: personName,
+      person_contact_type_id: personContactTypeId,
+      person_assignee_id: personAssigneeId,
+      company_id: companyId,
+      company_name: companyName,
+      company_exact_match: Property.Checkbox({
+        displayName: 'Company Exact Match',
+        description: 'If using "Company Name", set to true for an exact match, otherwise fuzzy matching applies.',
+        required: false,
+        defaultValue: false,
+      }),
+      opportunity_name: opportunityName,
+      opportunity_pipeline_id: opportunityPipelineId,
+      opportunity_pipeline_stage_id: opportunityPipelineId,
+      opportunity_monetary_value: opportunityMonetaryValue,
+      opportunity_assignee_id: opportunityAssigneeId
+    },
+    async run(context) {
+      const {
+        lead_id,
+        person_name,
+        person_contact_type_id,
+        person_assignee_id,
+        company_id,
+        company_name,
+        company_exact_match,
+        opportunity_name,
+        opportunity_pipeline_id,
+        opportunity_pipeline_stage_id,
+        opportunity_monetary_value,
+        opportunity_assignee_id,
+      } = context.propsValue;
   
-//       // Build the 'person' object for the conversion details
-//       const person: Record<string, any> = { name: person_name };
-//       if (person_contact_type_id !== undefined) {
-//         person.contact_type_id = person_contact_type_id;
-//       }
-//       if (person_assignee_id !== undefined) {
-//         person.assignee_id = person_assignee_id;
-//       }
+      const person: Record<string, any> = { name: person_name };
+      if (person_contact_type_id !== undefined) {
+        person['contact_type_id'] = person_contact_type_id;
+      }
+      if (person_assignee_id !== undefined) {
+        person['assignee_id'] = person_assignee_id;
+      }
   
-//       // Build the 'company' object for the conversion details
-//       const company: Record<string, any> = {};
-//       if (company_id !== undefined && company_name !== undefined && company_name !== '') {
-//           // According to sources [1], id and name are mutually exclusive.
-//           // Prioritize company_id if both are provided, or throw an error.
-//           throw new Error("Cannot specify both 'Company ID' and 'Company Name'. They are mutually exclusive.");
-//       } else if (company_id !== undefined) {
-//           company.id = company_id;
-//       } else if (company_name !== undefined) {
-//           // Only add company name if it's not undefined.
-//           // An empty string "" is explicitly allowed by the API to prevent company creation [1].
-//           company.name = company_name;
-//           if (company_exact_match !== undefined) {
-//               company.exact_match = company_exact_match;
-//           }
-//       }
-//       // If neither company_id nor company_name is provided, `company` remains empty and won't be sent,
-//       // which correctly means no company is created/associated by default.
+      const company: Record<string, any> = {};
+      if (company_id !== undefined && company_name !== undefined && company_name !== '') {
+          // According to API spec, id and name are mutually exclusive.
+          throw new Error("Cannot specify both 'Company ID' and 'Company Name'. They are mutually exclusive.");
+      } else if (company_id !== undefined) {
+          company['id'] = company_id;
+      } else if (company_name !== undefined) {
+          // An empty string "" is explicitly allowed by the API to prevent company creation [1].
+          company['name'] = company_name;
+          if (company_exact_match !== undefined) {
+              company['exact_match'] = company_exact_match;
+          }
+      }
+      // If neither company_id nor company_name is provided, `company` remains empty and won't be sent,
+      // which correctly means no company is created/associated by default.
   
-//       // Build the 'opportunity' object for the conversion details
-//       const opportunity: Record<string, any> = {};
-//       if (opportunity_name !== undefined) {
-//         opportunity.name = opportunity_name;
-//         // Pipeline ID is required if opportunity name is provided
-//         if (opportunity_pipeline_id === undefined) {
-//             throw new Error("'Opportunity Pipeline ID' is required when 'Opportunity Name' is provided.");
-//         }
-//         opportunity.pipeline_id = opportunity_pipeline_id;
+      // Build the 'opportunity' object for the conversion details
+      const opportunity: Record<string, any> = {};
+      if (opportunity_name !== undefined) {
+        opportunity['name'] = opportunity_name;
+        // Pipeline ID is required if opportunity name is provided
+        if (opportunity_pipeline_id === undefined) {
+            throw new Error("'Opportunity Pipeline ID' is required when 'Opportunity Name' is provided.");
+        }
+        opportunity['pipeline_id'] = opportunity_pipeline_id;
   
-//         if (opportunity_pipeline_stage_id !== undefined) {
-//           opportunity.pipeline_stage_id = opportunity_pipeline_stage_id;
-//         }
-//         if (opportunity_monetary_value !== undefined) {
-//           opportunity.monetary_value = opportunity_monetary_value;
-//         }
-//         if (opportunity_assignee_id !== undefined) {
-//           opportunity.assignee_id = opportunity_assignee_id;
-//         }
-//       }
+        if (opportunity_pipeline_stage_id !== undefined) {
+          opportunity['pipeline_stage_id'] = opportunity_pipeline_stage_id;
+        }
+        if (opportunity_monetary_value !== undefined) {
+          opportunity['monetary_value'] = opportunity_monetary_value;
+        }
+        if (opportunity_assignee_id !== undefined) {
+          opportunity['assignee_id'] = opportunity_assignee_id;
+        }
+      }
   
   
-//       // Construct the main 'details' payload
-//       const details: Record<string, any> = {
-//         person,
-//       };
+      const details: Record<string, any> = {
+        person,
+      };
   
-//       if (Object.keys(company).length > 0) { // Only add company if it has properties defined
-//         details.company = company;
-//       }
-//       if (Object.keys(opportunity).length > 0) { // Only add opportunity if it has properties defined
-//         details.opportunity = opportunity;
-//       }
+      if (Object.keys(company).length > 0) { // Only add company if it has properties defined
+        details['company'] = company;
+    }
+    if (Object.keys(opportunity).length > 0) { // Only add opportunity if it has properties defined
+        details['opportunity'] = opportunity;
+      }
   
-//       const payload = { details };
+      const payload = { details };
   
-//       // Execute the POST request to convert the Lead.
-//       return await makeCopperRequest(
-//         HttpMethod.POST,
-//         `leads/${lead_id}/convert`,
-//         context.auth,
-//         payload
-//       );
-//     },
-//   });
+      // Execute the POST request to convert the Lead.
+      return await makeCopperRequest(
+        HttpMethod.POST,
+        `leads/${lead_id}/convert`,
+        context.auth,
+        payload
+      );
+    },
+  });
   
 export const searchLead = createAction({
     auth: copperAuth,
