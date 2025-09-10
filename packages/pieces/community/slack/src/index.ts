6f4836c21b8c66210b1bd4bd2d7eaf3e690ead61
Perrine Pullicino <perrine.pullicino@alan.eu>
2025-09-10 15:32:24 +0200

    Add search:read permissions to Slack piece

--- packages/pieces/community/slack/src/index.ts
+++ packages/pieces/community/slack/src/index.ts
@@ -52,7 +52,7 @@ import { listUsers } from './lib/actions/list-users';
 export const slackAuth = PieceAuth.OAuth2({
   description: '',
   authUrl:
-    'https://slack.com/oauth/v2/authorize?user_scope=search:read,users.profile:write,reactions:read,im:history,stars:read,channels:write,groups:write,im:write,mpim:write,channels:write.invites,groups:write.invites,channels:history,groups:history,chat:write,users:read',
+    'https://slack.com/oauth/v2/authorize?user_scope=search:read,users.profile:write,reactions:read,im:history,stars:read,channels:write,groups:write,im:write,mpim:write,channels:write.invites,groups:write.invites,channels:history,groups:history,chat:write,users:read,search:read',
   tokenUrl: 'https://slack.com/api/oauth.v2.access',
   required: true,
   scope: [
