1ccc9e65facbb0b55e0a735a4dd18c0e57f7562e
NiketNannavare <138030315+NiketNannavare@users.noreply.github.com>
2025-09-14 13:51:48 +0000

    feat: improve piece

--- packages/pieces/community/slidespeak/src/index.ts
+++ packages/pieces/community/slidespeak/src/index.ts
@@ -1,13 +1,36 @@
 
-    import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { editPresentation } from "./lib/actions/edit-presentation";
+import { generatePresentation } from "./lib/actions/generate-presentation";
+import { getTaskStatus } from "./lib/actions/get-task-status";
+import { uploadDocument } from "./lib/actions/upload-document";
+import { newPresentation } from "./lib/triggers/new-presentation";
+import { createCustomApiCallAction } from "@activepieces/pieces-common";
+import { SlideSpeakAuth } from "./lib/common/auth";
+import { BASE_URL } from "./lib/common/client";
 
-    export const slidespeak = createPiece({
-      displayName: "Slidespeak",
-      auth: PieceAuth.None(),
-      minimumSupportedRelease: '0.36.1',
-      logoUrl: "https://cdn.activepieces.com/pieces/slidespeak.png",
-      authors: [],
-      actions: [],
-      triggers: [],
-    });
-    
+export const slidespeak = createPiece({
+  displayName: "Slidespeak",
+  auth: PieceAuth.None(),
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: "https://cdn.activepieces.com/pieces/slidespeak.png",
+  authors: ['Niket2035'],
+  actions: [
+    editPresentation,
+    generatePresentation,
+    getTaskStatus,
+    uploadDocument,
+    createCustomApiCallAction({
+      auth: SlideSpeakAuth,
+      baseUrl: () => BASE_URL,
+      authMapping: async (auth) => {
+        return {
+          'X-API-Key': auth as string,
+        };
+      },
+    }),
+  ],
+  triggers: [
+    newPresentation
+  ],
+});
