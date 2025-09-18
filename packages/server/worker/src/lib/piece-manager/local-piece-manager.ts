3d08f054240abf3a73859b7086013b6987076a0b
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2025-09-18 12:57:24 +0300

    fix: stop linking packages that aren't built

--- packages/server/worker/src/lib/piece-manager/local-piece-manager.ts
+++ packages/server/worker/src/lib/piece-manager/local-piece-manager.ts
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from 'node:fs/promises'
 import { join, resolve, sep } from 'node:path'
-import { ApLock, filePiecesUtils, memoryLock } from '@activepieces/server-shared'
+import { ApLock, fileExists, filePiecesUtils, memoryLock } from '@activepieces/server-shared'
 import { assertEqual, assertNotNullOrUndefined, isEmpty, PackageType, PiecePackage } from '@activepieces/shared'
 import { FastifyBaseLogger } from 'fastify'
 import { cacheState } from '../cache/cache-state'
@@ -31,7 +31,7 @@ export class LocalPieceManager extends PieceManager {
             lock = await memoryLock.acquire(PIECES_BUILDER_MUTEX_KEY)
             const { projectPath, pieces } = params
             const basePath = resolve(__dirname.split(`${sep}dist`)[0])
-            const baseLinkPath = join(
+            const communityPiecesDistPath = join(
                 basePath,
                 'dist',
                 'packages',
@@ -41,14 +41,14 @@ export class LocalPieceManager extends PieceManager {
             const packages = workerMachine.getSettings().DEV_PIECES || []
 
             const frameworkPackages = {
-                '@activepieces/pieces-common': `link:${baseLinkPath}/common`,
-                '@activepieces/pieces-framework': `link:${baseLinkPath}/framework`,
+                '@activepieces/pieces-common': `link:${communityPiecesDistPath}/common`,
+                '@activepieces/pieces-framework': `link:${communityPiecesDistPath}/framework`,
                 '@activepieces/shared': `link:${basePath}/dist/packages/shared`,
-                '@activepieces/common-ai': `link:${baseLinkPath}/common-ai`,
+                '@activepieces/common-ai': `link:${communityPiecesDistPath}/common-ai`,
             }
-            await linkPackages(projectPath, join(baseLinkPath, 'framework'), '@activepieces/pieces-framework', frameworkPackages, params.log)
-            await linkPackages(projectPath, join(baseLinkPath, 'common'), '@activepieces/pieces-common', frameworkPackages, params.log)
-            await linkPackages(projectPath, join(baseLinkPath, 'common-ai'), '@activepieces/common-ai', frameworkPackages, params.log)
+            await linkPackages(projectPath, join(communityPiecesDistPath, 'framework'), '@activepieces/pieces-framework', frameworkPackages, params.log)
+            await linkPackages(projectPath, join(communityPiecesDistPath, 'common'), '@activepieces/pieces-common', frameworkPackages, params.log)
+            await linkPackages(projectPath, join(communityPiecesDistPath, 'common-ai'), '@activepieces/common-ai', frameworkPackages, params.log)
             for (const piece of pieces) {
                 assertEqual(piece.packageType, PackageType.REGISTRY, 'packageType', `Piece ${piece.pieceName} is not of type REGISTRY`)
                 const directoryPath = await filePiecesUtils(packages, params.log).findDirectoryByPackageName(piece.pieceName)
@@ -85,12 +85,15 @@ const linkPackages = async (
 }
 
 const updatePackageJson = async (
-    directoryPath: string,
+    packagePath: string,
     frameworkPackages: Record<string, string>,
 ): Promise<void> => {
-    const packageJsonForPiece = join(directoryPath, 'package.json')
-
-    const packageJson = await readFile(packageJsonForPiece, 'utf-8').then(
+    const packageDotJsonPath = join(packagePath, 'package.json')
+    const packageDotJsonExists = await fileExists(packageDotJsonPath)
+    if (!packageDotJsonExists) {
+        return
+    }
+    const packageJson = await readFile(packageDotJsonPath, 'utf-8').then(
         JSON.parse,
     )
     for (const [key, value] of Object.entries(frameworkPackages)) {
@@ -101,7 +104,7 @@ const updatePackageJson = async (
             packageJson.dependencies[key] = value
         }
     }
-    await writeFile(packageJsonForPiece, JSON.stringify(packageJson, null, 2))
+    await writeFile(packageDotJsonPath, JSON.stringify(packageJson, null, 2))
 }
 
 type InstallParams = {
