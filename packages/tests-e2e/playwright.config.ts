d0e913fb3b6d122067b8daa4e5d09b2f7c75fa70
Amr Elmohamady <amr.osama.elmohamady@gmail.com>
2025-09-24 15:09:22 +0000

    feat: use github reported

--- packages/tests-e2e/playwright.config.ts
+++ packages/tests-e2e/playwright.config.ts
@@ -38,7 +38,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? '100%' : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
