c8897e9318c1f1d4da05394bf90e5c3615848e13
MrChaker <mr.chakerw@gmail.com>
2025-09-30 12:35:41 +0100

    chore: save job latest state for couter decrementing

--- packages/server/api/src/app/workers/queue/queue-events/save-queue-metrics.ts
+++ packages/server/api/src/app/workers/queue/queue-events/save-queue-metrics.ts
@@ -2,12 +2,16 @@ import { FastifyBaseLogger } from "fastify";
 import { Job, Queue, QueueEvents } from "bullmq";
 import { EventsManager } from "./events-manager";
 import { redisConnections } from "../../../database/redis";
-import { assertNotNullOrUndefined, WorkerJobStatus, WorkerJobType } from "@activepieces/shared";
+import { JobData, WorkerJobStatus, WorkerJobType } from "@activepieces/shared";
 import { QueueName } from "@activepieces/server-shared";
 import { bullMqGroups } from "../job-queue";
+import { Redis } from "ioredis";
 
 export const METRICS_KEY = "metrics"
+export const JOB_STATE_KEY = "job_state"
+
 export const METRICS_KEY_PREFIX = (jobType: WorkerJobType) => `${METRICS_KEY}:${jobType}`
+export const JOB_STATE_KEY_PREFIX = (jobId: string) => `${JOB_STATE_KEY}:${jobId}`
 
 export const saveQueueMetrics = (log: FastifyBaseLogger, queueEvents: QueueEvents): EventsManager => ({
 
@@ -17,7 +21,6 @@ export const saveQueueMetrics = (log: FastifyBaseLogger, queueEvents: QueueEvent
         queueEvents.on("completed", onCompleted)
        
     },
-
     detach: () => {
         queueEvents.off("waiting", onWaiting)
         queueEvents.off("failed", onFailed)
@@ -26,22 +29,33 @@ export const saveQueueMetrics = (log: FastifyBaseLogger, queueEvents: QueueEvent
 
 })
 
-const onWaiting = async (args: { jobId: string }) => {
+const onWaiting = async (args: { jobId: string, prev?: string }) => {
   const queue = bullMqGroups[QueueName.WORKER_JOBS].queue
   const job = await queue.getJob(args.jobId)
   if (!job?.data.jobType) return
 
   const redisConnectionInstance = await redisConnections.useExisting()
+
+  
+  await decrPrevState(redisConnectionInstance, job.data.jobType, args.jobId)
+
   await redisConnectionInstance.incr(`${METRICS_KEY_PREFIX(job.data.jobType)}:${WorkerJobStatus.DELAYED}`)
+  
+  await redisConnectionInstance.set(JOB_STATE_KEY_PREFIX(args.jobId), WorkerJobStatus.DELAYED)
 }
 
 const onFailed = async (args: { jobId: string }) => {
   const queue = bullMqGroups[QueueName.WORKER_JOBS].queue
-  const job = await queue.getJob(args.jobId)
+  const job: Job<JobData> = await queue.getJob(args.jobId)
   if (!job?.data.jobType) return
 
   const redisConnectionInstance = await redisConnections.useExisting()
+  
+  await decrPrevState(redisConnectionInstance, job.data.jobType, args.jobId)
+
   await redisConnectionInstance.incr(`${METRICS_KEY_PREFIX(job.data.jobType)}:${WorkerJobStatus.FAILED}`)
+  
+  await redisConnectionInstance.set(JOB_STATE_KEY_PREFIX(args.jobId), WorkerJobStatus.FAILED)
 }
 
 const onCompleted = async (args: { jobId: string }) => {
@@ -50,5 +64,17 @@ const onCompleted = async (args: { jobId: string }) => {
   if (!job?.data.jobType) return
 
   const redisConnectionInstance = await redisConnections.useExisting()
+  
+  await decrPrevState(redisConnectionInstance, job.data.jobType, args.jobId)
+
   await redisConnectionInstance.incr(`${METRICS_KEY_PREFIX(job.data.jobType)}:${WorkerJobStatus.COMPLETED}`)
+  
+  await redisConnectionInstance.set(JOB_STATE_KEY_PREFIX(args.jobId), WorkerJobStatus.COMPLETED)
+}
+
+const decrPrevState = async (redisConnectionInstance: Redis, jobType: WorkerJobType, jobId: string) => {
+  const prevState = await redisConnectionInstance.get(JOB_STATE_KEY_PREFIX(jobId))
+  if (prevState) {
+    await redisConnectionInstance.decr(`${METRICS_KEY_PREFIX(jobType)}:${prevState}`)
+  }
 }
