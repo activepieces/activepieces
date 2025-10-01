58c2d610e99a23f61285cf63b04163990f3c3c03
MrChaker <mr.chakerw@gmail.com>
2025-10-01 10:22:57 +0100

    chore: remove testing code

--- packages/server/api/src/app/workers/worker-module.ts
+++ packages/server/api/src/app/workers/worker-module.ts
@@ -7,9 +7,6 @@ import { jobQueueWorker } from './queue/job-queue-worker'
 import { queueMigration } from './queue/migration'
 import { setupBullMQBoard } from './queue/redis-bullboard'
 import { flowWorkerController } from './worker-controller'
-import { QueueName } from '@activepieces/server-shared'
-import { apId, WorkerJobType, LATEST_JOB_DATA_SCHEMA_VERSION, RunEnvironment } from '@activepieces/shared'
-import { JobType } from './queue/queue-manager'
 
 export const workerModule: FastifyPluginAsyncTypebox = async (app) => {
     await app.register(flowWorkerController, {
@@ -22,59 +19,8 @@ export const workerModule: FastifyPluginAsyncTypebox = async (app) => {
         prefix: '/v1/worker-machines',
     })
     await jobQueue(app.log).init()
-
     await setupBullMQBoard(app)
     await jobQueueWorker(app.log).init()
-
-
-    setTimeout(async () => {
-        console.log("Adding job to worker jobs queue")
-        jobQueue(app.log).add({
-            id: apId(),
-            data: {
-                platformId: 'platform_' + apId(),
-                projectId: 'proj_' + apId(),
-                schemaVersion: LATEST_JOB_DATA_SCHEMA_VERSION,
-                requestId: 'req_' + apId(),
-                payload: { mockData: 'example payload' },
-                jobType: WorkerJobType.EXECUTE_WEBHOOK,
-                flowId: 'flow_' + apId(),
-                saveSampleData: true,
-                flowVersionIdToRun: 'ver_' + apId(),
-                runEnvironment: RunEnvironment.TESTING,
-                execute: true,
-                parentRunId: 'parent_' + apId(),
-                failParentOnFailure: false,
-            },
-            type: JobType.ONE_TIME,
-        })
-
-
-
-    }, 5000)
-
-    setTimeout(async () => {
-
-        jobQueue(app.log).add({
-            id: apId(),
-            data: {
-                platformId: 'platform_' + apId(),
-                projectId: 'proj_' + apId(),
-                schemaVersion: LATEST_JOB_DATA_SCHEMA_VERSION,
-                requestId: 'req_' + apId(),
-                payload: { mockData: 'example payload' },
-                jobType: WorkerJobType.EXECUTE_WEBHOOK,
-                flowId: 'flow_' + apId(),
-                saveSampleData: true,
-                flowVersionIdToRun: 'ver_' + apId(),
-                runEnvironment: RunEnvironment.TESTING,
-                execute: true,
-                parentRunId: 'parent_' + apId(),
-                failParentOnFailure: false,
-            },
-            type: JobType.ONE_TIME,
-        })
-    }, 10000)
 }
 
 
