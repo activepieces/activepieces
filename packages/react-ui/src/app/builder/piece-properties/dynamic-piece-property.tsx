1f324a20e137cb8b89349c351763cbd3ea3213a0
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-10-02 16:59:15 +0300

    fix: don't include schema options in the dynamic property schema

--- packages/react-ui/src/app/builder/piece-properties/dynamic-piece-property.tsx
+++ packages/react-ui/src/app/builder/piece-properties/dynamic-piece-property.tsx
@@ -7,7 +7,7 @@ import { useBuilderStateContext } from '@/app/builder/builder-hooks';
 import { SkeletonList } from '@/components/ui/skeleton';
 import { formUtils } from '@/features/pieces/lib/form-utils';
 import { piecesHooks } from '@/features/pieces/lib/pieces-hooks';
-import { PiecePropertyMap, PropertyType } from '@activepieces/pieces-framework';
+import { PiecePropertyMap, PropertyType, StaticDropdownProperty, StaticMultiSelectDropdownProperty } from '@activepieces/pieces-framework';
 import { FlowAction, FlowTrigger } from '@activepieces/shared';
 
 import { useStepSettingsContext } from '../step-settings/step-settings-context';
@@ -20,6 +20,15 @@ type DynamicPropertiesProps = {
   propertyName: string;
   disabled: boolean;
 };
+
+const removeOptionsFromDropdownPropertiesSchema = (schema: PiecePropertyMap) => {
+  return Object.fromEntries(Object.entries(schema).map(([key, value]) => {
+    if(value.type === PropertyType.STATIC_DROPDOWN || value.type === PropertyType.STATIC_MULTI_SELECT_DROPDOWN) {
+      return [key, { ...value, options: { disabled: false, options: [] } }];
+    }
+    return [key, value];
+  }));
+}
 
 const DynamicPropertiesImplementation = React.memo(
   (props: DynamicPropertiesProps) => {
@@ -74,17 +83,19 @@ const DynamicPropertiesImplementation = React.memo(
         !isFirstRender.current &&
         !deepEqual(previousValues.current, refresherValues)
       ) {
+        
         // the field state won't be cleared if you only unset the parent prop value
         if (propertyMap) {
           Object.keys(propertyMap).forEach((childPropName) => {
-            form.setValue(
-              `settings.input.${props.propertyName}.${childPropName}` as const,
-              null,
-              {
-                shouldValidate: true,
-              },
-            );
-          });
+              form.setValue(
+                `settings.input.${props.propertyName}.${childPropName}` as const,
+                null,
+                {
+                  //never validate for each prop, it can be a long list of props and cause the browser to freeze
+                  shouldValidate: false,
+                },
+              );
+            })
         }
         form.setValue(`settings.input.${props.propertyName}` as const, null, {
           shouldValidate: true,
@@ -123,16 +134,16 @@ const DynamicPropertiesImplementation = React.memo(
                 ],
             });
             setPropertyMap(response.options);
-
+            const schemaWithoutDropdownOptions = removeOptionsFromDropdownPropertiesSchema(response.options);
             updateFormSchema(
               `settings.input.${props.propertyName}`,
-              response.options,
+              schemaWithoutDropdownOptions,
             );
 
             if (!readonly) {
               form.setValue(
                 `settings.propertySettings.${props.propertyName}.schema`,
-                response.options,
+                schemaWithoutDropdownOptions,
               );
             }
 
