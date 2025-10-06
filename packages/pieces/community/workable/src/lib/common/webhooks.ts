23e648ba655f8ef3b12934e609fd678f93d4bdef
Megan-Wong-FSMK <178718590+Cloudieunnie@users.noreply.github.com>
2025-10-06 23:23:11 +0800

    feat: added new trigger new candidate

---
+++ packages/pieces/community/workable/src/lib/common/webhooks.ts
@@ -0,0 +1,46 @@
+import { HttpMethod, httpClient, AuthenticationType, HttpRequest} from '@activepieces/pieces-common';
+import { unsubscribe } from 'diagnostics_channel';
+
+export const workableCommon = {
+    subscribeWebhook: async (
+        subdomain: string,
+        accessToken: string,
+        webhookUrl: string,
+        event: string,
+        args: Record<string, any> = {}
+    ) => {
+        const request: HttpRequest = {
+            method: HttpMethod.POST,
+            url: `https://${subdomain}.workable.com/spi/v3/subscriptions`,
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                Accept: 'application/json',
+            },
+            body: {
+                webhookUrl,
+                event,
+                args,
+            }
+        };
+        const response = await httpClient.sendRequest<{id: string}>(request);
+        return response.body;
+    },
+
+    unsubscribeWebhook: async (
+        subdomain: string,
+        accessToken: string,
+        subscriptionId: string
+    ) => {
+        const request: HttpRequest = {
+            method: HttpMethod.DELETE,
+            url: `https://${subdomain}.workable.com/spi/v3/subscriptions/${subscriptionId}`,
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                Accept: 'application/json'
+            }
+        }
+
+        return await httpClient.sendRequest(request);
+    }
+
+}
---
+++ packages/pieces/community/workable/src/lib/triggers/new-candidate.ts
@@ -0,0 +1,103 @@
+
+import { workableAuth } from '../../index';
+import { createTrigger, Property, TriggerStrategy, WebhookResponse } from '@activepieces/pieces-framework';
+import { httpClient, HttpMethod } from '@activepieces/pieces-common';
+import { getAccountSubdomain } from '../common/get-subdomain';
+import { workableCommon } from '../common/webhooks';
+
+interface WebhookInformation {
+    webhookId: string;
+}
+
+export const newCandidate = createTrigger({
+    auth: workableAuth,
+    name: 'newCandidate',
+    displayName: 'New Candidate',
+    description: 'Triggers when new candidate submits application',
+    props: {
+        shortcode: Property.ShortText({
+            displayName: "Shortcode",
+            description: "Shortcode of specific job",
+            required: true
+        })
+    },
+    sampleData: {
+        id: '123',
+        name: 'John Doe',
+        firstname: 'John',
+        lastname: 'Doe',
+        headline: 'Software Engineer',
+        account: {
+            subdomain: 'example',
+            name: 'Example Inc.'
+        },
+        job: {
+            shortcode: 'ENG123',
+            title: 'Software Engineer'
+        },
+        stage: 'applied',
+        disqualified: false,
+        disqualification_reason: '',
+        sourced: false,
+        profile_url: 'https://example.workable.com/candidates/123',
+        email: 'john.doe@example.com',
+        domain: 'example.com',
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+    },
+    type: TriggerStrategy.WEBHOOK,
+    async onEnable(context){
+        const accessToken = context.auth;
+        const subdomain = await getAccountSubdomain(accessToken);
+
+        const shortcode = context.propsValue.shortcode;
+        const webhookUrl = context.webhookUrl;
+        const event = "candidate_created";
+
+        const subscription = await workableCommon.subscribeWebhook(
+            subdomain,
+            accessToken,
+            webhookUrl,
+            event,
+            {job_shortcode: shortcode, stage_slug: ''}
+        );
+
+        await context.store?.put<WebhookInformation>('_new_candidate_created', {
+            webhookId: subscription.id,
+        })
+    },
+    async onDisable(context){
+        // implement webhook deletion logic
+        const accessToken = context.auth;
+
+        const webhookInfo = await context.store.get<WebhookInformation>('_new_candidate_created');
+
+        if(webhookInfo?.webhookId) {
+            const subdomain = await getAccountSubdomain(accessToken);
+            await workableCommon.unsubscribeWebhook(subdomain, accessToken, webhookInfo.webhookId);
+        }
+    },
+    async test(context){
+        const accessToken = context.auth;
+        const subdomain = await getAccountSubdomain(accessToken);
+        const shortcode = context.propsValue.shortcode;
+
+        const response = await httpClient.sendRequest({
+            method: HttpMethod.GET,
+            url: `https://${subdomain}.workable.com/spi/v3/candidates/`,
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                Accept: "application/json"
+            },
+            queryParams: {
+                shortcode: shortcode
+            }
+        });
+
+        const candidates = response.body.candidates?.slice(0, 3) ?? [];
+        return candidates;
+    },
+    async run(context){
+        return [context.payload.body]
+    }
+})
