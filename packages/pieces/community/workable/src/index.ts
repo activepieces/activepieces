c6c5eefbacb70fa1ad1e42411095fe1d29fd80f3
Megan-Wong-FSMK <178718590+Cloudieunnie@users.noreply.github.com>
2025-10-06 13:55:46 +0800

    feat: added workable authentication

--- packages/pieces/community/workable/src/index.ts
+++ packages/pieces/community/workable/src/index.ts
@@ -1,13 +1,19 @@
 
-    import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
 
-    export const workable = createPiece({
-      displayName: "Workable",
-      auth: PieceAuth.None(),
-      minimumSupportedRelease: '0.36.1',
-      logoUrl: "https://cdn.activepieces.com/pieces/workable.png",
-      authors: [],
-      actions: [],
-      triggers: [],
-    });
-    
+export const workableAuth = PieceAuth.SecretText({
+    displayName: "API Key",
+    description: "Enter API Key",
+    required: true
+  })
+
+
+export const workable = createPiece({
+  displayName: "Workable",
+  auth: workableAuth,
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: "https://cdn.activepieces.com/pieces/workable.png",
+  authors: [],
+  actions: [],
+  triggers: [],
+});
