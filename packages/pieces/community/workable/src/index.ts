5878a7bb7c8bede47e0e5cce36a4bddf061026a2
Megan-Wong-FSMK <178718590+Cloudieunnie@users.noreply.github.com>
2025-10-07 18:14:23 +0800

    feat: added custom api call action

--- packages/pieces/community/workable/src/index.ts
+++ packages/pieces/community/workable/src/index.ts
@@ -7,20 +7,37 @@ import { getStages } from "./lib/actions/get-stages";
 import { moveCandidate } from "./lib/actions/move-candidate";
 import { rateCandidate } from "./lib/actions/rate-candidate";
 import { newCandidate } from "./lib/triggers/new-candidate";
+import { createCustomApiCallAction } from "@activepieces/pieces-common";
 
 export const workableAuth = PieceAuth.SecretText({
     displayName: "API Key",
     description: "Enter API Key",
     required: true
-  })
-
+  }) 
 
 export const workable = createPiece({
   displayName: "Workable",
   auth: workableAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: "https://cdn.activepieces.com/pieces/workable.png",
-  authors: [],
-  actions: [getCandidate, getMembers, getJob, getStages, moveCandidate, rateCandidate],
-  triggers: [newCandidate],
+  authors: ['Cloudieunnie'],
+  actions: [
+    getCandidate, 
+    getMembers, 
+    getJob, 
+    getStages, 
+    moveCandidate, 
+    rateCandidate,
+    createCustomApiCallAction({
+      baseUrl: () => `https://workable.com/spi/v3/`,
+      auth: workableAuth,
+      authMapping: async (auth) => ({
+        Authorization: `Bearer ${auth}`,
+        Accept: 'application/json'
+      })
+    })
+  ],
+  triggers: [
+    newCandidate
+  ],
 });
