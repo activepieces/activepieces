17117f786beecb87f5baf7105263474aeb68e411
srimalleswari205 <srimalleswari205@gmail.com>
2025-10-16 22:57:26 +0530

    feat(google-cloud-storage): add custom api call function

--- packages/pieces/community/google-cloud-storage/src/index.ts
+++ packages/pieces/community/google-cloud-storage/src/index.ts
@@ -1,6 +1,8 @@
-import { createPiece } from '@activepieces/pieces-framework';
+import { createPiece, OAuth2PropertyValue } from '@activepieces/pieces-framework';
 import { PieceCategory } from '@activepieces/shared';
 import { googleCloudStorageAuth } from './lib/common/auth';
+import { createCustomApiCallAction } from "@activepieces/pieces-common"; 
+
 import { createBucket } from './lib/actions/create-bucket';
 import { deleteEmptyBucket } from './lib/actions/delete-empty-bucket';
 import { cloneObject } from './lib/actions/clone-object';
@@ -14,13 +16,14 @@ import { deleteBucketDefaultObjectAcl } from './lib/actions/delete-bucket-defaul
 import { searchObjects } from './lib/actions/search-objects';
 import { searchBucket } from './lib/actions/search-bucket';
 
+
 import { newObjectCreated } from './lib/triggers/new-object-created';
 import { objectUpdated } from './lib/triggers/object-updated';
 
 export const googleCloudStorage = createPiece({
   displayName: 'Google Cloud Storage',
   auth: googleCloudStorageAuth,
-  minimumSupportedRelease: '0.5.0',
+  minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/google-cloud-storage.png',
   description: 'A powerful and scalable object storage service to store, retrieve, and manage data objects in Google Cloud.',
   categories: [PieceCategory.CONTENT_AND_FILES],
@@ -38,6 +41,16 @@ export const googleCloudStorage = createPiece({
     deleteBucketAcl,
     createBucketDefaultObjectAcl,
     deleteBucketDefaultObjectAcl,
+    createCustomApiCallAction({
+        auth: googleCloudStorageAuth,
+        baseUrl: () => 'https://storage.googleapis.com/storage/v1',
+        authMapping: async (auth) => {
+            const oauth2 = auth as OAuth2PropertyValue;
+            return {
+                Authorization: `Bearer ${oauth2.access_token}`
+            };
+        }
+    })
   ],
   triggers: [
     newObjectCreated,
