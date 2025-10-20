e48d0a38629f352e4731db67b9825a40a3f9cc95
MrChaker <mr.chakerw@gmail.com>
2025-10-20 21:56:46 +0100

    chore: remove trigger run entity

---
+++ packages/server/api/src/app/database/migration/postgres/1760993216501-RemoveTriggerRunEntity.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class RemoveTriggerRunEntity1760993216501 implements MigrationInterface {
+    name = 'RemoveTriggerRunEntity1760993216501'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            TRUNCATE TABLE "trigger_run";
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
---
+++ packages/server/api/src/app/database/migration/sqlite/1760992394073-RemoveTriggerRunEntity.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class RemoveTriggerRunEntity1760992394073 implements MigrationInterface {
+    name = 'RemoveTriggerRunEntity1760992394073'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DELETE FROM "trigger_run";
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
