9428652eac8f6b5c5764603e0f98ca592b533595
Amr Elmohamady <amr.osama.elmohamady@gmail.com>
2025-10-22 02:17:00 +0300

    fix: migration

--- packages/server/api/src/app/database/migration/postgres/1760998784106-AddDedicatedWorkersToPlatformPlanPostgres.ts
+++ packages/server/api/src/app/database/migration/postgres/1760998784106-AddDedicatedWorkersToPlatformPlanPostgres.ts
@@ -5,37 +5,14 @@ export class AddDedicatedWorkersToPlatformPlanPostgres1760998784106 implements M
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            ALTER TABLE "flow" DROP CONSTRAINT "fk_flow_project_id"
-        `)
-        await queryRunner.query(`
             ALTER TABLE "platform_plan"
             ADD "dedicatedWorkers" jsonb
-        `)
-        await queryRunner.query(`
-            ALTER TABLE "platform_plan"
-            ALTER COLUMN "stripeBillingCycle" DROP DEFAULT
-        `)
-        await queryRunner.query(`
-            ALTER TABLE "flow"
-            ADD CONSTRAINT "fk_flow_project_id" FOREIGN KEY ("projectId") REFERENCES "project"("id") ON DELETE CASCADE ON UPDATE NO ACTION
         `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            ALTER TABLE "flow" DROP CONSTRAINT "fk_flow_project_id"
-        `)
-        await queryRunner.query(`
-            ALTER TABLE "platform_plan"
-            ALTER COLUMN "stripeBillingCycle"
-            SET DEFAULT 'monthly'
-        `)
-        await queryRunner.query(`
             ALTER TABLE "platform_plan" DROP COLUMN "dedicatedWorkers"
-        `)
-        await queryRunner.query(`
-            ALTER TABLE "flow"
-            ADD CONSTRAINT "fk_flow_project_id" FOREIGN KEY ("projectId") REFERENCES "project"("id") ON DELETE RESTRICT ON UPDATE NO ACTION
         `)
     }
 
