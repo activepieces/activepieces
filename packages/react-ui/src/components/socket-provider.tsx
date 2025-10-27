ae9176ee25baa1e7a3e7edff3282bf24e603da10
MrChaker <mr.chakerw@gmail.com>
2025-10-27 15:10:33 +0100

    chore: use socket io built in reconnection

--- packages/react-ui/src/components/socket-provider.tsx
+++ packages/react-ui/src/components/socket-provider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useEffectOnce } from 'react-use';
 import { io } from 'socket.io-client';
 
@@ -6,20 +6,18 @@ import { API_BASE_URL } from '@/lib/api';
 import { authenticationSession } from '@/lib/authentication-session';
 import { Alert, AlertDescription } from './ui/alert';
 import { LoadingSpinner } from './ui/spinner';
-import { isNil } from '@activepieces/shared';
 
 const socket = io(API_BASE_URL, {
   transports: ['websocket'],
   path: '/api/socket.io',
   autoConnect: false,
+  reconnection: true,
 });
 
 const SocketContext = React.createContext<typeof socket>(socket);
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   const token = authenticationSession.getToken();
-  const [reconnectInterval, setReconnectInterval] = useState<NodeJS.Timeout | null>(null);
-  const [disconnected, setDisconnected] = useState<boolean>(false);
 
   useEffectOnce(() => {
     if (token) {
@@ -29,36 +27,22 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
 
         socket.on('connect', () => {
           console.log("connected to socket")
-          setDisconnected(false);
         })
 
-        socket.on('disconnect', () => {
-          console.log("disconnected from socket")
-          setDisconnected(true);
+        socket.on('disconnect', (reason) => {
+          if (reason === 'io server disconnect') {
+            socket.connect();
+          }
         })
-
       }
     } else {
       socket.disconnect();
     }
   });
 
-  useEffect(() => {
-    if (disconnected) {
-      const interval = setInterval(() => {
-        socket.connect();
-      }, 1000)
-      setReconnectInterval(interval)
-      return () => clearInterval(interval)
-    }
-    if (!isNil(reconnectInterval)) {
-      clearInterval(reconnectInterval)
-    }
-  }, [disconnected]);
-
   return (
     <SocketContext.Provider value={socket}>
-      {disconnected && (
+      {socket.disconnected && (
         <Alert className="fixed bottom-10 right-10 w-96 z-50 bg-background items-center" variant="destructive" >
           <LoadingSpinner />
           <AlertDescription>Websocket Disconnected, reconnecting...</AlertDescription>
