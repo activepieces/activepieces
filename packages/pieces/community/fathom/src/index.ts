1e55613bcc60d8d8f772740b76247d1f068b24d7
omey12 <korakeom87@gmail.com>
2025-10-30 11:22:15 +0000

    feat: fathom piece actions and triggers

--- packages/pieces/community/fathom/src/index.ts
+++ packages/pieces/community/fathom/src/index.ts
@@ -1,13 +1,19 @@
 
     import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+import { findTeam } from "./lib/actions/find-team";
+import { findTeamMember } from "./lib/actions/find-team-member";
+import { getRecordingTranscript } from "./lib/actions/get-recording-transcript";
+import { listMeetings } from "./lib/actions/list-meetings";
+import { newRecording } from "./lib/triggers/new-recording";
+import { getRecordingSummary } from "./lib/actions/get-recording-summary";
 
     export const fathom = createPiece({
       displayName: "Fathom",
       auth: PieceAuth.None(),
       minimumSupportedRelease: '0.36.1',
       logoUrl: "https://cdn.activepieces.com/pieces/fathom.png",
-      authors: [],
-      actions: [],
-      triggers: [],
+      authors: ["omey12"],
+      actions: [findTeam,findTeamMember,getRecordingTranscript,listMeetings,getRecordingSummary],
+      triggers: [newRecording],
     });
     
