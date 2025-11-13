8a304ff6b20a19ceb771f6f9ff5beec8f72da755
sanket nannaware <sanket@activepieces.com>
2025-11-13 15:43:38 +0530

    feat(webex): custom api call

--- packages/pieces/community/webex/src/index.ts
+++ packages/pieces/community/webex/src/index.ts
@@ -1,4 +1,7 @@
-import { createPiece } from '@activepieces/pieces-framework';
+import {
+  createPiece,
+  OAuth2PropertyValue,
+} from '@activepieces/pieces-framework';
 import { createMessage } from './lib/actions/create-message';
 import { createRoom } from './lib/actions/create-room';
 import { createTeam } from './lib/actions/create-team';
@@ -8,6 +11,7 @@ import { webexAuth } from './lib/common/auth';
 import { newRoom } from './lib/triggers/new-room';
 import { newMeeting } from './lib/triggers/new-meeting';
 import { PieceCategory } from '@activepieces/shared';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
 
 export const webex = createPiece({
   displayName: 'Cisco Webex Meetings',
@@ -17,6 +21,19 @@ export const webex = createPiece({
   description: '',
   categories: [PieceCategory.COMMUNICATION],
   authors: ['sanket-a11y'],
-  actions: [createMessage, createRoom, createTeam, findMessage, findRoom],
+  actions: [
+    createMessage,
+    createRoom,
+    createTeam,
+    findMessage,
+    findRoom,
+    createCustomApiCallAction({
+      auth: webexAuth,
+      baseUrl: () => 'https://webexapis.com/v1',
+      authMapping: async (auth) => ({
+        Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
+      }),
+    }),
+  ],
   triggers: [newRoom, newMeeting],
 });
