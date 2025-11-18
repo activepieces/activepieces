b521527f94e3402166a376e50c89cbe6a55253e2
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-11-18 03:25:16 +0000

    refactor: simplify local piece cache logic by removing cron job and enhancing cache update mechanism

--- packages/server/api/src/app/pieces/metadata/local-piece-cache.ts
+++ packages/server/api/src/app/pieces/metadata/local-piece-cache.ts
@@ -1,7 +1,7 @@
-import { isNil, Result, tryCatch } from '@activepieces/shared'
+import { isNil } from '@activepieces/shared'
+import { Mutex } from 'async-mutex'
 import dayjs from 'dayjs'
 import { FastifyBaseLogger } from 'fastify'
-import cron from 'node-cron'
 import semVer from 'semver'
 import { repoFactory } from '../../core/db/repo-factory'
 import { PieceMetadataEntity, PieceMetadataSchema } from './piece-metadata-entity'
@@ -9,62 +9,48 @@ import { PieceMetadataEntity, PieceMetadataSchema } from './piece-metadata-entit
 let cache: PieceMetadataSchema[] = []
 
 const repo = repoFactory(PieceMetadataEntity)
-
-export const localPieceCache = (log: FastifyBaseLogger) => ({
-    async getSortedbyNameAscThenVersionDesc(): Promise<PieceMetadataSchema[]> {
-        return cache
-    },
-    async setup(): Promise<void> {
-        const piecesResult = await fetchPieces()
-        if (piecesResult.error) {
-            log.error({ error: piecesResult.error }, '[localPieceCache] Error fetching local pieces')
-            throw piecesResult.error
-        }
-        cache = piecesResult.data
-
-        cron.schedule('*/15 * * * *', async () => {
-            log.info('[localPieceCache] Refreshing pieces cache via cron job')
-
-            const piecesResult = await fetchPieces()
-            if (!isNil(piecesResult.data)) {
-                cache = piecesResult.data
-            }
-        })
-    },
-})
-
-// Removed the log arg since it's not used, and fixed types
-async function fetchPieces(): Promise<Result<PieceMetadataSchema[], Error>> {
-    const newestState: State | undefined = await repo()
-        .createQueryBuilder()
-        .select('MAX(updated)', 'recentUpdate')
-        .addSelect('count(*)', 'count')
-        .getRawOne()
-
-    if (!isNil(newestState)) {
-        const newestInCache = cache.reduce((acc, piece) => {
-            return Math.max(dayjs(piece.updated).unix(), acc)
-        }, 0)
-
-        const needUpdate = dayjs(newestState.recentUpdate).unix() !== newestInCache || Number(newestState.count) !== cache.length
-        if (!needUpdate) {
-            return { data: cache, error: null }
-        }
-    }
-
-    return tryCatch(async () => {
-        const piecesFromDatabase = await repo().find()
-
-        return piecesFromDatabase.sort((a, b) => {
-            if (a.name !== b.name) {
-                return a.name.localeCompare(b.name)
-            }
-            return semVer.rcompare(a.version, b.version)
-        })
-    })
-}
+const lock: Mutex = new Mutex()
 
 type State = {
     recentUpdate: string | undefined
     count: string
+}
+
+export const localPieceCache = (log: FastifyBaseLogger) => ({
+    async setup(): Promise<void> {
+        // no-op
+    },
+    async getSortedbyNameAscThenVersionDesc(): Promise<PieceMetadataSchema[]> {
+        const updatedRequired = await requireUpdate()
+        if (!updatedRequired) {
+            return cache
+        }
+        log.info({ time: dayjs().toISOString(), file: 'localPieceCache' }, 'Syncing pieces')
+        cache = await lock.runExclusive(async () => {
+            const updatedRequiredSecondCheck = await requireUpdate()
+            if (!updatedRequiredSecondCheck) {
+                return cache
+            }
+            log.info('Syncing pieces from database')
+            const result = await repo().find()
+            return result.sort((a, b) => {
+                if (a.name !== b.name) {
+                    return a.name.localeCompare(b.name)
+                }
+                return semVer.rcompare(a.version, b.version)
+            })
+        })
+        return cache
+    },
+})
+
+async function requireUpdate(): Promise<boolean> {
+    const newestState: State | undefined = await repo().createQueryBuilder().select('MAX(updated)', 'recentUpdate').addSelect('count(*)', 'count').getRawOne()
+    if (isNil(newestState)) {
+        return false
+    }
+    const newestInCache = cache.reduce((acc, piece) => {
+        return Math.max(dayjs(piece.updated).unix(), acc)
+    }, 0)
+    return dayjs(newestState.recentUpdate).unix() !== newestInCache || Number(newestState.count) !== cache.length
 }
