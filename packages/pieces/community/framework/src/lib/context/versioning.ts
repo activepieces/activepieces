bea1910be414d79d1d90cdda0e6101eb2bd41659
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-11-27 12:35:04 +0300

    fix: refactor backward compatibility utility to improve action context migration

--- packages/pieces/community/framework/src/lib/context/versioning.ts
+++ packages/pieces/community/framework/src/lib/context/versioning.ts
@@ -9,16 +9,18 @@ export const backwardCompatabilityContextUtils = {
     makeActionContextBackwardCompatible({ context, contextVersion }: MakeActionContextBackwardCompatibleParams): ActionContext {
         switch (contextVersion) {
             case undefined:
-                {
-                    return {
-                        ...context,
-                        serverUrl: context.server.publicUrl,
-                    } as unknown as ActionContext
-                }
+                return migrateActionContextV1ToV0(context);
             case ContextVersion.V1:
                 return context;
         }
     }
+}
+
+function migrateActionContextV1ToV0(context: ActionContext): ActionContext {
+    return {
+        ...context,
+        serverUrl: context.server.publicUrl,
+    } as unknown as ActionContext
 }
 
 type MakeActionContextBackwardCompatibleParams = {
