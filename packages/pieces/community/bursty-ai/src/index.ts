d5828d2a656a145d279cfe90b284ec4d1ac70ac7
sanket@activepieces.com <sanket@activepieces.com>
2025-12-02 17:25:03 +0530

    fix: desciption

--- packages/pieces/community/bursty-ai/src/index.ts
+++ packages/pieces/community/bursty-ai/src/index.ts
@@ -1,12 +1,16 @@
 import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 import { runWorkflow } from './lib/actions/run-workflow';
 import { burstyAiAuth } from './lib/common/auth';
+import { PieceCategory } from '@activepieces/shared';
 
 export const burstyAi = createPiece({
   displayName: 'Bursty-ai',
   auth: burstyAiAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/bursty-ai.png',
+  categories: [PieceCategory.ARTIFICIAL_INTELLIGENCE],
+  description:
+    "Automate content creation, SEO optimization, email outreach, and influencer partnerships with BurstyAI's no-code AI workflows.",
   authors: ['sanket-a11y'],
   actions: [runWorkflow],
   triggers: [],
