f1e75817cce9e55f8a775593e2bab34c09880447
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-12-03 14:28:01 +0300

    fix: google sheets build

--- packages/pieces/community/google-sheets/src/lib/triggers/new-worksheet.ts
+++ packages/pieces/community/google-sheets/src/lib/triggers/new-worksheet.ts
@@ -22,7 +22,7 @@ export const newWorksheetTrigger = createTrigger({
 		authClient.setCredentials(context.auth);
 		const sheets = google.sheets({ version: 'v4', auth: authClient });
 		const response = await sheets.spreadsheets.get({
-			spreadsheetId: context.propsValue.spreadsheetId,
+			spreadsheetId: context.propsValue.spreadsheetId as string,
 		});
 		if (response.data.sheets) {
 			for (const sheet of response.data.sheets) {
@@ -43,7 +43,7 @@ export const newWorksheetTrigger = createTrigger({
 		authClient.setCredentials(context.auth);
 		const sheets = google.sheets({ version: 'v4', auth: authClient });
 		const response = await sheets.spreadsheets.get({
-			spreadsheetId: context.propsValue.spreadsheetId,
+			spreadsheetId: context.propsValue.spreadsheetId as string,
 		});
 
 		if (response.data.sheets) {
@@ -63,7 +63,7 @@ export const newWorksheetTrigger = createTrigger({
 		const sheets = google.sheets({ version: 'v4', auth: authClient });
 
 		const response = await sheets.spreadsheets.get({
-			spreadsheetId: context.propsValue.spreadsheetId,
+			spreadsheetId: context.propsValue.spreadsheetId as string,
 		});
 		if (isNil(response.data.sheets) || response.data.sheets.length === 0) {
 			return [];
