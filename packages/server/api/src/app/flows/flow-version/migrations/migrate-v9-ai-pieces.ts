0162a04e919508dd981024b5f10d131555ff215e
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-12-07 13:55:26 +0000

    refactor: enhance AI migration logic and improve code consistency

--- packages/server/api/src/app/flows/flow-version/migrations/migrate-v9-ai-pieces.ts
+++ packages/server/api/src/app/flows/flow-version/migrations/migrate-v9-ai-pieces.ts
@@ -1,3 +1,4 @@
+import { AIProviderName } from '@activepieces/common-ai'
 import {
     FlowAction,
     FlowActionType,
@@ -13,16 +14,16 @@ export const migrateV9AiPieces: Migration = {
     migrate: async (flowVersion: FlowVersion): Promise<FlowVersion> => {
         const newVersion = flowStructureUtil.transferFlow(flowVersion, (step) => {
             if (step.type !== FlowActionType.PIECE) {
-                return step;
+                return step
             }
             if (step.settings.pieceName === '@activepieces/piece-text-ai') {
-                return migrateTextai(step);
+                return migrateTextai(step)
             }
             if (step.settings.pieceName === '@activepieces/piece-utility-ai') {
-                return migrateUtilityAction(step);
+                return migrateUtilityAction(step)
             }
             if (step.settings.pieceName === '@activepieces/piece-image-ai') {
-                return migrateImageai(step);
+                return migrateImageai(step)
             }
             return step
         })
@@ -35,7 +36,7 @@ export const migrateV9AiPieces: Migration = {
 }
 
 function migrateUtilityAction(step: PieceAction): FlowAction {
-    const input = step.settings?.input as Record<string, unknown>;
+    const input = step.settings?.input as Record<string, unknown>
     return {
         ...step,
         settings: {
@@ -44,21 +45,22 @@ function migrateUtilityAction(step: PieceAction): FlowAction {
             pieceVersion: '0.5.11',
             input: {
                 ...input,
+                ...migrateModel(input.provider as string, input.model as string),
                 // Fix typo in older version
                 schema: input?.schama as Record<string, unknown>,
                 maxOutputTokens: input.maxTokens ?? input?.maxOutputTokens,
                 maxTokens: undefined,
             },
         },
-    };
+    }
 }
 function migrateTextai(step: PieceAction): FlowAction {
-    const actionName = step.settings.actionName;
-    const input = step.settings?.input as Record<string, unknown>;
+    const actionName = step.settings.actionName
+    const input = step.settings?.input as Record<string, unknown>
        
     if (actionName === 'ask_ai') {
-        const webSearchOptions = (input['webSearchOptions'] ?? {}) as Record<string, unknown>;
-        const includeSources = webSearchOptions.includeSources ?? input?.includeSources;
+        const webSearchOptions = (input['webSearchOptions'] ?? {}) as Record<string, unknown>
+        const includeSources = webSearchOptions.includeSources ?? input?.includeSources
 
         // Max Tokens is replaced by maxOutputTokens
         // Include Sources is replaced by webSearchOptions.includeSources
@@ -73,12 +75,13 @@ function migrateTextai(step: PieceAction): FlowAction {
                     ...input,
                     webSearchOptions: {
                         ...webSearchOptions,
-                        includeSources: includeSources,
+                        includeSources,
                     },
+                    ...migrateModel(input.provider as string, input.model as string),
                     maxTokens: undefined,
                     includeSources: undefined,
                 },
-            }
+            },
         }
     }
     
@@ -88,6 +91,7 @@ function migrateTextai(step: PieceAction): FlowAction {
             ...step.settings,
             input: {
                 ...step.settings.input,
+                ...migrateModel(input.provider as string, input.model as string),
                 maxOutputTokens: input.maxTokens ?? input?.maxOutputTokens,
                 maxTokens: undefined,
             },
@@ -97,7 +101,7 @@ function migrateTextai(step: PieceAction): FlowAction {
 }
 
 function migrateImageai(step: PieceAction): FlowAction {
-    const input = step.settings?.input as Record<string, unknown>;
+    const input = step.settings?.input as Record<string, unknown>
     return {
         ...step,
         settings: {
@@ -106,12 +110,20 @@ function migrateImageai(step: PieceAction): FlowAction {
             pieceVersion: '0.2.18',
             input: {
                 ...input,
+                ...migrateModel(input.provider as string, input.model as string),
                 resolution: undefined,
                 advancedOptions: {
                     ...(input?.advancedOptions ?? {}),
                     size: input?.resolution ?? input?.size,
-                }
+                },
             },
         },
+    }
+}
+
+function migrateModel(provider: string, modelId: string): { model: string, provider: string } {
+    return {
+        provider: AIProviderName.ACTIVEPIECES,
+        model: `${provider.toLocaleLowerCase()}/${modelId}`,
     }
 }
