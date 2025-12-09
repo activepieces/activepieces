b9e86f59478107e363c610966bef1c9911f406ba
sanket@activepieces.com <sanket@activepieces.com>
2025-12-09 17:50:30 +0530

    feat: polling trigger, new actions

--- packages/pieces/community/echowin/src/index.ts
+++ packages/pieces/community/echowin/src/index.ts
@@ -1,13 +1,18 @@
-import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { createPiece } from '@activepieces/pieces-framework';
 import { echowinAuth } from './lib/common/auth';
-import { callCreated } from './lib/triggers/call-created';
+import { createContact } from './lib/actions/create-contact';
+import { findContactByName } from './lib/actions/find-contact-by-name';
+import { deleteContact } from './lib/actions/delete-contact';
+import { newContact } from './lib/triggers/new-contact';
+import { PieceCategory } from '@activepieces/shared';
 
 export const echowin = createPiece({
   displayName: 'Echowin',
   auth: echowinAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/echowin.png',
+  categories: [PieceCategory.ARTIFICIAL_INTELLIGENCE],
   authors: ['sanket-a11y'],
-  actions: [],
-  triggers: [callCreated],
+  actions: [createContact, deleteContact, findContactByName],
+  triggers: [newContact],
 });
--- packages/pieces/community/echowin/src/lib/common/auth.ts
+++ packages/pieces/community/echowin/src/lib/common/auth.ts
@@ -1,8 +1,7 @@
-import { PieceAuth } from "@activepieces/pieces-framework";
+import { PieceAuth } from '@activepieces/pieces-framework';
 
-
-export const echowinAuth= PieceAuth.SecretText({
-    displayName: "Echowin API Key",
-    description: "API Key for Echowin",
-    required: true,
+export const echowinAuth = PieceAuth.SecretText({
+  displayName: 'Echowin API Key',
+  description: 'API Key for Echowin. Get it from [Echowin Settings](https://echo.win/portal/settings/integrations)',
+  required: true,
 });
