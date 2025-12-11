253e02ee662c030fb97c4fb7c98deb835d2460dc
sanket@activepieces.com <sanket@activepieces.com>
2025-12-11 12:23:51 +0530

    feat: custom api call

--- packages/pieces/community/greenpt/src/index.ts
+++ packages/pieces/community/greenpt/src/index.ts
@@ -1,15 +1,33 @@
-import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { createPiece } from '@activepieces/pieces-framework';
 import { greenptAuth } from './lib/common/auth';
 import { chatCompletion } from './lib/actions/chat-completion';
 import { createEmbeddings } from './lib/actions/create-embeddings';
 import { transcribeAudio } from './lib/actions/transcribe-audio';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { PieceCategory } from '@activepieces/shared';
 
 export const greenpt = createPiece({
   displayName: 'Greenpt',
+  description:
+    'GreenPT is a green AI and privacy friendly GPT-powered chat platform',
   auth: greenptAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/greenpt.png',
   authors: ['sanket-a11y'],
-  actions: [chatCompletion, createEmbeddings, transcribeAudio],
+  categories: [PieceCategory.ARTIFICIAL_INTELLIGENCE],
+  actions: [
+    chatCompletion,
+    createEmbeddings,
+    transcribeAudio,
+    createCustomApiCallAction({
+      auth: greenptAuth,
+      baseUrl: () => `https://api.greenpt.ai/v1`,
+      authMapping: async (auth) => {
+        return {
+          Authorization: `Bearer ${auth.secret_text}`,
+        };
+      },
+    }),
+  ],
   triggers: [],
 });
