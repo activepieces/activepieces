6318722a106650ad42dfde43f1cbae6559a3eb6f
sanket@activepieces.com <sanket@activepieces.com>
2025-12-16 15:15:26 +0530

    feat: actions

--- packages/pieces/community/lofty/src/index.ts
+++ packages/pieces/community/lofty/src/index.ts
@@ -1,11 +1,15 @@
-import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { createPiece } from '@activepieces/pieces-framework';
+import { loftyAuth } from './lib/common/auth';
+import { createLead } from './lib/actions/create-lead';
+import { PieceCategory } from '@activepieces/shared';
 
 export const lofty = createPiece({
   displayName: 'Lofty',
-  auth: PieceAuth.None(),
+  auth: loftyAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/lofty.png',
-  authors: [],
-  actions: [],
+  categories: [PieceCategory.SALES_AND_CRM],
+  authors: ['sanket-a11y'],
+  actions: [createLead],
   triggers: [],
 });
