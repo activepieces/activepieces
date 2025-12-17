810af03f5b4f5ab5a21b51f5f3b6865012e3afcd
Mohammad AbuAboud <moabuaboud@gmail.com>
2025-12-18 01:39:48 +0300

    style: format date with time in FlowRunCard for improved readability

--- packages/react-ui/src/app/builder/run-list/flow-run-card.tsx
+++ packages/react-ui/src/app/builder/run-list/flow-run-card.tsx
@@ -158,7 +158,10 @@ const FlowRunCard = React.memo(
         </div>
         <div className="grid gap-2">
           <div className="text-sm font-medium leading-none flex gap-2 items-center">
-            {formatUtils.formatDateWithTime(new Date(run.created ?? new Date()), true)}{' '}
+            {formatUtils.formatDateWithTime(
+              new Date(run.created ?? new Date()),
+              true,
+            )}{' '}
             {run.id === viewedRunId && <Eye className="w-3.5 h-3.5"></Eye>}
           </div>
           {isFlowRunStateTerminal({
