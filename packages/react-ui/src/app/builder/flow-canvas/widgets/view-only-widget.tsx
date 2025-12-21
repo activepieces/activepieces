161d97458970ee13b141430b702f9ece2233d457
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2025-12-21 11:28:01 +0300

    fix: view only and end widgets

--- packages/react-ui/src/app/builder/flow-canvas/widgets/view-only-widget.tsx
+++ packages/react-ui/src/app/builder/flow-canvas/widgets/view-only-widget.tsx
@@ -3,7 +3,7 @@ import { t } from 'i18next';
 const ViewOnlyWidget = () => {
   return (
     <div
-      className="p-2 bg-border text-foreground/70 rounded-lg animate-fade"
+      className="p-1 px-2 text-sm  bg-border text-foreground/70 rounded-md animate-fade"
       key={'view-only-widget'}
     >
       {t('View Only')}
