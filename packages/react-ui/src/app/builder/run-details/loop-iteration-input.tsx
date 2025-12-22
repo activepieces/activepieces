e6fb3a539991ee5a88af156fd603abf036b5b462
AbdulTheActivePiecer <abdulyki@activepieces.com>
2025-12-22 12:13:01 +0300

    fix: finish design for iteration input in canvas

--- packages/react-ui/src/app/builder/run-details/loop-iteration-input.tsx
+++ packages/react-ui/src/app/builder/run-details/loop-iteration-input.tsx
@@ -1,13 +1,17 @@
 import { useMemo, useRef } from 'react';
 
 import { Input } from '@/components/ui/input';
-import { FlowActionType, flowStructureUtil } from '@activepieces/shared';
+import { FlowActionType, flowStructureUtil, isNil } from '@activepieces/shared';
 
 import { flowRunUtils } from '../../../features/flow-runs/lib/flow-run-utils';
 import { useBuilderStateContext } from '../builder-hooks';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { t } from 'i18next';
+import { Button } from '@/components/ui/button';
+import { CaretDownIcon, CaretUpIcon } from '@radix-ui/react-icons';
 
 const LoopIterationInput = ({ stepName }: { stepName: string }) => {
-  const [setLoopIndex, currentIndex, run, flowVersion, loopsIndexes, selectedStep,currentStep] =
+  const [setLoopIndex, currentIndex, run, flowVersion, loopsIndexes, selectedStep, currentStep] =
     useBuilderStateContext((state) => [
       state.setLoopIndex,
       state.loopsIndexes[stepName] ?? 0,
@@ -15,60 +19,87 @@ const LoopIterationInput = ({ stepName }: { stepName: string }) => {
       state.flowVersion,
       state.loopsIndexes,
       state.selectedStep,
-      flowStructureUtil.getStepOrThrow(stepName, state.flowVersion.trigger),
+      flowStructureUtil.getStep(stepName, state.flowVersion.trigger),
     ]);
   const stepOutput = useMemo(() => {
     return run && run.steps
       ? flowRunUtils.extractStepOutput(
-          stepName,
-          loopsIndexes,
-          run.steps,
-          flowVersion.trigger,
-        )
+        stepName,
+        loopsIndexes,
+        run.steps,
+        flowVersion.trigger,
+      )
       : null;
   }, [run, stepName, loopsIndexes, flowVersion.trigger]);
 
   const inputRef = useRef<HTMLInputElement>(null);
   const totalIterations =
     stepOutput &&
-    stepOutput.output &&
-    stepOutput.type === FlowActionType.LOOP_ON_ITEMS
+      stepOutput.output &&
+      stepOutput.type === FlowActionType.LOOP_ON_ITEMS
       ? stepOutput.output.iterations.length
       : 0;
 
-  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const value = e.target.value ?? '1';
+  function onChange(value: string) {
     const parsedValue = Math.max(
       1,
       Math.min(parseInt(value) ?? 1, totalIterations),
     );
     setLoopIndex(stepName, parsedValue - 1);
   }
-  const isChildSelected = selectedStep && currentStep ? flowStructureUtil.isChildOf(currentStep, selectedStep) : false;
+  const isChildSelected = selectedStep && currentStep && currentStep.name !== selectedStep ? flowStructureUtil.isChildOf(currentStep, selectedStep) : false;
 
-  if(totalIterations === 0 || !isChildSelected) {
+  if (!isChildSelected || isNil(run)) {
     return <></>
   }
 
   return (
-    <Input
-    ref={inputRef}
-    className="py-2 duration-300 min-w-[40px] w-[40px] max-w-[70px] h-[40px] animate-in fade-in absolute  top-2 -left-[50px]  bg-background border-border border border-solid rounded-md text-center text-sm "
-    value={currentIndex + 1}
-
-    type="number"
-    min={1}
-    max={totalIterations}
-    onClick={(e) => {
-      if(e.button === 0) {
-        e.preventDefault();
-        e.stopPropagation();
-      }
-    }}
-    onChange={onChange}
-  />
+    <div className="absolute -top-4 -left-[45px]">
+      <div className='flex items-center justify-center flex-col gap-0.5'>
+        <LoopIterationInputButton onChange={onChange} isIncreasing={true} currentIndex={currentIndex} />
+        <Tooltip >
+          <TooltipTrigger >
+            <Input
+              ref={inputRef}
+              className="py-2 duration-300 w-[35px] px-0 h-[35px] animate-in fade-in bg-background border-border border border-solid rounded-md text-center !text-xs"
+              type="number"
+              value={currentIndex + 1}
+              min={1}
+              max={totalIterations}
+              onClick={(e) => {
+                if (e.button === 0) {
+                  e.preventDefault();
+                  e.stopPropagation();
+                }
+              }}
+              onChange={(e) => {
+                const value = isNil(e.target.value) || e.target.value.length === 0 || e.target.value === 'e' ? '1' : e.target.value;
+                onChange(value);
+              }}
+            />
+          </TooltipTrigger>
+          <TooltipContent side='left' >
+            {t('View outputs of child steps for this iteration')}
+          </TooltipContent>
+        </Tooltip>
+        <LoopIterationInputButton onChange={onChange} isIncreasing={false} currentIndex={currentIndex} />
+      </div>
+    </div>
   );
 };
 
 LoopIterationInput.displayName = 'LoopIterationInput';
 export { LoopIterationInput };
+
+
+const LoopIterationInputButton = ({ onChange, isIncreasing, currentIndex }: { onChange: (val: string) => void, isIncreasing: boolean, currentIndex: number }) => {
+  return (
+    <Button variant="ghost" onClick={(e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      onChange((currentIndex + (isIncreasing ? 2 : 0)).toString());
+    }} className='hover:bg-builder-background size-6' size="icon">
+      {isIncreasing ? <CaretUpIcon className='w-2 h-2'></CaretUpIcon> : <CaretDownIcon className='w-2 h-2'></CaretDownIcon>}
+    </Button>
+  )
+}
