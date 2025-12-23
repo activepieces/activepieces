0e87b2cb6063ae45971544901383ef77239b74ce
sanket@activepieces.com <sanket@activepieces.com>
2025-12-23 16:52:33 +0530

    feat: actions

--- packages/pieces/community/octopush-sms/src/index.ts
+++ packages/pieces/community/octopush-sms/src/index.ts
@@ -1,13 +1,33 @@
+import { createPiece } from '@activepieces/pieces-framework';
+import { octopushAuth } from './lib/common/auth';
+import { addContact } from './lib/actions/add-contact';
+import { sendANewSms } from './lib/actions/send-a-new-sms';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { BASE_URL } from './lib/common/client';
+import { PieceCategory } from '@activepieces/shared';
 
-    import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-
-    export const octopushSms = createPiece({
-      displayName: "Octopush-sms",
-      auth: PieceAuth.None(),
-      minimumSupportedRelease: '0.36.1',
-      logoUrl: "https://cdn.activepieces.com/pieces/octopush-sms.png",
-      authors: [],
-      actions: [],
-      triggers: [],
-    });
-    
+export const octopushSms = createPiece({
+  displayName: 'Octopush-sms',
+  auth: octopushAuth,
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: 'https://cdn.activepieces.com/pieces/octopush-sms.png',
+  description:
+    'Send bulk messaging for your promotions, order tracking, appointment reminders, voice messages, one-time passwords (OTP), 2-way chat sms and much more!',
+  authors: ['sanket-a11y'],
+  categories: [PieceCategory.COMMUNICATION],
+  actions: [
+    addContact,
+    sendANewSms,
+    createCustomApiCallAction({
+      auth: octopushAuth,
+      baseUrl: () => BASE_URL,
+      authMapping: async (auth) => {
+        return {
+          'api-key': auth.props.api_key,
+          'api-login': auth.props.api_login,
+        };
+      },
+    }),
+  ],
+  triggers: [],
+});
--- packages/pieces/community/octopush-sms/src/lib/common/client.ts
+++ packages/pieces/community/octopush-sms/src/lib/common/client.ts
@@ -4,6 +4,7 @@ export const BASE_URL = `https://api.octopush.com/v1/public`;
 
 export async function makeRequest<T>(
   api_key: string,
+  api_login: string,
   method: HttpMethod,
   path: string,
   body?: unknown
@@ -12,7 +13,10 @@ export async function makeRequest<T>(
     const response = await httpClient.sendRequest<T>({
       method,
       url: `${BASE_URL}${path}`,
-      headers: {},
+      headers: {
+        'api-login': api_login,
+        'api-key': api_key,
+      },
       body,
     });
     return response.body;
