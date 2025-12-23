da5aecf7c3d06c4bdb99293981bf5c4cc343a80c
David Anyatonwu <davidanyatonwu@gmail.com>
2025-12-23 04:23:06 +0100

    chore(qwilr): undo validation

--- packages/pieces/community/qwilr/src/lib/common/auth.ts
+++ packages/pieces/community/qwilr/src/lib/common/auth.ts
@@ -8,25 +8,4 @@ export const qwilrAuth = PieceAuth.SecretText({
     1. Go to your Qwilr account settings.
     2. Navigate to API Settings.
     3. Copy your access token.`,
-    validate: async ({ auth }) => {
-        try {
-            await httpClient.sendRequest({
-                method: HttpMethod.GET,
-                url: 'https://api.qwilr.com/v1/users',
-                headers: {
-                    'Authorization': `Bearer ${auth}`
-                }
-            })
-
-            return {
-                valid: true
-            }
-        }
-        catch {
-            return {
-                valid: false,
-                error: 'Invalid API Key.'
-            }
-        }
-    }
 })
