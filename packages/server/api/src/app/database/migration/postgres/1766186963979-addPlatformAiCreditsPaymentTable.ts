e6ca39349c5abb0ac74f58958b15edb0b5b942f1
Yasser-Belatreche <yasser.belatreche0@gmail.com>
2025-12-24 01:46:06 +0100

    feat(aicredit-new-billing): rm left payment_history table in the migration

--- packages/server/api/src/app/database/migration/postgres/1766186963979-addPlatformAiCreditsPaymentTable.ts
+++ packages/server/api/src/app/database/migration/postgres/1766186963979-addPlatformAiCreditsPaymentTable.ts
@@ -26,16 +26,9 @@ export class AddPlatformAiCreditsPaymentTable1766186963979 implements MigrationI
             ALTER TABLE "platform_plan"
             ADD "aiCreditsAutoTopUpStripePaymentMethod" character varying
         `)
-        await queryRunner.query(`
-            ALTER TABLE "platform_ai_credits_payment"
-            ADD CONSTRAINT "fk_platform_ai_credits_payment_platform_id" FOREIGN KEY ("platformId") REFERENCES "platform"("id") ON DELETE CASCADE ON UPDATE NO ACTION
-        `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            ALTER TABLE "platform_ai_credits_payment" DROP CONSTRAINT "fk_platform_ai_credits_payment_platform_id"
-        `)
         await queryRunner.query(`
             ALTER TABLE "platform_plan" DROP COLUMN "aiCreditsAutoTopUpStripePaymentMethod"
         `)
