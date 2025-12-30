b7615bc664afdc1e30bd362729b9653bda0be41e
sanket@activepieces.com <sanket@activepieces.com>
2025-12-30 14:09:09 +0530

    feat: custom api call

--- packages/pieces/community/influencers-club/src/index.ts
+++ packages/pieces/community/influencers-club/src/index.ts
@@ -4,14 +4,28 @@ import { enrichCreatorByEmail } from './lib/actions/enrich-creator-by-email';
 import { enrichCreatorByHandle } from './lib/actions/enrich-creator-by-handle';
 import { findSimilarCreator } from './lib/actions/find-similar-creator';
 import { PieceCategory } from '@activepieces/shared';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
 
 export const influencersClub = createPiece({
   displayName: 'Influencers.club',
   auth: influencersClubAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/influencers-club.png',
-  categories: [PieceCategory.PRODUCTIVITY],
+  categories: [PieceCategory.MARKETING],
   authors: ['sanket-a11y'],
-  actions: [enrichCreatorByEmail, enrichCreatorByHandle, findSimilarCreator],
+  description:
+    'Connect to Influencers.club to enrich and find influencers for your marketing campaigns.',
+  actions: [
+    enrichCreatorByEmail,
+    enrichCreatorByHandle,
+    findSimilarCreator,
+    createCustomApiCallAction({
+      auth: influencersClubAuth,
+      baseUrl: () => `https://api-dashboard.influencers.club/public/v1`,
+      authMapping: async (auth) => {
+        return { Authorization: `Bearer ${auth.secret_text}` };
+      },
+    }),
+  ],
   triggers: [],
 });
