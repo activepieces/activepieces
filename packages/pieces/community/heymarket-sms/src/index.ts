05b921a22a7e2281f39860edebccab2b931f7700
sanket@activepieces.com <sanket@activepieces.com>
2025-12-30 11:31:59 +0530

    chore: replace webhook trigger with polling trigger

--- packages/pieces/community/heymarket-sms/src/index.ts
+++ packages/pieces/community/heymarket-sms/src/index.ts
@@ -1,18 +1,16 @@
-import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { createPiece } from '@activepieces/pieces-framework';
 import { PieceCategory } from '@activepieces/shared';
 import { heymarketSmsAuth } from './lib/common/auth';
-import { chatStarted } from './lib/triggers/chat-started';
-import { contactUnsubscribe } from './lib/triggers/contact-unsubscribe';
-import { incommingMessage } from './lib/triggers/incomming-message';
-import { outgoingMessage } from './lib/triggers/outgoing-message';
 import { createOrUpdateContact } from './lib/actions/create-or-update-contact';
 import { sendCustomMessage } from './lib/actions/send-custom-message';
 import { sendTemplateMessage } from './lib/actions/send-template-message';
 import { updateList } from './lib/actions/update-list';
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { chatStarted } from './lib/triggers/chat-started-';
+import { newOrUpdatedContact } from './lib/triggers/new-or-updated-contact';
 
 export const heymarketSms = createPiece({
-  displayName: 'Heymarket-sms',
+  displayName: 'Heymarket SMS',
   auth: heymarketSmsAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/heymarket-sms.png',
@@ -35,10 +33,5 @@ export const heymarketSms = createPiece({
       },
     }),
   ],
-  triggers: [
-    chatStarted,
-    contactUnsubscribe,
-    incommingMessage,
-    outgoingMessage,
-  ],
+  triggers: [chatStarted, newOrUpdatedContact],
 });
