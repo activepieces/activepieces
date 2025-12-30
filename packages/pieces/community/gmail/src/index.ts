de6038cec1384594d68c8228ddcf109c7e4d1e42
SIXxMENDER <BRUNO200406@HOTMAIL.COM>
2025-12-29 23:38:49 -0600

    eat: add starred, thread, attachment and label triggers to gmail.

--- packages/pieces/community/gmail/src/index.ts
+++ packages/pieces/community/gmail/src/index.ts
@@ -1,59 +1,59 @@
-import { createCustomApiCallAction } from '@activepieces/pieces-common';
 import {
-  OAuth2PropertyValue,
-  PieceAuth,
-  createPiece,
+    createCustomApiCallAction,
+} from '@activepieces/pieces-common';
+import {
+    OAuth2PropertyValue,
+    PieceAuth,
+    createPiece,
 } from '@activepieces/pieces-framework';
 import { PieceCategory } from '@activepieces/shared';
 import { gmailSendEmailAction } from './lib/actions/send-email-action';
 import { gmailNewEmailTrigger } from './lib/triggers/new-email';
 import { gmailNewLabeledEmailTrigger } from './lib/triggers/new-labeled-email';
+import { gmailNewStarredEmailTrigger } from './lib/triggers/new-starred-email';
+import { gmailNewThreadTrigger } from './lib/triggers/new-thread';
+import { gmailNewAttachmentTrigger } from './lib/triggers/new-attachment';
+import { gmailNewLabelTrigger } from './lib/triggers/new-label';
 
 export const gmailAuth = PieceAuth.OAuth2({
-  description: '',
-  authUrl: 'https://accounts.google.com/o/oauth2/auth',
-  tokenUrl: 'https://oauth2.googleapis.com/token',
-  required: true,
-  scope: [
-    'https://www.googleapis.com/auth/gmail.send',
-    'email',
-    'https://www.googleapis.com/auth/gmail.readonly',
-    'https://www.googleapis.com/auth/gmail.compose',
-  ],
+    description: '',
+    authUrl: 'https://accounts.google.com/o/oauth2/auth',
+    tokenUrl: 'https://oauth2.googleapis.com/token',
+    required: true,
+    scope: [
+        'https://www.googleapis.com/auth/gmail.send',
+        'email',
+        'https://www.googleapis.com/auth/gmail.readonly',
+        'https://www.googleapis.com/auth/gmail.compose',
+        'https://www.googleapis.com/auth/gmail.labels', // Required for new-label trigger
+    ],
 });
 
 export const gmail = createPiece({
-  minimumSupportedRelease: '0.30.0',
-  logoUrl: 'https://cdn.activepieces.com/pieces/gmail.png',
-  categories: [
-    PieceCategory.COMMUNICATION,
-    PieceCategory.BUSINESS_INTELLIGENCE,
-  ],
-  actions: [
-    gmailSendEmailAction,
-    createCustomApiCallAction({
-      baseUrl: () => 'https://gmail.googleapis.com/gmail/v1',
-      auth: gmailAuth,
-      authMapping: async (auth) => ({
-        Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
-      }),
-    }),
-  ],
-  displayName: 'Gmail',
-  description: 'Email service by Google',
-
-  authors: [
-    'kanarelo',
-    'abdullahranginwala',
-    'BastienMe',
-    'Salem-Alaa',
-    'kishanprmr',
-    'MoShizzle',
-    'AbdulTheActivePiecer',
-    'khaledmashaly',
-    'abuaboud',
-    'AdamSelene',
-  ],
-  triggers: [gmailNewEmailTrigger, gmailNewLabeledEmailTrigger],
-  auth: gmailAuth,
+    displayName: 'Gmail',
+    description: 'Send and receive emails with Gmail',
+    auth: gmailAuth,
+    minimumSupportedRelease: '0.20.0',
+    logoUrl: 'https://cdn.activepieces.com/pieces/gmail.png',
+    categories: [PieceCategory.COMMUNICATION, PieceCategory.APPS],
+    authors: ['activepieces', 'kishanprmr', 'MoShizzle', 'AbdulTheActivePiecer', 'PsiquisX-User'],
+    actions: [
+        gmailSendEmailAction,
+        createCustomApiCallAction({
+            baseUrl: () => 'https://gmail.googleapis.com/gmail/v1',
+            auth: gmailAuth,
+            authMapping: (auth) => ({
+                Authorization: `Bearer ${(auth as OAuth2PropertyValue).access_token}`,
+            }),
+        }),
+    ],
+    triggers: [
+        gmailNewEmailTrigger,
+        gmailNewLabeledEmailTrigger,
+        gmailNewStarredEmailTrigger,
+        gmailNewThreadTrigger,
+        gmailNewAttachmentTrigger,
+        gmailNewLabelTrigger
+    ],
 });
+
