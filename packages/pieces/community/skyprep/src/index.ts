97184cfa0f6c646074204802a5e45d238cf99e45
sanket@activepieces.com <sanket@activepieces.com>
2025-12-30 17:40:50 +0530

    fix: imports

--- packages/pieces/community/skyprep/src/index.ts
+++ packages/pieces/community/skyprep/src/index.ts
@@ -1,5 +1,11 @@
 import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 import { skyprepAuth } from './lib/common/auth';
+import { enrollAUserIntoACource } from './lib/actions/enroll-a-user-into-a-cource';
+import { enrollAUserIntoAUserGroup } from './lib/actions/enroll-a-user-into-a-user-group';
+import { updateUser } from './lib/actions/update-user';
+import { courceFailed } from './lib/triggers/cource-failed';
+import { newUser } from './lib/triggers/new-user';
+import { courcePassed } from './lib/triggers/cource-passed';
 
 export const skyprep = createPiece({
   displayName: 'SkyPrep',
@@ -7,6 +13,6 @@ export const skyprep = createPiece({
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/skyprep.png',
   authors: ['sanket-a11y'],
-  actions: [],
-  triggers: [],
+  actions: [enrollAUserIntoACource, enrollAUserIntoAUserGroup, updateUser],
+  triggers: [courceFailed, courcePassed, newUser],
 });
