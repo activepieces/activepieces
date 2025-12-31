09b619c2fba64cc12bd14ce19d0827de1b9a99ea
sanket@activepieces.com <sanket@activepieces.com>
2025-12-31 15:29:40 +0530

    feat: custom api call

--- packages/pieces/community/twitch/src/index.ts
+++ packages/pieces/community/twitch/src/index.ts
@@ -1,21 +1,34 @@
-
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-import { streamStarted } from "./lib/triggers/stream-started";
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { streamStarted } from './lib/triggers/stream-started';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
 
 export const twitchAuth = PieceAuth.OAuth2({
-  description: "Authenticate with your twitch account",
-  authUrl: "https://id.twitch.tv/oauth2/authorize",
-  tokenUrl: "https://id.twitch.tv/oauth2/token",
+  description: 'Authenticate with your twitch account',
+  authUrl: 'https://id.twitch.tv/oauth2/authorize',
+  tokenUrl: 'https://id.twitch.tv/oauth2/token',
   required: true,
-  scope: []
-})
+  scope: [],
+});
 export const twitch = createPiece({
-  displayName: "Twitch",
+  displayName: 'Twitch',
   auth: twitchAuth,
   minimumSupportedRelease: '0.36.1',
-  logoUrl: "https://cdn.activepieces.com/pieces/twitch.png",
-  authors: [],
-  actions: [],
-  triggers: [streamStarted]
+  logoUrl: 'https://cdn.activepieces.com/pieces/twitch.png',
+  authors: ['arinmakk', 'sanket-a11y'],
+  description:
+    "Twitch is the world's leading live streaming platform for gamers and the things we love!",
+  actions: [
+    createCustomApiCallAction({
+      auth: twitchAuth,
+      baseUrl: () => `https://api.twitch.tv/helix`,
+      authMapping: async (auth) => {
+        return {
+          Authorization: `Bearer ${auth.access_token}`,
+          'Content-Type': 'application/json',
+          'Client-ID': auth.client_id,
+        };
+      },
+    }),
+  ],
+  triggers: [streamStarted],
 });
-
