bcb315d4747127c9771e097f4d1025a6864ebb4a
Mohammad AbuAboud <moabuaboud@gmail.com>
2026-01-02 14:50:59 +0100

    test: update Flow and Project API tests to reflect changes in response structure and status codes

--- packages/server/api/test/integration/ce/flows/flow.test.ts
+++ packages/server/api/test/integration/ce/flows/flow.test.ts
@@ -75,7 +75,7 @@ describe('Flow API', () => {
             expect(response?.statusCode).toBe(StatusCodes.CREATED)
             const responseBody = response?.json()
 
-            expect(Object.keys(responseBody)).toHaveLength(12)
+            expect(Object.keys(responseBody)).toHaveLength(13)
             expect(responseBody?.id).toHaveLength(21)
             expect(responseBody?.created).toBeDefined()
             expect(responseBody?.updated).toBeDefined()
@@ -266,7 +266,7 @@ describe('Flow API', () => {
             expect(response?.statusCode).toBe(StatusCodes.OK)
             const responseBody = response?.json()
 
-            expect(Object.keys(responseBody)).toHaveLength(12)
+            expect(Object.keys(responseBody)).toHaveLength(13)
             expect(responseBody?.id).toBe(mockFlow.id)
             expect(responseBody?.created).toBeDefined()
             expect(responseBody?.updated).toBeDefined()
@@ -379,7 +379,7 @@ describe('Flow API', () => {
             const responseBody: PopulatedFlow | undefined = response?.json()
             expect(responseBody).toBeDefined()
             if (responseBody) {
-                expect(Object.keys(responseBody)).toHaveLength(12)
+                expect(Object.keys(responseBody)).toHaveLength(13)
                 expect(responseBody.id).toBe(mockFlow.id)
                 expect(responseBody.created).toBeDefined()
                 expect(responseBody.updated).toBeDefined()
