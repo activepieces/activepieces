263b166091a06dc433c37190515244af38518dea
Hazem Adel <hazemadelkhalel@gmail.com>
2026-01-06 16:52:08 +0000

    feat: support dynamic template categories for cloud

--- packages/server/api/src/app/flags/flag.service.ts
+++ packages/server/api/src/app/flags/flag.service.ts
@@ -1,5 +1,5 @@
 import { AppSystemProp, apVersionUtil, webhookSecretsUtils } from '@activepieces/server-shared'
-import { ApEdition, ApFlagId, ExecutionMode, Flag, isNil, TemplateCategory } from '@activepieces/shared'
+import { ApEdition, ApFlagId, ExecutionMode, Flag, isNil } from '@activepieces/shared'
 import { In } from 'typeorm'
 import { repoFactory } from '../core/db/repo-factory'
 import { federatedAuthnService } from '../ee/authentication/federated-authn/federated-authn-service'
@@ -323,7 +323,7 @@ export type FlagType =
     | BaseFlagStructure<ApFlagId.TELEMETRY_ENABLED, boolean>
     | BaseFlagStructure<ApFlagId.USER_CREATED, boolean>
     | BaseFlagStructure<ApFlagId.WEBHOOK_URL_PREFIX, string>
-    | BaseFlagStructure<ApFlagId.TEMPLATES_CATEGORIES, TemplateCategory[]>
+    | BaseFlagStructure<ApFlagId.TEMPLATES_CATEGORIES, string[]>
 
 type BaseFlagStructure<K extends ApFlagId, V> = {
     id: K
