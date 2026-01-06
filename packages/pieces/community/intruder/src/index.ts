eda3e83d9044c7771563f29f3a9e8336693fc554
sanket@activepieces.com <sanket@activepieces.com>
2026-01-06 11:27:19 +0530

    fix: piece index

--- packages/pieces/community/intruder/src/index.ts
+++ packages/pieces/community/intruder/src/index.ts
@@ -5,9 +5,10 @@ import { intruderAuth } from './lib/common/auth';
 import { searchForATarget } from './lib/actions/search-for-a-target';
 import { searchForAnIssue } from './lib/actions/search-for-an-issue';
 import { searchForAnIssueOccurrence } from './lib/actions/search-for-an-issue-occurrence';
-import { newIssue } from './lib/triggers/new-issue';
+// import { newIssue } from './lib/triggers/new-issue';
 import { scanComplete } from './lib/triggers/scan-complete';
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { PieceCategory } from '@activepieces/shared';
 
 export const intruder = createPiece({
   displayName: 'Intruder',
@@ -15,6 +16,9 @@ export const intruder = createPiece({
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/intruder.png',
   authors: ['sanket-a11y'],
+  categories: [PieceCategory.DEVELOPER_TOOLS],
+  description:
+    ' Intruder is a powerful vulnerability management platform that helps businesses identify, assess, and remediate security risks across their digital assets. With its comprehensive scanning capabilities and user-friendly interface, Intruder enables organizations to stay ahead of potential threats and maintain a robust security posture.',
   actions: [
     addTarget,
     startScan,
@@ -29,5 +33,5 @@ export const intruder = createPiece({
       }),
     }),
   ],
-  triggers: [newIssue, scanComplete],
+  triggers: [scanComplete],
 });
