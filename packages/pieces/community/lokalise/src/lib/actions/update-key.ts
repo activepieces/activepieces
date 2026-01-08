f32b84be0c29c50de03bbe6d4b0ee074788a2444
sanket@activepieces.com <sanket@activepieces.com>
2026-01-08 11:29:01 +0530

    feat: actions

---
+++ packages/pieces/community/lokalise/src/lib/actions/update-key.ts
@@ -0,0 +1,12 @@
+import { createAction, Property } from '@activepieces/pieces-framework';
+
+export const updateKey = createAction({
+  // auth: check https://www.activepieces.com/docs/developers/piece-reference/authentication,
+  name: 'updateKey',
+  displayName: 'Update Key',
+  description: '',
+  props: {},
+  async run() {
+    // Action logic here
+  },
+});
---
+++ packages/pieces/community/lokalise/src/lib/actions/update-translation.ts
@@ -0,0 +1,12 @@
+import { createAction, Property } from '@activepieces/pieces-framework';
+
+export const updateTranslation = createAction({
+  // auth: check https://www.activepieces.com/docs/developers/piece-reference/authentication,
+  name: 'updateTranslation',
+  displayName: 'Update Translation',
+  description: '',
+  props: {},
+  async run() {
+    // Action logic here
+  },
+});
