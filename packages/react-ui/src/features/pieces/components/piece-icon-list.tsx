1026404caa9ca7058575c6f10b5571aca5704e60
Mohammad AbuAboud <moabuaboud@gmail.com>
2026-01-11 15:21:59 +0100

    style: piece icon style

--- packages/react-ui/src/features/pieces/components/piece-icon-list.tsx
+++ packages/react-ui/src/features/pieces/components/piece-icon-list.tsx
@@ -18,7 +18,7 @@ import { stepsHooks } from '../lib/steps-hooks';
 import { PieceIcon } from './piece-icon';
 
 const extraIconVariants = cva(
-  'flex items-center justify-center p-2 rounded-full border border-solid text-xs select-none',
+  'flex rounded-md items-center justify-center p-2 bg-background border border-solid text-xs select-none',
   {
     variants: {
       size: {
