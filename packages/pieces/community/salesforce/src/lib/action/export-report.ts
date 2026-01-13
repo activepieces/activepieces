fed01e3846c84cf62b5708b010a4d85b66956265
sanket@activepieces.com <sanket@activepieces.com>
2026-01-13 17:00:00 +0530

    fix:  action

--- packages/pieces/community/salesforce/src/lib/action/export-report.ts
+++ packages/pieces/community/salesforce/src/lib/action/export-report.ts
@@ -1,37 +1,50 @@
 import { createAction } from '@activepieces/pieces-framework';
-import { HttpMethod, httpClient, AuthenticationType } from '@activepieces/pieces-common';
+import {
+  HttpMethod,
+  httpClient,
+  AuthenticationType,
+} from '@activepieces/pieces-common';
 import { salesforceAuth } from '../..';
-import { salesforcesCommon } from '../common';
-
+import { callSalesforceApi, salesforcesCommon } from '../common';
 
 export const exportReport = createAction({
   auth: salesforceAuth,
   name: 'export_report',
-  displayName: 'Export Report',
+  displayName: 'Export Report ',
   description: 'Export a Salesforce report as an Excel file.',
   props: {
     report_id: salesforcesCommon.report,
   },
   async run(context) {
     const { report_id } = context.propsValue;
-    const auth = context.auth.access_token;
-
+    const access_token = context.auth.access_token;
+    
     const response = await httpClient.sendRequest<ArrayBuffer>({
-      method: HttpMethod.GET,
+      method: HttpMethod.POST,
       url: `${context.auth.data['instance_url']}/services/data/v56.0/analytics/reports/${report_id}?export=1&enc=UTF-8&xf=excel`,
       authentication: {
         type: AuthenticationType.BEARER_TOKEN,
-        token: auth,
+        token: access_token,
       },
       headers: {
-        'Accept': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        Accept:
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
       },
       responseType: 'arraybuffer',
     });
 
-    return {
-      fileName: `report_${report_id}.xlsx`,
-      data: Buffer.from(response.body).toString('base64'),
-    };
+    const report = (await callSalesforceApi(
+      HttpMethod.GET,
+      context.auth,
+      `/services/data/v56.0/analytics/reports/${report_id}`,
+      undefined
+    )) as any;
+
+    const reportName = report.body.attributes.reportName || report_id;
+
+    return await context.files.write({
+      fileName: `report_${reportName}.xlsx`,
+      data: Buffer.from(response.body),
+    });
   },
 });
