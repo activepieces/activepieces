32695b44486ea90a727d98b1310b6858f4e7b139
louai <louai@activepieces.com>
2026-01-14 20:25:39 +0100

    feat: connect to mcp server before adding the tool (validation)

---
+++ packages/shared/src/lib/agents/mcp.ts
@@ -0,0 +1,76 @@
+import { Tool } from "ai";
+import { McpAuthConfig, McpAuthType, McpProtocol } from "./tools";
+import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp.js';
+import { MCPClient } from '@ai-sdk/mcp';
+
+export function buildAuthHeaders(authConfig: McpAuthConfig): Record<string, string> {
+  let headers: Record<string, string> = {};
+
+  switch (authConfig.type) {
+    case McpAuthType.NONE:
+      break;
+    case McpAuthType.HEADERS: {
+      headers = authConfig.headers
+      break;
+    }
+    case McpAuthType.ACCESS_TOKEN: {
+      headers['Authorization'] = `Bearer ${authConfig.accessToken}`
+      break;
+    }
+    case McpAuthType.API_KEY: {
+      const headerName = authConfig.apiKeyHeader;
+      headers[headerName] = authConfig.apiKey
+      break;
+    }
+  }
+
+  return headers;
+}
+
+export function createTransportConfig(
+  protocol: McpProtocol,
+  serverUrl: string,
+  headers: Record<string, string> = {}
+): any {
+  const url = new URL(serverUrl);
+
+  switch (protocol) {
+    case McpProtocol.SIMPLE_HTTP: {
+      return {
+        type: 'http',
+        url: serverUrl,
+        headers: headers,
+      };
+    }
+    case McpProtocol.STREAMABLE_HTTP: {
+      const sessionId = crypto.randomUUID()
+      return new StreamableHTTPClientTransport(url, {
+        sessionId: sessionId,
+        requestInit: {
+          headers
+        }
+      })
+    }
+    case McpProtocol.SSE: {
+      return {
+        type: 'sse',
+        url: serverUrl,
+        headers: headers,
+      };
+    }
+    default:
+      throw new Error(`Unsupported MCP protocol type: ${protocol}`);
+  }
+}
+
+export type McpServerTools = {
+  mcpName: string;
+  mcpClient: MCPClient;
+  tools: Record<string, Tool>
+}
+
+export type ValidateAgentMcpToolResponse = {
+  toolNames?: string[];
+  error?: string;
+};
+
