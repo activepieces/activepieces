781d79b3adbff9f712795bb87536e03963b7b48c
Abdul-rahman Yasir Khalil <Abdulyki@activepieces.com>
2026-01-18 11:20:10 +0300

    fix: make hover effect on transparent buttons more prominent + make loading skeletons more visible

--- packages/react-ui/src/components/ui/skeleton.tsx
+++ packages/react-ui/src/components/ui/skeleton.tsx
@@ -6,7 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn('animate-pulse rounded-md bg-muted/50', className)}
+      className={cn('animate-pulse rounded-md bg-muted', className)}
       {...props}
     />
   );
