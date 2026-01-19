f7e85fa3fab83e567ed0256cb91b1eaad91941ab
AbdulTheActivePiecer <abdulyki@activepieces.com>
2026-01-19 11:31:35 +0300

    fix: skeleton loading colors

--- packages/react-ui/src/components/ui/skeleton.tsx
+++ packages/react-ui/src/components/ui/skeleton.tsx
@@ -6,7 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn('animate-pulse rounded-md bg-muted', className)}
+      className={cn('animate-pulse rounded-md bg-gray-200 dark:bg-neutral-700', className)}
       {...props}
     />
   );
