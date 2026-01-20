fea2cf68d08a2f6d76d60a93ddb278fbfb1b45cc
Kishan Parmar <code.kishanparmar@gmail.com>
2026-01-20 14:07:24 +0530

    fix(bamboohr): fix test method for trigger

--- packages/pieces/community/bamboohr/src/index.ts
+++ packages/pieces/community/bamboohr/src/index.ts
@@ -1,49 +1,27 @@
-import {
-  createPiece,
-  PieceAuth,
-  Property,
-} from '@activepieces/pieces-framework';
+import { createPiece } from '@activepieces/pieces-framework';
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
 import { reportFieldChanged } from './lib/triggers/report-field-changed';
-
-export const bambooHrAuth = PieceAuth.CustomAuth({
-  required: true,
-  description:
-    'Follow [these instructions](https://documentation.bamboohr.com/docs/getting-started#authentication) to get your API key',
-  props: {
-    companyDomain: Property.ShortText({
-      displayName: 'Company domain',
-      description:
-        'The subdomain used to access BambooHR. If you access BambooHR at https://mycompany.bamboohr.com, then the companyDomain is "mycompany"',
-      required: true,
-    }),
-    apiKey: PieceAuth.SecretText({
-      displayName: 'API key',
-      required: true,
-    }),
-  },
-});
+import { bambooHrAuth } from './lib/common/auth';
+import { reportFieldChangedCopy } from './lib/triggers/report-field-changed copy';
 
 export const bambooHr = createPiece({
-  displayName: 'BambooHR',
-  auth: bambooHrAuth,
-  minimumSupportedRelease: '0.36.1',
-  logoUrl: 'https://cdn.activepieces.com/pieces/bamboohr.png',
-  authors: ['AdamSelene'],
-  actions: [
-    createCustomApiCallAction({
-      baseUrl: (auth) =>
-        `https://api.bamboohr.com/api/gateway.php/${auth?.props?.companyDomain}/v1/`,
-      auth: bambooHrAuth,
-      authMapping: async (auth) => {
-        const { apiKey } = auth.props;
-        return {
-          Authorization: `Basic ${Buffer.from(`${apiKey}:`).toString(
-            'base64'
-          )}`,
-        };
-      },
-    }),
-  ],
-  triggers: [reportFieldChanged],
+	displayName: 'BambooHR',
+	auth: bambooHrAuth,
+	minimumSupportedRelease: '0.36.1',
+	logoUrl: 'https://cdn.activepieces.com/pieces/bamboohr.png',
+	authors: ['AdamSelene'],
+	actions: [
+		createCustomApiCallAction({
+			baseUrl: (auth) =>
+				`https://api.bamboohr.com/api/gateway.php/${auth?.props?.companyDomain}/v1/`,
+			auth: bambooHrAuth,
+			authMapping: async (auth) => {
+				const { apiKey } = auth.props;
+				return {
+					Authorization: `Basic ${Buffer.from(`${apiKey}:`).toString('base64')}`,
+				};
+			},
+		}),
+	],
+	triggers: [reportFieldChanged,reportFieldChangedCopy],
 });
