058741d7812563a1b5ca699c826deac4b20a4310
David Anyatonwu <davidanyatonwu@gmail.com>
2026-01-22 10:19:16 +0000

    feat(gmail): remove actions requiring gmail.modify scope

--- packages/pieces/community/gmail/src/index.ts
+++ packages/pieces/community/gmail/src/index.ts
@@ -8,12 +8,7 @@ import { PieceCategory } from '@activepieces/shared';
 import { gmailSendEmailAction } from './lib/actions/send-email-action';
 import { gmailReplyToEmailAction } from './lib/actions/reply-to-email-action';
 import { gmailCreateDraftReplyAction } from './lib/actions/create-draft-reply-action';
-import { gmailAddLabelToEmailAction } from './lib/actions/add-label-to-email-action';
-import { gmailRemoveLabelFromEmailAction } from './lib/actions/remove-label-from-email-action';
 import { gmailCreateLabelAction } from './lib/actions/create-label-action';
-import { gmailArchiveEmailAction } from './lib/actions/archive-email-action';
-import { gmailDeleteEmailAction } from './lib/actions/delete-email-action';
-import { gmailRemoveLabelFromThreadAction } from './lib/actions/remove-label-from-thread-action';
 import { gmailNewEmailTrigger } from './lib/triggers/new-email';
 import { gmailNewLabeledEmailTrigger } from './lib/triggers/new-labeled-email';
 import { gmailNewAttachmentTrigger } from './lib/triggers/new-attachment';
@@ -31,7 +26,6 @@ export const gmailAuth = PieceAuth.OAuth2({
     'email',
     'https://www.googleapis.com/auth/gmail.readonly',
     'https://www.googleapis.com/auth/gmail.compose',
-    'https://www.googleapis.com/auth/gmail.modify',
     'https://www.googleapis.com/auth/gmail.labels',
   ],
 });
@@ -47,13 +41,8 @@ export const gmail = createPiece({
     gmailSendEmailAction,
     gmailReplyToEmailAction,
     gmailCreateDraftReplyAction,
-    gmailAddLabelToEmailAction,
-    gmailRemoveLabelFromEmailAction,
     gmailCreateLabelAction,
-    gmailArchiveEmailAction,
     gmailGetEmailAction,
-    gmailDeleteEmailAction,
-    gmailRemoveLabelFromThreadAction,
     gmailSearchMailAction,
     createCustomApiCallAction({
       baseUrl: () => 'https://gmail.googleapis.com/gmail/v1',
