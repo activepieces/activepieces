e291e9562a4f392c6b158252e2179790ef013a80
Hazem Adel <hazemadelkhalel@gmail.com>
2026-01-25 16:47:12 +0000

    fix: improve many email invitation

--- packages/react-ui/src/components/ui/tag-input.tsx
+++ packages/react-ui/src/components/ui/tag-input.tsx
@@ -8,6 +8,7 @@ import { cn } from '@/lib/utils';
 import { Badge } from './badge';
 import { Button } from './button';
 import type { InputProps } from './input';
+import { ScrollArea } from './scroll-area';
 
 type TagInputProps = Omit<InputProps, 'value' | 'onChange'> & {
   value?: ReadonlyArray<string>;
@@ -62,59 +63,63 @@ const TagInput = forwardRef<HTMLInputElement, TagInputProps>((props, ref) => {
     <div
       className={cn(
         // caveat: :has() variant requires tailwind v3.4 or above: https://tailwindcss.com/blog/tailwindcss-v3-4#new-has-variant
-        'has-focus-visible:ring-neutral-950 dark:has-focus-visible:ring-neutral-300 border-neutral-200 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 flex min-h-10 w-full flex-wrap gap-2 rounded-md border bg-white px-3 py-2 text-sm ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 has-focus-visible:outline-hidden has-focus-visible:ring-2 has-focus-visible:ring-offset-2',
+        'has-focus-visible:ring-neutral-950 dark:has-focus-visible:ring-neutral-300 border-neutral-200 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 flex min-h-10 w-full rounded-md border bg-white ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 has-focus-visible:outline-hidden has-focus-visible:ring-2 has-focus-visible:ring-offset-2',
         className,
       )}
     >
-      {value.map((item) => {
-        const isValid = validateItem ? validateItem(item) : true;
-        return (
-          <Badge
-            key={item}
-            variant={'accent'}
-            className={cn(
-              'font-medium',
-              badgeClassName,
-              !isValid && invalidBadgeClassName,
-            )}
-          >
-            <span className="text-xs">{item}</span>
-            <Button
-              variant={'ghost'}
-              size={'icon'}
-              className={'ml-2 h-3 w-3'}
-              onClick={() => {
-                onChange(value.filter((i) => i !== item));
-              }}
-            >
-              <XIcon className={'w-3'} />
-            </Button>
-          </Badge>
-        );
-      })}
-      <input
-        className={
-          'placeholder:text-neutral-500 dark:placeholder:text-neutral-400 w-full min-w-0 flex-1 outline-hidden'
-        }
-        value={pendingDataPoint}
-        onChange={(e) => setPendingDataPoint(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter' || e.key === SEPARATOR) {
-            e.preventDefault();
-            addPendingDataPoint();
-          } else if (
-            e.key === 'Backspace' &&
-            pendingDataPoint.length === 0 &&
-            value.length > 0
-          ) {
-            e.preventDefault();
-            onChange(value.slice(0, -1));
-          }
-        }}
-        {...domProps}
-        placeholder={value.length > 0 ? '' : domProps.placeholder}
-        ref={ref}
-      />
+      <ScrollArea className="w-full max-h-32" viewPortClassName="px-3 py-2">
+        <div className="flex flex-wrap gap-2 text-sm">
+          {value.map((item) => {
+            const isValid = validateItem ? validateItem(item) : true;
+            return (
+              <Badge
+                key={item}
+                variant={'accent'}
+                className={cn(
+                  'font-medium',
+                  badgeClassName,
+                  !isValid && invalidBadgeClassName,
+                )}
+              >
+                <span className="text-xs">{item}</span>
+                <Button
+                  variant={'ghost'}
+                  size={'icon'}
+                  className={'ml-2 h-3 w-3'}
+                  onClick={() => {
+                    onChange(value.filter((i) => i !== item));
+                  }}
+                >
+                  <XIcon className={'w-3'} />
+                </Button>
+              </Badge>
+            );
+          })}
+          <input
+            className={
+              'placeholder:text-neutral-500 dark:placeholder:text-neutral-400 w-full min-w-0 flex-1 outline-hidden bg-transparent'
+            }
+            value={pendingDataPoint}
+            onChange={(e) => setPendingDataPoint(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' || e.key === SEPARATOR) {
+                e.preventDefault();
+                addPendingDataPoint();
+              } else if (
+                e.key === 'Backspace' &&
+                pendingDataPoint.length === 0 &&
+                value.length > 0
+              ) {
+                e.preventDefault();
+                onChange(value.slice(0, -1));
+              }
+            }}
+            {...domProps}
+            placeholder={value.length > 0 ? '' : domProps.placeholder}
+            ref={ref}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 });
