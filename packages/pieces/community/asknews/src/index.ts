bb5488f9788df5e16be7c854fae0610dd4d97ff7
sanket@activepieces.com <sanket@activepieces.com>
2026-01-30 14:41:16 +0530

    feat(asknews): news action added

---
+++ packages/pieces/community/asknews/src/index.ts
@@ -0,0 +1,39 @@
+import { createPiece } from '@activepieces/pieces-framework';
+import { asknewsAuth } from './lib/common/auth';
+import { searchNews } from './lib/actions/search-news';
+import { generateNewsKnowledgeGraph } from './lib/actions/generate-news-knowledge-graph';
+import { getArticleById } from './lib/actions/get-article-by-id';
+import { asknewsChatCompletion } from './lib/actions/asknews-chat-completion';
+import { searchStories } from './lib/actions/search-stories';
+import { PieceCategory } from '@activepieces/shared';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { createANewsletter } from './lib/actions/create-a-newsletter';
+
+export const asknews = createPiece({
+  displayName: 'AskNews',
+  auth: asknewsAuth,
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: 'https://cdn.activepieces.com/pieces/asknews.png',
+  authors: ['sanket-a11y'],
+  categories: [PieceCategory.COMMERCE],
+  description:
+    'AskNews piece allows you to search and retrieve enriched real-time and historical news articles using AskNews API.',
+  actions: [
+    searchNews,
+    generateNewsKnowledgeGraph,
+    getArticleById,
+    asknewsChatCompletion,
+    searchStories,
+    createANewsletter,
+    createCustomApiCallAction({
+      auth: asknewsAuth,
+      baseUrl: () => 'https://api.asknews.com/v1',
+      authMapping: async (auth) => {
+        return {
+          Authorization: `Bearer ${auth.secret_text}`,
+        };
+      },
+    }),
+  ],
+  triggers: [],
+});
