f9c0436955b6c19bbb95bb0b7912843341ef225c
Mohammad AbuAboud <moabuaboud@gmail.com>
2026-02-04 12:58:11 +0300

    feat: revert settting dialog

--- packages/react-ui/src/app/builder/step-settings/index.tsx
+++ packages/react-ui/src/app/builder/step-settings/index.tsx
@@ -15,7 +15,7 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { pieceSelectorUtils } from '@/features/pieces/lib/piece-selector-utils';
 import { stepsHooks } from '@/features/pieces/lib/steps-hooks';
 import { projectCollectionUtils } from '@/hooks/project-collection';
-import { cn, GAP_SIZE_FOR_STEP_SETTINGS, parentWindow } from '@/lib/utils';
+import { cn, GAP_SIZE_FOR_STEP_SETTINGS } from '@/lib/utils';
 import {
   FlowAction,
   FlowActionType,
@@ -24,7 +24,6 @@ import {
   FlowTriggerType,
   isNil,
 } from '@activepieces/shared';
-import { ActivepiecesClientEventName } from 'ee-embed-sdk';
 
 import { formUtils } from '../../../features/pieces/lib/form-utils';
 import { ActionErrorHandlingForm } from '../piece-properties/action-error-handling';
@@ -43,9 +42,8 @@ import { RouterSettings } from './router-settings';
 import { useStepSettingsContext } from './step-settings-context';
 
 const StepSettingsContainer = () => {
-  const location = useLocation();
-  const { selectedStep, pieceModel, formSchema, hideTestStep } =
-    useStepSettingsContext();
+
+  const { selectedStep, pieceModel, formSchema } = useStepSettingsContext();
   const { project } = projectCollectionUtils.useCurrentProject();
   const [
     readonly,
@@ -131,9 +129,8 @@ const StepSettingsContainer = () => {
       pieceName: modifiedStep.settings.pieceName,
       triggerName: modifiedStep.settings.triggerName ?? '',
     });
-  const showGenerateSampleData = !readonly && !isManualTrigger && !hideTestStep;
-  const showStepInputOutFromRun =
-    !isNil(run) && !isManualTrigger && !hideTestStep;
+  const showGenerateSampleData = !readonly && !isManualTrigger;
+  const showStepInputOutFromRun = !isNil(run) && !isManualTrigger;
 
   const [isEditingStepOrBranchName, setIsEditingStepOrBranchName] =
     useState(false);
@@ -153,19 +150,6 @@ const StepSettingsContainer = () => {
 
   const { height, setHeight } = useResizableVerticalPanelsContext();
 
-  const handleClose = () => {
-    if (location.pathname.includes('/embed/step-settings')) {
-      parentWindow.postMessage(
-        {
-          type: ActivepiecesClientEventName.CLIENT_STEP_SETTINGS_DIALOG_CLOSED,
-          data: {},
-        },
-        '*',
-      );
-    }
-    exitStepSettings();
-  };
-
   return (
     <Form {...form}>
       <form
@@ -174,7 +158,7 @@ const StepSettingsContainer = () => {
         className="w-full h-full"
       >
         <div ref={sidebarHeaderContainerRef}>
-          <SidebarHeader onClose={handleClose}>
+          <SidebarHeader onClose={() => exitStepSettings()}>
             <EditableStepName
               selectedBranchIndex={selectedBranchIndex}
               setDisplayName={(value) => {
