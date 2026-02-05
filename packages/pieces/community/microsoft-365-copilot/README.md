7cf5ccfc0638f8b7cb27fe69419ff55faf4ffc61
sanket@activepieces.com <sanket@activepieces.com>
2026-02-05 15:49:58 +0530

    chore: change piece name

--- packages/pieces/community/microsoft-365-copilot/README.md
+++
@@ -1,7 +0,0 @@
-# pieces-microsoft-365-copilot
-
-This library was generated with [Nx](https://nx.dev).
-
-## Building
-
-Run `nx build pieces-microsoft-365-copilot` to build the library.
--- packages/pieces/community/microsoft-copilot/src/index.ts
+++ packages/pieces/community/microsoft-copilot/src/index.ts
@@ -1,14 +1,18 @@
-import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import {
+  createPiece,
+  OAuth2PropertyValue,
+} from '@activepieces/pieces-framework';
 import { microsoft365CopilotAuth } from './lib/common/auth';
 import { chatWithCopilot } from './lib/actions/chat-with-copilot';
 import { retrieveGroundingData } from './lib/actions/retrieve-grounding-data';
 import { copilotInteractionWebhook } from './lib/triggers/copilot-interaction-webhook';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
 
 export const microsoft365Copilot = createPiece({
   displayName: 'Microsoft 365 Copilot',
   auth: microsoft365CopilotAuth,
   minimumSupportedRelease: '0.36.1',
-  logoUrl: 'https://cdn.activepieces.com/pieces/microsoft-365-copilot.png',
+  logoUrl: 'https://cdn.activepieces.com/pieces/microsoft-copilot.png',
   authors: ['sanket-a11y'],
   actions: [
     chatWithCopilot,
