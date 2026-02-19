e30b5af7e912758c86ef6de16239629f54208508
Alena Hasslacher <63125577+AlenioH@users.noreply.github.com>
2026-02-19 01:14:04 +0000

    feat(logsnag): add logsnag piece + createEvent action

---
+++ packages/pieces/custom/logsnag/src/index.ts
@@ -0,0 +1,19 @@
+
+    import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+    import { createEvent } from './lib/actions/create-event';
+
+    export const logsnagAuth = PieceAuth.SecretText({
+      displayName: 'API Key',
+      required: true,
+      description: 'Please use **test-key** as value for API Key',
+    });
+
+    export const logsnag = createPiece({
+      displayName: "Logsnag",
+      auth: logsnagAuth,
+      minimumSupportedRelease: '0.36.1',
+      logoUrl: "https://cdn.activepieces.com/pieces/logsnag.png",
+      authors: [],
+      actions: [createEvent],
+      triggers: [],
+    });
