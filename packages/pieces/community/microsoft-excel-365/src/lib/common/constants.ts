5458467113bc0d781acb20dcae77414646a7b9d6
Kishan Parmar <135701940+kishanprmr@users.noreply.github.com>
2026-02-19 13:40:55 +0530

    feat(microsoft-excel): text in list/not in list filter for get rows action (#11306)

--- packages/pieces/community/microsoft-excel-365/src/lib/common/constants.ts
+++ packages/pieces/community/microsoft-excel-365/src/lib/common/constants.ts
@@ -3,6 +3,8 @@ export enum FilterOperator {
   TEXT_DOES_NOT_CONTAIN = 'TEXT_DOES_NOT_CONTAIN',
   TEXT_EXACTLY_MATCHES = 'TEXT_EXACTLY_MATCHES',
   TEXT_DOES_NOT_EXACTLY_MATCH = 'TEXT_DOES_NOT_EXACTLY_MATCH',
+  TEXT_IN_LIST = 'TEXT_IN_LIST',
+  TEXT_NOT_IN_LIST = 'TEXT_NOT_IN_LIST',
   NUMBER_IS_GREATER_THAN = 'NUMBER_IS_GREATER_THAN',
   NUMBER_IS_LESS_THAN = 'NUMBER_IS_LESS_THAN',
   NUMBER_IS_EQUAL_TO = 'NUMBER_IS_EQUAL_TO',
@@ -13,6 +15,7 @@ export enum FilterOperator {
   DATE_IS_ON_OR_AFTER = 'DATE_IS_ON_OR_AFTER',
   EXISTS = 'EXISTS',
   DOES_NOT_EXIST = 'DOES_NOT_EXIST',
+
 }
 
 export const filterOperatorLabels: Record<FilterOperator, string> = {
@@ -20,6 +23,8 @@ export const filterOperatorLabels: Record<FilterOperator, string> = {
   [FilterOperator.TEXT_DOES_NOT_CONTAIN]: '(Text) Does not contain',
   [FilterOperator.TEXT_EXACTLY_MATCHES]: '(Text) Exactly matches',
   [FilterOperator.TEXT_DOES_NOT_EXACTLY_MATCH]: '(Text) Does not exactly match',
+  [FilterOperator.TEXT_IN_LIST]: '(Text) In List',
+  [FilterOperator.TEXT_NOT_IN_LIST]: '(Text) Not in List',
 
   [FilterOperator.NUMBER_IS_GREATER_THAN]: '(Number) Is greater than',
   [FilterOperator.NUMBER_IS_LESS_THAN]: '(Number) Is less than',
@@ -33,4 +38,5 @@ export const filterOperatorLabels: Record<FilterOperator, string> = {
 
   [FilterOperator.EXISTS]: 'Exists',
   [FilterOperator.DOES_NOT_EXIST]: 'Does not exist',
+
 };
