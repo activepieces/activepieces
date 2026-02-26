bc326f4603e4eb1a66a711ef5de5515f24657430
Kishan Parmar <code.kishanparmar@gmail.com>
2026-02-26 10:54:20 +0530

    feat(logsnag): add custom api call action

--- packages/pieces/community/logsnag/src/index.ts
+++ packages/pieces/community/logsnag/src/index.ts
@@ -1,6 +1,7 @@
 import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 import { createEvent } from './lib/actions/create-event';
 import { newEventCreated } from './lib/triggers/new-event-created';
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
 
 export const logsnagAuth = PieceAuth.SecretText({
   displayName: 'API Key',
@@ -9,11 +10,22 @@ export const logsnagAuth = PieceAuth.SecretText({
 });
 
 export const logsnag = createPiece({
-  displayName: 'Logsnag',
+  displayName: 'LogSnag',
   auth: logsnagAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/logsnag.png',
   authors: [],
-  actions: [createEvent],
+  actions: [
+    createEvent,
+    createCustomApiCallAction({
+      auth: logsnagAuth,
+      baseUrl: () => 'https://api.logsnag.com/v1',
+      authMapping: async (auth) => {
+        return {
+          Authorization: `Bearer ${auth.secret_text}`,
+        };
+      },
+    }),
+  ],
   triggers: [newEventCreated],
 });
