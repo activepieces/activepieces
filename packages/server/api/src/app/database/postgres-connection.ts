a6354574def0fd81da7d0a5ec073e6c07f2aaaaf
Amr Elmohamady <amr.osama.elmohamady@gmail.com>
2026-02-26 17:33:17 +0000

    fix(database): remove AddTodoRunIdIndex migration and update related imports

--- packages/server/api/src/app/database/postgres-connection.ts
+++ packages/server/api/src/app/database/postgres-connection.ts
@@ -334,7 +334,6 @@ import { RemoveUsageCountFromTemplates1768738475196 } from './migration/postgres
 import { AddTemplateIdToFlowEntity1768829135202 } from './migration/postgres/1768829135202-AddTemplateIdToFlowEntity'
 import { AddEventStreaming1769084311004 } from './migration/postgres/1769084311004-AddEventStreaming'
 import { RemoveOperatorRole1769613456917 } from './migration/postgres/1769613456917-RemoveOperatorRole'
-import { AddTodoRunIdIndex1772111378756 } from './migration/postgres/1772111378756-AddTodoRunIdIndex'
 
 const getSslConfig = (): boolean | TlsOptions => {
     const useSsl = system.get(AppSystemProp.POSTGRES_USE_SSL)
@@ -685,7 +684,6 @@ export const getMigrations = (): (new () => MigrationInterface)[] => {
         AddTablesIntoTemplateEntity1768306510367,
         AddTemplateIdToFlowEntity1768829135202,
         RemoveOperatorRole1769613456917,
-        AddTodoRunIdIndex1772111378756,
     ]
     return migrations
 }
