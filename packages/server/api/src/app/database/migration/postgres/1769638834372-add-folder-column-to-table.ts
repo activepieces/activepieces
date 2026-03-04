6ac79a3eea8308db700dde17b67669425930d771
louai <louai@activepieces.com>
2026-03-04 17:11:18 +0100

    chore: remove unwanted index on table cell entity

--- packages/server/api/src/app/database/migration/postgres/1769638834372-add-folder-column-to-table.ts
+++ packages/server/api/src/app/database/migration/postgres/1769638834372-add-folder-column-to-table.ts
@@ -8,15 +8,9 @@ export class AddFolderColumnToTable1769638834372 implements MigrationInterface {
             ALTER TABLE "table"
             ADD "folderId" character varying(21)
         `)
-        await queryRunner.query(`
-            CREATE INDEX "idx_cell_record_id" ON "cell" ("recordId")
-        `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            DROP INDEX "public"."idx_cell_record_id"
-        `)
         await queryRunner.query(`
             ALTER TABLE "table" DROP COLUMN "folderId"
         `)
