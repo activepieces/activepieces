2efe4bb43b8f299a99ecc20cad1c75b8172f738d
AbdulTheActivePiecer <abdulyki@activepieces.com>
2026-03-08 14:07:35 +0300

    fix: remove card design inside dashboard

--- packages/web/src/app/components/project-layout/index.tsx
+++ packages/web/src/app/components/project-layout/index.tsx
@@ -85,8 +85,8 @@ export function ProjectDashboardLayout({
       <SidebarProvider hoverMode={true}>
         {!isEmbedded && <ProjectDashboardSidebar />}
         <SidebarInset className="flex flex-col h-full overflow-hidden bg-sidebar">
-          <div className="flex-1 flex flex-col p-2 pt-3 pb-3 overflow-hidden">
-            <div className="flex flex-col h-full bg-background rounded-xl shadow-[2px_0px_4px_-2px_rgba(0,0,0,0.05),0px_2px_4px_-2px_rgba(0,0,0,0.05)] border">
+          <div className="flex-1 flex flex-col  overflow-hidden">
+            <div className="flex flex-col h-full bg-background border-l">
               {!hideHeader && (
                 <>
                   <ProjectDashboardLayoutHeader key={currentProjectId} />
