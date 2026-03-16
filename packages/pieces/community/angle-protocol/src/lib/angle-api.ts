6f0cb48221be9f45825a03f2a72deac2cdfb978e
bossco7598 <bossco7598@gmail.com>
2026-03-16 15:25:01 -0400

    feat(piece-angle-protocol): add src/lib/angle-api.ts

---
+++ packages/pieces/community/angle-protocol/src/lib/angle-api.ts
@@ -0,0 +1,3 @@
+export const DEFILLAMA_ANGLE_URL = 'https://api.llama.fi/protocol/angle';
+export const COINGECKO_ANGLE_URL =
+  'https://api.coingecko.com/api/v3/coins/angle-protocol?localization=false&tickers=false&community_data=false&developer_data=false';
