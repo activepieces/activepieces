d85909fa4c9286976ee267d893db2be664dfeb33
bossco7598 <bossco7598@gmail.com>
2026-03-16 06:29:40 -0400

    feat: add Glassnode piece

---
+++ packages/pieces/community/glassnode/src/lib/actions/get-exchange-supply.ts
@@ -0,0 +1,37 @@
+import { createAction } from '@activepieces/pieces-framework';
+import { glassnodeAuth } from '../../index';
+import { fetchGlassnodeMetric } from '../common/glassnode-api';
+import {
+  assetProperty,
+  intervalProperty,
+  sinceProperty,
+  untilProperty,
+} from '../common/params';
+
+export const getExchangeSupplyAction = createAction({
+  name: 'get_exchange_supply',
+  displayName: 'Get Exchange Net Position Change',
+  description:
+    'Retrieve the net change of supply held on exchanges, indicating accumulation or distribution pressure.',
+  auth: glassnodeAuth,
+  props: {
+    asset: assetProperty,
+    interval: intervalProperty,
+    since: sinceProperty,
+    until: untilProperty,
+  },
+  async run(context) {
+    const { asset, interval, since, until } = context.propsValue;
+    const apiKey = context.auth as string;
+    return fetchGlassnodeMetric(
+      apiKey,
+      'distribution/exchange_net_position_change',
+      {
+        asset,
+        interval,
+        since: since ?? undefined,
+        until: until ?? undefined,
+      }
+    );
+  },
+});
