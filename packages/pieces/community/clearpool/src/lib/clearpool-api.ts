6cd2fff3dcf51bfb49b3e4f787211d921b2e7869
bossco7598 <bossco7598@gmail.com>
2026-03-16 15:25:34 -0400

    feat(piece-clearpool): add clearpool-api.ts

---
+++ packages/pieces/community/clearpool/src/lib/clearpool-api.ts
@@ -0,0 +1,3 @@
+export const DEFILLAMA_CLEARPOOL_URL = 'https://api.llama.fi/protocol/clearpool';
+export const COINGECKO_CLEARPOOL_URL =
+  'https://api.coingecko.com/api/v3/coins/clearpool?localization=false&tickers=false&community_data=false&developer_data=false';
