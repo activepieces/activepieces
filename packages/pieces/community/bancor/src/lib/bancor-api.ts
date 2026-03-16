f06499fb89455a6e0f0a977091135d4c817ba1ca
bossco7598 <bossco7598@gmail.com>
2026-03-16 15:24:55 -0400

    feat(piece-bancor): add packages/pieces/community/bancor/src/lib/bancor-api.ts

---
+++ packages/pieces/community/bancor/src/lib/bancor-api.ts
@@ -0,0 +1,3 @@
+export const DEFILLAMA_BANCOR_URL = 'https://api.llama.fi/protocol/bancor';
+export const COINGECKO_BNT_URL =
+  'https://api.coingecko.com/api/v3/coins/bancor?localization=false&tickers=false&community_data=false&developer_data=false';
