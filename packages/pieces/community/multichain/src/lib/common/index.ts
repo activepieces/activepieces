6768e7ad0a3b9179bcfe577fecfcde9def1fe4b0
bossco7598 <bossco7598@gmail.com>
2026-03-16 17:07:53 -0400

    feat(piece-multichain): add index.ts

---
+++ packages/pieces/community/multichain/src/lib/common/index.ts
@@ -0,0 +1,4 @@
+export const DEFILLAMA_API_BASE_URL = 'https://api.llama.fi';
+export const COINGECKO_API_BASE_URL = 'https://api.coingecko.com/api/v3';
+export const MULTICHAIN_DEFILLAMA_SLUG = 'multichain';
+export const MULTI_COINGECKO_ID = 'multichain';
