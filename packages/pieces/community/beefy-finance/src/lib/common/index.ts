0369c081a339b98893fc98e369a2ab1972a1d198
bossco7598 <bossco7598@gmail.com>
2026-03-16 16:23:38 -0400

    feat(piece-beefy-finance): add index.ts

---
+++ packages/pieces/community/beefy-finance/src/lib/common/index.ts
@@ -0,0 +1,4 @@
+export const DEFILLAMA_API_BASE_URL = 'https://api.llama.fi';
+export const COINGECKO_API_BASE_URL = 'https://api.coingecko.com/api/v3';
+export const BEEFY_DEFILLAMA_SLUG = 'beefy';
+export const BIFI_COINGECKO_ID = 'beefy-finance';
