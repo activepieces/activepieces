ddb885800a93b511815d4c7f7f18777c5af03b87
bossco7598 <bossco7598@gmail.com>
2026-03-16 18:07:34 -0400

    feat(piece-manta): add actions index

---
+++ packages/pieces/community/manta/src/lib/actions/index.ts
@@ -0,0 +1,5 @@
+export { getProtocolTvl } from './get-protocol-tvl';
+export { getMantaPrice } from './get-manta-price';
+export { getChainBreakdown } from './get-chain-breakdown';
+export { getTvlHistory } from './get-tvl-history';
+export { getProtocolStats } from './get-protocol-stats';
