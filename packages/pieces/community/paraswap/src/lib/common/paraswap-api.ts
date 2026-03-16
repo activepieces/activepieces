b05510b038776aeb87d6144fa8a6865b97b6ff36
bossco7598 <bossco7598@gmail.com>
2026-03-16 19:41:05 -0400

    feat: add paraswap piece

---
+++ packages/pieces/community/paraswap/src/lib/common/paraswap-api.ts
@@ -0,0 +1,15 @@
+import { HttpMethod } from '@activepieces/pieces-common';
+
+export const PARASWAP_API_BASE = 'https://apiv5.paraswap.io';
+export const DEFILLAMA_API_BASE = 'https://api.llama.fi';
+export const COINGECKO_API_BASE = 'https://api.coingecko.com/api/v3';
+
+export const NETWORK_OPTIONS = [
+  { label: 'Ethereum', value: '1' },
+  { label: 'Polygon', value: '137' },
+  { label: 'BNB Chain', value: '56' },
+  { label: 'Avalanche', value: '43114' },
+  { label: 'Fantom', value: '250' },
+  { label: 'Arbitrum', value: '42161' },
+  { label: 'Optimism', value: '10' },
+];
