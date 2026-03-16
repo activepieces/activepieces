792a5f54ffbbe2400261dffc0095656418345e2b
bossco7598 <bossco7598@gmail.com>
2026-03-16 15:25:13 -0400

    feat(piece-truefi): add packages/pieces/community/truefi/src/lib/truefi-api.ts

---
+++ packages/pieces/community/truefi/src/lib/truefi-api.ts
@@ -0,0 +1,3 @@
+export const TRUEFI_LLAMA_URL = 'https://api.llama.fi/protocol/truefi';
+export const TRUEFI_COINGECKO_URL =
+  'https://api.coingecko.com/api/v3/coins/truefi?localization=false&tickers=false&community_data=false&developer_data=false';
