dfa7f71446b2720c491eb21d7a22fad3c89c684b
bossco7598 <bossco7598@gmail.com>
2026-03-16 21:36:52 -0400

    feat(piece-multiversx): add get-protocol-tvl.ts

---
+++ packages/pieces/community/multiversx/src/lib/actions/get-protocol-tvl.ts
@@ -0,0 +1,29 @@
+import { createAction } from '@activepieces/pieces-framework';
+import { httpClient, HttpMethod } from '@activepieces/pieces-common';
+
+export const getProtocolTvl = createAction({
+  name: 'get_protocol_tvl',
+  displayName: 'Get Protocol TVL',
+  description:
+    'Fetch the current Total Value Locked (TVL) for MultiversX from DeFiLlama.',
+  props: {},
+  async run() {
+    const response = await httpClient.sendRequest({
+      method: HttpMethod.GET,
+      url: 'https://api.llama.fi/protocol/multiversx',
+    });
+
+    const data = response.body as Record<string, unknown>;
+    return {
+      name: data['name'],
+      symbol: data['symbol'],
+      tvl: data['tvl'],
+      currentChainTvls: data['currentChainTvls'],
+      category: data['category'],
+      chains: data['chains'],
+      description: data['description'],
+      url: data['url'],
+      twitter: data['twitter'],
+    };
+  },
+});
