4042e5549100603e70f96189eefb854d4c5bacaf
Hazem Adel <hazemadelkhalel@gmail.com>
2026-03-19 14:41:04 +0200

    fix(worker): propagate AP_DEV_PIECES to engine sandbox and skip npm install (#12050)

--- packages/server/worker/src/lib/execute/create-sandbox-for-job.ts
+++ packages/server/worker/src/lib/execute/create-sandbox-for-job.ts
@@ -71,6 +71,9 @@ function buildSandboxEnv(settings: ReturnType<typeof workerSettings.getSettings>
         AP_MAX_FILE_SIZE_MB: String(settings.MAX_FILE_SIZE_MB),
         NODE_PATH: '/usr/src/node_modules',
     }
+    if (settings.DEV_PIECES.length > 0) {
+        env['AP_DEV_PIECES'] = settings.DEV_PIECES.join(',')
+    }
     for (const key of settings.SANDBOX_PROPAGATED_ENV_VARS) {
         if (process.env[key]) {
             env[key] = process.env[key]!
