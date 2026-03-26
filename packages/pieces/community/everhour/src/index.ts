da13402d107ab5b68fc4215733b3896d15a24179
Slim-Hady <mohamed.elsogher.2024@aiu.edu.eg>
2026-03-26 05:28:35 +0200

    feat : Adding the index for everhour

---
+++ packages/pieces/community/everhour/src/index.ts
@@ -0,0 +1,15 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { everhourAuth } from './lib/auth';
+import { createTaskAction } from './lib/actions/create-task';
+import { startTimerAction } from './lib/actions/start-timer';
+import { stopTimerAction } from './lib/actions/stop-timer';
+
+export const everhour = createPiece({
+    displayName: 'Everhour',
+    auth: everhourAuth,
+    minimumSupportedRelease: '0.20.0',
+    logoUrl: 'https://cdn.activepieces.com/pieces/everhour.png',
+    authors: ['Slim-Hady'],
+    actions: [createTaskAction, startTimerAction, stopTimerAction],
+    triggers: [],
+});
