047047bb6d561dbeb045c732a286f06ecac674c1
zkk536 <106555838+zkk536@users.noreply.github.com>
2026-03-29 22:27:56 +0800

    fix(clockify): fix all P1 issues for 5/5
    - Delete orphaned trigger files (new-task.ts, new-timer-started.ts)
    - Fix tsconfig.lib.json extends path
    - Add actions/triggers index.ts barrel files

--- packages/pieces/community/clockify/src/lib/actions/index.ts
+++ packages/pieces/community/clockify/src/lib/actions/index.ts
@@ -1,1 +1,7 @@
-export { createTimeEntry, startTimeEntry, stopTimer, findTask, getTimeEntries } from './actions';
+export { createTimeEntry } from './create-time-entry';
+export { startTimerAction as startTimeEntry } from './start-timer';
+export { stopTimerAction as stopTimer } from './stop-timer';
+export { findTaskAction as findTask } from './find-task';
+export { findTimeEntryAction as getTimeEntries } from './find-time-entry';
+export { createTaskAction as createTask } from './create-task';
+export { findRunningTimerAction as findRunningTimer } from './find-running-timer';
