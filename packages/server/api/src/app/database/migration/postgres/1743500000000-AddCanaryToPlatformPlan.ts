d010d9ce69f5e038d79773ad7640d3efbe7ca81c
MrChaker <mr.chakerw@gmail.com>
2026-04-02 11:58:00 +0100

    chore: use new migration type for canary migration

--- packages/server/api/src/app/database/migration/postgres/1743500000000-AddCanaryToPlatformPlan.ts
+++ packages/server/api/src/app/database/migration/postgres/1743500000000-AddCanaryToPlatformPlan.ts
@@ -1,7 +1,10 @@
-import { MigrationInterface, QueryRunner } from 'typeorm'
+import { QueryRunner } from 'typeorm'
+import { Migration } from '../../migration'
 
-export class AddCanaryToPlatformPlan1743500000000 implements MigrationInterface {
+export class AddCanaryToPlatformPlan1743500000000 implements Migration {
     name = 'AddCanaryToPlatformPlan1743500000000'
+    breaking = false
+    release = '0.80.2'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query('ALTER TABLE "platform_plan" ADD "canary" boolean NOT NULL DEFAULT false')
