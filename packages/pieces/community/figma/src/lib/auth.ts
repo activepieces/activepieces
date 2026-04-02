7ecc906150b6ba1a0e047c38edf2b2df86ea8228
sanket@activepieces.com <sanket@activepieces.com>
2026-04-02 17:30:10 +0530

    feat(figma): add new scopes

--- packages/pieces/community/figma/src/lib/auth.ts
+++ packages/pieces/community/figma/src/lib/auth.ts
@@ -5,5 +5,5 @@ export const figmaAuth = PieceAuth.OAuth2({
   authUrl: 'https://www.figma.com/oauth',
   tokenUrl: 'https://www.figma.com/api/oauth/token',
   required: true,
-  scope: ['file_read'],
+  scope: ['file_content:read', 'file_metadata:read', 'file_comments:read', 'file_comments:write'],
 });
