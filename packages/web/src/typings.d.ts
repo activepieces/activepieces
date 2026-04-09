73d0dc3accda32c482664bcd70ec8adcf94489a0
Mohammad AbuAboud <moabuaboud@gmail.com>
2026-04-09 18:22:34 +0200

    fix(web): extend QueryCollectionMeta to include showErrorDialog
    The @tanstack/query-db-collection package globally overrides React
    Query's meta type to require loadSubsetOptions. Extend the interface
    to also accept our showErrorDialog flag.

--- packages/web/src/typings.d.ts
+++ packages/web/src/typings.d.ts
@@ -1,3 +1,9 @@
+declare module '@tanstack/query-db-collection' {
+  interface QueryCollectionMeta {
+    showErrorDialog?: boolean;
+  }
+}
+
 declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
