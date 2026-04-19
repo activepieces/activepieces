38523f37a3476791e9dcbdf1d09dd7ffb9a41252
Tosh94 <tosh.leonardlre@gmail.com>
2026-04-19 04:46:55 -0400

    feat(pieces): add delete/list subscriptions actions and new-subscriber-created trigger to Beehiiv

--- packages/pieces/community/beehiiv/src/index.ts
+++ packages/pieces/community/beehiiv/src/index.ts
@@ -1,12 +1,15 @@
 import { createPiece } from '@activepieces/pieces-framework';
 import { createSubscriptionAction } from './lib/actions/create-subscription.action';
 import { updateSubscriptionAction } from './lib/actions/update-subscription.action';
+import { deleteSubscriptionAction } from './lib/actions/delete-subscription.action';
+import { listSubscriptionsAction } from './lib/actions/list-subscriptions.action';
 import { addSubscriptionToAutomationAction } from './lib/actions/add-subscription-to-automation.action';
 import { listAutomationsAction } from './lib/actions/list-automations.action';
 import { listPostsAction } from './lib/actions/list-posts.action';
 import { newPostSentTrigger } from './lib/triggers/new-post-sent.trigger';
 import { userUnsubscribesTrigger } from './lib/triggers/user-unsubscribes.trigger';
 import { newSubscriptionConfirmedTrigger } from './lib/triggers/new-subscription-confirmed.trigger';
+import { newSubscriberCreatedTrigger } from './lib/triggers/new-subscriber-created.trigger';
 import { PieceCategory } from '@activepieces/shared';
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
 import { beehiivAuth } from './lib/common/auth';
@@ -17,11 +20,13 @@ export const beehiiv = createPiece({
 	auth: beehiivAuth,
 	minimumSupportedRelease: '0.36.1',
 	logoUrl: 'https://cdn.activepieces.com/pieces/beehiiv.png',
-	authors: ['onyedikachi-david', 'kishanprmr'],
+	authors: ['onyedikachi-david', 'kishanprmr', 'tosh2308'],
 	categories: [PieceCategory.MARKETING],
 	actions: [
 		createSubscriptionAction,
 		updateSubscriptionAction,
+		deleteSubscriptionAction,
+		listSubscriptionsAction,
 		addSubscriptionToAutomationAction,
 		listAutomationsAction,
 		listPostsAction,
@@ -35,5 +40,5 @@ export const beehiiv = createPiece({
 			},
 		}),
 	],
-	triggers: [newPostSentTrigger, userUnsubscribesTrigger, newSubscriptionConfirmedTrigger],
+	triggers: [newPostSentTrigger, userUnsubscribesTrigger, newSubscriptionConfirmedTrigger, newSubscriberCreatedTrigger],
 });
