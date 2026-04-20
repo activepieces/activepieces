404aa4a87fbbe44ced885275a9b9c9f37d1ed9b2
MrChaker <mr.chakerw@gmail.com>
2026-04-20 17:13:17 +0100

    chore: add GIT_SYNC_ENABLED env var for cloud

--- packages/server/api/src/app/helper/system/system.ts
+++ packages/server/api/src/app/helper/system/system.ts
@@ -66,6 +66,7 @@ const systemPropDefaultValues: Partial<Record<SystemProp, string>> = {
     [AppSystemProp.ISSUE_ARCHIVE_DAYS]: '7',
     [AppSystemProp.POSTGRES_IDLE_TIMEOUT_MS]: '300000',
     [AppSystemProp.SCIM_DEFAULT_PROJECT_ROLE]: DefaultProjectRole.EDITOR,
+    [AppSystemProp.GIT_SYNC_ENABLED]: 'true',
 }
 
 let globalLogger: FastifyBaseLogger
