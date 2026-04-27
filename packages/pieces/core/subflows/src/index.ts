234cd8223793dfdb6debce2f934c622cb59237c0
Amr Elmohamady <amr.osama.elmohamady@gmail.com>
2026-04-27 08:34:50 +0000

    fix(subflows): update minimumSupportedRelease to 0.82.2 in configuration

--- packages/pieces/core/subflows/src/index.ts
+++ packages/pieces/core/subflows/src/index.ts
@@ -8,7 +8,7 @@ export const flows = createPiece({
   displayName: 'Subflow',
   description: 'Call another flow and optionally wait for a response.',
   auth: PieceAuth.None(),
-  minimumSupportedRelease: '0.82.0',
+  minimumSupportedRelease: '0.82.2',
   categories: [PieceCategory.CORE, PieceCategory.FLOW_CONTROL],
   logoUrl: 'https://cdn.activepieces.com/pieces/new-core/subflows.svg',
   authors: ['hazemadelkhalel'],
