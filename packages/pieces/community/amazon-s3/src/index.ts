2b34970ce4cfd1ec19c38c6bad60209aa09e7ce5
Kishan Parmar <code.kishanparmar@gmail.com>
2026-04-27 08:59:21 +0000

    chore(piece-amazon-s3): remove unused import and description, bump to 0.5.8

--- packages/pieces/community/amazon-s3/src/index.ts
+++ packages/pieces/community/amazon-s3/src/index.ts
@@ -6,7 +6,6 @@ import {
 import { PieceCategory } from '@activepieces/shared';
 import { readFile } from './lib/actions/read-file';
 import { amazons3UploadFile } from './lib/actions/upload-file';
-import { createS3 } from './lib/common';
 import { newFile } from './lib/triggers/new-file';
 import { generateSignedUrl } from './lib/actions/generate-signed-url';
 import { generateSignedUploadUrl } from './lib/actions/generate-signed-upload-url';
@@ -16,21 +15,13 @@ import { listFiles } from './lib/actions/list-files';
 import { decryptPgpFile } from './lib/actions/decrypt-pgp-file';
 import { amazonS3Auth } from './lib/auth';
 
-const description = `
-This piece allows you to upload files to Amazon S3 or other S3 compatible services.
-
-Amazon S3 Settings:
-Regions: https://docs.aws.amazon.com/general/latest/gr/s3.html
-Endpoint: leave blank
-`;
-
 export const amazonS3 = createPiece({
   displayName: 'Amazon S3',
   description: 'Scalable storage in the cloud',
 
   logoUrl: 'https://cdn.activepieces.com/pieces/amazon-s3.png',
   minimumSupportedRelease: '0.30.0',
-  authors: ["Willianwg","kishanprmr","MoShizzle","AbdulTheActivePiecer","khaledmashaly","abuaboud", "Kevinyu-alan", "hugh-codes"],
+  authors: ["Willianwg", "kishanprmr", "MoShizzle", "AbdulTheActivePiecer", "khaledmashaly", "abuaboud", "Kevinyu-alan", "hugh-codes"],
   categories: [PieceCategory.DEVELOPER_TOOLS],
   auth: amazonS3Auth,
   actions: [amazons3UploadFile, readFile, generateSignedUrl, generateSignedUploadUrl, moveFile, deleteFile, listFiles, decryptPgpFile],
