b027f151333e727bbbeba8cc4086473ded386047
Mohammad AbuAboud <moabuaboud@gmail.com>
2026-05-06 20:56:16 +0200

    fix(lint): add missing trailing comma in admin-platform.service

--- packages/server/api/src/app/ee/platform/admin/admin-platform.service.ts
+++ packages/server/api/src/app/ee/platform/admin/admin-platform.service.ts
@@ -27,7 +27,7 @@ export const adminPlatformService = (log: FastifyBaseLogger) => ({
         const strategy = FlowRetryStrategy.FROM_FAILED_STEP
 
         let query = flowRunRepo().createQueryBuilder('flow_run').where({
-            id: In(runIds ?? [])
+            id: In(runIds ?? []),
         })
         if (!createdBefore) {
             query = query.andWhere('flow_run.created <= :createdBefore', {
