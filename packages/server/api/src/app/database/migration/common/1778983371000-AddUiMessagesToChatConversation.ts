97e12c6b762eb7c32a2bac60a4c0a4a11e002118
Hazem Adel <hazemadelkhalel@gmail.com>
2026-05-17 05:22:41 +0300

    fix(chat): add required migration properties (breaking, release)

--- packages/server/api/src/app/database/migration/common/1778983371000-AddUiMessagesToChatConversation.ts
+++ packages/server/api/src/app/database/migration/common/1778983371000-AddUiMessagesToChatConversation.ts
@@ -1,7 +1,11 @@
-import { MigrationInterface, QueryRunner } from 'typeorm'
+import { QueryRunner } from 'typeorm'
+import { Migration } from '../../migration'
 
-export class AddUiMessagesToChatConversation1778983371000 implements MigrationInterface {
+export class AddUiMessagesToChatConversation1778983371000 implements Migration {
     name = 'AddUiMessagesToChatConversation1778983371000'
+    breaking = false
+    release = '0.83.0'
+    transaction = true
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
