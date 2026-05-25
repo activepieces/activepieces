88533802dfe832766e704a6be6d0554763efd550
Harmatta <136149988+Harmatta@users.noreply.github.com>
2026-05-25 11:23:19 +0200

    feat(postmark): send email with template, get bounces and get delivery stats actions (#12109)
    Co-authored-by: Kishan Parmar <code.kishanparmar@gmail.com>

--- packages/pieces/community/postmark/src/index.ts
+++ packages/pieces/community/postmark/src/index.ts
@@ -1,24 +1,34 @@
-import { createPiece } from '@activepieces/pieces-framework';
-import { sendEmailAction } from './lib/actions/send-email';
-import { postmarkAuth } from './lib/auth';
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { createPiece } from '@activepieces/pieces-framework';
+import { PieceCategory } from '@activepieces/shared';
+
+import { getDeliveryStats } from './lib/actions/get-delivery-stats';
+import { getEmailBounces } from './lib/actions/get-bounces';
+import { sendEmail } from './lib/actions/send-email';
+import { sendEmailWithTemplate } from './lib/actions/send-email-with-template';
+import { postmarkAuth } from './lib/auth';
 
 export const postmark = createPiece({
   displayName: 'Postmark',
-  auth: postmarkAuth,
-  minimumSupportedRelease: '0.36.1',
+  description:
+    'Transactional email platform for sending email, templates, and retrieving delivery and bounce insights.',
+  minimumSupportedRelease: '0.30.0',
   logoUrl: 'https://cdn.activepieces.com/pieces/postmark.png',
-  authors: ['Angelebeats'],
-  actions: [sendEmailAction,
+  authors: ['Harmatta','Angelebeats'],
+  categories: [PieceCategory.COMMUNICATION, PieceCategory.MARKETING],
+  auth: postmarkAuth,
+  actions: [
+    sendEmail,
+    sendEmailWithTemplate,
+    getEmailBounces,
+    getDeliveryStats,
     createCustomApiCallAction({
-      auth:postmarkAuth,
-      baseUrl:()=>'https://api.postmarkapp.com',
-      authMapping:async (auth)=>{
-        return{
-          'X-Postmark-Server-Token':auth.secret_text
-        }
-      }
-    })
+      baseUrl: () => 'https://api.postmarkapp.com',
+      auth: postmarkAuth,
+      authMapping: async (auth) => ({
+        'X-Postmark-Server-Token': auth.secret_text,
+      }),
+    }),
   ],
   triggers: [],
 });
