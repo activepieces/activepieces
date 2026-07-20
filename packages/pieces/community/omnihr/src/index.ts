88ffd7d8938f642a93bfc80f4936879f8e19b608
Daniel Poon <17039704+danielpoonwj@users.noreply.github.com>
2026-07-20 21:48:59 +0800

    chore: register action and clean up index

--- packages/pieces/community/omnihr/src/index.ts
+++ packages/pieces/community/omnihr/src/index.ts
@@ -1,18 +1,12 @@
-import {
-  PieceAuth,
-  createPiece,
-  Property,
-} from '@activepieces/pieces-framework';
+import { createPiece } from '@activepieces/pieces-framework';
 import { createCustomApiCallAction } from '@activepieces/pieces-common';
 import { PieceCategory } from '@activepieces/pieces-framework';
 import { getEmployeeSystemId } from './lib/actions/get-employee-system-id';
 import { getEmployeeInfo } from './lib/actions/get-employee-info';
 import { getEmployeeOrganizationalChart } from './lib/actions/get-employee-organizational-chart';
 import { getDirectReports } from './lib/actions/get-direct-reports';
+import { generateReport } from './lib/actions/generate-report';
 import { omnihrAuth } from './lib/auth';
-
-const OMNIHR_API_BASE_URL = 'https://api.omnihr.co/api/';
-const markdown = 'Enter your OmniHR credentials to authenticate:';
 
 export const omnihr = createPiece({
   displayName: 'Omni HR',
@@ -21,25 +15,23 @@ export const omnihr = createPiece({
   auth: omnihrAuth,
   minimumSupportedRelease: '0.36.1',
   logoUrl: 'https://cdn.activepieces.com/pieces/omnihr.png',
-  authors: ['arinmak'],
+  authors: ['arinmak', 'danielpoonwj'],
   categories: [PieceCategory.HUMAN_RESOURCES],
   actions: [
     getEmployeeSystemId,
     getEmployeeInfo,
     getEmployeeOrganizationalChart,
     getDirectReports,
+    generateReport,
     createCustomApiCallAction({
-      baseUrl: () => OMNIHR_API_BASE_URL,
+      baseUrl: () => 'https://api.omnihr.co/api/v1/',
       auth: omnihrAuth,
       authMapping: async (auth) => {
         const headers: Record<string, string> = {
           Authorization: `Bearer ${auth.access_token}`,
           'Content-Type': 'application/json',
+          Origin: auth.props.origin,
         };
-
-        if (auth.props.origin) {
-          headers['Origin'] = auth.props.origin;
-        }
 
         return headers;
       },
