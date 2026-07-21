e7152ef666c619963cea68cb6eae1c036c154fac
MrChaker <mr.chakerw@gmail.com>
2026-07-21 11:35:38 +0100

    chore: set AddActiveFlowsLimitToProjectPlan migration release to 0.86.4

--- packages/server/api/src/app/database/migration/postgres/1814000000000-AddActiveFlowsLimitToProjectPlan.ts
+++ packages/server/api/src/app/database/migration/postgres/1814000000000-AddActiveFlowsLimitToProjectPlan.ts
@@ -4,7 +4,7 @@ import { Migration } from '../../migration'
 export class AddActiveFlowsLimitToProjectPlan1814000000000 implements Migration {
     name = 'AddActiveFlowsLimitToProjectPlan1814000000000'
     breaking = false
-    release = '0.86.3'
+    release = '0.86.4'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
