aa72f147e42445a5c7fd699bfd8a9e3ad3ce14c5
Hazem Adel <hazemadelkhalel@gmail.com>
2026-09-07 21:47:46 +0300

    chore: remove code nothing references (#15337)

--- packages/core/formula/src/lib/function-implementations.ts
+++ packages/core/formula/src/lib/function-implementations.ts
@@ -449,9 +449,3 @@ function toNumericFieldValues(list: unknown, field: unknown): number[] {
     return result
 }
 
-export const MONTH_NAMES = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December',
-]
-
-export const DAY_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
