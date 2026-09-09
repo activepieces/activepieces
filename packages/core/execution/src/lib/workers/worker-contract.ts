1545131542e55ee23add6ac81ffcd05645c9e938
AbdulTheActivePiecer <abdulyki@activepieces.com>
2026-09-09 22:54:43 +0300

    chore: park pr3b work in progress

--- packages/core/execution/src/lib/workers/worker-contract.ts
+++ packages/core/execution/src/lib/workers/worker-contract.ts
@@ -431,8 +431,7 @@ export type SaveFlowStepFileRequest = {
     platformId: string
     flowRunId: string
     data: Buffer
-    mediaType: string
-    fileName?: string
+    fileName: string
 }
 
 export type SaveFlowStepFileResponse = {
