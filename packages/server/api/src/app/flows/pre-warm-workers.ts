5b53eae18e60c4df34a2ab0fe4e5b6acead95e6b
MrChaker <mr.chakerw@gmail.com>
2026-09-14 16:36:10 +0100

    fix: lint

--- packages/server/api/src/app/flows/pre-warm-workers.ts
+++ packages/server/api/src/app/flows/pre-warm-workers.ts
@@ -148,7 +148,7 @@ function extractCodeSteps(flowVersion: FlowVersion): PrewarmCodeStep[] {
             sourceCode: step.settings.sourceCode,
             flowVersionId: flowVersion.id,
             flowVersionState: flowVersion.state,
-            useDeno: step.settings.useDeno ?? false
+            useDeno: step.settings.useDeno ?? false,
         }))
 }
 
