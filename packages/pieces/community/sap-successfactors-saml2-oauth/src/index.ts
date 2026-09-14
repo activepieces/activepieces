1765aa5cccdf35817f17421bab2a86ea3a1052cd
kevinyang-ctrl <kevin.yang@pipefy.com>
2026-09-14 17:03:43 -0300

    feat(pieces): add SAP SuccessFactors SAML2 OAuth piece

---
+++ packages/pieces/community/sap-successfactors-saml2-oauth/src/index.ts
@@ -0,0 +1,32 @@
+import { createCustomApiCallAction } from '@activepieces/pieces-common';
+import { createPiece, PieceCategory } from '@activepieces/pieces-framework';
+import { getUser } from './lib/actions/get-user';
+import { successFactorsAuth } from './lib/auth';
+import { successFactorsHttp } from './lib/common/http';
+
+export const sapSuccessFactorsSaml2Oauth = createPiece({
+  displayName: 'SAP SuccessFactors SAML2 OAuth',
+  description:
+    'SAP SuccessFactors OData V2 integration using the OAuth 2.0 SAML 2.0 Bearer Assertion grant only.',
+  logoUrl: 'https://cdn.activepieces.com/pieces/sap-successfactors.png',
+  auth: successFactorsAuth,
+  minimumSupportedRelease: '0.87.0',
+  authors: ['kevinvyang-bit'],
+  categories: [PieceCategory.PRODUCTIVITY],
+  actions: [
+    getUser,
+    createCustomApiCallAction({
+      auth: successFactorsAuth,
+      baseUrl: (auth) =>
+        `${successFactorsHttp.normalizeBaseUrl({
+          value: auth?.props.api_url ?? '',
+          fieldName: 'OData API Base URL',
+        })}/odata/v2`,
+      authMapping: async (auth) => ({
+        Authorization: `Bearer ${auth.access_token}`,
+        Accept: 'application/json',
+      }),
+    }),
+  ],
+  triggers: [],
+});
